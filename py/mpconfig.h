c90c0f68a2f1ee8563497a5380a16e71d18d702a
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-01-04 01:57:00 +0200

    Move INT_FMT, etc. declaration into global mpconfig.h .
    This in particular makes it available for stm port.

--- py/mpconfig.h
+++ py/mpconfig.h
@@ -4,6 +4,20 @@
 
 #include <mpconfigport.h>
 
+#ifndef INT_FMT
+// printf format spec to use for machine_int_t and friends
+#ifdef __LP64__
+// Archs where machine_int_t == long, long != int
+#define UINT_FMT "%lu"
+#define INT_FMT "%ld"
+#else
+// Archs where machine_int_t == int
+#define UINT_FMT "%u"
+#define INT_FMT "%d"
+#endif
+#endif //INT_FMT
+
+
 // Any options not explicitly set in mpconfigport.h will get default
 // values below.
 
