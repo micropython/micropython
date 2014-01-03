1a1ba4d555146fe3bf0cd32906c77f5d918423ca
mux <freelancer.c@gmail.com>
2014-01-03 08:51:02 +0200

    Change dfu.py to be Python 2/3 compatible
    * Chane dfu.py to use Python 3 syntax to avoid dependency on Python 2.
    * Update Makefile to call python instead of python2
    * Fix #33

--- tools/dfu.py
+++ tools/dfu.py
@@ -20,11 +20,11 @@ def compute_crc(data):
   return 0xFFFFFFFF & -zlib.crc32(data) -1
 
 def parse(file,dump_images=False):
-  print 'File: "%s"' % file
+  print ('File: "%s"' % file)
   data = open(file,'rb').read()
   crc = compute_crc(data[:-4])
   prefix, data = consume('<5sBIB',data,'signature version size targets')
-  print '%(signature)s v%(version)d, image size: %(size)d, targets: %(targets)d' % prefix
+  print ('%(signature)s v%(version)d, image size: %(size)d, targets: %(targets)d' % prefix)
   for t in range(prefix['targets']):
     tprefix, data  = consume('<6sBI255s2I',data,'signature altsetting named name size elements')
     tprefix['num'] = t
@@ -32,40 +32,40 @@ def parse(file,dump_images=False):
       tprefix['name'] = cstring(tprefix['name'])
     else:
       tprefix['name'] = ''
-    print '%(signature)s %(num)d, alt setting: %(altsetting)s, name: "%(name)s", size: %(size)d, elements: %(elements)d' % tprefix
+    print ('%(signature)s %(num)d, alt setting: %(altsetting)s, name: "%(name)s", size: %(size)d, elements: %(elements)d' % tprefix)
     tsize = tprefix['size']
     target, data = data[:tsize], data[tsize:]
     for e in range(tprefix['elements']):
       eprefix, target = consume('<2I',target,'address size')
       eprefix['num'] = e
-      print '  %(num)d, address: 0x%(address)08x, size: %(size)d' % eprefix
+      print ('  %(num)d, address: 0x%(address)08x, size: %(size)d' % eprefix)
       esize = eprefix['size']
       image, target = target[:esize], target[esize:]
       if dump_images:
         out = '%s.target%d.image%d.bin' % (file,t,e)
         open(out,'wb').write(image)
-        print '    DUMPED IMAGE TO "%s"' % out
+        print ('    DUMPED IMAGE TO "%s"' % out)
     if len(target):
-      print "target %d: PARSE ERROR" % t
+      print ("target %d: PARSE ERROR" % t)
   suffix = named(struct.unpack('<4H3sBI',data[:16]),'device product vendor dfu ufd len crc')
-  print 'usb: %(vendor)04x:%(product)04x, device: 0x%(device)04x, dfu: 0x%(dfu)04x, %(ufd)s, %(len)d, 0x%(crc)08x' % suffix
+  print ('usb: %(vendor)04x:%(product)04x, device: 0x%(device)04x, dfu: 0x%(dfu)04x, %(ufd)s, %(len)d, 0x%(crc)08x' % suffix)
   if crc != suffix['crc']:
-    print "CRC ERROR: computed crc32 is 0x%08x" % crc
+    print ("CRC ERROR: computed crc32 is 0x%08x" % crc)
   data = data[16:]
   if data:
-    print "PARSE ERROR"
+    print ("PARSE ERROR")
 
 def build(file,targets,device=DEFAULT_DEVICE):
-  data = ''
+  data = b''
   for t,target in enumerate(targets):
-    tdata = ''
+    tdata = b''
     for image in target:
       tdata += struct.pack('<2I',image['address'],len(image['data']))+image['data']
-    tdata = struct.pack('<6sBI255s2I','Target',0,1,'ST...',len(tdata),len(target)) + tdata
+    tdata = struct.pack('<6sBI255s2I',b'Target',0,1, b'ST...',len(tdata),len(target)) + tdata
     data += tdata
-  data  = struct.pack('<5sBIB','DfuSe',1,len(data)+11,len(targets)) + data
+  data  = struct.pack('<5sBIB',b'DfuSe',1,len(data)+11,len(targets)) + data
   v,d=map(lambda x: int(x,0) & 0xFFFF, device.split(':',1))
-  data += struct.pack('<4H3sB',0,d,v,0x011a,'UFD',16)
+  data += struct.pack('<4H3sB',0,d,v,0x011a,b'UFD',16)
   crc   = compute_crc(data)
   data += struct.pack('<I',crc)
   open(file,'wb').write(data)
@@ -89,15 +89,15 @@ if __name__=="__main__":
       try:
         address,binfile = arg.split(':',1)
       except ValueError:
-        print "Address:file couple '%s' invalid." % arg
+        print ("Address:file couple '%s' invalid." % arg)
         sys.exit(1)
       try:
         address = int(address,0) & 0xFFFFFFFF
       except ValueError:
-        print "Address %s invalid." % address
+        print ("Address %s invalid." % address)
         sys.exit(1)
       if not os.path.isfile(binfile):
-        print "Unreadable file '%s'." % binfile
+        print ("Unreadable file '%s'." % binfile)
         sys.exit(1)
       target.append({ 'address': address, 'data': open(binfile,'rb').read() })
     outfile = args[0]
@@ -107,13 +107,13 @@ if __name__=="__main__":
     try:
       v,d=map(lambda x: int(x,0) & 0xFFFF, device.split(':',1))
     except:
-      print "Invalid device '%s'." % device
+      print ("Invalid device '%s'." % device)
       sys.exit(1)
     build(outfile,[target],device)
   elif len(args)==1:
     infile = args[0]
     if not os.path.isfile(infile):
-      print "Unreadable file '%s'." % infile
+      print ("Unreadable file '%s'." % infile)
       sys.exit(1)
     parse(infile, dump_images=options.dump_images)
   else:
