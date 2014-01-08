d944a66ead38b15b5a3c7a4ae8b054451fd5c077
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-01-08 22:41:22 +0200

    Add string comparison tests.
    Use computed string to exercise string interning code better.

--- tests/basics/tests/string1.py
+++ tests/basics/tests/string1.py
@@ -10,3 +10,10 @@ print('123' + "456")
 
 # iter
 print(list('str'))
+
+print('123' + '789' == '123789')
+print('a' + 'b' != 'a' + 'b ')
+
+# Not implemented so far
+# print('1' + '2' > '2')
+# print('1' + '2' < '2')
