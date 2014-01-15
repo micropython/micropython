6f9c03676bdf66274c199d296e98e7027324d22e
Dave Hylands <dhylands@gmail.com>
2014-01-14 22:23:56 -0800

    Updated teensy to work with latest on master
    Added analogRead, analogWriteXxx and servo support for teensy.

--- teensy/lexerfatfs.c
+++ teensy/lexerfatfs.c
@@ -2,11 +2,13 @@
 #include <stdio.h>
 
 #include "misc.h"
+#include "mpconfig.h"
+#include "qstr.h"
 #include "lexer.h"
 typedef int FIL;
 #include "../stm/lexerfatfs.h"
 
-mp_lexer_t *mp_lexer_new_from_file(const char *filename, mp_lexer_file_buf_t *fb) {
+mp_lexer_t *mp_lexer_new_from_file(const char *filename) {
   printf("import not implemented\n");
   return NULL;
 }
@@ -14,4 +16,9 @@ mp_lexer_t *mp_lexer_new_from_file(const char *filename, mp_lexer_file_buf_t *fb
 mp_lexer_t *mp_import_open_file(qstr mod_name) {
     printf("import not implemented\n");
     return NULL;
+}
+
+mp_import_stat_t mp_import_stat(const char *path) {
+    // TODO implement me!
+    return MP_IMPORT_STAT_NO_EXIST;
 }
