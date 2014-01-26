14d28be344702b98f2694e245ed4a00c86f898c2
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-01-27 01:01:37 +0200

    gen.send(): Throw StopIteration. Also, explicitly shutdown finished gen.
    Otherwise, some generator statements still may be spuriously executed on
    subsequent calls to next()/send().

--- tests/basics/generator_send.py
+++ tests/basics/generator_send.py
@@ -13,3 +13,25 @@ except TypeError:
 print(g.send(None))
 print(g.send(100))
 print(g.send(200))
+
+
+def f2():
+    print("entering")
+    for i in range(3):
+        print(i)
+        yield
+    print("returning 1")
+    print("returning 2")
+
+g = f2()
+g.send(None)
+g.send(1)
+g.send(1)
+try:
+    g.send(1)
+except StopIteration:
+    print("caught")
+try:
+    g.send(1)
+except StopIteration:
+    print("caught")
