9b00dad7bb0125a3459ca4f9c939c7510bd2f77f
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-01-27 09:05:50 +0200

    long int: Implement more operations.

--- py/objint.h
+++ py/objint.h
@@ -6,4 +6,5 @@ typedef struct _mp_obj_int_t {
 } mp_obj_int_t;
 
 void int_print(void (*print)(void *env, const char *fmt, ...), void *env, mp_obj_t self_in, mp_print_kind_t kind);
+mp_obj_t int_unary_op(int op, mp_obj_t o_in);
 mp_obj_t int_binary_op(int op, mp_obj_t lhs_in, mp_obj_t rhs_in);
