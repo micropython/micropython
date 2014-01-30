382e8eeea23db94f2b636d993c5a10467ea19643
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-01-30 13:49:18 +0200

    vm: Add basic implementation of END_FINALLY opcode.
    Allows to have nested try blocks with except filters.
    
    TODO: Don't add END_FINALLY's exception re-raise points to traceback.

--- tests/basics/try2.py
+++ tests/basics/try2.py
@@ -10,3 +10,14 @@ try:
     bar()
 except:
     print("except 1")
+
+try:
+    print("try 1")
+    try:
+        print("try 2")
+        foo()
+    except TypeError:
+        print("except 2")
+    bar()
+except NameError:
+    print("except 1")
