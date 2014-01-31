c92a56dc937dc414139e2bff958190cfb18de5d9
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-01-31 17:05:07 +0200

    Add testcase with exception handler spread across functions.

--- tests/basics/try2.py
+++ tests/basics/try2.py
@@ -21,3 +21,20 @@ try:
     bar()
 except NameError:
     print("except 1")
+
+# Check that exceptions across function boundaries work as expected
+def func1():
+    try:
+        print("try func1")
+        func2()
+    except NameError:
+        print("except func1")
+
+def func2():
+    try:
+        print("try func2")
+        foo()
+    except TypeError:
+        print("except func2")
+
+func1()
