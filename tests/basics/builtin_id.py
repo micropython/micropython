00c0b8a989ae54f4466d15b78a2e8729baf441ef
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-02-04 00:50:22 +0200

    Fix unstable case in builtin_id.py test.

--- tests/basics/builtin_id.py
+++ tests/basics/builtin_id.py
@@ -1,6 +1,7 @@
 print(id(1) == id(2))
 print(id(None) == id(None))
-print(id([]) == id([]))
+# This can't be true per Python semantics, just CPython implementation detail
+#print(id([]) == id([]))
 
 l = [1, 2]
 print(id(l) == id(l))
