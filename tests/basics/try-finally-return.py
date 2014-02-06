3c95ba7e4eda0762d735503b718119e361eb7295
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-02-06 03:20:56 +0200

    Add additional testcase for finally/return.

--- tests/basics/try-finally-return.py
+++ tests/basics/try-finally-return.py
@@ -5,3 +5,19 @@ def func1():
         print("finally 1")
 
 print(func1())
+
+
+def func2():
+    try:
+        return "it worked"
+    finally:
+        print("finally 2")
+
+def func3():
+    try:
+        s = func2()
+        return s + ", did this work?"
+    finally:
+        print("finally 3")
+
+print(func3())
