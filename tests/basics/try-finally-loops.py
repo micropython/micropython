9fce77c993fce3690f6f6ce6a5c1097e6c74f448
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-02-06 03:27:39 +0200

    Add more finally + break/continue testcases.

---
+++ tests/basics/try-finally-loops.py
@@ -0,0 +1,35 @@
+# Test various loop types, some may be implemented/optimized differently
+while True:
+    try:
+        break
+    finally:
+        print('finally 1')
+
+
+for i in [1, 5, 10]:
+    try:
+        continue
+    finally:
+        print('finally 2')
+
+for i in range(3):
+    try:
+        continue
+    finally:
+        print('finally 3')
+
+# Multi-level
+for i in range(4):
+    print(i)
+    try:
+        while True:
+            try:
+                try:
+                    break
+                finally:
+                    print('finally 1')
+            finally:
+                print('finally 2')
+        print('here')
+    finally:
+        print('finnaly 3')
