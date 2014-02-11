8d90a382cf4124be6db3c0b187694dde26229228
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-02-11 15:32:51 +0200

    basics/enumerate.py: Don't turn enumerate test into heap test.
    With range(10000), doesn't fit into 128K heap.

--- tests/basics/enumerate.py
+++ tests/basics/enumerate.py
@@ -2,5 +2,5 @@ print(list(enumerate([])))
 print(list(enumerate([1, 2, 3])))
 print(list(enumerate([1, 2, 3], 5)))
 print(list(enumerate([1, 2, 3], -5)))
-print(list(enumerate(range(10000))))
+print(list(enumerate(range(1000))))
 
