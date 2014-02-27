531edd0216045ad2b2c958bb8f7a63037a339e33
Gully Chen <deepgully@gmail.com>
2014-02-27 15:14:06 +0800

    fixed keyword value overridden by default
    def foo(a, b=3):
    print(a, b)
    
    foo(1, 333)            # it works,  print 1 333
    foo(1, b=333)        # wrong, print 1 3
    foo(a=2, b=333)    # wrong, print 2 3

--- py/objfun.c
+++ py/objfun.c
@@ -248,9 +248,9 @@ continue2:;
         // Now fill in defaults
         mp_obj_t *d = &flat_args[self->n_args - 1];
         mp_obj_t *s = &self->extra_args[self->n_def_args - 1];
-        for (int i = self->n_def_args; i > 0; i--) {
-            if (*d != MP_OBJ_NULL) {
-                *d-- = *s--;
+        for (int i = self->n_def_args; i > 0; i--, d--, s--) {
+            if (*d == MP_OBJ_NULL) {
+                *d = *s;
             }
         }
         DEBUG_printf("Args after filling defaults: ");
