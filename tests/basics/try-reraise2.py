010043caaf1bf791ab23315d7f5977472e312276
Damien George <damien.p.george@gmail.com>
2014-03-30 00:39:15 +0000

    Add "tracing" to try-reraise2.py test.  It now fails.

--- tests/basics/try-reraise2.py
+++ tests/basics/try-reraise2.py
@@ -4,16 +4,25 @@ def f():
         raise ValueError("val", 3)
     except:
         try:
+            print(1)
             raise TypeError
         except:
+            print(2)
             try:
+                print(3)
                 try:
+                    print(4)
                     raise AttributeError
                 except:
+                    print(5)
                     pass
+                print(6)
                 raise
             except TypeError:
+                print(7)
                 pass
+            print(8)
+        print(9)
         # This should raise original ValueError, not the most recently occurred AttributeError
         raise
 
