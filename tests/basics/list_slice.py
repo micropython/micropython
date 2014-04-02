10e21b977091703dbb22f839465902e91fcc6ac3
Damien George <damien.p.george@gmail.com>
2014-04-02 14:23:04 +0100

    Add more tests.

---
+++ tests/basics/list_slice.py
@@ -0,0 +1,18 @@
+# test slices; only 2 argument version supported by Micro Python at the moment
+x = list(range(10))
+a = 2
+b = 4
+c = 3
+print(x[:])
+print(x[::])
+#print(x[::c])
+print(x[:b])
+print(x[:b:])
+#print(x[:b:c])
+print(x[a])
+print(x[a:])
+print(x[a::])
+#print(x[a::c])
+print(x[a:b])
+print(x[a:b:])
+#print(x[a:b:c])
