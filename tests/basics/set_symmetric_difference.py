4d2238d2cedafd9eb04fe01986df34bc099e80e4
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-04-07 03:06:39 +0300

    tests: Fix more tests which depend on order of elements in set.

--- tests/basics/set_symmetric_difference.py
+++ tests/basics/set_symmetric_difference.py
@@ -1,5 +1,5 @@
-print({1,2}.symmetric_difference({2,3}))
-print({1,2}.symmetric_difference([2,3]))
+print(sorted({1,2}.symmetric_difference({2,3})))
+print(sorted({1,2}.symmetric_difference([2,3])))
 s = {1,2}
 print(s.symmetric_difference_update({2,3}))
 l = list(s)
