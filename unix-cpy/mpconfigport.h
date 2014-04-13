674f9887ce54b0627758f2a84c52e8aab0d7f2db
Damien George <damien.p.george@gmail.com>
2014-04-13 13:19:28 +0100

    bare-arm/unix-cpy: Minor changes.

--- unix-cpy/mpconfigport.h
+++ unix-cpy/mpconfigport.h
@@ -22,5 +22,3 @@ typedef unsigned int machine_uint_t; // must be pointer size
 typedef void *machine_ptr_t; // must be of pointer size
 typedef const void *machine_const_ptr_t; // must be of pointer size
 typedef double machine_float_t;
-
-machine_float_t machine_sqrt(machine_float_t x);
