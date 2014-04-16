571d5a3363f66b80d8983050168bc06d70357341
Andrew Scheller <github@loowis.durge.org>
2014-04-16 01:46:01 +0100

    New config option MICROPY_FORCE_32BIT (defaulted to 0)
    Makes it easier for 64-bit unix hosts to build 32-bit unix
    binaries (for testing)

--- unix/mpconfigport.mk
+++ unix/mpconfigport.mk
@@ -1,5 +1,8 @@
 # Enable/disable modules and 3rd-party libs to be included in interpreter
 
+# Build 32-bit binaries on a 64-bit host
+MICROPY_FORCE_32BIT = 0
+
 # Linking with GNU readline causes binary to be licensed under GPL
 MICROPY_USE_READLINE = 1
 
