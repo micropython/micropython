1b997d52441287727ccc61d7541043549d5908ee
Andrew Scheller <github@loowis.durge.org>
2014-04-16 03:28:40 +0100

    run-tests can now skip certain tests when run under Travis CI
    See the `skip_travis_tests` variable. Fixes #495
    (also tidied up usage of os.path.basename() function)

--- tests/basics/memoryerror.py
+++ tests/basics/memoryerror.py
@@ -1,11 +1,6 @@
-# this test for MemoryError can be difficult to reproduce
-# on different machine configurations (notably Travis CI)
-# so we disable it
-# TODO is there a way of testing that we are on Travis CI?
-if False:
-    l = list(range(10000))
-    try:
-        100000000 * l
-    except MemoryError:
-        print('MemoryError')
-    print(len(l), l[0], l[-1])
+l = list(range(10000))
+try:
+    100000000 * l
+except MemoryError:
+    print('MemoryError')
+print(len(l), l[0], l[-1])
