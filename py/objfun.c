f26a30710c77299a715318bc487e1874d1bf9984
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-04-17 05:44:51 +0300

    objfun: Add local header.
    This follows pattern already used for objtuple, etc.: objfun.h's content
    is not public - each and every piece of code should not have access to it.
    It's not private either - with out architecture and implementation language
    (C) it doesn't make sense to keep implementation of each object strictly
    private and maintain cumbersome accessors. It's "local" - intended to be
    used by a small set of "friend" (in C++ terms) objects.

--- py/objfun.c
+++ py/objfun.c
@@ -9,6 +9,7 @@
 #include "qstr.h"
 #include "obj.h"
 #include "objtuple.h"
+#include "objfun.h"
 #include "runtime0.h"
 #include "runtime.h"
 #include "bc.h"
@@ -149,18 +150,6 @@ mp_obj_t mp_make_function_var_between(int n_args_min, int n_args_max, mp_fun_var
 
 /******************************************************************************/
 /* byte code functions                                                        */
-
-typedef struct _mp_obj_fun_bc_t {
-    mp_obj_base_t base;
-    mp_obj_dict_t *globals; // the context within which this function was defined
-    machine_uint_t n_args : 15;         // number of arguments this function takes
-    machine_uint_t n_def_args : 15;     // number of default arguments
-    machine_uint_t takes_var_args : 1;  // set if this function takes variable args
-    machine_uint_t takes_kw_args : 1;   // set if this function takes keyword args
-    const byte *bytecode;   // bytecode for the function
-    qstr *args;             // argument names (needed to resolve positional args passed as keywords)
-    mp_obj_t extra_args[];  // values of default args (if any), plus a slot at the end for var args and/or kw args (if it takes them)
-} mp_obj_fun_bc_t;
 
 #if DEBUG_PRINT
 STATIC void dump_args(const mp_obj_t *a, int sz) {
