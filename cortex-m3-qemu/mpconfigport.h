ee857853d60a135685c5088e5492dfe6e8a5acb0
Ilya Dmitrichenko <ilya@xively.com>
2014-04-21 21:00:23 +0100

    cortex-m3-qemu: refactor the port.
    Switch from CodeSourcery to ARM GCC and clean-up some stale files,
    also copy `main.c` and `mpconfigport.h` from bare-arm.

--- cortex-m3-qemu/mpconfigport.h
+++ cortex-m3-qemu/mpconfigport.h
@@ -2,22 +2,18 @@
 
 // options to control how Micro Python is built
 
+#define MICROPY_EMIT_X64            (0)
 #define MICROPY_EMIT_THUMB          (0)
 #define MICROPY_EMIT_INLINE_THUMB   (0)
 #define MICROPY_MEM_STATS           (0)
 #define MICROPY_DEBUG_PRINTERS      (0)
 #define MICROPY_ENABLE_GC           (0)
-#define MICROPY_ENABLE_FINALISER    (0)
-#define MICROPY_ENABLE_REPL_HELPERS (1)
-#define MICROPY_LONGINT_IMPL        (MICROPY_LONGINT_IMPL_MPZ)
-#define MICROPY_FLOAT_IMPL          (MICROPY_FLOAT_IMPL_FLOAT)
-#define MICROPY_PATH_MAX            (128)
-#define MICROPY_ENABLE_MOD_IO       (0)
-
-// extra built in names to add to the global namespace
-extern const struct _mp_obj_fun_native_t mp_builtin_help_obj;
-#define MICROPY_EXTRA_BUILTINS \
-    { MP_OBJ_NEW_QSTR(MP_QSTR_help), (mp_obj_t)&mp_builtin_help_obj },
+#define MICROPY_ENABLE_REPL_HELPERS (0)
+#define MICROPY_ENABLE_LEXER_UNIX   (0)
+#define MICROPY_ENABLE_SOURCE_LINE  (0)
+#define MICROPY_LONGINT_IMPL        (MICROPY_LONGINT_IMPL_NONE)
+#define MICROPY_FLOAT_IMPL          (MICROPY_FLOAT_IMPL_NONE)
+#define MICROPY_PATH_MAX            (512)
 
 // type definitions for the specific machine
 
@@ -30,4 +26,9 @@ typedef int32_t machine_int_t; // must be pointer size
 typedef uint32_t machine_uint_t; // must be pointer size
 typedef void *machine_ptr_t; // must be of pointer size
 typedef const void *machine_const_ptr_t; // must be of pointer size
+
+// extra built in names to add to the global namespace
+extern const struct _mp_obj_fun_native_t mp_builtin_open_obj;
+#define MICROPY_EXTRA_BUILTINS \
+    { MP_OBJ_NEW_QSTR(MP_QSTR_open), (mp_obj_t)&mp_builtin_open_obj },
 
