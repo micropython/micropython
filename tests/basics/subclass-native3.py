e276753b458ef7c4c05469324173a455c0e2db46
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-05-02 02:30:28 +0300

    tests: Add testcases for catching user Exception subclasses.

--- tests/basics/subclass-native3.py
+++ tests/basics/subclass-native3.py
@@ -5,3 +5,18 @@ e = MyExc(100, "Some error")
 print(e)
 print(repr(e))
 print(e.args)
+
+try:
+    raise MyExc("Some error")
+except MyExc as e:
+    print("Caught exception:", repr(e))
+
+try:
+    raise MyExc("Some error2")
+except Exception as e:
+    print("Caught exception:", repr(e))
+
+try:
+    raise MyExc("Some error2")
+except:
+    print("Caught user exception")
