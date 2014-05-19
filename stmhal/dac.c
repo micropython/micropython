eee31288dd9cc81a648dc033b320c6ccaf803a71
Damien George <damien.p.george@gmail.com>
2014-05-19 19:08:12 +0100

    stmhal: Fix DAC documentation: need to convert float to int for buf.

--- stmhal/dac.c
+++ stmhal/dac.c
@@ -62,7 +62,7 @@
 ///     # create a buffer containing a sine-wave
 ///     buf = bytearray(100)
 ///     for i in range(len(buf)):
-///         buf[i] = 128 + 127 * math.sin(2 * math.pi * i / len(buf))
+///         buf[i] = 128 + int(127 * math.sin(2 * math.pi * i / len(buf)))
 ///
 ///     # output the sine-wave at 400Hz
 ///     dac = DAC(1)
