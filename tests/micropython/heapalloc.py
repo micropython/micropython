4d659f566f094a42057023dcc82a3ca8810c5ba6
Damien George <damien.p.george@gmail.com>
2014-05-31 18:33:16 +0100

    tests: Add feature test for when heap allocation is disabled.

---
+++ tests/micropython/heapalloc.py
@@ -0,0 +1,26 @@
+# check that we can do certain things without allocating heap memory
+
+import gc
+
+def f(a):
+    print(a)
+
+def g(a, b=2):
+    print(a, b)
+
+global_var = 1
+
+def h():
+    global global_var
+    global_var = 2      # set an existing global variable
+    for i in range(2):  # for loop
+        f(i)            # function call
+        f(i * 2 + 1)    # binary operation with small ints
+        f(a=i)          # keyword arguments
+        g(i)            # default arg (second one)
+        g(i, i)         # 2 args
+
+# call h with heap allocation disabled
+gc.disable()
+h()
+gc.enable()
