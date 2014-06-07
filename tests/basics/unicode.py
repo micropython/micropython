01bd68684611585d437982dccdf05b33cbedc630
Chris Angelico <rosuav@gmail.com>
2014-06-07 11:33:43 +1000

    Expand the Unicode tests

--- tests/basics/unicode.py
+++ tests/basics/unicode.py
@@ -1,3 +1,9 @@
+# Test a UTF-8 encoded literal
 s = "asdf©qwer"
 for i in range(len(s)):
-    print("s[%d]: %s"%(i, s[i]))
+    print("s[%d]: %s   %X"%(i, s[i], ord(s[i])))
+# Test all three forms of Unicode escape, and
+# all blocks of UTF-8 byte patterns
+s = "a\xA9\xFF\u0123\u0800\uFFEE\U0001F44C"
+for i in range(len(s)):
+    print("s[%d]: %s   %X"%(i, s[i], ord(s[i])))
