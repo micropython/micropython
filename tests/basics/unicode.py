616c24ac014c3ca56008428c506034dd1bfff7a8
Chris Angelico <rosuav@gmail.com>
2014-06-10 02:03:11 +1000

    Add tests of string slicing, which currently fail

--- tests/basics/unicode.py
+++ tests/basics/unicode.py
@@ -7,3 +7,7 @@ for i in range(len(s)):
 s = "a\xA9\xFF\u0123\u0800\uFFEE\U0001F44C"
 for i in range(-len(s), len(s)):
     print("s[%d]: %s   %X"%(i, s[i], ord(s[i])))
+    print("s[:%d]: %d chars, '%s'"%(i, len(s[:i]), s[:i]))
+    for j in range(i, len(s)):
+        print("s[%d:%d]: %d chars, '%s'"%(i, j, len(s[i:j]), s[i:j]))
+    print("s[%d:]: %d chars, '%s'"%(i, len(s[i:]), s[i:]))
