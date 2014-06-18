9acb5e4cf00c752f16a9606572ad440e2f545dbf
stijn <stinos@zoho.com>
2014-06-18 12:29:03 +0200

    gc: Turn off debugging info again

--- py/gc.c
+++ py/gc.c
@@ -40,7 +40,7 @@
 
 #if MICROPY_ENABLE_GC
 
-#if 1 // print debugging info
+#if 0 // print debugging info
 #define DEBUG_PRINT (1)
 #define DEBUG_printf DEBUG_printf
 #else // don't print debugging info
