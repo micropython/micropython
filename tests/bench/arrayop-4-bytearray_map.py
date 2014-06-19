59ced651b542941f893293099a932252e498eb7c
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-06-19 21:54:51 +0300

    bench: Add test for map() vs inplace operations in array-likes.
    map() is 5 times slower. That's mostly because of inefficiency of creating
    containers from iterables of unknown length (like map()).

---
+++ tests/bench/arrayop-4-bytearray_map.py
@@ -0,0 +1,12 @@
+# Array operation
+# Type: list, map() call. This method requires allocation of
+# the same amount of memory as original array (to hold result
+# array). On the other hand, input array stays intact.
+import bench
+
+def test(num):
+    for i in iter(range(num//10000)):
+        arr = bytearray(b"\0" * 1000)
+        arr2 = bytearray(map(lambda x: x + 1, arr))
+
+bench.run(test)
