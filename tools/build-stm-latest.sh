c737086e1c42f15addefc0c6c8b5c4ddcca97907
Damien George <damien.p.george@gmail.com>
2014-09-30 23:04:08 +0000

    tools: Add network build to auto-build script.

--- tools/build-stm-latest.sh
+++ tools/build-stm-latest.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# function for building firmware
+function do_build() {
+    descr=$1
+    board=$2
+    shift
+    shift
+    echo "building $descr $board"
+    build_dir=/tmp/stm-build-$board
+    make -B $@ BOARD=$board BUILD=$build_dir || exit 1
+    mv $build_dir/firmware.dfu $dest_dir/$descr-$date-$git_tag.dfu
+    rm -rf $build_dir
+}
+
 # check/get parameters
 if [ $# != 1 ]; then
     echo "usage: $0 <dest-dir>"
@@ -21,11 +34,7 @@ date=$(date '+%Y-%m-%d')
 git_tag="$(git describe --dirty || echo unknown)"
 
 # build the versions
-for board in PYBV3 PYBV10; do
-    echo $board
-    lower_board=$(echo $board | tr A-Z a-z)
-    build_dir=/tmp/stm-build-$board
-    make -B BOARD=$board BUILD=$build_dir || exit 1
-    mv $build_dir/firmware.dfu $dest_dir/$lower_board-$date-$git_tag.dfu
-    rm -rf $build_dir
-done
+do_build pybv3 PYBV3
+do_build pybv3-network PYBV3 MICROPY_PY_WIZNET5K=1 MICROPY_PY_CC3K=1
+do_build pybv10 PYBV10
+do_build pybv10-network PYBV10 MICROPY_PY_WIZNET5K=1 MICROPY_PY_CC3K=1
