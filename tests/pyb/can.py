bfac1c3a73c96bcbfb275e09430c18cb42ff1ea2
Henrik Sölver <henrik.solver@gmail.com>
2014-10-30 23:13:26 +0100

    Added and adapted CAN tests for extended messages

--- tests/pyb/can.py
+++ tests/pyb/can.py
@@ -1,9 +1,42 @@
 from pyb import CAN
 
-can = CAN(1)
+can = CAN(1, CAN.LOOPBACK)
 print(can)
-can.init(CAN.LOOPBACK)
 print(can.any(0))
 can.send('abcd', 123)
 print(can.any(0))
 print(can.recv(0))
+
+can.send('abcd', -1)
+print(can.recv(0))
+
+can.send('abcd', 0x7FF + 1)
+print(can.recv(0))
+
+#Test too long message
+try:
+    can.send('abcdefghi', 0x7FF)
+except ValueError:
+    print('passed')
+else:
+    print('failed')
+
+del can
+
+#Testing extended IDs
+can = CAN(1, CAN.LOOPBACK, extframe = True)
+print(can)
+
+try:
+    can.send('abcde', 0x7FF + 1)
+except ValueError:
+        print('failed')
+else:
+    r = can.recv(0)
+    if r[0] == 0x7FF+1 and r[3] == b'abcde':
+        print('passed')
+    else:
+        print('failed, wrong data received')
+
+
+print('end')
