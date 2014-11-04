0e3722137f1c2c5f4ee6c24829b1f46e62aedf2e
Damien George <damien.p.george@gmail.com>
2014-11-04 18:51:29 +0000

    docs: Update front page to display date of last update.

--- docs/conf.py
+++ docs/conf.py
@@ -143,7 +143,7 @@ html_theme_path = ['.']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
-#html_last_updated_fmt = '%b %d, %Y'
+html_last_updated_fmt = '%d %b %Y'
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
