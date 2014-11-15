5228854f0e026838c21a1e603dab77bb61d2fada
Damien George <damien.p.george@gmail.com>
2014-11-15 21:33:07 +0000

    docs: Fix RTD build configuration.
    ReadTheDocs needs the root directory in its search path so it finds
    topindex.html.

--- docs/conf.py
+++ docs/conf.py
@@ -114,6 +114,8 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
     except:
         html_theme = 'default'
         html_theme_path = ['.']
+else:
+    html_theme_path = ['.']
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
