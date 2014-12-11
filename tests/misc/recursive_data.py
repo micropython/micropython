f42b3c7599d7777d057178da379b17249e118f0c
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-12-12 00:29:29 +0200

    tests: Activate recursive_data.py test, now that io.StringIO is available.

---
+++ tests/misc/recursive_data.py
@@ -0,0 +1,9 @@
+# This tests that printing recursive data structure doesn't lead to segfault.
+import _io as io
+
+l = [1, 2, 3, None]
+l[-1] = l
+try:
+    print(l, file=io.StringIO())
+except RuntimeError:
+    print("RuntimeError")
