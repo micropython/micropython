30d8a82220d8dd9b3b75f7476efcbe27336cf47b
Damien George <damien.p.george@gmail.com>
2015-01-12 22:41:55 +0000

    py: Allow to compile with -Wredundant-decls.

--- py/objcomplex.c
+++ py/objcomplex.c
@@ -48,8 +48,6 @@ typedef struct _mp_obj_complex_t {
     mp_float_t imag;
 } mp_obj_complex_t;
 
-mp_obj_t mp_obj_new_complex(mp_float_t real, mp_float_t imag);
-
 STATIC void complex_print(void (*print)(void *env, const char *fmt, ...), void *env, mp_obj_t o_in, mp_print_kind_t kind) {
     mp_obj_complex_t *o = o_in;
 #if MICROPY_FLOAT_IMPL == MICROPY_FLOAT_IMPL_FLOAT
