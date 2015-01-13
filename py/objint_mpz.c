0fb17f6ef46ade018a3fda8d4266f15638ddf5e3
David Steinberg <david.steinberg.dev@gmail.com>
2015-01-13 15:22:30 +0000

    py: Use float-to-int classifications for mp_obj_new_int_from_float() functions

--- py/objint_mpz.c
+++ py/objint_mpz.c
@@ -303,13 +303,15 @@ mp_obj_t mp_obj_new_int_from_float(mp_float_t val) {
         nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_OverflowError, "can't convert inf to int"));
     } else if (cl == FP_NAN) {
         nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_ValueError, "can't convert NaN to int"));
-    } else if (MICROPY_FLOAT_C_FUN(fabs)(val) < 10000) {
-        // temporary(?) fix for optimising case where int will be small int
-        return MP_OBJ_NEW_SMALL_INT(MICROPY_FLOAT_C_FUN(trunc)(val));
     } else {
-        mp_obj_int_t *o = mp_obj_int_new_mpz();
-        mpz_set_from_float(&o->mpz, val);
-        return o;
+        mp_fp_as_int_class_t icl = mp_classify_fp_as_int(val);
+        if (icl == MP_FP_CLASS_FIT_SMALLINT) {
+            return MP_OBJ_NEW_SMALL_INT((mp_int_t)val);
+        } else {
+            mp_obj_int_t *o = mp_obj_int_new_mpz();
+            mpz_set_from_float(&o->mpz, val);
+            return o;
+        }
     }
 }
 #endif
