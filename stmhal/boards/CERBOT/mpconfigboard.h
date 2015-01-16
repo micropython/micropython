a49d0f1ede40655a507c129f042e1e228d17e8a9
Dave Hylands <dhylands@gmail.com>
2015-01-16 00:03:38 -0800

    Added support for the CerBot.
    http://www.watterott.com/en/FEZ-Cerbot-Assembled
    
    although it looks like ghielectronics has discontinued this, so
    I'm not sure how many exist.

---
+++ stmhal/boards/CERBOT/mpconfigboard.h
@@ -0,0 +1,43 @@
+#define CERB40
+
+#define MICROPY_HW_BOARD_NAME       "Cerb40"
+#define MICROPY_HW_MCU_NAME         "STM32F405RG"
+#define MICROPY_PY_SYS_PLATFORM     "pyboard"
+
+#define MICROPY_HW_HAS_LED          (1)
+#define MICROPY_HW_HAS_SWITCH       (0)
+#define MICROPY_HW_HAS_SDCARD       (1)
+#define MICROPY_HW_HAS_MMA7660      (0)
+#define MICROPY_HW_HAS_LIS3DSH      (0)
+#define MICROPY_HW_HAS_LCD          (0)
+#define MICROPY_HW_ENABLE_RNG       (1)
+#define MICROPY_HW_ENABLE_RTC       (1)
+#define MICROPY_HW_ENABLE_TIMER     (1)
+#define MICROPY_HW_ENABLE_SERVO     (0)
+#define MICROPY_HW_ENABLE_DAC       (1)
+#define MICROPY_HW_ENABLE_I2C1      (1)
+#define MICROPY_HW_ENABLE_SPI1      (1)
+#define MICROPY_HW_ENABLE_SPI2      (0)
+#define MICROPY_HW_ENABLE_SPI3      (1)
+#define MICROPY_HW_ENABLE_CAN       (1)
+
+// USRSW has no pullup or pulldown, and pressing the switch makes the input go low
+//#define MICROPY_HW_USRSW_PIN        (pin_B3)
+//#define MICROPY_HW_USRSW_PULL       (GPIO_PULLUP)
+//#define MICROPY_HW_USRSW_EXTI_MODE  (GPIO_MODE_IT_FALLING)
+//#define MICROPY_HW_USRSW_PRESSED    (0)
+
+// The CerBot has a single LED (on a gpio pin)
+#define MICROPY_HW_LED1             (pin_A14) // red
+#define MICROPY_HW_LED_OTYPE        (GPIO_MODE_OUTPUT_PP)
+#define MICROPY_HW_LED_ON(pin)      (pin->gpio->BSRRL = pin->pin_mask)
+#define MICROPY_HW_LED_OFF(pin)     (pin->gpio->BSRRH = pin->pin_mask)
+
+// SD card detect switch
+#define MICROPY_HW_SDCARD_DETECT_PIN        (pin_B15)
+#define MICROPY_HW_SDCARD_DETECT_PULL       (GPIO_PULLUP)
+#define MICROPY_HW_SDCARD_DETECT_PRESENT    (GPIO_PIN_RESET)
+
+// USB config
+//#define MICROPY_HW_USB_VBUS_DETECT_PIN (pin_A9)
+//#define MICROPY_HW_USB_OTG_ID_PIN      (pin_A10)
