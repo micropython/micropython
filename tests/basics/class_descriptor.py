5809cfa77cf2d2d7646e97842850282c71ff581d
stijn <stinos@zoho.com>
2015-02-14 18:43:54 +0100

    py: Add optional support for descriptors' __get__ and __set__ methods

---
+++ tests/basics/class_descriptor.py
@@ -0,0 +1,20 @@
+class Descriptor:
+    def __get__(self, obj, cls):
+        print(type(obj) is Main)
+        print(cls is Main)
+        return 'result'
+
+    def __set__(self, obj, val):
+        print(type(obj) is Main)
+        print(val)
+
+class Main:
+    Forward = Descriptor()
+
+m = Main()
+r = m.Forward
+if 'Descriptor' in repr(r.__class__):
+    print('SKIP')
+else:
+    print(r)
+    m.Forward = 'a'
