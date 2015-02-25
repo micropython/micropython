8a5aee103d0741efbbcf3fa7d0d0b3d2100eddb2
danicampora <danicampora@gmail.com>
2015-02-25 22:26:16 +0100

    cc3200: Fix md5 application signing, add missing accents.

--- cc3200/appsign.sh
+++ cc3200/appsign.sh
@@ -5,7 +5,7 @@
 BUILD=build/$1
 
 # Generate the MD5 hash
-echo -n md5sum --binary $BUILD/application.bin | awk '{ print $1 }' > __md5hash.bin
+echo -n `md5sum --binary $BUILD/application.bin | awk '{ print $1 }'` > __md5hash.bin
 
 # Concatenate it with the application binary
 cat $BUILD/application.bin __md5hash.bin > $BUILD/MCUIMG.BIN
