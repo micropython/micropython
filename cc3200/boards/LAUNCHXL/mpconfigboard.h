5dd8ae6b9cb52c59531b0fbe4114935d20b4c232
danicampora <danicampora@gmail.com>
2015-03-15 20:53:03 +0100

    cc3200: Fix spaces aligment in LAUNCHXL/mpconfigboard.h

--- cc3200/boards/LAUNCHXL/mpconfigboard.h
+++ cc3200/boards/LAUNCHXL/mpconfigboard.h
@@ -38,13 +38,13 @@
 #define MICROPY_STDIO_UART_BAUD                     115200
 
 #define MICROPY_SYS_LED_PRCM                        PRCM_GPIOA1
-#define MICROPY_SAFE_BOOT_PRCM                        PRCM_GPIOA2
+#define MICROPY_SAFE_BOOT_PRCM                      PRCM_GPIOA2
 #define MICROPY_SYS_LED_PORT                        GPIOA1_BASE
-#define MICROPY_SAFE_BOOT_PORT                        GPIOA2_BASE
+#define MICROPY_SAFE_BOOT_PORT                      GPIOA2_BASE
 #define MICROPY_SYS_LED_PIN_NUM                     PIN_64      // GPIO9
-#define MICROPY_SAFE_BOOT_PIN_NUM                     PIN_15      // GPIO22
+#define MICROPY_SAFE_BOOT_PIN_NUM                   PIN_15      // GPIO22
 #define MICROPY_SYS_LED_PORT_PIN                    GPIO_PIN_1
-#define MICROPY_SAFE_BOOT_PORT_PIN                    GPIO_PIN_6
+#define MICROPY_SAFE_BOOT_PORT_PIN                  GPIO_PIN_6
 
 #define MICROPY_PORT_SFLASH_BLOCK_COUNT             32
 
