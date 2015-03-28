1fd117bbb22aba574b22d69948a696503aca8b1f
Damien George <damien.p.george@gmail.com>
2015-03-28 21:45:40 +0000

    pic16bit: Initial version of port to 16-bit PIC family.
    Reference MCU is dsPIC33J256GP506 with 256k ROM and 8k RAM, on the dsPIC
    DSC Starter Kit board.  The REPL works, GC works, pyb module has LED and
    Switch objects.  It passes some tests from the test suite (most it can't
    run because it doesn't have the Python features enabled).

---
+++ pic16bit/pic16bit_mphal.h
@@ -0,0 +1,44 @@
+/*
+ * This file is part of the Micro Python project, http://micropython.org/
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Damien P. George
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#ifndef __MICROPY_INCLUDED_PIC16BIT_PIC16BIT_MPHAL_H__
+#define __MICROPY_INCLUDED_PIC16BIT_PIC16BIT_MPHAL_H__
+
+#define HAL_GetTick mp_hal_get_milliseconds
+
+#include "py/mpstate.h"
+
+void mp_hal_init(void);
+
+mp_uint_t mp_hal_get_milliseconds(void);
+void mp_hal_milli_delay(mp_uint_t ms);
+
+void mp_hal_set_interrupt_char(int c);
+int mp_hal_stdin_rx_chr(void);
+void mp_hal_stdout_tx_str(const char *str);
+void mp_hal_stdout_tx_strn(const char *str, mp_uint_t len);
+void mp_hal_stdout_tx_strn_cooked(const char *str, mp_uint_t len);
+
+#endif // __MICROPY_INCLUDED_PIC16BIT_PIC16BIT_MPHAL_H__
