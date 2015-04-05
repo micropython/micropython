2192824ad876dfc1376791c7fa9ac8650a74c11e
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2015-04-05 22:15:37 +0300

    teensy: Fix function prototype.

--- teensy/std.h
+++ teensy/std.h
@@ -10,7 +10,7 @@ void *memcpy(void *dest, const void *src, size_t n);
 void *memmove(void *dest, const void *src, size_t n);
 void *memset(void *s, int c, size_t n);
 
-int strlen(const char *str);
+size_t strlen(const char *str);
 int strcmp(const char *s1, const char *s2);
 int strncmp(const char *s1, const char *s2, size_t n);
 char *strcpy(char *dest, const char *src);
