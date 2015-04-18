90e6d0c2ac3dcda46d93b061802692444bd931a4
Damien George <damien.p.george@gmail.com>
2015-04-18 21:27:10 +0100

    stmhal: Add support for Espruino Pico board.
    To build:
    
    make BOARD=ESPRUINO_PICO
    
    To deploy: short the BOOT0/BTN contact on the back of the board (eg by
    drawing over it with a graphite pencil), then hold down BTN while
    inserting the board into the USB port.  The board should then enter DFU
    mode, and the firmware can be downloaded using:
    
    make BOARD=ESPRUINO_PICO deploy

---
+++ stmhal/boards/ESPRUINO_PICO/mpconfigboard.h
@@ -0,0 +1,50 @@
+#define MICROPY_HW_BOARD_NAME       "Espruino Pico"
+#define MICROPY_HW_MCU_NAME         "STM32F401CD"
+#define MICROPY_PY_SYS_PLATFORM     "pyboard"
+
+#define MICROPY_HW_HAS_SWITCH       (1)
+#define MICROPY_HW_HAS_SDCARD       (0)
+#define MICROPY_HW_HAS_MMA7660      (0)
+#define MICROPY_HW_HAS_LIS3DSH      (0)
+#define MICROPY_HW_HAS_LCD          (0)
+#define MICROPY_HW_ENABLE_RNG       (0)
+#define MICROPY_HW_ENABLE_RTC       (1)
+#define MICROPY_HW_ENABLE_TIMER     (1)
+#define MICROPY_HW_ENABLE_SERVO     (1)
+#define MICROPY_HW_ENABLE_DAC       (0)
+#define MICROPY_HW_ENABLE_SPI1      (1)
+#define MICROPY_HW_ENABLE_SPI2      (1)
+#define MICROPY_HW_ENABLE_SPI3      (0)
+#define MICROPY_HW_ENABLE_CAN       (0)
+
+// Pico has an 8 MHz HSE and the F401 does 84 MHz max
+#define MICROPY_HW_CLK_PLLM (5)
+#define MICROPY_HW_CLK_PLLN (210)
+#define MICROPY_HW_CLK_PLLP (RCC_PLLP_DIV4)
+#define MICROPY_HW_CLK_PLLQ (7)
+
+// does not have a 32kHz crystal
+#define MICROPY_HW_RTC_USE_LSE      (0)
+
+// I2C busses
+#define MICROPY_HW_I2C1_SCL (pin_B6)
+#define MICROPY_HW_I2C1_SDA (pin_B7)
+#define MICROPY_HW_I2C2_SCL (pin_B10)
+#define MICROPY_HW_I2C2_SDA (pin_B3)
+#define MICROPY_HW_I2C3_SCL (pin_A8)
+#define MICROPY_HW_I2C3_SDA (pin_B4)
+
+// BTN1 has no pullup or pulldown; it is active high and broken out on a header
+#define MICROPY_HW_USRSW_PIN        (pin_C13)
+#define MICROPY_HW_USRSW_PULL       (GPIO_PULLDOWN)
+#define MICROPY_HW_USRSW_EXTI_MODE  (GPIO_MODE_IT_RISING)
+#define MICROPY_HW_USRSW_PRESSED    (1)
+
+// Pico has 2 LEDs
+#define MICROPY_HW_LED1             (pin_B2)  // red
+#define MICROPY_HW_LED2             (pin_B12) // green
+#define MICROPY_HW_LED3             (pin_B12) // green
+#define MICROPY_HW_LED4             (pin_B12) // green
+#define MICROPY_HW_LED_OTYPE        (GPIO_MODE_OUTPUT_PP)
+#define MICROPY_HW_LED_ON(pin)      (pin->gpio->BSRRL = pin->pin_mask)
+#define MICROPY_HW_LED_OFF(pin)     (pin->gpio->BSRRH = pin->pin_mask)
---
+++ stmhal/boards/ESPRUINO_PICO/mpconfigboard.mk
@@ -0,0 +1,2 @@
+AF_FILE = boards/stm32f401_af.csv
+LD_FILE = boards/stm32f401.ld
