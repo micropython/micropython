ba25f27f0d1bec28820c828019c8882fd00cfacd
adminpete <peter@hinch.me.uk>
2015-04-19 10:16:08 +0100

    getlbl removed.

--- py/asmthumb.h
+++ py/asmthumb.h
@@ -213,7 +213,6 @@ static inline void asm_thumb_ldrh_rlo_rlo_i5(asm_thumb_t *as, uint rlo_dest, uin
 
 void asm_thumb_mov_reg_reg(asm_thumb_t *as, uint reg_dest, uint reg_src);
 void asm_thumb_mov_reg_i16(asm_thumb_t *as, uint mov_op, uint reg_dest, int i16_src);
-mp_int_t get_label_abs_addr(asm_thumb_t *as, uint label); // For getlbl
 
 // these return true if the destination is in range, false otherwise
 bool asm_thumb_b_n_label(asm_thumb_t *as, uint label);
@@ -230,4 +229,5 @@ void asm_thumb_mov_reg_local_addr(asm_thumb_t *as, uint rlo_dest, int local_num)
 void asm_thumb_b_label(asm_thumb_t *as, uint label); // convenience: picks narrow or wide branch
 void asm_thumb_bcc_label(asm_thumb_t *as, int cc, uint label); // convenience: picks narrow or wide branch
 void asm_thumb_bl_ind(asm_thumb_t *as, void *fun_ptr, uint fun_id, uint reg_temp); // convenience
+
 #endif // __MICROPY_INCLUDED_PY_ASMTHUMB_H__
