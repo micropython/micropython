0589c19d5239bb4456f06049925d71851e896d1e
Damien George <damien.p.george@gmail.com>
2015-05-08 00:18:28 +0100

    tests: Add test for machine module (mem* functions).

---
+++ tests/extmod/machine1.py
@@ -0,0 +1,33 @@
+# test machine module
+
+import machine
+import uctypes
+
+print(machine.mem8)
+
+buf = bytearray(8)
+addr = uctypes.addressof(buf)
+
+machine.mem8[addr] = 123
+print(machine.mem8[addr])
+
+machine.mem16[addr] = 12345
+print(machine.mem16[addr])
+
+machine.mem32[addr] = 123456789
+print(machine.mem32[addr])
+
+try:
+    machine.mem16[1]
+except ValueError:
+    print("ValueError")
+
+try:
+    machine.mem16[1] = 1
+except ValueError:
+    print("ValueError")
+
+try:
+    del machine.mem8[0]
+except TypeError:
+    print("TypeError")
