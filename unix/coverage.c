f601390ef899a3b87b92d5075ded98a9766fdd6b
Damien George <damien.p.george@gmail.com>
2015-05-12 23:34:10 +0100

    unix: Add some extra coverage tests for vstr and attrtuple.

--- unix/coverage.c
+++ unix/coverage.c
@@ -8,8 +8,38 @@
 
 // function to run extra tests for things that can't be checked by scripts
 STATIC mp_obj_t extra_coverage(void) {
+    // vstr
+    {
+        printf("# vstr\n");
+        vstr_t *vstr = vstr_new_size(16);
+        vstr_hint_size(vstr, 32);
+        vstr_add_str(vstr, "ts");
+        vstr_ins_byte(vstr, 1, 'e');
+        vstr_ins_char(vstr, 3, 't');
+        vstr_ins_char(vstr, 10, 's');
+        printf("%.*s\n", (int)vstr->len, vstr->buf);
+
+        vstr_cut_head_bytes(vstr, 2);
+        printf("%.*s\n", (int)vstr->len, vstr->buf);
+
+        vstr_cut_tail_bytes(vstr, 10);
+        printf("%.*s\n", (int)vstr->len, vstr->buf);
+
+        vstr_printf(vstr, "t%cst", 'e');
+        printf("%.*s\n", (int)vstr->len, vstr->buf);
+
+        vstr_cut_out_bytes(vstr, 3, 10);
+        printf("%.*s\n", (int)vstr->len, vstr->buf);
+
+        VSTR_FIXED(fix, 4);
+        vstr_add_str(&fix, "large");
+        printf("%.*s\n", (int)fix.len, fix.buf);
+    }
+
     // repl autocomplete
     {
+        printf("# repl\n");
+
         const char *str;
         mp_uint_t len = mp_repl_autocomplete("__n", 3, &mp_plat_print, &str);
         printf("%.*s\n", (int)len, str);
@@ -18,6 +48,16 @@ STATIC mp_obj_t extra_coverage(void) {
         mp_repl_autocomplete("sys.", 4, &mp_plat_print, &str);
         len = mp_repl_autocomplete("sys.impl", 8, &mp_plat_print, &str);
         printf("%.*s\n", (int)len, str);
+    }
+
+    // attrtuple
+    {
+        printf("# attrtuple\n");
+
+        static const qstr fields[] = {MP_QSTR_start, MP_QSTR_stop, MP_QSTR_step};
+        static const mp_obj_t items[] = {MP_OBJ_NEW_SMALL_INT(1), MP_OBJ_NEW_SMALL_INT(2), MP_OBJ_NEW_SMALL_INT(3)};
+        mp_obj_print_helper(&mp_plat_print, mp_obj_new_attrtuple(fields, 3, items), PRINT_REPR);
+        printf("\n");
     }
 
     return mp_const_none;
