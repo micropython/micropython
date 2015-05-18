a3a14b9db79d3af24e724832092d1861ec231365
Dave Hylands <dhylands@gmail.com>
2015-05-18 08:26:58 -0700

    lib: Fix some issues in timeutils
    In particular, dates prior to Mar 1, 2000 are screwed up.
    
    The easiest way to see this is to do:
    
    >>> import time
    >>> time.localtime(0)
    (2000, 1, 1, 0, 0, 0, 5, 1)
    >>> time.localtime(1)
    (2000, 1, 2, 233, 197, 197, 6, 2)
    
    With this patch, we instead get:
    >>> import time
    >>> time.localtime(1)
    (2000, 1, 1, 0, 0, 1, 5, 1)
    
    Doh - In C % is NOT a modulo operator, it's a remainder operator.

--- tests/pyb/modtime.py
+++ tests/pyb/modtime.py
@@ -35,4 +35,25 @@ def test():
                 yday += 1
                 wday = (wday + 1) % 7
 
+def spot_test(seconds, expected_time):
+    actual_time = time.localtime(seconds)
+    for i in range(len(actual_time)):
+        if actual_time[i] != expected_time[i]:
+            print("time.localtime(", seconds, ") returned", actual_time, "expecting", expected_time)
+            return
+    print("time.localtime(", seconds, ") returned", actual_time, "(pass)")
+
+
 test()
+spot_test(          0,  (2000,  1,  1,  0,  0,  0, 5,   1))
+spot_test(          1,  (2000,  1,  1,  0,  0,  1, 5,   1))
+spot_test(         59,  (2000,  1,  1,  0,  0, 59, 5,   1))
+spot_test(         60,  (2000,  1,  1,  0,  1,  0, 5,   1))
+spot_test(       3599,  (2000,  1,  1,  0, 59, 59, 5,   1))
+spot_test(       3600,  (2000,  1,  1,  1,  0,  0, 5,   1))
+spot_test(         -1,  (1999, 12, 31, 23, 59, 59, 4, 365))
+spot_test(  447549467,  (2014,  3,  7, 23, 17, 47, 4,  66))
+spot_test( -940984933,  (1970,  3,  7, 23, 17, 47, 5,  66))
+spot_test(-1072915199,  (1966,  1,  1,  0,  0,  1, 5,   1))
+spot_test(-1072915200,  (1966,  1,  1,  0,  0,  0, 5,   1))
+spot_test(-1072915201,  (1965, 12, 31, 23, 59, 59, 4, 365))
