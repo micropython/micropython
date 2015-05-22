7bd273b8187a7beb3527df1641fa79f75f010464
Daniel Campora <daniel@wipy.io>
2015-05-22 09:51:29 +0200

    extmod: Actually expose the ubinascii python methods.

--- extmod/modubinascii.h
+++ extmod/modubinascii.h
@@ -30,4 +30,7 @@
 extern mp_obj_t mod_binascii_hexlify(mp_uint_t n_args, const mp_obj_t *args);
 extern mp_obj_t mod_binascii_unhexlify(mp_obj_t data);
 
+MP_DECLARE_CONST_FUN_OBJ(mod_binascii_hexlify_obj);
+MP_DECLARE_CONST_FUN_OBJ(mod_binascii_unhexlify_obj);
+
 #endif /* MICROPY_EXTMOD_MODUBINASCII */
