78744c4f7a368e74e8aff81320a8e1bb3265346e
Daniel Campora <daniel@wipy.io>
2015-05-28 13:22:26 +0200

    cc3200: Remove WIPY-SD variant, and make the WiPy the default board.

--- cc3200/boards/WIPY/mpconfigboard.h
+++ cc3200/boards/WIPY/mpconfigboard.h
@@ -30,7 +30,7 @@
 #define MICROPY_HW_BOARD_NAME                       "WiPy"
 #define MICROPY_HW_MCU_NAME                         "CC3200"
 
-#define MICROPY_HW_HAS_SDCARD                       (0)
+#define MICROPY_HW_HAS_SDCARD                       (1)
 #define MICROPY_HW_ENABLE_RNG                       (1)
 #define MICROPY_HW_ENABLE_RTC                       (1)
 
