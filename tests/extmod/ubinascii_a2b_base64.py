e1fae53e1b35546e494dae71ac2c3c164d9deb40
Galen Hazelwood <galenhz@gmail.com>
2015-07-01 22:15:06 -0700

    extmod: Add a2b_base64 and b2a_base64 functions to ubinascii.

---
+++ tests/extmod/ubinascii_a2b_base64.py
@@ -0,0 +1,34 @@
+try:
+    import ubinascii as binascii
+except ImportError:
+    import binascii
+
+print(binascii.a2b_base64(b''))
+print(binascii.a2b_base64(b'Zg=='))
+print(binascii.a2b_base64(b'Zm8='))
+print(binascii.a2b_base64(b'Zm9v'))
+print(binascii.a2b_base64(b'Zm9vYg=='))
+print(binascii.a2b_base64(b'Zm9vYmE='))
+print(binascii.a2b_base64(b'Zm9vYmFy'))
+
+print(binascii.a2b_base64(b'AAECAwQFBgc='))
+print(binascii.a2b_base64(b'CAkKCwwNDg8='))
+print(binascii.a2b_base64(b'f4D/'))
+print(binascii.a2b_base64(b'MTIzNEFCQ0RhYmNk'))
+
+try:
+    print(binascii.a2b_base64(b'abc'))
+except:
+    print("caught")
+try:
+    print(binascii.a2b_base64(b'abcde='))
+except:
+    print("caught")
+try:
+    print(binascii.a2b_base64(b'ab*d'))
+except:
+    print("caught")
+try:
+    print(binascii.a2b_base64(b'ab=cdef='))
+except:
+    print("caught")
