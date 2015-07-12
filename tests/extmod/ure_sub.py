49819ef0099c11273b837f82aeaae2ff41ac3313
dmazzella <damiano.mazzella.github@gmail.com>
2015-07-12 14:40:06 +0200

    fix test check

--- tests/extmod/ure_sub.py
+++ tests/extmod/ure_sub.py
@@ -4,7 +4,7 @@ except ImportError:
     import re
 
 s0 = re.sub(r'def\s+([a-zA-Z_][a-zA-Z_0-9]*)\s*\(\s*\):',
-       r'static PyObject*\npy_\1(void)\n{',
+       'static PyObject*\npy_\1(void)\n{',
        'def myfunc():')
 print(s0)
 
