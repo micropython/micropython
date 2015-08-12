924924211959c0bc597a303d53910e6d2b015a56
Daniel Campora <daniel@wipy.io>
2015-08-12 16:40:35 +0200

    cc3200: Remove unneeded loops in the FreeRTOS hooks.

--- cc3200/misc/FreeRTOSHooks.c
+++ cc3200/misc/FreeRTOSHooks.c
@@ -70,10 +70,7 @@ void vApplicationMallocFailedHook (void)
     __asm volatile ("bkpt #0  \n");
 #endif
 
-    for ( ; ; )
-    {
-        __fatal_error("FreeRTOS malloc failed!");
-    }
+    __fatal_error("FreeRTOS malloc failed!");
 }
 
 //*****************************************************************************
@@ -92,10 +89,7 @@ void vApplicationStackOverflowHook (OsiTaskHandle *pxTask, signed char *pcTaskNa
     __asm volatile ("bkpt #0  \n");
 #endif
 
-    for ( ; ; )
-    {
-        __fatal_error("Stack overflow!");
-    }
+    __fatal_error("Stack overflow!");
 }
 
 //*****************************************************************************
