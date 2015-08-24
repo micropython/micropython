7080e9632c98b06952ee480f4b4d0021e43d63b4
Gary Ashton-Jones <gary@ashton-jones.com.au>
2015-08-24 11:28:10 +1000

    cc3200/appsign.sh: Use md5 if running under Darwin.

--- cc3200/appsign.sh
+++ cc3200/appsign.sh
@@ -9,7 +9,12 @@ fi
 BUILD=$1
 
 # Generate the MD5 hash
+# md5 on Darwin, md5sum on Unix
+if [ `uname -s` = "Darwin" ]; then
+echo -n `md5 -q $BUILD/application.bin` > __md5hash.bin
+else
 echo -n `md5sum --binary $BUILD/application.bin | awk '{ print $1 }'` > __md5hash.bin
+fi
 
 # Concatenate it with the application binary
 cat $BUILD/application.bin __md5hash.bin > $BUILD/mcuimg.bin
