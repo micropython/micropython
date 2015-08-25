4e38d3879f5039cd898b49c69f3d53e361b46341
Tobias Badertscher <python@baerospace.ch>
2015-08-25 08:44:44 +0200

    Removed space in port descritioon in csv. Correct definition of LED ports in mpconfigboard.h.

--- stmhal/boards/STM32F429I-DISCO/mpconfigboard.h
+++ stmhal/boards/STM32F429I-DISCO/mpconfigboard.h
@@ -53,10 +53,8 @@
 #define MICROPY_HW_USRSW_PRESSED    (1)
 
 // LEDs
-#define MICROPY_HW_LED1             (pin_D14) // red
-#define MICROPY_HW_LED2             (pin_D12) // green
-#define MICROPY_HW_LED3             (pin_D13) // orange
-#define MICROPY_HW_LED4             (pin_D15) // blue
+#define MICROPY_HW_LED1             (pin_G14) // red
+#define MICROPY_HW_LED2             (pin_G13) // green
 #define MICROPY_HW_LED_OTYPE        (GPIO_MODE_OUTPUT_PP)
 #define MICROPY_HW_LED_ON(pin)      (pin->gpio->BSRRL = pin->pin_mask)
 #define MICROPY_HW_LED_OFF(pin)     (pin->gpio->BSRRH = pin->pin_mask)
