81794fcd31e604fb74bef89d749ee839b7dfc58f
Damien George <damien.p.george@gmail.com>
2015-09-01 16:25:29 +0100

    py/binary: Add support for array('q') and array('Q').

---
+++ tests/basics/array_q.py
@@ -0,0 +1,15 @@
+# test array('q') and array('Q')
+
+from array import array
+
+print(array('q'))
+print(array('Q'))
+
+print(array('q', [0]))
+print(array('Q', [0]))
+
+print(array('q', [-2**63, -1, 0, 1, 2, 2**63-1]))
+print(array('Q', [0, 1, 2, 2**64-1]))
+
+print(bytes(array('q', [-1])))
+print(bytes(array('Q', [2**64-1])))
