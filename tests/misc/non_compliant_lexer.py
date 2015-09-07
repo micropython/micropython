081f9325f522b25e2f6b4266dca3f2d2dd1c69ac
Damien George <damien.p.george@gmail.com>
2015-09-07 17:08:49 +0100

    py/lexer: Raise NotImplError for unicode name escape, instead of assert.

--- tests/misc/non_compliant_lexer.py
+++ tests/misc/non_compliant_lexer.py
@@ -1,5 +1,14 @@
 # lexer tests for things that are not implemented, or have non-compliant behaviour
 
+def test(code):
+    try:
+        exec(code)
+        print('no Error')
+    except SyntaxError:
+        print('SyntaxError')
+    except NotImplementedError:
+        print('NotImplementedError')
+
 # uPy requires spaces between literal numbers and keywords, CPy doesn't
 try:
     eval('1and 0')
@@ -17,3 +26,6 @@ try:
     eval('1if 0else 0')
 except SyntaxError:
     print('SyntaxError')
+
+# unicode name escapes are not implemented
+test('"\\N{LATIN SMALL LETTER A}"')
