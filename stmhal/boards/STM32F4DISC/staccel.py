3235f3a3dfe62831d26e775e84b8d42804272945
dinau <audgit@gmail.com>
2015-09-08 21:27:03 +0900

    (1) Mofified according to this post, STAccel script in STM32F4 DISC http://forum.micropython.org/viewtopic.php?f=2&t=595&p=3609&hilit=read_id#p3609 (2) Fixed SPI clock polarity(CPOL) to 1 from 0. Please refer to datasheet of LIS302DL and LIS3DSH. The detail explanation is here, https://sites.google.com/site/johnkneenmicrocontrollers/spi/spi_f407#linkSPIClkPhase (3) Modified SPI clock more high speed (2MHz).

--- stmhal/boards/STM32F4DISC/staccel.py
+++ stmhal/boards/STM32F4DISC/staccel.py
@@ -41,10 +41,11 @@ class STAccel:
     def __init__(self):
         self.cs_pin = Pin('PE3', Pin.OUT_PP, Pin.PULL_NONE)
         self.cs_pin.high()
-        self.spi = SPI(1, SPI.MASTER, baudrate=328125, polarity=0, phase=1, bits=8)
-
+        self.spi = SPI(1, SPI.MASTER, baudrate=2000000, polarity=1, phase=1, bits=8)
+        self.read_id()
+        # First SPI read always returns 255 --> discard and read ID again
         self.who_am_i = self.read_id()
-
+        # print( "id = 0x%X" % self.who_am_i ) # for debug
         if self.who_am_i == LIS302DL_WHO_AM_I_VAL:
             self.write_bytes(LIS302DL_CTRL_REG1_ADDR, bytearray([LIS302DL_CONF]))
             self.sensitivity = 18
