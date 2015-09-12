fb4a374417a915b2ebd3f975c2b05eb73e587f99
Tom Soulanille <soul@prama.com>
2015-09-11 17:41:14 -0700

    tests/basics/slice_attrs: Satisfy qemu-arm/test_main test skip signalling convention

--- tests/basics/slice_attrs.py
+++ tests/basics/slice_attrs.py
@@ -4,4 +4,16 @@
 class A:
     def __getitem__(self, idx):
         print(idx.start, idx.stop, idx.step)
+
+# The skip test method for qemu-arm/test-main.c requires we test
+# for the feature being testable and bail thusly. It does not use
+# the tests/feature_check mechanism used by tests/run-tests
+try:
+    t = a()[1:2]
+except:
+    import sys
+    print("SKIP")
+    sys.exit()
+
+
 A()[1:2:3]
