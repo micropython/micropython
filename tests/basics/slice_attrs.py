5259c4735bc75562dfd4ad2890d6a57de5e2ebff
Tom Soulanille <soul@prama.com>
2015-09-13 22:42:19 -0700

    tests/basics/slice_attrs: Bugfix

--- tests/basics/slice_attrs.py
+++ tests/basics/slice_attrs.py
@@ -5,11 +5,8 @@ class A:
     def __getitem__(self, idx):
         print(idx.start, idx.stop, idx.step)
 
-# The skip test method for qemu-arm/test-main.c requires we test
-# for the feature being testable and bail thusly. It does not use
-# the tests/feature_check mechanism used by tests/run-tests
 try:
-    t = a()[1:2]
+    t = A()[1:2]
 except:
     import sys
     print("SKIP")
