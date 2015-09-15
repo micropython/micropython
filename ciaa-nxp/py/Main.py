ca3a4ff20706eb7d211570423634501827542c38
Ernesto Gigliotti <ernestogigliotti@gmail.com>
2015-09-14 23:57:45 -0300

    callbacks in switch module

--- ciaa-nxp/py/Main.py
+++ ciaa-nxp/py/Main.py
@@ -1,36 +1,17 @@
 import pyb
 
-uart = pyb.UART(0)
+print("Test switch con callbacks")
 
-#uart.init(115200,bits=8, parity=None, stop=1,timeout=0, timeout_char=1000, read_buf_len=64,packet_mode=True,packet_end_char=ord('o'))
-uart.init(115200,bits=8, parity=None, stop=1,timeout=1000, timeout_char=1000, read_buf_len=64)
-#uart.init(115200)
+def callB():
+	print("evetno en callback!")
 
-print(uart)
+
+sw1 = pyb.Switch(1)
+sw1.callback(callB)
 
 while True:
-	uart.write("A")
+	value = sw1.value()
+	print("sw1:"+str(value))
 	pyb.delay(1000)
-	if uart.any():
-		print("hay data:")
-		
-		# Test 1
-		#d = uart.readchar()
-          	#print(str(d))
-		#uart.writechar(d)
-		
-		#Test 2
-		#data = uart.read()
-		data = uart.readall()
-		#data = uart.readline()
-		#data = uart.read(3)
-		
-		#buf = bytearray(256)
-		#bytes = uart.readinto(buf)
-		#if bytes != 0:
-		#	print(buf)
 
-		print(data)
-		uart.write(data)
 
-uart.deinit()
