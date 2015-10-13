c62cb0cedbb548cb5ca9afa9a0af89711934450c
Damien George <damien.p.george@gmail.com>
2015-10-13 18:24:36 +0100

    tests: Add more tests for viper 16/32-bit load/store, and ellipsis.

--- tests/micropython/viper_ptr16_store.py
+++ tests/micropython/viper_ptr16_store.py
@@ -5,7 +5,18 @@ def set(dest:ptr16, val:int):
     dest[0] = val
 
 @micropython.viper
+def set1(dest:ptr16, val:int):
+    dest[1] = val
+
+@micropython.viper
 def memset(dest:ptr16, val:int, n:int):
+    for i in range(n):
+        dest[i] = val
+
+@micropython.viper
+def memset2(dest_in, val:int):
+    dest = ptr16(dest_in)
+    n = int(len(dest_in)) >> 1
     for i in range(n):
         dest[i] = val
 
@@ -15,5 +26,11 @@ print(b)
 set(b, 0x4242)
 print(b)
 
-memset(b, 0x4343, len(b) // 2)
+set1(b, 0x4343)
+print(b)
+
+memset(b, 0x4444, len(b) // 2)
+print(b)
+
+memset2(b, 0x4545)
 print(b)
