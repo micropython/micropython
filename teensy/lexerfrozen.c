53ea2b5ce2f7088c871c6c264899e4eae7973859
Dave Hylands <dhylands@gmail.com>
2015-10-31 22:55:13 -0700

    teensy: Switch over to using frozen modules instead of memzip
    I left memzip in for the time being, so you can choose in
    the Makefile whether to USE_FROZEN or USE_MEMZIP.
    
    It looks like using frozen saves about 2472 bytes (using my
    set of 15 python files), mostly due to overheads in the
    zip file format.

---
+++ teensy/lexerfrozen.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#include "py/lexer.h"
+
+mp_import_stat_t mp_import_stat(const char *path) {
+    return MP_IMPORT_STAT_NO_EXIST;
+}
+
+mp_lexer_t *mp_lexer_new_from_file(const char *filename) {
+    return NULL;
+}
