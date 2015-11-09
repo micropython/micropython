746b752b8efd089c8dd37e615949315973a8d370
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2015-11-09 22:10:01 +0200

    stmhal/moduselect: Expose POLLIN/OUT/ERR/HUP constants.
    This makes select.poll() interface fully compatible with CpYthon. Also, make
    their numeric values of these options compatible with Linux (and by extension,
    with iBCS2 standard, which jopefully means compatibility with other Unices too).

--- stmhal/pybioctl.h
+++ stmhal/pybioctl.h
@@ -1,6 +1,8 @@
 #define MP_IOCTL_POLL (0x100 | 1)
 
+// These values are compatible with Linux, which are in turn
+// compatible with iBCS2 spec.
 #define MP_IOCTL_POLL_RD  (0x0001)
-#define MP_IOCTL_POLL_WR  (0x0002)
-#define MP_IOCTL_POLL_HUP (0x0004)
+#define MP_IOCTL_POLL_WR  (0x0004)
 #define MP_IOCTL_POLL_ERR (0x0008)
+#define MP_IOCTL_POLL_HUP (0x0010)
