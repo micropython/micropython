0f9d357b1ec53b2613e9992fa402de8c52a831e5
Ernesto Gigliotti <ernestogigliotti@gmail.com>
2015-12-08 00:27:08 -0300

    eeprom core

--- ciaa-nxp/board_ciaa_edu_4337/src/board.c
+++ ciaa-nxp/board_ciaa_edu_4337/src/board.c
@@ -86,6 +86,75 @@ typedef struct {
 static ExtIntData extIntData[4];
 
 
+//================================================[EEPROM Management]========================================================
+#define AUTOPROG_ON     1
+
+/* Read data from EEPROM */
+/* size must be multiple of 4 bytes */
+STATIC void EEPROM_Read(uint32_t pageOffset, uint32_t pageAddr, uint32_t* ptr, uint32_t size)
+{
+    uint32_t i = 0;
+    uint32_t *pEepromMem = (uint32_t*)EEPROM_ADDRESS(pageAddr,pageOffset);
+    for(i = 0; i < size/4; i++) {
+   	ptr[i] = pEepromMem[i];
+    }
+}
+
+/* Erase a page in EEPROM */
+STATIC void EEPROM_Erase(uint32_t pageAddr)
+{
+    uint32_t i = 0;
+    uint32_t *pEepromMem = (uint32_t*)EEPROM_ADDRESS(pageAddr,0);
+    for(i = 0; i < EEPROM_PAGE_SIZE/4; i++) {
+   	pEepromMem[i] = 0;
+	#if AUTOPROG_ON
+   	Chip_EEPROM_WaitForIntStatus(LPC_EEPROM, EEPROM_INT_ENDOFPROG);
+ 	#endif
+    }
+    #if (AUTOPROG_ON == 0)
+    Chip_EEPROM_EraseProgramPage(LPC_EEPROM);
+    #endif
+}
+
+/* Write data to a page in EEPROM */
+/* size must be multiple of 4 bytes */
+STATIC void EEPROM_Write(uint32_t pageOffset, uint32_t pageAddr, uint32_t* ptr, uint32_t size)
+{
+    uint32_t i = 0;
+    uint32_t *pEepromMem = (uint32_t*)EEPROM_ADDRESS(pageAddr,pageOffset);
+
+    if(size > EEPROM_PAGE_SIZE - pageOffset)
+	size = EEPROM_PAGE_SIZE - pageOffset;
+
+    for(i = 0; i < size/4; i++) {
+   	pEepromMem[i] = ptr[i];
+	#if AUTOPROG_ON
+    	Chip_EEPROM_WaitForIntStatus(LPC_EEPROM, EEPROM_INT_ENDOFPROG);
+ 	#endif
+    }
+
+    #if (AUTOPROG_ON == 0)
+    Chip_EEPROM_EraseProgramPage(LPC_EEPROM);
+    #endif
+}
+
+void Board_EEPROM_init(void)
+{
+	Chip_EEPROM_Init(LPC_EEPROM);
+	#if AUTOPROG_ON
+  	/* Set Auto Programming mode */
+ 	 Chip_EEPROM_SetAutoProg(LPC_EEPROM,EEPROM_AUTOPROG_AFT_1WORDWRITTEN);
+	#else
+  	/* Set Auto Programming mode */
+  	Chip_EEPROM_SetAutoProg(LPC_EEPROM,EEPROM_AUTOPROG_OFF);
+	#endif /*AUTOPROG_ON*/
+
+
+}
+
+
+//===========================================================================================================================
+
 
 
 //================================================[ADC Management]========================================================
