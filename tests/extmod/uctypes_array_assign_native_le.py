b50030b1d0eb3ae44d035bcde06fa213c30347ba
Antonin ENFRUN <antonin.e@me.com>
2015-12-27 20:15:00 +0100

    tests/uctypes: Test item assignment for scalar arrays.

---
+++ tests/extmod/uctypes_array_assign_native_le.py
@@ -0,0 +1,46 @@
+import sys
+import uctypes
+
+if sys.byteorder != "little":
+    print("SKIP")
+    sys.exit()
+
+desc = {
+    # arr is array at offset 0, of UINT8 elements, array size is 2
+    "arr": (uctypes.ARRAY | 0, uctypes.UINT8 | 2),
+    # arr2 is array at offset 0, size 2, of structures defined recursively
+    "arr2": (uctypes.ARRAY | 0, 2, {"b": uctypes.UINT8 | 0}),
+    "arr3": (uctypes.ARRAY | 2, uctypes.UINT16 | 2),
+
+    # aligned
+    "arr5": (uctypes.ARRAY | 0, uctypes.UINT32 | 1),
+    "arr7": (uctypes.ARRAY | 0, 1, {"l": uctypes.UINT32 | 0}),
+}
+
+data = bytearray(5)
+
+S = uctypes.struct(uctypes.addressof(data), desc)
+
+# assign byte
+S.arr[0] = 0x11
+print(hex(S.arr[0]))
+assert hex(S.arr[0]) == "0x11"
+
+# assign word
+S.arr3[0] = 0x2233
+print(hex(S.arr3[0]))
+assert hex(S.arr3[0]) == "0x2233"
+
+# assign word, with index
+S.arr3[1] = 0x4455
+print(hex(S.arr3[1]))
+assert hex(S.arr3[1]) == "0x4455"
+
+# assign long, aligned
+S.arr5[0] = 0x66778899
+print(hex(S.arr5[0]))
+assert hex(S.arr5[0]) == "0x66778899"
+
+print(S.arr5[0] == S.arr7[0].l)
+assert S.arr5[0] == S.arr7[0].l
+
