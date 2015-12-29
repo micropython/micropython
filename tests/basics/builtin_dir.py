7281d95aee4084603964c1914b5f08c647669994
Dave Hylands <dhylands@gmail.com>
2015-12-28 20:03:15 -0800

    py: Make dir report instance members

--- tests/basics/builtin_dir.py
+++ tests/basics/builtin_dir.py
@@ -7,3 +7,10 @@ print('__name__' in dir())
 import sys
 print('platform' in dir(sys))
 
+class Foo:
+    def __init__(self):
+        self.x = 1
+foo = Foo()
+print('__init__' in dir(foo))
+print('x' in dir(foo))
+
