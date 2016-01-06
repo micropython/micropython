42bd752fbad6567dde919a2c50f4a54823d73649
Martin Ribelotta <martinribelotta@gmail.com>
2016-01-06 20:15:50 -0300

    [EDU-CIAA-NXP] SPI write_readinto and readinto implementation (for interrupt driven and other ports compatibility)
    Signed-off-by: Martin Ribelotta <martinribelotta@gmail.com>

--- ciaa-nxp/modpybspi.c
+++ ciaa-nxp/modpybspi.c
@@ -29,6 +29,7 @@
 #include "board.h"
 #include "modpyb.h"
 #include "ciaanxp_mphal.h"
+#include "stmhal/bufhelper.h"
 
 typedef struct _pyb_spi_obj_t {
     mp_obj_base_t base;
@@ -80,16 +81,64 @@ STATIC mp_uint_t pyb_spi_read_m(mp_obj_t self_in, void *buf_in, mp_uint_t size, 
     return mp_hal_readSPI(buf, size);
 }
 
+STATIC mp_obj_t pyb_spi_readinto(mp_uint_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args) {
+    static const mp_arg_t allowed_args[] = {
+        { MP_QSTR_buf,       MP_ARG_REQUIRED | MP_ARG_OBJ, }
+    };
 
+    // parse args
+    mp_arg_val_t args[MP_ARRAY_SIZE(allowed_args)];
+    mp_arg_parse_all(n_args - 1, pos_args + 1, kw_args, MP_ARRAY_SIZE(args), allowed_args, args);
 
+    // get the buffer to receive into
+    vstr_t vstr;
+    pyb_buf_get_for_recv(args[0].u_obj, &vstr);
+
+    // just receive
+    mp_hal_readSPI((uint8_t*) vstr.buf, vstr.len);
+
+    // return the number of bytes received
+    return mp_obj_new_int(vstr.len);
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_KW(pyb_spi_readinto_obj, 1, pyb_spi_readinto);
+
+STATIC mp_obj_t pyb_spi_write_readinto (mp_obj_t self, mp_obj_t writebuf, mp_obj_t readbuf) {
+    // get buffers to write from/read to
+    mp_buffer_info_t bufinfo_write;
+    uint8_t data_send[1];
+    mp_buffer_info_t bufinfo_read;
+
+    if (writebuf == readbuf) {
+        // same object for writing and reading, it must be a r/w buffer
+        mp_get_buffer_raise(writebuf, &bufinfo_write, MP_BUFFER_RW);
+        bufinfo_read = bufinfo_write;
+    } else {
+        // get the buffer to write from
+        pyb_buf_get_for_send(writebuf, &bufinfo_write, data_send);
+
+        // get the read buffer
+        mp_get_buffer_raise(readbuf, &bufinfo_read, MP_BUFFER_WRITE);
+        if (bufinfo_read.len != bufinfo_write.len) {
+            nlr_raise(mp_obj_new_exception_msg(&mp_type_ValueError, "invalid argument(s) value"));
+        }
+    }
+
+    // send and receive
+    Board_SSP_transfer((uint8_t *)bufinfo_write.buf, (uint8_t *)bufinfo_read.buf, bufinfo_write.len);
+
+    // return the number of transferred bytes
+    return mp_obj_new_int(bufinfo_write.len);
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_3(pyb_spi_write_readinto_obj, pyb_spi_write_readinto);
 
 STATIC const mp_map_elem_t pyb_spi_locals_dict_table[] = {
     { MP_OBJ_NEW_QSTR(MP_QSTR_write), (mp_obj_t)&mp_stream_write_obj },
     { MP_OBJ_NEW_QSTR(MP_QSTR_read), (mp_obj_t)&mp_stream_read_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_readinto),            (mp_obj_t)&pyb_spi_readinto_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_write_readinto),      (mp_obj_t)&pyb_spi_write_readinto_obj },
 };
 
 STATIC MP_DEFINE_CONST_DICT(pyb_spi_locals_dict, pyb_spi_locals_dict_table);
-
 
 STATIC const mp_stream_p_t spi_stream_p = {
     .read = pyb_spi_read_m,
