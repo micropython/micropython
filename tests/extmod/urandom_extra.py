0ae97f531d700119a3b07026c9e77069710303a6
Damien George <damien.p.george@gmail.com>
2016-01-24 18:35:54 +0000

    tests: Add some tests for urandom module.

---
+++ tests/extmod/urandom_extra.py
@@ -0,0 +1,39 @@
+try:
+    import urandom as random
+except ImportError:
+    import random
+
+try:
+    random.randint
+except AttributeError:
+    import sys
+    print('SKIP')
+    sys.exit(1)
+
+print('randrange')
+for i in range(50):
+    assert 0 <= random.randrange(4) < 4
+    assert 2 <= random.randrange(2, 6) < 6
+    assert -2 <= random.randrange(-2, 2) < 2
+    assert random.randrange(1, 9, 2) in (1, 3, 5, 7)
+
+print('randint')
+for i in range(50):
+    assert 0 <= random.randint(0, 4) <= 4
+    assert 2 <= random.randint(2, 6) <= 6
+    assert -2 <= random.randint(-2, 2) <= 2
+
+print('choice')
+lst = [1, 2, 5, 6]
+for i in range(50):
+    assert random.choice(lst) in lst
+
+print('random')
+for i in range(50):
+    assert 0 <= random.random() < 1
+
+print('uniform')
+for i in range(50):
+    assert 0 <= random.uniform(0, 4) <= 4
+    assert 2 <= random.uniform(2, 6) <= 6
+    assert -2 <= random.uniform(-2, 2) <= 2
