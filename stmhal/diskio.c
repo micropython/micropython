42b641a99d8fdb22b9b4916847fd8916ddd41167
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-02-06 15:18:34 +0200

    stmhal: Move stmhal-specific FatFs routines/structs to fatfs_port.c.

--- stmhal/diskio.c
+++ stmhal/diskio.c
@@ -35,21 +35,9 @@
 #include "py/runtime.h"
 #include "lib/fatfs/ff.h"        /* FatFs lower layer API */
 #include "lib/fatfs/diskio.h"    /* FatFs lower layer API */
-#include "rtc.h"
 #include "storage.h"
 #include "sdcard.h"
 #include "extmod/fsusermount.h"
-
-const PARTITION VolToPart[] = {
-    {0, 1},     // Logical drive 0 ==> Physical drive 0, 1st partition
-    {1, 0},     // Logical drive 1 ==> Physical drive 1 (auto detection)
-    {2, 0},     // Logical drive 2 ==> Physical drive 2 (auto detection)
-    /*
-    {0, 2},     // Logical drive 2 ==> Physical drive 0, 2nd partition
-    {0, 3},     // Logical drive 3 ==> Physical drive 0, 3rd partition
-    */
-};
-
 
 /*-----------------------------------------------------------------------*/
 /* Initialize a Drive                                                    */
@@ -282,15 +270,3 @@ DRESULT disk_ioctl (
     return RES_PARERR;
 }
 #endif
-
-DWORD get_fattime (
-    void
-)
-{
-    rtc_init_finalise();
-    RTC_TimeTypeDef time;
-    RTC_DateTypeDef date;
-    HAL_RTC_GetTime(&RTCHandle, &time, FORMAT_BIN);
-    HAL_RTC_GetDate(&RTCHandle, &date, FORMAT_BIN);
-    return ((2000 + date.Year - 1980) << 25) | ((date.Month) << 21) | ((date.Date) << 16) | ((time.Hours) << 11) | ((time.Minutes) << 5) | (time.Seconds / 2);
-}
