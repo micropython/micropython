7bb501ef9f724e9ecd209d1fa46b640214928d2a
Dave Hylands <dhylands@gmail.com>
2016-02-10 21:20:14 -0800

    stmhal: Add a function for setting the pin alternate function
    mp_hal_gpio_set_af will search for a given function and unit
    and set the alternate function to the alternate function index
    found.

--- stmhal/mphalport.c
+++ stmhal/mphalport.c
@@ -118,3 +118,17 @@ void mp_hal_gpio_clock_enable(GPIO_TypeDef *gpio) {
     #endif
     }
 }
+
+bool mp_hal_gpio_set_af(const pin_obj_t *pin, GPIO_InitTypeDef *init, uint8_t fn, uint8_t unit) {
+    mp_hal_gpio_clock_enable(pin->gpio);
+
+    const pin_af_obj_t *af = pin_find_af(pin, fn, unit);
+    if (af == NULL) {
+        return false;
+    }
+    init->Pin = pin->pin_mask;
+    init->Alternate = af->idx;
+    HAL_GPIO_Init(pin->gpio, init);
+
+    return true;
+}
