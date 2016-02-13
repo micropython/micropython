b86c20676e4a014f8585fa07512fe89eb66da997
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-02-13 15:50:22 +0200

    tests/recursive_iternext.py: Make low-heap friendly.

--- tests/misc/recursive_iternext.py
+++ tests/misc/recursive_iternext.py
@@ -1,8 +1,14 @@
 # This tests that recursion with iternext doesn't lead to segfault.
 
 try:
+    [0] * 10000
+    N = 1000
+except:
+    N = 100
+
+try:
     x = (1, 2)
-    for i in range(1000):
+    for i in range(N):
         x = enumerate(x)
     tuple(x)
 except RuntimeError:
@@ -10,7 +16,7 @@ except RuntimeError:
 
 try:
     x = (1, 2)
-    for i in range(1000):
+    for i in range(N):
         x = filter(None, x)
     tuple(x)
 except RuntimeError:
@@ -18,7 +24,7 @@ except RuntimeError:
 
 try:
     x = (1, 2)
-    for i in range(1000):
+    for i in range(N):
         x = map(max, x, ())
     tuple(x)
 except RuntimeError:
@@ -26,7 +32,7 @@ except RuntimeError:
 
 try:
     x = (1, 2)
-    for i in range(1000):
+    for i in range(N):
         x = zip(x)
     tuple(x)
 except RuntimeError:
