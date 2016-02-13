9e8396accbcb695e2fe9def666bfb31b26efde06
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-02-13 19:26:17 +0200

    esp8266/modnetwork: Allow to configure STA and AP interfaces separately.
    On ESP8266, there're 2 different interfaces. Pretending it's not the case
    desn't make sense. So, network.WLAN() now takes interface id, and returns
    interface object. Individual operations are then methods of interface
    object. Some operations require i/f of specific type (e.g. .connect()
    makes sense only for STA), other are defined for any (e.g. .ifconfig(),
    .mac()).

--- esp8266/modnetwork.c
+++ esp8266/modnetwork.c
@@ -39,22 +39,44 @@
 #include "spi_flash.h"
 #include "utils.h"
 
-void error_check(bool status, const char *msg);
-extern const mp_obj_module_t network_module;
+typedef struct _wlan_if_obj_t {
+    mp_obj_base_t base;
+    int if_id;
+} wlan_if_obj_t;
 
-STATIC mp_obj_t get_module() {
-    return (mp_obj_t)&network_module;
+void error_check(bool status, const char *msg);
+const mp_obj_type_t wlan_if_type;
+
+STATIC const wlan_if_obj_t wlan_objs[] = {
+    {{&wlan_if_type}, STATION_IF},
+    {{&wlan_if_type}, SOFTAP_IF},
+};
+
+STATIC void require_if(mp_obj_t wlan_if, int if_no) {
+    wlan_if_obj_t *self = MP_OBJ_TO_PTR(wlan_if);
+    if (self->if_id != if_no) {
+        error_check(false, "STA required");
+    }
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_0(get_module_obj, get_module);
+
+STATIC mp_obj_t get_wlan(mp_uint_t n_args, const mp_obj_t *args) {
+    int idx = 0;
+    if (n_args > 0) {
+        idx = mp_obj_get_int(args[0]);
+    }
+    return MP_OBJ_FROM_PTR(&wlan_objs[idx]);
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(get_wlan_obj, 0, 1, get_wlan);
 
 STATIC mp_obj_t esp_connect(mp_uint_t n_args, const mp_obj_t *args) {
+    require_if(args[0], STATION_IF);
     struct station_config config = {{0}};
     mp_uint_t len;
     const char *p;
 
-    p = mp_obj_str_get_data(args[0], &len);
-    memcpy(config.ssid, p, len);
     p = mp_obj_str_get_data(args[1], &len);
+    memcpy(config.ssid, p, len);
+    p = mp_obj_str_get_data(args[2], &len);
     memcpy(config.password, p, len);
 
     error_check(wifi_station_set_config(&config), "Cannot set STA config");
@@ -62,20 +84,25 @@ STATIC mp_obj_t esp_connect(mp_uint_t n_args, const mp_obj_t *args) {
 
     return mp_const_none;
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(esp_connect_obj, 2, 6, esp_connect);
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(esp_connect_obj, 3, 7, esp_connect);
 
-STATIC mp_obj_t esp_disconnect() {
+STATIC mp_obj_t esp_disconnect(mp_obj_t self_in) {
+    require_if(self_in, STATION_IF);
     error_check(wifi_station_disconnect(), "Cannot disconnect from AP");
     return mp_const_none;
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_0(esp_disconnect_obj, esp_disconnect);
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(esp_disconnect_obj, esp_disconnect);
 
 #define MODNETWORK_INCLUDE_CONSTANTS (1)
 
-STATIC mp_obj_t esp_status() {
-    return MP_OBJ_NEW_SMALL_INT(wifi_station_get_connect_status());
+STATIC mp_obj_t esp_status(mp_obj_t self_in) {
+    wlan_if_obj_t *self = MP_OBJ_TO_PTR(self_in);
+    if (self->if_id == STATION_IF) {
+        return MP_OBJ_NEW_SMALL_INT(wifi_station_get_connect_status());
+    }
+    return MP_OBJ_NEW_SMALL_INT(-1);
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_0(esp_status_obj, esp_status);
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(esp_status_obj, esp_status);
 
 STATIC void esp_scan_cb(scaninfo *si, STATUS status) {
     struct bss_info *bs;
@@ -93,7 +120,7 @@ STATIC void esp_scan_cb(scaninfo *si, STATUS status) {
     }
 }
 
-STATIC mp_obj_t esp_scan(mp_obj_t cb_in) {
+STATIC mp_obj_t esp_scan(mp_obj_t self_in, mp_obj_t cb_in) {
     MP_STATE_PORT(scan_cb_obj) = cb_in;
     if (wifi_get_opmode() == SOFTAP_MODE) {
         nlr_raise(mp_obj_new_exception_msg(&mp_type_OSError, 
@@ -102,43 +129,52 @@ STATIC mp_obj_t esp_scan(mp_obj_t cb_in) {
     wifi_station_scan(NULL, (scan_done_cb_t)esp_scan_cb);
     return mp_const_none;
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_1(esp_scan_obj, esp_scan);
+STATIC MP_DEFINE_CONST_FUN_OBJ_2(esp_scan_obj, esp_scan);
 
 /// \method isconnected()
 /// Return True if connected to an AP and an IP address has been assigned,
 ///     false otherwise.
-STATIC mp_obj_t esp_isconnected() {
-    if (wifi_station_get_connect_status() == STATION_GOT_IP) {
-        return mp_const_true;
+STATIC mp_obj_t esp_isconnected(mp_obj_t self_in) {
+    wlan_if_obj_t *self = MP_OBJ_TO_PTR(self_in);
+    if (self->if_id == STATION_IF) {
+        if (wifi_station_get_connect_status() == STATION_GOT_IP) {
+            return mp_const_true;
+        }
+    } else {
+        if (wifi_softap_get_station_num() > 0) {
+            return mp_const_true;
+        }
     }
     return mp_const_false;
 }
 
-STATIC MP_DEFINE_CONST_FUN_OBJ_0(esp_isconnected_obj, esp_isconnected);
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(esp_isconnected_obj, esp_isconnected);
 
 STATIC mp_obj_t esp_mac(mp_uint_t n_args, const mp_obj_t *args) {
+    wlan_if_obj_t *self = MP_OBJ_TO_PTR(args[0]);
     uint8_t mac[6];
-    if (n_args == 0) {
-        wifi_get_macaddr(STATION_IF, mac);
+    if (n_args == 1) {
+        wifi_get_macaddr(self->if_id, mac);
         return mp_obj_new_bytes(mac, sizeof(mac));
     } else {
         mp_buffer_info_t bufinfo;
-        mp_get_buffer_raise(args[0], &bufinfo, MP_BUFFER_READ);
+        mp_get_buffer_raise(args[1], &bufinfo, MP_BUFFER_READ);
 
         if (bufinfo.len != 6) {
             nlr_raise(mp_obj_new_exception_msg(&mp_type_ValueError,
                 "invalid buffer length"));
         }
 
-        wifi_set_macaddr(STATION_IF, bufinfo.buf);
+        wifi_set_macaddr(self->if_id, bufinfo.buf);
         return mp_const_none;
     }
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(esp_mac_obj, 0, 1, esp_mac);
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(esp_mac_obj, 1, 2, esp_mac);
 
-STATIC mp_obj_t esp_ifconfig(void) {
+STATIC mp_obj_t esp_ifconfig(mp_obj_t self_in) {
+    wlan_if_obj_t *self = MP_OBJ_TO_PTR(self_in);
     struct ip_info info;
-    wifi_get_ip_info(STATION_IF, &info);
+    wifi_get_ip_info(self->if_id, &info);
     mp_obj_t ifconfig[4] = {
             netutils_format_ipv4_addr((uint8_t*)&info.ip, NETUTILS_BIG),
             netutils_format_ipv4_addr((uint8_t*)&info.netmask, NETUTILS_BIG),
@@ -147,15 +183,9 @@ STATIC mp_obj_t esp_ifconfig(void) {
     };
     return mp_obj_new_tuple(4, ifconfig);
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_0(esp_ifconfig_obj, esp_ifconfig);
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(esp_ifconfig_obj, esp_ifconfig);
 
-STATIC const mp_map_elem_t mp_module_network_globals_table[] = {
-    { MP_OBJ_NEW_QSTR(MP_QSTR___name__), MP_OBJ_NEW_QSTR(MP_QSTR_network) },
-    // MicroPython "network" module interface requires it to contains classes
-    // to instantiate. But as we have just a static network interace,
-    // use module as a "class", and just make all methods module-global
-    // functions.
-    { MP_OBJ_NEW_QSTR(MP_QSTR_WLAN), (mp_obj_t)&get_module_obj },
+STATIC const mp_map_elem_t wlan_if_locals_dict_table[] = {
     { MP_OBJ_NEW_QSTR(MP_QSTR_connect), (mp_obj_t)&esp_connect_obj },
     { MP_OBJ_NEW_QSTR(MP_QSTR_disconnect), (mp_obj_t)&esp_disconnect_obj },
     { MP_OBJ_NEW_QSTR(MP_QSTR_status), (mp_obj_t)&esp_status_obj },
@@ -163,6 +193,19 @@ STATIC const mp_map_elem_t mp_module_network_globals_table[] = {
     { MP_OBJ_NEW_QSTR(MP_QSTR_isconnected), (mp_obj_t)&esp_isconnected_obj },
     { MP_OBJ_NEW_QSTR(MP_QSTR_mac), (mp_obj_t)&esp_mac_obj },
     { MP_OBJ_NEW_QSTR(MP_QSTR_ifconfig), (mp_obj_t)&esp_ifconfig_obj },
+};
+
+STATIC MP_DEFINE_CONST_DICT(wlan_if_locals_dict, wlan_if_locals_dict_table);
+
+const mp_obj_type_t wlan_if_type = {
+    { &mp_type_type },
+    .name = MP_QSTR_WLAN,
+    .locals_dict = (mp_obj_t)&wlan_if_locals_dict,
+};
+
+STATIC const mp_map_elem_t mp_module_network_globals_table[] = {
+    { MP_OBJ_NEW_QSTR(MP_QSTR___name__), MP_OBJ_NEW_QSTR(MP_QSTR_network) },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_WLAN), (mp_obj_t)&get_wlan_obj },
 
 #if MODNETWORK_INCLUDE_CONSTANTS
     { MP_OBJ_NEW_QSTR(MP_QSTR_STAT_IDLE),
