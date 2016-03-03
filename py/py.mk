453a2a3d7c61be155bca0ac93d1d0193dd4ad1bc
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-03-03 13:25:44 +0200

    extmod/vfs_fat: Add lexer, move from stmhal port for reuse.

--- py/py.mk
+++ py/py.mk
@@ -174,6 +174,7 @@ PY_O_BASENAME = \
 	../extmod/vfs_fat_ffconf.o \
 	../extmod/vfs_fat_diskio.o \
 	../extmod/vfs_fat_file.o \
+	../extmod/vfs_fat_lexer.o \
 	../extmod/vfs_fat_misc.o \
 	../extmod/moduos_dupterm.o \
 
