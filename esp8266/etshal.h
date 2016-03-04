f22a4f8e0ac206b39cf76d697b890581cb55abbc
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-03-04 19:39:24 +0200

    esp8266/etshal.h: Add timer functions prototypes.

--- esp8266/etshal.h
+++ esp8266/etshal.h
@@ -1,9 +1,15 @@
 #ifndef _INCLUDED_ETSHAL_H_
 #define _INCLUDED_ETSHAL_H_
 
+#include <os_type.h>
+
 void ets_isr_unmask();
 void ets_install_putc1();
 void ets_isr_attach();
 void uart_div_modify();
+
+void ets_timer_arm_new(os_timer_t *tim, uint32_t millis, bool repeat, bool is_milli_timer);
+void ets_timer_setfn(os_timer_t *tim, ETSTimerFunc callback, void *cb_data);
+void ets_timer_disarm(os_timer_t *tim);
 
 #endif // _INCLUDED_ETSHAL_H_
