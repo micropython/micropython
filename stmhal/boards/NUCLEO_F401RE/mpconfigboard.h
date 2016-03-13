99fc0d120a139a1525384663552b568fab6215e2
Dave Hylands <dhylands@gmail.com>
2016-03-12 16:10:08 -0800

    stmhal: NUCELO_F401RE cleanup
    Added definitions for SPI1, SPI2, and SPI3
    Removed USB stuff (that could be removed)
    Updated BOARD name defintion

--- stmhal/boards/NUCLEO_F401RE/mpconfigboard.h
+++ stmhal/boards/NUCLEO_F401RE/mpconfigboard.h
@@ -1,4 +1,4 @@
-#define MICROPY_HW_BOARD_NAME       "F401NUCLEO"
+#define MICROPY_HW_BOARD_NAME       "NUCLEO-F401RE"
 #define MICROPY_HW_MCU_NAME         "STM32F401xE"
 
 #define MICROPY_HW_HAS_SWITCH       (1)
@@ -23,13 +23,28 @@
 #define MICROPY_HW_UART_REPL_BAUD   115200
 
 // I2C busses
-#define MICROPY_HW_I2C1_SCL (pin_B6)    // D10 on arduino connector, pin 17 on CN10
-#define MICROPY_HW_I2C1_SDA (pin_B7)    //                           pin 21 on CN7
-#define MICROPY_HW_I2C2_SCL (pin_B10)   // D6  on arduino connector, pin 25 on CN10
-#define MICROPY_HW_I2C2_SDA (pin_B3)    // D3  on arduino connector, pin 31 on CN10
-#define MICROPY_HW_I2C3_SCL (pin_A8)    // D7  on arduino connector, pin 23 on CN10
-#define MICROPY_HW_I2C3_SDA (pin_C9)    //                           pin  1 on CN10
+#define MICROPY_HW_I2C1_SCL (pin_B6)        // Arduino D10, pin 17 on CN10
+#define MICROPY_HW_I2C1_SDA (pin_B7)        //              pin 21 on CN7
+#define MICROPY_HW_I2C2_SCL (pin_B10)       // Arduino D6,  pin 25 on CN10
+#define MICROPY_HW_I2C2_SDA (pin_B3)        // Arduino D3,  pin 31 on CN10
+#define MICROPY_HW_I2C3_SCL (pin_A8)        // Arduino D7,  pin 23 on CN10
+#define MICROPY_HW_I2C3_SDA (pin_C9)        //              pin  1 on CN10
 
+// SPI busses
+#define MICROPY_HW_SPI1_NSS     (pin_A15)   //              pin 17 on CN7
+#define MICROPY_HW_SPI1_SCK     (pin_A5)    // Arduino D13, pin 11 on CN10
+#define MICROPY_HW_SPI1_MISO    (pin_A6)    // Arduino D12, pin 13 on CN10
+#define MICROPY_HW_SPI1_MOSI    (pin_A7)    // Arduino D11, pin 15 on CN10
+
+#define MICROPY_HW_SPI2_NSS     (pin_B12)   //              pin 16 on CN10
+#define MICROPY_HW_SPI2_SCK     (pin_B13)   //              pin 30 on CN10
+#define MICROPY_HW_SPI2_MISO    (pin_B14)   //              pin 28 on CN10
+#define MICROPY_HW_SPI2_MOSI    (pin_B15)   //              pin 26 on CN10
+
+#define MICROPY_HW_SPI3_NSS     (pin_A4)    // Arduino A2,  pin 32 on CN7
+#define MICROPY_HW_SPI3_SCK     (pin_B3)    // Arduino D3,  pin 31 on CN10
+#define MICROPY_HW_SPI3_MISO    (pin_B4)    // Arduino D5,  pin 27 on CN10
+#define MICROPY_HW_SPI3_MOSI    (pin_B5)    // Arduino D4,  pin 29 on CN10
 
 // USRSW is pulled low. Pressing the button makes the input go high.
 #define MICROPY_HW_USRSW_PIN        (pin_C13)
@@ -42,7 +57,3 @@
 #define MICROPY_HW_LED_OTYPE        (GPIO_MODE_OUTPUT_PP)
 #define MICROPY_HW_LED_ON(pin)      (pin->gpio->BSRRL = pin->pin_mask)
 #define MICROPY_HW_LED_OFF(pin)     (pin->gpio->BSRRH = pin->pin_mask)
-
-// USB config
-#define MICROPY_HW_USB_VBUS_DETECT_PIN (pin_A9)
-#define MICROPY_HW_USB_OTG_ID_PIN      (pin_A10)
