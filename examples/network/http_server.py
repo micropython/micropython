c07a03a36ddcb975478a81bef9eb6ac03fa134aa
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-04-02 20:57:58 +0300

    examples/http_server.py: Introduce main() function.
    Allows to re-run code if it was imported as a module (e.g., on bare-metal
    ports).

--- examples/network/http_server.py
+++ examples/network/http_server.py
@@ -10,33 +10,37 @@ HTTP/1.0 200 OK
 Hello #%d from MicroPython!
 """
 
-s = socket.socket()
+def main(use_stream=False):
+    s = socket.socket()
 
-ai = socket.getaddrinfo("127.0.0.1", 8080)
-print("Bind address info:", ai)
-addr = ai[0][4]
+    ai = socket.getaddrinfo("127.0.0.1", 8080)
+    print("Bind address info:", ai)
+    addr = ai[0][4]
 
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind(addr)
-s.listen(5)
-print("Listening, connect your browser to http://127.0.0.1:8080/")
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind(addr)
+    s.listen(5)
+    print("Listening, connect your browser to http://127.0.0.1:8080/")
 
-counter = 0
-while True:
-    res = s.accept()
-    client_s = res[0]
-    client_addr = res[1]
-    print("Client address:", client_addr)
-    print("Client socket:", client_s)
-    print("Request:")
-    if 0:
-        # MicroPython socket objects support stream (aka file) interface
-        # directly.
-        print(client_s.read(4096))
-        client_s.write(CONTENT % counter)
-    else:
-        print(client_s.recv(4096))
-        client_s.send(CONTENT % counter)
-    client_s.close()
-    counter += 1
-    print()
+    counter = 0
+    while True:
+        res = s.accept()
+        client_s = res[0]
+        client_addr = res[1]
+        print("Client address:", client_addr)
+        print("Client socket:", client_s)
+        print("Request:")
+        if use_stream:
+            # MicroPython socket objects support stream (aka file) interface
+            # directly.
+            print(client_s.read(4096))
+            client_s.write(CONTENT % counter)
+        else:
+            print(client_s.recv(4096))
+            client_s.send(CONTENT % counter)
+        client_s.close()
+        counter += 1
+        print()
+
+
+main()
