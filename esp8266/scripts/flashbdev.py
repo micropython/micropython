40f5ecd3a82e810203b39131cb61474f101de58e
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-04-12 00:37:04 +0300

    esp8266: Add Python modules for initial configuration.
    Main entry point is _boot.py which checks whether FAT FS in flash mountable,
    and if so, mounts it. Otherwise, it checks if flash is empty, and if so,
    performs initial module setup: makes FAT FS, configures default AP name,
    etc. As a last option, if flash is not empty, and could not be mounted,
    it means filesystem corruption, and warning message with instructions is
    printed in an infinite loop.

---
+++ esp8266/scripts/flashbdev.py
@@ -0,0 +1,29 @@
+import esp
+
+class FlashBdev:
+
+    SEC_SIZE = 4096
+    START_SEC = 0xa0000 // SEC_SIZE
+    NUM_BLK = 64
+
+    def __init__(self, blocks=NUM_BLK):
+        self.blocks = blocks
+
+    def readblocks(self, n, buf):
+        print("readblocks(%s, %x(%d))" % (n, id(buf), len(buf)))
+        esp.flash_read((n + self.START_SEC) * self.SEC_SIZE, buf)
+
+    def writeblocks(self, n, buf):
+        print("writeblocks(%s, %x(%d))" % (n, id(buf), len(buf)))
+        assert len(buf) <= self.SEC_SIZE, len(buf)
+        esp.flash_erase(n + self.START_SEC)
+        esp.flash_write((n + self.START_SEC) * self.SEC_SIZE, buf)
+
+    def ioctl(self, op, arg):
+        print("ioctl(%d, %r)" % (op, arg))
+        if op == 4:  # BP_IOCTL_SEC_COUNT
+            return self.blocks
+        if op == 5:  # BP_IOCTL_SEC_SIZE
+            return self.SEC_SIZE
+
+bdev = FlashBdev()
