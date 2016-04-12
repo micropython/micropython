69a1aaf654442247e01655035a5cfb34568b1b80
Damien George <damien.p.george@gmail.com>
2016-04-12 13:50:47 +0100

    stmhal: Implement basic C-level pin HAL.

--- stmhal/mphalport.h
+++ stmhal/mphalport.h
@@ -24,6 +24,7 @@
 #define GPIO_read_output_pin(gpio, pin) (((gpio)->ODR >> (pin)) & 1)
 
 void mp_hal_gpio_clock_enable(GPIO_TypeDef *gpio);
+void mp_hal_gpio_config(GPIO_TypeDef *gpio, uint32_t pin, uint32_t mode, uint32_t pull, uint32_t alt);
 bool mp_hal_gpio_set_af(const pin_obj_t *pin, GPIO_InitTypeDef *init, uint8_t fn, uint8_t unit);
 
 extern const unsigned char mp_hal_status_to_errno_table[4];
@@ -33,3 +34,12 @@ void mp_hal_set_interrupt_char(int c); // -1 to disable
 
 #define mp_hal_delay_ms HAL_Delay
 #define mp_hal_ticks_ms HAL_GetTick
+
+// C-level pin HAL
+#include "stmhal/pin.h"
+#define mp_hal_pin_obj_t pin_obj_t
+#define mp_hal_get_pin_obj(o) (pin_obj_t*)pin_find(o)
+#define mp_hal_pin_config_od(p) mp_hal_gpio_config((p)->gpio, (p)->pin, 5, 0, 0)
+#define mp_hal_pin_low(p) GPIO_clear_pin((p)->gpio, (p)->pin_mask)
+#define mp_hal_pin_od_high(p) GPIO_set_pin((p)->gpio, (p)->pin_mask)
+#define mp_hal_pin_read(p) GPIO_read_pin((p)->gpio, (p)->pin)
