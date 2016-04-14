7d2c685544bba08ea5f027d674f35bc0f4bc9a54
Damien George <damien.p.george@gmail.com>
2016-04-14 22:56:21 +0100

    esp8266/scripts/_boot: Mount block device on "" instead of "/".
    "" is the correct name of the root directory when mounting a device there
    (as opposed to "/").  One can now do os.listdir('/') and open('/abc'), as
    well as os.listdir() and open('abc').

--- esp8266/scripts/_boot.py
+++ esp8266/scripts/_boot.py
@@ -3,10 +3,10 @@ import builtins
 from flashbdev import bdev
 
 try:
-    vfs = uos.VfsFat(bdev, "/")
+    vfs = uos.VfsFat(bdev, "")
 except OSError:
     import inisetup
     inisetup.check_bootsec()
     uos.VfsFat.mkfs(bdev)
-    vfs = uos.VfsFat(bdev, "/")
+    vfs = uos.VfsFat(bdev, "")
     inisetup.wifi()
