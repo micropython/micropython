1e59bb8b16f73aef66f0b9f891f53cc4ebc38d88
misterdanb <danb@hasi.it>
2016-04-17 23:53:33 +0200

    esp8266/apa102: Rework code, especially make use of parts of pull request #1950

--- esp8266/espapa102.c
+++ esp8266/espapa102.c
@@ -1,63 +1,84 @@
-// Original version from https://github.com/adafruit/Adafruit_NeoPixel
-// Modifications by dpgeorge to support auto-CPU-frequency detection
-
-// This is a mash-up of the Due show() code + insights from Michael Miller's
-// ESP8266 work for the NeoPixelBus library: github.com/Makuna/NeoPixelBus
-// Needs to be a separate .c file to enforce ICACHE_RAM_ATTR execution.
-
 #include <stdio.h>
 #include "c_types.h"
 #include "eagle_soc.h"
 #include "user_interface.h"
 #include "espapa102.h"
 
-void esp_apa102_write(uint8_t clockPin, uint8_t dataPin, uint8_t *pixels, uint32_t numBytes) {
-    uint32_t clockPinMask, dataPinMask;
+#define NOP asm volatile(" nop \n\t")
 
-    clockPinMask = 1 << clockPin;
-    dataPinMask  = 1 << dataPin;
-
-    // start the frame
-    GPIO_REG_WRITE(GPIO_OUT_W1TC_ADDRESS, dataPinMask);
-
-    for (uint32_t i = 0; i < 32; i++) {
-        GPIO_REG_WRITE(GPIO_OUT_W1TS_ADDRESS, clockPinMask);
-        GPIO_REG_WRITE(GPIO_OUT_W1TC_ADDRESS, clockPinMask);
-    }
-
-    // write pixels
-    for (uint32_t i = 0; i < numBytes / 4; i++) {
-        for (uint32_t j = 0; j < 4; j++) {
-            uint8_t byte = pixels[i * 4 + (3 - j)] | (j == 0 ? 0xE0 : 0x00);
-
-            for (uint32_t k = 0; k < 8; k++) {
-                uint32_t dataHigh = (byte >> (7 - k)) & 0x01;
-
-                if (dataHigh) {
-                    GPIO_REG_WRITE(GPIO_OUT_W1TS_ADDRESS, dataPinMask);
-                }
-                GPIO_REG_WRITE(GPIO_OUT_W1TS_ADDRESS, clockPinMask);
-
-                GPIO_REG_WRITE(GPIO_OUT_W1TC_ADDRESS, clockPinMask);
-                if (dataHigh) {
-                    GPIO_REG_WRITE(GPIO_OUT_W1TC_ADDRESS, dataPinMask);
-                }
-            }
+static inline void _esp_apa102_send_byte(uint32_t clockPinMask, uint32_t dataPinMask, uint8_t byte) {
+    for (uint32_t i = 0; i < 8; i++) {
+        if (byte & 0x80) {
+            // set data pin high
+            GPIO_REG_WRITE(GPIO_OUT_W1TS_ADDRESS, dataPinMask);
+        } else {
+            // set data pin low
+            GPIO_REG_WRITE(GPIO_OUT_W1TC_ADDRESS, dataPinMask);
         }
-    }
 
-    // end the frame
+        // set clock pin high
+        GPIO_REG_WRITE(GPIO_OUT_W1TS_ADDRESS, clockPinMask);
+        byte <<= 1;
+        NOP;
+        NOP;
+
+        // set clock pin low
+        GPIO_REG_WRITE(GPIO_OUT_W1TC_ADDRESS, clockPinMask);
+        NOP;
+        NOP;
+    }
+}
+
+static inline void _esp_apa102_send_colors(uint32_t clockPinMask, uint32_t dataPinMask, uint8_t *pixels, uint32_t numBytes) {
+    for (uint32_t i = 0; i < numBytes / 4; i++) {
+        _esp_apa102_send_byte(clockPinMask, dataPinMask, pixels[i * 4 + 3] | 0xE0);
+        _esp_apa102_send_byte(clockPinMask, dataPinMask, pixels[i * 4 + 2]);
+        _esp_apa102_send_byte(clockPinMask, dataPinMask, pixels[i * 4 + 1]);
+        _esp_apa102_send_byte(clockPinMask, dataPinMask, pixels[i * 4]);
+    }
+}
+
+static inline void _esp_apa102_start_frame(uint32_t clockPinMask, uint32_t dataPinMask) {
+    for (uint32_t i = 0; i < 4; i++) {
+        _esp_apa102_send_byte(clockPinMask, dataPinMask, 0x00);
+    }
+}
+
+static inline void _esp_apa102_append_additionial_cycles(uint32_t clockPinMask, uint32_t dataPinMask, uint32_t numBytes) {
     GPIO_REG_WRITE(GPIO_OUT_W1TS_ADDRESS, dataPinMask);
 
     // we need to write some more clock cycles, because each led
     // delays the data by one edge after inverting the clock
-    for (uint32_t i = 0; i < numBytes / 4 - 1; i++) {
+    for (uint32_t i = 0; i < numBytes / 8 + ((numBytes / 4) % 2); i++) {
         GPIO_REG_WRITE(GPIO_OUT_W1TS_ADDRESS, clockPinMask);
-        GPIO_REG_WRITE(GPIO_OUT_W1TC_ADDRESS, clockPinMask);
-    }
+        NOP;
+        NOP;
 
-    for (uint32_t i = 0; i < 32; i++) {
-        GPIO_REG_WRITE(GPIO_OUT_W1TS_ADDRESS, clockPinMask);
         GPIO_REG_WRITE(GPIO_OUT_W1TC_ADDRESS, clockPinMask);
+        NOP;
+        NOP;
     }
+}
+
+static inline void _esp_apa102_end_frame(uint32_t clockPinMask, uint32_t dataPinMask) {
+    for (uint32_t i = 0; i < 4; i++) {
+        _esp_apa102_send_byte(clockPinMask, dataPinMask, 0xFF);
+    }
+}
+
+void esp_apa102_write(uint8_t clockPin, uint8_t dataPin, uint8_t *pixels, uint32_t numBytes) {
+    uint32_t clockPinMask, dataPinMask;
+
+    clockPinMask = 1 << clockPin;
+    dataPinMask = 1 << dataPin;
+
+    // start the frame
+    _esp_apa102_start_frame(clockPinMask, dataPinMask);
+
+    // write pixels
+    _esp_apa102_send_colors(clockPinMask, dataPinMask, pixels, numBytes);
+
+    // end the frame
+    _esp_apa102_append_additionial_cycles(clockPinMask, dataPinMask, numBytes);
+    _esp_apa102_end_frame(clockPinMask, dataPinMask);
 }
