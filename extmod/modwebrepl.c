18775d380758c21365da9454c034df7e71f1a9e7
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-04-29 19:17:37 +0300

    extmod/modwebrepl: Set debugging by default to off.
    That's production setting. Also, extra UART output may affect behavior of
    (subpar) network drivers.

--- extmod/modwebrepl.c
+++ extmod/modwebrepl.c
@@ -41,7 +41,7 @@
 
 #if MICROPY_PY_WEBREPL
 
-#if 1 // print debugging info
+#if 0 // print debugging info
 #define DEBUG_printf DEBUG_printf
 #else // don't print debugging info
 #define DEBUG_printf(...) (void)0
