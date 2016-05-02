5e94f0b43a28d919f568a786fe019cbfbdfa04a6
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-05-03 02:16:42 +0300

    esp8266/scripts/inisetup: Update for nic.mac() method being gone.

--- esp8266/scripts/inisetup.py
+++ esp8266/scripts/inisetup.py
@@ -5,7 +5,7 @@ from flashbdev import bdev
 def wifi():
     import ubinascii
     ap_if = network.WLAN(network.AP_IF)
-    essid = b"MicroPython-%s" % ubinascii.hexlify(ap_if.mac()[-3:])
+    essid = b"MicroPython-%s" % ubinascii.hexlify(ap_if.config("mac")[-3:])
     ap_if.config(essid=essid, authmode=network.AUTH_WPA_WPA2_PSK, password=b"micropythoN")
 
 def check_bootsec():
