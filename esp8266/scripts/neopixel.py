13d06a83e1c1f79bc9b1d627e7c0b02c59791304
Mike Causer <mcauser@gmail.com>
2016-05-06 17:28:14 +1000

    esp8266/scripts/: Add fill() to NeoPixel

--- esp8266/scripts/neopixel.py
+++ esp8266/scripts/neopixel.py
@@ -20,5 +20,12 @@ class NeoPixel:
         i = index * 3
         return self.buf[i + 1], self.buf[i], self.buf[i + 2]
 
+    def fill(self, color):
+        r, g, b = color
+        for i in range(len(self.buf) / 3):
+            self.buf[i * 3] = g
+            self.buf[i * 3 + 1] = r
+            self.buf[i * 3 + 2] = b
+
     def write(self):
         neopixel_write(self.pin, self.buf, True)
