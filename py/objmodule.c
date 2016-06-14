2b2fb35944fe270c780a868cf14ac959ad9feec9
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-06-15 00:52:45 +0300

    py: Support to build berkeley db 1.85 and "btree" module.

--- py/objmodule.c
+++ py/objmodule.c
@@ -205,6 +205,9 @@ STATIC const mp_rom_map_elem_t mp_builtin_module_table[] = {
 #if MICROPY_PY_FRAMEBUF
     { MP_ROM_QSTR(MP_QSTR_framebuf), MP_ROM_PTR(&mp_module_framebuf) },
 #endif
+#if MICROPY_PY_BTREE
+    { MP_ROM_QSTR(MP_QSTR_btree), MP_ROM_PTR(&mp_module_btree) },
+#endif
 
     // extra builtin modules as defined by a port
     MICROPY_PORT_BUILTIN_MODULES
