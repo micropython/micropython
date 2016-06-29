97ff826766938323c2605be698f85c84ba264bec
dpslwk <dps.lwk@gmail.com>
2016-06-29 15:59:35 +0100

    stmhal/boards: enable SD on TiLDA 475

--- stmhal/boards/STM32L475_EMFBADGE/mpconfigboard.h
+++ stmhal/boards/STM32L475_EMFBADGE/mpconfigboard.h
@@ -6,7 +6,7 @@
 
 #define MICROPY_HW_HAS_SWITCH       (1)
 #define MICROPY_HW_HAS_FLASH        (1)
-#define MICROPY_HW_HAS_SDCARD       (0)
+#define MICROPY_HW_HAS_SDCARD       (1)
 #define MICROPY_HW_HAS_LCD          (0)
 #define MICROPY_HW_ENABLE_RNG       (1)
 #define MICROPY_HW_ENABLE_RTC       (1)
