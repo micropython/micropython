737bd9c3144a57673db8b02217114b7f7f63387d
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-07-02 14:57:42 +0300

    py/mpconfig.h: Mention MICROPY_PY_BTREE config option.
    However, as it requires linking with external libraries, it actually
    should be ste on Makefile level.

--- py/mpconfig.h
+++ py/mpconfig.h
@@ -899,6 +899,10 @@ typedef double mp_float_t;
 #define MICROPY_PY_FRAMEBUF (0)
 #endif
 
+#ifndef MICROPY_PY_BTREE
+#define MICROPY_PY_BTREE (0)
+#endif
+
 /*****************************************************************************/
 /* Hooks for a port to add builtins                                          */
 
