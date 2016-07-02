c880f91ac0d5157c23a78e5e9739388b57419e5d
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-07-02 14:05:37 +0300

    extmod/modwebrepl: Add readinto() method.

--- extmod/modwebrepl.c
+++ extmod/modwebrepl.c
@@ -303,6 +303,7 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_1(webrepl_set_password_obj, webrepl_set_password)
 
 STATIC const mp_map_elem_t webrepl_locals_dict_table[] = {
     { MP_OBJ_NEW_QSTR(MP_QSTR_read), (mp_obj_t)&mp_stream_read_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_readinto), (mp_obj_t)&mp_stream_readinto_obj },
     { MP_OBJ_NEW_QSTR(MP_QSTR_write), (mp_obj_t)&mp_stream_write_obj },
     { MP_OBJ_NEW_QSTR(MP_QSTR_close), (mp_obj_t)&webrepl_close_obj },
 };
