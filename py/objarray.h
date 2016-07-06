ec7fe925312845f788590ce5ccb2a8b3a18a0486
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-07-06 18:18:46 +0300

    py/objarray: Split out header to allow direct access to object.
    This follows source code/header file organization similar to few other
    objects, and intended to be used only is special cases, where efficiency/
    simplicity matters.

---
+++ py/objarray.h
@@ -0,0 +1,43 @@
+/*
+ * This file is part of the MicroPython project, http://micropython.org/
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2013, 2014 Damien P. George
+ * Copyright (c) 2014 Paul Sokolovsky
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef __MICROPY_INCLUDED_PY_OBJARRAY_H__
+#define __MICROPY_INCLUDED_PY_OBJARRAY_H__
+
+#include "py/obj.h"
+
+typedef struct _mp_obj_array_t {
+    mp_obj_base_t base;
+    mp_uint_t typecode : 8;
+    // free is number of unused elements after len used elements
+    // alloc size = len + free
+    mp_uint_t free : (8 * sizeof(mp_uint_t) - 8);
+    mp_uint_t len; // in elements
+    void *items;
+} mp_obj_array_t;
+
+#endif // __MICROPY_INCLUDED_PY_OBJARRAY_H__
