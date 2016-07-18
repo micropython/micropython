39799f7564d28737fa815a3a5d370c91414998ef
Pavol Rusnak <stick@gk2.sk>
2016-07-18 18:35:33 +0200

    extmod/modubinascii: implement binascii.crc32

---
+++ tests/extmod/ubinascii_crc32.py
@@ -0,0 +1,14 @@
+try:
+    import ubinascii as binascii
+except ImportError:
+    import binascii
+
+print(hex(binascii.crc32(b'The quick brown fox jumps over the lazy dog')))
+print(hex(binascii.crc32(b'\x00' * 32)))
+print(hex(binascii.crc32(b'\xff' * 32)))
+print(hex(binascii.crc32(bytes(range(32)))))
+
+print(hex(binascii.crc32(b' over the lazy dog', binascii.crc32(b'The quick brown fox jumps'))))
+print(hex(binascii.crc32(b'\x00' * 16, binascii.crc32(b'\x00' * 16))))
+print(hex(binascii.crc32(b'\xff' * 16, binascii.crc32(b'\xff' * 16))))
+print(hex(binascii.crc32(bytes(range(16, 32)), binascii.crc32(bytes(range(16))))))
