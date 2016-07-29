abd5a57ea1e9f756690f6bad9ddc915cf30de42e
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-07-29 23:00:25 +0300

    tests/io/bytesio_ext: Test for .seek()/.flush() on BytesIO.

---
+++ tests/io/bytesio_ext.py
@@ -0,0 +1,19 @@
+# Extended stream operations on io.BytesIO
+try:
+    import uio as io
+except ImportError:
+    import io
+
+a = io.BytesIO()
+print(a.seek(8))
+a.write(b"123")
+print(a.getvalue())
+
+print(a.seek(0, 1))
+
+print(a.seek(-1, 2))
+a.write(b"0")
+print(a.getvalue())
+
+a.flush()
+print(a.getvalue())
