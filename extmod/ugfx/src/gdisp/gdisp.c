3ed3502d27ebc0e5b73df7e29ede2239cb950cad
Matt Brejza <mbrejza@gmail.com>
2016-07-30 15:17:08 +0100

    fixing text drawing bug

--- extmod/ugfx/src/gdisp/gdisp.c
+++ extmod/ugfx/src/gdisp/gdisp.c
@@ -3242,7 +3242,7 @@ void gdispGDrawBox(GDisplay *g, coord_t x, coord_t y, coord_t cx, coord_t cy, co
 
 	void gdispGFillString(GDisplay *g, coord_t x, coord_t y, const char *str, font_t font, color_t color, color_t bgcolor) {
 		MUTEX_ENTER(g);
-		g->p.cx = mf_get_string_width(font, str, 0, 0);
+		g->p.cx = mf_get_string_width(font, str, 0, 0) + font->baseline_x;
 		g->p.cy = font->height;
 		g->t.font = font;
 		g->t.clipx0 = g->p.x = x;
