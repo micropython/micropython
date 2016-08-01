8766bc02dcecc1176324b0b5e99aaf651ebe975b
Damien George <damien.p.george@gmail.com>
2016-08-02 01:12:45 +1000

    cc3200, teensy: Remove broken malloc/free/realloc macro helpers.
    These macros are broken and are anyway unused on these two ports.  If they
    are ever needed in the future then their implementation can be taken from
    either stmhal (working macros in mpconfigport.h) or esp8266 (functions).

--- teensy/mpconfigport.h
+++ teensy/mpconfigport.h
@@ -100,14 +100,6 @@ __attribute__(( always_inline )) static inline mp_uint_t disable_irq(void) {
 #define MICROPY_BEGIN_ATOMIC_SECTION()     disable_irq()
 #define MICROPY_END_ATOMIC_SECTION(state)  enable_irq(state)
 
-// There is no classical C heap in bare-metal ports, only Python
-// garbage-collected heap. For completeness, emulate C heap via
-// GC heap. Note that MicroPython core never uses malloc() and friends,
-// so these defines are mostly to help extension module writers.
-#define malloc gc_alloc
-#define free gc_free
-#define realloc gc_realloc
-
 // We need to provide a declaration/definition of alloca()
 #include <alloca.h>
 
