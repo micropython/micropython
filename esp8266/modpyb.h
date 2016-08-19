0079fa06dbb863e7b0aa876fe53c34fdfee42ecc
Radomir Dopieralski <openstack@sheep.art.pl>
2016-08-19 13:28:02 +0200

    esp8266/modpybspi: Rename machine.SPI to machine.SoftSPI
    In order to make room for a factory function machine.SPI that
    will create a software or hardware SPI depending on the parameters.

--- esp8266/modpyb.h
+++ esp8266/modpyb.h
@@ -9,7 +9,7 @@ extern const mp_obj_type_t pyb_adc_type;
 extern const mp_obj_type_t pyb_rtc_type;
 extern const mp_obj_type_t pyb_uart_type;
 extern const mp_obj_type_t pyb_i2c_type;
-extern const mp_obj_type_t pyb_spi_type;
+extern const mp_obj_type_t pyb_softspi_type;
 
 MP_DECLARE_CONST_FUN_OBJ(pyb_info_obj);
 
