b85bcd671c5dced62fa00f2e3c0c5541b0d16593
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-09-19 16:59:49 +0300

    tests/struct1: Test "l" specifier to improve coverage.

--- tests/basics/struct1.py
+++ tests/basics/struct1.py
@@ -10,6 +10,8 @@ print(struct.unpack(">bI", b"\x80\0\0\x01\0"))
 # 32-bit little-endian specific
 #print(struct.unpack("bI", b"\x80\xaa\x55\xaa\0\0\x01\0"))
 
+print(struct.pack("<l", 1))
+print(struct.pack(">l", 1))
 print(struct.pack("<i", 1))
 print(struct.pack(">i", 1))
 print(struct.pack("<h", 1))
