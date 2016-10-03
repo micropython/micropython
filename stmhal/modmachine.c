d4a5ca505676b207370d172f307b845380dea2da
Damien George <damien.p.george@gmail.com>
2016-10-03 16:45:46 +1100

    stmhal/spi: Make machine.SPI class conform to correct API.
    Includes both software and hardware SPI implementations.

--- stmhal/modmachine.c
+++ stmhal/modmachine.c
@@ -527,10 +527,10 @@ STATIC const mp_map_elem_t machine_module_globals_table[] = {
     { MP_OBJ_NEW_QSTR(MP_QSTR_RTC),                 (mp_obj_t)&pyb_rtc_type },
     { MP_OBJ_NEW_QSTR(MP_QSTR_ADC),                 (mp_obj_t)&pyb_adc_type },
 #endif
-    // TODO: Per new API, both types below, if called with 1 arg (ID), should still
+    // TODO: Per new API, I2C types below, if called with 1 arg (ID), should still
     // initialize master mode on the peripheral.
     { MP_OBJ_NEW_QSTR(MP_QSTR_I2C),                 (mp_obj_t)&machine_i2c_type },
-    { MP_OBJ_NEW_QSTR(MP_QSTR_SPI),                 (mp_obj_t)&pyb_spi_type },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_SPI),                 (mp_obj_t)&machine_hard_spi_type },
     { MP_OBJ_NEW_QSTR(MP_QSTR_WDT),                 (mp_obj_t)&pyb_wdt_type },
 #if 0
     { MP_OBJ_NEW_QSTR(MP_QSTR_UART),                (mp_obj_t)&pyb_uart_type },
