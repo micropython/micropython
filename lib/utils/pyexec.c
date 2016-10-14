8677f957fb6cb477152e0fc24ebdd8076f65d9bb
Scott Shawcroft <scott@chickadee.tech>
2016-10-14 12:38:46 -0700

    lib/utils: Clarify this is Adafruit's MicroPython derivative and help() isn't available yet.

--- lib/utils/pyexec.c
+++ lib/utils/pyexec.c
@@ -222,8 +222,8 @@ STATIC int pyexec_friendly_repl_process_char(int c) {
         } else if (ret == CHAR_CTRL_B) {
             // reset friendly REPL
             mp_hal_stdout_tx_str("\r\n");
-            mp_hal_stdout_tx_str("MicroPython " MICROPY_GIT_TAG " on " MICROPY_BUILD_DATE "; " MICROPY_HW_BOARD_NAME " with " MICROPY_HW_MCU_NAME "\r\n");
-            mp_hal_stdout_tx_str("Type \"help()\" for more information.\r\n");
+            mp_hal_stdout_tx_str("Adafruit MicroPython " MICROPY_GIT_TAG " on " MICROPY_BUILD_DATE "; " MICROPY_HW_BOARD_NAME " with " MICROPY_HW_MCU_NAME "\r\n");
+            // mp_hal_stdout_tx_str("Type \"help()\" for more information.\r\n");
             goto input_restart;
         } else if (ret == CHAR_CTRL_C) {
             // break
@@ -371,8 +371,8 @@ int pyexec_friendly_repl(void) {
 #endif
 
 friendly_repl_reset:
-    mp_hal_stdout_tx_str("MicroPython " MICROPY_GIT_TAG " on " MICROPY_BUILD_DATE "; " MICROPY_HW_BOARD_NAME " with " MICROPY_HW_MCU_NAME "\r\n");
-    mp_hal_stdout_tx_str("Type \"help()\" for more information.\r\n");
+    mp_hal_stdout_tx_str("Adafruit MicroPython " MICROPY_GIT_TAG " on " MICROPY_BUILD_DATE "; " MICROPY_HW_BOARD_NAME " with " MICROPY_HW_MCU_NAME "\r\n");
+    // mp_hal_stdout_tx_str("Type \"help()\" for more information.\r\n");
 
     // to test ctrl-C
     /*
