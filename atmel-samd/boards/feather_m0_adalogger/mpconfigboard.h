9b1170a65dc96fdbc3c8b0693068491e841bc504
Sebastian Plamauer <oeplse@gmail.com>
2016-10-16 18:19:08 +0200

    [atmel-samd] add adalogger board definitions

---
+++ atmel-samd/boards/feather_m0_adalogger/mpconfigboard.h
@@ -0,0 +1,7 @@
+// LEDs
+#define MICROPY_HW_LED1              PIN_PA17 // red
+// #define UART_REPL
+#define USB_REPL
+
+#define MICROPY_HW_BOARD_NAME "Adafruit Feather M0 Adalogger"
+#define MICROPY_HW_MCU_NAME "samd21g18"
