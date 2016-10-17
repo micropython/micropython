5c93d0b91655dcd32e633d720b5b790b10be989c
Damien George <damien.p.george@gmail.com>
2016-10-18 10:17:54 +1100

    cc3200: Enable loading of precompiled .mpy files.
    Adds 1072 bytes to the code size.

--- cc3200/mpconfigport.h
+++ cc3200/mpconfigport.h
@@ -38,6 +38,7 @@
 // options to control how Micro Python is built
 
 #define MICROPY_ALLOC_PATH_MAX                      (128)
+#define MICROPY_PERSISTENT_CODE_LOAD                (1)
 #define MICROPY_EMIT_THUMB                          (0)
 #define MICROPY_EMIT_INLINE_THUMB                   (0)
 #define MICROPY_COMP_MODULE_CONST                   (1)
