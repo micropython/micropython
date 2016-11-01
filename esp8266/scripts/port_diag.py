eddcf0a5de7a1fe73ccf5c9e85b57aaf9c527bad
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-11-01 06:56:10 +0300

    esp8266/scripts/port_diag: Add descriptions for esf_buf types.

--- esp8266/scripts/port_diag.py
+++ esp8266/scripts/port_diag.py
@@ -24,8 +24,8 @@ def main():
     print("STA ifconfig:", network.WLAN(network.STA_IF).ifconfig())
     print("AP ifconfig:", network.WLAN(network.AP_IF).ifconfig())
     print("Free WiFi driver buffers of type:")
-    for i in range(5):
-        print("%d: %d" % (i, esp.esf_free_bufs(i)))
+    for i, comm in enumerate(("1,2 TX", "4 Mngmt TX(len: 0x41-0x100)", "5 Mngmt TX (len: 0-0x40)", "7", "8 RX")):
+        print("%d: %d (%s)" % (i, esp.esf_free_bufs(i), comm))
     print("lwIP PCBs:")
     lwip.print_pcbs()
 
