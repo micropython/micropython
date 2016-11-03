9e1c3e370b6fe774693711900c3ce1642fb12296
Alex March <alex.march.dev@gmail.com>
2016-11-03 15:32:15 +0000

    tests/extmod/websocket: Read from/write to websocket.

--- tests/extmod/websocket.py
+++ tests/extmod/websocket.py
@@ -7,9 +7,34 @@ except ImportError:
     sys.exit()
 
 
+# server
 s = socket.socket()
-ws = websocket.websocket(s)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(socket.getaddrinfo("0.0.0.0", 8433)[0][4])
+s.listen(1)
 
+# echo dummy socket
+c = socket.socket()
+c.connect(socket.getaddrinfo("0.0.0.0", 8433)[0][-1])
+
+# start
+ws = s.accept()[0]
+ws = websocket.websocket(ws, True) # blocking write
 print(ws)
 
+# server receives from client
+def ping(msg):
+    c.write(msg)
+    return ws.read(len(msg)-2)
+
+# client receives from server
+def pong(msg):
+    ws.write(msg)
+    return c.read(len(msg)+2)
+
+
+print(ping(b"\x81\x04ping"))
+print(pong(b"pong"))
+
+c.close()
 ws.close()
