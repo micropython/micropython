7e03f6a5775af6765979f564da03fca4fa757a61
Matthew Else <matthewelse1997@gmail.com>
2016-11-11 10:40:41 +0000

    emscripten: restructure npm package:  - main.js is now importable, containing an init() and a run(code) function  - repl.js can now be run, producing a REPL

--- emscripten/main.js
+++ emscripten/main.js
@@ -1,15 +1,11 @@
 
-const repl = require('repl');
 const mpy = require('./build/firmware.js');
 
-function mpy_eval(cmd, context, filename, callback) {
-      callback(null, mpy.ccall('mp_js_run', 'null', ['string'], [cmd]));
+module.exports.run = function(code) {
+    mpy.ccall('mp_js_run', 'null', ['string'], [code]);
 }
 
-
-console.log("MicroPython on asmjs with emscripten");
-console.log("Type \"help()\" for more information.");
-
-mpy.ccall('mp_js_init', 'null', [], []);
-repl.start({prompt: '> ', eval: mpy_eval});
+module.exports.init = function() {
+    mpy.ccall('mp_js_init', 'null', [], []);
+}
 
---
+++ emscripten/repl.js
@@ -0,0 +1,15 @@
+
+const repl = require('repl');
+const mpy = require('./main.js');
+
+function mpy_eval(cmd, context, filename, callback) {
+      callback(null, mpy.run(cmd)); 
+}
+
+
+console.log("MicroPython on asmjs with emscripten");
+console.log("Type \"help()\" for more information.");
+
+mpy.init();
+repl.start({prompt: '> ', eval: mpy_eval});
+
