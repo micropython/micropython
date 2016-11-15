ceeac4791e84607a3f1211310d5f4070205db478
Scott Shawcroft <scott@chickadee.tech>
2016-11-14 17:22:56 -0800

    atmel-samd: Add Feather M0 with flash

---
+++ atmel-samd/boards/feather_m0_flash/mpconfigboard.h
@@ -0,0 +1,21 @@
+// LEDs
+#define USB_REPL
+
+#define MICROPY_HW_BOARD_NAME "Adafruit Feather M0 Flash"
+#define MICROPY_HW_MCU_NAME "samd21g18"
+
+#define MICROPY_HW_NEOPIXEL &pin_PA14
+
+#define SPI_FLASH_BAUDRATE  (1000000)
+
+// On-board flash
+#define SPI_FLASH_MUX_SETTING SPI_SIGNAL_MUX_SETTING_E
+#define SPI_FLASH_PAD0_PINMUX PINMUX_PA12D_SERCOM4_PAD0 // MISO
+// Use default pinmux for the chip select since we manage it ourselves.
+#define SPI_FLASH_PAD1_PINMUX PINMUX_DEFAULT // CS
+#define SPI_FLASH_PAD2_PINMUX PINMUX_PB10D_SERCOM4_PAD2 // MOSI
+#define SPI_FLASH_PAD3_PINMUX PINMUX_PB11D_SERCOM4_PAD3 // SCK
+#define SPI_FLASH_CS          PIN_PA13
+#define SPI_FLASH_SERCOM      SERCOM4
+
+#define AUTORESET_DELAY_MS 500
---
+++ atmel-samd/boards/feather_m0_flash/mpconfigboard.mk
@@ -0,0 +1,5 @@
+LD_FILE = boards/samd21x18-bootloader-external-flash.ld
+USB_VID = 0x239A
+USB_PID = 0x8015
+
+FLASH_IMPL = spi_flash.c
