dbc09d03f62bf2d5ee1661492a4c20a7942f81a9
Damien George <damien.p.george@gmail.com>
2016-11-26 16:39:25 +1100

    tests/basics: Enable tests for list slice getting with 3rd arg.
    Also add a test to check case when 3rd arg is 0.

--- tests/basics/list_slice.py
+++ tests/basics/list_slice.py
@@ -1,22 +1,28 @@
-# test slices; only 2 argument version supported by Micro Python at the moment
+# test list slices, getting values
+
 x = list(range(10))
 a = 2
 b = 4
 c = 3
 print(x[:])
 print(x[::])
-#print(x[::c])
+print(x[::c])
 print(x[:b])
 print(x[:b:])
-#print(x[:b:c])
+print(x[:b:c])
 print(x[a])
 print(x[a:])
 print(x[a::])
-#print(x[a::c])
+print(x[a::c])
 print(x[a:b])
 print(x[a:b:])
-#print(x[a:b:c])
+print(x[a:b:c])
 
 # these should not raise IndexError
 print([][1:])
 print([][-1:])
+
+try:
+    [][::0]
+except ValueError:
+    print('ValueError')
