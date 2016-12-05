0ba9aee428ca27d8b2cf562e3d5319d6fa9bc2ea
Scott Shawcroft <scott.shawcroft@gmail.com>
2016-12-05 11:14:01 -0800

    atmel-samd: Update Trinket and Gemma boards for rev B.

--- atmel-samd/boards/gemma_m0/mpconfigboard.h
+++ atmel-samd/boards/gemma_m0/mpconfigboard.h
@@ -3,7 +3,8 @@
 #define MICROPY_HW_BOARD_NAME "Adafruit Gemma M0 (Experimental)"
 #define MICROPY_HW_MCU_NAME "samd21e18"
 
-#define MICROPY_HW_APA102 &pin_PA03
+#define MICROPY_HW_APA102_MOSI &pin_PA04
+#define MICROPY_HW_APA102_SCK  &pin_PA05
 
 #define AUTORESET_DELAY_MS 500
 
--- atmel-samd/boards/trinket_m0/pins.c
+++ atmel-samd/boards/trinket_m0/pins.c
@@ -6,6 +6,6 @@ STATIC const mp_map_elem_t board_global_dict_table[] = {
   { MP_OBJ_NEW_QSTR(MP_QSTR_D1), (mp_obj_t)&pin_PA10 },
   { MP_OBJ_NEW_QSTR(MP_QSTR_D3), (mp_obj_t)&pin_PA09 },
   { MP_OBJ_NEW_QSTR(MP_QSTR_D4), (mp_obj_t)&pin_PA08 },
-  { MP_OBJ_NEW_QSTR(MP_QSTR_L),  (mp_obj_t)&pin_PA28 },
+  { MP_OBJ_NEW_QSTR(MP_QSTR_D13),(mp_obj_t)&pin_PA17 },
 };
 MP_DEFINE_CONST_DICT(board_module_globals, board_global_dict_table);
