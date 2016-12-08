6225b89c764f8e9e44fc03f9544e9d2162bb0659
Scott Shawcroft <scott.shawcroft@gmail.com>
2016-12-07 17:51:10 -0800

    atmel-samd: Stop dumping gc info on collect.
    Fixes #27.

--- atmel-samd/main.c
+++ atmel-samd/main.c
@@ -342,7 +342,6 @@ void gc_collect(void) {
     // range.
     gc_collect_root(&dummy, ((mp_uint_t)stack_top - (mp_uint_t)&dummy) / sizeof(mp_uint_t));
     gc_collect_end();
-    gc_dump_info();
 }
 
 mp_lexer_t *fat_vfs_lexer_new_from_file(const char *filename);
