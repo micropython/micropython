0c59c30fdeba4479817fe8cf3e5f0e4345a0d927
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2016-12-16 01:07:28 +0300

    zephyr: Use core-provided keyboard exception object.

--- zephyr/main.c
+++ zephyr/main.c
@@ -75,7 +75,6 @@ soft_reset:
     mp_obj_list_init(mp_sys_path, 0);
     mp_obj_list_append(mp_sys_path, MP_OBJ_NEW_QSTR(MP_QSTR_)); // current dir (or base dir of the script)
     mp_obj_list_init(mp_sys_argv, 0);
-    MP_STATE_PORT(mp_kbd_exception) = mp_obj_new_exception(&mp_type_KeyboardInterrupt);
 
     #if MICROPY_MODULE_FROZEN
     pyexec_frozen_module("main.py");
