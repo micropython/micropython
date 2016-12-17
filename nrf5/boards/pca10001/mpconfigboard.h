275bf468317a5da62efaefc1519f9584f3883dfb
Glenn Ruben Bakke <glennbakke@gmail.com>
2016-12-17 01:19:44 +0100

    nrf51: Disable MICROPY_PY_MACHINE_PWM for now in all nrf51 target boards as sw impl. is not yet included in the repo.

--- nrf5/boards/pca10001/mpconfigboard.h
+++ nrf5/boards/pca10001/mpconfigboard.h
@@ -30,6 +30,8 @@
 #define MICROPY_HW_MCU_NAME         "NRF51822"
 #define MICROPY_PY_SYS_PLATFORM     "nrf51-DK"
 
+#define MICROPY_PY_MACHINE_PWM      (0)
+
 #define MICROPY_HW_HAS_SWITCH       (0)
 #define MICROPY_HW_HAS_FLASH        (0)
 #define MICROPY_HW_HAS_SDCARD       (0)
--- nrf5/boards/pca10028/mpconfigboard.h
+++ nrf5/boards/pca10028/mpconfigboard.h
@@ -30,6 +30,8 @@
 #define MICROPY_HW_MCU_NAME         "NRF51822"
 #define MICROPY_PY_SYS_PLATFORM     "nrf51-DK"
 
+#define MICROPY_PY_MACHINE_PWM      (0)
+
 #define MICROPY_PY_USOCKET          (0)
 #define MICROPY_PY_NETWORK          (0)
 
