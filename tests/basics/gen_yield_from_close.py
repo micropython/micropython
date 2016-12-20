5d06a743035d942e9fbde924f85dd56992d97419
Rami Ali <flowergrass@users.noreply.github.com>
2016-12-20 12:57:39 +1100

    tests/basics: Improve test coverage for generators.

--- tests/basics/gen_yield_from_close.py
+++ tests/basics/gen_yield_from_close.py
@@ -85,3 +85,17 @@ try:
     print(next(g))
 except StopIteration:
     print("StopIteration")
+
+# case where generator ignores the close request and yields instead
+def gen7():
+    try:
+        yield 123
+    except GeneratorExit:
+        yield 456
+
+g = gen7()
+print(next(g))
+try:
+    g.close()
+except RuntimeError:
+    print('RuntimeError')
