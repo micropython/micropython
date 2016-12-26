22c62fb53f266156d4ae3915100b43ad523a5295
Glenn Ruben Bakke <glennbakke@gmail.com>
2016-12-26 16:48:56 +0100

    nrf5/hal: Adding structures and init function prototype to hal_rtc.h.

--- nrf5/hal/hal_rtc.h
+++ nrf5/hal/hal_rtc.h
@@ -49,4 +49,22 @@
 #error "Device not supported."
 #endif
 
+/**
+  * @brief  RTC Configuration Structure definition
+  */
+typedef struct {
+} hal_rtc_init_t;
+
+/**
+  * @brief  RTC handle Structure definition
+  */
+typedef struct __RTC_HandleTypeDef
+{
+    NRF_RTC_Type               *instance;    /* RTC registers base address */
+    hal_rtc_init_t             init;         /* RTC initialization parameters */
+    uint8_t                    id;           /* RTC instance id */
+} RTC_HandleTypeDef;
+
+void hal_rtc_init(NRF_RTC_Type * p_instance, hal_rtc_init_t const * p_rtc_init);
+
 #endif // HAL_RTC_H__
