61ba28bd7048b946cf42be419d285c6b5ac9e28d
Rami Ali <flowergrass@users.noreply.github.com>
2016-12-29 12:55:23 +1100

    tests/extmod: Improve ubinascii.c test coverage.

--- tests/extmod/ubinascii_b2a_base64.py
+++ tests/extmod/ubinascii_b2a_base64.py
@@ -15,3 +15,4 @@ print(binascii.b2a_base64(b'\x00\x01\x02\x03\x04\x05\x06\x07'))
 print(binascii.b2a_base64(b'\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f'))
 print(binascii.b2a_base64(b'\x7f\x80\xff'))
 print(binascii.b2a_base64(b'1234ABCDabcd'))
+print(binascii.b2a_base64(b'\x00\x00>')) # convert into '+'
