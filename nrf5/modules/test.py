02203389c2fd19d35d7069d04b368df91b9a3414
Glenn Ruben Bakke <glennbakke@gmail.com>
2016-12-29 08:53:38 +0100

    nrf5/modules: Updating pulse test to set output direction on the LED pin used in the test.

--- nrf5/modules/test.py
+++ nrf5/modules/test.py
@@ -3,8 +3,8 @@ from machine import PWM, Pin
 
 def pulse():
     for i in range(0, 101):
-        p = PWM(0, Pin("A17"), freq=PWM.FREQ_16MHZ, duty=i, period=16000)
+        p = PWM(0, Pin("A17", mode=Pin.OUT), freq=PWM.FREQ_16MHZ, duty=i, period=16000)
         time.sleep_ms(10)
     for i in range(0, 101):
-        p = PWM(0, Pin("A17"), freq=PWM.FREQ_16MHZ, duty=100-i, period=16000)
+        p = PWM(0, Pin("A17", mode=Pin.OUT), freq=PWM.FREQ_16MHZ, duty=100-i, period=16000)
         time.sleep_ms(10)
