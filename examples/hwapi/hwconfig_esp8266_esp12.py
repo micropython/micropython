297af6036ef28a8e1558b5ca0a2e6f689a7af474
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-01-01 20:05:35 +0300

    examples/hwapi: Use Signal for inverted LED on ESP-12.

--- examples/hwapi/hwconfig_esp8266_esp12.py
+++ examples/hwapi/hwconfig_esp8266_esp12.py
@@ -1,5 +1,5 @@
-from machine import Pin
+from machine import Pin, Signal
 
 # ESP12 module as used by many boards
-# Blue LED on pin 2
-LED = Pin(2, Pin.OUT)
+# Blue LED on pin 2, active low (inverted)
+LED = Signal(Pin(2, Pin.OUT), inverted=True)
