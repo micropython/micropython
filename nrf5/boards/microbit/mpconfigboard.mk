11fc46ca6f418b7a3a18d424d69835ffc78fe8a6
Glenn Ruben Bakke <glennbakke@gmail.com>
2017-01-03 17:02:49 +0100

    nrf5/boards: Adding initial microbit build files and board configurations.

---
+++ nrf5/boards/microbit/mpconfigboard.mk
@@ -0,0 +1,5 @@
+MCU_SERIES = m0
+MCU_VARIANT = nrf51
+LD_FILE = boards/nrf51822_aa.ld
+FLASHER = pyocd
+
---
+++ nrf5/boards/microbit/mpconfigboard_s110.mk
@@ -0,0 +1,4 @@
+MCU_SERIES = m0
+MCU_VARIANT = nrf51
+LD_FILE = boards/nrf51822_ac_s110.ld
+
