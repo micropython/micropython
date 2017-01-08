a722ed532ff254d1ce657466b7b1aac264433f7e
Damien George <damien.p.george@gmail.com>
2017-01-08 22:45:55 +1100

    tests/unix/extra_coverage: Add tests for importing frozen packages.

--- tests/unix/extra_coverage.py
+++ tests/unix/extra_coverage.py
@@ -17,3 +17,15 @@ print(hash(str(data[1], 'utf8')))
 # test basic import of frozen scripts
 import frzstr1
 import frzmpy1
+
+# test import of frozen packages with __init__.py
+import frzstr_pkg1
+print(frzstr_pkg1.x)
+import frzmpy_pkg1
+print(frzmpy_pkg1.x)
+
+# test import of frozen packages without __init__.py
+from frzstr_pkg2.mod import Foo
+print(Foo.x)
+from frzmpy_pkg2.mod import Foo
+print(Foo.x)
