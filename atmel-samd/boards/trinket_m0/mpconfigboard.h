24765929ea6babc9b24ae096c28e74bbf23da3ec
Scott Shawcroft <scott.shawcroft@gmail.com>
2017-01-08 22:11:27 -0800

    atmel-samd: Update pinouts for prototypes.

--- atmel-samd/boards/trinket_m0/mpconfigboard.h
+++ atmel-samd/boards/trinket_m0/mpconfigboard.h
@@ -3,10 +3,10 @@
 #define MICROPY_HW_BOARD_NAME "Adafruit Trinket M0 (Experimental)"
 #define MICROPY_HW_MCU_NAME "samd21e18"
 
-#define MICROPY_HW_APA102_MOSI   &pin_PA04
-#define MICROPY_HW_APA102_SCK    &pin_PA05
+#define MICROPY_HW_APA102_MOSI   &pin_PA01
+#define MICROPY_HW_APA102_SCK    &pin_PA00
 
-#define MICROPY_PORT_A        (PORT_PA04 | PORT_PA05 | PORT_PA24 | PORT_PA25)
+#define MICROPY_PORT_A        (PORT_PA00 | PORT_PA01 | PORT_PA24 | PORT_PA25)
 #define MICROPY_PORT_B        (0)
 
 #define AUTORESET_DELAY_MS 500
