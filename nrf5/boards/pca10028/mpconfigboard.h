a0b0e137859178052b4fbd77113a5d9f1e05fa38
Glenn Ruben Bakke <glennbakke@gmail.com>
2017-01-11 17:28:38 +0100

    nrf5/boards: Enable ADC machine module for pca10028, pca10040 and pca10056.

--- nrf5/boards/pca10028/mpconfigboard.h
+++ nrf5/boards/pca10028/mpconfigboard.h
@@ -34,6 +34,7 @@
 #define MICROPY_PY_MACHINE_TIMER    (1)
 #define MICROPY_PY_MACHINE_RTC      (1)
 #define MICROPY_PY_MACHINE_HW_I2C   (1)
+#define MICROPY_PY_MACHINE_ADC      (1)
 
 #define MICROPY_PY_USOCKET          (0)
 #define MICROPY_PY_NETWORK          (0)
