5314219f182e8a303e3fa68e2bacca978cb08002
Rami Ali <flowergrass@users.noreply.github.com>
2017-01-17 16:03:30 +1100

    tests/basics: Improve runtime.c test coverage.

--- tests/basics/fun_calldblstar3.py
+++ tests/basics/fun_calldblstar3.py
@@ -5,7 +5,8 @@ def foo(**kw):
 
 class Mapping:
     def keys(self):
-        return ['a', 'b', 'c']
+        # the long string checks the case of string interning
+        return ['a', 'b', 'c', 'abcdefghijklmnopqrst']
 
     def __getitem__(self, key):
         if key == 'a':
