20fc620327125e17ffa22493d1578835795bcc88
Damien George <damien.p.george@gmail.com>
2017-01-22 12:14:56 +1100

    tests/basics/builtin_help: Add test for help('modules').

--- tests/basics/builtin_help.py
+++ tests/basics/builtin_help.py
@@ -13,5 +13,6 @@ help(int) # help for a class
 help(1) # help for an instance
 import micropython
 help(micropython) # help for a module
+help('modules') # list available modules
 
 print('done') # so last bit of output is predictable
