d87d0141b35cff81c11e68eb231525e418b8f326
Glenn Ruben Bakke <glennbakke@gmail.com>
2017-01-26 21:58:12 +0100

    nrf5/hal: Updating uart hal to use pointers to Pin objects instead of uint pin and port number.

--- nrf5/hal/hal_uart.c
+++ nrf5/hal/hal_uart.c
@@ -100,32 +100,29 @@ void nrf_uart_buffer_read(uint8_t * p_buffer, uint32_t num_of_bytes, uart_comple
 }
 
 void nrf_uart_init(hal_uart_init_t const * p_uart_init) {
-    hal_gpio_cfg_pin(p_uart_init->tx_pin_port, p_uart_init->tx_pin, HAL_GPIO_MODE_OUTPUT, HAL_GPIO_PULL_DISABLED);
-    hal_gpio_cfg_pin(p_uart_init->tx_pin_port, p_uart_init->rx_pin, HAL_GPIO_MODE_INPUT, HAL_GPIO_PULL_DISABLED);
+    hal_gpio_cfg_pin(p_uart_init->tx_pin->port, p_uart_init->tx_pin->pin, HAL_GPIO_MODE_OUTPUT, HAL_GPIO_PULL_DISABLED);
+    hal_gpio_cfg_pin(p_uart_init->tx_pin->port, p_uart_init->rx_pin->pin, HAL_GPIO_MODE_INPUT, HAL_GPIO_PULL_DISABLED);
 
-    hal_gpio_pin_clear(p_uart_init->tx_pin_port, p_uart_init->tx_pin);
+    hal_gpio_pin_clear(p_uart_init->tx_pin->port, p_uart_init->tx_pin->pin);
 
-    UART_BASE->PSELTXD = p_uart_init->tx_pin;
+    UART_BASE->PSELTXD = p_uart_init->tx_pin->pin;
+    UART_BASE->PSELRXD = p_uart_init->rx_pin->pin;
+
 #if NRF52840_XXAA
-    UART_BASE->PSELTXD |= (p_uart_init->tx_pin_port << UARTE_PSEL_TXD_PORT_Pos);
+    UART_BASE->PSELTXD |= (p_uart_init->tx_pin->port << UARTE_PSEL_TXD_PORT_Pos);
+    UART_BASE->PSELRXD |= (p_uart_init->rx_pin->port << UARTE_PSEL_RXD_PORT_Pos);
 #endif
-    UART_BASE->PSELRXD = p_uart_init->rx_pin;
-#if NRF52840_XXAA
-    UART_BASE->PSELRXD |= (p_uart_init->rx_pin_port << UARTE_PSEL_RXD_PORT_Pos);
-#endif
+
     if (p_uart_init->flow_control) {
-#if MICROPY_HW_UART1_HWFC
-        hal_gpio_cfg_pin(p_uart_init->rts_pin_port, p_uart_init->rts_pin, HAL_GPIO_MODE_OUTPUT, HAL_GPIO_PULL_DISABLED);
-        hal_gpio_cfg_pin(p_uart_init->cts_pin_port, p_uart_init->cts_pin, HAL_GPIO_MODE_INPUT, HAL_GPIO_PULL_DISABLED);
+        hal_gpio_cfg_pin(p_uart_init->rts_pin->port, p_uart_init->rts_pin->pin, HAL_GPIO_MODE_OUTPUT, HAL_GPIO_PULL_DISABLED);
+        hal_gpio_cfg_pin(p_uart_init->cts_pin->port, p_uart_init->cts_pin->pin, HAL_GPIO_MODE_INPUT, HAL_GPIO_PULL_DISABLED);
 
-        UART_BASE->PSELCTS = p_uart_init->cts_pin;
+        UART_BASE->PSELCTS = p_uart_init->cts_pin->pin;
+        UART_BASE->PSELRTS = p_uart_init->rts_pin->pin;
+
 #if NRF52840_XXAA
-        UART_BASE->PSELCTS |= (p_uart_init->cts_pin_port << UARTE_PSEL_CTS_PORT_Pos);
-#endif
-        UART_BASE->PSELRTS = p_uart_init->rts_pin;
-#if NRF52840_XXAA
-        UART_BASE->PSELRTS |= (p_uart_init->rts_pin_port << UARTE_PSEL_RTS_PORT_Pos);
-#endif
+        UART_BASE->PSELCTS |= (p_uart_init->cts_pin->port << UARTE_PSEL_CTS_PORT_Pos);
+        UART_BASE->PSELRTS |= (p_uart_init->rts_pin->port << UARTE_PSEL_RTS_PORT_Pos);
 #endif
 
         UART_BASE->CONFIG  = (UART_CONFIG_HWFC_Enabled << UART_CONFIG_HWFC_Pos);
