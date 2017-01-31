1cded6cfa6b8fab856c7ee44af1a6063e878f915
Andrew Gatt <andrew.gatt@springltd.co>
2017-01-31 08:27:40 +0000

    /lib/oofatfs/ffconf.h: added MICROPY_FATFS_NORTC option

--- lib/oofatfs/ffconf.h
+++ lib/oofatfs/ffconf.h
@@ -278,7 +278,11 @@
 /  Note that enabling exFAT discards C89 compatibility. */
 
 
+#ifdef MICROPY_FATFS_NORTC
+#define _FS_NORTC   (MICROPY_FATFS_NORTC)
+#else
 #define _FS_NORTC   0
+#endif
 #define _NORTC_MON  1
 #define _NORTC_MDAY 1
 #define _NORTC_YEAR 2016
