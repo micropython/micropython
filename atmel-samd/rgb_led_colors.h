8569395b5d3a65545ebbdc0c3a59f1ebbccecdec
Scott Shawcroft <scott.shawcroft@gmail.com>
2017-02-01 13:15:46 -0800

    atmel-samd: Turn down the brightness of the status colors.

--- atmel-samd/rgb_led_colors.h
+++ atmel-samd/rgb_led_colors.h
@@ -1,19 +1,19 @@
 #define BLACK   0x000000
-#define GREEN   0x003f00
-#define BLUE    0x00003f
-#define CYAN    0x003f3f
-#define RED     0x3f0000
-#define ORANGE  0x3f1f00
-#define YELLOW  0x3f3f00
-#define PURPLE  0x3f003f
-#define WHITE   0x3f3f3f
+#define GREEN   0x001000
+#define BLUE    0x000010
+#define CYAN    0x001010
+#define RED     0x100000
+#define ORANGE  0x100800
+#define YELLOW  0x101000
+#define PURPLE  0x100010
+#define WHITE   0x101010
 
 #define BOOT_RUNNING BLUE
 #define MAIN_RUNNING GREEN
 #define ALL_DONE     GREEN
 #define REPL_RUNNING WHITE
 
-#define ACTIVE_WRITE 0x8f0000
+#define ACTIVE_WRITE 0x200000
 
 #define ALL_GOOD_CYCLE_MS 2000u
 
