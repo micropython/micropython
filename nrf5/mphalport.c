b6d54cbcafb7a2a2062caf5aac704ffce8c21d52
Glenn Ruben Bakke <glennbakke@gmail.com>
2017-02-04 16:15:49 +0100

    nrf5: Fallback to HW UART when not Bluetooth LE UART has been enabled.

--- nrf5/mphalport.c
+++ nrf5/mphalport.c
@@ -48,7 +48,7 @@ void mp_hal_set_interrupt_char(int c) {
 
 }
 
-#if (BLUETOOTH_SD != 132)
+#if (MICROPY_PY_BLE_NUS == 0)
 int mp_hal_stdin_rx_chr(void) {
     for (;;) {
         if (MP_STATE_PORT(pyb_stdio_uart) != NULL && uart_rx_any(MP_STATE_PORT(pyb_stdio_uart))) {
@@ -64,7 +64,7 @@ void mp_hal_stdout_tx_str(const char *str) {
     mp_hal_stdout_tx_strn(str, strlen(str));
 }
 
-#if (BLUETOOTH_SD != 132)
+#if (MICROPY_PY_BLE_NUS == 0)
 void mp_hal_stdout_tx_strn(const char *str, mp_uint_t len) {
     if (MP_STATE_PORT(pyb_stdio_uart) != NULL) {
         uart_tx_strn(MP_STATE_PORT(pyb_stdio_uart), str, len);
