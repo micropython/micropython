3cd518af3a730bf502df12e5ac09c9daa0e63b9c
Glenn Ruben Bakke <glennbakke@gmail.com>
2017-02-06 20:19:06 +0100

    nrf5/modules: Updating ubluepy with class function placeholders.

--- nrf5/modules/ubluepy/ubluepy_peripheral.c
+++ nrf5/modules/ubluepy/ubluepy_peripheral.c
@@ -28,4 +28,43 @@
 
 #if MICROPY_PY_UBLUEPY_PERIPHERAL
 
+STATIC const mp_map_elem_t ubluepy_peripheral_locals_dict_table[] = {
+#if 0
+
+// #if UBLUEPY_CENTRAL
+    { MP_OBJ_NEW_QSTR(MP_QSTR_connect),              (mp_obj_t)(&ubluepy_peripheral_connect_obj) },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_disconnect),           (mp_obj_t)(&ubluepy_peripheral_disconnect_obj) },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_getServices),          (mp_obj_t)(&ubluepy_peripheral_get_services_obj) },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_getServiceByUUID),     (mp_obj_t)(&ubluepy_peripheral_get_service_by_uuid_obj) },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_getCharacteristics),   (mp_obj_t)(&ubluepy_peripheral_get_chars_obj) },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_getDescriptors),       (mp_obj_t)(&ubluepy_peripheral_get_descs_obj) },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_withDelegate),         (mp_obj_t)(&ubluepy_peripheral_with_delegate_obj) },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_waitForNotifications), (mp_obj_t)(&ubluepy_peripheral_wait_for_notif_obj) },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_writeCharacteristic),  (mp_obj_t)(&ubluepy_peripheral_write_char_obj) },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_readCharacteristic),   (mp_obj_t)(&ubluepy_peripheral_read_char_obj) },
+// #if UBLUEPY_PERIPHERAL
+    { MP_OBJ_NEW_QSTR(MP_QSTR_advertise),            (mp_obj_t)(&ubluepy_peripheral_advertise_obj) },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_disconnect),           (mp_obj_t)(&ubluepy_peripheral_disconnect_obj) },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_setService),           (mp_obj_t)(&ubluepy_peripheral_set_service_obj) },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_setCharacteristic),    (mp_obj_t)(&ubluepy_peripheral_set_char_obj) },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_setDescriptor),        (mp_obj_t)(&ubluepy_peripheral_set_desc_obj) },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_writeCharacteristic),  (mp_obj_t)(&ubluepy_peripheral_write_char_obj) },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_readCharacteristic),   (mp_obj_t)(&ubluepy_peripheral_read_char_obj) },
+// #if UBLUEPY_BROADCASTER
+    { MP_OBJ_NEW_QSTR(MP_QSTR_advertise),            (mp_obj_t)(&ubluepy_peripheral_advertise_obj) },
+#endif
+};
+
+STATIC MP_DEFINE_CONST_DICT(ubluepy_peripheral_locals_dict, ubluepy_peripheral_locals_dict_table);
+
+const mp_obj_type_t ubluepy_peripheral_type = {
+    { &mp_type_type },
+    .name = MP_QSTR_Peripheral,
+#if 0
+    .print = ubluepy_peripheral_print,
+    .make_new = ubluepy_peripheral_make_new,
+    .locals_dict = (mp_obj_t)&ubluepy_peripheral_locals_dict
+#endif
+};
+
 #endif // MICROPY_PY_UBLUEPY_PERIPHERAL
