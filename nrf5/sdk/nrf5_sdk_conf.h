323533c3d35cbc361c65f2667de39b74027df2dd
Glenn Ruben Bakke <glennbakke@gmail.com>
2017-02-08 18:45:49 +0100

    nrf5/sdk: Adding configuration to enable the ubluepy peripheral class when using softdevice 132 from the SDK.

--- nrf5/sdk/nrf5_sdk_conf.h
+++ nrf5/sdk/nrf5_sdk_conf.h
@@ -19,6 +19,7 @@
 #define MICROPY_PY_BLE                  (1)
 #define MICROPY_PY_BLE_NUS              (0)
 #define MICROPY_PY_UBLUEPY              (1)
+#define MICROPY_PY_UBLUEPY_PERIPHERAL   (1)
 
 #else
 #error "SD not supported"
