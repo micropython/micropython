8a551e135486be91784e9a4cc38e6580a57821aa
Glenn Ruben Bakke <glennbakke@gmail.com>
2017-02-09 23:05:12 +0100

    nrf5/modules: Updating ubluepy UUID class with constructor that can construct an object based on hex value of 16-bit or string of 16-bit prefixed with '0x'.

--- nrf5/modules/ubluepy/ubluepy_uuid.c
+++ nrf5/modules/ubluepy/ubluepy_uuid.c
@@ -26,20 +26,35 @@
 
 #include "py/obj.h"
 #include "py/runtime.h"
+#include "py/objstr.h"
+#include "py/misc.h"
 
 #if MICROPY_PY_UBLUEPY
 
+// farward declare type
+const mp_obj_type_t ubluepy_uuid_type;
+
+typedef enum {
+    UBLUEPY_UUID_16_BIT,
+    UBLUEPY_UUID_128_BIT
+} ubluepy_uuid_type_t;
+
 typedef struct _ubluepy_uuid_obj_t {
-    mp_obj_base_t base;
-    // UUID value
+    mp_obj_base_t       base;
+    ubluepy_uuid_type_t type;
+    uint8_t             value[16];
 } ubluepy_uuid_obj_t;
 
 STATIC void ubluepy_uuid_print(const mp_print_t *print, mp_obj_t o, mp_print_kind_t kind) {
     ubluepy_uuid_obj_t * self = (ubluepy_uuid_obj_t *)o;
-    (void)self;
-    mp_printf(print, "UUID(value)");
+    if (self->type == UBLUEPY_UUID_16_BIT) {
+        mp_printf(print, "UUID(" HEX2_FMT HEX2_FMT ")", self->value[1], self->value[0]);
+    } else {
+
+    }
 }
 
+#include <stdio.h>
 STATIC mp_obj_t ubluepy_uuid_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *all_args) {
 
     enum { ARG_NEW_UUID };
@@ -54,6 +69,38 @@ STATIC mp_obj_t ubluepy_uuid_make_new(const mp_obj_type_t *type, size_t n_args, 
 
     ubluepy_uuid_obj_t *s = m_new_obj(ubluepy_uuid_obj_t);
     s->base.type = type;
+
+    mp_obj_t uuid_obj = args[ARG_NEW_UUID].u_obj;
+
+    if (uuid_obj == MP_OBJ_NULL) {
+        return MP_OBJ_FROM_PTR(s);
+    }
+
+    if (MP_OBJ_IS_INT(uuid_obj)) {
+        s->type = UBLUEPY_UUID_16_BIT;
+        s->value[1] = (((uint16_t)mp_obj_get_int(uuid_obj)) >> 8) & 0xFF;
+        s->value[0] = ((uint8_t)mp_obj_get_int(uuid_obj)) & 0xFF;
+    } else if (MP_OBJ_IS_STR(uuid_obj)) {
+        GET_STR_DATA_LEN(uuid_obj, str_data, str_len);
+        if (str_len == 6) { // Assume hex digit prefixed with 0x
+            s->value[0]  = unichar_xdigit_value(str_data[5]);
+            s->value[0] += unichar_xdigit_value(str_data[4]) << 4;
+            s->value[1]  = unichar_xdigit_value(str_data[3]);
+            s->value[1] += unichar_xdigit_value(str_data[2]) << 4;
+        } else if (str_len == 36) {
+            printf("string length 36\n");
+        } else {
+            nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_ValueError,
+                      "Invalid UUID string length"));
+        }
+    } else if (MP_OBJ_IS_TYPE(uuid_obj, &ubluepy_uuid_type)) {
+        printf("copy of UUID object\n");
+    } else {
+        nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_ValueError,
+                  "Invalid UUID parameter"));
+    }
+
+    // "6e400002-b5a3-f393-e0a9-e50e24dcca9e"
 
     // Check if UUID is of one of the following types and convert it:
     // int from 0 -> 0xFFFFFFFF
