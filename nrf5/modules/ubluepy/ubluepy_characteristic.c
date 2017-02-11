c127938103d11ac390b6a02105b53d47825e717b
Glenn Ruben Bakke <glennbakke@gmail.com>
2017-02-11 14:21:05 +0100

    nrf5/modules: Updating ubluepy characteristic implementation.

--- nrf5/modules/ubluepy/ubluepy_characteristic.c
+++ nrf5/modules/ubluepy/ubluepy_characteristic.c
@@ -25,8 +25,48 @@
  */
 
 #include "py/obj.h"
+#include "py/runtime.h"
+#include "modubluepy.h"
 
 #if MICROPY_PY_UBLUEPY_PERIPHERAL || MICROPY_PY_UBLUEPY_CENTRAL
+
+STATIC void ubluepy_characteristic_print(const mp_print_t *print, mp_obj_t o, mp_print_kind_t kind) {
+    ubluepy_characteristic_obj_t * self = (ubluepy_characteristic_obj_t *)o;
+
+    mp_printf(print, "Characteristic(handle: 0x" HEX2_FMT ")", self->handle);
+}
+
+STATIC mp_obj_t ubluepy_characteristic_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *all_args) {
+
+    enum { ARG_NEW_UUID };
+
+    static const mp_arg_t allowed_args[] = {
+        { ARG_NEW_UUID, MP_ARG_OBJ, {.u_obj = MP_OBJ_NULL} },
+    };
+
+    // parse args
+    mp_arg_val_t args[MP_ARRAY_SIZE(allowed_args)];
+    mp_arg_parse_all_kw_array(n_args, n_kw, all_args, MP_ARRAY_SIZE(allowed_args), allowed_args, args);
+
+    ubluepy_characteristic_obj_t *s = m_new_obj(ubluepy_characteristic_obj_t);
+    s->base.type = type;
+
+    mp_obj_t uuid_obj = args[ARG_NEW_UUID].u_obj;
+
+    if (uuid_obj == MP_OBJ_NULL) {
+        return MP_OBJ_FROM_PTR(s);
+    }
+
+    if (MP_OBJ_IS_TYPE(uuid_obj, &ubluepy_uuid_type)) {
+        s->p_uuid = MP_OBJ_TO_PTR(uuid_obj);
+        // (void)sd_characterstic_add(s);
+    } else {
+        nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_ValueError,
+                  "Invalid UUID parameter"));
+    }
+
+    return MP_OBJ_FROM_PTR(s);
+}
 
 STATIC const mp_map_elem_t ubluepy_characteristic_locals_dict_table[] = {
 #if 0
@@ -48,11 +88,9 @@ STATIC MP_DEFINE_CONST_DICT(ubluepy_characteristic_locals_dict, ubluepy_characte
 const mp_obj_type_t ubluepy_characteristic_type = {
     { &mp_type_type },
     .name = MP_QSTR_Characteristic,
-#if 0
     .print = ubluepy_characteristic_print,
     .make_new = ubluepy_characteristic_make_new,
     .locals_dict = (mp_obj_t)&ubluepy_characteristic_locals_dict
-#endif
 };
 
 #endif // MICROPY_PY_UBLUEPY_PERIPHERAL || MICROPY_PY_UBLUEPY_CENTRAL
