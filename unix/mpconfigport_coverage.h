1cd8906cfe6df4d75aa48df49376dd1969493a39
dmazzella <damianomazzella@gmail.com>
2017-02-13 12:31:27 +0100

    disabled MICROPY_PY_COLLECTIONS already enabled from unix/mpconfigport.h

--- unix/mpconfigport_coverage.h
+++ unix/mpconfigport_coverage.h
@@ -32,7 +32,6 @@
 
 #include <mpconfigport.h>
 
-#define MICROPY_PY_COLLECTIONS      (1)
 #define MICROPY_PY_COLLECTIONS_DEQUE (1)
 #define MICROPY_PY_DELATTR_SETATTR     (1)
 #define MICROPY_PY_BUILTINS_HELP       (1)
