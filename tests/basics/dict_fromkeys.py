7bb146350e5de1f406dc69679ecdf4cdded5be75
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-02-15 01:30:16 +0300

    tests/dict_fromkeys: Revert to use reversed() to run in native codegen mode.

--- tests/basics/dict_fromkeys.py
+++ tests/basics/dict_fromkeys.py
@@ -9,6 +9,6 @@ l.sort()
 print(l)
 
 # argument to fromkeys has no __len__
-#d = dict.fromkeys(reversed(range(1)))
-d = dict.fromkeys((x for x in range(1)))
+d = dict.fromkeys(reversed(range(1)))
+#d = dict.fromkeys((x for x in range(1)))
 print(d)
