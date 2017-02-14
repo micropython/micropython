687783821c3d69c65c2f5c35f798bb983747dc4b
Rami Ali <flowergrass@users.noreply.github.com>
2017-02-14 16:46:23 +1100

    docs: Move differences script to tools and tests to /tests/cpydiff.

--- tests/cpydiff/core_import_prereg.py
+++ tests/cpydiff/core_import_prereg.py
@@ -4,12 +4,14 @@ description: Failed to load modules are still registered as loaded
 cause: Unknown
 workaround: Unknown
 """
+import sys
+
 try:
-    from testmodules import foo
+    from modules import foo
 except NameError as e:
     print(e)
 try:
-    from testmodules import foo
+    from modules import foo
     print('Should not get here')
 except NameError as e:
     print(e)
