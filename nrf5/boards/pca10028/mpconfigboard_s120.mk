e60a06563ec8ebb277d630cdac0b964525f8855e
Glenn Ruben Bakke <glennbakke@gmail.com>
2017-02-24 15:03:53 +0100

    nrf5/boards: Updating pca10028 bluetooth stack targets to have a MCU_SUB_VARIANT.

--- nrf5/boards/pca10028/mpconfigboard_s120.mk
+++ nrf5/boards/pca10028/mpconfigboard_s120.mk
@@ -1,3 +1,4 @@
 MCU_SERIES = m0
 MCU_VARIANT = nrf51
+MCU_SUB_VARIANT = nrf51822
 LD_FILE = boards/nrf51822_ac_s120.ld
--- nrf5/boards/pca10028/mpconfigboard_s130.mk
+++ nrf5/boards/pca10028/mpconfigboard_s130.mk
@@ -1,4 +1,5 @@
 MCU_SERIES = m0
 MCU_VARIANT = nrf51
+MCU_SUB_VARIANT = nrf51822
 LD_FILE = boards/nrf51822_ac_s130.ld
 
