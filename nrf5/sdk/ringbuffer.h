235c0edacb7274857bd1725dd7757ff18e55bf2f
Glenn Ruben Bakke <glennbakke@gmail.com>
2017-03-03 00:11:42 +0100

    nrf5/sdk: Updating ringbuffer.h to use volatile variables for start and end.

--- nrf5/sdk/ringbuffer.h
+++ nrf5/sdk/ringbuffer.h
@@ -67,8 +67,8 @@
 #define ringBuffer_typedef(T, NAME) \
   typedef struct { \
     int size; \
-    int start; \
-    int end; \
+    volatile int start; \
+    volatile int end; \
     T* elems; \
   } NAME
 
