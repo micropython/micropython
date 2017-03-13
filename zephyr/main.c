9c9674a32599b8f9268fb479fc1cb3d62a5ec2f0
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-03-14 00:19:35 +0300

    zephyr/main: Remove unused __fatal_error().

--- zephyr/main.c
+++ zephyr/main.c
@@ -146,10 +146,6 @@ NORETURN void nlr_jump_fail(void *val) {
     while (1);
 }
 
-void NORETURN __fatal_error(const char *msg) {
-    while (1);
-}
-
 #ifndef NDEBUG
 void MP_WEAK __assert_func(const char *file, int line, const char *func, const char *expr) {
     printf("Assertion '%s' failed, at file %s:%d\n", expr, file, line);
