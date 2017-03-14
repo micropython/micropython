3c29c868d4e311c9c27b712306f3fa45bf20c339
Rami Ali <flowergrass@users.noreply.github.com>
2017-03-14 17:58:43 +1100

    tests: Improve binary.c test coverage.

--- tests/basics/struct_micropython.py
+++ tests/basics/struct_micropython.py
@@ -9,6 +9,7 @@ except:
         import sys
         print("SKIP")
         sys.exit()
+import uctypes
 
 class A():
     pass
@@ -18,3 +19,9 @@ o = A()
 s = struct.pack("<O", o)
 o2 = struct.unpack("<O", s)
 print(o is o2[0])
+
+ # get_val for pointer to string
+o = uctypes.addressof('abc')
+s = struct.pack("<S", o)
+o2 = struct.unpack("<S", s)
+print('abc' == o2[0])
