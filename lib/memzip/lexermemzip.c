41b1df604617bdde59bb722b9247c16fa4677d94
Damien George <damien.p.george@gmail.com>
2017-03-14 11:36:24 +1100

    lib/memzip: Make lexer constructor raise exception when file not found.

--- lib/memzip/lexermemzip.c
+++ lib/memzip/lexermemzip.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 
 #include "py/lexer.h"
+#include "py/runtime.h"
+#include "py/mperrno.h"
 #include "memzip.h"
 
 mp_lexer_t *mp_lexer_new_from_file(const char *filename)
@@ -9,7 +11,7 @@ mp_lexer_t *mp_lexer_new_from_file(const char *filename)
     size_t len;
 
     if (memzip_locate(filename, &data, &len) != MZ_OK) {
-        return NULL;
+        mp_raise_OSError(MP_ENOENT);
     }
 
     return mp_lexer_new_from_str_len(qstr_from_str(filename), (const char *)data, (mp_uint_t)len, 0);
