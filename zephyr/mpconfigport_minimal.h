344bfbb71c0cc12a69d06958cfcd583a14a1a360
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-03-30 22:17:18 +0300

    zephyr: Fix NLR segfault in minimal build.
    Requires inclusion of zephyr.h to properly detect that we're building for
    Zephyr.

--- zephyr/mpconfigport_minimal.h
+++ zephyr/mpconfigport_minimal.h
@@ -27,6 +27,8 @@
 
 // Include Zephyr's autoconf.h, which should be made first by Zephyr makefiles
 #include "autoconf.h"
+// Included here to get basic Zephyr environment (macros, etc.)
+#include <zephyr.h>
 
 // Usually passed from Makefile
 #ifndef MICROPY_HEAP_SIZE
