b6fff4186de098946cc1e4c0204f78936f73044f
Damien George <damien.p.george@gmail.com>
2017-04-05 12:38:18 +1000

    tests/basics: Add test for tuple inplace add.

--- tests/basics/tuple1.py
+++ tests/basics/tuple1.py
@@ -17,6 +17,10 @@ print(x[2:3])
 
 print(x + (10, 100, 10000))
 
+# inplace add operator
+x += (10, 11, 12)
+print(x)
+
 # construction of tuple from large iterator (tests implementation detail of uPy)
 print(tuple(range(20)))
 
