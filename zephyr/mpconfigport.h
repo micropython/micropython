0a88b44248a7fafb01e98019e4b79547772ea206
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-04-08 14:27:36 +0300

    zephyr/machine_pin: Implement pin protocol for machine.Signal support.

--- zephyr/mpconfigport.h
+++ zephyr/mpconfigport.h
@@ -58,6 +58,7 @@
 #define MICROPY_PY_IO               (0)
 #define MICROPY_PY_MICROPYTHON_MEM_INFO (1)
 #define MICROPY_PY_MACHINE          (1)
+#define MICROPY_PY_MACHINE_PIN_MAKE_NEW mp_pin_make_new
 #define MICROPY_MODULE_WEAK_LINKS   (1)
 #define MICROPY_PY_STRUCT           (0)
 #ifdef CONFIG_NETWORKING
