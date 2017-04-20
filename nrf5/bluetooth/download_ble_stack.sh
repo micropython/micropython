f6f8097f77fb84d9c9a03f5ab491e29541790ccf
Glenn Ruben Bakke <glennbakke@gmail.com>
2017-04-20 23:54:10 +0200

    nrf5/bluetooth: Adding bash script to automate download of bluetooth le stacks

---
+++ nrf5/bluetooth/download_ble_stack.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+function download_s110_nrf51
+{
+    echo "### Downloading nrf51_s110 ###"
+    wget https://developer.nordicsemi.com/nRF5_SDK/nRF51_SDK_v10.x.x/nRF51_SDK_10.0.0_dc26b5e.zip
+    unzip nRF51_SDK_10.0.0_dc26b5e.zip components/softdevice/s110/*
+    mv components s110_nrf51
+    rm nRF51_SDK_10.0.0_dc26b5e.zip
+}
+
+function download_s132_nrf52
+{
+    echo "### Downloading nrf52_s132 ###"
+    wget https://developer.nordicsemi.com/nRF5_SDK/nRF5_SDK_v12.x.x/nRF5_SDK_12.1.0_0d23e2a.zip
+    unzip nRF5_SDK_12.1.0_0d23e2a.zip components/softdevice/s132/*
+    mv components s132_nrf52
+    rm nRF5_SDK_12.1.0_0d23e2a.zip
+}
+
+cd bluetooth
+
+if [ $# -eq 0 ]; then 
+    echo "No Bluetooth LE stack defined, downloading all."
+    download_s110_nrf51
+    download_s132_nrf52
+else 
+    case $1 in
+        "s110_nrf51" )
+            download_s110_nrf51 ;;
+        "s132_nrf52" )
+            download_s132_nrf52 ;;
+    esac
+fi
+
+cd ..
+
+exit 0
