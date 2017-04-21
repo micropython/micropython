10b7f3ef83a0168092848d50f34a616ab27f7447
Glenn Ruben Bakke <glennbakke@gmail.com>
2017-04-21 18:46:32 +0200

    nrf5/bluetooth: Including bluetooth stack version in folder name after download to be able to detect if stack has been updated.

--- nrf5/bluetooth/download_ble_stack.sh
+++ nrf5/bluetooth/download_ble_stack.sh
@@ -3,28 +3,28 @@
 function download_s110_nrf51
 {
     echo ""
-    echo "##############################"
-    echo "### Downloading s110_nrf51 ###"
-    echo "##############################"
+    echo "####################################"
+    echo "### Downloading s110_nrf51_8.0.0 ###"
+    echo "####################################"
     echo ""
 
     wget https://developer.nordicsemi.com/nRF5_SDK/nRF51_SDK_v10.x.x/nRF51_SDK_10.0.0_dc26b5e.zip
     unzip nRF51_SDK_10.0.0_dc26b5e.zip components/softdevice/s110/*
-    mv components s110_nrf51
+    mv components s110_nrf51_8.0.0
     rm nRF51_SDK_10.0.0_dc26b5e.zip
 }
 
 function download_s132_nrf52
 {
     echo ""
-    echo "##############################"
-    echo "### Downloading s132_nrf52 ###"
-    echo "##############################"
+    echo "####################################"
+    echo "### Downloading s132_nrf52_3.0.0 ###"
+    echo "####################################"
     echo ""
 
     wget https://developer.nordicsemi.com/nRF5_SDK/nRF5_SDK_v12.x.x/nRF5_SDK_12.1.0_0d23e2a.zip
     unzip nRF5_SDK_12.1.0_0d23e2a.zip components/softdevice/s132/*
-    mv components s132_nrf52
+    mv components s132_nrf52_3.0.0
     rm nRF5_SDK_12.1.0_0d23e2a.zip
 }
 
