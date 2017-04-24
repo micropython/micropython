39d3d7bb280ab62ce7ab8e97ed4058e741f11366
Glenn Ruben Bakke <glennbakke@gmail.com>
2017-04-24 21:56:28 +0200

    nrf5/bluetooth: Add support for downloading s132_2.0.1 bluetooth stack.

--- nrf5/bluetooth/download_ble_stack.sh
+++ nrf5/bluetooth/download_ble_stack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function download_s110_nrf51
+function download_s110_nrf51_8_0_0
 {
     echo ""
     echo "####################################"
@@ -8,16 +8,34 @@ function download_s110_nrf51
     echo "####################################"
     echo ""
 
-    mkdir s110_nrf51_8.0.0
+    mkdir -p s110_nrf51_8.0.0
     cd s110_nrf51_8.0.0
     wget https://www.nordicsemi.com/eng/nordic/download_resource/45846/3/78153065/80234
     mv 80234 temp.zip
-    unzip temp.zip
+    unzip -u temp.zip
     rm temp.zip
     cd -
 }
 
-function download_s132_nrf52
+function download_s132_nrf52_2_0_1
+{
+    echo ""
+    echo "####################################"
+    echo "### Downloading s132_nrf52_2.0.1 ###"
+    echo "####################################"
+    echo ""
+
+    mkdir -p s132_nrf52_2.0.1
+    cd s132_nrf52_2.0.1
+
+    wget https://www.nordicsemi.com/eng/nordic/download_resource/51479/6/84640562/95151
+    mv 95151 temp.zip
+    unzip -u temp.zip
+    rm temp.zip
+    cd -
+}
+
+function download_s132_nrf52_3_0_0
 {
     echo ""
     echo "####################################"
@@ -25,26 +43,29 @@ function download_s132_nrf52
     echo "####################################"
     echo ""
 
-    mkdir s132_nrf52_3.0.0
+    mkdir -p s132_nrf52_3.0.0
     cd s132_nrf52_3.0.0
 
     wget https://www.nordicsemi.com/eng/nordic/download_resource/56261/6/26298825/108144
     mv 108144 temp.zip
-    unzip temp.zip
+    unzip -u temp.zip
     rm temp.zip
     cd -
 }
 
 if [ $# -eq 0 ]; then 
     echo "No Bluetooth LE stack defined, downloading all."
-    download_s110_nrf51
-    download_s132_nrf52
+    download_s110_nrf51_8_0_0
+    download_s132_nrf52_2_0_1
+    download_s132_nrf52_3_0_0
 else 
     case $1 in
         "s110_nrf51" )
-            download_s110_nrf51 ;;
-        "s132_nrf52" )
-            download_s132_nrf52 ;;
+            download_s110_nrf51_8_0_0 ;;
+        "s132_nrf52_2_0_1" )
+            download_s132_nrf52_2_0_1 ;;
+        "s132_nrf52_3_0_0" )
+            download_s132_nrf52_3_0_0 ;;
     esac
 fi
 
