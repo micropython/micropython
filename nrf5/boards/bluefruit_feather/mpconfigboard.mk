0ba9c62940a1a65f6673531dfe25a5dc0a992c03
Glenn Ruben Bakke <glennbakke@gmail.com>
2017-04-24 23:25:09 +0200

    nrf5/boards: Adding template board makefiles and configs for bluefruit nrf52 feather. Copied from pca10040 target board. Linker script reference updated to use s132 v2.0.1. Non-BLE enable build disabled for now. Board configuration for leds, uart etc has not been updated yet from pca10040 layout.

---
+++ nrf5/boards/bluefruit_feather/mpconfigboard.mk
@@ -0,0 +1,13 @@
+
+define HELPER
+
+#########################################################
+#                                                       #
+# Use SD=s132 make flag to generate a firmware suitable #
+# for bluefruit nrf52 feather.                          #
+#                                                       #
+#########################################################
+
+endef
+
+$(error $(HELPER))
