7bcc61b0c1b7397a25f3a6ad088c9681cad6b759
Glenn Ruben Bakke <glennbakke@gmail.com>
2017-04-26 00:18:10 +0200

    nrf5/boards: Adding board target for feather52 using s132 v.2.0.1 application offset even if the device is not using softdevice. To be worked on later.

--- nrf5/boards/feather52/mpconfigboard.mk
+++ nrf5/boards/feather52/mpconfigboard.mk
@@ -1,13 +1,16 @@
+MCU_SERIES = m4
+MCU_VARIANT = nrf52
+MCU_SUB_VARIANT = nrf52832
+SOFTDEV_VERSION = 2.0.1
 
-define HELPER
+LD_FILE = boards/feather52/custom_nrf52832_dfu_app.ld
 
-#########################################################
-#                                                       #
-# Use SD=s132 make flag to generate a firmware suitable #
-# for bluefruit nrf52 feather.                          #
-#                                                       #
-#########################################################
+NRF_DEFINES += -DNRF52832_XXAA
 
-endef
+.PHONY: dfu-gen dfu-flash
 
-$(error $(HELPER))
+dfu-gen:
+	nrfutil dfu genpkg --dev-type 0x0052 --application $(BUILD)/$(OUTPUT_FILENAME).hex $(BUILD)/dfu-package.zip
+
+dfu-flash:
+	sudo nrfutil dfu serial --package $(BUILD)/dfu-package.zip -p /dev/ttyACM3
