f9b9d59434a9adb5ef37269426db732ca2648750
Glenn Ruben Bakke <glennbakke@gmail.com>
2017-05-01 19:06:49 +0200

    nrf5/modules/microbit: Copying microbit music module to the port.

---
+++ nrf5/modules/microbit/microbitmusic.h
@@ -0,0 +1,10 @@
+#ifndef __MICROPY_INCLUDED_MICROBIT_MUSIC_H__
+#define __MICROPY_INCLUDED_MICROBIT_MUSIC_H__
+
+extern "C" {
+
+void microbit_music_tick(void);
+
+}
+
+#endif // __MICROPY_INCLUDED_MICROBIT_MUSIC_H__
