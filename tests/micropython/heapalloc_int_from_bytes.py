dce7dd425977320b822f8bb529d123eafebe6e55
Damien George <damien.p.george@gmail.com>
2017-05-05 21:44:12 +1000

    tests/micropython: Add test for int.from_bytes with many zero bytes.

--- tests/micropython/heapalloc_int_from_bytes.py
+++ tests/micropython/heapalloc_int_from_bytes.py
@@ -5,4 +5,5 @@ import micropython
 micropython.heap_lock()
 print(int.from_bytes(b"1", "little"))
 print(int.from_bytes(b"12", "little"))
+print(int.from_bytes(b"2\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00", "little"))
 micropython.heap_unlock()
