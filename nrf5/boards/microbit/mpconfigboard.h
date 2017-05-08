bf796998103359e7f879f502fd79e68db7d94654
Glenn Ruben Bakke <glennbakke@gmail.com>
2017-05-08 22:16:59 +0200

    nrf5/boards: Adding micro:bit default music pin definition. Also adding config flag for enabling pwm machine module.

--- nrf5/boards/microbit/mpconfigboard.h
+++ nrf5/boards/microbit/mpconfigboard.h
@@ -30,6 +30,7 @@
 #define MICROPY_HW_MCU_NAME         "NRF51822"
 #define MICROPY_PY_SYS_PLATFORM     "nrf51"
 
+#define MICROPY_PY_MACHINE_SOFT_PWM (0)
 #define MICROPY_PY_MACHINE_HW_SPI   (1)
 #define MICROPY_PY_MACHINE_TIMER    (1)
 #define MICROPY_PY_MACHINE_RTC      (1)
@@ -67,5 +68,8 @@
 #define MICROPY_HW_SPI0_SCK         (pin_A13)
 #define MICROPY_HW_SPI0_MOSI        (pin_A15)
 #define MICROPY_HW_SPI0_MISO        (pin_A14)
+
+// micro:bit music pin
+#define MICROPY_HW_MUSIC_PIN        (pin_A3)
 
 #define HELP_TEXT_BOARD_LED         "1,2,3,4"
