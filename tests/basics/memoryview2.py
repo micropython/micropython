e711e2d44a24024fcd7d7f5b39f285f979b66a77
Damien George <damien.p.george@gmail.com>
2017-05-09 10:49:19 +1000

    tests/basics: Add memoryview test for big ints.

--- tests/basics/memoryview2.py
+++ tests/basics/memoryview2.py
@@ -12,7 +12,3 @@ print(list(memoryview(array('b', [0x7f, -0x80]))))
 print(list(memoryview(array('B', [0x7f, 0x80, 0x81, 0xff]))))
 print(list(memoryview(array('h', [0x7f00, -0x8000]))))
 print(list(memoryview(array('H', [0x7f00, 0x8000, 0x8100, 0xffff]))))
-
-# these constructors give an internal overflow in uPy
-#print(list(memoryview(array('i', [0x7f000000, -0x80000000]))))
-#print(list(memoryview(array('I', [0x7f000000, 0x80000000, 0x81000000, 0xffffffff]))))
