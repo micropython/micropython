4b235800a4ebba19a98bba50a5a98d2c407d99a7
puuu <puuu@users.noreply.github.com>
2017-05-11 11:50:01 +0900

    esp8266/modnetwork: In connect, fix potential buffer overflows.

--- esp8266/modnetwork.c
+++ esp8266/modnetwork.c
@@ -102,13 +102,13 @@ STATIC mp_obj_t esp_connect(mp_uint_t n_args, const mp_obj_t *args) {
 
     if (n_args > 1) {
         p = mp_obj_str_get_data(args[1], &len);
+        len = MIN(len, sizeof(config.ssid));
         memcpy(config.ssid, p, len);
         if (n_args > 2) {
             p = mp_obj_str_get_data(args[2], &len);
-        } else {
-            p = "";
+            len = MIN(len, sizeof(config.password));
+            memcpy(config.password, p, len);
         }
-        memcpy(config.password, p, len);
 
         error_check(wifi_station_set_config(&config), "Cannot set STA config");
     }
