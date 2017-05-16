a004554dc168049f3656480c7b34608741f76765
Damien George <damien.p.george@gmail.com>
2017-05-16 17:44:14 +1000

    stmhal/modmachine: Remove TODO comment that is now implemented.

--- stmhal/modmachine.c
+++ stmhal/modmachine.c
@@ -554,8 +554,6 @@ STATIC const mp_rom_map_elem_t machine_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_RTC),                 MP_ROM_PTR(&pyb_rtc_type) },
     { MP_ROM_QSTR(MP_QSTR_ADC),                 MP_ROM_PTR(&pyb_adc_type) },
 #endif
-    // TODO: Per new API, I2C types below, if called with 1 arg (ID), should still
-    // initialize master mode on the peripheral.
     { MP_ROM_QSTR(MP_QSTR_I2C),                 MP_ROM_PTR(&machine_i2c_type) },
     { MP_ROM_QSTR(MP_QSTR_SPI),                 MP_ROM_PTR(&machine_hard_spi_type) },
     { MP_ROM_QSTR(MP_QSTR_UART),                MP_ROM_PTR(&pyb_uart_type) },
