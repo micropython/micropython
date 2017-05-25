cb9ed6bb55a5020770ad31fd30a05b4a91b70a81
Glenn Ruben Bakke <glennbakke@gmail.com>
2017-05-25 22:19:12 +0200

    nrf5/drivers/bluetooth: Enable ubluepy central by default if running nrf52/s132 bluetooth stack. Maturity of the module is pretty OK now.

--- nrf5/bluetooth_conf.h
+++ nrf5/bluetooth_conf.h
@@ -18,7 +18,7 @@
 #define BLUETOOTH_WEBBLUETOOTH_REPL     (0)
 #define MICROPY_PY_UBLUEPY              (1)
 #define MICROPY_PY_UBLUEPY_PERIPHERAL   (1)
-#define MICROPY_PY_UBLUEPY_CENTRAL      (0)
+#define MICROPY_PY_UBLUEPY_CENTRAL      (1)
 
 #else
 #error "SD not supported"
