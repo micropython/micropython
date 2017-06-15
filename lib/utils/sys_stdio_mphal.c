4f9858e86debaaa4a65f0663ac3c67be15000064
Damien George <damien.p.george@gmail.com>
2017-06-15 18:55:34 +1000

    stmhal: Move pybstdio.c to lib/utils/sys_stdio_mphal.c for common use.
    It provides sys.stdin, sys.stdout, sys.stderr for bare-metal targets based
    on mp_hal functions.

--- lib/utils/sys_stdio_mphal.c
+++ lib/utils/sys_stdio_mphal.c
@@ -1,9 +1,9 @@
 /*
- * This file is part of the Micro Python project, http://micropython.org/
+ * This file is part of the MicroPython project, http://micropython.org/
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2013, 2014, 2015 Damien P. George
+ * Copyright (c) 2013-2017 Damien P. George
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -37,28 +37,28 @@
 // objects are in a read-only module (py/modsys.c).
 
 /******************************************************************************/
-// Micro Python bindings
+// MicroPython bindings
 
 #define STDIO_FD_IN  (0)
 #define STDIO_FD_OUT (1)
 #define STDIO_FD_ERR (2)
 
-typedef struct _pyb_stdio_obj_t {
+typedef struct _sys_stdio_obj_t {
     mp_obj_base_t base;
     int fd;
-} pyb_stdio_obj_t;
+} sys_stdio_obj_t;
 
 #if MICROPY_PY_SYS_STDIO_BUFFER
-STATIC const pyb_stdio_obj_t stdio_buffer_obj;
+STATIC const sys_stdio_obj_t stdio_buffer_obj;
 #endif
 
 void stdio_obj_print(const mp_print_t *print, mp_obj_t self_in, mp_print_kind_t kind) {
-    pyb_stdio_obj_t *self = self_in;
+    sys_stdio_obj_t *self = self_in;
     mp_printf(print, "<io.FileIO %d>", self->fd);
 }
 
 STATIC mp_uint_t stdio_read(mp_obj_t self_in, void *buf, mp_uint_t size, int *errcode) {
-    pyb_stdio_obj_t *self = self_in;
+    sys_stdio_obj_t *self = self_in;
     if (self->fd == STDIO_FD_IN) {
         for (uint i = 0; i < size; i++) {
             int c = mp_hal_stdin_rx_chr();
@@ -75,7 +75,7 @@ STATIC mp_uint_t stdio_read(mp_obj_t self_in, void *buf, mp_uint_t size, int *er
 }
 
 STATIC mp_uint_t stdio_write(mp_obj_t self_in, const void *buf, mp_uint_t size, int *errcode) {
-    pyb_stdio_obj_t *self = self_in;
+    sys_stdio_obj_t *self = self_in;
     if (self->fd == STDIO_FD_OUT || self->fd == STDIO_FD_ERR) {
         mp_hal_stdout_tx_strn_cooked(buf, size);
         return size;
@@ -126,9 +126,9 @@ STATIC const mp_obj_type_t stdio_obj_type = {
     .locals_dict = (mp_obj_dict_t*)&stdio_locals_dict,
 };
 
-const pyb_stdio_obj_t mp_sys_stdin_obj = {{&stdio_obj_type}, .fd = STDIO_FD_IN};
-const pyb_stdio_obj_t mp_sys_stdout_obj = {{&stdio_obj_type}, .fd = STDIO_FD_OUT};
-const pyb_stdio_obj_t mp_sys_stderr_obj = {{&stdio_obj_type}, .fd = STDIO_FD_ERR};
+const sys_stdio_obj_t mp_sys_stdin_obj = {{&stdio_obj_type}, .fd = STDIO_FD_IN};
+const sys_stdio_obj_t mp_sys_stdout_obj = {{&stdio_obj_type}, .fd = STDIO_FD_OUT};
+const sys_stdio_obj_t mp_sys_stderr_obj = {{&stdio_obj_type}, .fd = STDIO_FD_ERR};
 
 #if MICROPY_PY_SYS_STDIO_BUFFER
 STATIC mp_uint_t stdio_buffer_read(mp_obj_t self_in, void *buf, mp_uint_t size, int *errcode) {
@@ -159,5 +159,5 @@ STATIC const mp_obj_type_t stdio_buffer_obj_type = {
     .locals_dict = (mp_obj_t)&stdio_locals_dict,
 };
 
-STATIC const pyb_stdio_obj_t stdio_buffer_obj = {{&stdio_buffer_obj_type}, .fd = 0}; // fd unused
+STATIC const sys_stdio_obj_t stdio_buffer_obj = {{&stdio_buffer_obj_type}, .fd = 0}; // fd unused
 #endif
