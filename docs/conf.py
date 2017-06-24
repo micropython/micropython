ba33c544bb9b5bd0476e07a12a34d2f72579075e
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-06-25 00:57:44 +0300

    docs/conf.py: Set default_role = 'any'.
    This causes `symbol` syntax to be equivalent to :any:`symbol`, which is
    in turn the easiest way to cross-reference an arbitrary symbol in the
    docs:
    
    http://www.sphinx-doc.org/en/stable/markup/inline.html#role-any
    
    :any: requires at least Sphinx 1.3 (for reference, Ubuntu 16.03 ships
    with 1.3.6, the latest 1.6.3).
    
    Any many of our docs, `symbol` is misused to specify arguments to
    functions, etc. Refactoring that is in progress. (CODECONVENTIONS
    already specify proper syntax for both arguments and xrefs, based
    on CPython conventions).

--- docs/conf.py
+++ docs/conf.py
@@ -117,7 +117,7 @@ exclude_patterns = ['build']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
-#default_role = None
+default_role = 'any'
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
 #add_function_parentheses = True
