ebb93962741d68e4530cb89d207641c1d663f61f
Alexander Steffen <Alexander.Steffen@infineon.com>
2017-06-27 17:13:09 +0200

    esp8266,minimal,pic16bit: Use size_t for mp_builtin_open argument.
    py/builtin.h declares mp_builtin_open with the first argument of type
    size_t.  Make all implementations conform to this declaration.

--- minimal/main.c
+++ minimal/main.c
@@ -77,7 +77,7 @@ mp_import_stat_t mp_import_stat(const char *path) {
     return MP_IMPORT_STAT_NO_EXIST;
 }
 
-mp_obj_t mp_builtin_open(uint n_args, const mp_obj_t *args, mp_map_t *kwargs) {
+mp_obj_t mp_builtin_open(size_t n_args, const mp_obj_t *args, mp_map_t *kwargs) {
     return mp_const_none;
 }
 MP_DEFINE_CONST_FUN_OBJ_KW(mp_builtin_open_obj, 1, mp_builtin_open);
