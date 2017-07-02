5f0c56bcf1fcb7f2d36a0c420b4681a812047c59
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-07-02 14:34:20 +0300

    docs/conf.py: Switch to "new" format of intersphinx_mapping.
    As described at
    http://www.sphinx-doc.org/en/stable/ext/intersphinx.html#confval-intersphinx_mapping
    
    This will allow to explicitly refer to CPython docs for cross-references.

--- docs/conf.py
+++ docs/conf.py
@@ -317,7 +317,7 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'http://docs.python.org/': None}
+intersphinx_mapping = {'python': ('http://docs.python.org/3', None)}
 
 # Append the other ports' specific folders/files to the exclude pattern
 exclude_patterns.extend([port + '*' for port in ports if port != micropy_port])
