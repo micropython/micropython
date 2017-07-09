c5efb8159f7bd2aee3aa80412f5c1a3f52f0e812
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-07-09 13:36:28 +0300

    tests/cpydiff/core_arguments: Move under Functions subsection.
    This is the last "orphan" case.

--- tests/cpydiff/core_function_argcount.py
+++ tests/cpydiff/core_function_argcount.py
@@ -1,5 +1,5 @@
 """
-categories: Core
+categories: Core,Functions
 description: Error messages for methods may display unexpected argument counts
 cause: MicroPython counts "self" as an argument.
 workaround: Interpret error messages with the information above in mind.
