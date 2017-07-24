5af97060f55cf5b9c67256d3f007528668dd0a23
Radomir Dopieralski <openstack@sheep.art.pl>
2017-07-24 23:53:37 +0200

    extmod/ure: Fix handling of '-' at the end of classes.
    Fix #3178
    
    When '-' appears at the end of a class, treat it as literal '-'.

--- extmod/re1.5/compilecode.c
+++ extmod/re1.5/compilecode.c
@@ -56,6 +56,14 @@ static const char *_compilecode(const char *re, ByteProg *prog, int sizecode)
                 if (!*re) return NULL;
                 EMIT(PC++, *re);
                 if (re[1] == '-') {
+                    if (re[2] == ']') {
+                        EMIT(PC++, *re);
+                        re += 1;
+                        cnt += 1;
+                        EMIT(PC++, *re);
+                        EMIT(PC++, *re);
+                        break;
+                    }
                     re += 2;
                 }
                 EMIT(PC++, *re);
