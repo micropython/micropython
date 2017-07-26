cb667391f48c8962f4596bb6361d6e9f8669ac44
Sylvain Pelissier <sylvain.pelissier@gmail.com>
2017-07-26 15:32:46 +0200

    Update adc.py
    According to documentation: "The returned value will be between 0 and 4095" and there is no need to test pin here.

--- tests/pyb/adc.py
+++ tests/pyb/adc.py
@@ -1,13 +1,12 @@
 from pyb import ADC
-from pyb import Pin
 
-pin = Pin('X22', mode=Pin.IN, pull=Pin.PULL_DOWN)
 adc = ADC('X22')
 print(adc)
 
 # read single sample
 val = adc.read()
-assert val < 500
+assert val >= 0
+assert val < 4095
 
 # timer for read_timed
 tim = pyb.Timer(5, freq=500)
@@ -17,7 +16,8 @@ buf = bytearray(50)
 adc.read_timed(buf, tim)
 print(len(buf))
 for i in buf:
-    assert i < 500
+    assert i >= 0
+    assert i < 4095
 
 # read into arrays with different element sizes
 import array
@@ -25,9 +25,11 @@ ar = array.array('h', 25 * [0])
 adc.read_timed(ar, tim)
 print(len(ar))
 for i in buf:
-    assert i < 500
+    assert i >= 0
+    assert i < 4095
 ar = array.array('i', 30 * [0])
 adc.read_timed(ar, tim)
 print(len(ar))
 for i in buf:
-    assert i < 500
+    assert i >= 0
+    assert i < 4095
