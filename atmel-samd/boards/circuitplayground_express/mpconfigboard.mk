ef65ee78c544f0996eeb77ae762d2c4c7ed17057
Dan Halbert <halbert@halwitz.org>
2017-10-03 18:10:13 -0400

    Freeze libraries needed by adafruit_circuitplayground library into firmware. This saves a lot of RAM. Fixes #287. Also fixed compilation of frozen_mpy.c to use supplied make rule rather than builtin rule (supplied rule suppresses printing out the gcc command line).

--- atmel-samd/boards/circuitplayground_express/mpconfigboard.mk
+++ atmel-samd/boards/circuitplayground_express/mpconfigboard.mk
@@ -7,4 +7,7 @@ FLASH_IMPL = spi_flash.c
 CHIP_VARIANT = SAMD21G18A
 
 # Include these Python libraries in firmware.
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_BusDevice
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_LIS3DH
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_NeoPixel
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Thermistor
