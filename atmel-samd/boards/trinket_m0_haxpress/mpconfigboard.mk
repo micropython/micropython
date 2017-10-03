c2bb9e2eb50e0dee6d7405fd738f1b4b0f94d10b
Radomir Dopieralski <deshipu@users.noreply.github.com>
2017-10-03 20:24:48 +0200

    Add board file for the hacked Trinket M0 Haxpress (#303)
    Add a hacked Tinket M0 Haxpress board definition
    
    Based on https://daveastels.com/2017/09/01/trinket-m0-express-hack/

---
+++ atmel-samd/boards/trinket_m0_haxpress/mpconfigboard.mk
@@ -0,0 +1,8 @@
+LD_FILE = boards/samd21x18-bootloader-external-flash-crystalless.ld
+USB_VID = 0x239A
+USB_PID = 0x801F
+
+FLASH_IMPL = spi_flash.c
+
+CHIP_VARIANT = SAMD21E18A
+CHIP_FAMILY = samd21
