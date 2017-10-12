4e083819f323cbadc429bcd81ed20344292382fb
Ayke van Laethem <aykevanlaethem@gmail.com>
2017-10-12 02:46:39 +0200

    nrf: Add compile switch to disable VFS.
    This saves about 17kB.

--- ports/nrf/main.c
+++ ports/nrf/main.c
@@ -30,6 +30,7 @@
 #include <string.h>
 
 #include "py/nlr.h"
+#include "py/mperrno.h"
 #include "py/lexer.h"
 #include "py/parse.h"
 #include "py/obj.h"
@@ -212,6 +213,21 @@ pin_init0();
 
     return 0;
 }
+
+#if !MICROPY_VFS
+mp_lexer_t *mp_lexer_new_from_file(const char *filename) {
+    mp_raise_OSError(MP_ENOENT);
+}
+
+mp_import_stat_t mp_import_stat(const char *path) {
+    return MP_IMPORT_STAT_NO_EXIST;
+}
+
+STATIC mp_obj_t mp_builtin_open(size_t n_args, const mp_obj_t *args, mp_map_t *kwargs) {
+    mp_raise_OSError(MP_EPERM);
+}
+MP_DEFINE_CONST_FUN_OBJ_KW(mp_builtin_open_obj, 1, mp_builtin_open);
+#endif
 
 void HardFault_Handler(void)
 {
