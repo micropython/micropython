85b30a7fbef13e9d17c73b2a73c729c4c282d7de
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-10-29 17:15:02 +0300

    unix: Enable MICROPY_CLASS_SLOTS.

--- ports/unix/mpconfigport.h
+++ ports/unix/mpconfigport.h
@@ -110,6 +110,8 @@
 #define MICROPY_STACKLESS           (0)
 #define MICROPY_STACKLESS_STRICT    (0)
 
+#define MICROPY_CLASS_SLOTS         (1)
+
 #define MICROPY_PY_OS_STATVFS       (1)
 #define MICROPY_PY_UTIME            (1)
 #define MICROPY_PY_UTIME_MP_HAL     (1)
