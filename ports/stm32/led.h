811ddcc65f9fb666228a9477139f4fceb31240dd
Damien George <damien.p.george@gmail.com>
2017-11-20 15:28:04 +1100

    stm32/led: Remove unused LED enum constants.

--- ports/stm32/led.h
+++ ports/stm32/led.h
@@ -27,21 +27,10 @@
 #define MICROPY_INCLUDED_STMHAL_LED_H
 
 typedef enum {
-    // PYBv3
-    PYB_LED_R1 = 1,
-    PYB_LED_R2 = 2,
-    PYB_LED_G1 = 3,
-    PYB_LED_G2 = 4,
-    // PYBv4
     PYB_LED_RED = 1,
     PYB_LED_GREEN = 2,
     PYB_LED_YELLOW = 3,
     PYB_LED_BLUE = 4,
-    //STM32F4DISC
-    PYB_LED_R = 1,
-    PYB_LED_G = 2,
-    PYB_LED_B = 3,
-    PYB_LED_O = 4,
 } pyb_led_t;
 
 void led_init(void);
