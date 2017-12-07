88a8043a27c3f75c7e4e52e4e8b0d47005cd6bef
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-12-07 10:52:40 +0200

    py/malloc: MICROPY_MEM_STATS requires MICROPY_MALLOC_USES_ALLOCATED_SIZE.
    Error out if they're set incompatibly.

--- py/malloc.c
+++ py/malloc.c
@@ -39,6 +39,9 @@
 #endif
 
 #if MICROPY_MEM_STATS
+#if !MICROPY_MALLOC_USES_ALLOCATED_SIZE
+#error MICROPY_MEM_STATS requires MICROPY_MALLOC_USES_ALLOCATED_SIZE
+#endif
 #define UPDATE_PEAK() { if (MP_STATE_MEM(current_bytes_allocated) > MP_STATE_MEM(peak_bytes_allocated)) MP_STATE_MEM(peak_bytes_allocated) = MP_STATE_MEM(current_bytes_allocated); }
 #endif
 
