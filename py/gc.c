dea3fb93c74ae61dc5168b62a780dc6ce7865e09
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-12-09 01:54:01 +0200

    py/gc: In sweep debug output, print pointer as a pointer.
    Or it will be truncated on a 64-bit platform.

--- py/gc.c
+++ py/gc.c
@@ -289,7 +289,7 @@ STATIC void gc_sweep(void) {
                 }
 #endif
                 free_tail = 1;
-                DEBUG_printf("gc_sweep(%x)\n", PTR_FROM_BLOCK(block));
+                DEBUG_printf("gc_sweep(%p)\n", PTR_FROM_BLOCK(block));
                 #if MICROPY_PY_GC_COLLECT_RETVAL
                 MP_STATE_MEM(gc_collected)++;
                 #endif
