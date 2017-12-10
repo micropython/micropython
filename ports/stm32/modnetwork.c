36be59c1080f822832cbb7aeff7289ed7cce0802
boochow <boochowp@gmail.com>
2017-12-10 23:44:52 +0900

    stm32: MICROPY_PY_ETH flag added to Makefile and modnetwork.c

--- ports/stm32/modnetwork.c
+++ ports/stm32/modnetwork.c
@@ -113,7 +113,7 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_0(network_route_obj, network_route);
 STATIC const mp_rom_map_elem_t mp_module_network_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_network) },
 
-    #if MICROPY_PY_ETH_RMII
+    #if MICROPY_PY_ETH
     { MP_ROM_QSTR(MP_QSTR_Ethernet), MP_ROM_PTR(&mod_network_nic_type_ethernet) ,},
     #endif
     #if MICROPY_PY_WIZNET5K
