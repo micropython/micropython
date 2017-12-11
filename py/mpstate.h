d3f82bc42576ccd80206a17a4941fd5c28c56530
Damien George <damien.p.george@gmail.com>
2017-12-11 22:51:52 +1100

    py/mpstate.h: Remove obsolete comment about nlr_top being coded in asm.

--- py/mpstate.h
+++ py/mpstate.h
@@ -215,7 +215,6 @@ typedef struct _mp_state_thread_t {
     mp_obj_dict_t *dict_locals;
     mp_obj_dict_t *dict_globals;
 
-    // Note: nlr asm code has the offset of this hard-coded
     nlr_buf_t *nlr_top; // ROOT POINTER
 
     // Stack top at the start of program
