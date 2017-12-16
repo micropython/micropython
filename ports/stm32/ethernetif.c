6d5e063f4087cfdedaabc19a36a41b9500dee017
boochow <boochowp@gmail.com>
2017-12-16 12:40:50 +0900

    stm32: ethernetif.c removed unnecessary comment lines.

--- ports/stm32/ethernetif.c
+++ ports/stm32/ethernetif.c
@@ -198,12 +198,6 @@ void low_level_init(struct netif *netif)
 
   /* set netif MAC hardware address */
   mac_address_from_hwid(netif->hwaddr);
-  /*  netif->hwaddr[0] =  MAC_ADDR0;
-  netif->hwaddr[1] =  MAC_ADDR1;
-  netif->hwaddr[2] =  MAC_ADDR2;
-  netif->hwaddr[3] =  MAC_ADDR3;
-  netif->hwaddr[4] =  MAC_ADDR4;
-  netif->hwaddr[5] =  MAC_ADDR5;*/
 
   /* set netif maximum transfer unit */
   netif->mtu = 1500;
