251b00457c02718373a03cd5c1aa04a67ba30711
Damien George <damien.p.george@gmail.com>
2017-12-19 14:45:53 +1100

    tests/extmod/uhashlib_sha256: Add test for hashing 56 bytes of data.

--- tests/extmod/uhashlib_sha256.py
+++ tests/extmod/uhashlib_sha256.py
@@ -23,6 +23,9 @@ print(h.digest())
 
 print(hashlib.sha256(b"\xff" * 64).digest())
 
+# 56 bytes is a boundary case in the algorithm
+print(hashlib.sha256(b"\xff" * 56).digest())
+
 # TODO: running .digest() several times in row is not supported()
 #h = hashlib.sha256(b'123')
 #print(h.digest())
