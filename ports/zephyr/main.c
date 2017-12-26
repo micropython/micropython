7a9a73ee8466111f73a7de7d27568c1260d802ef
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2017-12-26 20:16:08 +0200

    zephyr/main: Remove unused do_str() function.
    The artifact of initial porting effort.

--- ports/zephyr/main.c
+++ ports/zephyr/main.c
@@ -51,21 +51,6 @@
 static char *stack_top;
 static char heap[MICROPY_HEAP_SIZE];
 
-void do_str(const char *src, mp_parse_input_kind_t input_kind) {
-    nlr_buf_t nlr;
-    if (nlr_push(&nlr) == 0) {
-        mp_lexer_t *lex = mp_lexer_new_from_str_len(MP_QSTR__lt_stdin_gt_, src, strlen(src), 0);
-        qstr source_name = lex->source_name;
-        mp_parse_tree_t parse_tree = mp_parse(lex, input_kind);
-        mp_obj_t module_fun = mp_compile(&parse_tree, source_name, MP_EMIT_OPT_NONE, true);
-        mp_call_function_0(module_fun);
-        nlr_pop();
-    } else {
-        // uncaught exception
-        mp_obj_print_exception(&mp_plat_print, (mp_obj_t)nlr.ret_val);
-    }
-}
-
 void init_zephyr(void) {
     // We now rely on CONFIG_NET_APP_SETTINGS to set up bootstrap
     // network addresses.
