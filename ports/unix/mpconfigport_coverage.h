567bc2d6ce18f55e7a1d2c8e023ead44f5c2cc45
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-01-07 15:13:56 +0200

    extmod/moducryptolib: Add ucryptolib module with crypto functions.
    The API follows guidelines of https://www.python.org/dev/peps/pep-0272/,
    but is optimized for code size, with the idea that full PEP 0272
    compatibility can be added with a simple Python wrapper mode.
    
    The naming of the module follows (u)hashlib pattern.
    
    At the bare minimum, this module is expected to provide:
    
    * AES128, ECB (i.e. "null") mode, encrypt only
    
    Implementation in this commit is based on axTLS routines, and implements
    following:
    
    * AES 128 and 256
    * ECB and CBC modes
    * encrypt and decrypt

--- ports/unix/mpconfigport_coverage.h
+++ ports/unix/mpconfigport_coverage.h
@@ -49,6 +49,7 @@
 #define MICROPY_VFS_FAT                (1)
 #define MICROPY_PY_FRAMEBUF            (1)
 #define MICROPY_PY_COLLECTIONS_NAMEDTUPLE__ASDICT (1)
+#define MICROPY_PY_UCRYPTOLIB          (1)
 
 // TODO these should be generic, not bound to fatfs
 #define mp_type_fileio mp_type_vfs_posix_fileio
