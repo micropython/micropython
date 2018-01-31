c6a542d1669bac049a8b1bdd3c4abaaac46151f5
arturo182 <arturo182@tlen.pl>
2018-02-01 00:17:05 +0100

    nrf: Add SD 6.0.0-6.alpha to the download script

--- ports/nrf/drivers/bluetooth/download_ble_stack.sh
+++ ports/nrf/drivers/bluetooth/download_ble_stack.sh
@@ -70,6 +70,24 @@ function download_s132_nrf52_5_0_0
     cd -
 }
 
+function download_s140_nrf52_6_0_0_6_alpha
+{
+    echo ""
+    echo "############################################"
+    echo "### Downloading s140_nrf52_6.0.0-6.alpha ###"
+    echo "############################################"
+    echo ""
+
+    mkdir -p $1/s140_nrf52_6.0.0-6.alpha
+    cd $1/s140_nrf52_6.0.0-6.alpha
+
+    wget http://www.nordicsemi.com/eng/nordic/download_resource/60624/18/61756799/116072
+    mv 116072 temp.zip
+    unzip -u temp.zip
+    rm temp.zip
+    cd -
+}
+
 SCRIPT_DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 if [ $# -eq 0 ]; then
@@ -78,6 +96,7 @@ if [ $# -eq 0 ]; then
     download_s132_nrf52_2_0_1 ${SCRIPT_DIR}
     download_s132_nrf52_3_0_0 ${SCRIPT_DIR}
     download_s132_nrf52_5_0_0 ${SCRIPT_DIR}
+    download_s140_nrf52_6_0_0_6_alpha ${SCRIPT_DIR}
 else
     case $1 in
         "s110_nrf51" )
@@ -88,6 +107,8 @@ else
             download_s132_nrf52_3_0_0 ${SCRIPT_DIR} ;;
         "s132_nrf52_5_0_0" )
             download_s132_nrf52_5_0_0 ${SCRIPT_DIR} ;;
+        "s140_nrf52_6_0_0_6_alpha" )
+            download_s140_nrf52_6_0_0_6_alpha ${SCRIPT_DIR} ;;
     esac
 fi
 
