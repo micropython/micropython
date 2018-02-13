9f1cd41d7778b7e1ce8e010584e7a49e9158ae7a
Kevin Townsend <microbuilder@users.noreply.github.com>
2018-02-13 08:44:35 +0100

    Update to S140

--- ports/nrf/boards/feather52840/mpconfigboard.mk
+++ ports/nrf/boards/feather52840/mpconfigboard.mk
@@ -1,4 +1,4 @@
-# This file is a placeholder to enable building with 'SD=s132' flag
+# This file is a placeholder to enable building with 'SD=s140' flag
 # The actual config data is stored in the file referenced below, regardless
-# of whether S132 is actively used or not.
+# of whether S140 is actively used or not.
 include boards/$(BOARD)/mpconfigboard.mk
