d02251f8cd0726d9a25f414af1f2f66fd6709bd5
Scott Shawcroft <scott.shawcroft@gmail.com>
2018-02-20 12:34:07 -0800

    Add a new board for Metro M4 Express Rev B. This is the board send to community members for testing.
    Also tweaked and commented reset pin list for main Metro M4 Express
    which is currently unreleased and is Rev D.

---
+++ ports/atmel-samd/boards/metro_m4_express_revb/mpconfigboard.mk
@@ -0,0 +1,10 @@
+LD_FILE = boards/samd51x19-bootloader-external-flash.ld
+USB_VID = 0x239A
+USB_PID = 0x8021
+USB_PRODUCT = "Metro M4 Express Rev B (Black)"
+USB_MANUFACTURER = "Adafruit Industries LLC"
+
+SPI_FLASH_FILESYSTEM = 1
+
+CHIP_VARIANT = SAMD51J19A
+CHIP_FAMILY = samd51
