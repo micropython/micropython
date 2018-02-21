6c3faf6c17521940eb3ec48a8af3d28b513e2ba2
Damien George <damien.p.george@gmail.com>
2018-02-21 22:52:58 +1100

    py/objdeque: Allow to compile without warnings by disabling deque_clear.

--- py/objdeque.c
+++ py/objdeque.c
@@ -130,6 +130,7 @@ STATIC mp_obj_t deque_popleft(mp_obj_t self_in) {
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(deque_popleft_obj, deque_popleft);
 
+#if 0
 STATIC mp_obj_t deque_clear(mp_obj_t self_in) {
     mp_obj_deque_t *self = MP_OBJ_TO_PTR(self_in);
     self->i_get = self->i_put = 0;
@@ -137,6 +138,7 @@ STATIC mp_obj_t deque_clear(mp_obj_t self_in) {
     return mp_const_none;
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(deque_clear_obj, deque_clear);
+#endif
 
 STATIC const mp_rom_map_elem_t deque_locals_dict_table[] = {
     { MP_ROM_QSTR(MP_QSTR_append), MP_ROM_PTR(&deque_append_obj) },
