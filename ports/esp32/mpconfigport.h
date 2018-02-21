82828340a0e8dfd946bafac01d38e59ff63ebe3b
Damien George <damien.p.george@gmail.com>
2018-02-21 22:55:13 +1100

    ports: Enable ucollections.deque on relevant ports.
    These ports are all capable of running uasyncio.

--- ports/esp32/mpconfigport.h
+++ ports/esp32/mpconfigport.h
@@ -90,6 +90,7 @@
 #define MICROPY_PY_ARRAY_SLICE_ASSIGN       (1)
 #define MICROPY_PY_ATTRTUPLE                (1)
 #define MICROPY_PY_COLLECTIONS              (1)
+#define MICROPY_PY_COLLECTIONS_DEQUE        (1)
 #define MICROPY_PY_COLLECTIONS_ORDEREDDICT  (1)
 #define MICROPY_PY_MATH                     (1)
 #define MICROPY_PY_MATH_SPECIAL_FUNCTIONS   (1)
