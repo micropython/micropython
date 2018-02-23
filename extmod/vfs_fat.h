2ad555bc760833aae9991a4e6c893daa790c0c89
Damien George <damien.p.george@gmail.com>
2018-02-23 17:41:47 +1100

    extmod/vfs_fat: Remove declaration of mp_builtin_open_obj.
    It's declared already in py/builtin.h.

--- extmod/vfs_fat.h
+++ extmod/vfs_fat.h
@@ -58,6 +58,5 @@ extern const mp_obj_type_t mp_fat_vfs_type;
 
 mp_import_stat_t fat_vfs_import_stat(struct _fs_user_mount_t *vfs, const char *path);
 MP_DECLARE_CONST_FUN_OBJ_3(fat_vfs_open_obj);
-MP_DECLARE_CONST_FUN_OBJ_KW(mp_builtin_open_obj);
 
 #endif // MICROPY_INCLUDED_EXTMOD_VFS_FAT_H
