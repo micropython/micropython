ca85dd1c27f15a3a5af216e7b3212ab931135bc2
iabdalkader <i.abdalkader@gmail.com>
2018-02-24 19:21:32 +0200

    AF is already set.

--- ports/stm32/usbd_conf.c
+++ ports/stm32/usbd_conf.c
@@ -90,7 +90,6 @@ void HAL_PCD_MspInit(PCD_HandleTypeDef *hpcd)
     GPIO_InitStruct.Pin = GPIO_PIN_10;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
     GPIO_InitStruct.Pull = GPIO_PULLUP;
-    GPIO_InitStruct.Alternate = GPIO_AF10_OTG_FS;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct); 
 #endif
 
