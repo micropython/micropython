fba0d02185e724a5759ecd1dccee324017f03ef0
iabdalkader <i.abdalkader@gmail.com>
2018-02-26 14:40:04 +0200

    Fix H7 HAL HSE_VALUE

--- ports/stm32/boards/NUCLEO_H743ZI/stm32h7xx_hal_conf.h
+++ ports/stm32/boards/NUCLEO_H743ZI/stm32h7xx_hal_conf.h
@@ -112,7 +112,7 @@
   *        (when HSE is used as system clock source, directly or through the PLL).
   */
 #if !defined  (HSE_VALUE)
-#define HSE_VALUE    ((uint32_t)25000000) /*!< Value of the External oscillator in Hz */
+#define HSE_VALUE    ((uint32_t)8000000) /*!< Value of the External oscillator in Hz */
 #endif /* HSE_VALUE */
 
 #if !defined  (HSE_STARTUP_TIMEOUT)
