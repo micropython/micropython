ea633117d01d94e8d56ed94344e4b3e46b4eef03
Scott Shawcroft <scott.shawcroft@gmail.com>
2018-02-27 18:08:49 -0800

    Remove bad c2rst and fix the other for sphinx 1.7.1

--- conf.py
+++ conf.py
@@ -21,6 +21,7 @@ from recommonmark.parser import CommonMarkParser
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
+sys.path.insert(0, os.path.abspath('docs'))
 sys.path.insert(0, os.path.abspath('.'))
 
 master_doc = 'docs/index'
