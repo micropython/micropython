d4b55eff44da38e51616124883508c0d6b6678d3
Damien George <damien.p.george@gmail.com>
2018-03-13 13:23:30 +1100

    py/misc.h: Remove unused count_lead_ones() inline function.
    This function was never used for unicode/utf8 handling code, or anything
    else, so remove it to keep things clean.

--- py/misc.h
+++ py/misc.h
@@ -204,20 +204,6 @@ int DEBUG_printf(const char *fmt, ...);
 
 extern mp_uint_t mp_verbose_flag;
 
-// This is useful for unicode handling. Some CPU archs has
-// special instructions for efficient implementation of this
-// function (e.g. CLZ on ARM).
-// NOTE: this function is unused at the moment
-#ifndef count_lead_ones
-static inline mp_uint_t count_lead_ones(byte val) {
-    mp_uint_t c = 0;
-    for (byte mask = 0x80; val & mask; mask >>= 1) {
-        c++;
-    }
-    return c;
-}
-#endif
-
 /** float internals *************/
 
 #if MICROPY_PY_BUILTINS_FLOAT
