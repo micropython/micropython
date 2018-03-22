3bf4d69f67728e50cfc4c5ac50c5a66a7f4bd9e5
Scott Shawcroft <scott.shawcroft@gmail.com>
2018-03-22 16:42:47 -0700

    Update Metro M4 to rev F and make flash reset on start.

--- ports/atmel-samd/external_flash/spi_flash_api.h
+++ ports/atmel-samd/external_flash/spi_flash_api.h
@@ -29,6 +29,8 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#include "external_flash/devices.h"
+
 // This API is implemented for both normal SPI peripherals and QSPI peripherals.
 
 bool spi_flash_command(uint8_t command);
@@ -38,5 +40,6 @@ bool spi_flash_sector_command(uint8_t command, uint32_t address);
 bool spi_flash_write_data(uint32_t address, uint8_t* data, uint32_t data_length);
 bool spi_flash_read_data(uint32_t address, uint8_t* data, uint32_t data_length);
 void spi_flash_init(void);
+void spi_flash_init_device(const external_flash_device* device);
 
 #endif  // MICROPY_INCLUDED_ATMEL_SAMD_SPI_FLASH_H
