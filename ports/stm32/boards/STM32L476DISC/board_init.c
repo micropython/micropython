7e28212352c390f2be78c2d020b41b88e665c09f
Damien George <damien.p.george@gmail.com>
2018-03-28 16:22:43 +1100

    stm32/boards/STM32L476DISC: Update to not take the address of pin objs.

--- ports/stm32/boards/STM32L476DISC/board_init.c
+++ ports/stm32/boards/STM32L476DISC/board_init.c
@@ -2,8 +2,8 @@
 
 void STM32L476DISC_board_early_init(void) {
     // set SPI flash WP and HOLD pins high
-    mp_hal_pin_output(&pin_E14);
-    mp_hal_pin_output(&pin_E15);
-    mp_hal_pin_write(&pin_E14, 1);
-    mp_hal_pin_write(&pin_E15, 1);
+    mp_hal_pin_output(pin_E14);
+    mp_hal_pin_output(pin_E15);
+    mp_hal_pin_write(pin_E14, 1);
+    mp_hal_pin_write(pin_E15, 1);
 }
