43503e00c114352f6f0fe9a50b3f4abd21894039
Damien George <damien.p.george@gmail.com>
2018-04-05 01:03:57 +1000

    tests/basics/class_super.py: Add tests for store/delete of super attr.

--- tests/basics/class_super.py
+++ tests/basics/class_super.py
@@ -34,3 +34,14 @@ class B(A):
         print(super().bar) # accessing attribute after super()
         return super().foo().count(2) # calling a subsequent method
 print(B().foo())
+
+# store/delete of super attribute not allowed
+assert hasattr(super(B, B()), 'foo')
+try:
+    super(B, B()).foo = 1
+except AttributeError:
+    print('AttributeError')
+try:
+    del super(B, B()).foo
+except AttributeError:
+    print('AttributeError')
