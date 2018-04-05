e8307f7b4495c5071aa039238aea58a0ef51f722
iabdalkader <i.abdalkader@gmail.com>
2018-04-06 01:05:22 +0200

    stm32/boards/NUCLEO_H743ZI: Enable DAC peripheral.

--- ports/stm32/boards/NUCLEO_H743ZI/mpconfigboard.h
+++ ports/stm32/boards/NUCLEO_H743ZI/mpconfigboard.h
@@ -3,6 +3,7 @@
 
 #define MICROPY_HW_ENABLE_RTC       (1)
 #define MICROPY_HW_ENABLE_ADC       (0)
+#define MICROPY_HW_ENABLE_DAC       (1)
 #define MICROPY_HW_ENABLE_USB       (1)
 #define MICROPY_HW_HAS_SWITCH       (1)
 #define MICROPY_HW_HAS_FLASH        (1)
