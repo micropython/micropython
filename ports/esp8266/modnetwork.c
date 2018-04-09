d8fdb77ac91b578d851b873a004b530b42313cf1
Lars Kellogg-Stedman <lars@oddbit.com>
2018-04-09 15:34:18 -0400

    esp8266/modnetwork: Allow to get ESSID of AP that STA is connected to.
    This patch enables iface.config('essid') to work for both AP and STA
    interfaces.

--- ports/esp8266/modnetwork.c
+++ ports/esp8266/modnetwork.c
@@ -422,8 +422,11 @@ STATIC mp_obj_t esp_config(size_t n_args, const mp_obj_t *args, mp_map_t *kwargs
             return mp_obj_new_bytes(mac, sizeof(mac));
         }
         case MP_QSTR_essid:
-            req_if = SOFTAP_IF;
-            val = mp_obj_new_str((char*)cfg.ap.ssid, cfg.ap.ssid_len);
+            if (self->if_id == STATION_IF) {
+                val = mp_obj_new_str((char*)cfg.sta.ssid, strlen((char*)cfg.sta.ssid));
+            } else {
+                val = mp_obj_new_str((char*)cfg.ap.ssid, cfg.ap.ssid_len);
+            }
             break;
         case MP_QSTR_hidden:
             req_if = SOFTAP_IF;
