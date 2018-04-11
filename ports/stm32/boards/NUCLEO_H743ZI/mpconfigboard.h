ff25dc8306d8c996f18a5884aeccac31760c0899
rolandvs <roland@van-straten.org>
2018-04-11 09:40:14 +0200

    When using different HSE value the clock speed can be set in mpconfigboard.h, however to match the USB speed a second PLL is used and this was hard coded in system_stm32.h.

--- ports/stm32/boards/NUCLEO_H743ZI/mpconfigboard.h
+++ ports/stm32/boards/NUCLEO_H743ZI/mpconfigboard.h
@@ -19,6 +19,13 @@ void NUCLEO_H743ZI_board_early_init(void);
 #define MICROPY_HW_CLK_PLLQ (4)
 #define MICROPY_HW_CLK_PLLR (2)
 
+// with the 8MHz HSE, settings of the PLL for USB
+#define MICROPY_HW_PLL3_PLL3M (4)
+#define MICROPY_HW_PLL3_PLL3N (120)
+#define MICROPY_HW_PLL3_PLL3P (2)
+#define MICROPY_HW_PLL3_PLL3Q (5)
+#define MICROPY_HW_PLL3_PLL3R (2)
+
 // 4 wait states
 #define MICROPY_HW_FLASH_LATENCY    FLASH_LATENCY_4
 
