2a26dac2bcbc1bda067a44da60871988392ba806
Scott Shawcroft <scott.shawcroft@gmail.com>
2018-04-11 13:02:18 -0700

    Add initial ItsyBitsy M4 support

---
+++ ports/atmel-samd/boards/itsybitsy_m4_express/mpconfigboard.mk
@@ -0,0 +1,10 @@
+LD_FILE = boards/samd51x19-bootloader-external-flash.ld
+USB_VID = 0x239A
+USB_PID = 0x802C
+USB_PRODUCT = "ItsyBitsy M4 Express"
+USB_MANUFACTURER = "Adafruit Industries LLC"
+
+QSPI_FLASH_FILESYSTEM = 1
+
+CHIP_VARIANT = SAMD51G19A
+CHIP_FAMILY = samd51
