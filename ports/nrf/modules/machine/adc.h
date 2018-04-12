518c45815f7ed96a8cd62b708245194529581ab7
Glenn Ruben Bakke <glennbakke@gmail.com>
2018-04-12 23:02:19 +0200

    nrf/adc: Allow for external use of new and value read function.

--- ports/nrf/modules/machine/adc.h
+++ ports/nrf/modules/machine/adc.h
@@ -27,8 +27,12 @@
 #ifndef ADC_H__
 #define ADC_H__
 
+typedef struct _machine_adc_obj_t machine_adc_obj_t;
+
 extern const mp_obj_type_t machine_adc_type;
 
 void adc_init0(void);
+
+int16_t machine_adc_value_read(machine_adc_obj_t * adc_obj);
 
 #endif // ADC_H__
