e7f463320f8675d2f9acb6819ad0a2556672be08
Jeff Epler <jepler@gmail.com>
2018-04-30 19:58:12 -0500

    tests: Add new tests for ubinascii differences
    .. some of these tests currently fail, but a subsequent commit will
    fix them.

--- tests/extmod/ubinascii_crc32.py
+++ tests/extmod/ubinascii_crc32.py
@@ -22,3 +22,7 @@ print(hex(binascii.crc32(b' over the lazy dog', binascii.crc32(b'The quick brown
 print(hex(binascii.crc32(b'\x00' * 16, binascii.crc32(b'\x00' * 16))))
 print(hex(binascii.crc32(b'\xff' * 16, binascii.crc32(b'\xff' * 16))))
 print(hex(binascii.crc32(bytes(range(16, 32)), binascii.crc32(bytes(range(16))))))
+try:
+    binascii.crc32('')
+except TypeError:
+    print("TypeError")
--- tests/extmod/ubinascii_unhexlify.py
+++ tests/extmod/ubinascii_unhexlify.py
@@ -12,6 +12,9 @@ print(binascii.unhexlify(b'08090a0b0c0d0e0f'))
 print(binascii.unhexlify(b'7f80ff'))
 print(binascii.unhexlify(b'313233344142434461626364'))
 
+# Unicode strings can be decoded
+print(binascii.unhexlify('313233344142434461626364'))
+
 try:
     a = binascii.unhexlify(b'0') # odd buffer length
 except ValueError:
