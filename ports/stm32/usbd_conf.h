9f4eda542ae9e7780ddc49c40628707d31ab1014
Damien George <damien.p.george@gmail.com>
2018-05-11 22:04:56 +1000

    stm32/usbd_conf.h: Remove unused macros and clean up header file.

--- ports/stm32/usbd_conf.h
+++ ports/stm32/usbd_conf.h
@@ -29,62 +29,19 @@
   ******************************************************************************
   */
 
-/* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __USBD_CONF_H
 #define __USBD_CONF_H
 
-/* Includes ------------------------------------------------------------------*/
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-/* Exported types ------------------------------------------------------------*/
-/* Exported constants --------------------------------------------------------*/
-/* Common Config */
 #define USBD_MAX_NUM_INTERFACES               1
 #define USBD_MAX_NUM_CONFIGURATION            1
 #define USBD_MAX_STR_DESC_SIZ                 0x100
 #define USBD_SELF_POWERED                     0
 #define USBD_DEBUG_LEVEL                      0
-
-/* Exported macro ------------------------------------------------------------*/
-/* Memory management macros */
-/*
-these should not be used because the GC is reset on a soft reset but the usb is not
-#include "gc.h"
-#define USBD_malloc               gc_alloc
-#define USBD_free                 gc_free
-#define USBD_memset               memset
-#define USBD_memcpy               memcpy
-*/
-
-/* DEBUG macros */
-#if (USBD_DEBUG_LEVEL > 0)
-#define  USBD_UsrLog(...)   printf(__VA_ARGS__);\
-                            printf("\n");
-#else
-#define USBD_UsrLog(...)
-#endif
-
-#if (USBD_DEBUG_LEVEL > 1)
-
-#define  USBD_ErrLog(...)   printf("ERROR: ") ;\
-                            printf(__VA_ARGS__);\
-                            printf("\n");
-#else
-#define USBD_ErrLog(...)
-#endif
-
-#if (USBD_DEBUG_LEVEL > 2)
-#define  USBD_DbgLog(...)   printf("DEBUG : ") ;\
-                            printf(__VA_ARGS__);\
-                            printf("\n");
-#else
-#define USBD_DbgLog(...)
-#endif
-
-/* Exported functions ------------------------------------------------------- */
 
 #endif /* __USBD_CONF_H */
 
