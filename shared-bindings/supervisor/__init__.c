567f3e30a77f286713b697a0f8a85e8b31a4a27a
Roy Hooper <rhooper@toybox.ca>
2018-05-14 16:57:50 -0400

    Initial implementation of supervisor.reload()

--- shared-bindings/supervisor/__init__.c
+++ shared-bindings/supervisor/__init__.c
@@ -25,11 +25,15 @@
  */
  #include "py/obj.h"
  #include "py/runtime.h"
+
+ #include "lib/utils/interrupt_char.h"
  #include "supervisor/shared/autoreload.h"
+
  #include "supervisor/shared/rgb_led_status.h"
  
  #include "shared-bindings/supervisor/__init__.h"
  #include "shared-bindings/supervisor/Runtime.h"
+
 //| :mod:`supervisor` --- Supervisor settings
 //| =================================================
 //|
@@ -91,12 +95,31 @@ STATIC mp_obj_t supervisor_set_rgb_status_brightness(mp_obj_t lvl){
 }
 MP_DEFINE_CONST_FUN_OBJ_1(supervisor_set_rgb_status_brightness_obj, supervisor_set_rgb_status_brightness);
 
+//| .. method:: reload()
+//|
+//|   Reload the microcontroller (equivalent to hitting Ctrl-D at the REPL).
+//|
+STATIC mp_obj_t supervisor_reload(void) {
+    reload_next_character = true;
+
+    MP_STATE_VM(mp_pending_exception) = MP_OBJ_FROM_PTR(&MP_STATE_VM(mp_reload_exception));
+    #if MICROPY_ENABLE_SCHEDULER
+        if (MP_STATE_VM(sched_state) == MP_SCHED_IDLE) {
+        MP_STATE_VM(sched_state) = MP_SCHED_PENDING;
+    }
+    #endif
+    return mp_const_none;
+}
+MP_DEFINE_CONST_FUN_OBJ_0(supervisor_reload_obj, supervisor_reload);
+
+
 STATIC const mp_rom_map_elem_t supervisor_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_supervisor) },
     { MP_OBJ_NEW_QSTR(MP_QSTR_enable_autoreload),  MP_ROM_PTR(&supervisor_enable_autoreload_obj) },
     { MP_OBJ_NEW_QSTR(MP_QSTR_disable_autoreload),  MP_ROM_PTR(&supervisor_disable_autoreload_obj) },
     { MP_OBJ_NEW_QSTR(MP_QSTR_set_rgb_status_brightness),  MP_ROM_PTR(&supervisor_set_rgb_status_brightness_obj) },
     { MP_ROM_QSTR(MP_QSTR_runtime),  MP_ROM_PTR(&common_hal_supervisor_runtime_obj) },
+    { MP_ROM_QSTR(MP_QSTR_reload),  MP_ROM_PTR(&supervisor_reload_obj) },
 };
 
 STATIC MP_DEFINE_CONST_DICT(supervisor_module_globals, supervisor_module_globals_table);
