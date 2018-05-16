3c6f639aa58d0558a744af2f2fc32a6debf55c81
Eric Poulsen <eric@zyxod.com>
2018-05-16 14:35:21 -0700

    esp32/network_ppp: Add PPPoS functionality.
    This commit adds network.PPP(stream) which allows to create a TCP/IP
    network interface over a stream object (eg a UART).

--- ports/esp32/modnetwork.h
+++ ports/esp32/modnetwork.h
@@ -29,6 +29,7 @@
 enum { PHY_LAN8720, PHY_TLK110 };
 
 MP_DECLARE_CONST_FUN_OBJ_KW(get_lan_obj);
+MP_DECLARE_CONST_FUN_OBJ_1(ppp_make_new_obj);
 MP_DECLARE_CONST_FUN_OBJ_VAR_BETWEEN(esp_ifconfig_obj);
 
 void usocket_events_deinit(void);
