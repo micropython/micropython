ca8d3315c5c01e22f009a4d6ced8b257841012ed
mhe-fr <39079352+mhe-fr@users.noreply.github.com>
2018-05-17 22:42:16 +0200

    Update mpconfigport.h
    add UCRYPTO

--- ports/esp32/mpconfigport.h
+++ ports/esp32/mpconfigport.h
@@ -122,6 +122,7 @@
 #define MICROPY_PY_UHEAPQ                   (1)
 #define MICROPY_PY_UTIMEQ                   (1)
 #define MICROPY_PY_UHASHLIB                 (0) // We use the ESP32 version
+#define MICROPY_PY_UCRYPTO                  (0) // We use the ESP32 version
 #define MICROPY_PY_UHASHLIB_SHA1            (MICROPY_PY_USSL && MICROPY_SSL_AXTLS)
 #define MICROPY_PY_UBINASCII                (1)
 #define MICROPY_PY_UBINASCII_CRC32          (1)
