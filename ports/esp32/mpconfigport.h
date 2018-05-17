9edbb21a99c3fc24b9bc698b2b1b6fbe0beb5731
mhe-fr <39079352+mhe-fr@users.noreply.github.com>
2018-05-17 22:44:00 +0200

    Update mpconfigport.h

--- ports/esp32/mpconfigport.h
+++ ports/esp32/mpconfigport.h
@@ -185,6 +185,7 @@ extern const struct _mp_obj_module_t mp_module_onewire;
     { MP_OBJ_NEW_QSTR(MP_QSTR_network), (mp_obj_t)&mp_module_network }, \
     { MP_OBJ_NEW_QSTR(MP_QSTR__onewire), (mp_obj_t)&mp_module_onewire }, \
     { MP_OBJ_NEW_QSTR(MP_QSTR_uhashlib), (mp_obj_t)&mp_module_uhashlib }, \
+    { MP_OBJ_NEW_QSTR(MP_QSTR_ucrypto), (mp_obj_t)&mp_module_ucrypto }, \
 
 #define MICROPY_PORT_BUILTIN_MODULE_WEAK_LINKS \
     { MP_OBJ_NEW_QSTR(MP_QSTR_binascii), (mp_obj_t)&mp_module_ubinascii }, \
