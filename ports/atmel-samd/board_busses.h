4fd4adf974a3ac2f4c66bd04923cd9b4dde433bb
Matt Land <mland@sparefoot.com>
2018-05-17 12:14:51 -0400

    not working

--- ports/atmel-samd/board_busses.h
+++ ports/atmel-samd/board_busses.h
@@ -33,4 +33,7 @@ extern mp_obj_fun_builtin_fixed_t board_i2c_obj;
 void board_spi(void);
 extern mp_obj_fun_builtin_fixed_t board_spi_obj;
 
+void board_serial(void);
+extern mp_obj_fun_builtin_fixed_t board_serial_obj;
+
 #endif  // MICROPY_INCLUDED_ATMEL_SAMD_BOARD_BUSSES_H
