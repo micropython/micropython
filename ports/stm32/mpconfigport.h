bf9307afbf73574f8e7a46f34d76338fa4237cf7
Damien George <damien.p.george@gmail.com>
2018-05-21 16:48:24 +1000

    stm32: Integrate lwIP as implementation of usocket module.
    This patch allows to use lwIP as the implementation of the usocket module,
    instead of the existing socket-multiplexer that delegates the entire TCP/IP
    layer to the NIC itself.
    
    This is disabled by default, and enabled by defining MICROPY_PY_LWIP to 1.
    
    When enabled, the lwIP TCP/IP stack will be included in the build with
    default settings for memory usage and performance (see
    lwip_inc/lwipopts.h).  It is then up to a particular NIC to register itself
    with lwIP using the standard lwIP netif API.

--- ports/stm32/mpconfigport.h
+++ ports/stm32/mpconfigport.h
@@ -192,12 +192,21 @@ extern const struct _mp_obj_module_t mp_module_onewire;
 #define STM_BUILTIN_MODULE
 #endif
 
-#if MICROPY_PY_USOCKET
+#if MICROPY_PY_USOCKET && MICROPY_PY_LWIP
+// usocket implementation provided by lwIP
+#define SOCKET_BUILTIN_MODULE               { MP_ROM_QSTR(MP_QSTR_usocket), MP_ROM_PTR(&mp_module_lwip) },
+#define SOCKET_BUILTIN_MODULE_WEAK_LINKS    { MP_ROM_QSTR(MP_QSTR_socket), MP_ROM_PTR(&mp_module_lwip) },
+#define SOCKET_POLL extern void pyb_lwip_poll(void); pyb_lwip_poll();
+#elif MICROPY_PY_USOCKET
+// usocket implementation provided by skeleton wrapper
 #define SOCKET_BUILTIN_MODULE               { MP_ROM_QSTR(MP_QSTR_usocket), MP_ROM_PTR(&mp_module_usocket) },
 #define SOCKET_BUILTIN_MODULE_WEAK_LINKS    { MP_ROM_QSTR(MP_QSTR_socket), MP_ROM_PTR(&mp_module_usocket) },
+#define SOCKET_POLL
 #else
+// no usocket module
 #define SOCKET_BUILTIN_MODULE
 #define SOCKET_BUILTIN_MODULE_WEAK_LINKS
+#define SOCKET_POLL
 #endif
 
 #if MICROPY_PY_NETWORK
@@ -313,6 +322,7 @@ static inline mp_uint_t disable_irq(void) {
     do { \
         extern void mp_handle_pending(void); \
         mp_handle_pending(); \
+        SOCKET_POLL \
         if (pyb_thread_enabled) { \
             MP_THREAD_GIL_EXIT(); \
             pyb_thread_yield(); \
@@ -328,6 +338,7 @@ static inline mp_uint_t disable_irq(void) {
     do { \
         extern void mp_handle_pending(void); \
         mp_handle_pending(); \
+        SOCKET_POLL \
         __WFI(); \
     } while (0);
 
