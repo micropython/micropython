fc74a5e3aab1448e7a62cd0d86b9795d51309ee1
Damien George <damien.p.george@gmail.com>
2018-05-24 22:21:32 +1000

    tests/extmod: Update ure_span.py.

--- tests/extmod/ure_span.py
+++ tests/extmod/ure_span.py
@@ -9,12 +9,20 @@ except ImportError:
         print("SKIP")
         raise SystemExit
 
+try:
+    m = re.match(".", "a")
+    m.span
+except AttributeError:
+    print('SKIP')
+    raise SystemExit
+
+
 def print_spans(match):
     print('----')
     try:
         i = 0
         while True:
-            print(match.span(i))
+            print(match.span(i), match.start(i), match.end(i))
             i += 1
     except IndexError:
         pass
