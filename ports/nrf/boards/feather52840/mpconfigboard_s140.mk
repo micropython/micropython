feee808955b8a370c8e18235fe14b4caa56ba3bd
arturo182 <arturo182@tlen.pl>
2018-06-18 23:11:30 +0200

    nrf: Use the production version of s140 6.0.0

--- ports/nrf/boards/feather52840/mpconfigboard_s140.mk
+++ ports/nrf/boards/feather52840/mpconfigboard_s140.mk
@@ -1,7 +1,7 @@
 MCU_SERIES = m4
 MCU_VARIANT = nrf52
 MCU_SUB_VARIANT = nrf52840
-SOFTDEV_VERSION ?= 6.0.0-6.alpha
+SOFTDEV_VERSION ?= 6.0.0
 
 LD_FILE = boards/feather52840/bluefruit_nrf52840_s140_6.0.0.ld
 BOOTLOADER_FILENAME = boards/feather52840/bootloader/feather52840_bootloader_6.0.0_s140_single
