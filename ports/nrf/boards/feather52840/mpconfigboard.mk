54547683f98df10404011664167ebc9b649703fb
hathach <thach@tinyusb.org>
2018-06-25 23:31:14 +0700

    add uf2, bootloader target

--- ports/nrf/boards/feather52840/mpconfigboard.mk
+++ ports/nrf/boards/feather52840/mpconfigboard.mk
@@ -3,8 +3,8 @@ MCU_VARIANT = nrf52
 MCU_SUB_VARIANT = nrf52840
 SOFTDEV_VERSION ?= 6.0.0
 
-LD_FILE = boards/feather52840/bluefruit_nrf52840_s140_6.0.0.ld
-BOOTLOADER_FILENAME = boards/feather52840/bootloader/feather52840_bootloader_6.0.0_s140_single
+LD_FILE = boards/feather52840/bluefruit_nrf52840_s140_$(SOFTDEV_VERSION).ld
+BOOT_FILE = boards/feather52840/bootloader/feather52840_bootloader_$(SOFTDEV_VERSION)_s140_single
 
 NRF_DEFINES += -DNRF52840_XXAA
 
@@ -23,14 +23,25 @@ __check_defined = \
     $(if $(value $1),, \
     $(error Undefined make flag: $1$(if $2, ($2))))
 
-.PHONY: dfu-gen dfu-flash boot-flash
+.PHONY: dfu-gen dfu-flash dfu-bootloader uf2 bootloader
 
+
+uf2:
+	$(ECHO) "Create $(OUTPUT_FILENAME).uf2"
+	$(PYTHON2) $(TOP)/tools/uf2/utils/uf2conv.py -c -o "$(BUILD)/$(OUTPUT_FILENAME).uf2" "$(BUILD)/$(OUTPUT_FILENAME).hex"
+
+# Flash with DFU 
 dfu-gen:
 	$(NRFUTIL) dfu genpkg --sd-req 0xFFFE --dev-type 0x0052 --application $(BUILD)/$(OUTPUT_FILENAME).hex $(BUILD)/dfu-package.zip
 
 dfu-flash:
 	@:$(call check_defined, SERIAL, example: SERIAL=/dev/ttyUSB0)
-	$(NRFUTIL) --verbose dfu serial --package $(BUILD)/dfu-package.zip -p $(SERIAL) -b 115200
+	$(NRFUTIL) --verbose dfu serial --package $(BUILD)/dfu-package.zip -p $(SERIAL) -b 115200 --singlebank
 
-boot-flash:
-	nrfjprog --program $(BOOTLOADER_FILENAME).hex -f nrf52 --chiperase --reset
+dfu-bootloader:
+	@:$(call check_defined, SERIAL, example: SERIAL=/dev/ttyACM0)
+	$(NRFUTIL) --verbose dfu serial --package $(BOOT_FILE).zip -p $(SERIAL) -b 115200
+
+# Flash with jlink	
+bootloader:
+	nrfjprog --program $(BOOT_FILE).hex -f nrf52 --chiperase --reset
