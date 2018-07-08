887486582e94012061d48d1f5310fd979021b9ac
arturo182 <arturo182@tlen.pl>
2018-07-08 14:52:14 +0200

    nrf: Remove port-specific help
    It contains mentions to the pyb module which was removed.
    The ble-specific help will be re-added later when the API
    is stabilized.

--- ports/nrf/mpconfigport.h
+++ ports/nrf/mpconfigport.h
@@ -93,8 +93,8 @@
 #define MICROPY_PY_BUILTINS_EXECFILE             (0)
 #define MICROPY_PY_BUILTINS_COMPILE              (1)
 #define MICROPY_PY_BUILTINS_HELP                 (1)
-#define MICROPY_PY_BUILTINS_HELP_TEXT            nrf5_help_text
 #define MICROPY_PY_BUILTINS_HELP_MODULES         (1)
+#define MICROPY_PY_BUILTINS_HELP_TEXT            circuitpython_help_text
 #define MICROPY_PY_BUILTINS_INPUT                (1)
 #define MICROPY_MODULE_BUILTIN_INIT              (1)
 #define MICROPY_PY_ALL_SPECIAL_METHODS           (0)
