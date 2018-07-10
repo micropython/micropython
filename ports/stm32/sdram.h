6244b29c1ade74e430bee5827e32b046b955a133
Andrew Leech <andrew.leech@planetinnovation.com.au>
2018-07-10 17:30:27 +1000

    stm32/sdram: cherry pick openmv sdram driver from https://github.com/openmv/openmv/blob/7fbe54ad4e9dc1c527d9297a3f4c69c365dc9b2c/src/omv/sdram.c

---
+++ ports/stm32/sdram.h
@@ -0,0 +1,13 @@
+/*
+ * This file is part of the OpenMV project.
+ * Copyright (c) 2013/2014 Ibrahim Abdelkader <i.abdalkader@gmail.com>
+ * This work is licensed under the MIT license, see the file LICENSE for details.
+ *
+ * SDRAM Driver.
+ *
+ */
+#ifndef __SDRAM_H__
+#define __SDRAM_H__
+bool sdram_init();
+bool sdram_test();
+#endif // __SDRAM_H__
