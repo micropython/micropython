c458372ad8ebdfda9f9f5af0ba9a6fa7f4344c0b
forester3 <isaka2f@yahoo.co.jp>
2018-08-06 10:12:43 +0900

    Fix: MPU_Config region size

--- ports/stm32/sdram.c
+++ ports/stm32/sdram.c
@@ -225,7 +225,7 @@ static void sdram_init_seq(SDRAM_HandleTypeDef
     /* Configure the MPU attributes for SDRAM */
     MPU_InitStruct.Enable = MPU_REGION_ENABLE;
     MPU_InitStruct.BaseAddress = SDRAM_START_ADDRESS;
-    MPU_InitStruct.Size = MPU_REGION_SIZE_4MB;
+    MPU_InitStruct.Size = MPU_REGION_SIZE_8MB;
     MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
     MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
     MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
