2b6b0ffcd249e2aaa2b5bef03d8bb67f0ba42245
Scott Shawcroft <scott@tannewt.org>
2018-08-20 18:04:35 -0700

    Remove HID from hallowing so we have more room for other stuff.

--- ports/atmel-samd/boards/hallowing_m0_express/mpconfigboard.mk
+++ ports/atmel-samd/boards/hallowing_m0_express/mpconfigboard.mk
@@ -12,6 +12,5 @@ CHIP_FAMILY = samd21
 
 # Include these Python libraries in firmware.
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_BusDevice
-FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_HID
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_LIS3DH
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_NeoPixel
