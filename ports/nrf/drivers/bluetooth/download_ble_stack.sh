fed1960424969ddd1565520f9258d57e28447870
hathach <thach@tinyusb.org>
2018-08-23 00:27:39 +0700

    remove s140 6.0.0 and s132 6.1.0 (not supported yet)

--- ports/nrf/drivers/bluetooth/download_ble_stack.sh
+++ ports/nrf/drivers/bluetooth/download_ble_stack.sh
@@ -35,40 +35,6 @@ function download_s132_nrf52_5_0_0
     cd -
 }
 
-function download_s132_nrf52_6_1_0
-{
-    echo ""
-    echo "####################################"
-    echo "### Downloading s132_nrf52_6.1.0 ###"
-    echo "####################################"
-    echo ""
-     mkdir -p "${1}/s132_nrf52_6.1.0"
-    cd "${1}/s132_nrf52_6.1.0"
-     wget https://www.nordicsemi.com/eng/nordic/download_resource/67248/5/32206771/141008
-    mv 141008 temp.zip
-    unzip -u temp.zip
-    rm temp.zip
-    cd -
-}
-
-function download_s140_nrf52_6_0_0
-{
-    echo ""
-    echo "####################################"
-    echo "### Downloading s140_nrf52_6.0.0 ###"
-    echo "####################################"
-    echo ""
-
-    mkdir -p "${1}/s140_nrf52_6.0.0"
-    cd "${1}/s140_nrf52_6.0.0"
-
-    wget https://www.nordicsemi.com/eng/nordic/download_resource/60624/20/49271410/116072
-    mv 116072 temp.zip
-    unzip -u temp.zip
-    rm temp.zip
-    cd -
-}
-
 function download_s140_nrf52_6_1_0
 {
     echo ""
@@ -91,8 +57,6 @@ if [ $# -eq 0 ]; then
     echo "No Bluetooth LE stack defined, downloading all."
     download_s132_nrf52_2_0_1 "${SCRIPT_DIR}"
     download_s132_nrf52_5_0_0 "${SCRIPT_DIR}"
-    download_s132_nrf52_6_1_0 "${SCRIPT_DIR}"
-    download_s140_nrf52_6_0_0 "${SCRIPT_DIR}"
     download_s140_nrf52_6_1_0 "${SCRIPT_DIR}"
 else
     case $1 in
@@ -100,10 +64,6 @@ else
             download_s132_nrf52_2_0_1 "${SCRIPT_DIR}" ;;
         "s132_nrf52_5_0_0" )
             download_s132_nrf52_5_0_0 "${SCRIPT_DIR}" ;;
-        "s132_nrf52_6_1_0" )
-            download_s132_nrf52_6_1_0 "${SCRIPT_DIR}" ;;
-        "s140_nrf52_6_0_0" )
-            download_s140_nrf52_6_0_0 "${SCRIPT_DIR}" ;;
         "s140_nrf52_6_1_0" )
             download_s140_nrf52_6_1_0 "${SCRIPT_DIR}" ;;
     esac
