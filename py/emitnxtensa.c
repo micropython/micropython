d3147f0872a4d50c139911050c3448268bdf22ab
Damien George <damien.p.george@gmail.com>
2018-09-04 14:31:28 +1000

    py/emitnative: Add support for return/break/continue in try and with.
    This patch adds full support for unwinding jumps to the native emitter.
    This means that return/break/continue can be used in try-except,
    try-finally and with statements.  For code that doesn't use unwinding jumps
    there is almost no overhead added to the generated code.

--- py/emitnxtensa.c
+++ py/emitnxtensa.c
@@ -8,8 +8,10 @@
 #define GENERIC_ASM_API (1)
 #include "py/asmxtensa.h"
 
-// Word index of REG_LOCAL_1(=a12) in nlr_buf_t
-#define NLR_BUF_IDX_LOCAL_1 (8)
+// Word indices of REG_LOCAL_x in nlr_buf_t
+#define NLR_BUF_IDX_LOCAL_1 (8) // a12
+#define NLR_BUF_IDX_LOCAL_2 (9) // a13
+#define NLR_BUF_IDX_LOCAL_3 (10) // a14
 
 #define N_XTENSA (1)
 #define EXPORT_FUN(name) emit_native_xtensa_##name
