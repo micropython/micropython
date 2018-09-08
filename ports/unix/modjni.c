fe1d9ccb786efa4726c310f459b88ffcef18349c
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-09-09 01:26:06 +0300

    unix/modjni: Update .getiter signature to include mp_obj_iter_buf_t* .
    And thus be buildable again.

--- ports/unix/modjni.c
+++ ports/unix/modjni.c
@@ -316,9 +316,9 @@ MP_DEFINE_CONST_FUN_OBJ_2(subscr_load_adaptor_obj, subscr_load_adaptor);
 
 // .getiter special method which returns iterator which works in terms
 // of object subscription.
-STATIC mp_obj_t subscr_getiter(mp_obj_t self_in) {
+STATIC mp_obj_t subscr_getiter(mp_obj_t self_in, mp_obj_iter_buf_t *iter_buf) {
     mp_obj_t dest[2] = {(mp_obj_t)&subscr_load_adaptor_obj, self_in};
-    return mp_obj_new_getitem_iter(dest);
+    return mp_obj_new_getitem_iter(dest, iter_buf);
 }
 
 STATIC const mp_obj_type_t jobject_type = {
