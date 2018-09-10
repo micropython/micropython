21e3426f7dec96c46d921fa16bb8e5db06572771
shawwwn <shawwwn1@gmail.com>
2018-09-10 00:57:19 -0700

    copyright info

--- ports/esp8266/esp_espnow.c
+++ ports/esp8266/esp_espnow.c
@@ -4,6 +4,7 @@
  * The MIT License (MIT)
  *
  * Copyright (c) 2017 Nick Moore
+ * Copyright (c) 2018 shawwwn <shawwwn1@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
