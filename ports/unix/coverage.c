002a64c86801cea0e25c8ec2ef408539fed6d825
Damien George <damien.p.george@gmail.com>
2018-09-13 10:58:49 +1000

    WIP removing nlr from unix/coverage.c
    Running "make coverage_test" for unix port gives only 6 failed tests, which
    all have to do with the native emitter still using nlr:
    
    714 tests performed (19982 individual testcases)
    708 tests passed
    6 tests failed: native_try native_try_deep native_with viper_misc viper_try viper_with
    
    Change in code size wrt master:
    
       bare-arm:  +900 +1.332% [incl +4(bss)]
    minimal x86: +1304 +0.831% [incl +4(data) +4(bss)]
       unix x64: +3032 +0.623% [incl +8(bss)]
    unix nanbox: +1956 +0.446% [incl +8(data) +4(bss)]
          stm32: +1732 +0.486% [incl +4(bss)]
         cc3200: +1352 +0.731%
        esp8266: +1812 +0.280%
          esp32: +1897 +0.178% [incl -40(data)]

--- ports/unix/coverage.c
+++ ports/unix/coverage.c
@@ -206,20 +206,15 @@ STATIC mp_obj_t extra_coverage(void) {
         mp_printf(&mp_plat_print, "%.*s\n", (int)vstr->len, vstr->buf);
 
         VSTR_FIXED(fix, 4);
-        nlr_buf_t nlr;
-        if (nlr_push(&nlr) == 0) {
-            vstr_add_str(&fix, "large");
-            nlr_pop();
-        } else {
-            mp_obj_print_exception(&mp_plat_print, MP_OBJ_FROM_PTR(nlr.ret_val));
+        if (vstr_add_str(&fix, "large")) {
+            mp_obj_print_exception(&mp_plat_print, MP_STATE_THREAD(cur_exc));
+            MP_STATE_THREAD(cur_exc) = NULL;
         }
 
         fix.len = fix.alloc;
-        if (nlr_push(&nlr) == 0) {
-            vstr_null_terminated_str(&fix);
-            nlr_pop();
-        } else {
-            mp_obj_print_exception(&mp_plat_print, MP_OBJ_FROM_PTR(nlr.ret_val));
+        if (vstr_null_terminated_str(&fix) == NULL) {
+            mp_obj_print_exception(&mp_plat_print, MP_STATE_THREAD(cur_exc));
+            MP_STATE_THREAD(cur_exc) = NULL;
         }
     }
 
