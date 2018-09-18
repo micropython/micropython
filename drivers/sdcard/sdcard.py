1e1234c7b72c883e02099767dcddc2c211ac4ee5
Peter Hinch <peter@hinch.me.uk>
2018-09-18 06:45:45 +0100

    drivers/sdcard: Remove debugging print statement in ioctl method.

--- drivers/sdcard/sdcard.py
+++ drivers/sdcard/sdcard.py
@@ -274,6 +274,5 @@ class SDCard:
             self.write_token(_TOKEN_STOP_TRAN)
 
     def ioctl(self, op, arg):
-        print('ioctl', op, arg)
         if op == 4: # get number of blocks
             return self.sectors
