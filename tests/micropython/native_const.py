d8d981cc98abfaca125e99f95f1daaa335e49a63
Damien George <damien.p.george@gmail.com>
2018-09-27 23:37:33 +1000

    tests/micropython: Test loading const objs in native and viper funcs.

---
+++ tests/micropython/native_const.py
@@ -0,0 +1,14 @@
+# test loading constants in native functions
+
+@micropython.native
+def f():
+    return b'bytes'
+print(f())
+
+@micropython.native
+def f():
+    @micropython.native
+    def g():
+        return 123
+    return g
+print(f()())
