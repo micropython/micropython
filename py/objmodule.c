04f7da78dba7c37846a70ac108317e631950efc0
Damien George <damien.p.george@gmail.com>
2018-09-27 15:16:24 +1000

    py/objmodule: Remove TODO about checking store attr to a module.
    The code implements correct behaviour, as tested by basics/module1.py.

--- py/objmodule.c
+++ py/objmodule.c
@@ -84,7 +84,6 @@ STATIC void module_attr(mp_obj_t self_in, qstr attr, mp_obj_t *dest) {
             mp_obj_dict_delete(MP_OBJ_FROM_PTR(dict), MP_OBJ_NEW_QSTR(attr));
         } else {
             // store attribute
-            // TODO CPython allows STORE_ATTR to a module, but is this the correct implementation?
             mp_obj_dict_store(MP_OBJ_FROM_PTR(dict), MP_OBJ_NEW_QSTR(attr), dest[1]);
         }
         dest[0] = MP_OBJ_NULL; // indicate success
