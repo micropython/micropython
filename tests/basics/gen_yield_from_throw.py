e6078dfed21470dd3b0f3a5e33c78b7db3501711
Damien George <damien.p.george@gmail.com>
2018-09-28 11:33:08 +1000

    tests/basics: Split out gen throw tests from yield-from-throw tests.

--- tests/basics/gen_yield_from_throw.py
+++ tests/basics/gen_yield_from_throw.py
@@ -16,29 +16,3 @@ try:
     print(next(g))
 except TypeError:
     print("got TypeError from downstream!")
-
-# case where generator doesn't intercept the thrown/injected exception
-def gen3():
-    yield 123
-    yield 456
-        
-g3 = gen3()
-print(next(g3))
-try:
-    g3.throw(KeyError)
-except KeyError:
-    print('got KeyError from downstream!')
-
-# case where a thrown exception is caught and stops the generator
-def gen4():
-    try:
-        yield 1
-        yield 2
-    except:
-        pass
-g4 = gen4()
-print(next(g4))
-try:
-    g4.throw(ValueError)
-except StopIteration:
-    print('got StopIteration')
