71978ade5dc4aed97612e66d2383171299eeb0cf
pmp-p <pmp-p@users.noreply.github.com>
2018-10-09 10:21:44 +0200

    Update objmodule.c

--- py/objmodule.c
+++ py/objmodule.c
@@ -61,11 +61,9 @@ STATIC void module_attr(mp_obj_t self_in, qstr attr, mp_obj_t *dest) {
         mp_map_elem_t *elem = mp_map_lookup(&self->globals->map, MP_OBJ_NEW_QSTR(attr), MP_MAP_LOOKUP);
         if (elem != NULL) {
             dest[0] = elem->value;
-        #ifndef MICROPY_CPYTHON_COMPAT
-        }
-        #else
+        #if MICROPY_PEP562
         } else {
-            if (attr != MP_QSTR___getattr__){
+            if (attr != MP_QSTR___getattr__) {
                 mp_obj_t dest2[3];
                 mp_load_method_maybe(self_in, MP_QSTR___getattr__, dest2);
                 if (dest2[0] != MP_OBJ_NULL) {
@@ -73,6 +71,8 @@ STATIC void module_attr(mp_obj_t self_in, qstr attr, mp_obj_t *dest) {
                     dest[0] = mp_call_method_n_kw(1, 0, dest2);
                 }
             }
+        }
+        #else
         }
         #endif
     } else {
