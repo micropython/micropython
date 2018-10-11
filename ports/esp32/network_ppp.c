b76b76f417322c60fe1941be244b27ca326349e3
Eric Poulsen <eric@zyxod.com>
2018-10-11 09:15:03 -0700

    esp32/modnetwork.*: Changed function names esp32/network_ppp.c: Made PPP object non-singleton

--- ports/esp32/network_ppp.c
+++ ports/esp32/network_ppp.c
@@ -48,26 +48,21 @@
 
 typedef struct _ppp_if_obj_t {
     mp_obj_base_t base;
-    bool initialized;
     bool active;
     bool connected;
     ppp_pcb *pcb;
     mp_obj_t stream;
-    struct netif pppif;
     SemaphoreHandle_t inactiveWaitSem;
     TaskHandle_t client_task_handle;
+    struct netif pppif;
 } ppp_if_obj_t;
 
 const mp_obj_type_t ppp_if_type;
-STATIC ppp_if_obj_t ppp_obj = {{&ppp_if_type}, false, false, false, NULL, NULL};
 
-/* PPP status callback example */
 static void ppp_status_cb(ppp_pcb *pcb, int err_code, void *ctx) // wait for
 {
-    ppp_if_obj_t* self = &ppp_obj;
+    ppp_if_obj_t* self = ctx;
     struct netif *pppif = ppp_netif(self->pcb);
-    (void)pppif;
-    LWIP_UNUSED_ARG(ctx);
 
     switch (err_code) {
         case PPPERR_NONE: {
@@ -88,35 +83,40 @@ static void ppp_status_cb(ppp_pcb *pcb, int err_code, void *ctx) // wait for
     }
 }
 
-STATIC mp_obj_t get_ppp(mp_obj_t stream) {
-    ppp_if_obj_t* self = &ppp_obj;
-
-    if (self->initialized) {
-        return MP_OBJ_FROM_PTR(&ppp_obj);
-    }
+STATIC mp_obj_t ppp_make_new(mp_obj_t stream) {
     mp_get_stream_raise(stream, MP_STREAM_OP_READ | MP_STREAM_OP_WRITE);
-    self->stream = stream;
 
+#if MICROPY_PY_USSL_FINALISER
+    ppp_if_obj_t* self = m_new_obj_with_finaliser(ppp_if_obj_t);
+#else
+    ppp_if_obj_t* self = m_new_obj(ppp_if_obj_t);
+#endif
+
+    self->base.type = &ppp_if_type;
+    self->stream = stream;
+    self->active = false;
+    self->connected = false;
     self->inactiveWaitSem = xSemaphoreCreateBinary();
+    self->client_task_handle = NULL;
+
     assert(self->inactiveWaitSem != NULL);
-    self->initialized = true;
-    return MP_OBJ_FROM_PTR(&ppp_obj);
+    return MP_OBJ_FROM_PTR(self);
 }
-MP_DEFINE_CONST_FUN_OBJ_1(get_ppp_obj, get_ppp);
+MP_DEFINE_CONST_FUN_OBJ_1(ppp_make_new_obj, ppp_make_new);
+
 
 static u32_t ppp_output_callback(ppp_pcb *pcb, u8_t *data, u32_t len, void *ctx)
 {
-    ppp_if_obj_t* self = &ppp_obj;
+    ppp_if_obj_t* self = ctx;
     int err;
     return mp_stream_rw(self->stream, data, len, &err, MP_STREAM_RW_WRITE);
 }
 
-static void pppos_client_task() {
-    ppp_if_obj_t* self = &ppp_obj;
+static void pppos_client_task(void* self_in) {
+    ppp_if_obj_t* self = (ppp_if_obj_t*)self_in;
     uint8_t buf[256];
 
     int err;
-    const mp_stream_p_t *stream = mp_get_stream_raise(self->stream, MP_STREAM_OP_READ);
 
     while (ulTaskNotifyTake(pdTRUE, 0) == 0) {
         int len = mp_stream_rw(self->stream, buf, sizeof(buf), &err, 0);
@@ -136,7 +136,7 @@ STATIC mp_obj_t ppp_active(size_t n_args, const mp_obj_t *args) {
                 return mp_const_true;
             }
 
-            self->pcb = pppapi_pppos_create(&self->pppif, ppp_output_callback, ppp_status_cb, NULL);
+            self->pcb = pppapi_pppos_create(&self->pppif, ppp_output_callback, ppp_status_cb, self);
 
             if (self->pcb == NULL) {
                 mp_raise_msg(&mp_type_RuntimeError, "init failed");
@@ -144,7 +144,7 @@ STATIC mp_obj_t ppp_active(size_t n_args, const mp_obj_t *args) {
             pppapi_set_default(self->pcb);
             pppapi_connect(self->pcb, 0);
 
-            xTaskCreate(pppos_client_task, "ppp", 1024, NULL, 1, &self->client_task_handle);
+            xTaskCreate(pppos_client_task, "ppp", 2048, self, 1, &self->client_task_handle);
             self->active = true;
 
         } else {
@@ -163,6 +163,7 @@ STATIC mp_obj_t ppp_active(size_t n_args, const mp_obj_t *args) {
                 mp_hal_delay_ms(10);
             }
 
+            // Release PPP
             pppapi_free(self->pcb);
             self->pcb = NULL;
             self->active = false;
@@ -172,6 +173,17 @@ STATIC mp_obj_t ppp_active(size_t n_args, const mp_obj_t *args) {
     return mp_obj_new_bool(self->active);
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(ppp_active_obj, 1, 2, ppp_active);
+
+#if MICROPY_PY_USSL_FINALISER
+STATIC mp_obj_t ppp_delete(mp_obj_t self_in) {
+    ppp_if_obj_t* self = MP_OBJ_TO_PTR(self_in);
+    mp_obj_t args[] = {self, mp_const_false};
+    ppp_active(2, args);
+    return mp_const_none;
+}
+MP_DEFINE_CONST_FUN_OBJ_1(ppp_delete_obj, ppp_delete);
+#endif
+
 STATIC mp_obj_t ppp_ifconfig(size_t n_args, const mp_obj_t *args) {
     ppp_if_obj_t *self = MP_OBJ_TO_PTR(args[0]);
     ip_addr_t dns;
@@ -217,6 +229,9 @@ STATIC const mp_rom_map_elem_t ppp_if_locals_dict_table[] = {
     { MP_ROM_QSTR(MP_QSTR_isconnected), MP_ROM_PTR(&ppp_isconnected_obj) },
     { MP_ROM_QSTR(MP_QSTR_status), MP_ROM_PTR(&ppp_status_obj) },
     { MP_ROM_QSTR(MP_QSTR_ifconfig), MP_ROM_PTR(&ppp_ifconfig_obj) },
+#if MICROPY_PY_USSL_FINALISER
+    { MP_ROM_QSTR(MP_QSTR___del__), MP_ROM_PTR(&ppp_delete_obj) },
+#endif
 };
 
 STATIC MP_DEFINE_CONST_DICT(ppp_if_locals_dict, ppp_if_locals_dict_table);
