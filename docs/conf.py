86230ee1be2629d5fc78e988f1b80ce9e7be70f8
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2018-10-20 11:44:12 +0300

    docs/conf.py: Use https for link to docs.python.org.
    To get rid of warning saying there's redirect from http: to https:.

--- docs/conf.py
+++ docs/conf.py
@@ -298,4 +298,4 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'python': ('http://docs.python.org/3.5', None)}
+intersphinx_mapping = {'python': ('https://docs.python.org/3.5', None)}
