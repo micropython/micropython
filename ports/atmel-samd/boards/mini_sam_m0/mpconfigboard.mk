aec39fd02723537028371efda84878c584e0f02e
Benjamin Shockley <benjaminshockley@hotmail.com>
2018-10-26 12:09:59 -0500

    Updated to new D21G18 MCU
    Updated MIni SAM M0 to new D21G18A MCU to match layout/design of M4

--- ports/atmel-samd/boards/mini_sam_m0/mpconfigboard.mk
+++ ports/atmel-samd/boards/mini_sam_m0/mpconfigboard.mk
@@ -1,14 +1,11 @@
-LD_FILE = boards/samd21x18-bootloader.ld
+LD_FILE = boards/samd21x18-bootloader-external-flash-crystalless.ld
 USB_VID = 0x1209
 USB_PID = 0x2017
 USB_PRODUCT = "Mini SAM M0"
 USB_MANUFACTURER = "Benjamin Shockley"
 
-INTERNAL_FLASH_FILESYSTEM = 1
-LONGINT_IMPL = NONE
+SPI_FLASH_FILESYSTEM = 1
+LONGINT_IMPL = MPZ
 
-CHIP_VARIANT = SAMD21E18A
+CHIP_VARIANT = SAMD21G18A
 CHIP_FAMILY = samd21
-
-# Include these Python libraries in firmware.
-FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_NeoPixel
