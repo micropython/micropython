62c4f80ed3dfc898d4e8b1de1ebbbeb217cbe251
Krono <andreasgm93@gmail.com>
2019-01-18 12:55:02 +0100

    frozen boot.py and main.py executed instead of regular files

--- ports/esp32/main.c
+++ ports/esp32/main.c
@@ -93,9 +93,9 @@ soft_reset:
 
     // run boot-up scripts
     pyexec_frozen_module("_boot.py");
-    pyexec_file("boot.py");
+    pyexec_frozen_module("boot.py");
     if (pyexec_mode_kind == PYEXEC_MODE_FRIENDLY_REPL) {
-        pyexec_file("main.py");
+        pyexec_frozen_module("main.py");
     }
 
     for (;;) {
