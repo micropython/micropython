aba83e66d7d405d64e5788537eade62329f70929
Damien George <damien.p.george@gmail.com>
2019-01-26 00:44:35 +1100

    py/mpconfig.h: Remove parentheses from MICROPY_VERSION_xxx macros.
    Otherwise MICROPY_VERSION_STRING includes these parentheses in the string.

--- py/mpconfig.h
+++ py/mpconfig.h
@@ -27,9 +27,9 @@
 #define MICROPY_INCLUDED_PY_MPCONFIG_H
 
 // Current version of MicroPython
-#define MICROPY_VERSION_MAJOR (1)
-#define MICROPY_VERSION_MINOR (9)
-#define MICROPY_VERSION_MICRO (4)
+#define MICROPY_VERSION_MAJOR 1
+#define MICROPY_VERSION_MINOR 9
+#define MICROPY_VERSION_MICRO 4
 
 // Combined version as a 32-bit number for convenience
 #define MICROPY_VERSION ( \
