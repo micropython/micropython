a51f2b07161239e0a3805cb40914f159f2038c87
hathach <thach@tinyusb.org>
2019-01-30 22:30:23 +0700

    refactor nrfx from supervisor/usb.c

--- ports/nrf/supervisor/usb.c
+++ ports/nrf/supervisor/usb.c
@@ -45,6 +45,11 @@ void init_usb_hardware(void) {
     // 2 is max priority (0, 1 are reserved for SD)
     NVIC_SetPriority(USBD_IRQn, 2);
 
+    // USB power may already be ready at this time -> no event generated
+    // We need to invoke the handler based on the status initially for the first call
+    static bool first_call = true;
+    uint32_t usb_reg;
+
 #ifdef SOFTDEVICE_PRESENT
     uint8_t sd_en = false;
     (void) sd_softdevice_is_enabled(&sd_en);
@@ -53,6 +58,8 @@ void init_usb_hardware(void) {
         sd_power_usbdetected_enable(true);
         sd_power_usbpwrrdy_enable(true);
         sd_power_usbremoved_enable(true);
+
+        sd_power_usbregstatus_get(&usb_reg);
     }else
 #endif
     {
@@ -65,5 +72,18 @@ void init_usb_hardware(void) {
         nrfx_power_usbevt_init(&config);
 
         nrfx_power_usbevt_enable();
+
+        usb_reg = NRF_POWER->USBREGSTATUS;
+    }
+
+    if ( first_call ) {
+        first_call = false;
+        if ( usb_reg & POWER_USBREGSTATUS_VBUSDETECT_Msk ) {
+            tusb_hal_nrf_power_event(NRFX_POWER_USB_EVT_DETECTED);
+        }
+
+        if ( usb_reg & POWER_USBREGSTATUS_OUTPUTRDY_Msk ) {
+            tusb_hal_nrf_power_event(NRFX_POWER_USB_EVT_READY);
+        }
     }
 }
