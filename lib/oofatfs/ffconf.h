a393a6e0c578231123220e73adc32b8ee58db64b
Scott Shawcroft <scott@tannewt.org>
2019-02-03 13:41:20 -0800

    Add fast seek support to file objects

--- lib/oofatfs/ffconf.h
+++ lib/oofatfs/ffconf.h
@@ -74,7 +74,7 @@
 /* This option switches f_mkfs() function. (0:Disable or 1:Enable) */
 
 
-#define _USE_FASTSEEK   0
+#define _USE_FASTSEEK   1
 /* This option switches fast seek function. (0:Disable or 1:Enable) */
 
 
