ed33c6af3a5f83d2a43046ed35fcc6129e4efa9d
Jolatomme <47304809+Jolatomme@users.noreply.github.com>
2019-02-04 23:14:48 +0100

    /boards/NUCLEO_L476RG mpconfigboard.h update
    Added support for CAN bus, enabled RNG and DAC support.

--- ports/stm32/boards/NUCLEO_L476RG/mpconfigboard.h
+++ ports/stm32/boards/NUCLEO_L476RG/mpconfigboard.h
@@ -3,15 +3,17 @@
 
 #define MICROPY_HW_HAS_SWITCH       (1)
 #define MICROPY_HW_HAS_FLASH        (1)
+#define MICROPY_HW_ENABLE_RNG       (1)
 #define MICROPY_HW_ENABLE_RTC       (1)
 #define MICROPY_HW_ENABLE_USB       (1)
+#define MICROPY_HW_ENABLE_DAC       (1)
 
 // MSI is used and is 4MHz
 #define MICROPY_HW_CLK_PLLM (1)
 #define MICROPY_HW_CLK_PLLN (40)
-#define MICROPY_HW_CLK_PLLR (2)
-#define MICROPY_HW_CLK_PLLP (7)
-#define MICROPY_HW_CLK_PLLQ (4)
+#define MICROPY_HW_CLK_PLLP (RCC_PLLP_DIV7)
+#define MICROPY_HW_CLK_PLLQ (RCC_PLLQ_DIV2)
+#define MICROPY_HW_CLK_PLLR (RCC_PLLR_DIV2)
 
 // UART config
 #define MICROPY_HW_UART2_TX     (pin_A2)
@@ -23,22 +25,26 @@
 #define MICROPY_HW_FLASH_LATENCY    FLASH_LATENCY_4
 
 // I2C busses
-#define MICROPY_HW_I2C1_SCL (pin_B6)
-#define MICROPY_HW_I2C1_SDA (pin_B7)
+#define MICROPY_HW_I2C1_SCL (pin_B8)
+#define MICROPY_HW_I2C1_SDA (pin_B9)
 #define MICROPY_HW_I2C2_SCL (pin_B10)
 #define MICROPY_HW_I2C2_SDA (pin_B11)
-#define MICROPY_HW_I2C3_SCL (pin_C0)
-#define MICROPY_HW_I2C3_SDA (pin_C1)
+//#define MICROPY_HW_I2C3_SCL (pin_C0)
+//#define MICROPY_HW_I2C3_SDA (pin_C1)
 
 // SPI busses
 #define MICROPY_HW_SPI1_NSS     (pin_A4)
 #define MICROPY_HW_SPI1_SCK     (pin_B3)
 #define MICROPY_HW_SPI1_MISO    (pin_B4)
 #define MICROPY_HW_SPI1_MOSI    (pin_B5)
-#define MICROPY_HW_SPI2_NSS     (pin_B12)
-#define MICROPY_HW_SPI2_SCK     (pin_B13)
-#define MICROPY_HW_SPI2_MISO    (pin_B14)
-#define MICROPY_HW_SPI2_MOSI    (pin_B15)
+//#define MICROPY_HW_SPI2_NSS     (pin_B12)
+//#define MICROPY_HW_SPI2_SCK     (pin_B13)
+//#define MICROPY_HW_SPI2_MISO    (pin_B14)
+//#define MICROPY_HW_SPI2_MOSI    (pin_B15)
+
+// CAN bus
+#define MICROPY_HW_CAN1_TX (pin_A12)
+#define MICROPY_HW_CAN1_RX (pin_A11)
 
 // USRSW is pulled low. Pressing the button makes the input go high.
 #define MICROPY_HW_USRSW_PIN        (pin_C13)
