0c50154c837df7900dbc55dbc3fb81c2d25a21c9
Scott Shawcroft <scott@tannewt.org>
2019-02-04 16:11:16 -0800

    Use generic overflow so 64 bit is handled ok.

--- py/sequence.c
+++ py/sequence.c
@@ -37,7 +37,7 @@
 // Detect when a multiply causes an overflow.
 size_t mp_seq_multiply_len(size_t item_sz, size_t len) {
     size_t new_len;
-    if (__builtin_umul_overflow(item_sz, len, &new_len)) {
+    if (__builtin_mul_overflow(item_sz, len, &new_len)) {
         mp_raise_msg(&mp_type_OverflowError, translate("small int overflow"));
     }
     return new_len;
