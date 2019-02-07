39eb1e9f81200422095b1dd1a56d0311e7300978
Damien George <damien.p.george@gmail.com>
2019-02-07 16:04:06 +1100

    stm32/mboot: Add support for STM32F769 MCUs.

--- ports/stm32/mboot/main.c
+++ ports/stm32/mboot/main.c
@@ -369,7 +369,7 @@ static const flash_layout_t flash_layout[] = {
     #endif
 };
 
-#elif defined(STM32F765xx) || defined(STM32F767xx)
+#elif defined(STM32F765xx) || defined(STM32F767xx) || defined(STM32F769xx)
 
 #define FLASH_LAYOUT_STR "@Internal Flash  /0x08000000/04*032Kg,01*128Kg,07*256Kg" MBOOT_SPIFLASH_LAYOUT MBOOT_SPIFLASH2_LAYOUT
 
