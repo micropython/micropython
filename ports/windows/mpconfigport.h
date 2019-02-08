52b4b4e3185f76716db24fca598e640ab122ff54
Yonatan Goldschmidt <yon.goldschmidt@gmail.com>
2019-02-08 17:09:48 +0200

    windows: Convert to use MP_BUILTIN_MODULE.

--- ports/windows/mpconfigport.h
+++ ports/windows/mpconfigport.h
@@ -174,12 +174,12 @@ void mp_hal_dupterm_tx_strn(const char *str, size_t len);
 #define MICROPY_PORT_BUILTINS \
     { MP_ROM_QSTR(MP_QSTR_open), MP_ROM_PTR(&mp_builtin_open_obj) },
 
-extern const struct _mp_obj_module_t mp_module_os;
-extern const struct _mp_obj_module_t mp_module_time;
+extern const struct _mp_obj_module_t mp_module_uos;
+extern const struct _mp_obj_module_t mp_module_utime;
 #define MICROPY_PORT_BUILTIN_MODULES \
-    { MP_ROM_QSTR(MP_QSTR_utime), MP_ROM_PTR(&mp_module_time) }, \
-    { MP_ROM_QSTR(MP_QSTR_umachine), MP_ROM_PTR(&mp_module_machine) }, \
-    { MP_ROM_QSTR(MP_QSTR_uos), MP_ROM_PTR(&mp_module_os) }, \
+	MP_BUILTIN_MODULE(utime), \
+	MP_BUILTIN_MODULE(machine), \
+	MP_BUILTIN_MODULE(uos),
 
 #if MICROPY_USE_READLINE == 1
 #define MICROPY_PORT_ROOT_POINTERS \
