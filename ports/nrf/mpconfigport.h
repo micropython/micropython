9f7d19a937b23b96d18665258a65bd9afc1475b2
Scott Shawcroft <scott@tannewt.org>
2019-02-11 17:46:07 -0800

    Change EMERGENCY_EXCEPTION_BUF settings to match samd.

--- ports/nrf/mpconfigport.h
+++ ports/nrf/mpconfigport.h
@@ -121,9 +121,6 @@
 
 #define MICROPY_KBD_EXCEPTION                    (1)
 
-#define MICROPY_ENABLE_EMERGENCY_EXCEPTION_BUF   (1)
-#define MICROPY_EMERGENCY_EXCEPTION_BUF_SIZE     (0)
-
 // Scan gamepad every 32ms
 #define CIRCUITPY_GAMEPAD_TICKS 0x1f
 
