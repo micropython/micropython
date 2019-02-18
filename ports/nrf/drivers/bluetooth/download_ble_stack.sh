09cf7defc3d4460daf72a992cbb61b0a6e7fd352
Glenn Ruben Bakke <glennbakke@gmail.com>
2019-02-18 21:31:10 +0100

    nrf/bluetooth: Deprecate use of SoftDevice s140 v6.0.0.
    Removing linker script for nrf52840 s140 v6.0.0 as pca10056
    target board now points to the new v6.1.1. Also, removing the
    entry from the download_ble_stack.sh script.

--- ports/nrf/drivers/bluetooth/download_ble_stack.sh
+++ ports/nrf/drivers/bluetooth/download_ble_stack.sh
@@ -38,25 +38,6 @@ function download_s132_nrf52_6_1_1
     cd -
 }
 
-function download_s140_nrf52_6_0_0
-{
-    echo ""
-    echo "####################################"
-    echo "### Downloading s140_nrf52_6.0.0 ###"
-    echo "####################################"
-    echo ""
-
-    mkdir -p $1/s140_nrf52_6.0.0
-    cd $1/s140_nrf52_6.0.0
-    wget --post-data="fileName=DeviceDownload&ids=D631FCC10C9741A49135BC0450E42B19" https://www.nordicsemi.com/api/sitecore/Products/MedialibraryZipDownload2
-    mv MedialibraryZipDownload2 temp.zip
-    unzip -u temp.zip
-    unzip -u s140nrf52600.zip
-    rm s140nrf52600.zip
-    rm temp.zip
-    cd -
-}
-
 function download_s140_nrf52_6_1_1
 {
     echo ""
@@ -82,7 +63,6 @@ if [ $# -eq 0 ]; then
     echo "No Bluetooth LE stack defined, downloading all."
     download_s110_nrf51_8_0_0 ${SCRIPT_DIR}
     download_s132_nrf52_6_1_1 ${SCRIPT_DIR}
-    download_s140_nrf52_6_0_0 ${SCRIPT_DIR}
     download_s140_nrf52_6_1_1 ${SCRIPT_DIR}
 else 
     case $1 in
@@ -90,8 +70,6 @@ else
             download_s110_nrf51_8_0_0 ${SCRIPT_DIR} ;;
         "s132_nrf52_6_1_1" )
             download_s132_nrf52_6_1_1 ${SCRIPT_DIR} ;;
-        "s140_nrf52_6_0_0" )
-            download_s140_nrf52_6_0_0 ${SCRIPT_DIR} ;;
         "s140_nrf52_6_1_1" )
             download_s140_nrf52_6_1_1 ${SCRIPT_DIR} ;;
     esac
