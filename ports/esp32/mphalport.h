297092a76a6be2d5f5da32524e91f768e95937f4
Damien George <damien.p.george@gmail.com>
2019-03-12 15:46:44 +1100

    esp32/mphalport: Use ets_delay_us for mp_hal_delay_us_fast.
    The system provided one is in ROM and is more accurate.

--- ports/esp32/mphalport.h
+++ ports/esp32/mphalport.h
@@ -47,7 +47,7 @@ __attribute__((always_inline)) static inline uint32_t mp_hal_ticks_cpu(void) {
 }
 
 void mp_hal_delay_us(uint32_t);
-void mp_hal_delay_us_fast(uint32_t);
+#define mp_hal_delay_us_fast(us) ets_delay_us(us)
 void mp_hal_set_interrupt_char(int c);
 uint32_t mp_hal_get_cpu_freq(void);
 
