9d1f9b80d36ff2919992f08447961f8e60b2a677
Ilya Mordasov <carlogulliani@gmail.com>
2019-03-14 23:54:07 +0300

    Init GATTToolBackend -> Bug fixes

--- extmod/modupygatt.c
+++ extmod/modupygatt.c
@@ -9,20 +9,20 @@
 
 #if MICROPY_PY_BLUETOOTH
 
-STATIC mp_obj_t GATTToolBackend(size_t n_args, const mp_obj_t *args)
+STATIC mp_obj_t gatt_tool_backend(void)
 {
   printf("GATTToolBackend init\r\n");
   return mp_const_none;
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_0(GATTToolBackend_obj, GATTToolBackend);
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(gatt_tool_backend_obj, gatt_tool_backend);
 
 STATIC const mp_rom_map_elem_t mp_module_upygatt_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_upygatt) },
 
-    { MP_ROM_QSTR(MP_QSTR_GATTToolBackend), MP_ROM_PTR(&GATTToolBackend_obj) },
+    { MP_ROM_QSTR(MP_QSTR_GATTToolBackend), MP_ROM_PTR(&gatt_tool_backend_obj) },
 };
 
-STATIC MP_DEFINE_CONST_DICT(upygatt_module_globals, upygatt_module_globals_table);
+STATIC MP_DEFINE_CONST_DICT(mp_module_upygatt_globals, mp_module_upygatt_globals_table);
 
 const mp_obj_module_t mp_module_upygatt = {
     .base = { &mp_type_module },
