49d6b8a75c9b76b763a802f04b328e06aa2f980a
IlyaM <carlogulliani@gmail.com>
2019-03-15 12:03:14 +0300

    Extended GATTToolBackend class and bug fixes

--- extmod/modupygatt.c
+++ extmod/modupygatt.c
@@ -9,12 +9,45 @@
 
 #if MICROPY_PY_BLUETOOTH
 
+STATIC const mp_obj_type_t upygatt_type;
+
+typedef struct _mp_obj_upygatt_t {
+    mp_obj_base_t base;
+} mp_obj_upygatt_t;
+
+// instantiated Bluetooth object
+STATIC const mp_obj_upygatt_t upygatt_obj = {
+    { &upygatt_type },
+};
+
+STATIC mp_obj_t upygatt_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *all_args) {
+    return MP_OBJ_FROM_PTR(&upygatt_obj);
+}
+
+STATIC mp_obj_t upygatt_start(size_t n_args, const mp_obj_t *args) {
+  printf("start %d\r\n", n_args);
+  return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(upygatt_start_obj, 1, 2, upygatt_start);
+
 STATIC mp_obj_t gatt_tool_backend(void)
 {
   printf("GATTToolBackend init\r\n");
   return mp_const_none;
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_0(gatt_tool_backend_obj, gatt_tool_backend);
+
+STATIC const mp_rom_map_elem_t upygatt_locals_dict_table[] = {
+    { MP_ROM_QSTR(MP_QSTR_start),        MP_ROM_PTR(&upygatt_start_obj) },
+};
+STATIC MP_DEFINE_CONST_DICT(upygatt_locals_dict, upygatt_locals_dict_table);
+
+STATIC const mp_obj_type_t upygatt_type = {
+    { &mp_type_type },
+    .name = MP_QSTR_upygatt,
+    .make_new = upygatt_make_new,
+    .locals_dict = (void*)&upygatt_locals_dict,
+};
 
 STATIC const mp_rom_map_elem_t mp_module_upygatt_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_upygatt) },
