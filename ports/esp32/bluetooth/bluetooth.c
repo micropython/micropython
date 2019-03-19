975e81b0ac2f4b8ac11478c56576ace7b2682ab8
IlyaM <carlogulliani@gmail.com>
2019-03-19 13:05:36 +0300

    Bug fixes

--- ports/esp32/bluetooth/bluetooth.c
+++ ports/esp32/bluetooth/bluetooth.c
@@ -1,8 +1,11 @@
 #if MICROPY_PY_BLUETOOTH
 
 #include "esp_bt.h"
+#include "esp_gap_ble_api.h"
+#include "esp_gattc_api.h"
+#include "esp_gatt_defs.h"
 #include "esp_bt_main.h"
-#include "esp_gatts_api.h"
+#include "esp_gatt_common_api.h"
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/semphr.h"
