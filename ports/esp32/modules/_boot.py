fe3cb1306bde16eb6ad87fc6fe50d78adc3b46c3
Ilya Mordasov <carlogulliani@gmail.com>
2019-03-28 00:40:19 +0300

    Bug fixes and other minor improvements

--- ports/esp32/modules/_boot.py
+++ ports/esp32/modules/_boot.py
@@ -1,12 +1,5 @@
 import gc
 import uos
-import time
-from umqtt import MQTTClient
-import ubinascii
-import machine
-import micropython
-import network
-import esp
 from flashbdev import bdev
 
 try:
@@ -17,12 +10,3 @@ except OSError:
     vfs = inisetup.setup()
 
 gc.collect()
-
-wlan = network.WLAN(network.STA_IF)
-if not wlan.active() or not wlan.isconnected():
-wlan.active(True)
-print('connecting to:', ssid)
-wlan.connect("Smart_box-142", "natalia31081980")
-while not wlan.isconnected():
-    pass
-print('network config:', wlan.ifconfig())
