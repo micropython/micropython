c04dfcd5d12abb16d57984851ec7fa82d6c2cb9e
Peter D. Gray <peter@conalgo.com>
2019-04-11 14:46:56 -0400

    ports/stm32/gccollect.c: dont assume end of stack == end of RAM

--- ports/stm32/gccollect.c
+++ ports/stm32/gccollect.c
@@ -52,7 +52,7 @@ void gc_collect(void) {
     #if MICROPY_PY_THREAD
     gc_collect_root((void**)sp, ((uint32_t)MP_STATE_THREAD(stack_top) - sp) / sizeof(uint32_t));
     #else
-    gc_collect_root((void**)sp, ((uint32_t)&_ram_end - sp) / sizeof(uint32_t));
+    gc_collect_root((void**)sp, ((uint32_t)&_estack - sp) / sizeof(uint32_t));
     #endif
 
     // trace root pointers from any threads
