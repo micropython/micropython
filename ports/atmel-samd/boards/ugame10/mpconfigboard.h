0599286e32d0f81a175a20fe608c8a6c91725411
Radomir Dopieralski <openstack@sheep.art.pl>
2019-04-12 20:45:52 +0200

    Use never_reset in ugame10

--- ports/atmel-samd/boards/ugame10/mpconfigboard.h
+++ ports/atmel-samd/boards/ugame10/mpconfigboard.h
@@ -7,7 +7,7 @@
 #define SPI_FLASH_CS_PIN            &pin_PA18
 
 // These are pins not to reset.
-#define MICROPY_PORT_A        (PORT_PA06 | PORT_PA07 | PORT_PA08 | PORT_PA09)
+#define MICROPY_PORT_A        (0)
 #define MICROPY_PORT_B        (0)
 #define MICROPY_PORT_C        (0)
 
