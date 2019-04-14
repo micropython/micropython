042b5459248d0e3b63426e7231aa0ff3fcf0f379
Ilya Mordasov <carlogulliani@gmail.com>
2019-04-14 11:22:18 +0300

    Bug fixes and other minor improvements

--- ports/esp32/modules/g201s.py
+++ ports/esp32/modules/g201s.py
@@ -135,7 +135,6 @@ class G201S():
             self.write_handle(0x000c, [0x01, 0x00])
             self.write_handle(0x000e, [0x01])
             self.write_handle(0x000e, [0x06])
-            print("get temperature")
             time.sleep_ms(500)
             value = self.adapter.char_read(uuid="6e400003-b5a3-f393-e0a9-e50e24dcca9e", value_handle=0x000b)
             value = hexlify(value)
