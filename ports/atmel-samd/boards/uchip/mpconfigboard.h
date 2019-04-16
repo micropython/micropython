4f5a7ffe46a5ab18a5ff377315bb65b1bc51bbc1
ITACA Innovation S.R.L <40298126+ITACAInnovation@users.noreply.github.com>
2019-04-16 16:45:47 +0200

    Updates: VID&PID, pinout
    Changed VID & PID to new ones.
    Changed the layout accordingly to the prototype version.

--- ports/atmel-samd/boards/uchip/mpconfigboard.h
+++ ports/atmel-samd/boards/uchip/mpconfigboard.h
@@ -47,9 +47,9 @@
 #define DEFAULT_I2C_BUS_SCL (&pin_PA23)
 #define DEFAULT_I2C_BUS_SDA (&pin_PA22)
 
-#define DEFAULT_SPI_BUS_SCK (&pin_PA16)
-#define DEFAULT_SPI_BUS_MOSI (&pin_PA17)
+#define DEFAULT_SPI_BUS_SCK (&pin_PA17)
+#define DEFAULT_SPI_BUS_MOSI (&pin_PA16)
 #define DEFAULT_SPI_BUS_MISO (&pin_PA19)
 
-#define DEFAULT_UART_BUS_RX (&pin_PA11)
-#define DEFAULT_UART_BUS_TX (&pin_PA10)
+#define DEFAULT_UART_BUS_RX (&pin_PA09)
+#define DEFAULT_UART_BUS_TX (&pin_PA08)
--- ports/atmel-samd/boards/uchip/mpconfigboard.mk
+++ ports/atmel-samd/boards/uchip/mpconfigboard.mk
@@ -1,6 +1,6 @@
 LD_FILE = boards/samd21x18-bootloader.ld
 USB_VID = 0x04D8
-USB_PID = 0xED7D
+USB_PID = 0xED5F
 USB_PRODUCT = "uChip CircuitPython"
 USB_MANUFACTURER = "Itaca Innovation"
 
@@ -10,5 +10,3 @@ CIRCUITPY_SMALL_BUILD = 1
 
 CHIP_VARIANT = SAMD21E18A
 CHIP_FAMILY = samd21
-
-CIRCUITPY_FREQUENCYIO = 0
