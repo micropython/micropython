d174ce882119c9f652c2df5297fe18602416364a
sabas1080 <s@theinventorhouse.org>
2019-05-10 13:34:02 -0500

    Update pins define

--- ports/atmel-samd/boards/escornabot_makech/mpconfigboard.h
+++ ports/atmel-samd/boards/escornabot_makech/mpconfigboard.h
@@ -1,7 +1,7 @@
 #define MICROPY_HW_BOARD_NAME "Escornabot Makech"
 #define MICROPY_HW_MCU_NAME "samd21g18"
 
-#define MICROPY_HW_LED_STATUS   (&pin_PB23)
+#define MICROPY_HW_LED_STATUS   (&pin_PA02)
 
 // These are pins not to reset.
 #define MICROPY_PORT_A        (0)
@@ -19,15 +19,15 @@
 // Explanation of how a user got into safe mode.
 #define BOARD_USER_SAFE_MODE_ACTION "pressing both buttons at start up"
 
-#define DEFAULT_I2C_BUS_SCL (&pin_PA01)
-#define DEFAULT_I2C_BUS_SDA (&pin_PA00)
+#define DEFAULT_I2C_BUS_SCL (&pin_PA08)
+#define DEFAULT_I2C_BUS_SDA (&pin_PA09)
 
-#define DEFAULT_SPI_BUS_SCK (&pin_PA15)
-#define DEFAULT_SPI_BUS_MOSI (&pin_PA14)
-#define DEFAULT_SPI_BUS_MISO (&pin_PA12)
+#define DEFAULT_SPI_BUS_SCK (&pin_PA14)
+#define DEFAULT_SPI_BUS_MOSI (&pin_PA19)
+#define DEFAULT_SPI_BUS_MISO (&pin_PA20)
 
-#define DEFAULT_UART_BUS_RX (&pin_PA11)
-#define DEFAULT_UART_BUS_TX (&pin_PA10)
+#define DEFAULT_UART_BUS_RX (&pin_PA01)
+#define DEFAULT_UART_BUS_TX (&pin_PA00)
 
 // USB is always used internally so skip the pin objects for it.
 #define IGNORE_PIN_PA24     1
