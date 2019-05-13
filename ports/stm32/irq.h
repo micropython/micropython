07af74daef4398540fa623244b4802c22cee3a12
iabdalkader <i.abdalkader@gmail.com>
2019-05-13 19:33:22 +0200

    stm32/spi: Enable SPI IRQs and add IRQHandlers for H7 MCUs.
    The H7 HAL uses SPI IRQs when the SPI is running in DMA mode.

--- ports/stm32/irq.h
+++ ports/stm32/irq.h
@@ -144,6 +144,8 @@ MP_DECLARE_CONST_FUN_OBJ_0(pyb_irq_stats_obj);
 
 #define IRQ_PRI_CAN             NVIC_EncodePriority(NVIC_PRIORITYGROUP_4, 7, 0)
 
+#define IRQ_PRI_SPI             NVIC_EncodePriority(NVIC_PRIORITYGROUP_4, 8, 0)
+
 // Interrupt priority for non-special timers.
 #define IRQ_PRI_TIMX            NVIC_EncodePriority(NVIC_PRIORITYGROUP_4, 13, 0)
 
