a474ddf959fab641ab65631a8e140cf5bbda2541
Damien George <damien.p.george@gmail.com>
2019-05-14 17:22:49 +1000

    tests/basics: Add coverage tests for memoryview attributes.

--- tests/basics/memoryview_itemsize.py
+++ tests/basics/memoryview_itemsize.py
@@ -7,3 +7,9 @@ except:
 
 for code in ['b', 'h', 'i', 'l', 'q', 'f', 'd']:
     print(memoryview(array(code)).itemsize)
+
+# shouldn't be able to store to the itemsize attribute
+try:
+    memoryview(b'a').itemsize = 1
+except AttributeError:
+    print('AttributeError')
