016d9a40fe822a624449ad10486706991a97be29
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2019-05-14 15:51:57 +0300

    various: Add and update my copyright line based on git history.
    For modules I initially created or made substantial contributions to.

--- py/binary.c
+++ py/binary.c
@@ -3,6 +3,7 @@
  *
  * The MIT License (MIT)
  *
+ * Copyright (c) 2014-2017 Paul Sokolovsky
  * Copyright (c) 2013, 2014 Damien P. George
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
--- py/binary.h
+++ py/binary.h
@@ -3,6 +3,7 @@
  *
  * The MIT License (MIT)
  *
+ * Copyright (c) 2014 Paul Sokolovsky
  * Copyright (c) 2013, 2014 Damien P. George
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
