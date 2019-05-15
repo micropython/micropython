00c39805f19abc6af565c1e3faee10c196d5f666
Scott Shawcroft <scott@tannewt.org>
2019-05-15 14:17:09 -0700

    Remove old comment

--- shared-module/displayio/Group.c
+++ shared-module/displayio/Group.c
@@ -96,7 +96,6 @@ mp_obj_t common_hal_displayio_group_pop(displayio_group_t* self, size_t index) {
 }
 
 mp_int_t common_hal_displayio_group_index(displayio_group_t* self, mp_obj_t layer) {
-    // Shift everything left.
     for (size_t i = 0; i < self->size; i++) {
         if (self->children[i].original == layer) {
             return i;
