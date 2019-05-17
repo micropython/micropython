653e1756c095a761dfea5bd91b94b9b2a150702c
Damien George <damien.p.george@gmail.com>
2019-05-17 18:02:44 +1000

    various: Update early copyright years to match actual edit history.

--- py/frozenmod.h
+++ py/frozenmod.h
@@ -4,7 +4,7 @@
  * The MIT License (MIT)
  *
  * Copyright (c) 2015 Paul Sokolovsky
- * Copyright (c) 2014 Damien P. George
+ * Copyright (c) 2016 Damien P. George
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
