4a7122d3541529823a6dc84e0c91520c3c50618b
Tavish Naruka <tavishnaruka@gmail.com>
2019-05-23 13:58:02 +0530

    boards: add support for Electronut labs Blip
    Signed-off-by: Tavish Naruka <tavishnaruka@gmail.com>

---
+++ ports/nrf/boards/electronut_labs_blip/mpconfigboard.mk
@@ -0,0 +1,22 @@
+USB_VID = 0x239A
+USB_PID = 0x803C
+USB_PRODUCT = "Blip"
+USB_MANUFACTURER = "Electronut Labs"
+
+MCU_SERIES = m4
+MCU_VARIANT = nrf52
+MCU_SUB_VARIANT = nrf52840
+MCU_CHIP = nrf52840
+SD ?= s140
+SOFTDEV_VERSION ?= 6.1.0
+
+BOOT_SETTING_ADDR = 0xFF000
+
+ifeq ($(SD),)
+	LD_FILE = boards/nrf52840_1M_256k.ld
+else
+	LD_FILE = boards/adafruit_$(MCU_SUB_VARIANT)_$(SD_LOWER)_v$(firstword $(subst ., ,$(SOFTDEV_VERSION))).ld
+	CIRCUITPY_BLEIO = 1
+endif
+
+NRF_DEFINES += -DNRF52840_XXAA -DNRF52840
