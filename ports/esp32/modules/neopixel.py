c7ca6ba6b598b0574ae0152063410cfe412ed3ae
Pratham Singh <kumar.pratham@learner.manipal.edu>
2019-05-26 21:40:28 +0530

    Add argument to control brightness
    Inspired from circuitpython/rpi_ws281X. https://circuitpython.readthedocs.io/projects/neopixel/en/latest/api.html#neopixel.NeoPixel

--- ports/esp32/modules/neopixel.py
+++ ports/esp32/modules/neopixel.py
@@ -7,18 +7,19 @@ from esp import neopixel_write
 class NeoPixel:
     ORDER = (1, 0, 2, 3)
     
-    def __init__(self, pin, n, bpp=3, timing=1):
+    def __init__(self, pin, n, brightness=0.5, bpp=3, timing=1):
         self.pin = pin
         self.n = n
         self.bpp = bpp
         self.buf = bytearray(n * bpp)
         self.pin.init(pin.OUT)
         self.timing = timing
+        self.brightness = brightness
 
     def __setitem__(self, index, val):
         offset = index * self.bpp
         for i in range(self.bpp):
-            self.buf[offset + self.ORDER[i]] = val[i]
+            self.buf[offset + self.ORDER[i]] = int(round(val[i]*self.brightness))
 
     def __getitem__(self, index):
         offset = index * self.bpp
