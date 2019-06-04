elvis-epx/circuitpython

Add PS/2 support -- ps2io module

ports/atmel-samd/common-hal/ps2io/Ps2.h
@@ -3,7 +3,8 @@
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2019 Dan Halbert for Adafruit Industries
+ * Copyright (c) 2017 Scott Shawcroft for Adafruit Industries
+ * Copyright (c) 2019 Elvis Pfutzenreuter <epxx@epxx.co>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -24,42 +25,38 @@
  * THE SOFTWARE.
  */
 
-#include "common-hal/pulseio/PulseIn.h"
-#include "common-hal/ps2io/Ps2.h"
-#include "common-hal/rotaryio/IncrementalEncoder.h"
-#include "shared-bindings/microcontroller/__init__.h"
-//#include "samd/external_interrupts.h"
-#include "eic_handler.h"
+#ifndef MICROPY_INCLUDED_ATMEL_SAMD_COMMON_HAL_PS2IO_PS2_H
+#define MICROPY_INCLUDED_ATMEL_SAMD_COMMON_HAL_PS2IO_PS2_H
 
-// Which handler should be called for a particular channel?
-static uint8_t eic_channel_handler[EIC_EXTINT_NUM];
+#include "common-hal/microcontroller/Pin.h"
 
-void set_eic_handler(uint8_t channel, uint8_t eic_handler) {
-    eic_channel_handler[channel] = eic_handler;
-}
+#include "py/obj.h"
 
-void shared_eic_handler(uint8_t channel) {
-    uint8_t handler = eic_channel_handler[channel];
-    switch (handler) {
-#if CIRCUITPY_PULSEIO
-    case EIC_HANDLER_PULSEIN:
-        pulsein_interrupt_handler(channel);
-        break;
-#endif
+typedef struct {
+    mp_obj_base_t base;
+    uint8_t channel;
+    uint8_t clk_pin;
+    uint8_t data_pin;
 
-#if CIRCUITPY_PS2IO
-    case EIC_HANDLER_PS2:
-        ps2_interrupt_handler(channel);
-        break;
-#endif
+    uint8_t state;
+    uint64_t last_int_ms;
+    uint32_t last_int_us;
 
-#if CIRCUITPY_ROTARYIO
-    case EIC_HANDLER_INCREMENTAL_ENCODER:
-        incrementalencoder_interrupt_handler(channel);
-        break;
-#endif
+    uint16_t bits;
+    bool parity;
+    uint8_t bitcount;
 
-    default:
-        break;
-    }
-}
+    uint8_t buffer[16];
+    uint8_t bufcount;
+    uint8_t bufposr;
+    uint8_t bufposw;
+
+    uint16_t last_error;
+
+    bool waiting_cmd_response;
+    uint8_t cmd_response;
+} ps2io_ps2_obj_t;
+
+void ps2_interrupt_handler(uint8_t channel);
+
+#endif // MICROPY_INCLUDED_ATMEL_SAMD_COMMON_HAL_PS2IO_PS2_H

shared-bindings/ps2io/Ps2.h
@@ -1,9 +1,10 @@
 /*
- * This file is part of the MicroPython project, http://micropython.org/
+ * This file is part of the Micro Python project, http://micropython.org/
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2019 Dan Halbert for Adafruit Industries
+ * Copyright (c) 2017 Scott Shawcroft for Adafruit Industries
+ * Copyright (c) 2019 Elvis Pfutzenreuter <epxx@epxx.co>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -23,15 +24,22 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-#ifndef MICROPY_INCLUDED_ATMEL_SAMD_EIC_HANDLER_H
-#define MICROPY_INCLUDED_ATMEL_SAMD_EIC_HANDLER_H
 
-#define EIC_HANDLER_NO_INTERRUPT 0x0
-#define EIC_HANDLER_PULSEIN 0x1
-#define EIC_HANDLER_INCREMENTAL_ENCODER 0x2
-#define EIC_HANDLER_PS2 0x3
+#ifndef MICROPY_INCLUDED_SHARED_BINDINGS_PS2IO_PS2_H
+#define MICROPY_INCLUDED_SHARED_BINDINGS_PS2IO_PS2_H
 
-void set_eic_handler(uint8_t channel, uint8_t eic_handler);
-void shared_eic_handler(uint8_t channel);
+#include "common-hal/microcontroller/Pin.h"
+#include "common-hal/ps2io/Ps2.h"
 
-#endif  // MICROPY_INCLUDED_ATMEL_SAMD_EIC_HANDLER_H
+extern const mp_obj_type_t ps2io_ps2_type;
+
+extern void common_hal_ps2io_ps2_construct(ps2io_ps2_obj_t* self,
+    const mcu_pin_obj_t* data_pin, const mcu_pin_obj_t* clk_pin);
+extern void common_hal_ps2io_ps2_deinit(ps2io_ps2_obj_t* self);
+extern bool common_hal_ps2io_ps2_deinited(ps2io_ps2_obj_t* self);
+extern uint16_t common_hal_ps2io_ps2_get_len(ps2io_ps2_obj_t* self);
+extern int16_t common_hal_ps2io_ps2_get_byte(ps2io_ps2_obj_t* self);
+extern int16_t common_hal_ps2io_ps2_send_byte(ps2io_ps2_obj_t* self, uint8_t b);
+extern uint16_t common_hal_ps2io_ps2_get_error(ps2io_ps2_obj_t* self);
+
+#endif // MICROPY_INCLUDED_SHARED_BINDINGS_PS2IO_PS2_H

micropython/micropython

ports/atmel-samd/common-hal/ps2io/Ps2.h
@@ -3,7 +3,8 @@
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2019 Dan Halbert for Adafruit Industries
+ * Copyright (c) 2017 Scott Shawcroft for Adafruit Industries
+ * Copyright (c) 2019 Elvis Pfutzenreuter <epxx@epxx.co>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -24,42 +25,38 @@
  * THE SOFTWARE.
  */
 
-#include "common-hal/pulseio/PulseIn.h"
-#include "common-hal/ps2io/Ps2.h"
-#include "common-hal/rotaryio/IncrementalEncoder.h"
-#include "shared-bindings/microcontroller/__init__.h"
-//#include "samd/external_interrupts.h"
-#include "eic_handler.h"
+#ifndef MICROPY_INCLUDED_ATMEL_SAMD_COMMON_HAL_PS2IO_PS2_H
+#define MICROPY_INCLUDED_ATMEL_SAMD_COMMON_HAL_PS2IO_PS2_H
 
-// Which handler should be called for a particular channel?
-static uint8_t eic_channel_handler[EIC_EXTINT_NUM];
+#include "common-hal/microcontroller/Pin.h"
 
-void set_eic_handler(uint8_t channel, uint8_t eic_handler) {
-    eic_channel_handler[channel] = eic_handler;
-}
+#include "py/obj.h"
 
-void shared_eic_handler(uint8_t channel) {
-    uint8_t handler = eic_channel_handler[channel];
-    switch (handler) {
-#if CIRCUITPY_PULSEIO
-    case EIC_HANDLER_PULSEIN:
-        pulsein_interrupt_handler(channel);
-        break;
-#endif
+typedef struct {
+    mp_obj_base_t base;
+    uint8_t channel;
+    uint8_t clk_pin;
+    uint8_t data_pin;
 
-#if CIRCUITPY_PS2IO
-    case EIC_HANDLER_PS2:
-        ps2_interrupt_handler(channel);
-        break;
-#endif
+    uint8_t state;
+    uint64_t last_int_ms;
+    uint32_t last_int_us;
 
-#if CIRCUITPY_ROTARYIO
-    case EIC_HANDLER_INCREMENTAL_ENCODER:
-        incrementalencoder_interrupt_handler(channel);
-        break;
-#endif
+    uint16_t bits;
+    bool parity;
+    uint8_t bitcount;
 
-    default:
-        break;
-    }
-}
+    uint8_t buffer[16];
+    uint8_t bufcount;
+    uint8_t bufposr;
+    uint8_t bufposw;
+
+    uint16_t last_errors;
+
+    bool waiting_cmd_response;
+    uint8_t cmd_response;
+} ps2io_ps2_obj_t;
+
+void ps2_interrupt_handler(uint8_t channel);
+
+#endif // MICROPY_INCLUDED_ATMEL_SAMD_COMMON_HAL_PS2IO_PS2_H

shared-bindings/ps2io/Ps2.h
@@ -1,9 +1,10 @@
 /*
- * This file is part of the MicroPython project, http://micropython.org/
+ * This file is part of the Micro Python project, http://micropython.org/
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2019 Dan Halbert for Adafruit Industries
+ * Copyright (c) 2017 Scott Shawcroft for Adafruit Industries
+ * Copyright (c) 2019 Elvis Pfutzenreuter <epxx@epxx.co>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -23,15 +24,22 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-#ifndef MICROPY_INCLUDED_ATMEL_SAMD_EIC_HANDLER_H
-#define MICROPY_INCLUDED_ATMEL_SAMD_EIC_HANDLER_H
 
-#define EIC_HANDLER_NO_INTERRUPT 0x0
-#define EIC_HANDLER_PULSEIN 0x1
-#define EIC_HANDLER_INCREMENTAL_ENCODER 0x2
-#define EIC_HANDLER_PS2 0x3
+#ifndef MICROPY_INCLUDED_SHARED_BINDINGS_PS2IO_PS2_H
+#define MICROPY_INCLUDED_SHARED_BINDINGS_PS2IO_PS2_H
 
-void set_eic_handler(uint8_t channel, uint8_t eic_handler);
-void shared_eic_handler(uint8_t channel);
+#include "common-hal/microcontroller/Pin.h"
+#include "common-hal/ps2io/Ps2.h"
 
-#endif  // MICROPY_INCLUDED_ATMEL_SAMD_EIC_HANDLER_H
+extern const mp_obj_type_t ps2io_ps2_type;
+
+extern void common_hal_ps2io_ps2_construct(ps2io_ps2_obj_t* self,
+    const mcu_pin_obj_t* data_pin, const mcu_pin_obj_t* clk_pin);
+extern void common_hal_ps2io_ps2_deinit(ps2io_ps2_obj_t* self);
+extern bool common_hal_ps2io_ps2_deinited(ps2io_ps2_obj_t* self);
+extern uint16_t common_hal_ps2io_ps2_get_len(ps2io_ps2_obj_t* self);
+extern int16_t common_hal_ps2io_ps2_popleft(ps2io_ps2_obj_t* self);
+extern int16_t common_hal_ps2io_ps2_sendcmd(ps2io_ps2_obj_t* self, uint8_t b);
+extern uint16_t common_hal_ps2io_ps2_clear_errors(ps2io_ps2_obj_t* self);
+
+#endif // MICROPY_INCLUDED_SHARED_BINDINGS_PS2IO_PS2_H