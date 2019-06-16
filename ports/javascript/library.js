4cbae1ab6b50f7940eb21d7e3e34c14cdda3a338
Themba Dube <embeddedthemba@gmail.com>
2019-06-16 12:17:47 -0400

    Add new script editor

--- ports/javascript/library.js
+++ ports/javascript/library.js
@@ -33,7 +33,7 @@ mergeInto(LibraryManager.library, {
                 process.stdout.write(b);
             } else {
                 var c = String.fromCharCode(getValue(ptr + i, 'i8'));
-                var mp_js_stdout = document.getElementById('mp_js_stdout');
+                var mp_js_stdout = window.top.document.getElementById('mp_js_stdout');
                 var print = new Event('print');
                 print.data = c;
                 mp_js_stdout.dispatchEvent(print);
