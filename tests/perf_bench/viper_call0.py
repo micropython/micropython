73fccf59672c7087d6290bb036fe5c72899086b6
Damien George <damien.p.george@gmail.com>
2019-06-26 14:27:32 +1000

    tests/perf_bench: Add some viper performance benchmarks.
    To test raw viper function call overhead: function entry, exit and
    conversion of arguments to/from objects.

---
+++ tests/perf_bench/viper_call0.py
@@ -0,0 +1,19 @@
+@micropython.viper
+def f0():
+    pass
+
+@micropython.native
+def call(r):
+    f = f0
+    for _ in r:
+        f()
+
+bm_params = {
+    (50, 10): (15000,),
+    (100, 10): (30000,),
+    (1000, 10): (300000,),
+    (5000, 10): (1500000,),
+}
+
+def bm_setup(params):
+    return lambda: call(range(params[0])), lambda: (params[0] // 1000, None)
