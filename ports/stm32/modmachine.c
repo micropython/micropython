f88cb8a51412d8570e473045adf09699664f553a
Damien George <damien.p.george@gmail.com>
2019-07-03 16:46:07 +1000

    stm32/modmachine: Make RTC class available in machine module.
    This is a start to make a more consistent machine.RTC class across ports.
    The stm32 pyb.RTC class at least has the datetime() method which behaves
    the same as esp8266 and esp32, and with this patch the ntptime.py script
    now works with stm32.

--- ports/stm32/modmachine.c
+++ ports/stm32/modmachine.c
@@ -384,8 +384,8 @@ STATIC const mp_rom_map_elem_t machine_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_Pin),                 MP_ROM_PTR(&pin_type) },
     { MP_ROM_QSTR(MP_QSTR_Signal),              MP_ROM_PTR(&machine_signal_type) },
 
-#if 0
     { MP_ROM_QSTR(MP_QSTR_RTC),                 MP_ROM_PTR(&pyb_rtc_type) },
+#if 0
     { MP_ROM_QSTR(MP_QSTR_ADC),                 MP_ROM_PTR(&pyb_adc_type) },
 #endif
 #if MICROPY_PY_MACHINE_I2C
