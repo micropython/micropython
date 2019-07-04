f63757cd45d34951d8b01dea9bfe5a9b9ed4467e
pmp-p <pmpp.pub@gmail.com>
2019-07-04 13:40:28 +0200

    clarify caching options

--- ports/javascript/wasm_file_api.c
+++ ports/javascript/wasm_file_api.c
@@ -12,14 +12,17 @@ int wasm_file_open(const char *url) {
     if (strlen(url)>1 && url[0]==':') {
         fprintf(stderr,"  -> same host[%s]\n", url);
         int fidx = EM_ASM_INT({return wasm_file_open(UTF8ToString($0)); }, url );
-        char fname[256];
+
+        // TODO rebuild local path in ramdisk relative to getcwd()
+        char fname[MICROPY_ALLOC_PATH_MAX];
+
         snprintf(fname, sizeof(fname), "cache_%d", fidx);
         return fileno( fopen(fname,"r") );
     }
     if ( (strlen(url)>6) && (url[4]==':' || url[5]==':') ) {
         fprintf(stderr,"  -> remote host[%s]\n", url);
         int fidx = EM_ASM_INT({return wasm_file_open(UTF8ToString($0)); }, url );
-        char fname[256];
+        char fname[20];
         snprintf(fname, sizeof(fname), "cache_%d", fidx);
         return fileno( fopen(fname,"r") );
     }
