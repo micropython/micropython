a17b901a9ea8f2dfe2db0822cdcd47b9c719a90a
Damien George <damien.p.george@gmail.com>
2019-07-09 12:54:09 +1000

    stm32/boards/B_L072Z_LRWAN1: Add definition files for new board.

---
+++ ports/stm32/boards/B_L072Z_LRWAN1/mpconfigboard.mk
@@ -0,0 +1,7 @@
+MCU_SERIES = l0
+CMSIS_MCU = STM32L072xx
+AF_FILE = boards/stm32l072_af.csv
+LD_FILES = boards/stm32l072xz.ld boards/common_basic.ld
+
+# Don't include default frozen modules because MCU is tight on flash space
+FROZEN_MPY_DIR ?=
