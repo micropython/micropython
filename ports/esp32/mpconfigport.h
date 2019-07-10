a29334761dc7b8eaf9f78611e3cd9033928b110c
Damien George <damien.p.george@gmail.com>
2019-07-10 13:43:52 +1000

    esp32: Add support for hardware I2C.

--- ports/esp32/mpconfigport.h
+++ ports/esp32/mpconfigport.h
@@ -137,6 +137,7 @@
 #define MICROPY_PY_MACHINE_PIN_MAKE_NEW     mp_pin_make_new
 #define MICROPY_PY_MACHINE_PULSE            (1)
 #define MICROPY_PY_MACHINE_I2C              (1)
+#define MICROPY_PY_MACHINE_I2C_MAKE_NEW     machine_hw_i2c_make_new
 #define MICROPY_PY_MACHINE_SPI              (1)
 #define MICROPY_PY_MACHINE_SPI_MSB          (0)
 #define MICROPY_PY_MACHINE_SPI_LSB          (1)
