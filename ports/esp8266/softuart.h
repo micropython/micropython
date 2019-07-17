d90a4974392dad32b070d487f106df0465d48957
Damien Mascord <damien.mascord@sita.aero>
2019-07-17 14:37:59 +1000

    esp8266/softuart.c: Integrated the latest code from plieningerweb.
    Link: https://github.com/plieningerweb/esp8266-software-uart.

--- ports/esp8266/softuart.h
+++ ports/esp8266/softuart.h
@@ -1,56 +1,34 @@
-/*
-The MIT License (MIT)
-
-Copyright (c) 2015 plieningerweb
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
-
 #ifndef SOFTUART_H_
 #define SOFTUART_H_
 
-#define SOFTUART_MAX_RX_BUFF 64
+#include "user_interface.h"
+
+#define SOFTUART_MAX_RX_BUFF 64 
 
 #define SOFTUART_GPIO_COUNT 16
 
 typedef struct softuart_pin_t {
-  uint8_t gpio_id;
-  uint32_t gpio_mux_name;
-  uint8_t gpio_func;
+	uint8_t gpio_id;
+	uint32_t gpio_mux_name;
+	uint8_t gpio_func;
 } softuart_pin_t;
 
 typedef struct softuart_buffer_t {
-  char receive_buffer[SOFTUART_MAX_RX_BUFF];
-  uint8_t receive_buffer_tail;
-  uint8_t receive_buffer_head;
-  uint8_t buffer_overflow;
+	char receive_buffer[SOFTUART_MAX_RX_BUFF]; 
+	uint8_t receive_buffer_tail;
+	uint8_t receive_buffer_head;
+	uint8_t buffer_overflow; 
 } softuart_buffer_t;
 
 typedef struct {
-  softuart_pin_t pin_rx;
-  softuart_pin_t pin_tx;
-  //optional rs485 tx enable pin (high -> tx enabled)
-  uint8_t pin_rs485_tx_enable;
-  //wether or not this softuart is rs485 and controlls rs485 tx enable pin
-  uint8_t is_rs485;
-  volatile softuart_buffer_t buffer;
-  uint16_t bit_time;
+	softuart_pin_t pin_rx;
+	softuart_pin_t pin_tx;
+	//optional rs485 tx enable pin (high -> tx enabled)
+	uint8_t pin_rs485_tx_enable;
+	//wether or not this softuart is rs485 and controlls rs485 tx enable pin
+	uint8_t is_rs485;
+	volatile softuart_buffer_t buffer;
+	uint16_t bit_time;
 } Softuart;
 
 
@@ -69,8 +47,8 @@ uint8_t Softuart_Readline(Softuart *s, char* Buffer, uint8_t MaxLen );
 
 //define mapping from pin to functio mode
 typedef struct {
-  uint32_t gpio_mux_name;
-  uint8_t gpio_func;
+	uint32_t gpio_mux_name;
+	uint8_t gpio_func;
 } softuart_reg_t;
 
 #endif /* SOFTUART_H_ */
