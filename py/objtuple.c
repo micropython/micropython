a6b8ec4fc44cb4cf352c9739b4c21d093a2d0471
Jeff Epler <jepler@gmail.com>
2019-08-19 07:22:41 -0500

    objtuple: respond to review comments
     - is_tuple_compatible: Remove mp_obj_is_obj from check, to save code size
    
     - rename it so a future developer will not be deceived that it follows
       the general pattern of mp_obj_is_TYPE to check mp_obj_is_obj
    
     - Change both sites to assert, rather than mp_check_self, as they are
       internal APIs and user facing type checks should have been performed
       previously
    
    This last is a potential behavioral change; in v1.9.4, tuple_cmp_helper
    did use mp_check_self.  However, the same "is internal API" argument
    appears to apply, since its caller mp_obj_tuple_binary_op has already
    done `mp_obj_tuple_t *o = MP_OBJ_TO_PTR(lhs);` unchecked

--- py/objtuple.c
+++ py/objtuple.c
@@ -32,7 +32,8 @@
 #include "py/runtime.h"
 
 // type check is done on getiter method to allow tuple, namedtuple, attrtuple
-#define mp_obj_is_tuple_compatible(o) (mp_obj_is_obj(o) && ((mp_obj_base_t*)MP_OBJ_TO_PTR(o))->type->getiter == mp_obj_tuple_getiter)
+// Assumes mp_obj_is_obj has already been checked
+#define _mp_obj_is_tuple_compatible(o) ((mp_obj_base_t*)MP_OBJ_TO_PTR(o))->type->getiter == mp_obj_tuple_getiter
 
 /******************************************************************************/
 /* tuple                                                                      */
@@ -105,7 +106,7 @@ STATIC mp_obj_t mp_obj_tuple_make_new(const mp_obj_type_t *type_in, size_t n_arg
 // Don't pass MP_BINARY_OP_NOT_EQUAL here
 STATIC mp_obj_t tuple_cmp_helper(mp_uint_t op, mp_obj_t self_in, mp_obj_t another_in) {
     // type check is done on getiter method to allow tuple, namedtuple, attrtuple
-    mp_check_self(mp_obj_is_tuple_compatible(self_in));
+    assert(_mp_obj_is_tuple_compatible(self_in));
     mp_obj_type_t *another_type = mp_obj_get_type(another_in);
     mp_obj_tuple_t *self = MP_OBJ_TO_PTR(self_in);
     if (another_type->getiter != mp_obj_tuple_getiter) {
@@ -252,7 +253,7 @@ mp_obj_t mp_obj_new_tuple(size_t n, const mp_obj_t *items) {
 }
 
 void mp_obj_tuple_get(mp_obj_t self_in, size_t *len, mp_obj_t **items) {
-    mp_check_self(mp_obj_is_tuple_compatible(self_in));
+    assert(_mp_obj_is_tuple_compatible(self_in));
     mp_obj_tuple_t *self = MP_OBJ_TO_PTR(self_in);
     *len = self->len;
     *items = &self->items[0];
