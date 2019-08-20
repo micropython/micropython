cd292d8bed311f5e9ee17320372ea4e8a5487796
Dev Aggarwal <devxpy@gmail.com>
2019-08-21 05:05:16 +0530

    fix docker image cache

--- travis/restore-docker-img.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -x
-
-KEY=docker-image-$IMG_TAG
-cache has_key $KEY || exit 0
-cache restore $KEY
-docker load -i $IMG_TAR
--- travis/store-docker-img.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -x
-
-docker save -o $IMG_TAR $IMG_TAG
-cache store docker-image-$IMG_TAR img.tar
