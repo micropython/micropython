344c30b3abe0e6b561817aaafd34a9e6380f6ddc
Dev Aggarwal <devxpy@gmail.com>
2019-08-21 13:11:46 +0530

    fix docker build

--- ci-scripts/esp32.sh
+++ ci-scripts/esp32.sh
@@ -4,4 +4,4 @@ set -x
 
 git submodule update --init lib/berkeley-db-1.xx
 make ${MAKEOPTS} -C mpy-cross
-make ${MAKEOPTS} -C ports/esp32 ESPIDF=$HOME/esp-idf
+make ${MAKEOPTS} -C ports/esp32 ESPIDF=$IDF_PATH
