28cc51c562b12e1b6cda91f0c58bd23ebc430a1d
Dev Aggarwal <devxpy@gmail.com>
2019-08-22 19:39:53 +0530

    travis

--- ci-scripts/build-all.sh
+++ ci-scripts/build-all.sh
@@ -1,4 +1,4 @@
 for PORT in stm32 qemu-arm unix-coverage unix-standard unix-nanobox unix-stackless windows esp32 esp8266 nrf bare-arm cc3200 samd teensy
 do
-  docker run -v $PWD:$PWD -w $PWD $IMAGE ci-scripts/$PORT.sh
+  docker run -v $PWD:$PWD -w $PWD $IMAGE ci-scripts/$PORT.sh > logs || cat logs
 done
