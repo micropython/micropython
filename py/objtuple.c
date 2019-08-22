bae9c2486b1739406d68c971ca7547e3d59f7e5c
Jeff Epler <jepler@gmail.com>
2019-08-21 19:04:38 -0500

    objtuple: remove redundant comment

--- py/objtuple.c
+++ py/objtuple.c
@@ -104,7 +104,6 @@ STATIC mp_obj_t mp_obj_tuple_make_new(const mp_obj_type_t *type_in, size_t n_arg
 
 // Don't pass MP_BINARY_OP_NOT_EQUAL here
 STATIC mp_obj_t tuple_cmp_helper(mp_uint_t op, mp_obj_t self_in, mp_obj_t another_in) {
-    // type check is done on getiter method to allow tuple, namedtuple, attrtuple
     mp_check_self(mp_obj_is_tuple_compatible(self_in));
     mp_obj_type_t *another_type = mp_obj_get_type(another_in);
     mp_obj_tuple_t *self = MP_OBJ_TO_PTR(self_in);
