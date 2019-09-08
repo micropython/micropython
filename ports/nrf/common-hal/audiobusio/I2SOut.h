f38ee42874e4581080cd3bf808b4ab5e32e4807d
jepler <jepler@gmail.com>
2019-09-08 13:08:15 -0500

    nrf: Add i2s audio output
    Testing performed: I used a Particle Xenon with a HDA1334 I2S DAC.
    I played a variety of mono 16-bit samples at 11025 and 22050Hz nominal
    bit rates.  With this setup, all the 11025Hz samples sound good.
    I tested play, pause,  and loop functionality.
    
    During some runs with 22050Hz samples, there were glitches.  However,
    these may have only occurred during runs where I had set breakpoints
    and watchpoints in gdb.
    
    I also tested with a MAX98357A I2S amplifier.  On this device, everything
    sounded "scratchy".  I was powering it from 5V and the 5V rail seemed
    steady, so I don't have an explanation for this.  However, I haven't
    tried it with a SAMD board.

--- ports/nrf/common-hal/audiobusio/I2SOut.h
+++ ports/nrf/common-hal/audiobusio/I2SOut.h
@@ -31,6 +31,33 @@
 
 typedef struct {
     mp_obj_base_t base;
+
+    mp_obj_t *sample;
+    uint8_t *buffers[2];
+    uint8_t *sample_data, *sample_end;
+
+    uint16_t buffer_length;
+    uint16_t sample_rate;
+    uint32_t hold_value;
+
+    uint8_t next_buffer;
+    uint8_t bit_clock_pin_number;
+    uint8_t word_select_pin_number;
+    uint8_t data_pin_number;
+
+    uint8_t channel_count;
+    uint8_t bytes_per_sample;
+
+    bool left_justified : 1;
+    bool playing : 1;
+    bool stopping : 1;
+    bool paused : 1;
+    bool loop : 1;
+    bool samples_signed : 1;
+    bool single_buffer : 1;
 } audiobusio_i2sout_obj_t;
+
+void i2s_reset(void);
+void i2s_background(void);
 
 #endif // MICROPY_INCLUDED_NRF_COMMON_HAL_AUDIOBUSIO_I2SOUT_H
