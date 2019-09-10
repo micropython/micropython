41c61ba0c500fef32a21220dc27dcb5092d750cb
Christopher Wilson <christopher.david.wilson@gmail.com>
2019-09-09 20:17:23 -0700

    Use MICROPY_PY_SYS_PLATFORM default value ("pyboard")

--- ports/stm32/boards/MIKROE_CLICKER2_STM32/mpconfigboard.h
+++ ports/stm32/boards/MIKROE_CLICKER2_STM32/mpconfigboard.h
@@ -1,6 +1,5 @@
 #define MICROPY_HW_BOARD_NAME       "MIKROE_CLICKER2_STM32"
 #define MICROPY_HW_MCU_NAME         "STM32F407"
-#define MICROPY_PY_SYS_PLATFORM     "Mikroe Clicker 2 for STM32"
 
 #define MICROPY_HW_HAS_SWITCH       (1)
 #define MICROPY_HW_HAS_FLASH        (1)
