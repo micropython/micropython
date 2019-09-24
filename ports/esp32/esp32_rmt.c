2d9f0bf892fc3f610e39b3ec96aa5452eb3e6163
Matt Trentini <matt.trentini@gmail.com>
2019-09-24 13:55:39 +1000

    Add TODO, complete config init, add deinit method, tidy up resolution calculation.

--- ports/esp32/esp32_rmt.c
+++ ports/esp32/esp32_rmt.c
@@ -3,7 +3,7 @@
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2018 "Matt Trentini" <matt.trentini@gmail.com>
+ * Copyright (c) 2019 "Matt Trentini" <matt.trentini@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -42,6 +42,30 @@ typedef struct _esp32_rmt_obj_t {
     uint8_t clock_divider;
 } esp32_rmt_obj_t;
 
+
+// TODO
+//
+//   o Check all the input parameters
+//   o Return exceptions when errors occur
+//   o Manage an array of channels (so user can't create the same one repeatedly)
+//     - Check if channel is initialised (in particular: after deinit)
+//   o Investigate: Send without blocking? (Need an 'is_sending' method)
+//   o Add debug option? Emit printf debug messages?
+//     - Idea: Generate timing diagram (maybe with https://wavedrom.com/ ?) Extension.
+//   o Consider: Auto-manage RMT channels? Currently user chooses a channel
+//     - Alternative: Give the user the next available channel
+//   o Raise exception if pulses is not a tuple
+//     - Consider: Using iterables. Will make memory management much trickier though
+//     - Consider: Allowing lists as well as tuples
+//   o Do we need to allow the option to use multiple memory blocks?
+//   o Consider supporting other ways to specify pulses:
+//     - Fixed interval (specify pattern of 1's/0's)
+//     - Specify both value and ticks
+//     - See PyCom RMT docs
+//   o Add carrier modulation methods
+//   o Add rx support
+
+
 STATIC mp_obj_t esp32_rmt_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args) {
     // fixme: check arguments
     // 1 <= channel_id <= 8
@@ -60,18 +84,17 @@ STATIC mp_obj_t esp32_rmt_make_new(const mp_obj_type_t *type, size_t n_args, siz
 
     rmt_config_t config;
     config.rmt_mode = RMT_MODE_TX;
-    config.channel = self->channel_id; // Technically should use enum eg RMT_CHANNEL_0
+    config.channel = (rmt_channel_t) self->channel_id;
     config.gpio_num = self->pin;
     config.mem_block_num = 1;
     config.tx_config.loop_en = 0;
 
-    // Shouldn't be necessary to set these (until we use them...)
-    // config.tx_config.carrier_en = 0;
-    // config.tx_config.idle_output_en = 0;
-    // config.tx_config.idle_level = 0;
-    // config.tx_config.carrier_duty_percent = 0;
-    // config.tx_config.carrier_freq_hz = 0;
-    // config.tx_config.carrier_level = 1;
+    config.tx_config.carrier_en = 0;
+    config.tx_config.idle_output_en = 0;
+    config.tx_config.idle_level = 0;
+    config.tx_config.carrier_duty_percent = 0;
+    config.tx_config.carrier_freq_hz = 0;
+    config.tx_config.carrier_level = 1;
 
     config.clk_div = self->clock_divider;
 
@@ -89,10 +112,24 @@ STATIC void esp32_rmt_print(const mp_print_t *print, mp_obj_t self_in, mp_print_
 }
 
 
+STATIC mp_obj_t esp32_rmt_deinit(mp_obj_t self_in) {
+    // fixme: check for valid channel. Return exception if error occurs.
+    esp32_rmt_obj_t *self = MP_OBJ_TO_PTR(self_in);
+    rmt_driver_uninstall(self->channel_id);
+
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(esp32_rmt_deinit_obj, esp32_rmt_deinit);
+
+static float resolution_for_divider(uint8_t clock_divider)
+{
+    return 1.0 / (APB_CLK_FREQ / clock_divider);
+}
+
 // Return the resolution (shortest possible pulse), in seconds
 STATIC mp_obj_t esp32_rmt_resolution(mp_obj_t self_in) {
     esp32_rmt_obj_t *self = MP_OBJ_TO_PTR(self_in);
-    return mp_obj_new_float(1.0/(APB_CLK_FREQ / self->clock_divider));
+    return mp_obj_new_float(resolution_for_divider(self->clock_divider));
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(esp32_rmt_resolution_obj, esp32_rmt_resolution);
 
@@ -100,7 +137,7 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_1(esp32_rmt_resolution_obj, esp32_rmt_resolution)
 // Return the longest pulse duration
 STATIC mp_obj_t esp32_rmt_max_duration(mp_obj_t self_in) {
     esp32_rmt_obj_t *self = MP_OBJ_TO_PTR(self_in);
-    return mp_obj_new_float((1.0/(APB_CLK_FREQ / self->clock_divider)) * 32768);
+    return mp_obj_new_float(resolution_for_divider(self->clock_divider) * 32768); // 2^15 is the maximum number of bits for ticks
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(esp32_rmt_max_duration_obj, esp32_rmt_max_duration);
 
@@ -122,19 +159,19 @@ STATIC mp_obj_t esp32_rmt_send_pulses(mp_obj_t self_in, mp_obj_t pulses, mp_obj_
     mp_uint_t num_items = (pulses_length / 2) + (pulses_length % 2);
     rmt_item32_t* items = (rmt_item32_t*)m_malloc(num_items * sizeof(rmt_item32_t));
 
-    printf("num_items=%d pulses_length=%d\n\n", num_items, pulses_length);
+    //printf("num_items=%d pulses_length=%d\n\n", num_items, pulses_length);
 
     for (mp_uint_t item_index = 0; item_index < num_items; item_index++)
     {
         mp_uint_t pulse_index = item_index * 2;
         items[item_index].duration0 = mp_obj_get_int(pulses_ptr[pulse_index++]);
         items[item_index].level0 = level++; // Note that level _could_ wrap.
-        printf("duration=%d level=%d\n", items[item_index].duration0, items[item_index].level0);
+        //printf("duration=%d level=%d\n", items[item_index].duration0, items[item_index].level0);
         if (pulse_index < pulses_length)
         {
             items[item_index].duration1 = mp_obj_get_int(pulses_ptr[pulse_index]);
             items[item_index].level1 = level++;
-            printf("duration=%d level=%d\n", items[item_index].duration1, items[item_index].level1);
+            //printf("duration=%d level=%d\n", items[item_index].duration1, items[item_index].level1);
         }
     }
     ESP_ERROR_CHECK(rmt_write_items(self->channel_id, items, num_items, true));
@@ -152,6 +189,7 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_3(esp32_rmt_send_pulses_obj, esp32_rmt_send_pulse
 
 STATIC const mp_rom_map_elem_t esp32_rmt_locals_dict_table[] = {
     { MP_ROM_QSTR(MP_QSTR_send_pulses), MP_ROM_PTR(&esp32_rmt_send_pulses_obj) },
+    { MP_ROM_QSTR(MP_QSTR_deinit), MP_ROM_PTR(&esp32_rmt_deinit_obj) },
     { MP_ROM_QSTR(MP_QSTR_resolution), MP_ROM_PTR(&esp32_rmt_resolution_obj) },
     { MP_ROM_QSTR(MP_QSTR_max_duration), MP_ROM_PTR(&esp32_rmt_max_duration_obj) },
 };
@@ -164,6 +202,3 @@ const mp_obj_type_t esp32_rmt_type = {
     .make_new = esp32_rmt_make_new,
     .locals_dict = (mp_obj_dict_t *)&esp32_rmt_locals_dict,
 };
-
-// ch1 = RMT(1, Pin(18), 80, 0) # channel, pin, clock_divider, idle_level
-// ch1.send_items((1000, 500, 1000), start=0)
