b7e8a031d0ec360e9ebd1d533d9b40ee25c389b6
wallarug <wallarug@coliemore.com.au>
2019-09-26 19:19:54 +1000

    remove frozen busio library

--- ports/atmel-samd/boards/robohatmm1_m4/mpconfigboard.mk
+++ ports/atmel-samd/boards/robohatmm1_m4/mpconfigboard.mk
@@ -22,7 +22,7 @@ CIRCUITPY_DISPLAYIO = 0
 CIRCUITPY_FREQUENCYIO = 0
 
 # Include these Python libraries in firmware.
-FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_BusDevice
+#FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_BusDevice
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_NeoPixel
 #FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_INA219
 #FROZEN_MPY_DIRS += $(TOP)/frozen/RoboticsMasters_CircuitPython_MPU9250
