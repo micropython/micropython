64d9785052b1247899df51f19c10f09dd22ac7fa
Damien George <damien.p.george@gmail.com>
2019-10-16 17:24:19 +1100

    py/stream.h: Add MP_STREAM_POLL_NVAL constant.

--- py/stream.h
+++ py/stream.h
@@ -45,10 +45,11 @@
 #define MP_STREAM_GET_FILENO    (10) // Get fileno of underlying file
 
 // These poll ioctl values are compatible with Linux
-#define MP_STREAM_POLL_RD  (0x0001)
-#define MP_STREAM_POLL_WR  (0x0004)
-#define MP_STREAM_POLL_ERR (0x0008)
-#define MP_STREAM_POLL_HUP (0x0010)
+#define MP_STREAM_POLL_RD       (0x0001)
+#define MP_STREAM_POLL_WR       (0x0004)
+#define MP_STREAM_POLL_ERR      (0x0008)
+#define MP_STREAM_POLL_HUP      (0x0010)
+#define MP_STREAM_POLL_NVAL     (0x0020)
 
 // Argument structure for MP_STREAM_SEEK
 struct mp_stream_seek_t {
