e2cb29f2a0ca07c8b8b089675969e11d62cd13ac
Kamil Tomaszewski <kamil.tomaszewski@sony.com>
2019-10-18 11:12:47 +0200

    Change default stack size to 64kiB for Spresense

--- ports/cxd56/mpconfigport.h
+++ ports/cxd56/mpconfigport.h
@@ -27,8 +27,8 @@
 #ifndef __INCLUDED_MPCONFIGPORT_H
 #define __INCLUDED_MPCONFIGPORT_H
 
-// 24kiB stack
-#define CIRCUITPY_DEFAULT_STACK_SIZE            0x6000
+// 64kiB stack
+#define CIRCUITPY_DEFAULT_STACK_SIZE            0x10000
 
 #include "py/circuitpy_mpconfig.h"
 
