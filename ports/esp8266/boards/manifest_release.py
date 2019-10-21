f1d91908fa427cb419d6fcd5f64f6c581a0cdd0f
Damien George <damien.p.george@gmail.com>
2019-10-22 00:23:22 +1100

    esp8266/boards: Add manifest_release.py with files for a release.
    A release also sets: UART_OS = -1

---
+++ ports/esp8266/boards/manifest_release.py
@@ -0,0 +1,27 @@
+include('manifest.py')
+
+# drivers
+freeze('$(MPY_DIR)/drivers/display', 'ssd1306.py')
+
+# file utilities
+freeze('$(MPY_LIB_DIR)/upysh', 'upysh.py')
+
+# uasyncio
+freeze('$(MPY_LIB_DIR)/uasyncio', 'uasyncio/__init__.py')
+freeze('$(MPY_LIB_DIR)/uasyncio.core', 'uasyncio/core.py')
+
+# requests
+freeze('$(MPY_LIB_DIR)/urequests', 'urequests.py')
+freeze('$(MPY_LIB_DIR)/urllib.urequest', 'urllib/urequest.py')
+
+# umqtt with examples
+freeze('$(MPY_LIB_DIR)/umqtt.simple', 'umqtt/simple.py')
+freeze('$(MPY_LIB_DIR)/umqtt.robust', 'umqtt/robust.py')
+freeze('$(MPY_LIB_DIR)/umqtt.simple', 'example_pub_button.py')
+freeze('$(MPY_LIB_DIR)/umqtt.simple', 'example_sub_led.py')
+
+# HTTP examples
+freeze('$(MPY_DIR)/examples/network', 'http_client.py')
+freeze('$(MPY_DIR)/examples/network', 'http_client_ssl.py')
+freeze('$(MPY_DIR)/examples/network', 'http_server.py')
+freeze('$(MPY_DIR)/examples/network', 'http_server_ssl.py')
