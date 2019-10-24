0d82f5d8c868d7f4e93d48e400cd4d769175e3e5
Seon Rozenblum <seonr@3sprockets.com>
2019-10-24 19:20:39 +1100

    esp32/boards/TINYPICO: Add tinypico.py, dotstar.py with custom manifest.

--- ports/esp32/boards/TINYPICO/mpconfigboard.mk
+++ ports/esp32/boards/TINYPICO/mpconfigboard.mk
@@ -4,3 +4,5 @@ SDKCONFIG += boards/sdkconfig.base
 SDKCONFIG += boards/sdkconfig.240mhz
 SDKCONFIG += boards/sdkconfig.spiram
 SDKCONFIG += boards/TINYPICO/sdkconfig.board
+
+FROZEN_MANIFEST ?= $(BOARD_DIR)/manifest.py
