40f920fbdfbeaf5c698aa1348ed559731dc4eb26
Hierophect <hierophect@gmail.com>
2019-11-04 14:39:41 -0500

    complete attribution edits

--- ports/stm32f4/boards/stm32f412zg_discovery/mpconfigboard.h
+++ ports/stm32f4/boards/stm32f412zg_discovery/mpconfigboard.h
@@ -3,8 +3,7 @@
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2016 Glenn Ruben Bakke
- * Copyright (c) 2018 Dan Halbert for Adafruit Industries
+ * Copyright (c) 2019 Lucian Copeland for Adafruit Industries
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
