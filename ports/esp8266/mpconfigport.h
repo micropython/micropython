cea9209e0f16d0849426f418316eeafbc91f60dc
Damien George <damien.p.george@gmail.com>
2019-11-07 18:44:16 +1100

    esp8266/moduos: Add optional support for VfsLfs1 and VfsLfs2.
    With this commit an esp8266-based board can now be built with littlefs
    support via, eg "make MICROPY_VFS_LFS2=1".

--- ports/esp8266/mpconfigport.h
+++ ports/esp8266/mpconfigport.h
@@ -137,8 +137,16 @@ void *esp_native_code_commit(void*, size_t);
 // printer for debugging output, goes to UART only
 extern const struct _mp_print_t mp_debug_print;
 
+#if MICROPY_VFS_FAT
 #define mp_type_fileio mp_type_vfs_fat_fileio
 #define mp_type_textio mp_type_vfs_fat_textio
+#elif MICROPY_VFS_LFS1
+#define mp_type_fileio mp_type_vfs_lfs1_fileio
+#define mp_type_textio mp_type_vfs_lfs1_textio
+#elif MICROPY_VFS_LFS2
+#define mp_type_fileio mp_type_vfs_lfs2_fileio
+#define mp_type_textio mp_type_vfs_lfs2_textio
+#endif
 
 // use vfs's functions for import stat and builtin open
 #define mp_import_stat mp_vfs_import_stat
