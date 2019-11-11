4f9ca198635bff1338bff464dbae02309d06100d
Yonatan Goldschmidt <yon.goldschmidt@gmail.com>
2019-11-11 15:09:25 +0200

    qemu-arm: Enable MICROPY_SMALL_DATA_SECTION.
    Store often accessed objects in the lower address space area,
    so 16-bit instructions can be used to access their address
    when loading / comparing them.
    
    Saves 1048 bytes when bulding the netduino2 board.

---
+++ ports/qemu-arm/mpconfigport.mk
@@ -0,0 +1,8 @@
+# address of mp_const_none_obj in the small data section
+MP_SMALL_DATA_CONST_NONE = 0x40
+
+# address of mp_const_false_obj
+MP_SMALL_DATA_CONST_FALSE = 0x44
+
+# address of mp_const_true_obj
+MP_SMALL_DATA_CONST_TRUE = 0x4c
