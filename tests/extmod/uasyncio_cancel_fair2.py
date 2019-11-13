c4935f30490d0446e16a51dbf7a6397b771cf804
Damien George <damien.p.george@gmail.com>
2019-11-13 21:08:22 +1100

    tests/extmod: Add uasyncio tests.
    All .exp files are included because they require CPython 3.8 which may not
    always be available.

---
+++ tests/extmod/uasyncio_cancel_fair2.py
@@ -0,0 +1,37 @@
+# Test fairness of cancelling a task
+# That tasks which keeps being cancelled by multiple other tasks gets a chance to run
+
+try:
+    import uasyncio as asyncio
+except ImportError:
+    try:
+        import asyncio
+    except ImportError:
+        print("SKIP")
+        raise SystemExit
+
+
+async def task_a():
+    try:
+        while True:
+            print("sleep a")
+            await asyncio.sleep(0)
+    except asyncio.CancelledError:
+        print("cancelled a")
+
+
+async def task_b(id, other):
+    while other.cancel():
+        print("sleep b", id)
+        await asyncio.sleep(0)
+    print("done b", id)
+
+
+async def main():
+    t = asyncio.create_task(task_a())
+    for i in range(3):
+        asyncio.create_task(task_b(i, t))
+    await t
+
+
+asyncio.run(main())
