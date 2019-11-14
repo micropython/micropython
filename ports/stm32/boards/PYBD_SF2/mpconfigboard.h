120368ba1ab444b2f1c17d1eb69bc6f09072ec5d
Damien George <damien.p.george@gmail.com>
2019-11-14 16:36:05 +1100

    stm32/boards: Enable LFS2 on PYBv1.x and PYBD boards.

--- ports/stm32/boards/PYBD_SF2/mpconfigboard.h
+++ ports/stm32/boards/PYBD_SF2/mpconfigboard.h
@@ -84,6 +84,7 @@ extern struct _spi_bdev_t spi_bdev;
 )
 #define MICROPY_HW_BDEV_READBLOCKS(dest, bl, n) spi_bdev_readblocks(&spi_bdev, (dest), (bl), (n))
 #define MICROPY_HW_BDEV_WRITEBLOCKS(src, bl, n) spi_bdev_writeblocks(&spi_bdev, (src), (bl), (n))
+#define MICROPY_HW_BDEV_SPIFLASH_EXTENDED (&spi_bdev) // for extended block protocol
 
 // SPI flash #2, to be memory mapped
 #define MICROPY_HW_QSPIFLASH_SIZE_BITS_LOG2 (24)
