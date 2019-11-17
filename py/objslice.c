4cecb447be6e17b121991630847c5b10b365400a
Nicko van Someren <nicko@nicko.org>
2019-11-16 17:07:11 -0700

    Added support for indices() method on slice objects.

--- py/objslice.c
+++ py/objslice.c
@@ -28,6 +28,8 @@
 #include <assert.h>
 
 #include "py/obj.h"
+#include "py/runtime.h"
+#include "py/misc.h"
 
 /******************************************************************************/
 /* slice object                                                               */
@@ -53,6 +55,75 @@ STATIC void slice_print(const mp_print_t *print, mp_obj_t o_in, mp_print_kind_t 
     mp_print_str(print, ")");
 }
 
+#if MICROPY_PY_BUILTINS_SLICE_INDICES
+STATIC mp_obj_t slice_indices(mp_obj_t self_in, mp_obj_t length_obj) {
+    mp_obj_slice_t *self = MP_OBJ_TO_PTR(self_in);
+    mp_int_t length = mp_obj_int_get_checked(length_obj);
+    mp_int_t start, stop, step;
+    mp_obj_t results[3]; // In start, stop, step order
+
+    if (self->step == mp_const_none) {
+        step = 1;
+    } else {
+        step = mp_obj_int_get_checked(self->step);
+        if (step == 0) {
+            mp_raise_ValueError("slice step cannot be zero");
+        }
+    }
+
+    if (step > 0) {
+        // Positive step
+        if (self->start == mp_const_none) {
+            start = 0;
+        } else {
+            start = mp_obj_int_get_checked(self->start);
+            if (start < 0) {
+                start += length;
+            }
+            start = MIN(length, MAX(start, 0));
+        }
+
+        if (self->stop == mp_const_none) {
+            stop = length;
+        } else {
+            stop = mp_obj_int_get_checked(self->stop);
+            if (stop < 0) {
+                stop += length;
+            }
+            stop = MIN(length, MAX(stop, 0));
+        }
+    } else {
+        // Negative step
+        if (self->start == mp_const_none) {
+            start = length - 1;
+        } else {
+            start = mp_obj_int_get_checked(self->start);
+            if (start < 0) {
+                start += length;
+            }
+            start = MIN(length, MAX(start, 0));
+        }
+
+        if (self->stop == mp_const_none) {
+            stop = -1;
+        } else {
+            stop = mp_obj_int_get_checked(self->stop);
+            if (stop < 0) {
+                stop += length;
+            }
+            stop = MIN(length-1, MAX(stop, -1));
+        }
+    }
+
+    results[0] = MP_OBJ_NEW_SMALL_INT(start);
+    results[1] = MP_OBJ_NEW_SMALL_INT(stop);
+    results[2] = MP_OBJ_NEW_SMALL_INT(step);
+
+    return mp_obj_new_tuple(3, results);
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_2(slice_indices_obj, slice_indices);
+#endif
+
 #if MICROPY_PY_BUILTINS_SLICE_ATTRS
 STATIC void slice_attr(mp_obj_t self_in, qstr attr, mp_obj_t *dest) {
     if (dest[0] != MP_OBJ_NULL) {
@@ -60,14 +131,29 @@ STATIC void slice_attr(mp_obj_t self_in, qstr attr, mp_obj_t *dest) {
         return;
     }
     mp_obj_slice_t *self = MP_OBJ_TO_PTR(self_in);
+
     if (attr == MP_QSTR_start) {
         dest[0] = self->start;
     } else if (attr == MP_QSTR_stop) {
         dest[0] = self->stop;
     } else if (attr == MP_QSTR_step) {
         dest[0] = self->step;
+#if MICROPY_PY_BUILTINS_SLICE_INDICES
+    } else if (attr == MP_QSTR_indices) {
+        dest[0] = mp_obj_new_bound_meth(MP_OBJ_FROM_PTR(&slice_indices_obj),
+                                        self_in);
+#endif
     }
 }
+#endif
+
+#if MICROPY_PY_BUILTINS_SLICE_INDICES && !MICROPY_PY_BUILTINS_SLICE_ATTRS
+STATIC const mp_rom_map_elem_t slice_locals_dict_table[] = {
+    { MP_ROM_QSTR(MP_QSTR_indices), MP_ROM_PTR(&slice_indices_obj) },
+    { MP_ROM_QSTR(MP_QSTR_wombat), MP_ROM_PTR(&slice_indices_obj) },
+};
+
+STATIC MP_DEFINE_CONST_DICT(slice_locals_dict, slice_locals_dict_table);
 #endif
 
 const mp_obj_type_t mp_type_slice = {
@@ -76,6 +162,8 @@ const mp_obj_type_t mp_type_slice = {
     .print = slice_print,
 #if MICROPY_PY_BUILTINS_SLICE_ATTRS
     .attr = slice_attr,
+#elif MICROPY_PY_BUILTINS_SLICE_INDICES
+    .locals_dict = (mp_obj_dict_t*)&slice_locals_dict,
 #endif
 };
 
@@ -85,6 +173,7 @@ mp_obj_t mp_obj_new_slice(mp_obj_t ostart, mp_obj_t ostop, mp_obj_t ostep) {
     o->start = ostart;
     o->stop = ostop;
     o->step = ostep;
+
     return MP_OBJ_FROM_PTR(o);
 }
 
