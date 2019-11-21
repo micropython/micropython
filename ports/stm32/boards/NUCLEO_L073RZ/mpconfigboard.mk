bf077e7d594f91479f63b13b095fc41dea4f4256
Yonatan Goldschmidt <yon.goldschmidt@gmail.com>
2019-11-21 14:54:16 +0200

    stm32/boards/NUCLEO_L073RZ: Enable MICROPY_SMALL_DATA_SECTION.

--- ports/stm32/boards/NUCLEO_L073RZ/mpconfigboard.mk
+++ ports/stm32/boards/NUCLEO_L073RZ/mpconfigboard.mk
@@ -5,6 +5,11 @@ LD_FILES = boards/stm32l072xz.ld boards/common_basic.ld
 
 # MicroPython settings
 MICROPY_VFS_FAT = 0
+MICROPY_SMALL_DATA_SECTION = 1
+MP_SMALL_DATA_CONST_NONE = 0xc0
+MP_SMALL_DATA_CONST_FALSE = 0xc4
+MP_SMALL_DATA_CONST_TRUE = 0xcc
+MP_SMALL_DATA_TYPE_TYPE = 0xd4
 
 # Don't include default frozen modules because MCU is tight on flash space
 FROZEN_MANIFEST ?=
