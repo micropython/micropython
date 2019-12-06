d5eca87ca23e9288b31c51f3d841fd2bb37c631b
Jeff Epler <jepler@gmail.com>
2019-12-06 13:25:40 -0600

    audiocore: use mp_obj_t in prototypes

--- shared-module/audiocore/__init__.h
+++ shared-module/audiocore/__init__.h
@@ -39,14 +39,14 @@ typedef enum {
     GET_BUFFER_ERROR,           // Error while reading data.
 } audioio_get_buffer_result_t;
 
-typedef uint32_t (*audiosample_sample_rate_fun)(void* sample_obj);
-typedef uint8_t (*audiosample_bits_per_sample_fun)(void* sample_obj);
-typedef uint8_t (*audiosample_channel_count_fun)(void* sample_obj);
-typedef void (*audiosample_reset_buffer_fun)(void* sample_obj);
-typedef audioio_get_buffer_result_t (*audiosample_get_buffer_fun)(void* sample_obj,
+typedef uint32_t (*audiosample_sample_rate_fun)(mp_obj_t);
+typedef uint8_t (*audiosample_bits_per_sample_fun)(mp_obj_t);
+typedef uint8_t (*audiosample_channel_count_fun)(mp_obj_t);
+typedef void (*audiosample_reset_buffer_fun)(mp_obj_t);
+typedef audioio_get_buffer_result_t (*audiosample_get_buffer_fun)(mp_obj_t,
         bool single_channel, uint8_t channel, uint8_t** buffer,
         uint32_t* buffer_length);
-typedef void (*audiosample_get_buffer_structure_fun)(void* sample_obj,
+typedef void (*audiosample_get_buffer_structure_fun)(mp_obj_t,
         bool single_channel, bool* single_buffer,
         bool* samples_signed, uint32_t *max_buffer_length,
         uint8_t* spacing);
