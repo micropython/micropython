006d85d5c2c50c694df4b0eda1fab6f93d073b9f
JoeBakalor <jmbakalor@gmail.com>
2019-12-11 14:49:59 -0500

    add make file and board configuration, add accelerometer pin mappings

--- ports/nrf/boards/teknikio_bluebird/mpconfigboard.h
+++ ports/nrf/boards/teknikio_bluebird/mpconfigboard.h
@@ -27,48 +27,24 @@
 
 //https://github.com/Teknikio/TKInventionBuilderFramework
 
+
 #include "nrfx/hal/nrf_gpio.h"
 
-#define MICROPY_HW_BOARD_NAME       "Adafruit Circuit Playground Bluefruit"
+#define MICROPY_HW_BOARD_NAME       "Teknikio Bluebird"
 #define MICROPY_HW_MCU_NAME         "nRF52840"
-#define MICROPY_PY_SYS_PLATFORM     "CircuitPlaygroundBluefruit"
-
-#define FLASH_SIZE                  (0x100000)
-#define FLASH_PAGE_SIZE             (4096)
-
-#define MICROPY_HW_LED_STATUS       (&pin_P1_14)
-
-// Unusually, board does not have a 32 kHz xtal. Nearly all boards do.
-#define BOARD_HAS_32KHZ_XTAL (0)
-
-#if QSPI_FLASH_FILESYSTEM
-#define MICROPY_QSPI_DATA0                NRF_GPIO_PIN_MAP(0, 21)
-#define MICROPY_QSPI_DATA1                NRF_GPIO_PIN_MAP(0, 23)
-#define MICROPY_QSPI_DATA2                NRF_GPIO_PIN_MAP(1, 00)
-#define MICROPY_QSPI_DATA3                NRF_GPIO_PIN_MAP(0, 22)
-#define MICROPY_QSPI_SCK                  NRF_GPIO_PIN_MAP(0, 19)
-#define MICROPY_QSPI_CS                   NRF_GPIO_PIN_MAP(0, 15)
-#endif
-
-#if SPI_FLASH_FILESYSTEM
-#define SPI_FLASH_MOSI_PIN &pin_P0_21
-#define SPI_FLASH_MISO_PIN &pin_P0_23
-#define SPI_FLASH_SCK_PIN &pin_P0_19
-#define SPI_FLASH_CS_PIN &pin_P0_15
-#endif
 
 #define CIRCUITPY_AUTORELOAD_DELAY_MS 500
 
-#define CIRCUITPY_INTERNAL_NVM_SIZE (4096)
+#define MICROPY_HW_NEOPIXEL         (&pin_P0_26)
 
-#define BOARD_FLASH_SIZE (FLASH_SIZE - 0x4000 - CIRCUITPY_INTERNAL_NVM_SIZE)
+#define DEFAULT_I2C_BUS_SCL         (&pin_P0_27)
+#define DEFAULT_I2C_BUS_SDA         (&pin_P0_26)
 
-#define DEFAULT_I2C_BUS_SCL         (&pin_P0_04)
-#define DEFAULT_I2C_BUS_SDA         (&pin_P0_05)
+#define DEFAULT_SPI_BUS_SCK         (&pin_P1_15)
+#define DEFAULT_SPI_BUS_MOSI        (&pin_P1_13)
+#define DEFAULT_SPI_BUS_MISO        (&pin_P1_14)
 
-#define DEFAULT_SPI_BUS_SCK         (&pin_P0_05)
-#define DEFAULT_SPI_BUS_MOSI        (&pin_P1_03)
-#define DEFAULT_SPI_BUS_MISO        (&pin_P0_29)
+#define DEFAULT_UART_BUS_RX         (&pin_P1_07)
+#define DEFAULT_UART_BUS_TX         (&pin_P1_08)
 
-#define DEFAULT_UART_BUS_RX         (&pin_P0_30)
-#define DEFAULT_UART_BUS_TX         (&pin_P0_14)
+#define BOARD_HAS_CRYSTAL 1  // according to the schematic we do
