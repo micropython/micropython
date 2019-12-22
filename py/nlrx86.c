300eb65ae75857b247fc39fbe677f6e28a7a259d
Damien George <damien.p.george@gmail.com>
2019-12-22 22:04:45 +1100

    py/nlrx86: Silence possible warnings about unused nlr argument.

--- py/nlrx86.c
+++ py/nlrx86.c
@@ -56,9 +56,7 @@ __attribute__((used)) unsigned int nlr_push_tail(nlr_buf_t *nlr);
 __attribute__((naked))
 #endif
 unsigned int nlr_push(nlr_buf_t *nlr) {
-    #if !USE_NAKED
     (void)nlr;
-    #endif
 
     __asm volatile (
     #if UNDO_PRELUDE
