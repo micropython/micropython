b2e4a57289d17d6d4377880c5fc143b3d51f3678
Yonatan Goldschmidt <yon.goldschmidt@gmail.com>
2019-12-25 09:22:40 +0200

    nrf/main: Remove unnecessary repl_info(0) call.
    It's statically initialized to 0.

--- ports/nrf/main.c
+++ ports/nrf/main.c
@@ -121,8 +121,6 @@ soft_reset:
     mp_obj_list_append(mp_sys_path, MP_OBJ_NEW_QSTR(MP_QSTR_)); // current dir (or base dir of the script)
     mp_obj_list_init(mp_sys_argv, 0);
 
-    pyb_set_repl_info(MP_OBJ_NEW_SMALL_INT(0));
-
     readline_init0();
 
 
