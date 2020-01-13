1a2ef7e4f5c2910bb2fd8122b97bac25d1be1559
Mike Causer <mcauser@gmail.com>
2020-01-13 17:21:49 +1100

    stm32/boards: Add MCUDEV_DEVEBOX_F407VE board.

---
+++ ports/stm32/boards/MCUDEV_DEVEBOX_F407VE/bdev.c
@@ -0,0 +1,28 @@
+#include "storage.h"
+
+#if !MICROPY_HW_ENABLE_INTERNAL_FLASH_STORAGE
+
+// External SPI flash uses standard SPI interface
+
+STATIC const mp_soft_spi_obj_t soft_spi_bus = {
+    .delay_half = MICROPY_HW_SOFTSPI_MIN_DELAY,
+    .polarity = 0,
+    .phase = 0,
+    .sck = MICROPY_HW_SPIFLASH_SCK,
+    .mosi = MICROPY_HW_SPIFLASH_MOSI,
+    .miso = MICROPY_HW_SPIFLASH_MISO,
+};
+
+STATIC mp_spiflash_cache_t spi_bdev_cache;
+
+const mp_spiflash_config_t spiflash_config = {
+    .bus_kind = MP_SPIFLASH_BUS_SPI,
+    .bus.u_spi.cs = MICROPY_HW_SPIFLASH_CS,
+    .bus.u_spi.data = (void*)&soft_spi_bus,
+    .bus.u_spi.proto = &mp_soft_spi_proto,
+    .cache = &spi_bdev_cache,
+};
+
+spi_bdev_t spi_bdev;
+
+#endif
