e91af3d77cb9a787bb77bcce3259b323242fc3ba
Yonatan Goldschmidt <yon.goldschmidt@gmail.com>
2020-01-17 12:56:55 +0200

    linux-kernel: Update license years.

--- ports/linux-kernel/modkernel_ffi.c
+++ ports/linux-kernel/modkernel_ffi.c
@@ -3,7 +3,7 @@
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2019 Yonatan Goldschmidt
+ * Copyright (c) 2019, 2020 Yonatan Goldschmidt
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
