1c3960634520749180e349cee5fe4ea76fb75937
Scott Shawcroft <scott@tannewt.org>
2020-01-18 18:06:56 -0800

    Fix other builds missing new heap bounds functions

--- ports/atmel-samd/supervisor/port.c
+++ ports/atmel-samd/supervisor/port.c
@@ -331,6 +331,14 @@ uint32_t *port_stack_get_top(void) {
     return &_estack;
 }
 
+uint32_t *port_heap_get_bottom(void) {
+    return port_stack_get_limit();
+}
+
+uint32_t *port_heap_get_top(void) {
+    return port_stack_get_top();
+}
+
 // Place the word to save 8k from the end of RAM so we and the bootloader don't clobber it.
 #ifdef SAMD21
 uint32_t* safe_word = (uint32_t*) (HMCRAMC0_ADDR + HMCRAMC0_SIZE - 0x2000);
--- ports/mimxrt10xx/boards/imxrt1010_evk/flash_config.c
+++ ports/mimxrt10xx/boards/imxrt1010_evk/flash_config.c
@@ -8,11 +8,6 @@
 #include "fsl_flexspi_nor_boot.h"
 #include "fsl_flexspi_nor_config.h"
 
-/* Component ID definition, used by tools. */
-#ifndef FSL_COMPONENT_ID
-#define FSL_COMPONENT_ID "platform.drivers.xip_device"
-#endif
-
 __attribute__((section(".boot_hdr.ivt")))
 /*************************************
  *  IVT Data
@@ -38,7 +33,6 @@ const BOOT_DATA_T boot_data = {
   PLUGIN_FLAG,                /* Plugin flag*/
   0xFFFFFFFF                  /* empty - extra data word */
 };
-#endif
 
 __attribute__((section(".boot_hdr.conf")))
 // Values copied from https://github.com/PaulStoffregen/cores/blob/ddb23fa5d97dac763bc06e11b9b41f026bd51f0a/teensy4/bootdata.c#L39
