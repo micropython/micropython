57d27a7195efa85633e1d3f940bb179c43b638e2
Yonatan Goldschmidt <yon.goldschmidt@gmail.com>
2020-01-24 00:44:16 +0200

    linux-kernel: Add minimal "utime" module.
    It's required in many tests, for the sleep() function.

--- ports/linux-kernel/mphalport.h
+++ ports/linux-kernel/mphalport.h
@@ -1,7 +1,29 @@
 #include <linux/ktime.h>
+#include <linux/delay.h>
+
 
 static inline mp_uint_t mp_hal_ticks_ms(void) {
     return ktime_to_ms(ktime_get_boottime());
+}
+
+static inline void mp_hal_delay_ms(mp_uint_t ms) {
+    msleep(ms);
+}
+
+static inline void mp_hal_delay_us(mp_uint_t us) {
+    // not currently needed because we don't expose mp_utime_sleep_us_obj.
+    // we don't compile with -ffunction-section however, so this symbol
+    // is still required.
+}
+
+static inline mp_uint_t mp_hal_ticks_us(void) {
+    // same comment as in mp_hal_delay_us
+    return 0;
+}
+
+static inline mp_uint_t mp_hal_ticks_cpu(void) {
+    // same comment as in mp_hal_delay_us
+    return 0;
 }
 
 void mp_hal_set_interrupt_char(int c);
