f2dfcee04241faad5edb7ef678c95cecf7408c60
Lady Ada <limor@ladyada.net>
2020-01-26 20:10:20 -0500

    add missing displayref

--- ports/nrf/boards/clue_nrf52840_express/pins.c
+++ ports/nrf/boards/clue_nrf52840_express/pins.c
@@ -1,5 +1,8 @@
 #include "shared-bindings/board/__init__.h"
 
+#include "boards/board.h"
+#include "shared-module/displayio/__init__.h"
+
 STATIC const mp_rom_map_elem_t board_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_P0), MP_ROM_PTR(&pin_P0_04) },
     { MP_ROM_QSTR(MP_QSTR_D0), MP_ROM_PTR(&pin_P0_04) },
@@ -96,6 +99,8 @@ STATIC const mp_rom_map_elem_t board_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_I2C), MP_ROM_PTR(&board_i2c_obj) },
     { MP_ROM_QSTR(MP_QSTR_SPI), MP_ROM_PTR(&board_spi_obj) },
     { MP_ROM_QSTR(MP_QSTR_UART), MP_ROM_PTR(&board_uart_obj) },
+
+    { MP_ROM_QSTR(MP_QSTR_DISPLAY), MP_ROM_PTR(&displays[0].display)}
 };
 
 MP_DEFINE_CONST_DICT(board_module_globals, board_module_globals_table);
