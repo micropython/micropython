b9525138ec4ef1c7203998586567295fdd60c4b0
Salvatore Musumeci <salvo.nos86@gmail.com>
2020-01-30 19:01:23 +0100

    beautify: brackets inline

--- ports/esp32/machine_can.c
+++ ports/esp32/machine_can.c
@@ -57,15 +57,13 @@
     {                              \
         mp_raise_OSError(-status); \
     }
-typedef enum _filter_mode_t
-{
+typedef enum _filter_mode_t{
     FILTER_RAW_SINGLE = 0,
     FILTER_RAW_DUAL,
     FILTER_ADDRESS
 } filter_mode_t;
 
-typedef struct _machine_can_config_t
-{
+typedef struct _machine_can_config_t{
     can_timing_config_t timing;
     can_filter_config_t filter;
     can_general_config_t general;
@@ -73,8 +71,7 @@ typedef struct _machine_can_config_t
     bool initialized;
 } machine_can_config_t;
 
-typedef struct _machine_can_obj_t
-{
+typedef struct _machine_can_obj_t{
     mp_obj_base_t base;
     machine_can_config_t *config;
     mp_obj_t rxcallback;
@@ -86,8 +83,7 @@ typedef struct _machine_can_obj_t
     uint16_t num_bus_off;
 } machine_can_obj_t;
 
-typedef enum _rx_state_t
-{
+typedef enum _rx_state_t{
     RX_STATE_FIFO_EMPTY = 0,
     RX_STATE_MESSAGE_PENDING,
     RX_STATE_FIFO_FULL,
@@ -114,8 +110,7 @@ machine_can_config_t can_config = {.general = CAN_GENERAL_CONFIG_DEFAULT(2, 4, 0
 STATIC machine_can_obj_t machine_can_obj = {{&machine_can_type}, .config = &can_config};
 
 // INTERNAL FUNCTION Return status information
-STATIC can_status_info_t _machine_hw_can_get_status()
-{
+STATIC can_status_info_t _machine_hw_can_get_status(){
     can_status_info_t status;
     uint32_t err_code = can_get_status_info(&status);
     if (err_code != ESP_OK)
@@ -126,41 +121,31 @@ STATIC can_status_info_t _machine_hw_can_get_status()
 }
 
 //INTERNAL FUNCTION Populates the filter register according to inputs
-STATIC void _machine_hw_can_set_filter(machine_can_obj_t *self, uint32_t addr, uint32_t mask, uint8_t bank, bool rtr)
-{
+STATIC void _machine_hw_can_set_filter(machine_can_obj_t *self, uint32_t addr, uint32_t mask, uint8_t bank, bool rtr){
     //Check if bank is allowed
-    if (bank < 0 && bank > ((self->extframe && self->config->filter.single_filter) ? 0 : 1))
-    {
+    if (bank < 0 && bank > ((self->extframe && self->config->filter.single_filter) ? 0 : 1)){
         mp_raise_ValueError("CAN filter parameter error");
     }
     uint32_t preserve_mask;
-    if (self->extframe)
-    {
+    if (self->extframe){
         addr = (addr & 0x1FFFFFFF) << 3 | (rtr ? 0x04 : 0);
         mask = (mask & 0x1FFFFFFF) << 3 | 0x03;
         preserve_mask = 0;
-    }
-    else if (self->config->filter.single_filter)
-    {
+    }else if (self->config->filter.single_filter){
         addr = (((addr & 0x7FF) << 5) | (rtr ? 0x10 : 0));
         mask = ((mask & 0x7FF) << 5);
         mask |= 0xFFFFF000;
         preserve_mask = 0;
-    }
-    else
-    {
+    }else{
         addr = (((addr & 0x7FF) << 5) | (rtr ? 0x10 : 0));
         mask = ((mask & 0x7FF) << 5);
         preserve_mask = 0xFFFF << (bank == 0 ? 16 : 0);
-        if ((self->config->filter.acceptance_mask & preserve_mask) == (0xFFFF << (bank == 0 ? 16 : 0)))
-        {
+        if ((self->config->filter.acceptance_mask & preserve_mask) == (0xFFFF << (bank == 0 ? 16 : 0))){
             // Other filter accepts all; it will replaced duplicating current filter
             addr = addr | (addr << 16);
             mask = mask | (mask << 16);
             preserve_mask = 0;
-        }
-        else
-        {
+        }else{
             addr = addr << (bank == 1 ? 16 : 0);
             mask = mask << (bank == 1 ? 16 : 0);
         }
@@ -172,17 +157,14 @@ STATIC void _machine_hw_can_set_filter(machine_can_obj_t *self, uint32_t addr, u
 }
 
 // Force a software restart of the controller, to allow transmission after a bus error
-STATIC mp_obj_t machine_hw_can_restart(mp_obj_t self_in)
-{
+STATIC mp_obj_t machine_hw_can_restart(mp_obj_t self_in){
     uint32_t status = can_initiate_recovery();
-    if (status != ESP_OK)
-    {
+    if (status != ESP_OK){
         mp_raise_OSError(-status);
     }
     mp_hal_delay_ms(200);
     status = can_start();
-    if (status != ESP_OK)
-    {
+    if (status != ESP_OK){
         mp_raise_OSError(-status);
     }
     return mp_const_none;
@@ -190,39 +172,31 @@ STATIC mp_obj_t machine_hw_can_restart(mp_obj_t self_in)
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(machine_hw_can_restart_obj, machine_hw_can_restart);
 
 // any() - return `True` if any message waiting, else `False`
-STATIC mp_obj_t machine_hw_can_any(mp_obj_t self_in)
-{
+STATIC mp_obj_t machine_hw_can_any(mp_obj_t self_in){
     can_status_info_t status = _machine_hw_can_get_status();
     return mp_obj_new_bool((status.msgs_to_rx) > 0);
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(machine_hw_can_any_obj, machine_hw_can_any);
 
 // Get the state of the controller
-STATIC mp_obj_t machine_hw_can_state(mp_obj_t self_in)
-{
+STATIC mp_obj_t machine_hw_can_state(mp_obj_t self_in){
     can_status_info_t status = _machine_hw_can_get_status();
     return mp_obj_new_int(status.state);
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(machine_hw_can_state_obj, machine_hw_can_state);
 
 // Get info about error states and TX/RX buffers
-STATIC mp_obj_t machine_hw_can_info(size_t n_args, const mp_obj_t *args)
-{
+STATIC mp_obj_t machine_hw_can_info(size_t n_args, const mp_obj_t *args){
     machine_can_obj_t *self = MP_OBJ_TO_PTR(args[0]);
     mp_obj_list_t *list;
-    if (n_args == 1)
-    {
+    if (n_args == 1){
         list = MP_OBJ_TO_PTR(mp_obj_new_list(8, NULL));
-    }
-    else
-    {
-        if (!mp_obj_is_type(args[1], &mp_type_list))
-        {
+    }else{
+        if (!mp_obj_is_type(args[1], &mp_type_list)){
             mp_raise_TypeError(NULL);
         }
         list = MP_OBJ_TO_PTR(args[1]);
-        if (list->len < 8)
-        {
+        if (list->len < 8){
             mp_raise_ValueError(NULL);
         }
     }
@@ -240,12 +214,10 @@ STATIC mp_obj_t machine_hw_can_info(size_t n_args, const mp_obj_t *args)
 STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(machine_hw_can_info_obj, 1, 2, machine_hw_can_info);
 
 // Get Alert info
-STATIC mp_obj_t machine_hw_can_alert(mp_obj_t self_in)
-{
+STATIC mp_obj_t machine_hw_can_alert(mp_obj_t self_in){
     uint32_t alerts;
     uint32_t status = can_read_alerts(&alerts, 0);
-    if (status != ESP_OK)
-    {
+    if (status != ESP_OK){
         mp_raise_OSError(-status);
     }
     return mp_obj_new_int(alerts);
@@ -253,24 +225,20 @@ STATIC mp_obj_t machine_hw_can_alert(mp_obj_t self_in)
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(machine_hw_can_alert_obj, machine_hw_can_alert);
 
 // Clear TX Queue
-STATIC mp_obj_t machine_hw_can_clear_tx_queue(mp_obj_t self_in)
-{
+STATIC mp_obj_t machine_hw_can_clear_tx_queue(mp_obj_t self_in){
     return mp_obj_new_bool(can_clear_transmit_queue() == ESP_OK);
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(machine_hw_can_clear_tx_queue_obj, machine_hw_can_clear_tx_queue);
 
 // Clear RX Queue
-STATIC mp_obj_t machine_hw_can_clear_rx_queue(mp_obj_t self_in)
-{
+STATIC mp_obj_t machine_hw_can_clear_rx_queue(mp_obj_t self_in){
     return mp_obj_new_bool(can_clear_receive_queue() == ESP_OK);
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(machine_hw_can_clear_rx_queue_obj, machine_hw_can_clear_rx_queue);
 
 // send([data], id, *)
-STATIC mp_obj_t machine_hw_can_send(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args)
-{
-    enum
-    {
+STATIC mp_obj_t machine_hw_can_send(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args){
+    enum{
         ARG_data,
         ARG_id,
         ARG_timeout,
@@ -293,53 +261,41 @@ STATIC mp_obj_t machine_hw_can_send(size_t n_args, const mp_obj_t *pos_args, mp_
     size_t length;
     mp_obj_t *items;
     mp_obj_get_array(args[ARG_data].u_obj, &length, &items);
-    if (length > 8)
-    {
+    if (length > 8){
         mp_raise_ValueError("CAN data field too long");
     }
     uint8_t flags = (args[ARG_rtr].u_bool == true ? CAN_MSG_FLAG_RTR : CAN_MSG_FLAG_NONE);
     uint32_t id = args[ARG_id].u_int;
-    if (self->extframe)
-    {
+    if (self->extframe){
         flags += CAN_MSG_FLAG_EXTD;
         id &= 0x1FFFFFFF;
-    }
-    else
-    {
+    }else{
         id &= 0x1FF;
     }
-    if (self->loopback)
-    {
+    if (self->loopback){
         flags += CAN_MSG_FLAG_SELF;
     }
     can_message_t tx_msg = {.data_length_code = length,
                             .identifier = id,
                             .flags = flags};
-    for (uint8_t i = 0; i < length; i++)
-    {
+    for (uint8_t i = 0; i < length; i++){
         tx_msg.data[i] = mp_obj_get_int(items[i]);
     }
-    if (_machine_hw_can_get_status().state == CAN_STATE_RUNNING)
-    {
+    if (_machine_hw_can_get_status().state == CAN_STATE_RUNNING){
         int status = can_transmit(&tx_msg, args[ARG_timeout].u_int);
-        if (status != ESP_OK)
-        {
+        if (status != ESP_OK){
             mp_raise_OSError(-status);
         }
         return mp_const_none;
-    }
-    else
-    {
+    }else{
         nlr_raise(mp_obj_new_exception_msg(&mp_type_RuntimeError, "CAN Device is not ready"));
     }
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_KW(machine_hw_can_send_obj, 3, machine_hw_can_send);
 
 // recv(list=None, *, timeout=5000)
-STATIC mp_obj_t machine_hw_can_recv(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args)
-{
-    enum
-    {
+STATIC mp_obj_t machine_hw_can_recv(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args){
+    enum{
         ARG_list,
         ARG_timeout
     };
@@ -354,42 +310,34 @@ STATIC mp_obj_t machine_hw_can_recv(size_t n_args, const mp_obj_t *pos_args, mp_
 
     can_message_t rx_message;
     int status = can_receive(&rx_message, 1);
-    if (status != ESP_OK)
-    {
+    if (status != ESP_OK){
         mp_raise_OSError(-status);
     }
     // Create the tuple, or get the list, that will hold the return values
     // Also populate the fourth element, either a new bytes or reuse existing memoryview
     mp_obj_t ret_obj = args[ARG_list].u_obj;
     mp_obj_t *items;
-    if (ret_obj == mp_const_none)
-    {
+    if (ret_obj == mp_const_none){
         ret_obj = mp_obj_new_tuple(4, NULL);
         items = ((mp_obj_tuple_t *)MP_OBJ_TO_PTR(ret_obj))->items;
         items[3] = mp_obj_new_bytes(rx_message.data, rx_message.data_length_code);
-    }
-    else
-    {
+    }else{
         // User should provide a list of length at least 4 to hold the values
-        if (!mp_obj_is_type(ret_obj, &mp_type_list))
-        {
+        if (!mp_obj_is_type(ret_obj, &mp_type_list)){
             mp_raise_TypeError(NULL);
         }
         mp_obj_list_t *list = MP_OBJ_TO_PTR(ret_obj);
-        if (list->len < 4)
-        {
+        if (list->len < 4){
             mp_raise_ValueError(NULL);
         }
         items = list->items;
         // Fourth element must be a memoryview which we assume points to a
         // byte-like array which is large enough, and then we resize it inplace
-        if (!mp_obj_is_type(items[3], &mp_type_memoryview))
-        {
+        if (!mp_obj_is_type(items[3], &mp_type_memoryview)){
             mp_raise_TypeError(NULL);
         }
         mp_obj_array_t *mv = MP_OBJ_TO_PTR(items[3]);
-        if (!(mv->typecode == (MP_OBJ_ARRAY_TYPECODE_FLAG_RW | BYTEARRAY_TYPECODE) || (mv->typecode | 0x20) == (MP_OBJ_ARRAY_TYPECODE_FLAG_RW | 'b')))
-        {
+        if (!(mv->typecode == (MP_OBJ_ARRAY_TYPECODE_FLAG_RW | BYTEARRAY_TYPECODE) || (mv->typecode | 0x20) == (MP_OBJ_ARRAY_TYPECODE_FLAG_RW | 'b'))){
             mp_raise_ValueError(NULL);
         }
         mv->len = rx_message.data_length_code;
@@ -403,8 +351,7 @@ STATIC mp_obj_t machine_hw_can_recv(size_t n_args, const mp_obj_t *pos_args, mp_
 STATIC MP_DEFINE_CONST_FUN_OBJ_KW(machine_hw_can_recv_obj, 0, machine_hw_can_recv);
 
 // Clear filters setting
-STATIC mp_obj_t machine_hw_can_clearfilter(mp_obj_t self_in)
-{
+STATIC mp_obj_t machine_hw_can_clearfilter(mp_obj_t self_in){
     machine_can_obj_t *self = MP_OBJ_TO_PTR(self_in);
     self->config->filter.single_filter = self->extframe;
     self->config->filter.acceptance_code = 0;
@@ -425,8 +372,7 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_1(machine_hw_can_clearfilter_obj, machine_hw_can_
 // params: [id, mask]
 // rtr: ignored if FILTER_RAW
 // Set CAN HW filter
-STATIC mp_obj_t machine_hw_can_setfilter(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args)
-{
+STATIC mp_obj_t machine_hw_can_setfilter(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args){
     enum
     {
         ARG_bank,
@@ -449,20 +395,16 @@ STATIC mp_obj_t machine_hw_can_setfilter(size_t n_args, const mp_obj_t *pos_args
     size_t len;
     mp_obj_t *params;
     mp_obj_get_array(args[ARG_params].u_obj, &len, &params);
-    if (len != 2)
-    {
+    if (len != 2){
         mp_raise_ValueError("params shall be a 2-values list");
     }
     uint32_t id = mp_obj_get_int(params[0]);
     uint32_t mask = mp_obj_get_int(params[1]); // FIXME: Overflow in case 0xFFFFFFFF for mask
-    if (args[ARG_mode].u_int == FILTER_RAW_SINGLE || args[ARG_mode].u_int == FILTER_RAW_DUAL)
-    {
+    if (args[ARG_mode].u_int == FILTER_RAW_SINGLE || args[ARG_mode].u_int == FILTER_RAW_DUAL){
         self->config->filter.single_filter = (args[ARG_mode].u_int == FILTER_RAW_SINGLE);
         self->config->filter.acceptance_code = id;
         self->config->filter.acceptance_mask = mask;
-    }
-    else
-    {
+    }else{
         self->config->filter.single_filter = self->extframe;
         _machine_hw_can_set_filter(self, id, mask, args[ARG_bank].u_int, args[ARG_rtr].u_int);
     }
@@ -477,11 +419,9 @@ STATIC mp_obj_t machine_hw_can_setfilter(size_t n_args, const mp_obj_t *pos_args
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_KW(machine_hw_can_setfilter_obj, 1, machine_hw_can_setfilter);
 
-STATIC void machine_hw_can_print(const mp_print_t *print, mp_obj_t self_in, mp_print_kind_t kind)
-{
+STATIC void machine_hw_can_print(const mp_print_t *print, mp_obj_t self_in, mp_print_kind_t kind){
     machine_can_obj_t *self = MP_OBJ_TO_PTR(self_in);
-    if (self->config->initialized)
-    {
+    if (self->config->initialized){
         qstr mode;
         switch (self->config->general.mode)
         {
@@ -506,19 +446,15 @@ STATIC void machine_hw_can_print(const mp_print_t *print, mp_obj_t self_in, mp_p
                   mode,
                   self->loopback,
                   self->extframe);
-    }
-    else
-    {
+    }else{
         mp_printf(print, "CAN Device is not initialized");
     }
 }
 
 // init(tx, rx, baudrate, mode = CAN_MODE_NORMAL, tx_queue = 2, rx_queue = 5)
-STATIC mp_obj_t machine_hw_can_init(size_t n_args, const mp_obj_t *args, mp_map_t *kw_args)
-{
+STATIC mp_obj_t machine_hw_can_init(size_t n_args, const mp_obj_t *args, mp_map_t *kw_args){
     machine_can_obj_t *self = MP_OBJ_TO_PTR(args[0]);
-    if (self->config->initialized)
-    {
+    if (self->config->initialized){
         ESP_LOGW(DEVICE_NAME, "Device is already initialized");
         return mp_const_none;
     }
@@ -527,22 +463,18 @@ STATIC mp_obj_t machine_hw_can_init(size_t n_args, const mp_obj_t *args, mp_map_
 STATIC MP_DEFINE_CONST_FUN_OBJ_KW(machine_hw_can_init_obj, 4, machine_hw_can_init);
 
 // deinit()
-STATIC mp_obj_t machine_hw_can_deinit(const mp_obj_t self_in)
-{
+STATIC mp_obj_t machine_hw_can_deinit(const mp_obj_t self_in){
     const machine_can_obj_t *self = &machine_can_obj;
-    if (self->config->initialized != true)
-    {
+    if (self->config->initialized != true){
         ESP_LOGW(DEVICE_NAME, "Device is not initialized");
         return mp_const_none;
     }
     uint32_t status = can_stop();
-    if (status != ESP_OK)
-    {
+    if (status != ESP_OK){
         mp_raise_OSError(-status);
     }
     status = can_driver_uninstall();
-    if (status != ESP_OK)
-    {
+    if (status != ESP_OK){
         mp_raise_OSError(-status);
     }
     self->config->initialized = false;
@@ -553,24 +485,19 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_1(machine_hw_can_deinit_obj, machine_hw_can_deini
 // CAN(bus, ...) No argument to get the object
 // If no arguments are provided, the initialized object will be returned
 mp_obj_t machine_hw_can_make_new(const mp_obj_type_t *type, size_t n_args,
-                                 size_t n_kw, const mp_obj_t *args)
-{
+                                 size_t n_kw, const mp_obj_t *args){
     // check arguments
     mp_arg_check_num(n_args, n_kw, 1, MP_OBJ_FUN_ARGS_MAX, true);
-    if (mp_obj_is_int(args[0]) != true)
-    {
+    if (mp_obj_is_int(args[0]) != true){
         mp_raise_TypeError("bus must be a number");
     }
     mp_uint_t can_idx = mp_obj_get_int(args[0]);
-    if (can_idx > 1)
-    {
+    if (can_idx > 1){
         nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_ValueError, "CAN(%d) doesn't exist", can_idx));
     }
     machine_can_obj_t *self = &machine_can_obj;
-    if (n_args > 1 || n_kw > 0)
-    {
-        if (self->config->initialized)
-        {
+    if (n_args > 1 || n_kw > 0){
+        if (self->config->initialized){
             // The caller is requesting a reconfiguration of the hardware
             // this can only be done if the hardware is in init mode
             ESP_LOGW(DEVICE_NAME, "Device is going to be reconfigured");
@@ -588,10 +515,8 @@ mp_obj_t machine_hw_can_make_new(const mp_obj_type_t *type, size_t n_args,
 }
 
 // init(mode, extframe=False, baudrate=500, *)
-STATIC mp_obj_t machine_hw_can_init_helper(machine_can_obj_t *self, size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args)
-{
-    enum
-    {
+STATIC mp_obj_t machine_hw_can_init_helper(machine_can_obj_t *self, size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args){
+    enum{
         ARG_mode,
         ARG_extframe,
         ARG_baudrate,
@@ -635,8 +560,7 @@ STATIC mp_obj_t machine_hw_can_init_helper(machine_can_obj_t *self, size_t n_arg
     self->config->general.clkout_divider = 0;
     self->loopback = ((args[ARG_mode].u_int & 0x10) > 0);
     self->extframe = args[ARG_extframe].u_bool;
-    if (args[ARG_auto_restart].u_bool)
-    {
+    if (args[ARG_auto_restart].u_bool){
         mp_raise_NotImplementedError("Auto-restart not supported");
     }
     can_filter_config_t f_config = CAN_FILTER_CONFIG_ACCEPT_ALL();
@@ -644,8 +568,7 @@ STATIC mp_obj_t machine_hw_can_init_helper(machine_can_obj_t *self, size_t n_arg
     self->config->filter.acceptance_code = f_config.acceptance_code;
     self->config->filter.acceptance_mask = f_config.acceptance_mask;
     can_timing_config_t *timing;
-    switch ((int)args[ARG_baudrate].u_int)
-    {
+    switch ((int)args[ARG_baudrate].u_int){
     case 0:
         timing = &((can_timing_config_t){
             .brp = args[ARG_prescaler].u_int,
@@ -690,19 +613,13 @@ STATIC mp_obj_t machine_hw_can_init_helper(machine_can_obj_t *self, size_t n_arg
         &self->config->general,
         &self->config->timing,
         &(can_filter_config_t)CAN_FILTER_CONFIG_ACCEPT_ALL());
-    if (status != ESP_OK)
-    {
+    if (status != ESP_OK){
         mp_raise_OSError(-status);
-    }
-    else
-    {
+    }else{
         status = can_start();
-        if (status != ESP_OK)
-        {
+        if (status != ESP_OK){
             mp_raise_OSError(-status);
-        }
-        else
-        {
+        }else{
             self->config->initialized = true;
         }
     }
