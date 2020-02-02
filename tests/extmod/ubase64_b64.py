62749989eaa78423d4b6401fabf42cc93c78427b
odewdney <oliver.dewdney@lbi.com>
2020-02-02 09:16:24 +0000

    create test for base64 mod

---
+++ tests/extmod/ubase64_b64.py
@@ -0,0 +1,44 @@
+try:
+    try:
+        import ubase64 as base64
+    except ImportError:
+        import base64
+except ImportError:
+    print("SKIP")
+    raise SystemExit
+
+
+print(base64.b64encode(b"foo"))
+print(base64.b64encode(b"\xfb\xff", altchars=b"[]"))
+
+print(base64.b64decode(b"Zm9v"))
+print(base64.b64decode(b"[]8=", altchars=b"[]"))
+
+print(base64.b64decode(b"Zm9v", validate=True))
+print(base64.b64decode("+/8=", validate=True))
+
+print(base64.b64decode(b"[]8=", altchars=b"[]", validate=True))
+print(base64.b64decode("[]8=", altchars=b"[]", validate=True))
+
+print(base64.standard_b64encode(b"foo"))
+print(base64.standard_b64encode(b"\xfb\xff"))
+
+print(base64.standard_b64decode("Zm9v"))
+print(base64.standard_b64decode(b"Zm9v"))
+print(base64.standard_b64decode(b"/+8="))
+
+print(base64.urlsafe_b64encode(b"foo"))
+print(base64.urlsafe_b64encode(b"\xfb\xff"))
+
+print(base64.urlsafe_b64decode("Zm9v"))
+print(base64.urlsafe_b64decode(b"Zm9v"))
+print(base64.urlsafe_b64decode(b"-_8="))
+
+try:
+    print(base64.b64decode(b"Zm9-", validate=True))
+except ValueError:
+    print("ValueError")
+try:
+    print(base64.b64decode(b"Zm9", validate=True))
+except ValueError:
+    print("ValueError")
