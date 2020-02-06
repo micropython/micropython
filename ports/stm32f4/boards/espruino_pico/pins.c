698ad745f6f707cf759a617990d186e1a8527b68
Lucian Copeland <hierophect@gmail.com>
2020-02-06 10:49:03 -0500

    Fix oscillator issue, add button and LED pins

--- ports/stm32f4/boards/espruino_pico/pins.c
+++ ports/stm32f4/boards/espruino_pico/pins.c
@@ -25,5 +25,9 @@ STATIC const mp_rom_map_elem_t board_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_A10), MP_ROM_PTR(&pin_PA10) },
     { MP_ROM_QSTR(MP_QSTR_B9), MP_ROM_PTR(&pin_PB09) },
     { MP_ROM_QSTR(MP_QSTR_B8), MP_ROM_PTR(&pin_PB08) },
+
+    { MP_ROM_QSTR(MP_QSTR_BTN), MP_ROM_PTR(&pin_PC13) },
+    { MP_ROM_QSTR(MP_QSTR_LED1), MP_ROM_PTR(&pin_PB02) },
+    { MP_ROM_QSTR(MP_QSTR_LED2), MP_ROM_PTR(&pin_PB12) },
 };
 MP_DEFINE_CONST_DICT(board_module_globals, board_module_globals_table);
