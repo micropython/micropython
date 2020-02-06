a9b34f45dc821ad14c24aec7df6be74138fe4196
James Bowman <jamesb@excamera.com>
2020-02-05 18:01:25 -0800

    My name

--- shared-bindings/eveL/__init__.c
+++ shared-bindings/eveL/__init__.c
@@ -3,7 +3,7 @@
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2014 Paul Sokolovsky
+ * Copyright (c) 2020 James Bowman
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
