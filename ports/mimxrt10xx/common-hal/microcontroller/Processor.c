d13f04f9534813ab1c1d975350961bfc0f5b7f67
Dave Marples <dave@marples.net>
2020-02-14 23:02:02 +0000

    Updated formatting

--- ports/mimxrt10xx/common-hal/microcontroller/Processor.c
+++ ports/mimxrt10xx/common-hal/microcontroller/Processor.c
@@ -59,16 +59,12 @@ void common_hal_mcu_processor_get_uid(uint8_t raw_id[]) {
 
     // Reads shadow registers 0x01 - 0x04 (Configuration and Manufacturing Info)
     // into 8 bit wide destination, avoiding punning.
-    for (int i = 0; i < 4; ++i)
-      {
-	uint32_t wr = OCOTP_ReadFuseShadowRegister(OCOTP, i + 1);
-
-	for (int j = 0; j < 4; j++)
-	  {
-	    raw_id[i*4+j] = wr&0xff;
-	    wr>>=8;
-	  }
-      }
-
+    for (int i = 0; i < 4; ++i) {
+        uint32_t wr = OCOTP_ReadFuseShadowRegister(OCOTP, i + 1);
+        for (int j = 0; j < 4; j++) {
+            raw_id[i*4+j] = wr&0xff;
+            wr>>=8;
+        }
+    }
     OCOTP_Deinit(OCOTP);
 }
