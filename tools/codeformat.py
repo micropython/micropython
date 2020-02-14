b22ab7c21a57d1070f49eb5a8991117318a02b43
Damien George <damien.p.george@gmail.com>
2020-02-14 11:42:44 +1100

    tools: Add codeformat.sh script and corresponding .astylerc.

---
+++ tools/codeformat.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import sys, os, glob
+
+FILES = [
+    'extmod/*.[ch]',
+    'ports/*/*.[ch]',
+    'py/*.[ch]',
+]
+
+EXCLUSIONS = [
+    'ports/*/qstrdefsport.h',
+    'py/grammar.h',
+    'py/qstrdefs.h',
+    'py/vm.c',
+]
+
+def list_files():
+    files = set()
+    for pattern in FILES:
+        files.update(glob.glob(pattern))
+    for pattern in EXCLUSIONS:
+        files.difference_update(glob.fnmatch.filter(files, pattern))
+    return sorted(files)
+
+def main():
+    try:
+        os.stat('.astylerc')
+    except OSError:
+        print('Run this from the top-level directory, e.g. ./tools/codeformat.py', file=sys.stderr)
+        sys.exit(1)
+
+    os.system('astyle --project ' + ' '.join(list_files()))
+
+if __name__ == '__main__':
+    main()
