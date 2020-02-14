ffb81479f3cda4aa1cd51e9e6f3a4342fb941dd3
Damien George <damien.p.george@gmail.com>
2020-02-14 11:42:44 +1100

    tools: Add codeformat.sh script and corresponding .clang-format.

---
+++ tools/codeformat.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import sys, os, glob
+
+FILES = [
+    'extmod/*.[ch]',
+    'ports/*/*.[ch]',
+    'py/*.[ch]',
+]
+
+EXCLUSIONS = [
+    'ports/*/qstrdefsport.h',
+    'py/grammar.h',
+    'py/qstrdefs.h',
+    'py/vm.c',
+]
+
+def list_files():
+    files = set()
+    for pattern in FILES:
+        files.update(glob.glob(pattern))
+    for pattern in EXCLUSIONS:
+        files.difference_update(glob.fnmatch.filter(files, pattern))
+    return sorted(files)
+
+def main():
+    try:
+        os.stat('.clang-format')
+    except OSError:
+        print('Run this from the top-level directory, e.g. ./tools/codeformat.py', file=sys.stderr)
+        sys.exit(1)
+
+    os.system('clang-format --style=file -i ' + ' '.join(list_files()))
+    # Add spaces inside struct initialisation: `    {MP_QSTR_name, foo, {.x = y}},` --> `    { MP_QSTR_name, foo, {.x = y} },`
+    os.system(r'sed -r -i "s/^(\s+\{)\s*(.*)\s*(\},?)$/\1 \2 \3/g" ' + ' '.join(list_files()))
+
+if __name__ == '__main__':
+    main()
