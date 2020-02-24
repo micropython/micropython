e9eeb2470a452d88447d7aea7aeb4e5ac9f18183
Jim Mussared <jim.mussared@gmail.com>
2020-02-24 15:45:57 +1100

    extmod/modbluetooth_nimble: Clarify active state and check active.

--- ports/esp32/main.c
+++ ports/esp32/main.c
@@ -58,6 +58,10 @@
 #include "modnetwork.h"
 #include "mpthreadport.h"
 
+#if MICROPY_BLUETOOTH_NIMBLE
+#include "extmod/modbluetooth.h"
+#endif
+
 // MicroPython runs as a task under FreeRTOS
 #define MP_TASK_PRIORITY        (ESP_TASK_PRIO_MIN + 1)
 #define MP_TASK_STACK_SIZE      (16 * 1024)
@@ -137,6 +141,10 @@ soft_reset:
     }
 
     machine_timer_deinit_all();
+
+    #if MICROPY_BLUETOOTH_NIMBLE
+    mp_bluetooth_deinit();
+    #endif
 
     #if MICROPY_PY_THREAD
     mp_thread_deinit();
