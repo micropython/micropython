91e7668ae4f2c15f48bdaa2e5cf7032d654da2ef
Damien George <damien.p.george@gmail.com>
2020-02-26 11:06:45 +1100

    tools/codeformat.py: Add script to format code.

---
+++ tools/codeformat.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import sys, os, glob
+
+FILES = [
+    'drivers/**/*.py',
+    'examples/**/*.py',
+    'extmod/**/*.py',
+    'lib/**/*.py',
+    'ports/**/*.py',
+    'py/**/*.py',
+    'tools/**/*.py',
+]
+
+EXCLUSIONS = []
+
+
+def list_files():
+    files = set()
+    for pattern in FILES:
+        files.update(glob.glob(pattern, recursive=True))
+    for pattern in EXCLUSIONS:
+        files.difference_update(glob.fnmatch.filter(files, pattern))
+    return sorted(files)
+
+
+def main():
+    try:
+        os.stat('./tools/codeformat.py')
+    except OSError:
+        print('Run this from the top-level directory, e.g. ./tools/codeformat.py', file=sys.stderr)
+        sys.exit(1)
+
+    files = list_files()
+    for file in files:
+        print(file)
+        os.system(
+            "yapf --style '{based_on_style: pep8, column_limit: 100, spaces_before_comment=1}' -i "
+            + file)
+
+
+if __name__ == '__main__':
+    main()
