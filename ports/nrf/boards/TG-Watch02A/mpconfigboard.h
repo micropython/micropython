6d4ffc69fdfe19366b15861694f97cbf77452937
TG-Techie <TGTechie01@gmail.com>
2020-02-28 02:52:46 -0500

    add TG-Watch02A

--- ports/nrf/boards/TG-Watch02A/mpconfigboard.h
+++ ports/nrf/boards/TG-Watch02A/mpconfigboard.h
@@ -4,7 +4,7 @@
  * The MIT License (MIT)
  *
  * Copyright (c) 2016 Glenn Ruben Bakke
- *
+ * Copyright (c) 2018 Dan Halbert for Adafruit Industries
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -27,40 +27,46 @@
 
 #include "nrfx/hal/nrf_gpio.h"
 
-#define MICROPY_HW_BOARD_NAME       "PCA10056 nRF52840-DK"
+#define MICROPY_HW_BOARD_NAME       "TG-Watch02A-rev00"
 #define MICROPY_HW_MCU_NAME         "nRF52840"
 
-#define CIRCUITPY_AUTORELOAD_DELAY_MS 500
+#define FLASH_SIZE                  (0x100000)
+#define FLASH_PAGE_SIZE             (4096)
 
-#define MICROPY_HW_LED_STATUS          (&pin_P0_13)
+#define MICROPY_HW_NEOPIXEL         (&pin_P0_16)
 
-#define DEFAULT_I2C_BUS_SCL         (&pin_P0_27)
-#define DEFAULT_I2C_BUS_SDA         (&pin_P0_26)
+#define MICROPY_HW_LED_STATUS       (&pin_P1_15)
 
-#define DEFAULT_SPI_BUS_SCK         (&pin_P1_15)
-#define DEFAULT_SPI_BUS_MOSI        (&pin_P1_13)
-#define DEFAULT_SPI_BUS_MISO        (&pin_P1_14)
-
-#define DEFAULT_UART_BUS_RX         (&pin_P1_01)
-#define DEFAULT_UART_BUS_TX         (&pin_P1_02)
-
-// Flash operation mode is determined by MICROPY_QSPI_DATAn pin configuration.
-// A pin config is valid if it is defined and its value is not 0xFF.
-// Quad   mode: If all DATA0 --> DATA3 are valid
-// Dual   mode: If DATA0 and DATA1 are valid while either DATA2 and/or DATA3 are invalid
-// Single mode: If only DATA0 is valid
 #if QSPI_FLASH_FILESYSTEM
-#define MICROPY_QSPI_DATA0                NRF_GPIO_PIN_MAP(0, 20)
-#define MICROPY_QSPI_DATA1                NRF_GPIO_PIN_MAP(0, 21)
-#define MICROPY_QSPI_DATA2                NRF_GPIO_PIN_MAP(0, 22)
-#define MICROPY_QSPI_DATA3                NRF_GPIO_PIN_MAP(0, 23)
+#define MICROPY_QSPI_DATA0                NRF_GPIO_PIN_MAP(0, 17)
+#define MICROPY_QSPI_DATA1                NRF_GPIO_PIN_MAP(0, 22)
+#define MICROPY_QSPI_DATA2                NRF_GPIO_PIN_MAP(0, 23)
+#define MICROPY_QSPI_DATA3                NRF_GPIO_PIN_MAP(0, 21)
 #define MICROPY_QSPI_SCK                  NRF_GPIO_PIN_MAP(0, 19)
-#define MICROPY_QSPI_CS NRF_GPIO_PIN_MAP(0, 17)
+#define MICROPY_QSPI_CS                   NRF_GPIO_PIN_MAP(0, 20)
 #endif
 
 #if SPI_FLASH_FILESYSTEM
-#define SPI_FLASH_MOSI_PIN &pin_P0_20
-#define SPI_FLASH_MISO_PIN &pin_P0_21
+#define SPI_FLASH_MOSI_PIN &pin_P0_17
+#define SPI_FLASH_MISO_PIN &pin_P0_22
 #define SPI_FLASH_SCK_PIN &pin_P0_19
-#define SPI_FLASH_CS_PIN &pin_P0_17
+#define SPI_FLASH_CS_PIN &pin_P0_20
 #endif
+
+#define CIRCUITPY_AUTORELOAD_DELAY_MS 500
+
+#define CIRCUITPY_INTERNAL_NVM_SIZE (4096)
+
+#define BOARD_FLASH_SIZE (FLASH_SIZE - 0x4000 - CIRCUITPY_INTERNAL_NVM_SIZE)
+
+#define BOARD_HAS_CRYSTAL 0
+
+#define DEFAULT_I2C_BUS_SCL         (&pin_P0_11)
+#define DEFAULT_I2C_BUS_SDA         (&pin_P0_12)
+
+#define DEFAULT_SPI_BUS_SCK         (&pin_P0_14)
+#define DEFAULT_SPI_BUS_MOSI        (&pin_P0_13)
+#define DEFAULT_SPI_BUS_MISO        (&pin_P0_15)
+
+#define DEFAULT_UART_BUS_RX         (&pin_P0_24)
+#define DEFAULT_UART_BUS_TX         (&pin_P0_25)
