50b451ae0c9f6c35961d18c4c0b7ec85153228b5
TG-Techie <TGTechie01@gmail.com>
2020-03-10 22:40:55 -0400

    add TG-Watch02A board def

--- ports/nrf/boards/TG-Watch02A/pins.c
+++ ports/nrf/boards/TG-Watch02A/pins.c
@@ -48,8 +48,6 @@ STATIC const mp_rom_map_elem_t board_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_UART), MP_ROM_PTR(&board_uart_obj) },
     { MP_ROM_QSTR(MP_QSTR_SPI), MP_ROM_PTR(&board_spi_obj) },
     { MP_ROM_QSTR(MP_QSTR_I2C), MP_ROM_PTR(&board_i2c_obj) },
-<<<<<<< HEAD
-
     /*TG-Watch02A specific pins*/
 
         //tft / display pins
@@ -74,7 +72,7 @@ STATIC const mp_rom_map_elem_t board_module_globals_table[] = {
 
         { MP_ROM_QSTR(MP_QSTR_ACCEL_INT1), MP_ROM_PTR(&pin_P1_11) },
         { MP_ROM_QSTR(MP_QSTR_ACCEL_INT2), MP_ROM_PTR(&pin_P1_10) },
-        
+
         { MP_ROM_QSTR(MP_QSTR_HAPTIC_INT), MP_ROM_PTR(&pin_P1_07) },
         { MP_ROM_QSTR(MP_QSTR_HAPTIC_ENABLE), MP_ROM_PTR(&pin_P1_06) },
 
@@ -82,8 +80,6 @@ STATIC const mp_rom_map_elem_t board_module_globals_table[] = {
         { MP_ROM_QSTR(MP_QSTR_SMC_RESET), MP_ROM_PTR(&pin_P0_08) },
         { MP_ROM_QSTR(MP_QSTR_BUTTON0), MP_ROM_PTR(&pin_P1_08) },
         { MP_ROM_QSTR(MP_QSTR_BUTTON1), MP_ROM_PTR(&pin_P1_09) },
-=======
->>>>>>> 7198056bc31ebae22337af119c07c7dca03e85d5
 };
 
 MP_DEFINE_CONST_DICT(board_module_globals, board_module_globals_table);
