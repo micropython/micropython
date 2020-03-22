ad004db662b383a9bf310601e13e896c99e11e30
Damien George <damien.p.george@gmail.com>
2020-03-22 23:17:17 +1100

    esp32: Enable and freeze uasyncio.

--- ports/esp32/mpconfigport.h
+++ ports/esp32/mpconfigport.h
@@ -125,6 +125,7 @@
 #define MICROPY_PY_THREAD_GIL_VM_DIVISOR    (32)
 
 // extended modules
+#define MICROPY_PY_UASYNCIO                 (1)
 #define MICROPY_PY_UCTYPES                  (1)
 #define MICROPY_PY_UZLIB                    (1)
 #define MICROPY_PY_UJSON                    (1)
