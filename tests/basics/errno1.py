a15d9fab3a9ed3c1578ba907e7b6bb57c710a843
David Lechner <david@pybricks.com>
2020-04-08 12:17:28 -0500

    py/objexcept: Handle OSError with 2 args.
    This allows `OSError`s with two arguments to also be pretty-printed.
    
    Signed-off-by: David Lechner <david@pybricks.com>

--- tests/basics/errno1.py
+++ tests/basics/errno1.py
@@ -12,6 +12,8 @@ print(type(uerrno.EIO))
 # check that errors are rendered in a nice way
 msg = str(OSError(uerrno.EIO))
 print(msg[:7], msg[-5:])
+msg = str(OSError(uerrno.EIO, "details"))
+print(msg[:7], msg[-14:])
 
 # check that unknown errno is still rendered
 print(str(OSError(9999)))
