49771b4d7f46a056579d3690977e772fa91f30b5
sabas1080 <s@electroniccats.com>
2020-04-09 14:27:51 -0500

    Update to v1.3

--- ports/atmel-samd/boards/nfc_copy_cat/mpconfigboard.h
+++ ports/atmel-samd/boards/nfc_copy_cat/mpconfigboard.h
@@ -1,22 +1,23 @@
 #define MICROPY_HW_BOARD_NAME "Electronic Cats NFC Copy Cat"
 #define MICROPY_HW_MCU_NAME "samd21e18"
 
+#define MICROPY_HW_LED_STATUS   (&pin_PA14)
+
 #define MICROPY_PORT_A        (0)
 #define MICROPY_PORT_B        (0)
 #define MICROPY_PORT_C        (0)
 
+#define SPI_FLASH_MOSI_PIN          &pin_PA08
+#define SPI_FLASH_MISO_PIN          &pin_PA11
+#define SPI_FLASH_SCK_PIN           &pin_PA09
+#define SPI_FLASH_CS_PIN            &pin_PA10
+
 // No microcontroller.nvm
 #define CIRCUITPY_INTERNAL_NVM_SIZE 0
-
-#define DEFAULT_I2C_BUS_SCL (&pin_PA08)
-#define DEFAULT_I2C_BUS_SDA (&pin_PA09)
 
 #define DEFAULT_SPI_BUS_SCK (&pin_PA17)
 #define DEFAULT_SPI_BUS_MOSI (&pin_PA16)
 #define DEFAULT_SPI_BUS_MISO (&pin_PA19)
-
-#define DEFAULT_UART_BUS_RX (&pin_PA01)
-#define DEFAULT_UART_BUS_TX (&pin_PA00)
 
 #define IGNORE_PIN_PA03     1
 #define IGNORE_PIN_PA20     1
