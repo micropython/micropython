43022d829491b47710c2c1d9689239c2ea7f2a8a
Radomir Dopieralski <openstack@sheep.art.pl>
2020-04-17 09:50:11 +0200

    Add copyrights to pewpew m4 and ugame board.c

--- ports/atmel-samd/boards/pewpew_m4/board.c
+++ ports/atmel-samd/boards/pewpew_m4/board.c
@@ -3,7 +3,8 @@
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2017 Scott Shawcroft for Adafruit Industries
+ * Copyright (c) 2017 Scott Shawcroft for Adafruit Industries, 2020 Radomir
+ * Dopieralski
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
