214648ca2b26d743958a89161900c1bb80dc13ce
Jim Mussared <jim.mussared@gmail.com>
2020-04-20 16:22:28 +1000

    nrf: Add openocd as a supported flasher.
    Tested with the Particle Debugger on a Xenon.

--- ports/nrf/boards/particle_xenon/mpconfigboard.mk
+++ ports/nrf/boards/particle_xenon/mpconfigboard.mk
@@ -5,3 +5,6 @@ SOFTDEV_VERSION = 6.1.1
 LD_FILES += boards/nrf52840_1M_256k.ld
 
 NRF_DEFINES += -DNRF52840_XXAA
+
+# Note: This requires openocd >0.10
+OPENOCD_TARGET ?= boards/$(BOARD)/nrf52-particle.cfg
