3d4e8527166c9c8d45e74dc5ad6678941ef528bb
Thorsten von Eicken <tve@voneicken.com>
2020-04-26 22:32:52 -0700

    partially completed SNTP client based on LwIP

--- docs/library/network.SNTP.rst
+++ docs/library/network.SNTP.rst
@@ -25,17 +25,18 @@ Returns the SNTP singleton object.
 Methods
 -------
 
-.. method:: SNTP.start(servers=["pool.ntp.org"], poll=True, slew=True)
+.. method:: SNTP.start(server=None, poll=3600, smooth=600)
 
     Starts the SNTP process to run in the background.
     The keyword parameters are:
-    * servers: a list of NTP servers to query, the max number is implementation
-      dependent.
-    * poll: whether to explicitly poll NTP servers (True) or passively listen
-      for NTP broadcasts
-    * slew: whether to gradually adjust the time using adjtime (True) or
-      step the time (False). (If the time difference is large the system
-      time may be stepped even if slew==True.)
+    * server: the hostname of the NTP server to query, a value of None causes the
+      server (if any) provided via DHCP to be used. If DHCP doesn't provide a server
+      then `status()` will be stuck in `SNTP.NOSRV`.
+    * poll: interval at which the SNTP server should be polled in seconds;
+      a value of zero switches to passive mode listening for NTP broadcasts.
+    * smooth: a time difference threshold in milliseconds below which the system
+      clock should be adjusted smoothly using ``time.adjtime()``. Above the threshold
+      the system clock is stepped.
 
 .. method:: SNTP.stop()
 
@@ -44,7 +45,13 @@ Methods
 
 .. method:: SNTP.status()
 
-    Returns the current clock synchronization status: (semantics not yet clear)
-    * SNTP.STOPPED
-    * SNTP.INSYNC
-    * SNTP.ADJUSTING
+    Returns the number of seconds since the last server response. If the
+    return value is negative it indicates one of the following error conditions:
+    * SNTP.STOPPED - SNTP is not running
+    * SNTP.NOIP - SNTP does not have an IP address, e.g. DNS lookup failed
+    * SNTP.NOSRV - No SNTP server has been provided by DHCP
+    * SNTP.UNREACHABLE - the SNTP server has not responded to the last eight requests
+
+    If the return value is positive the time may still not be truly in-synch if
+    smooth synchronization has been requested in `start()`. The clock adjustment
+    state can be queried via ``time.adjtime()``.
--- ports/esp32/modsntp.c
+++ ports/esp32/modsntp.c
@@ -33,33 +33,65 @@
 #include "py/obj.h"
 #include "py/runtime.h"
 
-STATIC mp_obj_t sntp_make_new(void) {
-}
-STATIC MP_DEFINE_CONST_FUN_OBJ_0(sntp_make_new_obj, sntp_make_new);
+// SNTP service singleton object
+typedef struct modsntp_obj_t {
+    mp_obj_base_t base;
+    uint32_t poll;   // polling frequency in seconds
+    uint32_t smooth; // smooth update threshold in milliseconds
+} modsntp_obj_t;
+STATIC mp_obj_t modsntp_obj = NULL;
 
-STATIC mp_obj_t sntp_start(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args) {
+STATIC mp_obj_t modsntp_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *all_args) {
+    mp_arg_check_num(n_args, n_kw, 0, 0, false);
+    // return singleton
+    if (modsntp_obj == NULL) {
+        modsntp_obj = m_new_obj(modsntp_obj_t);
+        modsntp_obj.poll = 0;
+        modsntp_obj.smooth = 0;
+    }
+    return modsntp_obj;
+}
+
+// modsntp_start starts the SNTP service by stopping it, confguring it, and starting it.
+// The LwIP SNTP service has a number of config knobs and the code here makes some assumptions...
+// Two in particular are that SNTP_SERVER_DNS is enabled and SNTP_MAX_SERVERS==1.
+// It would be easy to pass an array of servers into modsntp_start but that makes getting good
+// status out in the status() method much more complicated and in the end good status is
+// probably more useful 'cause the app can always restart SNTP with a different server if it
+// doesn't converge.
+STATIC mp_obj_t modsntp_start(size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args) {
+    enum { ARG_server, ARG_poll, ARG_smooth }
     const mp_arg_t allowed_args[] = {
-        { MP_QSTR_poll,  MP_ARG_BOOL, {.u_bool = 1} },
-        { MP_QSTR_slew,  MP_ARG_BOOL, {.u_bool = 1} },
-        { MP_QSTR_servers,  MP_ARG_OBJ, {.u_obj = mp_obj_new_str()} },
+        { MP_QSTR_server,  MP_ARG_OBJ, {.u_obj = mp_const_none} },
+        { MP_QSTR_poll,  MP_ARG_INT, {.u_int = 3600} },
+        { MP_QSTR_smooth,  MP_ARG_INT, {.u_int = 600} },
     };
     mp_arg_val_t args[MP_ARRAY_SIZE(allowed_args)];
     mp_arg_parse_all(n_args, pos_args, kw_args, MP_ARRAY_SIZE(allowed_args), allowed_args, args);
-}
-STATIC MP_DEFINE_CONST_FUN_OBJ_KW(sntp_start_obj, 0, sntp_start);
 
-STATIC mp_obj_t sntp_stop(void) {
-}
-STATIC MP_DEFINE_CONST_FUN_OBJ_0(sntp_stop_obj, sntp_stop);
+    sntp_stop();
 
-STATIC mp_obj_t sntp_status(void) {
+    sntp_setservername(0, mp_obj_str_get_str(servers[i]));
+    sntp_setoperatingmode(args[ARG_poll].u_bool ? SNTP_OPMODE_POLL : SNTP_OPMODE_LISTENONLY);
+    sntp_set_sync_mode(args[ARG_smooth].u_bool ? SNTP_SYNC_MODE_SMOOTH : SNTP_SYNC_MODE_IMMED);
+
+    sntp_init();
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_0(sntp_status_obj, sntp_status);
+STATIC MP_DEFINE_CONST_FUN_OBJ_KW(modsntp_start_obj, 0, modsntp_start);
+
+STATIC mp_obj_t modsntp_stop(void) {
+    sntp_stop();
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(modsntp_stop_obj, modsntp_stop);
+
+STATIC mp_obj_t modsntp_status(void) {
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(modsntp_status_obj, modsntp_status);
 
 STATIC const mp_rom_map_elem_t sntp_locals_dict_table[] = {
-    { MP_ROM_QSTR(MP_QSTR_start), MP_ROM_PTR(&start_obj) },
-    { MP_ROM_QSTR(MP_QSTR_stop), MP_ROM_PTR(&stop_obj) },
-    { MP_ROM_QSTR(MP_QSTR_status), MP_ROM_PTR(&status_obj) },
+    { MP_ROM_QSTR(MP_QSTR_start), MP_ROM_PTR(&modsntp_start_obj) },
+    { MP_ROM_QSTR(MP_QSTR_stop), MP_ROM_PTR(&modsntp_stop_obj) },
+    { MP_ROM_QSTR(MP_QSTR_status), MP_ROM_PTR(&modsntp_status_obj) },
     { MP_ROM_QSTR(MP_QSTR_STOPPED), MP_ROM_INT(0) },
     { MP_ROM_QSTR(MP_QSTR_INSYNC), MP_ROM_INT(1) },
     { MP_ROM_QSTR(MP_QSTR_ADJUSTING), MP_ROM_INT(2) },
@@ -69,6 +101,6 @@ STATIC MP_DEFINE_CONST_DICT(sntp_locals_dict, sntp_locals_dict_table);
 const mp_obj_type_t network_sntp_type = {
     { &mp_type_type },
     .name = MP_QSTR_SNTP,
-    .make_new = sntp_make_new,
+    .make_new = modsntp_make_new,
     .locals_dict = (mp_obj_t)&sntp_locals_dict,
 };
