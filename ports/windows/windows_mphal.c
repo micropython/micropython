a003ec4c819242ef84d5c58b16308a56f2594e0f
Yu-Ming Chang <yuming.chang@iot-hmi.com>
2020-05-20 15:22:54 +0800

    Fix code format.

--- ports/windows/windows_mphal.c
+++ ports/windows/windows_mphal.c
@@ -62,9 +62,9 @@ void mp_hal_stdio_mode_raw(void) {
     mode &= ~ENABLE_ECHO_INPUT;
     mode &= ~ENABLE_LINE_INPUT;
     mode &= ~ENABLE_PROCESSED_INPUT;
-#if MICROPY_HAL_HAS_VT100
+    #if MICROPY_HAL_HAS_VT100
     mode |= ENABLE_VIRTUAL_TERMINAL_INPUT;
-#endif
+    #endif
     SetConsoleMode(std_in, mode);
 }
 
@@ -193,13 +193,13 @@ STATIC int esc_seq_chr() {
 #endif
 
 int mp_hal_stdin_rx_chr(void) {
-#if MICROPY_HAL_HAS_VT100
+    #if MICROPY_HAL_HAS_VT100
     assure_stdin_handle();
+    BOOL read_ok;
     DWORD num_read;
     char inchar;
     LPVOID buf = &inchar;
     for (;;) {
-        BOOL read_ok;
         read_ok = ReadFile(std_in, buf, 1, &num_read, NULL);
         if (!read_ok || !num_read) {
             continue;
@@ -208,7 +208,7 @@ int mp_hal_stdin_rx_chr(void) {
             return inchar;
         }
     }
-#else
+    #else
     // currently processing escape seq?
     const int ret = esc_seq_chr();
     if (ret) {
@@ -241,7 +241,7 @@ int mp_hal_stdin_rx_chr(void) {
             return c;
         }
     }
-#endif
+    #endif
 }
 
 void mp_hal_stdout_tx_strn(const char *str, size_t len) {
