d65639b18e02cd1de4fa584c394236084228c805
ilyamordasov <carlogulliani@gmail.com>
2020-06-05 13:48:33 +0300

    Minor changes

--- ports/esp32/modules/_boot.py
+++ ports/esp32/modules/_boot.py
@@ -1,24 +1,12 @@
 import gc
 import uos
 from flashbdev import bdev
-from ota_updater import OTAUpdater
 
-WIFI_SSID_PASSWORD = 'Smart_box-142','natalia31081980'
+try:
+    if bdev:
+        uos.mount(bdev, '/')
+except OSError:
+    import inisetup
+    inisetup.setup()
 
-def download_and_install_update_if_available():
-    ota_updater = OTAUpdater('https://github.com/ilyamordasov/micropython/tree/esp32-bluetooth')
-    ota_updater.download_and_install_update_if_available(_,WIFI_SSID_PASSWORD)
-    
-def boot():
-    download_and_install_update_if_available()
-    
-    try:
-        if bdev:
-            uos.mount(bdev, '/')
-    except OSError:
-        import inisetup
-        inisetup.setup()
-
-    gc.collect()
-
-boot()
+gc.collect()
