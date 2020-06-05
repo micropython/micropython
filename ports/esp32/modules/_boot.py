2764d5307de8b408fe26ce63fca91e46df489c34
ilyamordasov <carlogulliani@gmail.com>
2020-06-05 15:12:29 +0300

    Minor changes

--- ports/esp32/modules/_boot.py
+++ ports/esp32/modules/_boot.py
@@ -4,7 +4,7 @@ from flashbdev import bdev
 from ota_updater import OTAUpdater
 
 def download_and_install_update_if_available():
-    ota_updater = OTAUpdater('https://github.com/ilyamordasov/micropython/tree/esp32-bluetooth')
+    ota_updater = OTAUpdater('https://github.com/ilyamordasov/micropython')
     ota_updater.download_and_install_update_if_available('Smart_box-142','natalia31081980')
     
 def boot():
