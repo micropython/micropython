b90ee4567d6efd3676e2cb1593e0efe54dadb764
Albort Xue <yao.xue@nxp.com>
2020-06-07 11:20:19 +0800

    ports/mimxrt/boards/MIMXRT1060_EVK: Enabled led class for MIMXRT1060_EVK board.
    - Enabled led class for MIMXRT1060_EVK.

--- ports/mimxrt/boards/MIMXRT1060_EVK/mpconfigboard.h
+++ ports/mimxrt/boards/MIMXRT1060_EVK/mpconfigboard.h
@@ -3,6 +3,7 @@
 
 #define BOARD_FLASH_SIZE (8 * 1024 * 1024)
 
-#define MICROPY_HW_LED_PINMUX  IOMUXC_GPIO_AD_B0_09_GPIO1_IO09
-#define MICROPY_HW_LED_PORT    GPIO1
-#define MICROPY_HW_LED_PIN     9
+// MIMXRT1060_EVK has 1 user LED
+#define MICROPY_HW_LED1_PIN (GPIO_AD_B0_09)
+#define MICROPY_HW_LED_ON(pin) (mp_hal_pin_low(pin))
+#define MICROPY_HW_LED_OFF(pin) (mp_hal_pin_high(pin))
