42fbbe14144b658521ecd554d6faf0aa4ab83113
Nicko van Someren <nicko@nicko.org>
2020-06-14 19:05:03 +0000

    Added extra tests for failure cases

--- tests/float/custom_cast.py
+++ tests/float/custom_cast.py
@@ -1,17 +1,21 @@
+# Test a class that behaves correctly
+
 class Test1:
     def __float__(self):
         return 123.4
     def __complex__(self):
         return 123.4j
 
+print(float(Test1()))
+print(complex(Test1()))
+
+# Test a class that returns the wrong type
+
 class Test2:
     def _float__(self):
         return "Not a float"
     def __complex__(self):
         return "Not very complex"
-
-print(float(Test1()))
-print(complex(Test1()))
 
 try:
     print(float(Test2()))
@@ -23,3 +27,17 @@ try:
 except TypeError:
     print("TypeError")
 
+# Test a class that doesn't implement the conversion methods
+
+class Test3:
+    pass
+
+try:
+    print(float(Test3()))
+except TypeError:
+    print("TypeError")
+
+try:
+    print(complex(Test3()))
+except TypeError:
+    print("TypeError")
