38b0fd13694a3f6ba6e844bf55232adac3270144
Nicko van Someren <nicko@nicko.org>
2020-06-14 21:14:47 +0000

    Apparently you can never have enough blank lines in test cases

--- tests/float/custom_cast.py
+++ tests/float/custom_cast.py
@@ -1,21 +1,27 @@
 # Test a class that behaves correctly
 
+
 class Test1:
     def __float__(self):
         return 123.4
+
     def __complex__(self):
         return 123.4j
+
 
 print(float(Test1()))
 print(complex(Test1()))
 
 # Test a class that returns the wrong type
 
+
 class Test2:
     def __float__(self):
         return "Not a float"
+
     def __complex__(self):
         return "Not very complex"
+
 
 try:
     print(float(Test2()))
@@ -29,8 +35,10 @@ except TypeError:
 
 # Test a class that doesn't implement the conversion methods
 
+
 class Test3:
     pass
+
 
 try:
     print(float(Test3()))
