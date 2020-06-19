3c354a76e9726d40873d6082cc1df85dd10e0a64
Damien George <damien@micropython.org>
2020-06-19 11:07:15 +1000

    py/map: Convert map implementation to preserve insertion order.
    TODO:
    - make it optional at compile-time
    - make OrderedDict use this new implementation
    - implement more efficient deletion
    - profile performance and memory use
    
    Signed-off-by: Damien George <damien@micropython.org>

--- py/obj.h
+++ py/obj.h
@@ -431,9 +431,11 @@ static inline bool mp_map_slot_is_filled(const mp_map_t *map, size_t pos) {
 
 void mp_map_init(mp_map_t *map, size_t n);
 void mp_map_init_fixed_table(mp_map_t *map, size_t n, const mp_obj_t *table);
+void mp_map_init_copy(mp_map_t *map, const mp_map_t *src);
 mp_map_t *mp_map_new(size_t n);
 void mp_map_deinit(mp_map_t *map);
 void mp_map_free(mp_map_t *map);
+size_t mp_map_num_filled_slots(mp_map_t *map);
 mp_map_elem_t *mp_map_lookup(mp_map_t *map, mp_obj_t index, mp_map_lookup_kind_t lookup_kind);
 void mp_map_clear(mp_map_t *map);
 void mp_map_dump(mp_map_t *map);
