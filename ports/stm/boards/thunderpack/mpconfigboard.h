017c188ab5894723b4da004f01b45f84a5b3441c
jgillick <j_gillick@yahoo.com>
2020-06-25 16:31:28 -0700

    Move NVM block to increase program size.

--- ports/stm/boards/thunderpack/mpconfigboard.h
+++ ports/stm/boards/thunderpack/mpconfigboard.h
@@ -28,16 +28,16 @@
 
 // Non-volatile memory config
 #define CIRCUITPY_INTERNAL_NVM_SIZE        (0x4000)
-#define CIRCUITPY_INTERNAL_NVM_START_ADDR  (0x0800C000)
-#define CIRCUITPY_INTERNAL_NVM_SECTOR      FLASH_SECTOR_3
+#define CIRCUITPY_INTERNAL_NVM_START_ADDR  (0x08010000)
+#define CIRCUITPY_INTERNAL_NVM_SECTOR      FLASH_SECTOR_4
 
 // Putting the entire flash sector in the NVM byte array buffer
 // would take up too much RAM. This limits how much of the sector we use.
-#define NVM_BYTEARRAY_BUFFER_SIZE           512
+#define NVM_BYTEARRAY_BUFFER_SIZE   512
 
 // Flash config
-#define FLASH_SIZE                         (0x80000)
-#define FLASH_PAGE_SIZE                    (0x4000)
+#define FLASH_SIZE                  (0x80000)
+#define FLASH_PAGE_SIZE             (0x4000)
 #define BOARD_FLASH_SIZE            (FLASH_SIZE - CIRCUITPY_INTERNAL_NVM_SIZE- 0x2000 - 0xC000)
 
 #define HSE_VALUE                   ((uint32_t)24000000U)
@@ -47,7 +47,7 @@
 #define BOARD_HAS_LOW_SPEED_CRYSTAL (0)
 
 // Status LEDs
-#define MICROPY_HW_LED_STATUS  (&pin_PA02)
+#define MICROPY_HW_LED_STATUS       (&pin_PA02)
 
-#define DEFAULT_I2C_BUS_SCL (&pin_PB06)
-#define DEFAULT_I2C_BUS_SDA (&pin_PB07)
+#define DEFAULT_I2C_BUS_SCL         (&pin_PB06)
+#define DEFAULT_I2C_BUS_SDA         (&pin_PB07)
