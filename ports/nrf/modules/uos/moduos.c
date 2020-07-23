6f877762a1592bdf179f14b3019af2045741bdd0
Glenn Ruben Bakke <glennbakke@gmail.com>
2020-07-23 21:10:49 +0200

    nrf/modules/uos: Add support for LFS1 and LFS2.

--- ports/nrf/modules/uos/moduos.c
+++ ports/nrf/modules/uos/moduos.c
@@ -36,6 +36,7 @@
 #include "modules/uos/microbitfs.h"
 #include "extmod/vfs.h"
 #include "extmod/vfs_fat.h"
+#include "extmod/vfs_lfs.h"
 #include "genhdr/mpversion.h"
 #include "uart.h"
 
@@ -178,6 +179,12 @@ STATIC const mp_rom_map_elem_t os_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_umount), MP_ROM_PTR(&mp_vfs_umount_obj) },
 #if MICROPY_VFS_FAT
     { MP_ROM_QSTR(MP_QSTR_VfsFat), MP_ROM_PTR(&mp_fat_vfs_type) },
+#endif
+#if MICROPY_VFS_LFS1
+    { MP_ROM_QSTR(MP_QSTR_VfsLfs1), MP_ROM_PTR(&mp_type_vfs_lfs1) },
+#endif
+#if MICROPY_VFS_LFS2
+    { MP_ROM_QSTR(MP_QSTR_VfsLfs2), MP_ROM_PTR(&mp_type_vfs_lfs2) },
 #endif
 #endif
 };
