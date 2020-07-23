55d4321c3eb1a99b7f135ca930878d0ce97620e6
Glenn Ruben Bakke <glennbakke@gmail.com>
2020-07-23 23:37:25 +0200

    nrf/boards: Enable MICROPY_VFS_LFS2 for all target boards.
    Enable LittleFS v2 for all targets, except nrf51 targets when SoftDevice is
    present.

--- ports/nrf/boards/microbit/mpconfigboard.mk
+++ ports/nrf/boards/microbit/mpconfigboard.mk
@@ -6,6 +6,8 @@ SOFTDEV_VERSION = 8.0.0
 ifneq ($(SD),)
 LD_FILES += boards/microbit/custom_nrf51822_s110_microbit.ld
 FROZEN_MANIFEST ?=
+else
+MICROPY_VFS_LFS2 = 1
 endif
 
 LD_FILES += boards/nrf51x22_256k_16k.ld
