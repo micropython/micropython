92899354d96a5b5463f3002b2476a73c11ebe626
Damien George <damien@micropython.org>
2020-07-29 01:01:31 +1000

    unix/fatfs_port: Implement get_fattime.
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/unix/fatfs_port.c
+++ ports/unix/fatfs_port.c
@@ -1,5 +1,13 @@
+#include <time.h>
 #include "lib/oofatfs/ff.h"
 
 DWORD get_fattime(void) {
-    return 0;
+    time_t now = time(NULL);
+    struct tm *tm = localtime(&now);
+    return ((1900 + tm->tm_year - 1980) << 25)
+           | (tm->tm_mon << 21)
+           | (tm->tm_mday << 16)
+           | (tm->tm_hour << 11)
+           | (tm->tm_min << 5)
+           | (tm->tm_sec / 2);
 }
