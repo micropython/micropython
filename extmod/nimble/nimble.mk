94737fbd9f8e3f14bf129d66b59bdf02a1719b34
Andrew Leech <andrew.leech@planetinnovation.com.au>
2020-08-06 05:23:48 +1000

    bluetooth: Add/enable bluetooth logging if MICROPY_PY_BLUETOOTH_DIAGNOSTIC_LOGGING is set. Expects to be set from make / mpconfigboard.mk MICROPY_PY_BLUETOOTH_DIAGNOSTIC_LOGGING = 1  # ble stack logging MICROPY_PY_BLUETOOTH_DIAGNOSTIC_LOGGING = 2  # ble stack with hci packets
    Signed-off-by: Andrew Leech <andrew.leech@planetinnovation.com.au>

--- extmod/nimble/nimble.mk
+++ extmod/nimble/nimble.mk
@@ -8,6 +8,9 @@ NIMBLE_EXTMOD_DIR = $(EXTMOD_DIR)/nimble
 SRC_EXTMOD_C += $(NIMBLE_EXTMOD_DIR)/modbluetooth_nimble.c
 
 CFLAGS_EXTMOD += -DMICROPY_BLUETOOTH_NIMBLE=1
+ifneq ("$(MICROPY_PY_BLUETOOTH_DIAGNOSTIC_LOGGING)", "")
+CFLAGS_EXTMOD += -DMICROPY_PY_BLUETOOTH_DIAGNOSTIC_LOGGING=$(MICROPY_PY_BLUETOOTH_DIAGNOSTIC_LOGGING)
+endif
 
 # Use NimBLE from the submodule in lib/mynewt-nimble by default,
 # allowing a port to use their own system version (e.g. ESP32).
@@ -34,74 +37,74 @@ CFLAGS_EXTMOD += -DMICROPY_PY_BLUETOOTH_ENABLE_PAIRING_BONDING=1
 NIMBLE_LIB_DIR = lib/mynewt-nimble
 
 SRC_THIRDPARTY_C += $(addprefix $(NIMBLE_LIB_DIR)/, \
-	$(addprefix ext/tinycrypt/src/, \
-		aes_encrypt.c \
-		cmac_mode.c \
-		ecc.c \
-		ecc_dh.c \
-		utils.c \
-		) \
-	nimble/host/services/gap/src/ble_svc_gap.c \
-	nimble/host/services/gatt/src/ble_svc_gatt.c \
-	$(addprefix nimble/host/src/, \
-		ble_att.c \
-		ble_att_clt.c \
-		ble_att_cmd.c \
-		ble_att_svr.c \
-		ble_eddystone.c \
-		ble_gap.c \
-		ble_gattc.c \
-		ble_gatts.c \
-		ble_hs_adv.c \
-		ble_hs_atomic.c \
-		ble_hs.c \
-		ble_hs_cfg.c \
-		ble_hs_conn.c \
-		ble_hs_flow.c \
-		ble_hs_hci.c \
-		ble_hs_hci_cmd.c \
-		ble_hs_hci_evt.c \
-		ble_hs_hci_util.c \
-		ble_hs_id.c \
-		ble_hs_log.c \
-		ble_hs_mbuf.c \
-		ble_hs_misc.c \
-		ble_hs_mqueue.c \
-		ble_hs_pvcy.c \
-		ble_hs_startup.c \
-		ble_hs_stop.c \
-		ble_ibeacon.c \
-		ble_l2cap.c \
-		ble_l2cap_coc.c \
-		ble_l2cap_sig.c \
-		ble_l2cap_sig_cmd.c \
-		ble_monitor.c \
-		ble_sm_alg.c \
-		ble_sm.c \
-		ble_sm_cmd.c \
-		ble_sm_lgcy.c \
-		ble_sm_sc.c \
-		ble_store.c \
-		ble_store_util.c \
-		ble_uuid.c \
-		) \
-	nimble/host/util/src/addr.c \
-	nimble/transport/uart/src/ble_hci_uart.c \
-	$(addprefix porting/nimble/src/, \
-		endian.c \
-		mem.c \
-		nimble_port.c \
-		os_mbuf.c \
-		os_mempool.c \
-		os_msys_init.c \
-		) \
-	)
-	# nimble/host/store/ram/src/ble_store_ram.c \
+		$(addprefix ext/tinycrypt/src/, \
+			aes_encrypt.c \
+			cmac_mode.c \
+			ecc.c \
+			ecc_dh.c \
+			utils.c \
+			) \
+		nimble/host/services/gap/src/ble_svc_gap.c \
+		nimble/host/services/gatt/src/ble_svc_gatt.c \
+		$(addprefix nimble/host/src/, \
+			ble_att.c \
+			ble_att_clt.c \
+			ble_att_cmd.c \
+			ble_att_svr.c \
+			ble_eddystone.c \
+			ble_gap.c \
+			ble_gattc.c \
+			ble_gatts.c \
+			ble_hs_adv.c \
+			ble_hs_atomic.c \
+			ble_hs.c \
+			ble_hs_cfg.c \
+			ble_hs_conn.c \
+			ble_hs_flow.c \
+			ble_hs_hci.c \
+			ble_hs_hci_cmd.c \
+			ble_hs_hci_evt.c \
+			ble_hs_hci_util.c \
+			ble_hs_id.c \
+			ble_hs_log.c \
+			ble_hs_mbuf.c \
+			ble_hs_misc.c \
+			ble_hs_mqueue.c \
+			ble_hs_pvcy.c \
+			ble_hs_startup.c \
+			ble_hs_stop.c \
+			ble_ibeacon.c \
+			ble_l2cap.c \
+			ble_l2cap_coc.c \
+			ble_l2cap_sig.c \
+			ble_l2cap_sig_cmd.c \
+			ble_monitor.c \
+			ble_sm_alg.c \
+			ble_sm.c \
+			ble_sm_cmd.c \
+			ble_sm_lgcy.c \
+			ble_sm_sc.c \
+			ble_store.c \
+			ble_store_util.c \
+			ble_uuid.c \
+			) \
+		nimble/host/util/src/addr.c \
+		nimble/transport/uart/src/ble_hci_uart.c \
+		$(addprefix porting/nimble/src/, \
+			endian.c \
+			mem.c \
+			nimble_port.c \
+			os_mbuf.c \
+			os_mempool.c \
+			os_msys_init.c \
+			) \
+		)
+		# nimble/host/store/ram/src/ble_store_ram.c \
 
 SRC_THIRDPARTY_C += $(addprefix $(NIMBLE_EXTMOD_DIR)/, \
-	nimble/nimble_npl_os.c \
-	hal/hal_uart.c \
-	)
+		nimble/nimble_npl_os.c \
+		hal/hal_uart.c \
+		)
 
 INC += -I$(TOP)/$(NIMBLE_EXTMOD_DIR)
 INC += -I$(TOP)/$(NIMBLE_LIB_DIR)
