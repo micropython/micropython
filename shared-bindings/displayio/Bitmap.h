64c9baa6aa6203e21ff0420ce5a9889f44422334
Margaret Matocha <ksmatocha@gmail.com>
2020-08-07 15:46:00 -0500

    Added bitmap.insert function for slice copy into a bitmap from another bitmap

--- shared-bindings/displayio/Bitmap.h
+++ shared-bindings/displayio/Bitmap.h
@@ -40,6 +40,10 @@ uint16_t common_hal_displayio_bitmap_get_height(displayio_bitmap_t *self);
 uint16_t common_hal_displayio_bitmap_get_width(displayio_bitmap_t *self);
 uint32_t common_hal_displayio_bitmap_get_bits_per_value(displayio_bitmap_t *self);
 void common_hal_displayio_bitmap_set_pixel(displayio_bitmap_t *bitmap, int16_t x, int16_t y, uint32_t value);
+void common_hal_displayio_bitmap_insert(displayio_bitmap_t *self, int16_t x, int16_t y, displayio_bitmap_t *source,  
+                                        int16_t x1, int16_t y1, int16_t x2, int16_t y2);
+// New selective bitmap copy function "insert": KMatocha 8/7/2020
+
 uint32_t common_hal_displayio_bitmap_get_pixel(displayio_bitmap_t *bitmap, int16_t x, int16_t y);
 void common_hal_displayio_bitmap_fill(displayio_bitmap_t *bitmap, uint32_t value);
 
