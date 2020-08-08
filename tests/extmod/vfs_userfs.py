811cee3adfc16b56a096e9371b8c3b29ee270d35
Damien George <damien@micropython.org>
2020-08-08 15:39:56 +1000

    extmod/vfs_reader: Fix mp_reader_new_file to open file in "rb" mode.
    mp_reader_new_file() is used to read in files for importing, either .py or
    .mpy files, for the lexer and persistent code loader respectively.  In both
    cases the file should be opened in raw bytes mode: the lexer handles
    unicode characters itself, and .mpy files contain 8-bit bytes by nature.
    
    Before this commit importing was working correctly because, although the
    file was opened in text mode, all native filesystem implementations (POSIX,
    FAT, LFS) would access the file in raw bytes mode via mp_stream_rw()
    calling mp_stream_p_t.read().  So it was only an issue for non-native
    filesystems, such as those implemented in Python.  For Python-based
    filesystem implementations, a call to mp_stream_rw() would go via IOBase
    and then to readinto() at the Python level, and readinto() is only defined
    on files opened in raw bytes mode.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- tests/extmod/vfs_userfs.py
+++ tests/extmod/vfs_userfs.py
@@ -16,14 +16,20 @@ except (ImportError, AttributeError):
 
 
 class UserFile(uio.IOBase):
-    def __init__(self, data):
+    def __init__(self, mode, data):
+        assert isinstance(data, bytes)
+        self.is_text = mode.find("b") == -1
         self.data = data
         self.pos = 0
 
     def read(self):
-        return self.data
+        if self.is_text:
+            return str(self.data, "utf8")
+        else:
+            return self.data
 
     def readinto(self, buf):
+        assert not self.is_text
         n = 0
         while n < len(buf) and self.pos < len(self.data):
             buf[n] = self.data[self.pos]
@@ -54,12 +60,12 @@ class UserFS:
 
     def open(self, path, mode):
         print("open", path, mode)
-        return UserFile(self.files[path])
+        return UserFile(mode, self.files[path])
 
 
 # create and mount a user filesystem
 user_files = {
-    "/data.txt": b"some data in a text file\n",
+    "/data.txt": b"some data in a text file",
     "/usermod1.py": b"print('in usermod1')\nimport usermod2",
     "/usermod2.py": b"print('in usermod2')",
 }
