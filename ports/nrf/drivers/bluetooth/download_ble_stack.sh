a6ee4957ea3025764dd93d07d53f42d397406e1b
Nguyen Hoan Hoang <hnhoan@i-syst.com>
2020-08-22 02:26:34 -0400

    Update Softdevice S132 & S140 to version 7.2.0

--- ports/nrf/drivers/bluetooth/download_ble_stack.sh
+++ ports/nrf/drivers/bluetooth/download_ble_stack.sh
@@ -19,41 +19,35 @@ function download_s110_nrf51_8_0_0
     cd -
 }
 
-function download_s132_nrf52_6_1_1
+function download_s132_nrf52_7_2_0
 {
     echo ""
     echo "####################################"
-    echo "### Downloading s132_nrf52_6.1.1 ###"
+    echo "### Downloading s132_nrf52_7.2.0 ###"
     echo "####################################"
     echo ""
 
-    mkdir -p $1/s132_nrf52_6.1.1
-    cd $1/s132_nrf52_6.1.1
-    wget --post-data="fileName=DeviceDownload&ids=3AB3E86666FE4361A4A3B7E0D1CBB9B9" https://www.nordicsemi.com/api/sitecore/Products/MedialibraryZipDownload2
-    mv MedialibraryZipDownload2 temp.zip
-    unzip -u temp.zip
-    unzip -u s132nrf52611.zip
-    rm s132nrf52611.zip
-    rm temp.zip
+    mkdir -p $1/s132_nrf52_7.2.0
+    cd $1/s132_nrf52_7.2.0
+    wget https://www.nordicsemi.com/-/media/Software-and-other-downloads/SoftDevices/S132/s132nrf52720.zip
+    unzip -u s132nrf52720.zip
+    rm s132nrf52720.zip
     cd -
 }
 
-function download_s140_nrf52_6_1_1
+function download_s140_nrf52_7_2_0
 {
     echo ""
     echo "####################################"
-    echo "### Downloading s140_nrf52_6.1.1 ###"
+    echo "### Downloading s140_nrf52_7.2.0 ###"
     echo "####################################"
     echo ""
 
-    mkdir -p $1/s140_nrf52_6.1.1
-    cd $1/s140_nrf52_6.1.1
-    wget --post-data="fileName=DeviceDownload&ids=CE89BA7633C540AFA48AB88E934DBF05" https://www.nordicsemi.com/api/sitecore/Products/MedialibraryZipDownload2
-    mv MedialibraryZipDownload2 temp.zip
-    unzip -u temp.zip
-    unzip -u s140nrf52611.zip
-    rm s140nrf52611.zip
-    rm temp.zip
+    mkdir -p $1/s140_nrf52_7.2.0
+    cd $1/s140_nrf52_7.2.0
+    wget https://www.nordicsemi.com/-/media/Software-and-other-downloads/SoftDevices/S140/s140nrf52720.zip
+    unzip -u s140nrf52720.zip
+    rm s140nrf52720.zip
     cd -
 }
 
@@ -62,16 +56,16 @@ SCRIPT_DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 if [ $# -eq 0 ]; then 
     echo "No Bluetooth LE stack defined, downloading all."
     download_s110_nrf51_8_0_0 ${SCRIPT_DIR}
-    download_s132_nrf52_6_1_1 ${SCRIPT_DIR}
-    download_s140_nrf52_6_1_1 ${SCRIPT_DIR}
+    download_s132_nrf52_7_2_0 ${SCRIPT_DIR}
+    download_s140_nrf52_7_2_0 ${SCRIPT_DIR}
 else 
     case $1 in
         "s110_nrf51" )
             download_s110_nrf51_8_0_0 ${SCRIPT_DIR} ;;
         "s132_nrf52_6_1_1" )
-            download_s132_nrf52_6_1_1 ${SCRIPT_DIR} ;;
-        "s140_nrf52_6_1_1" )
-            download_s140_nrf52_6_1_1 ${SCRIPT_DIR} ;;
+            download_s132_nrf52_7_2_0 ${SCRIPT_DIR} ;;
+        "s140_nrf52_7_2_0" )
+            download_s140_nrf52_7_2_0 ${SCRIPT_DIR} ;;
     esac
 fi
 
