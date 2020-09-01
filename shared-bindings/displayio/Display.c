c6529daac576aa3492abef2073b7e597c3ffee5d
Kevin Matocha <kmatocha@icloud.com>
2020-09-01 14:44:32 -0500

    Added some backticks to clarify the docs

--- shared-bindings/displayio/Display.c
+++ shared-bindings/displayio/Display.c
@@ -227,11 +227,11 @@ MP_DEFINE_CONST_FUN_OBJ_2(displayio_display_show_obj, displayio_display_obj_show
 //|         When auto refresh is on, updates the display immediately. (The display will also update
 //|         without calls to this.)
 //|
-//|         When auto refresh is off, refresh() or refresh(target_frames_per_second=None) will update
+//|         When auto refresh is off, `refresh()` or `refresh(target_frames_per_second=None)` will update
 //|         the display immediately.
 //|
 //|         :param int target_frames_per_second: How many times a second `refresh` should be called and the screen updated.
-//|             Set to None for immediate refresh.
+//|             Set to `None` for immediate refresh.
 //|         :param int minimum_frames_per_second: The minimum number of times the screen should be updated per second."""
 //|         ...
 //|
