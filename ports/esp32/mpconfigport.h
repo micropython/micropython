50efce81740901fee6f5191db36a48ada13ef6df
Jim Mussared <jim.mussared@gmail.com>
2020-09-10 11:31:01 +1000

    esp32/mpconfigport.h: Remove duplicate uhashlib registration.

--- ports/esp32/mpconfigport.h
+++ ports/esp32/mpconfigport.h
@@ -200,7 +200,6 @@ extern const struct _mp_obj_module_t mp_module_onewire;
     { MP_OBJ_NEW_QSTR(MP_QSTR_machine), (mp_obj_t)&mp_module_machine }, \
     { MP_OBJ_NEW_QSTR(MP_QSTR_network), (mp_obj_t)&mp_module_network }, \
     { MP_OBJ_NEW_QSTR(MP_QSTR__onewire), (mp_obj_t)&mp_module_onewire }, \
-    { MP_OBJ_NEW_QSTR(MP_QSTR_uhashlib), (mp_obj_t)&mp_module_uhashlib }, \
 
 #define MP_STATE_PORT MP_STATE_VM
 
