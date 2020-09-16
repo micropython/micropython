81ee8745319a4e2802db55eb2dae5adc8fe47b35
Brian Dean <bsd@bdmicro.com>
2020-09-16 16:40:04 -0400

    pins.c: fix trailing whitespace

--- ports/atmel-samd/boards/bdmicro_vina_d21/pins.c
+++ ports/atmel-samd/boards/bdmicro_vina_d21/pins.c
@@ -36,4 +36,3 @@ STATIC const mp_rom_map_elem_t board_global_dict_table[] = {
     { MP_ROM_QSTR(MP_QSTR_UART), MP_ROM_PTR(&board_uart_obj) },
 };
 MP_DEFINE_CONST_DICT(board_module_globals, board_global_dict_table);
-
