ad3d96dd3fd076aebdc00d159daa4b141582c244
Max <sonthaya.nt@hotmail.com>
2020-09-18 01:53:17 +0700

    Fixed servo

--- ports/esp32/boards/KidBright32/modules/servo.py
+++ ports/esp32/boards/KidBright32/modules/servo.py
@@ -2,8 +2,8 @@
 
 from machine import Pin, PWM
 
-SV1 = PWM(Pin(15), freq=50)
-SV2 = PWM(Pin(17), freq=50)
+SV1 = Pin(15)
+SV2 = Pin(17)
 
-def angle(n, angle):
-    n.duty(int(25.57 + ((angle / 180.0) * 102.3)))
+def angle(pin, angle):
+    PWM(pin, freq=50).duty(int(25.57 + ((angle / 180.0) * 102.3)))
