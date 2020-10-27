27827a4fef74dc66996ecb670f01ad5d58500899
Gaetan <gaetan.noel@viacesi.fr>
2020-10-28 00:34:07 +0100

    Fix EOF error modifié :         mpconfigboard.mk modifié :         pins.c

--- ports/nrf/boards/holyiot_nrf52840/pins.c
+++ ports/nrf/boards/holyiot_nrf52840/pins.c
@@ -43,7 +43,6 @@ STATIC const mp_rom_map_elem_t board_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_P0_13), MP_ROM_PTR(&pin_P0_13) },
     { MP_ROM_QSTR(MP_QSTR_P0_16), MP_ROM_PTR(&pin_P0_16) },
 
-
     { MP_ROM_QSTR(MP_QSTR_P0_07), MP_ROM_PTR(&pin_P0_07) },
     { MP_ROM_QSTR(MP_QSTR_P1_09), MP_ROM_PTR(&pin_P1_09) },
     { MP_ROM_QSTR(MP_QSTR_P0_12), MP_ROM_PTR(&pin_P0_12) },
@@ -54,6 +53,5 @@ STATIC const mp_rom_map_elem_t board_module_globals_table[] = {
     // RESET { MP_ROM_QSTR(MP_QSTR_P0_18), MP_ROM_PTR(&pin_P0_18) },
 
 };
-
 
 MP_DEFINE_CONST_DICT(board_module_globals, board_module_globals_table);
