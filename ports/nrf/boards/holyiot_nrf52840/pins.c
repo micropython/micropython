9b911b2211715e9da7ae55b691f54fe2f33a4486
Gaetan <gaetan.noel@viacesi.fr>
2020-10-28 00:38:07 +0100

    modifié :         pins.c

--- ports/nrf/boards/holyiot_nrf52840/pins.c
+++ ports/nrf/boards/holyiot_nrf52840/pins.c
@@ -50,8 +50,12 @@ STATIC const mp_rom_map_elem_t board_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_P0_21), MP_ROM_PTR(&pin_P0_21) },
     { MP_ROM_QSTR(MP_QSTR_P0_19), MP_ROM_PTR(&pin_P0_19) },
     
-    // RESET { MP_ROM_QSTR(MP_QSTR_P0_18), MP_ROM_PTR(&pin_P0_18) },
+    // RESET { MP_ROM_QSTR(MP_QSTR_P0_18), MP_ROM_PTR(&pin_P0_18) }
 
 };
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> d0426b3438a934257c5351f7eb2d9cdd79881c03
 MP_DEFINE_CONST_DICT(board_module_globals, board_module_globals_table);
