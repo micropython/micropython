ca935c0dafba767f8eae681664972d0f6a0c5507
ITACA Innovation S.R.L <40298126+ITACAInnovation@users.noreply.github.com>
2020-11-03 21:22:19 +0100

    Update pins.c
    Changed builtin to standard

--- ports/atmel-samd/boards/uchip/pins.c
+++ ports/atmel-samd/boards/uchip/pins.c
@@ -1,7 +1,7 @@
 #include "shared-bindings/board/__init__.h"
 
 STATIC const mp_rom_map_elem_t board_global_dict_table[] = {
-    { MP_ROM_QSTR(MP_QSTR_LED_BUILTIN), MP_ROM_PTR(&pin_PA07) },
+    { MP_ROM_QSTR(MP_QSTR_LED), MP_ROM_PTR(&pin_PA07) },
     { MP_ROM_QSTR(MP_QSTR_BOOST_EN), MP_ROM_PTR(&pin_PA14) },
     { MP_ROM_QSTR(MP_QSTR_VEXT_SELECT), MP_ROM_PTR(&pin_PA15) },
     { MP_ROM_QSTR(MP_QSTR_A0), MP_ROM_PTR(&pin_PA06) },
