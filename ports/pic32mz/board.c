55d2f945ab05850c91c8fdc3e3a28569c0e6623f
abdousam <abdou.sam7@gmail.com>
2020-11-04 13:20:35 +0100

    ports/pic32mz/board.* Replace uart and gpio setting code, by routines from gpio.c and uart.c

--- ports/pic32mz/board.c
+++ ports/pic32mz/board.c
@@ -1,94 +1,50 @@
 #include "board.h"
 #include "sysclk.h"
 #include "interrupt.h"
-
-#include <xc.h>
+#include "gpio.h"
+#include "uart.h"
 
 void mpu_init(void)
 {
   sysclk_init();
   interrupt_init();
+  gpio_init();
+  
+  // i use rs485 convert, RS_pin need to be set.
+  gpio_state_set(RS_PIN, true);
+
+  uart_init(PIC32_UART_4, NO_PARITY_8_BIT_DATA, ONE_STOP_BIT, 115200);
+
 }
 
 void led_init(void)
 {
-  ANSELB = 0; // PIC starts all pins as analog, needs to be deactivated to be used
-  TRISBCLR = 1 << 13; //set bit 13 port B to output
-  TRISBCLR = 1 << 12; //set bit 12 port B to output
+  // the pins are already set as output in pic32_config, as a demo.
+  gpio_output_set(RED_LED);
+  gpio_output_set(ORANGE_LED);
 }
 
 void led_state(int led, int state)
 {
-  if (state == 0)
-  {
-    LATBSET = 1 << led;
-  }
-  else
-  {
-    LATBCLR = 1 << led;
-  }
+  gpio_state_set(led, state);
 }
+
 void led_toggle(int led)
 {
-  LATBINV = 1 << led;
-}
-
-void uart_init(void)
-{
-  int pb_uart_clk = SYS_FREQ / 2;
-  int baud_rate = 19200;
-
-  // Set rs485 bit to output
-  // this pin is special for this board and not for all uart init
-  ANSELA = 0;
-  TRISACLR = 1 << RS_PIN;
-  LATASET = 1 << RS_PIN; // clear rspin for reading
-  
-  // Set up the UART 4
-  // RX to RPF2
-  U4RXR = 0b0010;
-  TRISBbits.TRISB14 = 1; //set its IO to input
-
-  // TX to RPF3
-  RPF12R = 0b0010;
-
-  U4MODE = 0; // Set UART 5 off, prior to setting it up.
-  U4MODEbits.BRGH = 0; // standard speed mode, included in the previsous statmem
-
-  // formulat from datasheet.
-  U4BRG = pb_uart_clk / ( 16  * baud_rate) - 1; 
-  U4STA = 0; // disable Tx Rx, clear all flags
-  U4STAbits.UTXEN = 1;
-  U4STAbits.URXEN = 1;
-
-  U4MODEbits.PDSEL = 0; // controls how many data bits parity,... we chose default
-  U4MODEbits.STSEL = 0; // stop bit we chose 1
-  
-  U4MODEbits.ON = 1; // Turn ON
-
-  print_str("Hello UART \n");
+  gpio_state_toggle(led);
 }
 
 int uart_rx_any(void)
 {
-  return (int)U4STAbits.URXDA & 0x01;
+  return uart_rx_ready(PIC32_UART_4);
 }
 
 int uart_rx_char(void)
 {
-  return (int)U4RXREG;
+  return uart_read_char(PIC32_UART_4);
 }
 
-void uart_tx_char(int c)
+void uart_tx_char(int chr)
 {
-  while (U4STAbits.UTXBF);
-  U4TXREG = (char)c;
-}
-
-void print_str(char *s)
-{
-  while ( *s != '\0')
-  {
-    uart_tx_char((int)*s++);
-  }
+  uart_send_char(PIC32_UART_4, chr);
 }
--- ports/pic32mz/board.h
+++ ports/pic32mz/board.h
@@ -1,11 +1,12 @@
 #ifndef MICROPY_INCLUDED_PIC32BIT_BOARD_H
 #define MICROPY_INCLUDED_PIC32BIT_BOARD_H
 
-#define RED_LED    (13)
-#define ORANGE_LED (12)
-#define RS_PIN     (10)
+#include "pic32_config.h"
+#include "gpio.h"
 
-#define SYS_FREQ 200000000 // 200MHz
+#define RED_LED    (pinB13)
+#define ORANGE_LED (pinB12)
+#define RS_PIN     (pinA10)
 
 extern int _stack;
 extern int _heap;
@@ -19,11 +20,8 @@ void led_init(void);
 void led_state(int led, int state);
 void led_toggle(int led);
 
-void uart_init(void);
 int uart_rx_any(void);
 int uart_rx_char(void);
 void uart_tx_char(int chr);
-void print_str(char *s);
-void set_rs_pin(void);
-void clear_rs_pin(void);
+
 #endif // MICROPY_INCLUDED_PIC32BIT_BOARD_H
