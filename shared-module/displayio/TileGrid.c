9adb77a2d0179f89c7402451f6b25be74629ffbd
foamyguy <foamyguy@gmail.com>
2020-11-08 19:28:46 -0600

    set moved true when unhiding tilegrid

--- shared-module/displayio/TileGrid.c
+++ shared-module/displayio/TileGrid.c
@@ -83,10 +83,16 @@ bool common_hal_displayio_tilegrid_get_hidden(displayio_tilegrid_t* self) {
 
 void common_hal_displayio_tilegrid_set_hidden(displayio_tilegrid_t* self, bool hidden) {
     self->hidden = hidden;
+    if(!hidden){
+        self->moved = true;
+    }
 }
 
 void displayio_tilegrid_set_hidden_by_parent(displayio_tilegrid_t *self, bool hidden) {
     self->hidden_by_parent = hidden;
+    if(!hidden){
+        self->moved = true;
+    }
 }
 
 bool displayio_tilegrid_get_previous_area(displayio_tilegrid_t *self, displayio_area_t* area) {
