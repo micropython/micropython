118ca7cff44bc3aa50e57915e691e1b8c2f4e563
foamyguy <foamyguy@gmail.com>
2020-11-11 06:56:57 -0600

    adding default uart pins

--- ports/esp32s2/boards/unexpectedmaker_feathers2/mpconfigboard.h
+++ ports/esp32s2/boards/unexpectedmaker_feathers2/mpconfigboard.h
@@ -44,3 +44,6 @@
 #define DEFAULT_SPI_BUS_SCK (&pin_GPIO36)
 #define DEFAULT_SPI_BUS_MOSI (&pin_GPIO35)
 #define DEFAULT_SPI_BUS_MISO (&pin_GPIO37)
+
+#define DEFAULT_UART_BUS_RX (&pin_GPIO44)
+#define DEFAULT_UART_BUS_TX (&pin_GPIO43)
