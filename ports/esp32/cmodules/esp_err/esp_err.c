11e9e53b8c33e259344702724ab9a378ab566cd5
IhorNehrutsa <Ihor.Nehrutsa@gmail.com>
2020-11-16 10:23:42 -0800

    Fix EspError: esp_err_to_name()

--- ports/esp32/cmodules/esp_err/esp_err.c
+++ ports/esp32/cmodules/esp_err/esp_err.c
@@ -26,6 +26,9 @@
  */
 
 /*
+Error Codes and Helper Functions  
+https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/system/esp_err.html
+
 Wrapped around
 https://github.com/espressif/esp-idf/blob/master/components/esp_common/include/esp_err.h
 */
--- ports/esp32/cmodules/esp_err/esp_error.h
+++ ports/esp32/cmodules/esp_err/esp_error.h
@@ -1,3 +1,4 @@
 const mp_obj_type_t mp_type_EspError;
 
-#define raise_esp_error(err) mp_raise_msg_varg(&mp_type_EspError, MP_ERROR_TEXT("%d(0x%X)"), err, err);
+#define raise_esp_error(err) mp_raise_msg_varg(&mp_type_EspError, MP_ERROR_TEXT("%d(0x%X) - %s"), err, err, esp_err_to_name(err));
+
