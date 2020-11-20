c6d6dce1caf0764afa30e858b3b4de23c889ddcd
Ihor Nehrutsa <Ihor.Nehrutsa@gmail.com>
2020-11-20 16:00:38 +0200

    esp32/esp32_pcnt.* : Add files for Pulse counter and Quadrature encoder.

---
+++ ports/esp32/esp32_pcnt.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <driver/gpio.h>
+#include "driver/pcnt.h" 
+
+#define _INT16_MAX (32766)
+#define _INT16_MIN (-32766)
+
+enum puType {
+    NONE,
+    DOWN,
+    UP 
+};
+
+enum edgeKind {
+    RAISE,
+    FALL,
+    BOTH
+};
+
+enum encType {
+    SINGLE, 
+    HALF, 
+    FULL
+};
+
+typedef struct _pcnt_PCNT_obj_t {
+    mp_obj_base_t base;
+    gpio_num_t aPinNumber;
+    gpio_num_t bPinNumber;
+    enum puType useInternalWeakPullResistors;
+
+    pcnt_config_t r_enc_config;
+    bool attached;
+    pcnt_unit_t unit;
+    volatile int64_t count; 
+} pcnt_PCNT_obj_t;
+
+extern int machine_pin_get_id(mp_obj_t pin_in);
+
+#pragma once
