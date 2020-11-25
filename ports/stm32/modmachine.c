484dcff08d04bfdb0b984434b80106b34e918332
iabdalkader <i.abdalkader@gmail.com>
2020-11-25 15:15:49 +0200

    Add device and revision ids to machine.info()

--- ports/stm32/modmachine.c
+++ ports/stm32/modmachine.c
@@ -155,6 +155,8 @@ STATIC mp_obj_t machine_info(size_t n_args, const mp_obj_t *args) {
         printf("ID=%02x%02x%02x%02x:%02x%02x%02x%02x:%02x%02x%02x%02x\n", id[0], id[1], id[2], id[3], id[4], id[5], id[6], id[7], id[8], id[9], id[10], id[11]);
     }
 
+    printf("DEVID: 0x%04lx REVID: 0x%04lx\n", HAL_GetDEVID(), HAL_GetREVID());
+
     // get and print clock speeds
     // SYSCLK=168MHz, HCLK=168MHz, PCLK1=42MHz, PCLK2=84MHz
     {
