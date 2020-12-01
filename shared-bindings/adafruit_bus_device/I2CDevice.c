d3bbb99e071a97d7295f6d46dbed766c2aa51fb5
gamblor21 <mark.komus@gmail.com>
2020-12-01 17:49:15 -0600

    Fixing stubs

--- shared-bindings/adafruit_bus_device/I2CDevice.c
+++ shared-bindings/adafruit_bus_device/I2CDevice.c
@@ -86,7 +86,7 @@ STATIC mp_obj_t adafruit_bus_device_i2cdevice_make_new(const mp_obj_type_t *type
     return (mp_obj_t)self;
 }
 
-//|     def __enter__(self) -> I2C:
+//|     def __enter__(self) -> I2CDevice:
 //|         """Context manager entry to lock bus."""
 //|         ...
 //|
@@ -107,14 +107,13 @@ STATIC mp_obj_t adafruit_bus_device_i2cdevice_obj___exit__(size_t n_args, const 
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(adafruit_bus_device_i2cdevice___exit___obj, 4, 4, adafruit_bus_device_i2cdevice_obj___exit__);
 
-//| def readinto(self, buf, *, start=0, end=None) -> None:
-//|         """
-//|         Read into ``buf`` from the device. The number of bytes read will be the
+//|     def readinto(self, buf: WriteableBuffer, *, start: int = 0, end: int = 0) -> None:
+//|         """Read into ``buf`` from the device. The number of bytes read will be the
 //|         length of ``buf``.
 //|         If ``start`` or ``end`` is provided, then the buffer will be sliced
 //|         as if ``buf[start:end]``. This will not cause an allocation like
 //|         ``buf[start:end]`` will so it saves memory.
-//|         :param bytearray buffer: buffer to write into
+//|         :param bytearray buf: buffer to write into
 //|         :param int start: Index to start writing at
 //|         :param int end: Index to write up to but not include; if None, use ``len(buf)``"""
 //|         ...
@@ -153,17 +152,16 @@ STATIC mp_obj_t adafruit_bus_device_i2cdevice_readinto(size_t n_args, const mp_o
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_KW(adafruit_bus_device_i2cdevice_readinto_obj, 2, adafruit_bus_device_i2cdevice_readinto);
 
-//| def write(self, buf, *, start=0, end=None) -> None:
-//|     """
-//|     Write the bytes from ``buffer`` to the device, then transmit a stop bit.
-//|     If ``start`` or ``end`` is provided, then the buffer will be sliced
-//|     as if ``buffer[start:end]``. This will not cause an allocation like
-//|     ``buffer[start:end]`` will so it saves memory.
-//|     :param bytearray buffer: buffer containing the bytes to write
-//|     :param int start: Index to start writing from
-//|     :param int end: Index to read up to but not include; if None, use ``len(buf)``
-//|     """
-//|     ...
+//|     def write(self, buf: ReadableBuffer, *, start: int = 0, end: int = 0) -> None:
+//|         """Write the bytes from ``buffer`` to the device, then transmit a stop bit.
+//|         If ``start`` or ``end`` is provided, then the buffer will be sliced
+//|         as if ``buffer[start:end]``. This will not cause an allocation like
+//|         ``buffer[start:end]`` will so it saves memory.
+//|         :param bytearray buf: buffer containing the bytes to write
+//|         :param int start: Index to start writing from
+//|         :param int end: Index to read up to but not include; if None, use ``len(buf)``
+//|         """
+//|         ...
 //|
 STATIC void write(adafruit_bus_device_i2cdevice_obj_t *self, mp_obj_t buffer, int32_t start, mp_int_t end) {
     mp_buffer_info_t bufinfo;
@@ -199,9 +197,8 @@ STATIC mp_obj_t adafruit_bus_device_i2cdevice_write(size_t n_args, const mp_obj_
 MP_DEFINE_CONST_FUN_OBJ_KW(adafruit_bus_device_i2cdevice_write_obj, 2, adafruit_bus_device_i2cdevice_write);
 
 
-//| def write_then_readinto(self, out_buffer, in_buffer, *, out_start=0, out_end=None, in_start=0, in_end=None) -> None:
-//|         """
-//|         Write the bytes from ``out_buffer`` to the device, then immediately
+//|     def write_then_readinto(self, out_buffer: WriteableBuffer, in_buffer: ReadableBuffer, *, out_start: int = 0, out_end: int = 0, in_start: int = 0, in_end: int = 0) -> None:
+//|         """Write the bytes from ``out_buffer`` to the device, then immediately
 //|         reads into ``in_buffer`` from the device. The number of bytes read
 //|         will be the length of ``in_buffer``.
 //|         If ``out_start`` or ``out_end`` is provided, then the output buffer
