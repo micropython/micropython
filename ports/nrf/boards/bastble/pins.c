88b1e2fe61d68c4a4d2ef9e48147c20a929e779e
sabas1080 <sabasjimenez@gmail.com>
2020-12-20 13:35:15 -0600

    remove Internal flash unnecessary

--- ports/nrf/boards/bastble/pins.c
+++ ports/nrf/boards/bastble/pins.c
@@ -36,7 +36,6 @@ STATIC const mp_rom_map_elem_t board_module_globals_table[] = {
     // voltage sense battery
     { MP_ROM_QSTR(MP_QSTR_VBAT), MP_ROM_PTR(&pin_P0_26)  },
 
-
     { MP_ROM_QSTR(MP_QSTR_TX), MP_ROM_PTR(&pin_P0_10) },
     { MP_ROM_QSTR(MP_QSTR_RX), MP_ROM_PTR(&pin_P0_09) },
 
