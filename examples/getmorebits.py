50be9662a89e12cba38c765538e6b46a9926f762
Macathur Inbody <admin-contact@transcendental.us>
2020-12-23 06:29:33 +0000

    examples/getmorebits.py:I forgot to import the random module.
    Originally I forgot to include the random module as would be required for
    this to actually work in the real world. It'd probably be best to just 
    import random within the function context but I don't think it matters too
    much overall.

--- examples/getmorebits.py
+++ examples/getmorebits.py
@@ -1,16 +1,20 @@
+import random
+
+
 def getmorebits(bits: int) -> int:
     """
     :param bits: The number of bits to generate.
     :return: The resulting integer.
     """
 
-    n = bits // 32
+    iterations = bits // 32
     d = 0
-    for i in range(n):
+    for i in range(iterations):
         d |= random.getrandbits(32) << (i * 32)
+   
     r = bits % 32
 
     if bits:
-        d |= random.getrandbits(bits % 32) << (n * 32)
+        d |= random.getrandbits(r) << (n * 32)
 
     return d
