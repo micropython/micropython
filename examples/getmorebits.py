5f6f6fd365b914c9b19aadb9c035a688a78c697f
Macarthur Inbody <133794m3r@gmail.com>
2020-12-23 11:35:01 -0500

    examples/getmorebits.py: Changd to while loop, remaining bits bux fixed.
    The original code was a while loop, so it's been changed to the more
    pythonic for in loop. Also before I wasn't making sure that the number
    of bits that we were asking for was actually more than 0. As such an
    error would have been caused if the person was asking for an even multiple
    of 32 bits. This error has been fixed and is now bug free(I hope).

--- examples/getmorebits.py
+++ examples/getmorebits.py
@@ -2,12 +2,18 @@ import random
 
 
 def getmorebits(bits: int) -> int:
+    """
+    :param bits: The number of bits to generate.
+    :return: The resulting integer.
+    """
+
     n = bits // 32
-    k = 0
     d = 0
-    while k < n:
-        d |= random.getrandbits(32) << (k * 32)
-        k += 1
-    d |= random.getrandbits(bits % 32) << (k * 32)
+    for i in range(n):
+        d |= getrandbits(32) << (i * 32)
+
+    r = bits % 32
+    if bits >= 1:
+        d |= getrandbits(bits % 32) << (n * 32)
 
     return d
