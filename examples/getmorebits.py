1adc241a8ecdfcd56fde2dd59f02946d3697e493
Macarthur Inbody <133794m3r@gmail.com>
2020-12-23 01:09:42 -0500

    tests/cpydiff/modules_random_getrandbits.py: Removed example code. examples/getmorebits.py: Moved the example function over here.
    I've removed the example function from within the documentaton and instead
    I've moved it to the examples folder. I don't know the best way to link to
    a folder from within the docs so I simply reference it from the workaround
    message that's given. It's the same function but now fixed since it had a
    bug in it originally where it could try to get 0 random bits which would
    cause an error.

---
+++ examples/getmorebits.py
@@ -0,0 +1,16 @@
+def getmorebits(bits: int) -> int:
+    """
+    :param bits: The number of bits to generate.
+    :return: The resulting integer.
+    """
+
+    n = bits // 32
+    d = 0
+    for i in range(n):
+        d |= random.getrandbits(32) << (i * 32)
+    r = bits % 32
+
+    if bits:
+        d |= random.getrandbits(bits % 32) << (n * 32)
+
+    return d
--- tests/cpydiff/modules_random_getrandbits.py
+++ tests/cpydiff/modules_random_getrandbits.py
@@ -2,25 +2,10 @@
 categories: Modules,random
 description: ``getrandbits`` method can only return a maximum of 32 bits at a time.
 cause: Internal PRNG's state is only 32 bits.
-workaround: If you need a number that's larger than 32bits and want to use getrandbits use the code below.
+workaround: If you need a number that's larger than 32bits and want to use getrandbits use a function like the one in the examples folder called "getmorebits.py". The function contained within will let you work around this issue if you need more than 32 bits of random data.
 """
 
 import random
 
-x = random.getrandbits(64)
+x = random.getrandbits(65)
 print("{} is bits".format(x))
-
-
-def getmorebits(bits: int) -> int:
-    """
-    :param bits: The number of bits to generate.
-    :return: The resulting integer.
-    """
-
-    n = bits // 32
-    d = 0
-    for i in range(n):
-        d |= random.getrandbits(32) << (i * 32)
-    d |= random.getrandbits(bits % 32) << (n * 32)
-
-    return d
