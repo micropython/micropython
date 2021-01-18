b90d97462a525dda4a71680105fdc2549e401890
Thorsten von Eicken <tve@voneicken.com>
2021-01-18 01:06:12 -0800

    esp32: add basic support for NVS Non-Volatile-Storage

--- ports/esp32/modesp32.h
+++ ports/esp32/modesp32.h
@@ -29,5 +29,6 @@
 extern const mp_obj_type_t esp32_partition_type;
 extern const mp_obj_type_t esp32_rmt_type;
 extern const mp_obj_type_t esp32_ulp_type;
+extern const mp_obj_type_t esp32_nvs_type;
 
 #endif // MICROPY_INCLUDED_ESP32_MODESP32_H
