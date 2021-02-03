ec748e8be943628da6074ec8154dc38f1497657b
StereoRocker <technpizza@gmail.com>
2021-02-03 22:59:36 +0000

    rp2: Add SD card + FAT support.
    manifest.py: Added sdcard module.
    mpconfigport.h: Added FAT driver and configuration.

--- ports/rp2/manifest.py
+++ ports/rp2/manifest.py
@@ -1,3 +1,4 @@
 freeze("modules")
 freeze("$(MPY_DIR)/drivers/onewire")
+freeze("$(MPY_DIR)/drivers/sdcard")
 include("$(MPY_DIR)/extmod/uasyncio/manifest.py")
--- ports/rp2/mpconfigport.h
+++ ports/rp2/mpconfigport.h
@@ -117,6 +117,13 @@
 #define MICROPY_PY_FRAMEBUF                     (1)
 #define MICROPY_VFS                             (1)
 #define MICROPY_VFS_LFS2                        (1)
+#define MICROPY_VFS_FAT                         (1)
+
+// fatfs configuration
+#define MICROPY_FATFS_ENABLE_LFN                (1)
+#define MICROPY_FATFS_LFN_CODE_PAGE             437 /* 1=SFN/ANSI 437=LFN/U.S.(OEM) */
+#define MICROPY_FATFS_RPATH                     (2)
+#define MICROPY_FATFS_NORTC                     (1)
 
 // Use VfsLfs2's types for fileio/textio
 #define mp_type_fileio mp_type_vfs_lfs2_fileio
