16df159b3d0300fc8c649aebcc6fb20a9c5fb14f
Peter Züger <zueger.peter@icloud.com>
2021-02-03 11:11:19 +0100

    extmod/modujson.c: Added dump/dumps separators
    added support for the separators keyword argument in ujson's dump and
    dumps.
    
    added keyword argument indent, only accepts indent=None.
    indent was only added because it is connected to separators.

--- extmod/modujson.c
+++ extmod/modujson.c
@@ -35,6 +35,24 @@
 
 #if MICROPY_PY_UJSON
 
+static void mod_ujson_separators(mp_obj_t separators_in, const char **item_separator, const char **key_separator) {
+    if (separators_in == mp_const_none) {
+        *item_separator = ", ";
+        *key_separator = ": ";
+    } else {
+        mp_obj_t *items;
+        size_t len;
+        mp_obj_tuple_get(separators_in, &len, &items);
+
+        if (len != 2) {
+            mp_raise_ValueError(MP_ERROR_TEXT("too many values to unpack (expected 2)"));
+        }
+
+        *item_separator = mp_obj_str_get_str(items[0]);
+        *key_separator = mp_obj_str_get_str(items[1]);
+    }
+}
+
 const char *ujson_item_separator = ", ";
 const char *ujson_key_separator = ": ";
 
@@ -54,15 +72,29 @@ STATIC mp_obj_t mod_ujson_dump(size_t n_args, const mp_obj_t *pos_args, mp_map_t
 
     mp_get_stream_raise(pos_args[1], MP_STREAM_OP_WRITE);
 
+    const char *old_item_separator = ujson_item_separator;
+    const char *old_key_separator = ujson_key_separator;
+    bool raise = false;
+    mod_ujson_separators(args[ARG_separators].u_obj, &ujson_item_separator, &ujson_key_separator);
+
     nlr_buf_t nlr;
     if (nlr_push(&nlr) == 0) {
         mp_print_t print = {MP_OBJ_TO_PTR(pos_args[1]), mp_stream_write_adaptor};
         mp_obj_print_helper(&print, pos_args[0], PRINT_JSON);
         nlr_pop();
     } else {
-        // Re-raise the exception
+        raise = true;
+    }
+
+    // revert old values in case of nested dump
+    ujson_item_separator = old_item_separator;
+    ujson_key_separator = old_key_separator;
+
+    // Re-raise the exception
+    if (raise) {
         nlr_raise(MP_OBJ_FROM_PTR(nlr.ret_val));
     }
+
     return mp_const_none;
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_KW(mod_ujson_dump_obj, 2, mod_ujson_dump);
@@ -85,14 +117,28 @@ STATIC mp_obj_t mod_ujson_dumps(size_t n_args, const mp_obj_t *pos_args, mp_map_
     mp_print_t print;
     vstr_init_print(&vstr, 8, &print);
 
+    const char *old_item_separator = ujson_item_separator;
+    const char *old_key_separator = ujson_key_separator;
+    bool raise = false;
+    mod_ujson_separators(args[ARG_separators].u_obj, &ujson_item_separator, &ujson_key_separator);
+
     nlr_buf_t nlr;
     if (nlr_push(&nlr) == 0) {
         mp_obj_print_helper(&print, pos_args[0], PRINT_JSON);
         nlr_pop();
     } else {
-        // Re-raise the exception
+        raise = true;
+    }
+
+    // revert old values in case of nested dump
+    ujson_item_separator = old_item_separator;
+    ujson_key_separator = old_key_separator;
+
+    // Re-raise the exception
+    if (raise) {
         nlr_raise(MP_OBJ_FROM_PTR(nlr.ret_val));
     }
+
     return mp_obj_new_str_from_vstr(&mp_type_str, &vstr);
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_KW(mod_ujson_dumps_obj, 1, mod_ujson_dumps);
