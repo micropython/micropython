26b4ef4c46520f595f39d53bbea7f247c9467370
Damien George <damien@micropython.org>
2021-02-11 22:53:36 +1100

    extmod/vfs_posix_file: Allow closing an already closed file.
    Signed-off-by: Damien George <damien@micropython.org>

--- tests/extmod/vfs_posix.py
+++ tests/extmod/vfs_posix.py
@@ -8,6 +8,15 @@ except (ImportError, AttributeError):
     print("SKIP")
     raise SystemExit
 
+# We need a file for testing that doesn't already exist.
+# Skip the test if it does exist.
+temp_file = "micropy_test_file.txt"
+try:
+    uos.stat(temp_file)
+    print("SKIP")
+    raise SystemExit
+except OSError:
+    pass
 
 # getcwd and chdir
 curdir = uos.getcwd()
@@ -21,3 +30,19 @@ print(type(uos.stat("/")))
 
 # listdir and ilistdir
 print(type(uos.listdir("/")))
+
+# file create
+f = open(temp_file, "w")
+f.write("hello")
+f.close()
+
+# close on a closed file should succeed
+f.close()
+
+# file read
+f = open(temp_file, "r")
+print(f.read())
+f.close()
+
+# remove
+uos.remove(temp_file)
