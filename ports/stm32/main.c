85ea4ac0e56faca680aa03c41eb4009464401f07
Braiden Kindt <braiden@kindt.dev>
2021-02-14 23:42:57 -0500

    stm32/main: Fix passing state.reset_mode to init_flash_fs.
    state.reset_mode is updated by `MICROPY_BOARD_BEFORE_SOFT_RESET_LOOP` but
    not passed to `init_flash_fs`, and so factory reset is not executed on
    boards that do not have a bootloader.  This bug was introduced by
    4c3976bbcaf58266fdcaab264fee5b7a94a682e5
    
    Fixes #6903.

--- ports/stm32/main.c
+++ ports/stm32/main.c
@@ -531,7 +531,7 @@ soft_reset:
     // Create it if needed, mount in on /flash, and set it as current dir.
     bool mounted_flash = false;
     #if MICROPY_HW_FLASH_MOUNT_AT_BOOT
-    mounted_flash = init_flash_fs(reset_mode);
+    mounted_flash = init_flash_fs(state.reset_mode);
     #endif
 
     bool mounted_sdcard = false;
