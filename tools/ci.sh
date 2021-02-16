2eed9780ba7074de9e464a2bc771ad14f0332a6c
David Michieli <dmm@planetinnovation.com.au>
2021-02-16 12:34:34 +1100

    stm32/mboot: Add unpack-dfu command to mboot_pack_dfu.py tool.
    This command unpacks a previously packed DFU file, writing out a DFU which
    should be the same as the original (before packing).

--- tools/ci.sh
+++ tools/ci.sh
@@ -223,6 +223,11 @@ function ci_stm32_nucleo_build {
     make ${MAKEOPTS} -C ports/stm32 BOARD=NUCLEO_L476RG DEBUG=1
     make ${MAKEOPTS} -C ports/stm32 BOARD=NUCLEO_WB55
     make ${MAKEOPTS} -C ports/stm32/mboot BOARD=NUCLEO_WB55
+    # Test mboot_pack_dfu.py created a valid file, and that its unpack-dfu command works.
+    BOARD_WB55=ports/stm32/boards/NUCLEO_WB55
+    BUILD_WB55=ports/stm32/build-NUCLEO_WB55
+    python3 ports/stm32/mboot/mboot_pack_dfu.py -k $BOARD_WB55/mboot_keys.h unpack-dfu $BUILD_WB55/firmware.pack.dfu $BUILD_WB55/firmware.unpack.dfu
+    diff $BUILD_WB55/firmware.unpack.dfu $BUILD_WB55/firmware.dfu
 }
 
 ########################################################################################
