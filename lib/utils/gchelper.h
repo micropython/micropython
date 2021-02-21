7989d63f7d526b12c24ba91dad84e353b30fb765
Yonatan Goldschmidt <yon.goldschmidt@gmail.com>
2021-02-21 02:05:40 +0200

    lib/utils/gchelper_generic: Implement AArch64 support.

--- lib/utils/gchelper.h
+++ lib/utils/gchelper.h
@@ -39,6 +39,8 @@ typedef uintptr_t gc_helper_regs_t[6];
 typedef uintptr_t gc_helper_regs_t[4];
 #elif defined(__thumb2__) || defined(__thumb__) || defined(__arm__)
 typedef uintptr_t gc_helper_regs_t[10];
+#elif defined(__aarch64__)
+typedef uintptr_t gc_helper_regs_t[11]; // x19-x29
 #endif
 
 #endif
