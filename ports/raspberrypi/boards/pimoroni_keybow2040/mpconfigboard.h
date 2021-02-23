a9951c9b6c883858e599d5102d5901278b3bf469
ZodiusInfuser <christopher.parrott2@gmail.com>
2021-02-23 11:01:40 +0000

    Added flash size for Keybow2040

--- ports/raspberrypi/boards/pimoroni_keybow2040/mpconfigboard.h
+++ ports/raspberrypi/boards/pimoroni_keybow2040/mpconfigboard.h
@@ -28,6 +28,8 @@
 #define DEFAULT_UART_BUS_RX (&pin_GPIO1)
 #define DEFAULT_UART_BUS_TX (&pin_GPIO0)
 
+#define TOTAL_FLASH_SIZE (2 * 1024 * 1024)
+
 // These pins are unconnected
 #define IGNORE_PIN_GPIO2    1
 #define IGNORE_PIN_GPIO22   1
