69b10d79253a6920f754e53b733674a768a069fa
Seon Rozenblum <seon@unexpectedmaker.com>
2021-02-25 17:32:34 +1100

    Added DAC1 & DAC2 pin names for FeatherS2 Expanded pin names for TinyS2 and added some new ones for functionality I forgot to add pin names for Cleaned up mpconfigboard

--- ports/esp32s2/boards/unexpectedmaker_tinys2/mpconfigboard.mk
+++ ports/esp32s2/boards/unexpectedmaker_tinys2/mpconfigboard.mk
@@ -14,10 +14,7 @@ CIRCUITPY_ESP_FLASH_MODE=qio
 CIRCUITPY_ESP_FLASH_FREQ=80m
 CIRCUITPY_ESP_FLASH_SIZE=4MB
 
-# CIRCUITPY_MODULE=wroom
-
 CIRCUITPY_BITBANG_NEOPIXEL = 1
 
 # Include these Python libraries in firmware.
-# FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Requests
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_NeoPixel
