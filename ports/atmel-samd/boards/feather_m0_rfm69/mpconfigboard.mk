ec4cd68edcc8bbbbc5edb5eefb1cfdec70c70a99
Kevin Matocha <kmatocha@icloud.com>
2021-02-25 17:48:50 -0600

    More mpconfigboard.mk corrections for small builds

--- ports/atmel-samd/boards/feather_m0_rfm69/mpconfigboard.mk
+++ ports/atmel-samd/boards/feather_m0_rfm69/mpconfigboard.mk
@@ -13,6 +13,7 @@ CIRCUITPY_FULL_BUILD = 0
 # A number of modules are removed for RFM69 to make room for frozen libraries.
 # Many I/O functions are not available.
 CIRCUITPY_ANALOGIO = 1
+CIRCUITPY_BITMAPTOOLS = 0
 CIRCUITPY_PULSEIO = 0
 CIRCUITPY_NEOPIXEL_WRITE = 1
 CIRCUITPY_ROTARYIO = 0
