32475ce98c2eaa13240fa24e713ce3d5df60afd9
Jeff Epler <jepler@gmail.com>
2021-03-07 12:51:47 -0600

    Automatically count EXTERNAL_FLASH_DEVICES (mk2)
    Instead of counting words in make, which is slightly awful, notice that
    possible_devices is local to external_flash.c, so we can declare the array
    with an automatic bound, and then get the count as the element-count
    (MP_ARRAY_SIZE) of the array.
    
    Since EXTERNAL_FLASH_DEVICE_COUNT is no longer a global macro, switch
    a few sites to using EXTERNAL_FLASH_DEVICES in `#if` checks instead.

--- supervisor/supervisor.mk
+++ supervisor/supervisor.mk
@@ -33,10 +33,7 @@ endif
 # (Right now INTERNAL_FLASH_FILESYSTEM and (Q)SPI_FLASH_FILESYSTEM are mutually exclusive.
 # But that might not be true in the future.)
 ifdef EXTERNAL_FLASH_DEVICES
-  COMMA := ,
-  EXTERNAL_FLASH_DEVICE_COUNT := $(words $(subst $(COMMA), ,$(EXTERNAL_FLASH_DEVICES)))
   CFLAGS += -DEXTERNAL_FLASH_DEVICES=$(EXTERNAL_FLASH_DEVICES) \
-	    -DEXTERNAL_FLASH_DEVICE_COUNT=$(EXTERNAL_FLASH_DEVICE_COUNT)
 
   SRC_SUPERVISOR += supervisor/shared/external_flash/external_flash.c
     ifeq ($(SPI_FLASH_FILESYSTEM),1)
