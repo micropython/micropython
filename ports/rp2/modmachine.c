dcaf702578045299d5bc007d81669fe1ba640654
Damien George <damien@micropython.org>
2021-03-14 00:18:18 +1100

    rp2/modmachine: Enable machine.Signal class.
    Fixes issue #6863.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/rp2/modmachine.c
+++ ports/rp2/modmachine.c
@@ -30,6 +30,7 @@
 #include "extmod/machine_i2c.h"
 #include "extmod/machine_mem.h"
 #include "extmod/machine_pulse.h"
+#include "extmod/machine_signal.h"
 #include "extmod/machine_spi.h"
 
 #include "modmachine.h"
@@ -163,6 +164,7 @@ STATIC const mp_rom_map_elem_t machine_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_SoftI2C),             MP_ROM_PTR(&mp_machine_soft_i2c_type) },
     { MP_ROM_QSTR(MP_QSTR_Pin),                 MP_ROM_PTR(&machine_pin_type) },
     { MP_ROM_QSTR(MP_QSTR_PWM),                 MP_ROM_PTR(&machine_pwm_type) },
+    { MP_ROM_QSTR(MP_QSTR_Signal),              MP_ROM_PTR(&machine_signal_type) },
     { MP_ROM_QSTR(MP_QSTR_SPI),                 MP_ROM_PTR(&machine_spi_type) },
     { MP_ROM_QSTR(MP_QSTR_SoftSPI),             MP_ROM_PTR(&mp_machine_soft_spi_type) },
     { MP_ROM_QSTR(MP_QSTR_Timer),               MP_ROM_PTR(&machine_timer_type) },
