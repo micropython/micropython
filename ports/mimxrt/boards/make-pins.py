896dde42432f591b3097917de86883bbcc97b9a7
Philipp Ebensberger <philipp.ebensberger@3bricks-software.de>
2021-04-17 18:53:00 +0200

    mimxrt/machine_pin: Implemented basic make-pins.py.
    - added all alternative functions for pins of  mimxrt1021-evk board
    which are available through arduino pin header
    - added arduino and pad names of mimxrt1021-evk board in respective
    pins.csv
    - implemented basic version of make-pins.py to genertate pins.c/.h files
    automatically. The only alternate function currently supported is GPIO.

--- ports/mimxrt/boards/make-pins.py
+++ ports/mimxrt/boards/make-pins.py
@@ -7,7 +7,8 @@ import argparse
 import sys
 import csv
 
-SUPPORTED_AFS = {"GPIO", "FLEXSPI"}
+SUPPORTED_AFS = {"GPIO"}
+MAX_AF = 10  # AF0 .. AF9
 
 def parse_pad(pad_str):
     """Parses a string and returns a (port, gpio_bit) tuple."""
@@ -18,14 +19,23 @@ def parse_pad(pad_str):
     return pad_str
 
 
+def af_supported(af_str):
+    for supported_af in SUPPORTED_AFS:
+        if af_str.startswith(supported_af):
+            return True
+    else:
+        return False
+
+
 class Pin(object):
     """Holds the information associated with a pin."""
 
-    def __init__(self, name, gpio, pin):
+    def __init__(self, pad, gpio, pin):
+        self.name = pad
+        self.pad = pad
         self.gpio = gpio
         self.pin = pin
         self.alt_fn = []
-        self.alt_fn_count = 0
         self.adc_channel = 0
         self.board_pin = False
 
@@ -41,8 +51,19 @@ class Pin(object):
     def parse_af(self, af_idx, af_strs_in):
         pass
 
+    def add_af(self, af):
+        self.alt_fn.append(af)
+
     def print(self):
-        print("machine_pin_obj_t {0} = PIN({0}, {1}, {2}, {3});".format(self.pin, self.gpio, 5, self.pin + "_af"))
+        if self.alt_fn:
+            print("static machine_pin_af_obj_t {0}_af[{1}] = {{".format(self.name, len(self.alt_fn)))
+            for af in self.alt_fn:
+                af.print()
+            print("};")
+            print("machine_pin_obj_t {0} = PIN({0}, {1}, {2}, {3});\n".format(self.name, self.gpio, int(self.pin),
+                                                                              self.name + "_af"))
+        else:
+            raise ValueError("Pin '{}' has no alternative functions".format(self.name))
 
     def print_header(self, hdr_file):
         pass
@@ -53,12 +74,9 @@ class AlternateFunction(object):
 
     def __init__(self, idx, af_str):
         self.idx = idx
-        # Special case. We change I2S2ext_SD into I2S2_EXTSD so that it parses
-        # the same way the other peripherals do.
-        af_str = af_str.replace("ext_", "_EXT")
-
         self.af_str = af_str
-
+        self.instance = self.af_str.split("_")[0]
+        """
         self.func = ""
         self.fn_num = None
         self.pin_type = ""
@@ -72,6 +90,7 @@ class AlternateFunction(object):
             pin_types = SUPPORTED_FN[self.func]
             if self.pin_type in pin_types:
                 self.supported = True
+        """
 
     def is_supported(self):
         return self.supported
@@ -87,6 +106,11 @@ class AlternateFunction(object):
 
     def print(self):
         """Prints the C representation of this AF."""
+        print("    PIN_AF({0}, PIN_AF_MODE_ALT{1}, {2}, {3}),".format(self.af_str,
+                                                       self.idx,
+                                                       self.instance,
+                                                       "0x10B0U"))
+        """
         cond_var = None
         if self.supported:
             cond_var = conditional_var("{}{}".format(self.func, self.fn_num))
@@ -103,46 +127,59 @@ class AlternateFunction(object):
             )
         )
         print_conditional_endif(cond_var)
+        """
 
     def qstr_list(self):
         return [self.mux_name()]
 
 
+class NamedPin(object):
+    def __init__(self, name, pad):
+        self.name = name
+        self.pad = pad
+
 
 class Pins(object):
     def __init__(self):
+        self.cpu_pins = []
         self.board_pins = []
 
     def find_pin_by_num(self, pin_num):
-        for pin in self.board_pins:
+        for pin in self.cpu_pins:
             if pin.pin_num == pin_num:
                 return pin
 
-    def find_pin_by_name(self, name):
-        for pin in self.board_pins:
-            if pin.name == name:
-                return pin
+    def find_pin_by_name(self, pad):
+        for pin in self.cpu_pins:
+            if pin.pad == pad:
+                return pad
 
-    def parse_board_file(self, filename, cpu_pin_col):
+    def parse_board_file(self, filename):
         with open(filename, "r") as csvfile:
             rows = csv.reader(csvfile)
             for row in rows:
-                # Pin numbers must start from 0 when used with the TI API
-                if row[cpu_pin_col].isdigit():
-                    pin = self.find_pin_by_num(int(row[cpu_pin_col]) - 1)
-                else:
-                    pin = self.find_pin_by_name(row[cpu_pin_col])
-                if pin:
-                    pin.board_pin = True
+                pad = self.find_pin_by_name(row[1])  #TODO: create replacement for magic number
+                if pad and row[0]: #TODO: create replacement for magic number
+                    self.board_pins.append(NamedPin(row[0], pad))
 
     def parse_af_file(self, filename, pad_col, af_start_col):
+        af_end_col = af_start_col + MAX_AF
         with open(filename, "r") as csvfile:
             rows = csv.reader(csvfile)
+            header = next(rows)
             for row in rows:
-                pad = parse_pad(row[pad_col])
+                pad = row[pad_col]
+                gpio, pin = row[6].split("_")
+                pin_number = pin.lstrip("IO")
+
+                pin = Pin(pad, gpio, pin_number)
+
                 # Parse alternate functions
                 af_idx = 0
-                for af in row[af_start_col:]:
+                for af_idx, af in enumerate(row[af_start_col:af_end_col]):
+                    if af and af_supported(af):
+                        pin.add_af(AlternateFunction(af_idx, af))
+                """
                     af_splitted = af.split("_")
                     fn_name = af_splitted[0].rstrip("0123456789")
                     if fn_name in SUPPORTED_AFS:
@@ -151,25 +188,21 @@ class Pins(object):
                             unit_idx = af_splitted[0][-1]
                             pin.add_af(AF(af, af_idx, fn_name, int(unit_idx), type_name))
                     af_idx += 1
-                
-                # Pin numbers must start from 0 when used with the TI API
-                pin_num = int(row[pin_col]) - 1
-                pin = Pin(pad, port_num, gpio_bit, pin_num)
-                self.board_pins.append(pin)
+                """
+                self.cpu_pins.append(pin)
 
-
-    def print_named(self, label, pins):
+    @staticmethod
+    def print_named(label, pins):
         print("")
         print(
             "STATIC const mp_rom_map_elem_t pin_{:s}_pins_locals_dict_table[] = {{".format(label)
         )
         for pin in pins:
-            if pin.board_pin:
-                print(
-                    "    {{ MP_ROM_QSTR(MP_QSTR_{:6s}), MP_ROM_PTR(&pin_{:6s}) }},".format(
-                        pin.name, pin.name
-                    )
+            print(
+                "    {{ MP_ROM_QSTR(MP_QSTR_{}), MP_ROM_PTR(&pin_{}) }},".format(
+                    pin.name, pin.pad
                 )
+            )
         print("};")
         print(
             "MP_DEFINE_CONST_DICT(pin_{:s}_pins_locals_dict, pin_{:s}_pins_locals_dict_table);".format(
@@ -178,9 +211,9 @@ class Pins(object):
         )
 
     def print(self):
-        for pin in self.board_pins:
-            if pin.board_pin:
-                pin.print()
+        for pin in self.cpu_pins:
+            pin.print()
+        self.print_named("cpu", self.cpu_pins)
         self.print_named("board", self.board_pins)
         print("")
 
@@ -211,11 +244,11 @@ def main():
 
     if args.af_filename:
         print("// --af {:s}".format(args.af_filename))
-        pins.parse_af_file(args.af_filename, 0, 0, 1, 3)
+        pins.parse_af_file(args.af_filename, 0, 1)
 
     if args.board_filename:
         print("// --board {:s}".format(args.board_filename))
-        pins.parse_board_file(args.board_filename, 1)
+        pins.parse_board_file(args.board_filename)
 
     pins.print()
 
