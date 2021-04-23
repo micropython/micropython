7b6531f88d6ea8000afe8d5ea0d948fdaf9901df
Themba Dube <embeddedthemba@gmail.com>
2021-04-23 17:25:02 -0400

    fix(stm32) minor changes to work with v8

--- ports/stm32/mpconfigport.h
+++ ports/stm32/mpconfigport.h
@@ -363,10 +363,10 @@ extern const struct _mp_obj_module_t mp_module_lodepng;
 #if MICROPY_PY_LVGL
 #ifndef MICROPY_INCLUDED_PY_MPSTATE_H
 #define MICROPY_INCLUDED_PY_MPSTATE_H
-#include "lib/lv_bindings/lvgl/src/lv_misc/lv_gc.h"
+#include "lib/lv_bindings/lvgl/src/misc/lv_gc.h"
 #undef MICROPY_INCLUDED_PY_MPSTATE_H
 #else
-#include "lib/lv_bindings/lvgl/src/lv_misc/lv_gc.h"
+#include "lib/lv_bindings/lvgl/src/misc/lv_gc.h"
 #endif
 #else
 #define LV_ROOTS
