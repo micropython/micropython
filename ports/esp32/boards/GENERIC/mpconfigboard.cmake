e3af123aa3738a944a4a40feaa745538b3a23dbe
Steve App <hklang10@gmail.com>
2021-04-25 11:05:47 +0100

    esp32: Restore FROZEN_MANIFEST support with new CMake build system.

--- ports/esp32/boards/GENERIC/mpconfigboard.cmake
+++ ports/esp32/boards/GENERIC/mpconfigboard.cmake
@@ -2,5 +2,6 @@ set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.base
     boards/sdkconfig.ble
 )
-
-set(MICROPY_FROZEN_MANIFEST ${MICROPY_PORT_DIR}/boards/manifest.py)
+if(NOT MICROPY_FROZEN_MANIFEST)
+    set(MICROPY_FROZEN_MANIFEST ${MICROPY_PORT_DIR}/boards/manifest.py)
+endif()
