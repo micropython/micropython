da1fcd5f908b21cd418366f2aa14ac4c71013509
Jan Jurgen Griesfeller <jang@met.no>
2021-04-29 09:26:38 +0200

    added board definition for Sparkfun Thin Plus RP2040

---
+++ ports/rp2/boards/SPARKFUN_THINGPLUS/mpconfigboard.h
@@ -0,0 +1,3 @@
+// Board and hardware specific configuration
+#define MICROPY_HW_BOARD_NAME                   "Sparkfun Thing Plus RP2040"
+#define MICROPY_HW_FLASH_STORAGE_BYTES          (15 * 1024 * 1024)
