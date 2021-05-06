bd557e152e2184c6de25dfc412811b9307e89e7f
Philipp Ebensberger <philipp.ebensberger@3bricks-software.de>
2021-05-06 11:04:34 +0200

    mimxrt/machine_pin: Implemented call class method.

--- ports/mimxrt/machine_pin.c
+++ ports/mimxrt/machine_pin.c
@@ -33,14 +33,59 @@
 #include "pin.h"
 #include "mphalport.h"
 
+// Local functions
+STATIC mp_obj_t pin_obj_init_helper(machine_pin_obj_t *self, size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args);
+STATIC void named_pin_obj_print(const mp_print_t *print, mp_obj_t self_in, mp_print_kind_t kind);
 
+// Class Methods
+STATIC void machine_pin_obj_print(const mp_print_t *print, mp_obj_t o, mp_print_kind_t kind);
+STATIC mp_obj_t machine_pin_call(mp_obj_t self_in, mp_uint_t n_args, mp_uint_t n_kw, const mp_obj_t *args);
+STATIC mp_obj_t machine_pin_obj_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args);
+
+// Instance Methods
+STATIC mp_obj_t machine_pin_off(mp_obj_t self_in);
+STATIC mp_obj_t machine_pin_on(mp_obj_t self_in);
+STATIC mp_obj_t machine_pin_value(size_t n_args, const mp_obj_t *args);
+STATIC mp_obj_t machine_pin_mode(mp_obj_t self_in);
+
+// Local data
 enum {
     PIN_INIT_ARG_MODE = 0,
     PIN_INIT_ARG_VALUE
 };
 
+// Pin mapping dictionaries
+const mp_obj_type_t machine_pin_cpu_pins_obj_type = {
+    { &mp_type_type },
+    .name = MP_QSTR_cpu,
+    .print = named_pin_obj_print,
+    .locals_dict = (mp_obj_t)&machine_pin_cpu_pins_locals_dict,
+};
 
-// Helper Functions
+const mp_obj_type_t machine_pin_board_pins_obj_type = {
+    { &mp_type_type },
+    .name = MP_QSTR_board,
+    .print = named_pin_obj_print,
+    .locals_dict = (mp_obj_t)&machine_pin_board_pins_locals_dict,
+};
+
+
+STATIC mp_obj_t machine_pin_call(mp_obj_t self_in, mp_uint_t n_args, mp_uint_t n_kw, const mp_obj_t *args) {
+    mp_arg_check_num(n_args, n_kw, 0, 1, false);
+    machine_pin_obj_t *self = self_in;
+
+    if (n_args == 0) {
+        return MP_OBJ_NEW_SMALL_INT(mp_hal_pin_read(self));
+    } else {
+        if (MP_OBJ_SMALL_INT_VALUE(args[0]) == 1U) {
+            mp_hal_pin_high(self);
+        } else {
+            mp_hal_pin_low(self);
+        }
+        return mp_const_none;
+    }
+}
+
 STATIC mp_obj_t pin_obj_init_helper(machine_pin_obj_t *self, size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args) {
     static const mp_arg_t allowed_args[] = {
         [PIN_INIT_ARG_MODE] { MP_QSTR_mode, MP_ARG_REQUIRED | MP_ARG_INT },
@@ -111,23 +156,7 @@ STATIC void named_pin_obj_print(const mp_print_t *print, mp_obj_t self_in, mp_pr
     mp_printf(print, "<Pin.%q>", self->name);
 }
 
-// Pin mapping dictionaries
-const mp_obj_type_t machine_pin_cpu_pins_obj_type = {
-    { &mp_type_type },
-    .name = MP_QSTR_cpu,
-    .print = named_pin_obj_print,
-    .locals_dict = (mp_obj_t)&machine_pin_cpu_pins_locals_dict,
-};
-
-const mp_obj_type_t machine_pin_board_pins_obj_type = {
-    { &mp_type_type },
-    .name = MP_QSTR_board,
-    .print = named_pin_obj_print,
-    .locals_dict = (mp_obj_t)&machine_pin_board_pins_locals_dict,
-};
-
-
-STATIC void pin_obj_print(const mp_print_t *print, mp_obj_t o, mp_print_kind_t kind) {
+STATIC void machine_pin_obj_print(const mp_print_t *print, mp_obj_t o, mp_print_kind_t kind) {
     (void)kind;
     machine_pin_obj_t *self = MP_OBJ_TO_PTR(o);
     mp_printf(print, "PIN(%s)", qstr_str(self->name));
@@ -168,11 +197,11 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_1(machine_pin_on_obj, machine_pin_on);
 // pin.value()
 STATIC mp_obj_t machine_pin_value(size_t n_args, const mp_obj_t *args) {
     machine_pin_obj_t *self = args[0];
-    if ((n_args > 1) && (self->mode == PIN_MODE_OUT)) {
+    if (n_args > 1) {
         if (MP_OBJ_SMALL_INT_VALUE(args[1]) == 1U) {
-            machine_pin_on(args[0]);
+            mp_hal_pin_high(self);
         } else {
-            machine_pin_off(args[0]);
+            mp_hal_pin_low(self);
         }
         return mp_const_none;
     } else {
@@ -210,7 +239,8 @@ STATIC MP_DEFINE_CONST_DICT(machine_pin_locals_dict, machine_pin_locals_dict_tab
 const mp_obj_type_t machine_pin_type = {
     {&mp_type_type},
     .name = MP_QSTR_Pin,
-    .print = pin_obj_print,
+    .print = machine_pin_obj_print,
+    .call = machine_pin_call,
     .make_new = machine_pin_obj_make_new,
     .locals_dict = (mp_obj_dict_t *)&machine_pin_locals_dict,
 };
@@ -219,7 +249,7 @@ const mp_obj_type_t machine_pin_type = {
 const mp_obj_type_t machine_pin_af_type = {
     {&mp_type_type},
     .name = MP_QSTR_PinAF,
-    .print = pin_obj_print,
+    .print = machine_pin_obj_print,
     .make_new = machine_pin_obj_make_new,
     .locals_dict = (mp_obj_dict_t *)&machine_pin_locals_dict,
 };
