dcfb7949d7b58dea7cb3d9c7991809c253b6d6b8
Neradoc <Neradoc@users.noreply.github.com>
2021-05-10 05:34:03 +0200

    fix FunHome to FunHouse
    FunHouse, sweet FunHouse.

--- ports/esp32s2/boards/adafruit_funhouse/mpconfigboard.h
+++ ports/esp32s2/boards/adafruit_funhouse/mpconfigboard.h
@@ -26,7 +26,7 @@
 
 // Micropython setup
 
-#define MICROPY_HW_BOARD_NAME       "Adafruit FunHome"
+#define MICROPY_HW_BOARD_NAME       "Adafruit FunHouse"
 #define MICROPY_HW_MCU_NAME         "ESP32S2"
 
 #define MICROPY_HW_APA_MOSI (&pin_GPIO14)
