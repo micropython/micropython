260440defb4b8a2ee30a1c2991f94dfba22b90d2
stijn <stijn@ignitron.net>
2021-05-12 17:02:06 +0200

    py/objarray: Prohibit comparison of mismatching types.
    Array equality is defined as each element being equal but to keep
    code size down MicroPython implements a binary comparison.  This
    can only be used correctly for elements with the same binary layout
    though so turn it into an NotImplementedError when comparing types
    for which the binary comparison yielded incorrect results: types
    with different sizes, and floating point numbers because nan != nan.

--- tests/basics/array1.py
+++ tests/basics/array1.py
@@ -41,13 +41,22 @@ except ValueError:
 # equality (CPython requires both sides are array)
 print(bytes(array.array('b', [0x61, 0x62, 0x63])) == b'abc')
 print(array.array('b', [0x61, 0x62, 0x63]) == b'abc')
+print(array.array('B', [0x61, 0x62, 0x63]) == b'abc')
 print(array.array('b', [0x61, 0x62, 0x63]) != b'abc')
 print(array.array('b', [0x61, 0x62, 0x63]) == b'xyz')
 print(array.array('b', [0x61, 0x62, 0x63]) != b'xyz')
 print(b'abc' == array.array('b', [0x61, 0x62, 0x63]))
+print(b'abc' == array.array('B', [0x61, 0x62, 0x63]))
 print(b'abc' != array.array('b', [0x61, 0x62, 0x63]))
 print(b'xyz' == array.array('b', [0x61, 0x62, 0x63]))
 print(b'xyz' != array.array('b', [0x61, 0x62, 0x63]))
+
+compatible_typecodes = []
+for t in ["b", "h", "i", "l", "q"]:
+  compatible_typecodes.append((t, t))
+  compatible_typecodes.append((t, t.upper()))
+for a, b in compatible_typecodes:
+  print(array.array(a, [1, 2]) == array.array(b, [1, 2]))
 
 class X(array.array):
     pass
