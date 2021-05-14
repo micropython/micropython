9e65662a11c37599299ba4ab9c3ddf0510306498
Mike Causer <mcauser@gmail.com>
2021-05-14 14:00:01 +1000

    esp32/boards: Set default I2C and SPI pins on UM_xxx boards.
    And fix incorrect I2C and SPI pins in the feathers2 module.

--- ports/esp32/boards/UM_FEATHERS2/mpconfigboard.h
+++ ports/esp32/boards/UM_FEATHERS2/mpconfigboard.h
@@ -3,3 +3,10 @@
 
 #define MICROPY_PY_BLUETOOTH                (0)
 #define MICROPY_HW_ENABLE_SDCARD            (0)
+
+#define MICROPY_HW_I2C0_SCL (9)
+#define MICROPY_HW_I2C0_SDA (8)
+
+#define MICROPY_HW_SPI1_MOSI (35)  // SDO
+#define MICROPY_HW_SPI1_MISO (37)  // SDI
+#define MICROPY_HW_SPI1_SCK (36)
