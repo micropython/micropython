6efad5c728f5fd18e71ff6a214b97a31ef99270b
Mike Causer <mcauser@gmail.com>
2021-05-14 14:01:10 +1000

    esp32/boards/UM_FEATHERS2: Set default SPI pins.

--- ports/esp32/boards/UM_FEATHERS2/mpconfigboard.h
+++ ports/esp32/boards/UM_FEATHERS2/mpconfigboard.h
@@ -7,3 +7,6 @@
 #define MICROPY_HW_I2C0_SCL (9)
 #define MICROPY_HW_I2C0_SDA (8)
 
+#define MICROPY_HW_SPI1_MOSI (35)  // SDO
+#define MICROPY_HW_SPI1_MISO (37)  // SDI
+#define MICROPY_HW_SPI1_SCK (36)
