ea0e18d2e0c904b86ea3fa45ecaf14b300e56905
Dan Halbert <halbert@halwitz.org>
2021-05-14 23:41:44 -0400

    fix logic for suppressing ja and ko clean builds when not necessary

--- ports/atmel-samd/mpconfigport.mk
+++ ports/atmel-samd/mpconfigport.mk
@@ -60,18 +60,19 @@ CIRCUITPY_VECTORIO = 0
 
 MICROPY_PY_ASYNC_AWAIT = 0
 
-ifeq ($(TRANSLATION),ja)
-ifeq ($(CIRCUITPY_DISPLAYIO),1)
-RELEASE_NEEDS_CLEAN_BUILD = 1
+# We don't have room for the fonts for terminalio for ja and ko
+# so turn off terminalio, and if it's off and displayio is on,
+# force a clean build.
+# Note that we cannot test $(CIRCUITPY_DISPLAYIO) directly with an
+# ifeq, because it's not set yet.
+ifeq ($(TRANSLATION), ja)
 CIRCUITPY_TERMINALIO = 0
-endif
+RELEASE_NEEDS_CLEAN_BUILD = $(CIRCUITPY_DISPLAYIO)
 endif
 
-ifeq ($(TRANSLATION),ko)
-ifeq ($(CIRCUITPY_DISPLAYIO),1)
-RELEASE_NEEDS_CLEAN_BUILD = 1
+ifeq ($(TRANSLATION), ko)
 CIRCUITPY_TERMINALIO = 0
-endif
+RELEASE_NEEDS_CLEAN_BUILD = $(CIRCUITPY_DISPLAYIO)
 endif
 
 SUPEROPT_GC = 0
