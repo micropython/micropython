745b4319a21cfebefead76c7fcacd0b619d48063
robert-hh <robert@hammelrath.com>
2021-05-17 12:31:11 +0200

    mimxrt/boards/TEENSY40: Re-create the flash FS after deploy.
    This is required since the Teensy Halfkay loader attempts to erase all of
    the flash but fails to do so, at least in my tests.  Formatting brings it
    back to a known state.

--- ports/mimxrt/boards/TEENSY40/mpconfigboard.mk
+++ ports/mimxrt/boards/TEENSY40/mpconfigboard.mk
@@ -3,3 +3,5 @@ MCU_VARIANT = MIMXRT1062DVJ6A
 
 deploy: $(BUILD)/firmware.hex
 	teensy_loader_cli --mcu=imxrt1062 -v -w $<
+	sleep 1
+	$(PYTHON) $(TOP)/tools/pyboard.py --device $(PORT) $(BOARD_DIR)/format.py
