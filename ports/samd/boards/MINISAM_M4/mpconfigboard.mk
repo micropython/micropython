3d33dbedc9d9f6df19e68461dc66cdff08ed01cc
Peter van der Burg <vdb_peter@hotmail.com>
2021-05-22 17:04:04 +1000

    samd/boards/MINISAM_M4: Update for flash and pins.
    - mpconfigboard.h: flash and USART config
    - mpconfigboard.mk: enable LFS1
    - pins.c: define pins and LEDs
    - pins.h: define structs and consts

--- ports/samd/boards/MINISAM_M4/mpconfigboard.mk
+++ ports/samd/boards/MINISAM_M4/mpconfigboard.mk
@@ -3,3 +3,7 @@ MCU_SERIES = SAMD51
 CMSIS_MCU = SAMD51G19A
 LD_FILES = boards/samd51g19a.ld sections.ld
 TEXT0 = 0x4000
+
+# The ?='s allow overriding in mpconfigboard.mk.
+# MicroPython settings
+MICROPY_VFS_LFS1 ?= 1
