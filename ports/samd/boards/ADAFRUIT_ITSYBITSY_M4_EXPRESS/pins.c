33f0130b1c1e400ca757ac34991a999bdcc8f362
Peter van der Burg <vdb_peter@hotmail.com>
2021-05-24 14:02:17 +1000

    ports/samd/boards/ADAFRUIT_ITSYBITSY_M4_EXPRESS/pins.c: Reordered pins to match Feather_M0

--- ports/samd/boards/ADAFRUIT_ITSYBITSY_M4_EXPRESS/pins.c
+++ ports/samd/boards/ADAFRUIT_ITSYBITSY_M4_EXPRESS/pins.c
@@ -31,26 +31,26 @@
 
 // Ensure Declaration in 'pins.h' reflects # of Pins defined here.
 const machine_pin_obj_t machine_pin_obj[] = {
-    {{&machine_pin_type}, PIN_PA02}, // A0
-    {{&machine_pin_type}, PIN_PA05}, // A1
-    {{&machine_pin_type}, PIN_PB08}, // A2
-    {{&machine_pin_type}, PIN_PB09}, // A3
-    {{&machine_pin_type}, PIN_PA04}, // A4
-    {{&machine_pin_type}, PIN_PA06}, // A5
     {{&machine_pin_type}, PIN_PA16}, // RX_D0
     {{&machine_pin_type}, PIN_PA17}, // TX_D1
     {{&machine_pin_type}, PIN_PA07}, // D2
     {{&machine_pin_type}, PIN_PB22}, // D3
     {{&machine_pin_type}, PIN_PA14}, // D4
     {{&machine_pin_type}, PIN_PA15}, // D5
-    {{NULL}, -1}, // D6
+    {{NULL}, -1}, // D6- not terminated on breakout.
     {{&machine_pin_type}, PIN_PA18}, // D7
-    {{NULL}, -1}, // D8
+    {{NULL}, -1}, // D8- not terminated on breakout.
     {{&machine_pin_type}, PIN_PA19}, // D9
     {{&machine_pin_type}, PIN_PA20}, // D10
     {{&machine_pin_type}, PIN_PA21}, // D11
     {{&machine_pin_type}, PIN_PA23}, // D12
     {{&machine_pin_type}, PIN_PA22}, // D13
+	{{&machine_pin_type}, PIN_PA02}, // A0
+    {{&machine_pin_type}, PIN_PA05}, // A1
+    {{&machine_pin_type}, PIN_PB08}, // A2
+    {{&machine_pin_type}, PIN_PB09}, // A3
+    {{&machine_pin_type}, PIN_PA04}, // A4
+    {{&machine_pin_type}, PIN_PA06}, // A5
 };
 
 const machine_led_obj_t machine_led_obj[] = {
