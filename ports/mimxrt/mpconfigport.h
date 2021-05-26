dc1ef853e9540c03c51e45450e6e3c00e4e521af
robert-hh <robert@hammelrath.com>
2021-05-26 11:29:34 +0200

    mimxrt: Add floating point support.
    Since not all boards support double fp, all board specific .mk files
    are affected too.

--- ports/mimxrt/mpconfigport.h
+++ ports/mimxrt/mpconfigport.h
@@ -30,44 +30,45 @@
 #include "mpconfigboard.h"
 
 // Memory allocation policies
-#define MICROPY_GC_STACK_ENTRY_TYPE         uint16_t
-#define MICROPY_GC_ALLOC_THRESHOLD          (0)
-#define MICROPY_ALLOC_PARSE_CHUNK_INIT      (32)
-#define MICROPY_ALLOC_PATH_MAX              (256)
-#define MICROPY_QSTR_BYTES_IN_HASH          (1)
+#define MICROPY_GC_STACK_ENTRY_TYPE             uint16_t
+#define MICROPY_GC_ALLOC_THRESHOLD              (0)
+#define MICROPY_ALLOC_PARSE_CHUNK_INIT          (32)
+#define MICROPY_ALLOC_PATH_MAX                  (256)
+#define MICROPY_QSTR_BYTES_IN_HASH              (1)
 
-// Compiler configuration
-#define MICROPY_COMP_CONST                  (0)
+// Compiler configuration   
+#define MICROPY_COMP_CONST                      (0)
 
-// Python internal features
-#define MICROPY_ENABLE_GC                   (1)
-#define MICROPY_KBD_EXCEPTION               (1)
-#define MICROPY_HELPER_REPL                 (1)
-#define MICROPY_LONGINT_IMPL                (MICROPY_LONGINT_IMPL_MPZ)
-#define MICROPY_ENABLE_SOURCE_LINE          (1)
-#define MICROPY_ERROR_REPORTING             (MICROPY_ERROR_REPORTING_TERSE)
-#define MICROPY_CAN_OVERRIDE_BUILTINS       (1)
+// Python internal features 
+#define MICROPY_ENABLE_GC                       (1)
+#define MICROPY_KBD_EXCEPTION                   (1)
+#define MICROPY_HELPER_REPL                     (1)
+#define MICROPY_LONGINT_IMPL                    (MICROPY_LONGINT_IMPL_MPZ)
+#define MICROPY_ENABLE_SOURCE_LINE              (1)
 
-// Control over Python builtins
-#define MICROPY_PY_BUILTINS_STR_COUNT       (0)
-#define MICROPY_PY_BUILTINS_MEMORYVIEW      (1)
-#define MICROPY_PY_BUILTINS_SET             (1)
-#define MICROPY_PY_BUILTINS_FROZENSET       (0)
-#define MICROPY_PY_BUILTINS_PROPERTY        (0)
-#define MICROPY_PY_BUILTINS_ENUMERATE       (0)
-#define MICROPY_PY_BUILTINS_FILTER          (0)
-#define MICROPY_PY_BUILTINS_REVERSED        (1)
-#define MICROPY_PY_BUILTINS_MIN_MAX         (0)
-#define MICROPY_PY_BUILTINS_HELP            (1)
-#define MICROPY_PY___FILE__                 (0)
-#define MICROPY_PY_MICROPYTHON_MEM_INFO     (1)
-#define MICROPY_PY_ARRAY_SLICE_ASSIGN       (1)
-#define MICROPY_PY_COLLECTIONS              (0)
-#define MICROPY_PY_SYS_MAXSIZE              (1)
+#define MICROPY_ERROR_REPORTING                 (MICROPY_ERROR_REPORTING_TERSE)
+#define MICROPY_CAN_OVERRIDE_BUILTINS           (1)
+
+// Control over Python builtins 
+#define MICROPY_PY_BUILTINS_STR_COUNT           (0)
+#define MICROPY_PY_BUILTINS_MEMORYVIEW          (1)
+#define MICROPY_PY_BUILTINS_SET                 (1)
+#define MICROPY_PY_BUILTINS_FROZENSET           (0)
+#define MICROPY_PY_BUILTINS_PROPERTY            (0)
+#define MICROPY_PY_BUILTINS_ENUMERATE           (0)
+#define MICROPY_PY_BUILTINS_FILTER              (0)
+#define MICROPY_PY_BUILTINS_REVERSED            (1)
+#define MICROPY_PY_BUILTINS_MIN_MAX             (0)
+#define MICROPY_PY_BUILTINS_HELP                (1)
+#define MICROPY_PY___FILE__                     (0)
+#define MICROPY_PY_MICROPYTHON_MEM_INFO         (1)
+#define MICROPY_PY_ARRAY_SLICE_ASSIGN           (1)
+#define MICROPY_PY_COLLECTIONS                  (0)
+#define MICROPY_PY_SYS_MAXSIZE                  (1)
 
 // Extended modules
-#define MICROPY_PY_UTIME_MP_HAL             (1)
-#define MICROPY_PY_MACHINE                  (1)
+#define MICROPY_PY_UTIME_MP_HAL                 (1)
+#define MICROPY_PY_MACHINE                      (1)
 
 // Hooks to add builtins
 
