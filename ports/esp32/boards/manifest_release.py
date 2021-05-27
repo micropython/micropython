db8704ecbd910e598fba4bc1817acf91d9da79e3
Damien George <damien@micropython.org>
2021-05-28 00:09:34 +1000

    esp8266,esp32: Update manifest to point to new dirs in micropython-lib.
    Following a refactoring of micropython-lib.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/esp32/boards/manifest_release.py
+++ ports/esp32/boards/manifest_release.py
@@ -1,6 +1,7 @@
 include("manifest.py")
 
-freeze("$(MPY_LIB_DIR)/upysh", "upysh.py")
-freeze("$(MPY_LIB_DIR)/urequests", "urequests.py")
-freeze("$(MPY_LIB_DIR)/umqtt.simple", "umqtt/simple.py")
-freeze("$(MPY_LIB_DIR)/umqtt.robust", "umqtt/robust.py")
+freeze("$(MPY_LIB_DIR)/python-ecosys/urequests", "urequests.py")
+
+freeze("$(MPY_LIB_DIR)/micropython/upysh", "upysh.py")
+freeze("$(MPY_LIB_DIR)/micropython/umqtt.simple", "umqtt/simple.py")
+freeze("$(MPY_LIB_DIR)/micropython/umqtt.robust", "umqtt/robust.py")
