10d3c754419ee54973dc2835411cffdc8fb270ed
robert-hh <robert@hammelrath.com>
2021-05-27 08:25:33 +0200

    mimxrt: Add making mpy-cross to ci.sh for mimxrt.
    And add the modules directory with an empty _boot.py.

---
+++ ports/mimxrt/modules/_boot.py
@@ -0,0 +1,1 @@
+# this is a placeholder for _boot.py avoiding an empty directory
--- tools/ci.sh
+++ tools/ci.sh
@@ -146,6 +146,7 @@ function ci_mimxrt_setup {
 }
 
 function ci_mimxrt_build {
+    make ${MAKEOPTS} -C mpy-cross
     make ${MAKEOPTS} -C ports/mimxrt submodules
     make ${MAKEOPTS} -C ports/mimxrt BOARD=MIMXRT1020_EVK
     make ${MAKEOPTS} -C ports/mimxrt BOARD=TEENSY40
