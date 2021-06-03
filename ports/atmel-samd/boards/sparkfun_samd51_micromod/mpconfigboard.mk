db1830d62b07d17c7fcb74dcfafaf60113f61e72
George White <stonehippo@gmail.com>
2021-06-02 21:14:02 -0400

    Add Sparkfun MicroMod SAMD51

---
+++ ports/atmel-samd/boards/sparkfun_samd51_micromod/mpconfigboard.mk
@@ -0,0 +1,14 @@
+LD_FILE = boards/samd51x20-bootloader-external-flash.ld
+USB_VID = 0x1b4f
+USB_PID = 0x0020 # Used by uf2 bootloader
+USB_PRODUCT = "SparkFun MicroMod SAMD51"
+USB_MANUFACTURER = "SparkFun Electronics"
+
+CHIP_VARIANT = SAMD51J20A
+CHIP_FAMILY = samd51
+
+SPI_FLASH_FILESYSTEM = 1
+EXTERNAL_FLASH_DEVICES = W25Q128JVxM
+LONGINT_IMPL = MPZ
+
+CIRCUITPY_PS2IO = 1
