e1efb792d45af4ee5429bdad28bf709384415f60
Mike Teachman <mike.teachman@gmail.com>
2021-06-03 09:43:56 -0700

    extmod/uasyncio: Add readinto() method to Stream class.
    Signed-off-by: Mike Teachman <mike.teachman@gmail.com>

--- extmod/uasyncio/stream.py
+++ extmod/uasyncio/stream.py
@@ -30,6 +30,10 @@ class Stream:
         yield core._io_queue.queue_read(self.s)
         return self.s.read(n)
 
+    async def readinto(self, buf):
+        yield core._io_queue.queue_read(self.s)
+        return self.s.readinto(buf)
+
     async def readexactly(self, n):
         r = b""
         while n:
