f85ece7a8d944169b60fbc9a7f5c43fdc3fcf67b
Braden Mars <bradenmars@bradenmars.me>
2021-07-04 21:34:24 -0500

    esp32/boards: Fix typo in mpconfigboard SOALA -> SAOLA.
    Signed-off-by: Braden Mars <bradenmars@bradenmars.me>

--- ports/esp32/boards/GENERIC_S2_SAOLA/mpconfigboard.h
+++ ports/esp32/boards/GENERIC_S2_SAOLA/mpconfigboard.h
@@ -1,4 +1,4 @@
-#define MICROPY_HW_BOARD_NAME               "ESP32-S2-SOALA-1"
+#define MICROPY_HW_BOARD_NAME               "ESP32-S2-SAOLA-1"
 #define MICROPY_HW_MCU_NAME                 "ESP32S2"
 
 #define MICROPY_PY_BLUETOOTH                (0)
