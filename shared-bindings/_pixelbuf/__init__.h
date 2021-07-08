825a706135f3de49ae6a6c3bd785da5f8201b68f
Kattni Rembor <kattni@adafruit.com>
2021-07-08 17:31:10 -0400

    Turn off MIDI on one build, fix name.

--- shared-bindings/_pixelbuf/__init__.h
+++ shared-bindings/_pixelbuf/__init__.h
@@ -3,7 +3,7 @@
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2018 Roy Hooper
+ * Copyright (c) 2018 Rose Hooper
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
