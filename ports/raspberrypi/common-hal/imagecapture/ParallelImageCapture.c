8221a12cf60fdd423eb60ed1b16e5d14d017bc80
Nathan Young <77929198+NathanY3G@users.noreply.github.com>
2021-07-22 18:39:04 +0200

    Added support for specifying the JMP pin (RP2 PIO)

--- ports/raspberrypi/common-hal/imagecapture/ParallelImageCapture.c
+++ ports/raspberrypi/common-hal/imagecapture/ParallelImageCapture.c
@@ -111,6 +111,7 @@ void common_hal_imagecapture_parallelimagecapture_construct(imagecapture_paralle
         #else
         NULL, 0, 0, 0, // sideset pins
         #endif
+        NULL, // jump pin
         (1 << vertical_sync->number) | (1 << horizontal_reference->number) | (1 << data_clock->number), // wait gpio pins
         true, // exclusive pin use
         false, 32, false, // out settings
