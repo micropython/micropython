23e2e0077910a7a47c0c0fc752b9056bdccfd5bb
iabdalkader <i.abdalkader@gmail.com>
2021-07-30 03:42:08 +0200

    rp2/boards/ADAFRUIT_FEATHER_RP2040: Configure custom VID/PID.

--- ports/rp2/boards/ADAFRUIT_FEATHER_RP2040/mpconfigboard.h
+++ ports/rp2/boards/ADAFRUIT_FEATHER_RP2040/mpconfigboard.h
@@ -1,3 +1,6 @@
 // Board and hardware specific configuration
 #define MICROPY_HW_BOARD_NAME                   "Adafruit Feather RP2040"
 #define MICROPY_HW_FLASH_STORAGE_BYTES          (3072 * 1024)
+
+#define MICROPY_HW_USB_VID (0x239A)
+#define MICROPY_HW_USB_PID (0x80F2)
