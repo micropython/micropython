f9393c9e51ecdf84b7209dddf8c050fd4151c6f5
Jeff Epler <jepler@unpythonic.net>
2021-08-05 11:12:07 -0500

    enable qrio in unix coverage build, and add a test

---
+++ tests/extmod/qrio.py
@@ -0,0 +1,13 @@
+try:
+    import qrio
+except:
+    print("SKIP")
+    raise SystemExit
+
+loc = __file__.rsplit("/", 1)[0]
+with open(f"{loc}/data/qr.pgm") as f:
+    content = f.read()[-320 * 240 :]
+
+decoder = qrio.QRDecoder(320, 240)
+for r in decoder.decode(content):
+    print(r)
