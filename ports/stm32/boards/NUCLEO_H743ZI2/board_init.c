f3986c7e4c779f3b57bfca71395f14a2b4a3c225
Ned Konz <ned@bike-nomad.com>
2021-08-09 21:33:07 -0700

    STM32: made NUCLEO_H743ZI2 include NUCLEO_H743ZI files where possible

--- ports/stm32/boards/NUCLEO_H743ZI2/board_init.c
+++ ports/stm32/boards/NUCLEO_H743ZI2/board_init.c
@@ -1,8 +1,1 @@
-#include "py/mphal.h"
-
-void NUCLEO_H743ZI2_board_early_init(void) {
-    // Turn off the USB switch
-    #define USB_PowerSwitchOn pin_D10
-    mp_hal_pin_output(USB_PowerSwitchOn);
-    mp_hal_pin_low(USB_PowerSwitchOn);
-}
+#include "boards/NUCLEO_H743ZI/board_init.c"
