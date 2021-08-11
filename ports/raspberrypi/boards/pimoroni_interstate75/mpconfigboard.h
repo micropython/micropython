47c8ff05707fb23814a44101d77df2f9cc2c737b
ZodiusInfuser <christopher.parrott2@gmail.com>
2021-08-11 23:34:20 +0100

    Removed unnecessary defines

--- ports/raspberrypi/boards/pimoroni_interstate75/mpconfigboard.h
+++ ports/raspberrypi/boards/pimoroni_interstate75/mpconfigboard.h
@@ -1,41 +1,10 @@
 #define MICROPY_HW_BOARD_NAME "Pimoroni Interstate 75"
 #define MICROPY_HW_MCU_NAME "rp2040"
 
-#define MICROPY_HW_R0 (&pin_GPIO0)
-#define MICROPY_HW_G0 (&pin_GPIO1)
-#define MICROPY_HW_B0 (&pin_GPIO2)
-#define MICROPY_HW_R1 (&pin_GPIO3)
-#define MICROPY_HW_G1 (&pin_GPIO4)
-#define MICROPY_HW_B1 (&pin_GPIO5)
-
-#define MICROPY_HW_ROW_A (&pin_GPIO6)
-#define MICROPY_HW_ROW_B (&pin_GPIO7)
-#define MICROPY_HW_ROW_C (&pin_GPIO8)
-#define MICROPY_HW_ROW_D (&pin_GPIO9)
-#define MICROPY_HW_ROW_E (&pin_GPIO10)
-
-#define MICROPY_HW_CLK (&pin_GPIO11)
-#define MICROPY_HW_LAT (&pin_GPIO12)
-#define MICROPY_HW_OE (&pin_GPIO13)
-
-#define MICROPY_HW_SW_A (&pin_GPIO14)
-
 #define CIRCUITPY_RGB_STATUS_INVERTED_PWM
 #define CIRCUITPY_RGB_STATUS_R (&pin_GPIO16)
 #define CIRCUITPY_RGB_STATUS_G (&pin_GPIO17)
 #define CIRCUITPY_RGB_STATUS_B (&pin_GPIO18)
 
-#define MICROPY_HW_I2C_INT (&pin_GPIO19)
-
 #define DEFAULT_I2C_BUS_SCL (&pin_GPIO21)
 #define DEFAULT_I2C_BUS_SDA (&pin_GPIO20)
-
-#define MICROPY_HW_USER_SW (&pin_GPIO23)
-
-#define MICROPY_HW_CURRENT_SENSE (&pin_GPIO29)
-
-// These pins are unconnected
-#define IGNORE_PIN_GPIO15   1
-#define IGNORE_PIN_GPIO22   1
-#define IGNORE_PIN_GPIO24   1
-#define IGNORE_PIN_GPIO25   1
--- ports/raspberrypi/boards/pimoroni_plasma2040/mpconfigboard.h
+++ ports/raspberrypi/boards/pimoroni_plasma2040/mpconfigboard.h
@@ -1,39 +1,10 @@
 #define MICROPY_HW_BOARD_NAME "Pimoroni Plasma 2040"
 #define MICROPY_HW_MCU_NAME "rp2040"
 
-#define MICROPY_HW_SW_A (&pin_GPIO12)
-#define MICROPY_HW_SW_B (&pin_GPIO13)
-
-#define MICROPY_HW_CLK (&pin_GPIO14)
-#define MICROPY_HW_DATA (&pin_GPIO15)
-
 #define CIRCUITPY_RGB_STATUS_INVERTED_PWM
 #define CIRCUITPY_RGB_STATUS_R (&pin_GPIO16)
 #define CIRCUITPY_RGB_STATUS_G (&pin_GPIO17)
 #define CIRCUITPY_RGB_STATUS_B (&pin_GPIO18)
 
-#define MICROPY_HW_I2C_INT (&pin_GPIO19)
-
 #define DEFAULT_I2C_BUS_SCL (&pin_GPIO21)
 #define DEFAULT_I2C_BUS_SDA (&pin_GPIO20)
-
-#define MICROPY_HW_USER_SW (&pin_GPIO23)
-
-#define MICROPY_HW_CURRENT_SENSE (&pin_GPIO29)
-
-// These pins are unconnected
-#define IGNORE_PIN_GPIO0    1
-#define IGNORE_PIN_GPIO1    1
-#define IGNORE_PIN_GPIO2    1
-#define IGNORE_PIN_GPIO3    1
-#define IGNORE_PIN_GPIO4    1
-#define IGNORE_PIN_GPIO5    1
-#define IGNORE_PIN_GPIO6    1
-#define IGNORE_PIN_GPIO7    1
-#define IGNORE_PIN_GPIO8    1
-#define IGNORE_PIN_GPIO9    1
-#define IGNORE_PIN_GPIO10   1
-#define IGNORE_PIN_GPIO11   1
-#define IGNORE_PIN_GPIO22   1
-#define IGNORE_PIN_GPIO24   1
-#define IGNORE_PIN_GPIO25   1
