8c214ed2000fd6334c8f03589f36c6fd2286f2c8
Ned Konz <ned@bike-nomad.com>
2021-08-19 08:12:19 -0700

    stm32: Extended flash filesystem space to 512K on H743 boards.
    The H743 has equal sized pages of 128k, which means the filesystem doesn't
    need to be near the beginning.  This commit moves the filesystem to the
    very end of flash, and extends it to 512k (4 pages).
    
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/stm32/boards/NUCLEO_H743ZI/mpconfigboard.mk
+++ ports/stm32/boards/NUCLEO_H743ZI/mpconfigboard.mk
@@ -7,14 +7,13 @@ MICROPY_FLOAT_IMPL = double
 AF_FILE = boards/stm32h743_af.csv
 
 ifeq ($(USE_MBOOT),1)
-# When using Mboot all the text goes together after the filesystem
-LD_FILES = boards/stm32h743.ld boards/common_blifs.ld
-TEXT0_ADDR = 0x08040000
+# When using Mboot everything goes after the bootloader
+LD_FILES = boards/stm32h743.ld boards/common_bl.ld
+TEXT0_ADDR = 0x08020000
 else
-# When not using Mboot the ISR text goes first, then the rest after the filesystem
-LD_FILES = boards/stm32h743.ld boards/common_ifs.ld
+# When not using Mboot everything goes at the start of flash
+LD_FILES = boards/stm32h743.ld boards/common_basic.ld
 TEXT0_ADDR = 0x08000000
-TEXT1_ADDR = 0x08040000
 endif
 
 # MicroPython settings
