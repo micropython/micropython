d79a9e0e69603f2d22114e23a80637e37e1652b0
Christian Walther <cwalther@gmx.ch>
2021-08-20 18:06:40 +0200

    Fix linker error when ONEWIREIO = 0
    This addendum to #5139 allows actually turning off onewireio. (Not
    currently used by any board.)

--- shared-bindings/bitbangio/__init__.c
+++ shared-bindings/bitbangio/__init__.c
@@ -72,7 +72,9 @@
 STATIC const mp_rom_map_elem_t bitbangio_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_bitbangio) },
     { MP_ROM_QSTR(MP_QSTR_I2C),   MP_ROM_PTR(&bitbangio_i2c_type) },
+    #if CIRCUITPY_ONEWIREIO
     { MP_ROM_QSTR(MP_QSTR_OneWire),   MP_ROM_PTR(&onewireio_onewire_type) },
+    #endif
     { MP_ROM_QSTR(MP_QSTR_SPI),   MP_ROM_PTR(&bitbangio_spi_type) },
 };
 
