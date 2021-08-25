17af5cd492e926c2d3a2c06b6519242bf8f3885c
Rob Capellini <rob.capellini@gmail.com>
2021-08-24 20:07:10 -0400

    Use MP_REGISTER_MODULE with displayio, terminalio, and fontio
    Convert from using MICROPY_PORT_BUILTIN_MODULES_STRONG_LINKS to using MP_REGISTER_MODULE for displayio, terminalio, and fontio modules.
    
    Related to #5183.

--- shared-bindings/fontio/__init__.c
+++ shared-bindings/fontio/__init__.c
@@ -48,3 +48,5 @@ const mp_obj_module_t fontio_module = {
     .base = { &mp_type_module },
     .globals = (mp_obj_dict_t *)&fontio_module_globals,
 };
+
+MP_REGISTER_MODULE(MP_QSTR_fontio, fontio_module, CIRCUITPY_DISPLAYIO && CIRCUITPY_TERMINALIO);
