9bfa2a416d6fd8377a48869525a023794a96b916
Seon Rozenblum <seon@unexpectedmaker.com>
2021-09-02 18:09:08 +1000

    Added STAGE support for my TinyS2 board

--- ports/esp32s2/boards/unexpectedmaker_tinys2/mpconfigboard.mk
+++ ports/esp32s2/boards/unexpectedmaker_tinys2/mpconfigboard.mk
@@ -16,5 +16,7 @@ CIRCUITPY_ESP_FLASH_SIZE=4MB
 
 CIRCUITPY_BITBANG_NEOPIXEL = 1
 
+CIRCUITPY_STAGE = 1
+
 # Include these Python libraries in firmware.
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_NeoPixel
