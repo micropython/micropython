7dbf750dd0b0bde966e84da4543fd3a16f346d13
microDev <70126934+microDev1@users.noreply.github.com>
2021-09-03 20:44:03 +0530

    add test for traceback module

---
+++ tests/circuitpython/traceback_test.py
@@ -0,0 +1,25 @@
+try:
+    import traceback
+except ImportError:
+    print("SKIP")
+    raise SystemExit
+
+
+def fun():
+    raise Exception("test")
+
+
+try:
+    fun()
+except Exception as exc:
+    print("\nNo Trace:")
+    traceback.print_exception(None, exc, None)
+    print("\nDefault Trace:")
+    traceback.print_exception(None, exc, exc.__traceback__)
+    print("\nLimit=1 Trace:")
+    traceback.print_exception(None, exc, exc.__traceback__, limit=1)
+    print("\nLimit=0 Trace:")
+    traceback.print_exception(None, exc, exc.__traceback__, limit=0)
+    print("\nLimit=-1 Trace:")
+    traceback.print_exception(None, exc, exc.__traceback__, limit=-1)
+    print("")
