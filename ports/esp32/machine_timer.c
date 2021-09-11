a9bf97f646a4ba12e0341bab6b3dfd38e1d94949
Seon Rozenblum <seon@unexpectedmaker.com>
2021-09-11 14:48:11 +1000

    port/esp32s3: IDF 4.4.x is now required for S3 support.

--- ports/esp32/machine_timer.c
+++ ports/esp32/machine_timer.c
@@ -137,7 +137,11 @@ STATIC void machine_timer_isr(void *self_in) {
     #if CONFIG_IDF_TARGET_ESP32
     device->hw_timer[self->index].update = 1;
     #else
+    #if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 4, 0)
+    device->hw_timer[self->index].update.tx_update = 1;
+    #else
     device->hw_timer[self->index].update.update = 1;
+    #endif
     #endif
     timer_ll_clear_intr_status(device, self->index);
     timer_ll_set_alarm_enable(device, self->index, self->repeat);
