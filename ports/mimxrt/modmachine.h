c2e4759cfaa4080356424b2602d90085a99bfb2b
robert-hh <robert@hammelrath.com>
2021-09-12 16:44:24 +0200

    mimxrt/modmachine: Implement machine.WDT() and machine.reset_cause().
    The API follows that of rp2, stm32, esp32, and the docs.
    
        wdt=machine.WDT(0, timeout)
    
            Timeout is given in ms. The valid range is 500 to 128000 (128
            seconds) with 500 ms granularity. Values outside of that range will
            be silently aligned.
    
        wdt.feed()
    
            Resets the watchdog timer (feeding).
    
        wdt.timeout_ms(value)
    
            Sets a new timeout and feeds the watchdog.
    
            This is a new, preliminary method which is not yet documented.
    
        reset_cause = machine.reset_cause()
    
            Values returned:
    
            1  Power On reset
            3  Watchdog reset
            5  Software reset: state after calling machine.reset()
    
    More elaborate API functions are supported by the MCU, like an interrupt
    called a certain time after feeding.  But for port cosistency that is not
    implemented.

--- ports/mimxrt/modmachine.h
+++ ports/mimxrt/modmachine.h
@@ -30,12 +30,13 @@
 #include "py/obj.h"
 
 extern const mp_obj_type_t machine_adc_type;
-extern const mp_obj_type_t machine_timer_type;
-extern const mp_obj_type_t machine_rtc_type;
 extern const mp_obj_type_t machine_i2c_type;
-extern const mp_obj_type_t machine_spi_type;
-extern const mp_obj_type_t machine_uart_type;
+extern const mp_obj_type_t machine_rtc_type;
 extern const mp_obj_type_t machine_sdcard_type;
+extern const mp_obj_type_t machine_spi_type;
+extern const mp_obj_type_t machine_timer_type;
+extern const mp_obj_type_t machine_uart_type;
+extern const mp_obj_type_t machine_wdt_type;
 
 void machine_adc_init(void);
 void machine_pin_irq_deinit(void);
