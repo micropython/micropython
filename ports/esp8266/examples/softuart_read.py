b883820869601b732709284f783fed132ead3713
MrJake222 <norbertzpilicy@gmail.com>
2021-09-14 00:20:58 +0200

    esp8266/examples: Fixed code formatting.

--- ports/esp8266/examples/softuart_read.py
+++ ports/esp8266/examples/softuart_read.py
@@ -6,19 +6,21 @@ import select
 
 poll = select.poll()
 
-s1 = SoftUART(Pin(2), Pin(4), baudrate=9600, timeout=0) # tx=2 rx=4
-s2 = SoftUART(Pin(0), Pin(5), baudrate=9600, timeout=0) # tx=0 rx=5
+s1 = SoftUART(Pin(2), Pin(4), baudrate=9600, timeout=0)  # tx=2 rx=4
+s2 = SoftUART(Pin(0), Pin(5), baudrate=9600, timeout=0)  # tx=0 rx=5
 poll.register(s1)
 poll.register(s2)
 
 print("polling...")
 
 while True:
-	read = poll.ipoll()
+    read = poll.ipoll()
 
-	for r, ev in read:
-		if (ev & select.POLLIN) != 0:
-			i=None
-			if r is s1: i="s1"
-			else: i="s2"
-			print(i, r.read())
+    for r, ev in read:
+        if (ev & select.POLLIN) != 0:
+            i = None
+            if r is s1:
+                i = "s1"
+            else:
+                i = "s2"
+            print(i, r.read())
--- ports/esp8266/examples/softuart_write.py
+++ ports/esp8266/examples/softuart_write.py
@@ -3,13 +3,13 @@
 from machine import SoftUART, Pin
 import time
 
-s1 = SoftUART(Pin(2), Pin(4), baudrate=9600, timeout=0) # tx=2 rx=4
-s2 = SoftUART(Pin(0), Pin(5), baudrate=9600, timeout=0) # tx=0 rx=5
+s1 = SoftUART(Pin(2), Pin(4), baudrate=9600, timeout=0)  # tx=2 rx=4
+s2 = SoftUART(Pin(0), Pin(5), baudrate=9600, timeout=0)  # tx=0 rx=5
 
 while True:
-	print("sending... ",end="")
-	s1.write("Hello world s1\r\n")
-	print("1, ", end="")
-	s2.write("Hello world s2\r\n")
-	print("2, done.")
-	time.sleep(1)
+    print("sending... ", end="")
+    s1.write("Hello world s1\r\n")
+    print("1, ", end="")
+    s2.write("Hello world s2\r\n")
+    print("2, done.")
+    time.sleep(1)
