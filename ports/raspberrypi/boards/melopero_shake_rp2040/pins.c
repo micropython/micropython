45600b1b99ed6b3395b5ec45ba421433122b96ab
Leonardo La Rocca <46094699+leoli51@users.noreply.github.com>
2021-10-15 16:52:25 +0200

    Update ports/raspberrypi/boards/melopero_shake_rp2040/pins.c
    Co-authored-by: microDev <70126934+microDev1@users.noreply.github.com>

--- ports/raspberrypi/boards/melopero_shake_rp2040/pins.c
+++ ports/raspberrypi/boards/melopero_shake_rp2040/pins.c
@@ -26,6 +26,9 @@ STATIC const mp_rom_map_elem_t board_global_dict_table[] = {
     { MP_ROM_QSTR(MP_QSTR_D12), MP_ROM_PTR(&pin_GPIO12) },
     { MP_ROM_QSTR(MP_QSTR_D13), MP_ROM_PTR(&pin_GPIO13) },
     { MP_ROM_QSTR(MP_QSTR_D17), MP_ROM_PTR(&pin_GPIO17) },
+
+    { MP_ROM_QSTR(MP_QSTR_BATTERY), MP_ROM_PTR(&pin_GPIO17) },
+
     { MP_ROM_QSTR(MP_QSTR_LED), MP_ROM_PTR(&pin_GPIO13) },
 
     { MP_ROM_QSTR(MP_QSTR_NEOPIXEL), MP_ROM_PTR(&pin_GPIO16) },
