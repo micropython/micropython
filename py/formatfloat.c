1fea4578c2e84d5bda60e10930e446e5350d78ec
stijn <stijn@ignitron.net>
2021-10-18 11:55:54 +0200

    py: Extract floating point routines from objfloat.c.
    Especially the fp_is* functions are useful to have in the public
    API, and will be used in upcoming commits regarding JSON.

--- py/formatfloat.c
+++ py/formatfloat.c
@@ -30,7 +30,7 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <math.h>
+#include "py/float.h"
 #include "py/formatfloat.h"
 
 /***********************************************************************
@@ -47,56 +47,6 @@
   Dave Hylands
 
 ***********************************************************************/
-
-#if MICROPY_FLOAT_IMPL == MICROPY_FLOAT_IMPL_FLOAT
-// 1 sign bit, 8 exponent bits, and 23 mantissa bits.
-// exponent values 0 and 255 are reserved, exponent can be 1 to 254.
-// exponent is stored with a bias of 127.
-// The min and max floats are on the order of 1x10^37 and 1x10^-37
-
-#define FPTYPE float
-#define FPCONST(x) x##F
-#define FPROUND_TO_ONE 0.9999995F
-#define FPDECEXP 32
-#define FPMIN_BUF_SIZE 6 // +9e+99
-
-#define FLT_SIGN_MASK   0x80000000
-#define FLT_EXP_MASK    0x7F800000
-#define FLT_MAN_MASK    0x007FFFFF
-
-union floatbits {
-    float f;
-    uint32_t u;
-};
-static inline int fp_signbit(float x) {
-    union floatbits fb = {x};
-    return fb.u & FLT_SIGN_MASK;
-}
-#define fp_isnan(x) isnan(x)
-#define fp_isinf(x) isinf(x)
-static inline int fp_iszero(float x) {
-    union floatbits fb = {x};
-    return fb.u == 0;
-}
-static inline int fp_isless1(float x) {
-    union floatbits fb = {x};
-    return fb.u < 0x3f800000;
-}
-
-#elif MICROPY_FLOAT_IMPL == MICROPY_FLOAT_IMPL_DOUBLE
-
-#define FPTYPE double
-#define FPCONST(x) x
-#define FPROUND_TO_ONE 0.999999999995
-#define FPDECEXP 256
-#define FPMIN_BUF_SIZE 7 // +9e+199
-#define fp_signbit(x) signbit(x)
-#define fp_isnan(x) isnan(x)
-#define fp_isinf(x) isinf(x)
-#define fp_iszero(x) (x == 0)
-#define fp_isless1(x) (x < 1.0)
-
-#endif
 
 static const FPTYPE g_pos_pow[] = {
     #if FPDECEXP > 32
