02573676e7c1de1f977159b5a04e14a6134fc1b8
lady ada <limor@ladyada.net>
2021-11-05 12:43:44 -0400

    add KB2040 and fix Trinkey QT2040 pid

---
+++ ports/raspberrypi/boards/adafruit_kb2040/mpconfigboard.h
@@ -0,0 +1,10 @@
+#define MICROPY_HW_BOARD_NAME "Adafruit KB2040"
+#define MICROPY_HW_MCU_NAME "rp2040"
+
+#define MICROPY_HW_NEOPIXEL (&pin_GPIO17)
+
+#define DEFAULT_I2C_BUS_SDA (&pin_GPIO12)
+#define DEFAULT_I2C_BUS_SCL (&pin_GPIO13)
+
+#define DEFAULT_UART_BUS_TX (&pin_GPIO0)
+#define DEFAULT_UART_BUS_RX (&pin_GPIO1)
