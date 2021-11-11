bcec249091b1bd3fa1207b51fa2d2bdf456b13ae
lady ada <limor@ladyada.net>
2021-11-10 23:13:22 -0500

    displaaaaaaaaay

--- ports/espressif/boards/adafruit_feather_esp32s2_tft/board.c
+++ ports/espressif/boards/adafruit_feather_esp32s2_tft/board.c
@@ -32,22 +32,42 @@
 #include "shared-module/displayio/__init__.h"
 #include "shared-module/displayio/mipi_constants.h"
 
-/*
 displayio_fourwire_obj_t board_display_obj;
 
 #define DELAY 0x80
 
+// display init sequence according to LilyGO example app
 uint8_t display_init_sequence[] = {
-    0x01, 0 | DELAY, 150, // SWRESET
-    0x11, 0 | DELAY, 255, // SLPOUT
-    0x36, 1, 0x00,        // _MADCTL bottom to top refresh in vsync aligned order.
-    0x3a, 1, 0x55, // COLMOD - 16bit color
-    0x21, 0 | DELAY, 10,                 // _INVON
-    0x13, 0 | DELAY, 10,                 // _NORON
-    0x29, 0 | DELAY, 255,                // _DISPON
+    // sw reset
+    0x01, 0 | DELAY, 150,
+    // sleep out
+    0x11, 0 | DELAY, 255,
+    // normal display mode on
+    0x13, 0,
+    // display and color format settings
+    0x36, 1, 0x08,
+    0xB6, 2, 0x0A, 0x82,
+    0x3A, 1 | DELAY,  0x55, 10,
+    // ST7789V frame rate setting
+    0xB2, 5, 0x0C, 0x0C, 0x00, 0x33, 0x33,
+    // voltages: VGH / VGL
+    0xB7, 1, 0x35,
+    // ST7789V power setting
+    0xBB, 1, 0x28,
+    0xC0, 1, 0x0C,
+    0xC2, 2, 0x01, 0xFF,
+    0xC3, 1, 0x10,
+    0xC4, 1, 0x20,
+    0xC6, 1, 0x0F,
+    0xD0, 2, 0xA4, 0xA1,
+    // ST7789V gamma setting
+    0xE0, 14, 0xD0, 0x00, 0x02, 0x07, 0x0A, 0x28, 0x32, 0x44, 0x42, 0x06, 0x0E, 0x12, 0x14, 0x17,
+    0xE1, 14, 0xD0, 0x00, 0x02, 0x07, 0x0A, 0x28, 0x31, 0x54, 0x47, 0x0E, 0x1C, 0x17, 0x1B, 0x1E,
+    0x21, 0,
+    // display on
+    0x29, 0 | DELAY, 255,
 };
 
-*/
 
 void board_init(void) {
     // USB
@@ -55,53 +75,69 @@ void board_init(void) {
     common_hal_never_reset_pin(&pin_GPIO20);
 
 
-    /*
-    busio_spi_obj_t* spi = &displays[0].fourwire_bus.inline_bus;
-    common_hal_busio_spi_construct(spi, &pin_GPIO36, &pin_GPIO35, NULL);
+    busio_spi_obj_t *spi = &displays[0].fourwire_bus.inline_bus;
+
+    common_hal_busio_spi_construct(
+        spi,
+        &pin_GPIO36,    // CLK
+        &pin_GPIO35,    // MOSI
+        NULL            // MISO not connected
+        );
+
     common_hal_busio_spi_never_reset(spi);
 
     displayio_fourwire_obj_t* bus = &displays[0].fourwire_bus;
     bus->base.type = &displayio_fourwire_type;
-    common_hal_displayio_fourwire_construct(bus,
-        spi,
-        &pin_GPIO40, // TFT_DC Command or data
-        &pin_GPIO42, // TFT_CS Chip select
-        &pin_GPIO41, // TFT_RST Reset
-        4000000, // Baudrate
-        0, // Polarity
-        0); // Phase
 
+    common_hal_displayio_fourwire_construct(
+        bus,
+        spi,
+        &pin_GPIO39,    // DC
+        &pin_GPIO21,    // CS
+        &pin_GPIO40,    // RST
+        40000000,       // baudrate
+        0,              // polarity
+        0               // phase
+        );
     displayio_display_obj_t* display = &displays[0].display;
     display->base.type = &displayio_display_type;
-    common_hal_displayio_display_construct(display,
+
+    // workaround as board_init() is called before reset_port() in main.c
+    pwmout_reset();
+
+
+    common_hal_displayio_display_construct(
+        display,
         bus,
-        240, // Width (after rotation)
-        135, // Height (after rotation)
-        0, // column start
-        0, // row start
-        0, // rotation
-        16, // Color depth
-        false, // Grayscale
-        false, // Pixels in a byte share a row. Only used for depth < 8
-        1, // bytes per cell. Only valid for depths < 8
-        false, // reverse_pixels_in_byte. Only valid for depths < 8
-        true, // reverse_pixels_in_word
-        MIPI_COMMAND_SET_COLUMN_ADDRESS, // Set column command
-        MIPI_COMMAND_SET_PAGE_ADDRESS, // Set row command
-        MIPI_COMMAND_WRITE_MEMORY_START, // Write memory command
+        240,            // width (after rotation)
+        135,            // height (after rotation)
+        52,             // column start
+        40,             // row start
+        90,             // rotation
+        16,             // color depth
+        false,          // grayscale
+        false,          // pixels in a byte share a row. Only valid for depths < 8
+        1,              // bytes per cell. Only valid for depths < 8
+        false,          // reverse_pixels_in_byte. Only valid for depths < 8
+        true,           // reverse_pixels_in_word
+        MIPI_COMMAND_SET_COLUMN_ADDRESS, // set column command
+        MIPI_COMMAND_SET_PAGE_ADDRESS,   // set row command
+        MIPI_COMMAND_WRITE_MEMORY_START, // write memory command
         display_init_sequence,
         sizeof(display_init_sequence),
-        &pin_GPIO7,  // backlight pin
+        &pin_GPIO45,    // backlight pin
         NO_BRIGHTNESS_COMMAND,
-        1.0f, // brightness (ignored)
-        true, // auto_brightness
-        false, // single_byte_bounds
-        false, // data_as_commands
-        true, // auto_refresh
-        60, // native_frames_per_second
-        true, // backlight_on_high
-        false); // not SH1107
-        */
+        1.0f,           // brightness (ignored)
+        false,          // auto_brightness
+        false,          // single_byte_bounds
+        false,          // data_as_commands
+        true,           // auto_refresh
+        60,             // native_frames_per_second
+        true,           // backlight_on_high
+        false           // SH1107_addressing
+        );
+
+    common_hal_never_reset_pin(&pin_GPIO45); // backlight pin
 }
 
 bool board_requests_safe_mode(void) {
--- ports/espressif/boards/adafruit_feather_esp32s2_tft/pins.c
+++ ports/espressif/boards/adafruit_feather_esp32s2_tft/pins.c
@@ -68,6 +68,6 @@ STATIC const mp_rom_map_elem_t board_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_SPI), MP_ROM_PTR(&board_spi_obj) },
     { MP_ROM_QSTR(MP_QSTR_UART), MP_ROM_PTR(&board_uart_obj) }
 
-    //   { MP_ROM_QSTR(MP_QSTR_DISPLAY), MP_ROM_PTR(&displays[0].display)}
+    { MP_ROM_QSTR(MP_QSTR_DISPLAY), MP_ROM_PTR(&displays[0].display)}
 };
 MP_DEFINE_CONST_DICT(board_module_globals, board_module_globals_table);
