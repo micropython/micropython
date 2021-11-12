bb8e9ef483f0abd4fca8aefd46e2fdbe5e3b8f26
anecdata <16617689+anecdata@users.noreply.github.com>
2021-11-11 21:54:41 -0600

    tweak exception messages (re-tested)

--- ports/espressif/common-hal/wifi/Radio.c
+++ ports/espressif/common-hal/wifi/Radio.c
@@ -122,7 +122,7 @@ mp_obj_t common_hal_wifi_radio_get_mac_address(wifi_radio_obj_t *self) {
 
 void common_hal_wifi_radio_set_mac_address(wifi_radio_obj_t *self, const uint8_t *mac) {
     if (self->sta_mode) {
-        mp_raise_RuntimeError(translate("Can't change MAC address while station is started"));
+        mp_raise_RuntimeError(translate("Can't change MAC address while station is connected"));
     }
     if ((mac[0] & 0b1) == 0b1) {
         mp_raise_RuntimeError(translate("MAC address can't be a multicast address"));
