56db620284765a1c70b4a7f713dd6bf44f25c321
Lorenzo Cappelletti <lorenzo.cappelletti@gmail.com>
2021-11-15 22:23:02 +0100

    ports/stm32/boards/MIKROE_QUAIL: Remove untested mboot.

--- ports/stm32/boards/MIKROE_QUAIL/mpconfigboard.mk
+++ ports/stm32/boards/MIKROE_QUAIL/mpconfigboard.mk
@@ -1,13 +1,6 @@
 MCU_SERIES = f4
 CMSIS_MCU = STM32F427xx
 AF_FILE = boards/stm32f429_af.csv
-ifeq ($(USE_MBOOT),1)
-# When using Mboot all the text goes together after the filesystem
-LD_FILES = boards/stm32f427xi.ld boards/common_blifs.ld
-TEXT0_ADDR = 0x08020000
-else
-# When not using Mboot the ISR text goes first, then the rest after the filesystem
 LD_FILES = boards/stm32f427xi.ld boards/common_ifs.ld
 TEXT0_ADDR = 0x08000000
 TEXT1_ADDR = 0x08020000
-endif
