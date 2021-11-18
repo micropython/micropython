9cd1d089186160b54ba2cf21bcb7c151305cfa01
Jim Mussared <jim.mussared@gmail.com>
2021-11-18 23:29:48 +1100

    tests/cpydiff: Clarify f-string diffs.
    - Concatenation of any literals (including f-strings) should be avoided.
    
    - The colon symbol anywhere in the expression will trigger end of
    expression.
    
    Signed-off-by: Jim Mussared <jim.mussared@gmail.com>

--- tests/cpydiff/core_fstring_parser.py
+++ tests/cpydiff/core_fstring_parser.py
@@ -1,9 +1,10 @@
 """
 categories: Core
-description: f-strings cannot support expressions that require parsing to resolve nested braces
+description: f-strings cannot support expressions that require parsing to resolve nested braces or colons
 cause: MicroPython is optimised for code space.
 workaround: Only use simple expressions inside f-strings
 """
 
 f'{"hello {} world"}'
 f"{repr({})}"
+f"{x[1:3]}"
