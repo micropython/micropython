9a8d8b1bf1d175d5e29b1be46608c0a00897597d
Kamil Tomaszewski <kamil.tomaszewski@sony.com>
2021-11-30 12:38:48 +0100

    spresense: Fix time.time()

--- shared-bindings/rtc/__init__.h
+++ shared-bindings/rtc/__init__.h
@@ -27,6 +27,8 @@
 #ifndef MICROPY_INCLUDED_SHARED_BINDINGS_RTC___INIT___H
 #define MICROPY_INCLUDED_SHARED_BINDINGS_RTC___INIT___H
 
+#include "py/obj.h"
+
 extern void rtc_reset(void);
 extern mp_obj_t rtc_get_time_source_time(void);
 
