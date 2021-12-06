bde65d0f28365da5214f9df4449f2a23bcf7408f
Kong Wai Weng <waiweng83@gmail.com>
2021-12-06 17:26:00 +0800

    Added Maker Nano RP2040

---
+++ ports/raspberrypi/boards/cytron_maker_nano_rp2040/mpconfigboard.h
@@ -0,0 +1,12 @@
+#define MICROPY_HW_BOARD_NAME "Cytron Maker Nano RP2040"
+#define MICROPY_HW_MCU_NAME "rp2040"
+
+#define DEFAULT_I2C_BUS_SCL (&pin_GPIO1)
+#define DEFAULT_I2C_BUS_SDA (&pin_GPIO0)
+
+#define DEFAULT_SPI_BUS_SCK (&pin_GPIO18)
+#define DEFAULT_SPI_BUS_MOSI (&pin_GPIO19)
+#define DEFAULT_SPI_BUS_MISO (&pin_GPIO16)
+
+#define DEFAULT_UART_BUS_RX (&pin_GPIO1)
+#define DEFAULT_UART_BUS_TX (&pin_GPIO0)
