d3c5a99b933223d507109b54e6f1ae38f0bd4d2f
mybays <mybays@mybayss-iMac-2.local>
2021-12-17 23:54:34 +0800

    esp32-solo support

---
+++ ports/esp32/boards/ESP32_SOLO/mpconfigboard.cmake
@@ -0,0 +1,8 @@
+set(SDKCONFIG_DEFAULTS
+    boards/sdkconfig.base
+    boards/sdkconfig.ble
+    boards/GENERIC/sdkconfig.unicore
+)
+if(NOT MICROPY_FROZEN_MANIFEST)
+    set(MICROPY_FROZEN_MANIFEST ${MICROPY_PORT_DIR}/boards/manifest.py)
+endif()
