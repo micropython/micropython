1d13c4ccd1df70e5f71c75075f729874303fe889
Jeff Epler <jepler@gmail.com>
2021-12-27 10:21:47 -0600

    Centralize LONGINT_IMPL handling
    This closes #5782

--- ports/nrf/mpconfigport.mk
+++ ports/nrf/mpconfigport.mk
@@ -2,11 +2,6 @@
 # parameters that vary based on chip and/or board.
 LD_TEMPLATE_FILE = boards/common.template.ld
 
-# Define an equivalent for MICROPY_LONGINT_IMPL, to pass to $(MPY-TOOL) in py/mkrules.mk
-# $(MPY-TOOL) needs to know what kind of longint to use (if any) to freeze long integers.
-# This should correspond to the MICROPY_LONGINT_IMPL definition in mpconfigport.h.
-MPY_TOOL_LONGINT_IMPL = -mlongint-impl=mpz
-
 INTERNAL_LIBM = 1
 
 # Number of USB endpoint pairs.
