f64fede735bf258559355de243f606c720d8cb04
Scott Shawcroft <scott@tannewt.org>
2022-01-04 16:11:40 -0800

    Correct board name and fix USB with memory barrier

--- ports/broadcom/boards/raspberrypi_zero_w/mpconfigboard.h
+++ ports/broadcom/boards/raspberrypi_zero_w/mpconfigboard.h
@@ -1,4 +1,4 @@
-#define MICROPY_HW_BOARD_NAME "Raspberry Pi Zero"
+#define MICROPY_HW_BOARD_NAME "Raspberry Pi Zero W"
 
 #define DEFAULT_I2C_BUS_SCL         (&pin_GPIO3)
 #define DEFAULT_I2C_BUS_SDA         (&pin_GPIO2)
