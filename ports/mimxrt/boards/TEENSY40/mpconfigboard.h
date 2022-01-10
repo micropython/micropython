30380962cff1a42dabbbdf172b951f21b5bffe14
robert-hh <robert@hammelrath.com>
2022-01-10 18:14:40 +0100

    mimxrt: Allow to select cs0 or cs1 for SPI.
    Using the keyword argument cs=nnn in the constructor. The cs1
    pin has to be defined in mpconfigboard.h.
    Note: Only a few boards have the CS1 pin exposed to the connectors.

--- ports/mimxrt/boards/TEENSY40/mpconfigboard.h
+++ ports/mimxrt/boards/TEENSY40/mpconfigboard.h
@@ -27,12 +27,16 @@
 #define IOMUX_TABLE_SPI \
     { 0 }, { 0 }, \
     { 0 }, { 0 }, \
+    { 0 }, \
     { 0 }, { 0 }, \
     { 0 }, { 0 }, \
+    { 0 }, \
     { IOMUXC_GPIO_AD_B1_15_LPSPI3_SCK }, { IOMUXC_GPIO_AD_B0_03_LPSPI3_PCS0 }, \
     { IOMUXC_GPIO_AD_B1_14_LPSPI3_SDO }, { IOMUXC_GPIO_AD_B0_02_LPSPI3_SDI }, \
+    { 0 }, \
     { IOMUXC_GPIO_B0_03_LPSPI4_SCK }, { IOMUXC_GPIO_B0_00_LPSPI4_PCS0 }, \
-    { IOMUXC_GPIO_B0_02_LPSPI4_SDO }, { IOMUXC_GPIO_B0_01_LPSPI4_SDI },
+    { IOMUXC_GPIO_B0_02_LPSPI4_SDO }, { IOMUXC_GPIO_B0_01_LPSPI4_SDI }, \
+    { 0 },
 
 #define DMA_REQ_SRC_RX { 0, kDmaRequestMuxLPSPI1Rx, kDmaRequestMuxLPSPI2Rx, \
                          kDmaRequestMuxLPSPI3Rx, kDmaRequestMuxLPSPI4Rx }
