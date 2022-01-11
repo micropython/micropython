2251d0f4b0692ab6c1104ec6b04ff513b3f028fe
arturo182 <arturo182@tlen.pl>
2022-01-11 15:13:36 +0100

    raspberrypi: Claim UART after checking pins
    If, for some reason, you mix up TX and RX when calling `busio.UART` (who would do that ;) ), you get `Invalid pins`. When you go to try again, you'll get `All UART peripherals are in use` because the interface was claimed as busy before pins are verified. This should fix that issue.

--- ports/raspberrypi/common-hal/busio/UART.c
+++ ports/raspberrypi/common-hal/busio/UART.c
@@ -118,16 +118,16 @@ void common_hal_busio_uart_construct(busio_uart_obj_t *self,
 
     uint8_t uart_id = ((((tx != NULL) ? tx->number : rx->number) + 4) / 8) % NUM_UARTS;
 
+    self->tx_pin = pin_init(uart_id, tx, 0);
+    self->rx_pin = pin_init(uart_id, rx, 1);
+    self->cts_pin = pin_init(uart_id, cts, 2);
+    self->rts_pin = pin_init(uart_id, rts, 3);
+
     if (uart_status[uart_id] != STATUS_FREE) {
         mp_raise_RuntimeError(translate("All UART peripherals are in use"));
     } else {
         uart_status[uart_id] = STATUS_BUSY;
     }
-
-    self->tx_pin = pin_init(uart_id, tx, 0);
-    self->rx_pin = pin_init(uart_id, rx, 1);
-    self->cts_pin = pin_init(uart_id, cts, 2);
-    self->rts_pin = pin_init(uart_id, rts, 3);
 
     self->uart = UART_INST(uart_id);
     self->uart_id = uart_id;
