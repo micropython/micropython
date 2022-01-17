d4dab792340364b8125c350aa10833ee3fb2132f
lady ada <limor@ladyada.net>
2022-01-17 00:31:19 -0500

    add buttonpin

--- ports/espressif/boards/adafruit_esp32s2_camera/pins.c
+++ ports/espressif/boards/adafruit_esp32s2_camera/pins.c
@@ -36,6 +36,7 @@ STATIC const mp_rom_map_elem_t board_module_globals_table[] = {
 
     { MP_ROM_QSTR(MP_QSTR_SPEAKER), MP_ROM_PTR(&pin_GPIO45) },
 
+    { MP_ROM_QSTR(MP_QSTR_BUTTON), MP_ROM_PTR(&pin_GPIO0) },
     { MP_ROM_QSTR(MP_QSTR_LED), MP_ROM_PTR(&pin_GPIO2) },
 
     { MP_ROM_QSTR(MP_QSTR_A0), MP_ROM_PTR(&pin_GPIO17) },
