e034d3270c4ff4c10bdb152d15300315e9d37e3a
syabyr <mybays@sina.com>
2022-01-29 10:34:18 +0800

    unicore

--- ports/esp32/boards/ESP32_SOLO/mpconfigboard.cmake
+++ ports/esp32/boards/ESP32_SOLO/mpconfigboard.cmake
@@ -1,7 +1,7 @@
 set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.base
     boards/sdkconfig.ble
-    boards/GENERIC/sdkconfig.unicore
+    boards/ESP32_SOLO/sdkconfig.unicore
 )
 if(NOT MICROPY_FROZEN_MANIFEST)
     set(MICROPY_FROZEN_MANIFEST ${MICROPY_PORT_DIR}/boards/manifest.py)
