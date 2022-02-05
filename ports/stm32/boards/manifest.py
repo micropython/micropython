21bf1c96fc94c608f092b8503d7de1ee36a96eaa
Shenghao Yang <me@shenghaoyang.info>
2022-02-05 19:11:20 +0800

    stm32/boards: Allow opting-out of freezing extra drivers.
    Signed-off-by: Shenghao Yang <me@shenghaoyang.info>

--- ports/stm32/boards/manifest.py
+++ ports/stm32/boards/manifest.py
@@ -1,4 +1,8 @@
+options.defaults(extra_drivers=True)
+
 include("$(MPY_DIR)/extmod/uasyncio/manifest.py")
-freeze("$(MPY_DIR)/drivers/dht", "dht.py")
-freeze("$(MPY_DIR)/drivers/display", ("lcd160cr.py", "lcd160cr_test.py"))
-freeze("$(MPY_DIR)/drivers/onewire", "onewire.py")
+
+if options.extra_drivers:
+    freeze("$(MPY_DIR)/drivers/dht", "dht.py")
+    freeze("$(MPY_DIR)/drivers/display", ("lcd160cr.py", "lcd160cr_test.py"))
+    freeze("$(MPY_DIR)/drivers/onewire", "onewire.py")
