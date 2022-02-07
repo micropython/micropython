af021a16165437e778896a9d222b3ce248640c16
Shenghao Yang <me@shenghaoyang.info>
2022-02-07 15:30:02 +0800

    Revert "stm32/boards: Allow opting-out of freezing extra drivers."
    This reverts commit 21bf1c96fc94c608f092b8503d7de1ee36a96eaa.

--- ports/stm32/boards/manifest.py
+++ ports/stm32/boards/manifest.py
@@ -1,8 +1,4 @@
-options.defaults(extra_drivers=True)
-
 include("$(MPY_DIR)/extmod/uasyncio/manifest.py")
-
-if options.extra_drivers:
-    freeze("$(MPY_DIR)/drivers/dht", "dht.py")
-    freeze("$(MPY_DIR)/drivers/display", ("lcd160cr.py", "lcd160cr_test.py"))
-    freeze("$(MPY_DIR)/drivers/onewire", "onewire.py")
+freeze("$(MPY_DIR)/drivers/dht", "dht.py")
+freeze("$(MPY_DIR)/drivers/display", ("lcd160cr.py", "lcd160cr_test.py"))
+freeze("$(MPY_DIR)/drivers/onewire", "onewire.py")
