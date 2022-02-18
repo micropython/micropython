e57e4bd6674c67ba2e2506a9aa68e1046bde901a
arturo182 <arturo182@tlen.pl>
2022-02-18 21:32:36 +0100

    esp32s3: Rename to devkitm_1 to devkitm_1_n8 to make more future-proof

--- ports/espressif/boards/espressif_esp32s3_devkitm_1_n8/mpconfigboard.h
+++ ports/espressif/boards/espressif_esp32s3_devkitm_1_n8/mpconfigboard.h
@@ -26,7 +26,7 @@
 
 // Micropython setup
 
-#define MICROPY_HW_BOARD_NAME       "ESP32-S3-DevKitM-1"
+#define MICROPY_HW_BOARD_NAME       "ESP32-S3-DevKitM-1-N8"
 #define MICROPY_HW_MCU_NAME         "ESP32S3"
 
 #define MICROPY_HW_NEOPIXEL         (&pin_GPIO48)
