8bcd3990071458acc0db116d551356c0cbd84140
Damien George <damien@micropython.org>
2022-03-03 18:11:09 +1100

    esp32: Enable VfsRom.
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/esp32/modules/_boot.py
+++ ports/esp32/modules/_boot.py
@@ -1,6 +1,13 @@
 import gc
 import vfs
+from esp32 import Partition
 from flashbdev import bdev
+
+if romfs := Partition.find(Partition.TYPE_DATA, label="romfs"):
+    import sys
+
+    sys.path.insert(0, "/romfs")
+del romfs
 
 try:
     if bdev:
