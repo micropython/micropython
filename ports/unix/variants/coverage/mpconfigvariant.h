3c20ddb41adc177442b68a8fdff221c80e0b0b5e
Damien George <damien@micropython.org>
2022-03-10 13:21:11 +1100

    unix/variants: Use rom feature config for standard, dev, coverage.
    This change is a no-op in terms of functionality.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/unix/variants/coverage/mpconfigvariant.h
+++ ports/unix/variants/coverage/mpconfigvariant.h
@@ -27,36 +27,32 @@
 // This config enables almost all possible features such that it can be used
 // for coverage testing.
 
+// Set base feature level.
+#define MICROPY_CONFIG_ROM_LEVEL (MICROPY_CONFIG_ROM_LEVEL_EXTRA_FEATURES)
+
+// Disable some features that come enabled by default with the feature level.
+#define MICROPY_OPT_MPZ_BITWISE                 (0)
+#define MICROPY_MODULE_BUILTIN_INIT             (0)
+#define MICROPY_PY_BUILTINS_EXECFILE            (0)
+#define MICROPY_PY_SYS_STDIO_BUFFER             (0)
+#define MICROPY_PY_USELECT                      (0)
+
+// Enable additional features.
 #define MICROPY_DEBUG_PARSE_RULE_NAME  (1)
-#define MICROPY_OPT_MATH_FACTORIAL     (1)
 #define MICROPY_FLOAT_HIGH_QUALITY_HASH (1)
-#define MICROPY_ENABLE_SCHEDULER       (1)
 #define MICROPY_REPL_EMACS_WORDS_MOVE  (1)
 #define MICROPY_REPL_EMACS_EXTRA_WORDS_MOVE (1)
 #define MICROPY_WARNINGS_CATEGORY      (1)
-#define MICROPY_MODULE_ATTR_DELEGATION (1)
-#define MICROPY_MODULE_GETATTR         (1)
-#define MICROPY_PY_DELATTR_SETATTR     (1)
 #define MICROPY_PY_ALL_INPLACE_SPECIAL_METHODS (1)
-#define MICROPY_PY_REVERSE_SPECIAL_METHODS (1)
 #define MICROPY_PY_BUILTINS_MEMORYVIEW_ITEMSIZE (1)
 #define MICROPY_PY_BUILTINS_NEXT2      (1)
 #define MICROPY_PY_BUILTINS_RANGE_BINOP (1)
-#define MICROPY_PY_BUILTINS_HELP       (1)
-#define MICROPY_PY_BUILTINS_HELP_MODULES (1)
 #define MICROPY_PY_SYS_GETSIZEOF       (1)
-#define MICROPY_PY_SYS_PS1_PS2         (1)
 #define MICROPY_PY_SYS_TRACEBACKLIMIT  (1)
-#define MICROPY_PY_MATH_CONSTANTS      (1)
-#define MICROPY_PY_MATH_FACTORIAL      (1)
-#define MICROPY_PY_URANDOM_EXTRA_FUNCS (1)
 #define MICROPY_PY_IO_BUFFEREDWRITER (1)
-#define MICROPY_PY_UASYNCIO            (1)
 #define MICROPY_PY_URE_DEBUG           (1)
 #define MICROPY_PY_URE_MATCH_GROUPS    (1)
 #define MICROPY_PY_URE_MATCH_SPAN_START_END (1)
-#define MICROPY_PY_URE_SUB             (1)
-#define MICROPY_PY_FRAMEBUF            (1)
 #define MICROPY_PY_COLLECTIONS_NAMEDTUPLE__ASDICT (1)
 #define MICROPY_PY_UCRYPTOLIB          (1)
 #define MICROPY_PY_UCRYPTOLIB_CTR      (1)
--- ports/unix/variants/dev/mpconfigvariant.h
+++ ports/unix/variants/dev/mpconfigvariant.h
@@ -24,16 +24,23 @@
  * THE SOFTWARE.
  */
 
+// Set base feature level.
+#define MICROPY_CONFIG_ROM_LEVEL (MICROPY_CONFIG_ROM_LEVEL_EXTRA_FEATURES)
+
+// Disable some features that come enabled by default with the feature level.
+#define MICROPY_OPT_MPZ_BITWISE                 (0)
+#define MICROPY_OPT_MATH_FACTORIAL              (0)
+#define MICROPY_MODULE_ATTR_DELEGATION          (0)
+#define MICROPY_MODULE_BUILTIN_INIT             (0)
+#define MICROPY_PY_BUILTINS_EXECFILE            (0)
+#define MICROPY_PY_MATH_FACTORIAL               (0)
+#define MICROPY_PY_SYS_PS1_PS2                  (0)
+#define MICROPY_PY_SYS_STDIO_BUFFER             (0)
+#define MICROPY_PY_USELECT                      (0)
+#define MICROPY_PY_URE_SUB                      (0)
+#define MICROPY_PY_FRAMEBUF                     (0)
+
+// Enable some additional features.
 #define MICROPY_REPL_EMACS_WORDS_MOVE           (1)
 #define MICROPY_REPL_EMACS_EXTRA_WORDS_MOVE     (1)
-#define MICROPY_ENABLE_SCHEDULER                (1)
-
-#define MICROPY_PY_BUILTINS_HELP                (1)
-#define MICROPY_PY_BUILTINS_HELP_MODULES        (1)
-#define MICROPY_PY_MATH_CONSTANTS               (1)
 #define MICROPY_PY_SYS_SETTRACE                 (1)
-#define MICROPY_PY_URANDOM_EXTRA_FUNCS          (1)
-
-#ifndef MICROPY_PY_UASYNCIO
-#define MICROPY_PY_UASYNCIO                     (1)
-#endif
