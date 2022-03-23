8045ac07f599c0ccc447c88a0b778f704b497559
Damien George <damien@micropython.org>
2022-03-23 17:22:53 +1100

    stm32/mpbthciport: Switch scheduling callback to use node scheduler.
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/stm32/mpconfigport.h
+++ ports/stm32/mpconfigport.h
@@ -75,6 +75,7 @@
 #define MICROPY_FLOAT_IMPL          (MICROPY_FLOAT_IMPL_FLOAT)
 #endif
 #define MICROPY_USE_INTERNAL_ERRNO  (1)
+#define MICROPY_SCHEDULER_STATIC_NODES (1)
 #define MICROPY_SCHEDULER_DEPTH     (8)
 #define MICROPY_VFS                 (1)
 
