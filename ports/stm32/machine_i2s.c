6804a8a891d61d7b4c2b613997021d12f960763c
Damien George <damien@micropython.org>
2022-03-26 00:07:19 +1100

    stm32/machine_i2s: Fix 16-bit stereo i2s_frame_map.
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/stm32/machine_i2s.c
+++ ports/stm32/machine_i2s.c
@@ -158,7 +158,7 @@ STATIC mp_obj_t machine_i2s_deinit(mp_obj_t self_in);
 STATIC const int8_t i2s_frame_map[NUM_I2S_USER_FORMATS][I2S_RX_FRAME_SIZE_IN_BYTES] = {
     { 0,  1, -1, -1, -1, -1, -1, -1 },  // Mono, 16-bits
     { 2,  3,  0,  1, -1, -1, -1, -1 },  // Mono, 32-bits
-    { 0,  1,  4,  5, -1, -1, -1, -1 },  // Stereo, 16-bits
+    { 0,  1, -1, -1,  2,  3, -1, -1 },  // Stereo, 16-bits
     { 2,  3,  0,  1,  6,  7,  4,  5 },  // Stereo, 32-bits
 };
 
