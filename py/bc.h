micropython/micropython

commit 7e8222ae063d78ae8f901bb0f9fef663edd2edb6

py/emitnative: Don't store prelude at end of machine code if not needed.
Signed-off-by: Damien George <damien@micropython.org>

py/emitnative.c
@@ -723,7 +723,13 @@ STATIC bool emit_native_end_pass(emit_t *emit) {
 
     if (emit->pass == MP_PASS_EMIT) {
         void *f = mp_asm_base_get_code(&emit->as->base);
+        #if N_PRELUDE_AS_BYTES_OBJ
+        // Keep only the machine code, not the prelude, which is in a separate bytes object.
+        mp_uint_t f_len = emit->prelude_offset;
+        #else
+        // Keep both the machine code and the prelude.
         mp_uint_t f_len = mp_asm_base_get_code_size(&emit->as->base);
+        #endif
 
         mp_emit_glue_assign_native(emit->scope->raw_code,
             emit->do_viper_types ? MP_CODE_NATIVE_VIPER : MP_CODE_NATIVE_PY,

commit 26aec8e3054f1f27eff9f78a2051fc0ab381dada

py: Fix saving/loading/freezing of .mpy when prelude is bytes object.
This problem was introduced by 7e8222ae063d78ae8f901bb0f9fef663edd2edb6

Fixes issue #8474.

Signed-off-by: Damien George <damien@micropython.org>

py/bc.h
@@ -28,6 +28,7 @@
 #define MICROPY_INCLUDED_PY_BC_H
 
 #include "py/runtime.h"
+#include "py/objstr.h"
 
 // bytecode layout:
 //
@@ -253,6 +254,21 @@ typedef struct _mp_code_state_t {
     // Variable-length, never accessed by name, only as (void*)(state + n_state)
     // mp_exc_stack_t exc_state[0];
 } mp_code_state_t;
+
+#if MICROPY_EMIT_NATIVE
+static inline const uint8_t *mp_get_prelude(const uint8_t *fun_data, const mp_module_context_t *context, size_t prelude_offset) {
+    #if MICROPY_EMIT_NATIVE_PRELUDE_AS_BYTES_OBJ
+    // Prelude is in bytes object in const_table, at index prelude_offset.
+    (void)fun_data;
+    mp_obj_str_t *prelude_bytes = MP_OBJ_TO_PTR(context->constants.obj_table[prelude_offset]);
+    return prelude_bytes->data;
+    #else
+    // Prelude is in the function data, at offset prelude_offset.
+    (void)context;
+    return fun_data + prelude_offset;
+    #endif
+}
+#endif
 
 // Allocator may return NULL, in which case data is not stored (can be used to compute size).
 typedef uint8_t *(*mp_encode_uint_allocator_t)(void *env, size_t nbytes);

py/emitnative.c
@@ -737,7 +737,11 @@ STATIC bool emit_native_end_pass(emit_t *emit) {
             emit->emit_common->children,
             #if MICROPY_PERSISTENT_CODE_SAVE
             emit->emit_common->ct_cur_child,
+            #if N_PRELUDE_AS_BYTES_OBJ
+            emit->prelude_const_table_offset,
+            #else
             emit->prelude_offset,
+            #endif
             emit->qstr_link_cur, emit->qstr_link,
             #endif
             emit->scope->scope_flags, 0, 0);

py/objgenerator.c
@@ -93,13 +93,7 @@ STATIC mp_obj_t native_gen_wrap_call(mp_obj_t self_in, size_t n_args, size_t n_k
 
     // Determine start of prelude.
     uintptr_t prelude_offset = ((uintptr_t *)self_fun->bytecode)[0];
-    #if MICROPY_EMIT_NATIVE_PRELUDE_AS_BYTES_OBJ
-    // Prelude is in bytes object in const_table, at index prelude_offset
-    mp_obj_str_t *prelude_bytes = MP_OBJ_TO_PTR(self_fun->context->constants.obj_table[prelude_offset]);
-    const uint8_t *prelude_ptr = prelude_bytes->data;
-    #else
-    const uint8_t *prelude_ptr = self_fun->bytecode + prelude_offset;
-    #endif
+    const uint8_t *prelude_ptr = mp_get_prelude(self_fun->bytecode, self_fun->context, prelude_offset);
 
     // Extract n_state from the prelude.
     const uint8_t *ip = prelude_ptr;

py/persistentcode.c
@@ -225,7 +225,11 @@ STATIC mp_obj_t load_obj(mp_reader_t *reader) {
     }
 }
 
-STATIC mp_raw_code_t *load_raw_code(mp_reader_t *reader) {
+STATIC mp_raw_code_t *load_raw_code(mp_reader_t *reader, mp_module_context_t *context) {
+    #if !MICROPY_EMIT_MACHINE_CODE
+    (void)context;
+    #endif
+
     // Load function kind and data length
     size_t kind_len = read_uint(reader);
     int kind = (kind_len & 3) + MP_CODE_BYTECODE;
@@ -283,8 +287,8 @@ STATIC mp_raw_code_t *load_raw_code(mp_reader_t *reader) {
         if (kind == MP_CODE_NATIVE_PY) {
             // Read prelude offset within fun_data, and extract scope flags.
             prelude_offset = read_uint(reader);
-            const byte *ip = fun_data + prelude_offset;
-            MP_BC_PRELUDE_SIG_DECODE(ip);
+            const uint8_t *prelude_ptr = mp_get_prelude(fun_data, context, prelude_offset);
+            MP_BC_PRELUDE_SIG_DECODE(prelude_ptr);
             native_scope_flags = scope_flags;
         } else {
             // Load basic scope info for viper and asm.
@@ -338,7 +342,7 @@ STATIC mp_raw_code_t *load_raw_code(mp_reader_t *reader) {
         n_children = read_uint(reader);
         children = m_new(mp_raw_code_t *, n_children);
         for (size_t i = 0; i < n_children; ++i) {
-            children[i] = load_raw_code(reader);
+            children[i] = load_raw_code(reader, context);
         }
     }
 
@@ -431,7 +435,7 @@ mp_compiled_module_t mp_raw_code_load(mp_reader_t *reader, mp_module_context_t *
 
     // Load top-level module.
     mp_compiled_module_t cm2;
-    cm2.rc = load_raw_code(reader);
+    cm2.rc = load_raw_code(reader, context);
     cm2.context = context;
 
     #if MICROPY_PERSISTENT_CODE_SAVE

tools/mpy-tool.py
@@ -719,20 +719,26 @@ class RawCode(object):
         MP_CODE_NATIVE_ASM: "MP_CODE_NATIVE_ASM",
     }
 
-    def __init__(self, cm_escaped_name, qstr_table, fun_data, prelude_offset, code_kind):
+    def __init__(self, cm_escaped_name, qstr_table, obj_table, fun_data, prelude_offset, code_kind):
         self.qstr_table = qstr_table
+        self.obj_table = obj_table
         self.fun_data = fun_data
         self.prelude_offset = prelude_offset
         self.code_kind = code_kind
 
         if code_kind in (MP_CODE_BYTECODE, MP_CODE_NATIVE_PY):
+            if code_kind == MP_CODE_NATIVE_PY and config.native_arch == MP_NATIVE_ARCH_XTENSAWIN:
+                prelude_data = self.obj_table[prelude_offset]
+                prelude_offset = 0
+            else:
+                prelude_data = self.fun_data
             (
                 self.offset_names,
                 self.offset_opcodes,
                 self.offset_line_info,
                 self.prelude,
                 self.names,
-            ) = extract_prelude(self.fun_data, prelude_offset)
+            ) = extract_prelude(prelude_data, prelude_offset)
             self.scope_flags = self.prelude[2]
             self.n_pos_args = self.prelude[3]
             self.simple_name = self.qstr_table[self.names[0]]
@@ -822,9 +828,8 @@ class RawCode(object):
 
 class RawCodeBytecode(RawCode):
     def __init__(self, cm_escaped_name, qstr_table, obj_table, fun_data):
-        self.obj_table = obj_table
         super(RawCodeBytecode, self).__init__(
-            cm_escaped_name, qstr_table, fun_data, 0, MP_CODE_BYTECODE
+            cm_escaped_name, qstr_table, obj_table, fun_data, 0, MP_CODE_BYTECODE
         )
 
     def disassemble(self):
@@ -902,6 +907,7 @@ class RawCodeNative(RawCode):
         self,
         cm_escaped_name,
         qstr_table,
+        obj_table,
         kind,
         fun_data,
         prelude_offset,
@@ -911,7 +917,7 @@ class RawCodeNative(RawCode):
         type_sig,
     ):
         super(RawCodeNative, self).__init__(
-            cm_escaped_name, qstr_table, fun_data, prelude_offset, kind
+            cm_escaped_name, qstr_table, obj_table, fun_data, prelude_offset, kind
         )
 
         if kind in (MP_CODE_NATIVE_VIPER, MP_CODE_NATIVE_ASM):
@@ -1191,6 +1197,7 @@ def read_raw_code(reader, cm_escaped_name, qstr_table, obj_table, segments):
         rc = RawCodeNative(
             cm_escaped_name,
             qstr_table,
+            obj_table,
             kind,
             fun_data,
             prelude_offset,