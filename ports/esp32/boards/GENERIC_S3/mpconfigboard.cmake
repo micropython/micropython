df1d1743e3c1809173cf2e2ff21f7d82769831c2
iabdalkader <i.abdalkader@gmail.com>
2022-04-03 18:22:17 +0200

    esp32: Allow Overriding cmake frozen manifest from the command line.
    * If MICROPY_FROZEN_MANIFEST is set from the cmake command line,
    then it will override the default and any manifest set by the board.

--- ports/esp32/boards/GENERIC_S3/mpconfigboard.cmake
+++ ports/esp32/boards/GENERIC_S3/mpconfigboard.cmake
@@ -6,7 +6,3 @@ set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.ble
     boards/GENERIC_S3/sdkconfig.board
 )
-
-if(NOT MICROPY_FROZEN_MANIFEST)
-    set(MICROPY_FROZEN_MANIFEST ${MICROPY_PORT_DIR}/boards/manifest.py)
-endif()
