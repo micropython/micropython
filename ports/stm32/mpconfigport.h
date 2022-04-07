43d50f006d60e40f5c716e095a98477b3e9b154d
Damien George <damien@micropython.org>
2022-04-07 12:36:32 +1000

    stm32/mpconfigport.h: Let MICROPY_PY_IO_FILEIO take its default value.
    This is enabled at MICROPY_CONFIG_ROM_LEVEL_EXTRA_FEATURES, which is the
    default for stm32.  Not setting the value in mpconfigboard.h allows boards
    to optionally configure it.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/stm32/mpconfigport.h
+++ ports/stm32/mpconfigport.h
@@ -82,7 +82,6 @@
 #ifndef MICROPY_PY_BUILTINS_HELP_TEXT
 #define MICROPY_PY_BUILTINS_HELP_TEXT stm32_help_text
 #endif
-#define MICROPY_PY_IO_FILEIO        (MICROPY_VFS_FAT || MICROPY_VFS_LFS1 || MICROPY_VFS_LFS2)
 #ifndef MICROPY_PY_SYS_PLATFORM     // let boards override it if they want
 #define MICROPY_PY_SYS_PLATFORM     "pyboard"
 #endif
