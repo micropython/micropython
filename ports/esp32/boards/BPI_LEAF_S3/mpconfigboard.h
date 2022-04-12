0f2127d36647bea2778357851669f3bb725f9fec
Wind-stormger <76462385+Wind-stormger@users.noreply.github.com>
2022-04-12 14:31:30 +0800

    ports/esp32: Add support for 2 new BPI boards
    Add support for BPI-Leaf-S2 and BPI-Leaf-S3 board , the former uses esp32-s2 , the latter uses esp32-s3.

---
+++ ports/esp32/boards/BPI_LEAF_S3/mpconfigboard.h
@@ -0,0 +1,8 @@
+#define MICROPY_HW_BOARD_NAME               "BPI-Leaf-S3"
+#define MICROPY_HW_MCU_NAME                 "ESP32-S3"
+
+#define MICROPY_PY_BLUETOOTH                (0)
+#define MICROPY_PY_MACHINE_DAC              (0)
+
+#define MICROPY_HW_I2C0_SCL                 (16)
+#define MICROPY_HW_I2C0_SDA                 (15)
