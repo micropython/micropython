60c5968f99393506cf97fe6bf5a3d3d0d39723f3
Jim Mussared <jim.mussared@gmail.com>
2022-04-20 21:14:47 +1000

    pic16bit: Make port-specific modules use MP_REGISTER_MODULE.
    Signed-off-by: Jim Mussared <jim.mussared@gmail.com>

--- ports/pic16bit/mpconfigport.h
+++ ports/pic16bit/mpconfigport.h
@@ -88,11 +88,6 @@ typedef int mp_off_t;
 #define MICROPY_PORT_BUILTINS \
     { MP_ROM_QSTR(MP_QSTR_open), MP_ROM_PTR(&mp_builtin_open_obj) },
 
-// extra builtin modules to add to the list of known ones
-extern const struct _mp_obj_module_t pyb_module;
-#define MICROPY_PORT_BUILTIN_MODULES \
-    { MP_ROM_QSTR(MP_QSTR_pyb), MP_ROM_PTR(&pyb_module) }, \
-
 #define MP_STATE_PORT MP_STATE_VM
 
 #define MICROPY_PORT_ROOT_POINTERS \
