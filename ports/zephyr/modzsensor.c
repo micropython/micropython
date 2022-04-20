ec2fe5d6ca30cec6162e27e154d3e2deaf83bf93
Jim Mussared <jim.mussared@gmail.com>
2022-04-20 21:27:10 +1000

    zephyr: Make port-specific modules use MP_REGISTER_MODULE.
    Signed-off-by: Jim Mussared <jim.mussared@gmail.com>

--- ports/zephyr/modzsensor.c
+++ ports/zephyr/modzsensor.c
@@ -142,4 +142,6 @@ const mp_obj_module_t mp_module_zsensor = {
     .globals = (mp_obj_dict_t *)&mp_module_zsensor_globals,
 };
 
+MP_REGISTER_MODULE(MP_QSTR_zsensor, mp_module_zsensor, MICROPY_PY_ZSENSOR);
+
 #endif // MICROPY_PY_UHASHLIB
