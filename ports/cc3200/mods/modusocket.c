1d33ceb0d0235d657afac69ba27ab08c6477df4f
Jim Mussared <jim.mussared@gmail.com>
2022-04-20 21:33:45 +1000

    cc3200: Make port-specific modules use MP_REGISTER_MODULE.
    Signed-off-by: Jim Mussared <jim.mussared@gmail.com>

--- ports/cc3200/mods/modusocket.c
+++ ports/cc3200/mods/modusocket.c
@@ -816,3 +816,5 @@ const mp_obj_module_t mp_module_usocket = {
     .base = { &mp_type_module },
     .globals = (mp_obj_dict_t*)&mp_module_usocket_globals,
 };
+
+MP_REGISTER_MODULE(MP_QSTR_usocket, mp_module_usocket, 1);
