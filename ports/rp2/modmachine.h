857883d9953083af4136c1f027e03561cb633890
iabdalkader <i.abdalkader@gmail.com>
2022-04-27 11:11:24 +0200

    rp2/modmachine.c: Allow boards to provide custom bootloader code.

--- ports/rp2/modmachine.h
+++ ports/rp2/modmachine.h
@@ -18,5 +18,6 @@ void machine_pin_deinit(void);
 void machine_i2s_init0(void);
 
 struct _machine_spi_obj_t *spi_from_mp_obj(mp_obj_t o);
+NORETURN mp_obj_t machine_bootloader(size_t n_args, const mp_obj_t *args);
 
 #endif // MICROPY_INCLUDED_RP2_MODMACHINE_H
