466f2a3644d01fabc85c3454465a72ad293b8b25
Kong Wai Weng <waiweng83@gmail.com>
2022-05-11 15:25:32 +0800

    Updated copyright information.

--- ports/espressif/boards/cytron_maker_feather_aiot_s3/board.c
+++ ports/espressif/boards/cytron_maker_feather_aiot_s3/board.c
@@ -3,7 +3,7 @@
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2020 Scott Shawcroft for Adafruit Industries
+ * Copyright (c) 2022 Wai Weng for Cytron Technologies
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
