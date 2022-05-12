b9ea7458ab5a3034988b26efe3fcd78dce04c481
SimonVail <35699287+SimonVail@users.noreply.github.com>
2022-05-12 11:41:53 -0400

    Add files via upload

---
+++ ports/espressif/boards/Lilygo-T-oi-Plus-ESP32C3/board.c
@@ -0,0 +1,34 @@
+#include "shared-bindings/microcontroller/Pin.h"
+#include "supervisor/board.h"
+
+#include "components/driver/include/driver/gpio.h"
+
+void board_init(void) {
+    
+}
+
+bool board_requests_safe_mode(void) {
+    return false;
+}
+
+bool espressif_board_reset_pin_number(gpio_num_t pin_number) {
+    // Pull LED down on reset rather than the default up
+    if (pin_number == MICROPY_HW_LED_STATUS->number) {
+        gpio_config_t cfg = {
+            .pin_bit_mask = BIT64(pin_number),
+            .mode = GPIO_MODE_DISABLE,
+            .pull_up_en = false,
+            .pull_down_en = true,
+            .intr_type = GPIO_INTR_DISABLE,
+        };
+        gpio_config(&cfg);
+        return true;
+    }
+    return false;
+}
+
+void reset_board(void) {
+}
+
+void board_deinit(void) {
+}
