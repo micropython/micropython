a30de85d575833d4f38001fe48a65b915040dad6
Scott Shawcroft <scott@tannewt.org>
2022-05-18 14:01:35 -0700

    Add dotenv read support
    os.getenv() will use it (when available) to load variables from
    /.env
    
    This will also be useful when we need secrets or config for
    CircuitPython outside of the VM (like WiFi credentials.)
    
    Fixes #4212

--- shared-module/dotenv/__init__.h
+++ shared-module/dotenv/__init__.h
@@ -3,7 +3,7 @@
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2016 Scott Shawcroft for Adafruit Industries
+ * Copyright (c) 2022 Scott Shawcroft for Adafruit Industries
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -24,29 +24,5 @@
  * THE SOFTWARE.
  */
 
-#ifndef MICROPY_INCLUDED_SHARED_BINDINGS_OS___INIT___H
-#define MICROPY_INCLUDED_SHARED_BINDINGS_OS___INIT___H
-
-#include <stdint.h>
-#include <stdbool.h>
-
-#include "py/objtuple.h"
-
-extern const mp_rom_obj_tuple_t common_hal_os_uname_info_obj;
-
-mp_obj_t common_hal_os_uname(void);
-void common_hal_os_chdir(const char *path);
-mp_obj_t common_hal_os_getcwd(void);
-mp_obj_t common_hal_os_getenv(const char *key, mp_obj_t default_);
-mp_obj_t common_hal_os_listdir(const char *path);
-void common_hal_os_mkdir(const char *path);
-void common_hal_os_remove(const char *path);
-void common_hal_os_rename(const char *old_path, const char *new_path);
-void common_hal_os_rmdir(const char *path);
-mp_obj_t common_hal_os_stat(const char *path);
-mp_obj_t common_hal_os_statvfs(const char *path);
-
-// Returns true if data was correctly sourced from a true random number generator.
-bool common_hal_os_urandom(uint8_t *buffer, mp_uint_t length);
-
-#endif  // MICROPY_INCLUDED_SHARED_BINDINGS_OS___INIT___H
+// Allocation free version that returns the full length of the value.
+mp_int_t dotenv_get_key(const char *path, const char *key, char *value, mp_int_t value_len);
