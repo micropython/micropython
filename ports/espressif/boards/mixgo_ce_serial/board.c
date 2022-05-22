b62799a798e21c1e7b56a8d38b97c0c4c240c929
dahanzimin <353767514@qq.com>
2022-05-22 10:42:56 +0800

    Add mixgo_ ce_ Serial function, repair sdkconfig

---
+++ ports/espressif/boards/mixgo_ce_serial/board.c
@@ -0,0 +1,70 @@
+/*
+ * This file is part of the MicroPython project, http://micropython.org/
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 Scott Shawcroft for Adafruit Industries
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include "supervisor/board.h"
+#include "mpconfigboard.h"
+#include "shared-bindings/microcontroller/Pin.h"
+
+#include "lib/oofatfs/ff.h"
+#include "extmod/vfs_fat.h"
+#include "py/mpstate.h"
+
+void board_init(void) {
+    // Debug UART
+    #ifdef DEBUG
+    common_hal_never_reset_pin(&pin_GPIO43);
+    common_hal_never_reset_pin(&pin_GPIO44);
+    #endif /* DEBUG */
+	
+	mp_import_stat_t stat_b = mp_import_stat("boot.py");
+    if (stat_b != MP_IMPORT_STAT_FILE){
+        FATFS *fatfs = &((fs_user_mount_t *) MP_STATE_VM(vfs_mount_table)->obj)->fatfs;
+        FIL fs;
+        UINT char_written = 0;
+        const byte buffer[] = "#Serial port upload mode\nimport storage\nstorage.remount(\"/\", False)\nstorage.disable_usb_drive()\n";
+        // Create or modify existing boot.py file
+        f_open(fatfs, &fs, "/boot.py", FA_WRITE | FA_CREATE_ALWAYS);
+        f_write(&fs, buffer, sizeof(buffer) - 1, &char_written);
+        f_close(&fs);
+        //Delete code.Py, use main.py
+        mp_import_stat_t stat_c = mp_import_stat("code.py");
+        if (stat_c == MP_IMPORT_STAT_FILE){
+            f_unlink(fatfs,"/code.py");
+        }
+    }
+	
+}
+
+bool board_requests_safe_mode(void) {
+    return false;
+}
+
+void reset_board(void) {
+
+}
+
+void board_deinit(void) {
+}
