a5d5ecbf8434e480b42a8a477ef9ba7979bba927
robert-hh <robert@hammelrath.com>
2022-06-04 13:27:12 +0200

    samd/boards: Move mcu-specific settings into a mpconfig_samdXX.h file.
    Located at the boards directory. That way, the mpconfigboard.h files are
    almost empty, just setting the board name and the MCU name.

--- ports/samd/boards/MINISAM_M4/mpconfigboard.h
+++ ports/samd/boards/MINISAM_M4/mpconfigboard.h
@@ -1,17 +1,2 @@
 #define MICROPY_HW_BOARD_NAME "Mini SAM M4"
 #define MICROPY_HW_MCU_NAME   "SAMD51G19A"
-
-#define MICROPY_FLOAT_IMPL              (MICROPY_FLOAT_IMPL_FLOAT)
-#define MICROPY_PY_BUILTINS_COMPLEX     (0)
-#define MICROPY_PY_MATH                 (0)
-#define MICROPY_PY_CMATH                (0)
-
-// MicroPython configs
-// samd_flash.c flash parameters
-// Build a 128k Flash storage at top. 512k-128k=384k=0x60000
-// 512*1024= 0x80000 minus 128*1024= 0x20000 =  0x60000
-#define MICROPY_HW_FLASH_STORAGE_BASE       (0x60000)
-#define MICROPY_HW_FLASH_STORAGE_BYTES      (0x1FFFF)
-#define VFS_BLOCK_SIZE_BYTES                (1536) //
-
-#define CPU_FREQ (48000000) // For selecting Baud from clock.
---
+++ ports/samd/boards/mpconfig_samd51.h
@@ -0,0 +1,21 @@
+// Deinitions common to all SAMD51 boards
+#include "samd51.h"
+
+#define MICROPY_FLOAT_IMPL              (MICROPY_FLOAT_IMPL_FLOAT)
+#define MICROPY_PY_BUILTINS_COMPLEX     (0)
+#define MICROPY_PY_MATH                 (0)
+#define MICROPY_PY_CMATH                (0)
+
+// Due to a limitation in the TC counter for us, the ticks period is 2**29
+#define MICROPY_PY_UTIME_TICKS_PERIOD   (0x20000000)
+// MicroPython configs
+// samd_flash.c flash parameters
+// Build a 128k Flash storage at top. 512k-128k=384k=0x60000
+// 512*1024= 0x80000 minus 128*1024= 0x20000 =  0x60000
+#define MICROPY_HW_FLASH_STORAGE_BASE       (0x60000)
+#define MICROPY_HW_FLASH_STORAGE_BYTES      (0x1FFFF)
+#define VFS_BLOCK_SIZE_BYTES                (1536) //
+
+#define CPU_FREQ            (120000000)
+#define APB_FREQ            (48000000)
+#define DPLLx_REF_FREQ      (32768)
