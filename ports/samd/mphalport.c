3d9940bc28e66464e22386916cb71100d6b24f1d
robert-hh <robert@hammelrath.com>
2022-06-04 18:09:11 +0200

    samd/mphalport: Add pin open-drain funcs, and improve delay and ticks.
    The changes in this commit are:
    - Add an interface for pin open-drain mode.
    - Improve ticks_us() by using the us-counter.
    - Improve ticks_cpu() by using the CPU's SysTick.

--- ports/samd/mphalport.c
+++ ports/samd/mphalport.c
@@ -4,6 +4,7 @@
  * The MIT License (MIT)
  *
  * Copyright (c) 2019 Damien P. George
+ * Copyright (c) 2022 Robert Hammelrath
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -27,10 +28,13 @@
 #include "py/runtime.h"
 #include "py/mphal.h"
 #include "py/stream.h"
+#include "shared/runtime/interrupt_char.h"
+#include "extmod/misc.h"
 #include "samd_soc.h"
 #include "tusb.h"
 
 #if MICROPY_KBD_EXCEPTION
+int mp_interrupt_char = -1;
 
 void tud_cdc_rx_wanted_cb(uint8_t itf, char wanted_char) {
     (void)itf;
@@ -40,24 +44,47 @@ void tud_cdc_rx_wanted_cb(uint8_t itf, char wanted_char) {
 }
 
 void mp_hal_set_interrupt_char(int c) {
+    mp_interrupt_char = c;
     tud_cdc_set_wanted_char(c);
 }
 
 #endif
 
+void mp_hal_set_pin_mux(mp_hal_pin_obj_t pin, uint8_t mux) {
+    int pin_grp = pin / 32;
+    int port_grp = (pin % 32) / 2;
+    PORT->Group[pin_grp].PINCFG[pin % 32].bit.PMUXEN = 1; // Enable Mux
+    if (pin & 1) {
+        PORT->Group[pin_grp].PMUX[port_grp].bit.PMUXO = mux;
+    } else {
+        PORT->Group[pin_grp].PMUX[port_grp].bit.PMUXE = mux;
+    }
+}
+
+
 void mp_hal_delay_ms(mp_uint_t ms) {
-    ms += 1;
-    uint32_t t0 = systick_ms;
-    while (systick_ms - t0 < ms) {
-        MICROPY_EVENT_POLL_HOOK
+    if (ms > 10) {
+        uint32_t t0 = systick_ms;
+        while (systick_ms - t0 < ms) {
+            MICROPY_EVENT_POLL_HOOK
+        }
+    } else {
+        mp_hal_delay_us(ms * 1000);
     }
 }
 
 void mp_hal_delay_us(mp_uint_t us) {
-    uint32_t ms = us / 1000 + 1;
-    uint32_t t0 = systick_ms;
-    while (systick_ms - t0 < ms) {
-        __WFI();
+    if (us > 0) {
+        uint32_t start = mp_hal_ticks_us();
+        #if defined(MCU_SAMD21)
+        // SAMD21 counter has effective 32 bit width
+        while ((mp_hal_ticks_us() - start) < us) {
+        }
+        #elif defined(MCU_SAMD51)
+        // SAMD51 counter has effective 29 bit width
+        while (((mp_hal_ticks_us() - start) & (MICROPY_PY_UTIME_TICKS_PERIOD - 1)) < us) {
+        }
+        #endif
     }
 }
 
@@ -78,7 +105,7 @@ int mp_hal_stdin_rx_chr(void) {
                 return buf[0];
             }
         }
-        __WFI();
+        MICROPY_EVENT_POLL_HOOK
     }
 }
 
@@ -90,7 +117,7 @@ void mp_hal_stdout_tx_strn(const char *str, mp_uint_t len) {
                 n = CFG_TUD_CDC_EP_BUFSIZE;
             }
             while (n > tud_cdc_write_available()) {
-                __WFI();
+                MICROPY_EVENT_POLL_HOOK
             }
             uint32_t n2 = tud_cdc_write(str + i, n);
             tud_cdc_write_flush();
