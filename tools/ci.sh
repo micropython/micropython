17f0297c1ac274b4e9f1040e8282a592715e8cb8
David Lechner <david@pybricks.com>
2022-06-08 11:32:17 -0500

    tools/ci.sh: Drop ppa requirement for code formatting CI.
    The CI scripts were using a PPA to get a backported version of uncrustify
    on Ubuntu 20.04.  However, this causes CI to intermittently fail due to
    connection issues to launchpad.net or the key server.
    
    Ubuntu 22.04 has a newer version of uncrustify removing the need for the
    PPA.  Ubuntu 22.04 is now in beta on GitHub actions, so it can be used.
    
    Signed-off-by: David Lechner <david@pybricks.com>

--- tools/ci.sh
+++ tools/ci.sh
@@ -18,7 +18,6 @@ function ci_gcc_arm_setup {
 # code formatting
 
 function ci_code_formatting_setup {
-    sudo apt-add-repository --yes --update ppa:pybricks/ppa
     sudo apt-get install uncrustify
     pip3 install black
     uncrustify --version
