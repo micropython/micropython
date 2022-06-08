6a1dbaad1ca7a8e065ee69e4107497e507afdea6
Damien George <damien@micropython.org>
2022-06-08 14:51:48 +1000

    tools/ci.sh: Build NUCLEO_H743ZI with -O2 optimisation level.
    This tests the build when -O2 is used, which can lead to additional
    compiler analysis and warnings.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- tools/ci.sh
+++ tools/ci.sh
@@ -320,7 +320,7 @@ function ci_stm32_nucleo_build {
 
     # Test building various MCU families, some with additional options.
     make ${MAKEOPTS} -C ports/stm32 BOARD=NUCLEO_F091RC
-    make ${MAKEOPTS} -C ports/stm32 BOARD=NUCLEO_H743ZI CFLAGS_EXTRA='-DMICROPY_PY_THREAD=1'
+    make ${MAKEOPTS} -C ports/stm32 BOARD=NUCLEO_H743ZI COPT=-O2 CFLAGS_EXTRA='-DMICROPY_PY_THREAD=1'
     make ${MAKEOPTS} -C ports/stm32 BOARD=NUCLEO_L073RZ
     make ${MAKEOPTS} -C ports/stm32 BOARD=NUCLEO_L476RG DEBUG=1
 
