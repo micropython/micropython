ac45780795db5adb9ff1c10c7bdd2ddb23718c04
robert-hh <robert@hammelrath.com>
2022-06-15 18:58:02 +0200

    samd: Two extension to the clock set-up.
    Two new compile flags:
    
    MICROPY_HW_DFLL_USB_SYNC
    
    Effective only if DFLL48 does not run from the crystal. It will
    synchronize the DFLL48M clock with the USB's SOF pulse. If no USB
    is connected, it will fall back to open loop mode. The DFLL48M clock
    is then pretty precise, but with a higher clock jitter at SAMD51
    devices.
    
    MICROPY_HW_MCU_OSC32KULP
    
    Effective only if the devics uses a crystal as clock source.
    Run the MCU clock from the ULP 32kHz oszillator instead of the crystal.
    This flag was added to cater for a interference problem
    of the crystal and Neopixel/Debug pins at Adafruit FEATHER Mx boards,
    which causes the board to crash. Drawback: ticks_ms() and time.time()
    vs. than ticks_us() and the peripherals like PWM run at not
    synchronous clocks.

--- ports/samd/variants/samd51/clock_config.c
+++ ports/samd/variants/samd51/clock_config.c
@@ -29,10 +29,12 @@
 #include <stdint.h>
 
 #include "py/runtime.h"
+#include "py/mphal.h"
 #include "samd_soc.h"
 
 static uint32_t cpu_freq = CPU_FREQ;
 static uint32_t apb_freq = APB_FREQ;
+static uint32_t dfll48m_calibration;
 
 int sercom_gclk_id[] = {
     SERCOM0_GCLK_ID_CORE, SERCOM1_GCLK_ID_CORE,
@@ -84,12 +86,43 @@ void set_cpu_freq(uint32_t cpu_freq_arg) {
     }
 }
 
+void check_usb_recovery_mode(void) {
+    #if !MICROPY_HW_XOSC32K
+    mp_hal_delay_ms(500);
+    // Check USB status. If not connected, switch DFLL48M back to open loop
+    if (USB->DEVICE.DeviceEndpoint[0].EPCFG.reg == 0) {
+        // as per Errata 2.8.3
+        OSCCTRL->DFLLMUL.reg = 0;
+        while (OSCCTRL->DFLLSYNC.bit.DFLLMUL == 1) {
+        }
+        // Set the mode to open loop mode
+        OSCCTRL->DFLLCTRLB.reg = 0;
+        while (OSCCTRL->DFLLSYNC.bit.DFLLCTRLB == 1) {
+        }
+        OSCCTRL->DFLLCTRLA.reg = OSCCTRL_DFLLCTRLA_RUNSTDBY | OSCCTRL_DFLLCTRLA_ENABLE;
+        while (OSCCTRL->DFLLSYNC.bit.ENABLE == 1) {
+        }
+        OSCCTRL->DFLLVAL.reg = dfll48m_calibration; // Reload DFLLVAL register
+        while (OSCCTRL->DFLLSYNC.bit.DFLLVAL == 1) {
+        }
+        // Set the mode to open loop mode
+        OSCCTRL->DFLLCTRLB.reg = 0;
+        while (OSCCTRL->DFLLSYNC.bit.DFLLCTRLB == 1) {
+        }
+    }
+    #endif // MICROPY_HW_XOSC32K
+}
+
 void init_clocks(uint32_t cpu_freq) {
+
+    dfll48m_calibration = 0; // please the compiler
+
     // SAMD51 clock settings
     // GCLK0: 48MHz from DFLL48M or 48 - 200 MHz from DPLL0 (SAMD51)
-    // GCLK1: DPLLx_REF_FREQ 32768 Hz from 32KULP or 32k Crystal
+    // GCLK1: 32768 Hz from 32KULP or DFLL48M
     // GCLK2: 48MHz from DFLL48M for Peripheral devices
     // GCLK3: 16Mhz for the us-counter (TC0/TC1)
+    // GCLK4: 32kHz from crystal, if present
     // DPLL0: 48 - 200 MHz
 
     // Steps to set up clocks:
@@ -102,6 +135,7 @@ void init_clocks(uint32_t cpu_freq) {
     // Setup GCLK0 to 120MHz
     // Setup GCLK2 to 48MHz for Peripherals
     // Setup GCLK3 to 8MHz for TC0/TC1
+    // Setup GCLK4 to 32kHz crystal, if present
 
     // Setup GCLK0 for 48MHz as default state to keep the MCU running during config change.
     GCLK->GENCTRL[0].reg = GCLK_GENCTRL_RUNSTDBY | GCLK_GENCTRL_GENEN | GCLK_GENCTRL_SRC_DFLL;
@@ -124,15 +158,26 @@ void init_clocks(uint32_t cpu_freq) {
     while (OSC32KCTRL->STATUS.bit.XOSC32KRDY == 0) {
     }
 
+    #if MICROPY_HW_MCU_OSC32KULP
+    // Setup GCLK1 for 32kHz ULP
+    GCLK->GENCTRL[1].reg = GCLK_GENCTRL_RUNSTDBY | GCLK_GENCTRL_GENEN | GCLK_GENCTRL_SRC_OSCULP32K;
+    #else
     // Setup GCLK1 for 32kHz crystal
     GCLK->GENCTRL[1].reg = GCLK_GENCTRL_RUNSTDBY | GCLK_GENCTRL_GENEN | GCLK_GENCTRL_SRC_XOSC32K;
+    #endif
+
     while (GCLK->SYNCBUSY.bit.GENCTRL1) {
+    }
+
+    // Setup GCLK4 for 32kHz crystal
+    GCLK->GENCTRL[4].reg = GCLK_GENCTRL_RUNSTDBY | GCLK_GENCTRL_GENEN | GCLK_GENCTRL_SRC_XOSC32K;
+    while (GCLK->SYNCBUSY.bit.GENCTRL4) {
     }
 
     // Set-up the DFLL48M in closed loop mode with input from the 32kHz crystal
 
-    // Step 1: Peripheral channel 0 is driven by GCLK1 and it feeds DFLL48M
-    GCLK->PCHCTRL[0].reg = GCLK_PCHCTRL_GEN_GCLK1 | GCLK_PCHCTRL_CHEN;
+    // Step 1: Peripheral channel 0 is driven by GCLK4 and it feeds DFLL48M
+    GCLK->PCHCTRL[0].reg = GCLK_PCHCTRL_GEN_GCLK4 | GCLK_PCHCTRL_CHEN;
     while (GCLK->PCHCTRL[0].bit.CHEN == 0) {
     }
     // Step 2: Set the multiplication values. The offset of 16384 to the freq is for rounding.
@@ -141,7 +186,7 @@ void init_clocks(uint32_t cpu_freq) {
     while (OSCCTRL->DFLLSYNC.bit.DFLLMUL == 1) {
     }
     // Step 3: Set the mode to closed loop
-    OSCCTRL->DFLLCTRLB.reg = OSCCTRL_DFLLCTRLB_BPLCKC | OSCCTRL_DFLLCTRLB_MODE;
+    OSCCTRL->DFLLCTRLB.reg = OSCCTRL_DFLLCTRLB_BPLCKC | OSCCTRL_DFLLCTRLB_STABLE | OSCCTRL_DFLLCTRLB_MODE;
     while (OSCCTRL->DFLLSYNC.bit.DFLLCTRLB == 1) {
     }
     // Wait for lock fine
@@ -154,11 +199,33 @@ void init_clocks(uint32_t cpu_freq) {
 
     #else // MICROPY_HW_XOSC32K
 
-    // Set GCLK1 to DPLL0_REF_FREQ as defined in mpconfigboard.h (e.g. 32768 Hz)
+    // Derive GCLK1 from DFLL48M at DPLL0_REF_FREQ as defined in mpconfigboard.h (e.g. 32768 Hz)
     GCLK->GENCTRL[1].reg = ((APB_FREQ + DPLLx_REF_FREQ / 2) / DPLLx_REF_FREQ) << GCLK_GENCTRL_DIV_Pos
         | GCLK_GENCTRL_GENEN | GCLK_GENCTRL_SRC_DFLL;
     while (GCLK->SYNCBUSY.bit.GENCTRL1) {
     }
+
+    OSCCTRL->DFLLCTRLA.bit.RUNSTDBY = 1;
+    OSCCTRL->DFLLCTRLA.bit.ONDEMAND = 0;
+
+    OSCCTRL->DFLLCTRLA.bit.ENABLE = 1;
+    while (OSCCTRL->DFLLSYNC.bit.ENABLE == 1) {
+    }
+
+    #if MICROPY_HW_DFLL_USB_SYNC
+    // Configure the DFLL48M for USB clock recovery.
+    // Will have to switch back if no USB
+    dfll48m_calibration = OSCCTRL->DFLLVAL.reg;
+    // Set the Multiplication factor.
+    OSCCTRL->DFLLMUL.reg = OSCCTRL_DFLLMUL_MUL(48000) |
+        OSCCTRL_DFLLMUL_FSTEP(1) | OSCCTRL_DFLLMUL_CSTEP(1);
+    while (OSCCTRL->DFLLSYNC.bit.DFLLMUL == 1) {
+    }
+    // Set the mode to closed loop USB Recovery
+    OSCCTRL->DFLLCTRLB.reg = OSCCTRL_DFLLCTRLB_USBCRM | OSCCTRL_DFLLCTRLB_CCDIS | OSCCTRL_DFLLCTRLB_MODE;
+    while (OSCCTRL->DFLLSYNC.bit.DFLLCTRLB == 1) {
+    }
+    #endif
 
     #endif // MICROPY_HW_XOSC32K
 
@@ -183,7 +250,6 @@ void init_clocks(uint32_t cpu_freq) {
 }
 
 void enable_sercom_clock(int id) {
-    // Next: Set up the clocks
     GCLK->PCHCTRL[sercom_gclk_id[id]].reg = GCLK_PCHCTRL_CHEN | GCLK_PCHCTRL_GEN_GCLK2;
     // no easy way to set the clocks, except enabling all of them
     switch (id) {
