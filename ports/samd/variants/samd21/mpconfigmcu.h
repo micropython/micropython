5a14fa37fa41354744392c8c38a32d896a6f745a
robert-hh <robert@hammelrath.com>
2022-06-15 15:16:07 +0200

    samd: Restructure the mpconfigxxx.h files.
    - Set the feature level for each MCU. CORE features for SAMD21, and
      EXTRA features for SAMD51.
    - Remove all definitions that are included in the core feature level.
    - Keep the default settings for feature level and float,
      to make the choice obvious.

--- ports/samd/variants/samd21/mpconfigmcu.h
+++ ports/samd/variants/samd21/mpconfigmcu.h
@@ -1,16 +1,24 @@
 // Deinitions common to all SAMD21 boards
 #include "samd21.h"
 
-#define MICROPY_HW_FLASH_STORAGE_BASE       (0x30000)
-#define MICROPY_HW_FLASH_STORAGE_BYTES      (0xFFFF)
-#define VFS_BLOCK_SIZE_BYTES                (1536) // 24x 64B flash pages;
+#define MICROPY_CONFIG_ROM_LEVEL        (MICROPY_CONFIG_ROM_LEVEL_CORE_FEATURES)
 
-#define MICROPY_HW_UART_TXBUF               (1)
+// MicroPython emitters
+#define MICROPY_EMIT_THUMB              (0)
+#define MICROPY_EMIT_INLINE_THUMB       (0)
 
-#define CPU_FREQ            (48000000)
-#define APB_FREQ            (48000000)
+#define MICROPY_FLOAT_IMPL              (MICROPY_FLOAT_IMPL_NONE)
 
-#define IRQ_PRI_PENDSV          ((1 << __NVIC_PRIO_BITS) - 1)
+#define MICROPY_HW_FLASH_STORAGE_BASE   (0x30000)
+#define MICROPY_HW_FLASH_STORAGE_BYTES  (0xFFFF)
+#define VFS_BLOCK_SIZE_BYTES            (1536) // 24x 64B flash pages;
+
+#define MICROPY_HW_UART_TXBUF           (1)
+
+#define CPU_FREQ                        (48000000)
+#define APB_FREQ                        (48000000)
+
+#define IRQ_PRI_PENDSV                  ((1 << __NVIC_PRIO_BITS) - 1)
 
 static inline uint32_t raise_irq_pri(uint32_t pri) {
     (void)pri;
