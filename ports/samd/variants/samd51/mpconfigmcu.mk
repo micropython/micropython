accce2cc03147a20e77497d3db903b3c7c03fb25
robert-hh <robert@hammelrath.com>
2022-06-21 20:31:20 +0200

    samd: Add the math module for SAMD51.

--- ports/samd/variants/samd51/mpconfigmcu.mk
+++ ports/samd/variants/samd51/mpconfigmcu.mk
@@ -1,2 +1,31 @@
 
 SRC_S += shared/runtime/gchelper_m3.s
+
+SRC_MOD +=  $(addprefix lib/libm/,\
+	acoshf.c \
+	asinfacosf.c \
+	asinhf.c \
+	atan2f.c \
+	atanf.c \
+	atanhf.c \
+	ef_rem_pio2.c \
+	erf_lgamma.c \
+	fmodf.c \
+	kf_cos.c \
+	kf_rem_pio2.c \
+	kf_sin.c \
+	kf_tan.c \
+	log1pf.c \
+	math.c \
+	nearbyintf.c \
+	roundf.c \
+	sf_cos.c \
+	sf_erf.c \
+	sf_frexp.c \
+	sf_ldexp.c \
+	sf_modf.c \
+	sf_sin.c \
+	sf_tan.c \
+	wf_lgamma.c \
+	wf_tgamma.c \
+	)
