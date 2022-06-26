d1e704bece803108573155c80c0183c159afe262
Fabio <fs.embarcados@gmail.com>
2022-06-26 09:44:48 -0300

    ports/esp32/boards: Add supoprt to Franzininho WiFi WROVER(SPIRAM).
    Adding files to Franzininho WiFi Wrover board. A variation of the 
    Franzininho WiFi based on the ESP32-S2 with PSRAM
    
    Signed-off-by: Fabio Souza <fs.embarcados@gmail.com>

---
+++ ports/esp32/boards/FRANZININHO_WIFI_WROVER/mpconfigboard.cmake
@@ -0,0 +1,10 @@
+set(IDF_TARGET esp32s2)
+
+set(SDKCONFIG_DEFAULTS
+    boards/sdkconfig.base
+    boards/sdkconfig.spiram_sx
+    boards/sdkconfig.usb
+    boards/FRANZININHO_WIFI_WROVER/sdkconfig.board
+)
+
+set(MICROPY_FROZEN_MANIFEST ${MICROPY_BOARD_DIR}/manifest.py)
