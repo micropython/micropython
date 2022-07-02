9bf97deba0b23fc1bbdd819a2ee5fa7dc8c63924
robert-hh <robert@hammelrath.com>
2022-07-02 16:36:11 +0200

    samd: Use the same loader file for all samd51x19 variants.

--- ports/samd/boards/ADAFRUIT_FEATHER_M4_EXPRESS/mpconfigboard.mk
+++ ports/samd/boards/ADAFRUIT_FEATHER_M4_EXPRESS/mpconfigboard.mk
@@ -1,6 +1,6 @@
 MCU_SERIES = SAMD51
 CMSIS_MCU = SAMD51J19A
-LD_FILES = boards/samd51j19a.ld sections.ld
+LD_FILES = boards/samd51x19a.ld sections.ld
 TEXT0 = 0x4000
 
 # The ?='s allow overriding in mpconfigboard.mk.
