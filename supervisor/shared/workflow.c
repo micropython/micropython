d869b441f4cf060dac214209527307161f89ba71
Dan Halbert <halbert@halwitz.org>
2022-07-08 15:27:00 -0400

    further ESP32 sdkconfig fixes; add CIRCUITPY_STATUS_BAR

--- supervisor/shared/workflow.c
+++ supervisor/shared/workflow.c
@@ -46,22 +46,26 @@
 #endif
 static background_callback_t workflow_background_cb;
 
+#if CIRCUITPY_STATUS_BAR
 static void supervisor_workflow_update_status_bar(void) {
-    // // Neighboring "" "" are concatenated by the compiler. Without this separation, the hex code
-    // // doesn't get terminated after two following characters and the value is invalid.
-    // // This is the OSC command to set the title and the icon text. It can be up to 255 characters
-    // // but some may be cut off.
-    // serial_write("\x1b" "]0;");
-    // serial_write("🐍 ");
-    // #if CIRCUITPY_WEB_WORKFLOW
-    // supervisor_web_workflow_status();
-    // #endif
-    // // Send string terminator
-    // serial_write("\x1b" "\\");
+    // Neighboring "" "" are concatenated by the compiler. Without this separation, the hex code
+    // doesn't get terminated after two following characters and the value is invalid.
+    // This is the OSC command to set the title and the icon text. It can be up to 255 characters
+    // but some may be cut off.
+    serial_write("\x1b" "]0;");
+    serial_write("🐍 ");
+    #if CIRCUITPY_WEB_WORKFLOW
+    supervisor_web_workflow_status();
+    #endif
+    // Send string terminator
+    serial_write("\x1b" "\\");
 }
+#endif
 
 static void workflow_background(void *data) {
+#if CIRCUITPY_STATUS_BAR
     supervisor_workflow_update_status_bar();
+#endif
 }
 
 // Called during a VM reset. Doesn't actually reset things.
