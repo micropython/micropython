fa62c875361b0370f2e94bd13eb058e7ae146c1b
Dan Halbert <halbert@halwitz.org>
2022-07-08 20:37:47 -0400

    ESP32 .bin only; update nvm.toml

--- tools/build_board_info.py
+++ tools/build_board_info.py
@@ -71,6 +71,8 @@ extension_by_board = {
     "meowbit_v121": UF2,
     "sparkfun_stm32_thing_plus": BIN_UF2,
     "swan_r5": BIN_UF2,
+    # esp32
+    "adafruit_feather_esp32_v2": BIN,
     # esp32c3
     "adafruit_qtpy_esp32c3": BIN,
     "ai_thinker_esp32-c3s": BIN,
