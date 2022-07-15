02620c2236e948b1899d122359a1eb150e5a9ee0
Angus Gratton <angus@redyak.com.au>
2022-07-15 14:04:33 +1000

    stm32/subghz: Add STM32WL55 subghz radio interface to stm module.
    This is the minimum C interface to allow a modem driver to be built in
    Python.  Interface is simple, with the intention that the micropython-lib
    driver is the main (only) consumer of it.
    
    Signed-off-by: Angus Gratton <angus@redyak.com.au>

--- ports/stm32/boards/NUCLEO_WL55/mpconfigboard.h
+++ ports/stm32/boards/NUCLEO_WL55/mpconfigboard.h
@@ -14,7 +14,7 @@
 #define MICROPY_PY_SOCKET                       (0)
 #define MICROPY_PY_NETWORK                      (0)
 #define MICROPY_PY_ONEWIRE                      (0)
-#define MICROPY_PY_STM                          (0)
+#define MICROPY_PY_STM                          (1)
 #define MICROPY_PY_PYB_LEGACY                   (0)
 #define MICROPY_PY_HEAPQ                        (0)
 
