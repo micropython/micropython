61883b2079c41a9fa5b7b856fb8584b575979c85
omogenot <olivier.mogenot@mymeterinfo.info>
2022-07-24 09:44:15 +0200

    Fix 4th optional parameter in wiznet5k_make_new
    Originally the code was proposing to add a 4th parameter to the WIZNET5K object in micropython, but this was not accepted.
    The 4th parameter is representing the use of an interrupt pin.

--- extmod/network_wiznet5k.c
+++ extmod/network_wiznet5k.c
@@ -687,7 +687,6 @@ STATIC mp_obj_t wiznet5k_make_new(const mp_obj_type_t *type, size_t n_args, size
 
     #ifdef MICROPY_HW_WIZNET_SPI_ID
     // check arguments
-    mp_arg_check_num(n_args, n_kw, 0, 3, false);
     // Allow auto-configuration of SPI if defined for board and no args passed
     if (n_args == 0) {
         // Initialize SPI.
