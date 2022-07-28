b4bb29c0fc392ffc8d4be3f83fa29c15272954ed
robert-hh <robert@hammelrath.com>
2022-07-28 16:47:40 +0200

    network_wiznet5k.c: Check for the absence of keyword arguments.
    Using keyword arguments raises an error now.

--- extmod/network_wiznet5k.c
+++ extmod/network_wiznet5k.c
@@ -693,7 +693,7 @@ STATIC mp_obj_t wiznet5k_make_new(const mp_obj_type_t *type, size_t n_args, size
 
     #ifdef MICROPY_HW_WIZNET_SPI_ID
     // Allow auto-configuration of SPI if defined for board and no args passed
-    if (n_args == 0) {
+    if (n_args == 0 && n_kw == 0) {
         // Initialize SPI.
         mp_obj_t spi_obj = MP_OBJ_NEW_SMALL_INT(MICROPY_HW_WIZNET_SPI_SCK);
         mp_obj_t miso_obj = MP_OBJ_NEW_SMALL_INT(MICROPY_HW_WIZNET_SPI_MISO);
