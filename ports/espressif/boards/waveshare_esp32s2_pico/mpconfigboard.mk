c71cc4e0dd9b73dda436a27da5e7bdd728c80abb
bill88t <bill88t@feline.gr>
2022-07-30 15:35:48 +0300

    Add Waveshare ESP32-S2-Pico

---
+++ ports/espressif/boards/waveshare_esp32s2_pico/mpconfigboard.mk
@@ -0,0 +1,17 @@
+USB_VID = 0x2E8A
+USB_PID = 0x101F
+USB_PRODUCT = "ESP32-S2-Pico"
+USB_MANUFACTURER = "Waveshare Electronics"
+
+IDF_TARGET = esp32s2
+
+INTERNAL_FLASH_FILESYSTEM = 1
+LONGINT_IMPL = MPZ
+
+# The default queue depth of 16 overflows on release builds,
+# so increase it to 32.
+CFLAGS += -DCFG_TUD_TASK_QUEUE_SZ=32
+
+CIRCUITPY_ESP_FLASH_MODE = dio
+CIRCUITPY_ESP_FLASH_FREQ = 80m
+CIRCUITPY_ESP_FLASH_SIZE = 4MB
