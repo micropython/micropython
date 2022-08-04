e24610548b7d6f85f5c957829df6f4276facd287
Joel Stanley <joel@jms.id.au>
2022-08-04 19:14:03 +0930

    libc: Implement strrchr.
    Used by libfdt.
    
    Signed-off-by: Joel Stanley <joel@jms.id.au>

--- shared/libc/string0.c
+++ shared/libc/string0.c
@@ -237,6 +237,23 @@ char *strchr(const char *s, int c)
     return ((*s == c) ? (char *) s : 0);
 }
 
+// Public Domain implementation of strchr from:
+// http://en.wikibooks.org/wiki/C_Programming/Strings#The_strrchr_function
+char *(strrchr)(const char *s, int c)
+{
+    const char *last = NULL;
+    /* If the character we're looking for is the terminating null,
+       we just need to look for that character as there's only one
+       of them in the string.  */
+    if (c == '\0')
+        return strchr(s, c);
+    /* Loop through, finding the last match before hitting NULL.  */
+    while ((s = strchr(s, c)) != NULL) {
+        last = s;
+        s++;
+    }
+    return (char *) last;
+}
 
 // Public Domain implementation of strstr from:
 // http://en.wikibooks.org/wiki/C_Programming/Strings#The_strstr_function
