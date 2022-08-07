029b6b6fbe5afc0429e8420cf55577758f1318f5
lady ada <limor@ladyada.net>
2022-08-07 14:12:40 -0400

    add default display

--- ports/espressif/boards/hardkernel_odroid_go/pins.c
+++ ports/espressif/boards/hardkernel_odroid_go/pins.c
@@ -53,5 +53,6 @@ STATIC const mp_rom_map_elem_t board_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_EXT4), MP_ROM_PTR(&pin_GPIO15) },
 
     { MP_ROM_QSTR(MP_QSTR_SPI), MP_ROM_PTR(&board_spi_obj) },
+    { MP_ROM_QSTR(MP_QSTR_DISPLAY), MP_ROM_PTR(&displays[0].display)},
 };
 MP_DEFINE_CONST_DICT(board_module_globals, board_module_globals_table);
