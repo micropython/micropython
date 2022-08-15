8b5937c436ce061cb14ba384dbbc32608e89a896
Jim Mussared <jim.mussared@gmail.com>
2022-08-16 01:56:25 +1000

    ports/unix: Refactor mpconfigvariant.mk.
    All variants (except minimal) enable text compression and fat/lfs, so move
    them to the common mpconfigport.mk.
    
    Signed-off-by: Jim Mussared <jim.mussared@gmail.com>

--- ports/unix/variants/standard/mpconfigvariant.mk
+++ ports/unix/variants/standard/mpconfigvariant.mk
@@ -1,8 +1,3 @@
 # This is the default variant when you `make` the Unix port.
 
 FROZEN_MANIFEST ?= $(VARIANT_DIR)/manifest.py
-
-MICROPY_ROM_TEXT_COMPRESSION = 1
-MICROPY_VFS_FAT ?= 1
-MICROPY_VFS_LFS1 ?= 1
-MICROPY_VFS_LFS2 ?= 1
