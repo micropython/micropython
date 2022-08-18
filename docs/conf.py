1bef709c0e19bb8c6c6f7b5c644354479472f59c
Matt Trentini <matt.trentini@gmail.com>
2022-08-19 01:15:50 +1000

    docs: Update CPython differences and improve the look of table layouts.

--- docs/conf.py
+++ docs/conf.py
@@ -166,6 +166,10 @@ html_favicon = 'static/favicon.ico'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['static']
 
+# Add a custom CSS file for HTML generation
+html_css_files = [
+    'custom.css',
+]
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
