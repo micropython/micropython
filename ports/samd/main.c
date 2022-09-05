2063d7b11087f243d5be6cd75462bfd8dab36a7f
robert-hh <robert@hammelrath.com>
2022-09-05 11:29:08 +0200

    samd/DAC: Add the dac.write_timed().
    Call:
    dac.write_timed(data, freq [, count])
    dac.deinit()
    
    Working range for dac_timed():
    
    SAMD21: 1 Hz - 100 kHz (1 MHz clock, 10 bit)
    SAMD51: 1 Hz - ~500 kHz (8 MHz clock, 12 bit)
    
    The buffer has to be a byte array or a halfword array,
    and the data is sent once.
    
    The default for count is 1. If set to a value > 0, the data will be
    transmitted count times. If set to 0 or  < 0, the date will be
    transmitted until deliberately stopped. The playback
    can be stopped with dac.deinit().
    
    dac.deinit() just releases the timer and DMA channel needed by
    dac_timed(). The DAC object itself does not have to be released.
    
    Signed-off-by: robert-hh <robert@hammelrath.com>

--- ports/samd/main.c
+++ ports/samd/main.c
@@ -35,6 +35,8 @@
 #include "shared/runtime/softtimer.h"
 #include "shared/tinyusb/mp_usbd.h"
 #include "clock_config.h"
+#include "dma_manager.h"
+#include "tc_manager.h"
 
 extern uint8_t _sstack, _estack, _sheap, _eheap;
 extern void adc_deinit_all(void);
@@ -87,6 +89,8 @@ void samd_main(void) {
 
     soft_reset_exit:
         mp_printf(MP_PYTHON_PRINTER, "MPY: soft reboot\n");
+        dma_deinit();
+        tc_deinit();
         #if MICROPY_PY_MACHINE_ADC
         adc_deinit_all();
         #endif
