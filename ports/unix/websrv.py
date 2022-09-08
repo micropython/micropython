3a34e478daca8d68459a1d589f542ca4088e1289
Jasper Di Francesco <jasper.difrancesco@gmail.com>
2022-09-08 07:03:34 +0000

    Add route to use request

--- ports/unix/websrv.py
+++ ports/unix/websrv.py
@@ -1,13 +1,32 @@
 from phew import server
 
-@server.route("/random", methods=["GET"])
-def random_number(request):
-  import random
-  bits = int(request.query.get("bits", 0))
-  return str(random.getrandbits(bits))
+@server.route("/simple", methods=["GET"])
+def simple_request(request):
+    results = "Some result from API"
+    otherRes = ["fruit", "apple", "orange"]
+    modded = map(lambda a : f"<ul>{a}</ul>", otherRes)
+    otherResStr = ' '.join(modded)
+    ret = f"""
+    <html>
+        <head></head>
+        <body>
+            <p>This is something {results} in a web page</p> 
+            <ul>
+                {otherResStr}
+            </ul>
+        </body>
+    </html>
+    """
+    return ret
+
+@server.route("/get_simple", methods=["GET"])
+def simple_request(request):
+    import http_lib
+    res = http_lib.http_get('https://catfact.ninja/fact')
+    return res
 
 @server.catchall()
 def catchall(request):
-  return "Not found", 404
+    return "Not found", 404
 
 server.run()
