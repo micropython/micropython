1bdf9fca40e9dafbaf33a07009379459e9207fb3
RetiredWizard <github@retiredwizard.com>
2022-09-10 21:40:31 -0400

    Define CIRCUITPY_CONSOLE_UART_TX/RX

--- ports/broadcom/boards/raspberrypi_zero2w/mpconfigboard.h
+++ ports/broadcom/boards/raspberrypi_zero2w/mpconfigboard.h
@@ -2,3 +2,6 @@
 
 #define DEFAULT_I2C_BUS_SCL         (&pin_GPIO3)
 #define DEFAULT_I2C_BUS_SDA         (&pin_GPIO2)
+
+#define CIRCUITPY_CONSOLE_UART_TX   (&pin_GPIO14)
+#define CIRCUITPY_CONSOLE_UART_RX   (&pin_GPIO15)
