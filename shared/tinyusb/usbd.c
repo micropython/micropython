9d7800db1da6b3c7efefcd46d7bd7a208277d510
Blake Felt <blake.w.felt@gmail.com>
2022-09-18 15:07:16 -0600

    shared/tinyusb: Add a few USB device functions.

--- shared/tinyusb/usbd.c
+++ shared/tinyusb/usbd.c
@@ -3,13 +3,37 @@
 #include "usbd.h"
 #include "tusb_config.h"
 
+// tusb.h is not available when running the string preprocessor
+#ifndef NO_QSTR
+#include "tusb.h"
+#endif
+
 void usbd_reset_all(void) {
     usbd_reset_descriptor();
     usbd_reset_hid();
 }
 
+STATIC mp_obj_t usbd_ready(void) {
+    return mp_obj_new_bool(tud_ready());
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(usbd_ready_obj, usbd_ready);
+
+STATIC mp_obj_t usbd_connected(void) {
+    return mp_obj_new_bool(tud_connected());
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(usbd_connected_obj, usbd_connected);
+
+STATIC mp_obj_t usbd_remote_wakeup(void) {
+    return mp_obj_new_bool(tud_remote_wakeup());
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(usbd_remote_wakeup_obj, usbd_remote_wakeup);
+
 STATIC const mp_rom_map_elem_t usb_device_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_usb_device) },
+
+    { MP_ROM_QSTR(MP_QSTR_ready), (mp_obj_t)&usbd_ready_obj},
+    { MP_ROM_QSTR(MP_QSTR_connected), (mp_obj_t)&usbd_connected_obj},
+    { MP_ROM_QSTR(MP_QSTR_remote_wakeup), (mp_obj_t)&usbd_remote_wakeup_obj},
 
     { MP_ROM_QSTR(MP_QSTR_Descriptor), MP_ROM_PTR(&usbd_descriptor_type) },
     #if MICROPY_HW_USB_HID
