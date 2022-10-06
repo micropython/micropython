4f753251db844e61bbf621f6d9288eaae33b667c
microDev <70126934+microDev1@users.noreply.github.com>
2022-10-06 16:05:33 +0530

    cleanup `mpconfigboard.mk` of espressif boards
    - move `INTERNAL_FLASH_FILESYSTEM` to `mpconfigport.mk`
    - move `LONGINT_IMPL` to `mpconfigport.mk`
    - move `CFG_TUD_TASK_QUEUE_SZ` to `Makefile`

--- ports/espressif/boards/targett_module_clip_wrover/mpconfigboard.mk
+++ ports/espressif/boards/targett_module_clip_wrover/mpconfigboard.mk
@@ -5,13 +5,6 @@ USB_MANUFACTURER = "Targett"
 
 IDF_TARGET = esp32s2
 
-INTERNAL_FLASH_FILESYSTEM = 1
-LONGINT_IMPL = MPZ
-
-# The default queue depth of 16 overflows on release builds,
-# so increase it to 32.
-CFLAGS += -DCFG_TUD_TASK_QUEUE_SZ=32
-
 CIRCUITPY_ESP_FLASH_MODE = dio
 CIRCUITPY_ESP_FLASH_FREQ = 40m
 CIRCUITPY_ESP_FLASH_SIZE = 4MB
