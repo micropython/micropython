bfa6f34404374bb7c7d892ca6cb32175e8dd19f6
iabdalkader <i.abdalkader@gmail.com>
2022-10-06 20:25:34 +0200

    renesas-ra: Rename machine SPI type consistently across ports.
    This renames:
    - machine_hard_spi_type -> machine_spi_type

--- ports/renesas-ra/modmachine.c
+++ ports/renesas-ra/modmachine.c
@@ -286,7 +286,7 @@ STATIC const mp_rom_map_elem_t machine_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_SoftI2C),             MP_ROM_PTR(&mp_machine_soft_i2c_type) },
     #endif
     #if MICROPY_PY_MACHINE_SPI
-    { MP_ROM_QSTR(MP_QSTR_SPI),                 MP_ROM_PTR(&machine_hard_spi_type) },
+    { MP_ROM_QSTR(MP_QSTR_SPI),                 MP_ROM_PTR(&machine_spi_type) },
     { MP_ROM_QSTR(MP_QSTR_SoftSPI),             MP_ROM_PTR(&mp_machine_soft_spi_type) },
     #endif
     { MP_ROM_QSTR(MP_QSTR_UART),                MP_ROM_PTR(&machine_uart_type) },
