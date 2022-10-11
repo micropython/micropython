e19abef57ea2f440a96f64239a004a0544d20a39
Dan Halbert <halbert@halwitz.org>
2022-10-11 13:09:24 -0400

    forgot to add these!

--- ports/raspberrypi/boards/vcc_gnd_yd_rp2040/mpconfigboard.mk
+++ ports/raspberrypi/boards/vcc_gnd_yd_rp2040/mpconfigboard.mk
@@ -1,15 +1,11 @@
 USB_VID = 0x2E8A
-USB_PID = 0x000B
+USB_PID = 0x102E
 USB_PRODUCT = "YD-RP2040"
 USB_MANUFACTURER = "VCC-GND Studio"
 
 CHIP_VARIANT = RP2040
 CHIP_FAMILY = rp2
 
-EXTERNAL_FLASH_DEVICES = "W25Q16JVxQ"
+EXTERNAL_FLASH_DEVICES = "W25Q16JVxQ,W25Q32JVxQ,W25Q128JVxQ"
 
 CIRCUITPY__EVE = 1
-
-# Include these Python libraries in firmware
-FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_NeoPixel
-FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_SimpleIO
