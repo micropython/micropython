220a5bb06dd55ebc7dc48417da06eb0c9736ec11
Patrick <patrick@thinktransit.com.au>
2022-10-23 00:33:48 +1100

    Added custom boards

---
+++ ports/esp32/boards/INSIGHT_PM/manifest.py
@@ -0,0 +1,7 @@
+include("$(PORT_DIR)/boards/manifest.py")
+freeze("modules")
+require("datetime")
+require("functools")
+require("urequests")
+require("webrepl")
+require("aiorepl")
---
+++ ports/esp32/boards/INSIGHT_V1/board.md
@@ -0,0 +1,2 @@
+The following files are daily firmware for INSIGHT V1. This firmware is
+compiled using ESP-IDF v4.3 or later.
