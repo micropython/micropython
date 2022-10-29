9ca057385abf903be474e43c39e33eec166af27d
Jozsef Kiraly <fonix232@gmail.com>
2022-10-29 18:52:43 +0100

    feat: Add support for Waveshare RP2040-LCD-1.28

---
+++ ports/raspberrypi/boards/waveshare_rp2040_lcd_1_28/mpconfigboard.mk
@@ -0,0 +1,17 @@
+USB_VID = 0x2E8A
+USB_PID = 0x000A
+USB_PRODUCT = "Waveshare RP2040-LCD-1.28"
+USB_MANUFACTURER = "Waveshare Electronics"
+
+CHIP_VARIANT = RP2040
+CHIP_FAMILY = rp2
+
+EXTERNAL_FLASH_DEVICES = "W25Q32JVxQ"
+
+CIRCUITPY__EVE = 1
+
+# TODO: Add custom QMI8658 driver
+# FROZEN_MPY_DIRS += $(TOP)/frozen/circuitpython_qmi8658
+
+# TODO: Add custom GC9A01 driver
+# FROZEN_MPY_DIRS += $(TOP)/frozen/circuitpython_gc9a01
