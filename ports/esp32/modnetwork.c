b1053778ec52a2f2ca6c0b82e8ca1faac71255c8
sramrajkar <55571093+sramrajkar@users.noreply.github.com>
2022-10-31 19:32:05 -0400

    Fix indentation

--- ports/esp32/modnetwork.c
+++ ports/esp32/modnetwork.c
@@ -139,7 +139,7 @@ STATIC mp_obj_t esp_initialize() {
         esp_netif_init();
         ESP_LOGD("modnetwork", "Initializing Event Loop");
         esp_exceptions(esp_event_loop_create_default());
-		esp_exceptions(esp_event_handler_register(ESP_EVENT_ANY_BASE, ESP_EVENT_ANY_ID, event_handler, NULL));
+        esp_exceptions(esp_event_handler_register(ESP_EVENT_ANY_BASE, ESP_EVENT_ANY_ID, event_handler, NULL));
         ESP_LOGD("modnetwork", "esp_event_loop_init done");
         initialized = 1;
     }
