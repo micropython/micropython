febc13ca4e7b6bbacf0f49144a8db3bd4bc9ea29
iabdalkader <i.abdalkader@gmail.com>
2022-11-04 18:42:50 +0100

    rp2/boards/ARDUINO_NANO_RP2040: Update board config files.
    * Enable MD5 hash to verify Nina WiFi firmware.
    * Add ntptime, urequests and espflash to manifest.

--- ports/rp2/boards/ARDUINO_NANO_RP2040_CONNECT/manifest.py
+++ ports/rp2/boards/ARDUINO_NANO_RP2040_CONNECT/manifest.py
@@ -3,9 +3,14 @@ include("$(PORT_DIR)/boards/manifest.py")
 # Networking
 require("webrepl")
 require("urequests")
+require("ntptime")
 
 # Drivers
 require("lsm6dsox")
+require("espflash")
+
+# Utils
+require("logging")
 
 # Bluetooth
 require("aioble", client=True, central=True, l2cap=True, security=True)
--- ports/rp2/boards/ARDUINO_NANO_RP2040_CONNECT/mpconfigboard.h
+++ ports/rp2/boards/ARDUINO_NANO_RP2040_CONNECT/mpconfigboard.h
@@ -7,6 +7,9 @@
 // Enable networking.
 #define MICROPY_PY_NETWORK              (1)
 
+// Enable MD5 hash.
+#define MICROPY_PY_UHASHLIB_MD5         (1)
+
 // Disable internal error numbers.
 #define MICROPY_USE_INTERNAL_ERRNO      (0)
 
