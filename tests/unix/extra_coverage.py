cdec97b9c4d5acbe87261955ff38e9eb7852d2bf
Laurens Valk <laurens@pybricks.com>
2022-11-21 15:13:24 +0100

    tests/misc: Add test for cexample module.
    This also moves the existing test for cexample.add_ints
    originally done in extra_coverage.
    
    Signed-off-by: Laurens Valk <laurens@pybricks.com>

--- tests/unix/extra_coverage.py
+++ tests/unix/extra_coverage.py
@@ -49,11 +49,6 @@ print(buf.write(bytearray(16)))
 # function defined in C++ code
 print("cpp", extra_cpp_coverage())
 
-# test user C module
-import cexample
-
-print(cexample.add_ints(3, 2))
-
 # test user C module mixed with C++ code
 import cppexample
 
