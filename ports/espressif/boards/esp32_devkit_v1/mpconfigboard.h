26fbb25653f78335ff5b4c70194b08f98492fb0c
m1cha1s <msulej04@gmail.com>
2022-11-25 13:55:06 +0100

    Fixed formatting

--- ports/espressif/boards/esp32_devkit_v1/mpconfigboard.h
+++ ports/espressif/boards/esp32_devkit_v1/mpconfigboard.h
@@ -29,31 +29,16 @@
 #define MICROPY_HW_BOARD_NAME "ESP32 Devkit V1"
 #define MICROPY_HW_MCU_NAME "ESP32"
 
-#define MICROPY_HW_LED_STATUS (&pin_GPIO2)
+#define MICROPY_HW_LED_STATUS       (&pin_GPIO2)
 
-#define CIRCUITPY_BOARD_I2C (1)
-#define CIRCUITPY_BOARD_I2C_PIN                    \
-    {                                              \
-        {                                          \
-            .scl = &pin_GPIO22, .sda = &pin_GPIO21 \
-        }                                          \
-    }
+#define CIRCUITPY_BOARD_I2C         (1)
+#define CIRCUITPY_BOARD_I2C_PIN     {{.scl = &pin_GPIO22, .sda = &pin_GPIO21}}
 
-#define CIRCUITPY_BOARD_SPI (1)
-#define CIRCUITPY_BOARD_SPI_PIN                                            \
-    {                                                                      \
-        {                                                                  \
-            .clock = &pin_GPIO18, .mosi = &pin_GPIO23, .miso = &pin_GPIO19 \
-        }                                                                  \
-    }
+#define CIRCUITPY_BOARD_SPI         (1)
+#define CIRCUITPY_BOARD_SPI_PIN     {{.clock = &pin_GPIO18, .mosi = &pin_GPIO23, .miso = &pin_GPIO19}}
 
-#define CIRCUITPY_BOARD_UART (1)
-#define CIRCUITPY_BOARD_UART_PIN                 \
-    {                                            \
-        {                                        \
-            .tx = &pin_GPIO17, .rx = &pin_GPIO16 \
-        }                                        \
-    }
+#define CIRCUITPY_BOARD_UART        (1)
+#define CIRCUITPY_BOARD_UART_PIN    {{.tx = &pin_GPIO17, .rx = &pin_GPIO16}}
 
 // UART pins attached to the USB-serial converter chip
 #define CIRCUITPY_CONSOLE_UART_TX (&pin_GPIO1)
