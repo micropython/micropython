9bec52a2f8371847e1d75fee84bf37a9b2876cd4
Jim Mussared <jim.mussared@gmail.com>
2022-12-14 12:17:42 +1100

    esp32/main: Don't call usocket_events_deinit if unavailable.
    usocket_events_deinit will only be available if MICROPY_PY_USOCKET_EVENTS
    is enabled (which is only enabled when webrepl is enabled).
    
    Signed-off-by: Jim Mussared <jim.mussared@gmail.com>

--- ports/esp32/main.c
+++ ports/esp32/main.c
@@ -215,7 +215,9 @@ soft_reset_exit:
     // TODO: machine_rmt_deinit_all();
     machine_pins_deinit();
     machine_deinit();
+    #if MICROPY_PY_USOCKET_EVENTS
     usocket_events_deinit();
+    #endif
 
     mp_deinit();
     fflush(stdout);
