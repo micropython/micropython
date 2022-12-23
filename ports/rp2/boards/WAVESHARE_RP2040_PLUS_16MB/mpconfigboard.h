b506131f807afa4e42bdea5ab9e5c784ce271901
RetiredWizard <github@retiredwizard.com>
2022-12-23 14:16:34 -0500

    WAVESHARE_RP2040_PLUS_16MB/mpconfigboard.h: Fix boardname.
    Update the MICROPY_HW_BOARD_NAME parameter with the correct name
    indicating 16MB of flash rather than 4MB.
    
    Signed-off-by: RetiredWizard <github@retiredwizard.com>

--- ports/rp2/boards/WAVESHARE_RP2040_PLUS_16MB/mpconfigboard.h
+++ ports/rp2/boards/WAVESHARE_RP2040_PLUS_16MB/mpconfigboard.h
@@ -1,4 +1,4 @@
-#define MICROPY_HW_BOARD_NAME "Waveshare RP2040-Plus 4MB"
+#define MICROPY_HW_BOARD_NAME "Waveshare RP2040-Plus 16MB"
 
 // Allow 1MB for the firmware image itself, allocate the remainder to the filesystem
 #define MICROPY_HW_FLASH_STORAGE_BYTES (PICO_FLASH_SIZE_BYTES - (640 * 1024))
