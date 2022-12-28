83bbfd1815ed65031ad9b6eb805af3f2640876fc
Jeff Epler <jepler@gmail.com>
2022-12-28 12:36:58 -0600

    Allow the tests to directly call the non-heap using _int and _str variants
    .. of getenv. These can have their own special bugs.

--- tests/circuitpython/getenv.py
+++ tests/circuitpython/getenv.py
@@ -79,6 +79,9 @@ content_good = content_good.replace(b"\n", b"\r\n")
 for i in range(13):
     run_test(f"key{i}", content_good)
 
+run_test(f"K", b"K = 7\r\n")
+print(uos.getenv_int("K"))
+
 # Test value without trailing newline
 run_test(f"noeol", b"noeol=3")
 
