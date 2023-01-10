304eb847f945afed010b99984601692ce0aaa504
Wind-stormger <storm-wind@foxmail.com>
2023-01-10 18:18:08 +0800

    esp32/modules: Format the filesystem as FAT in UF2 bootloader.

--- ports/esp32/modules/inisetup.py
+++ ports/esp32/modules/inisetup.py
@@ -1,6 +1,20 @@
 import uos
 from flashbdev import bdev
 
+str_corrupted = """
+The filesystem appears to be corrupted. If you had important data there, you
+may want to make a flash snapshot to try to recover it. Otherwise, perform
+factory reprogramming of MicroPython firmware (completely erase flash, followed
+by firmware programming).
+"""
+str_boot_py = """
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+#import webrepl
+#webrepl.start()
+"""
+
 
 def check_bootsec():
     buf = bytearray(bdev.ioctl(5, 0))  # 5 is SEC_SIZE
@@ -19,31 +33,20 @@ def fs_corrupted():
     import time
 
     while 1:
-        print(
-            """\
-The filesystem appears to be corrupted. If you had important data there, you
-may want to make a flash snapshot to try to recover it. Otherwise, perform
-factory reprogramming of MicroPython firmware (completely erase flash, followed
-by firmware programming).
-"""
-        )
+        print(str_corrupted)
         time.sleep(3)
 
 
 def setup():
     check_bootsec()
     print("Performing initial setup")
-    uos.VfsLfs2.mkfs(bdev)
-    vfs = uos.VfsLfs2(bdev)
+    try:
+        uos.VfsLfs2.mkfs(bdev)
+        vfs = uos.VfsLfs2(bdev)
+    except OSError:
+        uos.VfsFat.mkfs(bdev)
+        vfs = uos.VfsFat(bdev)
     uos.mount(vfs, "/")
     with open("boot.py", "w") as f:
-        f.write(
-            """\
-# This file is executed on every boot (including wake-boot from deepsleep)
-#import esp
-#esp.osdebug(None)
-#import webrepl
-#webrepl.start()
-"""
-        )
+        f.write(str_boot_py)
     return vfs
