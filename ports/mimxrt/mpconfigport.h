1583c1f6701a2545f0de042e886ee9afc26dfb74
robert-hh <robert@hammelrath.com>
2023-01-12 22:02:26 +0100

    mimxrt: Set MICROPY_GC_STACK_ENTRY_TYPE to uint32_t for large RAM.
    It was uint16_t. The change sets it to uint32_t for devices with SDRAM.
    
    Fixes issue #10366.

--- ports/mimxrt/mpconfigport.h
+++ ports/mimxrt/mpconfigport.h
@@ -37,7 +37,11 @@ uint32_t trng_random_u32(void);
 #define MICROPY_CONFIG_ROM_LEVEL (MICROPY_CONFIG_ROM_LEVEL_FULL_FEATURES)
 
 // Memory allocation policies
+#if MICROPY_HW_SDRAM_AVAIL
+#define MICROPY_GC_STACK_ENTRY_TYPE         uint32_t
+#else
 #define MICROPY_GC_STACK_ENTRY_TYPE         uint16_t
+#endif
 #define MICROPY_ALLOC_PARSE_CHUNK_INIT      (32)
 #define MICROPY_ALLOC_PATH_MAX              (256)
 
