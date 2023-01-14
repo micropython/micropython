6bc0274aec15afd8f73c9616a5ae621846eadf09
MicroDev <70126934+MicroDev1@users.noreply.github.com>
2023-01-14 10:58:04 +0530

    cache common submodules and share cp version

--- tools/ci_fetch_deps.py
+++ tools/ci_fetch_deps.py
@@ -4,16 +4,12 @@ import subprocess
 import sys
 import time
 
-# target will be a board, "test", "docs", "mpy-cross-mac", or "windows"
-
-target = sys.argv[1]
-ref = sys.argv[2]
-
-print(target, ref)
+# Target will be a board, "test", "docs", "mpy-cross-mac", or "windows"
+TARGET = sys.argv[1]
 
 # Submodules needed by port builds outside of their ports directory.
 # Should we try and detect these?
-port_deps = {
+PORT_DEPS = {
     "atmel-samd": [
         "extmod/ulab/",
         "lib/adafruit_floppy/",
@@ -58,73 +54,58 @@ def run(title, command, check=True):
     try:
         subprocess.run(shlex.split(command), stderr=subprocess.STDOUT, check=check)
     finally:
-        print("Duration:", time.monotonic() - start, flush=True)
         print("::endgroup::", flush=True)
+        print("Duration:", time.monotonic() - start, flush=True)
 
 
-run(
-    "Fetch back to the start of 2021 to get tag history",
-    'git fetch --tags --recurse-submodules=no --shallow-since="2021-07-01" https://github.com/adafruit/circuitpython HEAD',
-)
-run(
-    "Fetch back to the start of 2021 to get commit history",
-    f'git fetch --recurse-submodules=no --shallow-since="2021-07-01" origin {ref}',
-)
-# See https://stackoverflow.com/questions/63878612/git-fatal-error-in-object-unshallow-sha-1#comment118418373_63879454
-run('Fix for bug "fatal: error in object: unshallow"', "git repack -d")
-run("Init submodules", "git submodule init")
-run("Submodule status", "git submodule status")
+def main():
+    submodules = []
+    print("Target:", TARGET)
 
-submodules = []
+    if TARGET == "scheduler":
+        submodules = ["extmod/ulab", "lib/", "tools/", "frozen/"]
+    elif TARGET == "mpy-cross-tests":
+        submodules = ["extmod/ulab", "lib/", "tools/"]
+    elif TARGET == "build-doc":
+        # used in .readthedocs.yml to generate RTD
+        submodules = ["extmod/ulab", "frozen/"]
+    elif TARGET == "mpy-cross-mac":
+        submodules = ["tools/"]  # for huffman
+    elif TARGET == "windows":
+        # This builds one board from a number of ports so fill out a bunch of submodules
+        submodules = ["extmod/ulab", "lib/", "tools/", "ports/", "data/nvm.toml"]
+    elif TARGET == "website":
+        submodules = ["tools/adabot/", "frozen/"]
+    else:
+        p = list(pathlib.Path(".").glob(f"ports/*/boards/{TARGET}/mpconfigboard.mk"))
+        if not p:
+            raise RuntimeError(f"Unsupported target: {TARGET}")
 
-if target == "test":
-    submodules = ["extmod/", "lib/", "tools/", "extmod/ulab", "lib/berkeley-db-1.xx"]
-elif target == "docs":
-    # used in .readthedocs.yml to generate RTD
-    submodules = ["extmod/ulab/", "frozen/"]
-elif target == "mpy-cross-mac":
-    submodules = ["tools/"]  # for huffman
-elif target == "windows":
-    # This builds one board from a number of ports so fill out a bunch of submodules
-    submodules = ["extmod/", "lib/", "tools/", "ports/", "data/nvm.toml/"]
-elif target == "website":
-    submodules = ["tools/adabot/", "frozen/"]
-else:
-    p = list(pathlib.Path(".").glob(f"ports/*/boards/{target}/mpconfigboard.mk"))
-    if not p:
-        raise RuntimeError(f"Unsupported target: {target}")
+        config = p[0]
+        # Add the ports folder to init submodules
+        port_folder = config.parents[2]
+        port = port_folder.name
+        submodules.append(str(port_folder))
+        submodules.append("tools/")  # for huffman
+        submodules.extend(PORT_DEPS[port])
+        with config.open() as f:
+            for line in f.readlines():
+                prefix = "FROZEN_MPY_DIRS += $(TOP)/"
+                if line.startswith(prefix):
+                    lib_folder = line.strip()[len(prefix) :]
+                    # Drop everything after the second folder because the frozen
+                    # folder may be inside the submodule.
+                    if lib_folder.count("/") > 1:
+                        lib_folder = lib_folder.split("/", maxsplit=2)
+                        lib_folder = "/".join(lib_folder[:2])
+                    submodules.append(lib_folder)
 
-    config = p[0]
-    # Add the ports folder to init submodules
-    port_folder = config.parents[2]
-    port = port_folder.name
-    submodules.append(str(port_folder))
-    submodules.append("tools/")  # for huffman
-    submodules.extend(port_deps[port])
-    with config.open() as f:
-        for line in f.readlines():
-            prefix = "FROZEN_MPY_DIRS += $(TOP)/"
-            if line.startswith(prefix):
-                lib_folder = line.strip()[len(prefix) :]
-                # Drop everything after the second folder because the frozen
-                # folder may be inside the submodule.
-                if lib_folder.count("/") > 1:
-                    lib_folder = lib_folder.split("/", maxsplit=2)
-                    lib_folder = "/".join(lib_folder[:2])
-                submodules.append(lib_folder)
+    print("Submodule paths:", submodules)
 
-print(submodules)
-if submodules:
-    submodules = " ".join(submodules)
-    # This line will fail because the submodule's need different commits than the tip of the branch. We
-    # fix it later.
-    run(
-        "Init the submodules we'll need",
-        f"git submodule update --init -N --depth 1 {submodules}",
-        check=False,
-    )
+    if submodules:
+        submodules = " ".join(submodules)
+        run("Init the required submodules", f"git submodule update --init --depth=1 {submodules}")
 
-    run(
-        "Fetch the submodule sha",
-        "git submodule foreach 'git fetch --tags --depth 1 origin $sha1 && git checkout -q $sha1'",
-    )
+
+if __name__ == "__main__":
+    main()
