315eb949da3bb3fe476050e164f03b613ca64afa
robert-hh <robert@hammelrath.com>
2023-01-27 16:02:05 +0100

    samd: Rearrange the MCU-specific loader files.
    Such that they are easier to adapt.  The maximum code size is set by:
    
        MICROPY_HW_CODESIZE=xxxK
    
    in mpconfigmcu.mk for the MCU family as default or in mpconfigboard.mk for
    a specific board.  Setting the maximum code size allows the loader to error
    out if the code gets larger than the space dedicated for it.
    
    Signed-off-by: robert-hh <robert@hammelrath.com>

--- ports/samd/mcu/samd21/mpconfigmcu.mk
+++ ports/samd/mcu/samd21/mpconfigmcu.mk
@@ -2,6 +2,8 @@ CFLAGS_MCU += -mtune=cortex-m0plus -mcpu=cortex-m0plus -msoft-float
 
 MPY_CROSS_MCU_ARCH = armv6m
 
+MICROPY_HW_CODESIZE ?= 184K
+
 SRC_S += shared/runtime/gchelper_thumb1.s
 
 LIBM_SRC_C +=  $(addprefix lib/libm/,\
