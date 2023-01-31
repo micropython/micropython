f3b1ceeb81eb2c15ea7f8713c95d23025e55f5fb
brainboardz <brainboardz@gmail.com>
2023-01-30 20:45:57 -0500

    Seventh Commit

--- ports/espressif/boards/brainboardz_neuron/pins.c
+++ ports/espressif/boards/brainboardz_neuron/pins.c
@@ -49,6 +49,5 @@ STATIC const mp_rom_map_elem_t board_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_SD_CS), MP_ROM_PTR(&pin_GPIO16) },
     { MP_ROM_QSTR(MP_QSTR_LED), MP_ROM_PTR(&pin_GPIO21) },
     { MP_ROM_QSTR(MP_QSTR_BOOT), MP_ROM_PTR(&pin_GPIO0) },
-    
 };
 MP_DEFINE_CONST_DICT(board_module_globals, board_module_globals_table);
