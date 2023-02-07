9f3cbaa8081b8b3e4a0e4ddca430eced54738f43
Mark Burton <mark.burton@pinpoint-software.com>
2023-02-07 16:11:35 +0000

    examples/rp2: Fix python code formatting.
    Signed-off-by: mark <mark.burton@pinpoint-software.com>

--- examples/rp2/dma_write_from_irq.py
+++ examples/rp2/dma_write_from_irq.py
@@ -3,6 +3,7 @@ import time
 from rp2 import DMA, Timer
 from machine import Pin, PWM
 
+
 async def led():
     print("DMA write_from demo")
     led = Pin(16, Pin.OUT)
@@ -11,32 +12,32 @@ async def led():
     pwm1.duty_u16(0)
 
     # pwm 0A attached to GPIO 16
-    dest = 0x4005000e 
+    dest = 0x4005000E
     dma = DMA()
     dma.claim()
     t = Timer()
 
     # slow the timer to it's slowest rate
     X = 0x0001
-    Y = 0xffff
+    Y = 0xFFFF
     t.claim()
     t.set(X, Y)
 
     print(f"Starting\nDMA:{dma}\nTimer:{t}")
-    
+
     dreq = t.dreq()
-    
+
     data_size = dma.DMA_SIZE_16
     buffer_size = 32768
     from_buffer = bytearray(buffer_size)
-    
+
     # create a 16 bit fade
     for i in range(0, buffer_size, 2):
-        from_buffer[i]= i & 0xff
-        from_buffer[i+1]= (i>>8) & 0xff
-    
+        from_buffer[i]= i & 0xFF
+        from_buffer[i+1]= (i>>8) & 0xFF
+
     start = time.ticks_ms()
-    
+
     e = uasyncio.ThreadSafeFlag()
 
     def _callback(dma_status):
@@ -44,28 +45,32 @@ async def led():
         e.set()
 
     dma.write_from(from_buffer=from_buffer,
-                   to_address=dest,
-                   transfer_count=buffer_size>>data_size,
-                   dreq=dreq,
-                   data_size=data_size,
-                   handler=_callback)
-    
+        to_address=dest,
+        transfer_count=buffer_size>>data_size,
+        dreq=dreq,
+        data_size=data_size,
+        handler=_callback
+    )
+
     await e.wait()
-    
+
     end = time.ticks_ms()
-    
+
     t.unclaim()
     dma.unclaim()
     print(f"Write from fade took {end-start}mS")
     print("Done")
     pwm1.duty_u16(0)
 
+
 async def async_led():
     t = uasyncio.create_task(led())
     await t
 
+
 def main():
     uasyncio.run(async_led())
+
 
 if __name__ == '__main__':
     main()
