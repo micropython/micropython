c234a269540b7c5779d4b879b9edb0fdb11202d1
Damien George <damien@micropython.org>
2023-03-10 12:21:04 +1100

    mimxrt/modutime: Use extmod version of time module.
    No API or functional change.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/mimxrt/mpconfigport.h
+++ ports/mimxrt/mpconfigport.h
@@ -70,7 +70,9 @@ uint32_t trng_random_u32(void);
 // Extended modules
 #define MICROPY_EPOCH_IS_1970               (1)
 #define MICROPY_PY_USSL_FINALISER           (MICROPY_PY_USSL)
-#define MICROPY_PY_UTIME_MP_HAL             (1)
+#define MICROPY_PY_UTIME_GMTIME_LOCALTIME_MKTIME (1)
+#define MICROPY_PY_UTIME_TIME_TIME_NS       (1)
+#define MICROPY_PY_UTIME_INCLUDEFILE        "ports/mimxrt/modutime.c"
 #define MICROPY_PY_UOS_INCLUDEFILE          "ports/mimxrt/moduos.c"
 #define MICROPY_PY_OS_DUPTERM               (3)
 #define MICROPY_PY_UOS_DUPTERM_NOTIFY       (1)
