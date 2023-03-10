127fd170c860b0d04216bc14a41e3adce77c9e31
Damien George <damien@micropython.org>
2023-03-10 13:43:38 +1100

    unix/modutime: Use extmod version of time module.
    No API or functional change.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/unix/variants/mpconfigvariant_common.h
+++ ports/unix/variants/mpconfigvariant_common.h
@@ -96,7 +96,9 @@
 
 // Enable the unix-specific "time" module.
 #define MICROPY_PY_UTIME               (1)
-#define MICROPY_PY_UTIME_MP_HAL        (1)
+#define MICROPY_PY_UTIME_TIME_TIME_NS  (1)
+#define MICROPY_PY_UTIME_CUSTOM_SLEEP  (1)
+#define MICROPY_PY_UTIME_INCLUDEFILE   "ports/unix/modutime.c"
 
 // Enable the utimeq module used by the previous (v2) version of uasyncio.
 #define MICROPY_PY_UTIMEQ              (1)
