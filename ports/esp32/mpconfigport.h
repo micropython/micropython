5be20b67df5d83d2a9b2301e71a769d3932612f8
Damien George <damien@micropython.org>
2023-03-10 12:20:16 +1100

    esp32/modutime: Use extmod version of time module.
    No API or functional change.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/esp32/mpconfigport.h
+++ ports/esp32/mpconfigport.h
@@ -62,7 +62,9 @@
 #define MICROPY_PY_ALL_INPLACE_SPECIAL_METHODS (1)
 #define MICROPY_PY_BUILTINS_HELP_TEXT       esp32_help_text
 #define MICROPY_PY_IO_BUFFEREDWRITER        (1)
-#define MICROPY_PY_UTIME_MP_HAL             (1)
+#define MICROPY_PY_UTIME_GMTIME_LOCALTIME_MKTIME (1)
+#define MICROPY_PY_UTIME_TIME_TIME_NS       (1)
+#define MICROPY_PY_UTIME_INCLUDEFILE        "ports/esp32/modutime.c"
 #define MICROPY_PY_THREAD                   (1)
 #define MICROPY_PY_THREAD_GIL               (1)
 #define MICROPY_PY_THREAD_GIL_VM_DIVISOR    (32)
