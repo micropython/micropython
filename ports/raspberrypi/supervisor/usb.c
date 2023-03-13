b432cf62019d40b103e1cba6fbfecb5813cd61a7
hathach <thach@tinyusb.org>
2023-03-13 21:49:01 +0700

    fix rp2040 with new shared irq usb handler

--- ports/raspberrypi/supervisor/usb.c
+++ ports/raspberrypi/supervisor/usb.c
@@ -39,15 +39,8 @@ STATIC void _usb_irq_wrapper(void) {
 }
 
 void post_usb_init(void) {
-    irq_set_enabled(USBCTRL_IRQ, false);
-
-    irq_handler_t usb_handler = irq_get_exclusive_handler(USBCTRL_IRQ);
-    if (usb_handler) {
-        irq_remove_handler(USBCTRL_IRQ, usb_handler);
-    }
-    irq_set_exclusive_handler(USBCTRL_IRQ, _usb_irq_wrapper);
-
-    irq_set_enabled(USBCTRL_IRQ, true);
+    irq_add_shared_handler(USBCTRL_IRQ, _usb_irq_wrapper,
+        PICO_SHARED_IRQ_HANDLER_LOWEST_ORDER_PRIORITY);
 
     // There is a small window where the USB interrupt may be handled by the
     // pico-sdk instead of CircuitPython. If that is the case, then we'll have
