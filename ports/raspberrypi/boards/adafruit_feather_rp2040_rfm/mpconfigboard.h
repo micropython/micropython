2a4383e633766e01c4c07149161def59c11a576e
Kattni Rembor <kattni@adafruit.com>
2023-03-28 15:23:19 -0400

    Add Feather RP2040 RFM, update DVI.

--- ports/raspberrypi/boards/adafruit_feather_rp2040_rfm/mpconfigboard.h
+++ ports/raspberrypi/boards/adafruit_feather_rp2040_rfm/mpconfigboard.h
@@ -1,4 +1,4 @@
-#define MICROPY_HW_BOARD_NAME "Adafruit Feather RP2040 DVI"
+#define MICROPY_HW_BOARD_NAME "Adafruit Feather RP2040 RFM"
 #define MICROPY_HW_MCU_NAME "rp2040"
 
 #define MICROPY_HW_NEOPIXEL (&pin_GPIO4)
