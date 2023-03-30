3a7ad642672a61d3adb1510bf26913376ff837c8
UnexpectedMaker <seon@unexpectedmaker.com>
2023-03-30 13:27:17 +1100

    esp32/boards: Add some missing board configs for two UM boards.

--- ports/esp32/boards/UM_TINYS2/mpconfigboard.cmake
+++ ports/esp32/boards/UM_TINYS2/mpconfigboard.cmake
@@ -3,4 +3,7 @@ set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.base
     boards/sdkconfig.spiram_sx
     boards/sdkconfig.usb
+    boards/UM_TINYS2/sdkconfig.board
 )
+
+set(MICROPY_FROZEN_MANIFEST ${MICROPY_BOARD_DIR}/manifest.py)
