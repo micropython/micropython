fbb9a1b411442d018a402310c4842f75ca741bdc
Filip Budiša <filip@circuitmess.com>
2023-03-30 14:47:48 +0200

    esp32/boards: Add Combined board variant

---
+++ ports/esp32/boards/CM_-Combined/manifest.py
@@ -0,0 +1,5 @@
+include("$(PORT_DIR)/boards/manifest.py")
+
+package("Chatter2", base_path="$(PORT_DIR)/libraries")
+package("BatCtrl", base_path="$(PORT_DIR)/libraries")
+package("Batmobile", base_path="$(PORT_DIR)/libraries")
---
+++ ports/esp32/boards/CM_-Combined/mpconfigboard.cmake
@@ -0,0 +1,6 @@
+set(SDKCONFIG_DEFAULTS
+    boards/sdkconfig.base
+    boards/sdkconfig.ble
+)
+
+set(MICROPY_FROZEN_MANIFEST ${MICROPY_BOARD_DIR}/manifest.py)
