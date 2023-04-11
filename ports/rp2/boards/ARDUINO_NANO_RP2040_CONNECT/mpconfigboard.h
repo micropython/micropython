5ec881dd11ff20f8627be9fb0dbea6afaf2b0cd5
iabdalkader <i.abdalkader@gmail.com>
2023-04-11 20:59:56 +0200

    rp2/boards/ARDUINO_NANO_RP2040_CONNECT: Use standard HCI UART baudrate.
    Signed-off-by: iabdalkader <i.abdalkader@gmail.com>

--- ports/rp2/boards/ARDUINO_NANO_RP2040_CONNECT/mpconfigboard.h
+++ ports/rp2/boards/ARDUINO_NANO_RP2040_CONNECT/mpconfigboard.h
@@ -39,7 +39,7 @@
 
 // Bluetooth config.
 #define MICROPY_HW_BLE_UART_ID          (1)
-#define MICROPY_HW_BLE_UART_BAUDRATE    (119600)
+#define MICROPY_HW_BLE_UART_BAUDRATE    (115200)
 
 // WiFi/NINA-W10 config.
 #define MICROPY_HW_WIFI_SPI_ID          (1)
