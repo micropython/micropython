19c9dd1f4c0fdb9a01dc55fa1cab677185c60850
pallasmanul <pallasmanul@outlook.com>
2023-04-18 21:57:40 +0800

    add winusb support

--- ports/rp2/boards/PICO/mpconfigboard.h
+++ ports/rp2/boards/PICO/mpconfigboard.h
@@ -1,12 +1,3 @@
 // Board and hardware specific configuration
 #define MICROPY_HW_BOARD_NAME                   "Raspberry Pi Pico"
 #define MICROPY_HW_FLASH_STORAGE_BYTES          (1408 * 1024)
-
-#define MICROPY_HW_USB_VID (0x2E8A)
-#define MICROPY_HW_USB_PID (0x000c)
-
-#define MICROPY_HW_USB_CDC                      (1)
-#define MICROPY_HW_USB_MSC                      (1)
-#define MICROPY_HW_USB_VENDOR                   (1)
-
-#define MICROPY_PY_THREAD                       (1)
--- shared/tinyusb/tusb_config.h
+++ shared/tinyusb/tusb_config.h
@@ -84,12 +84,12 @@
 // VENDOR Configuration
 #if CFG_TUD_VENDOR
 #ifndef MICROPY_HW_USB_VENDOR_INTERFACE_STRING
-#define MICROPY_HW_USB_VENDOR_INTERFACE_STRING "Kiwidap CMSIS-DAP"
+#define MICROPY_HW_USB_VENDOR_INTERFACE_STRING "Board VENDOR"
 #endif
 #define CFG_TUD_VENDOR_EP_BUFSIZE  (64)
 #define CFG_TUD_VENDOR_RX_BUFSIZE  (64)
 #define CFG_TUD_VENDOR_TX_BUFSIZE  (64)
-#endif     
+#endif
 
 // Define static descriptor size and interface count based on the above config
 
@@ -110,8 +110,10 @@
 
 #define USBD_MAX_POWER_MA (250)
 
-#define USBD_DESC_STR_MAX (20)
-/*
+#ifndef MICROPY_HW_USB_DESC_STR_MAX
+#define MICROPY_HW_USB_DESC_STR_MAX (20)
+#endif
+
 #if CFG_TUD_CDC
 #define USBD_ITF_CDC (0) // needs 2 interfaces
 #define USBD_CDC_EP_CMD (0x81)
@@ -133,44 +135,39 @@
 #endif // CFG_TUD_MSC
 
 #if CFG_TUD_VENDOR
-#if CFG_TUD_CDC
 #if CFG_TUD_MSC
+
+#if CFG_TUD_CDC
 #define USBD_ITF_VENDOR (3)
 #define EPNUM_VENDOR_OUT (0x04)
 #define EPNUM_VENDOR_IN (0x84)
 #else
+#define USBD_ITF_VENDOR (1)
+#define EPNUM_VENDOR_OUT (0x02)
+#define EPNUM_VENDOR_IN (0x82)
+#endif // CFG_TUD_CDC
+
+#else
+
+#if CFG_TUD_CDC
 #define USBD_ITF_VENDOR (2)
 #define EPNUM_VENDOR_OUT (0x03)
 #define EPNUM_VENDOR_IN (0x83)
-#endif // CFG_TUD_MSC
 #else
 #define USBD_ITF_VENDOR (0)
 #define EPNUM_VENDOR_OUT (0x01)
 #define EPNUM_VENDOR_IN (0x81)
 #endif // CFG_TUD_CDC
+
+#endif // CFG_TUD_MSC
 #endif // CFG_TUD_VENDOR
-*/
 
-#define USBD_ITF_VENDOR (0)
-#define EPNUM_VENDOR_OUT (0x04)
-#define EPNUM_VENDOR_IN (0x84)
 
-#define USBD_ITF_CDC (1) // needs 2 interfaces
-#define USBD_CDC_EP_CMD (0x81)
-#define USBD_CDC_EP_OUT (0x02)
-#define USBD_CDC_EP_IN (0x82)
-
-#define USBD_ITF_MSC (3)
-#define EPNUM_MSC_OUT (0x03)
-#define EPNUM_MSC_IN (0x83)
-
-#define USBD_ITF_STATIC_MAX 4
-
-/* Limits of statically defined USB interfaces, endpoints, strings 
+/* Limits of statically defined USB interfaces, endpoints, strings */
 #if CFG_TUD_VENDOR
 #define USBD_ITF_STATIC_MAX (USBD_ITF_VENDOR + 1)
 #define USBD_STR_STATIC_MAX (USBD_STR_VENDOR + 1)
-#define USBD_EP_STATIC_MAX (EPNUM_VENDOR_OUT + 1)  
+#define USBD_EP_STATIC_MAX (EPNUM_VENDOR_OUT + 1)
 #elif CFG_TUD_MSC
 #define USBD_ITF_STATIC_MAX (USBD_ITF_MSC + 1)
 #define USBD_STR_STATIC_MAX (USBD_STR_MSC + 1)
@@ -184,7 +181,7 @@
 #define USBD_STR_STATIC_MAX (0)
 #define USBD_EP_STATIC_MAX (0)
 #endif
-*/
+
 
 
 #endif // MICROPY_HW_ENABLE_USBDEV
