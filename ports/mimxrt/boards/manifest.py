8b47937cb5516308ec12a37999605557cb4f4c51
robert-hh <robert@hammelrath.com>
2023-04-23 11:15:42 +0200

    mimxrt/BLE: Change main.c, pendsv.h, mpconfigport.h, Makefile for BLE.
    And reduce the stack size for BLE.
    
    Nibmle uses 13k of static data, increasing the .bss section. Moving the
    stack or bss to OCRM is possible, but reduces the Python heap size.
    
    Signed-off-by: robert-hh <robert@hammelrath.com>

--- ports/mimxrt/boards/manifest.py
+++ ports/mimxrt/boards/manifest.py
@@ -3,3 +3,10 @@ include("$(MPY_DIR)/extmod/asyncio")
 require("onewire")
 require("ds18x20")
 require("dht")
+include(
+    "$(MPY_LIB_DIR)/micropython/bluetooth/aioble/manifest.py",
+    client=True,
+    central=True,
+    l2cap=True,
+    security=True,
+)
--- ports/mimxrt/main.c
+++ ports/mimxrt/main.c
@@ -57,6 +57,10 @@
 #include "extmod/modmachine.h"
 #include "extmod/modnetwork.h"
 #include "extmod/vfs.h"
+#if MICROPY_PY_BLUETOOTH
+#include "extmod/modbluetooth.h"
+#include "mpbthciport.h"
+#endif
 
 extern uint8_t _sstack, _estack, _gc_heap_start, _gc_heap_end;
 
@@ -67,6 +71,10 @@ int main(void) {
     ticks_init();
     pendsv_init();
 
+    #if MICROPY_PY_BLUETOOTH
+    mp_bluetooth_hci_init();
+    #endif
+
     #if MICROPY_PY_LWIP
     // lwIP doesn't allow to reinitialise itself by subsequent calls to this function
     // because the system timeout list (next_timeout) is only ever reset by BSS clearing.
@@ -75,9 +83,8 @@ int main(void) {
     #if LWIP_MDNS_RESPONDER
     mdns_resp_init();
     #endif
-
-    systick_enable_dispatch(SYSTICK_DISPATCH_LWIP, mod_network_lwip_poll_wrapper);
     #endif
+
     #if MICROPY_PY_BLUETOOTH
     mp_bluetooth_hci_init();
     #endif
@@ -107,6 +114,15 @@ int main(void) {
         #if MICROPY_PY_NETWORK
         mod_network_init();
         #endif
+        #if MICROPY_PY_BLUETOOTH
+        mp_bluetooth_hci_init();
+        #endif
+
+        #if MICROPY_PY_LWIP
+        // mod_network_lwip_init();
+        systick_enable_dispatch(SYSTICK_DISPATCH_LWIP, mod_network_lwip_poll_wrapper);
+        #endif
+
 
         // Initialise sub-systems.
         readline_init0();
@@ -164,11 +180,18 @@ int main(void) {
         #if MICROPY_PY_MACHINE_I2S
         machine_i2s_deinit_all();
         #endif
-        #if MICROPY_PY_BLUETOOTH
-        mp_bluetooth_deinit();
+        #if MICROPY_PY_LWIP
+        systick_disable_dispatch(SYSTICK_DISPATCH_LWIP);
+        #endif
+        #if MICROPY_PY_NETWORK_ESP_HOSTED
+        int esp_hosted_wifi_deinit(void);
+        esp_hosted_wifi_deinit();
         #endif
         #if MICROPY_PY_NETWORK
         mod_network_deinit();
+        #endif
+        #if MICROPY_PY_BLUETOOTH
+        mp_bluetooth_deinit();
         #endif
         #if MICROPY_PY_MACHINE_UART
         machine_uart_deinit_all();
@@ -191,6 +214,11 @@ void gc_collect(void) {
 }
 
 void nlr_jump_fail(void *val) {
+    for (;;) {
+    }
+}
+
+void abort(void) {
     for (;;) {
     }
 }
