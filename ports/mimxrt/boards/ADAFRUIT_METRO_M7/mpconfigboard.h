28263fc4f7553a6e3186833d608db03b373109a6
robert-hh <robert@hammelrath.com>
2023-04-23 12:08:23 +0200

    mimxrt/NINAW10: Add or change board configuration files.
    For:
    MIMXRT1010_EVK
    MIMXRT1015_EVK
    ADAFRUIT Metro M7
    OLIMEX RT1010
    
    Adafruit Metro M7 board:
    
    Add the SD variant of the Adafruit Metro M7 board.
    It supports WIFI/BLE as well, but at pins matching the Airlift UNO
    breakout.
    
    Olimex 1010 board:
    
    The pins are selected in a way that they are accessible at the UEXT1
    connector of the Dev board.
    
    There is no free Pin for GPIO0. So GPIO0 must be connected with a
    resistor to a Pin which is not used for FW upload and is used as
    output during normal operation, e.g. SPI SCL.
    The NINA FW configures GPIO0 as output during normal operation.
    
    Signed-off-by: robert-hh <robert@hammelrath.com>

--- ports/mimxrt/boards/ADAFRUIT_METRO_M7/mpconfigboard.h
+++ ports/mimxrt/boards/ADAFRUIT_METRO_M7/mpconfigboard.h
@@ -58,25 +58,26 @@
 #define MICROPY_HW_WIFI_SPI_ID       (0)
 #define MICROPY_HW_WIFI_SPI_BAUDRATE (8000000)
 
-// ESP_BUSY: GPIO_AD_11
-#define MICROPY_HW_NINA_ACK          (&pin_GPIO_AD_11)
-// ESP_CS: GPIO_AD_14
-#define MICROPY_HW_NINA_CS           (&pin_GPIO_AD_14)
-// ESP_RESET: GPIO_AD_07
-#define MICROPY_HW_NINA_RESET        (&pin_GPIO_AD_07)
-
 // BLE definitions
 #define MICROPY_PY_BLUETOOTH_NINAW10 (1)
 
 #define MICROPY_HW_BLE_UART_ID       (1)
 #define MICROPY_HW_BLE_UART_BAUDRATE (115200)
+#define MICROPY_HW_BLE_UART_FLOW_CONTROL (0)
+
+#if MICROPY_HW_WIFI_AIRLIFT
+
+// ESP_BUSY: GPIO_AD_11
+#define MICROPY_HW_NINA_ACK          (pin_GPIO_AD_11)
+// ESP_CS: GPIO_AD_14
+#define MICROPY_HW_NINA_CS           (pin_GPIO_AD_14)
+// ESP_RESET: GPIO_AD_07
+#define MICROPY_HW_NINA_RESET        (pin_GPIO_AD_07)
 
 // MOSI == RTS: GPIO_AD_04
-#define MICROPY_HW_NINA_RTS          (&pin_GPIO_AD_04)
+#define MICROPY_HW_NINA_RTS          (pin_GPIO_AD_04)
 // CTS == NINA_ACK:  GPIO_AD_11
 #define MICROPY_HW_NINA_CTS          MICROPY_HW_NINA_ACK
-<<<<<<< Updated upstream
-=======
 
 #else
 
@@ -93,4 +94,3 @@
 #define MICROPY_HW_NINA_CTS          MICROPY_HW_NINA_ACK
 
 #endif
->>>>>>> Stashed changes
