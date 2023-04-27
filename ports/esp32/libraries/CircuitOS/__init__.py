a31c12bfb5a611dbabbb2b51c25d8d01e8dafb92
Filip Budiša <filip@circuitmess.com>
2023-04-27 15:45:56 +0200

    esp32/libraries/CircuitOS: Update __init__.py

--- ports/esp32/libraries/CircuitOS/__init__.py
+++ ports/esp32/libraries/CircuitOS/__init__.py
@@ -3,5 +3,10 @@ from ._Input import *
 from ._Display import *
 from ._LED import *
 from ._Encoder import *
-from .Matrix import *
+from ._Slider import *
+
 from .Devices.PCA95XX import *
+from .Devices.IS31FL3731 import *
+from .Devices.ShiftOutput import *
+
+from ._Matrix import *
