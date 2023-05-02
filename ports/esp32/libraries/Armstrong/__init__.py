ad81196cc14b4325fcbaa9a176704f85d2de39f8
Filip Budiša <filip@circuitmess.com>
2023-05-02 14:39:27 +0200

    esp32/libraries/Armstrong: Add LEDs

--- ports/esp32/libraries/Armstrong/__init__.py
+++ ports/esp32/libraries/Armstrong/__init__.py
@@ -1,4 +1,4 @@
-from CircuitOS import InputShift, Encoder, RGB_LED, Encoders
+from CircuitOS import InputShift, Encoder, RGB_LED, Encoders, ShiftOutput
 from .Pins import *
 from .Servos import ServoControl
 
@@ -10,10 +10,23 @@ enc_C = Encoder(Pins.ENC_1A, Pins.ENC_1B)
 enc_D = Encoder(Pins.ENC_2A, Pins.ENC_2B)
 encoders = Encoders([enc_A, enc_B, enc_C, enc_D])
 
-led = RGB_LED((Pins.LED_R, Pins.LED_G, Pins.LED_B), reverse_logic=True)
+rgb = RGB_LED((Pins.LED_R, Pins.LED_G, Pins.LED_B), reverse_logic=True)
+
+leds = ShiftOutput(Pins.SHIFTOUT_CLOCK, Pins.SHIFTOUT_DATA)
+class LED:
+	Slot_1 = const(1)
+	Slot_2 = const(2)
+	Slot_3 = const(3)
+	Slot_4 = const(4)
+	PlayPause = const(0)
+
 
 servos = ServoControl((Pins.SERVO_1, Pins.SERVO_2, Pins.SERVO_3, Pins.SERVO_4))
 
 
 def begin():
+	leds.set_all(False)
+	rgb.set(0, 0, 0)
+	servos.center()
 	buttons.scan()
+	encoders.scan()
