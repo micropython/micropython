b33c9f6b4b0ae428682560496c8c89015542496e
Jeff Epler <jepler@gmail.com>
2023-05-03 09:32:51 -0500

    synthio: put license notice on copied files

--- tests/circuitpython-manual/synthio/wave/chunk.py
+++ tests/circuitpython-manual/synthio/wave/chunk.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2023 Guido van Rossum <guido@cwi.nl> and others.
+#
+# SPDX-License-Identifier: PSF-2.0
+
 """Simple class to read IFF chunks.
 
 An IFF chunk (used in formats such as AIFF, TIFF, RMFF (RealMedia File
--- tests/circuitpython-manual/synthio/wave/wave.py
+++ tests/circuitpython-manual/synthio/wave/wave.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2023 Guido van Rossum <guido@cwi.nl> and others.
+#
+# SPDX-License-Identifier: PSF-2.0
+
 """Stuff to parse WAVE files.
 
 Usage.
