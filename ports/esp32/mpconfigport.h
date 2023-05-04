38243cd8e0792fd6e597e4761f9d9949a9d41599
Damien George <damien@micropython.org>
2023-05-04 12:58:04 +1000

    extmod/machine_pwm: Remove PWM_INIT and PWM_DUTY_U16_NS config options.
    All ports that enable MICROPY_PY_MACHINE_PWM now enable these two
    sub-options, so remove these sub-options altogether to force consistency in
    new ports that implement machine.PWM.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/esp32/mpconfigport.h
+++ ports/esp32/mpconfigport.h
@@ -98,9 +98,7 @@
 #define MICROPY_PY_MACHINE_BITSTREAM        (1)
 #define MICROPY_PY_MACHINE_PULSE            (1)
 #define MICROPY_PY_MACHINE_PWM              (1)
-#define MICROPY_PY_MACHINE_PWM_INIT         (1)
 #define MICROPY_PY_MACHINE_PWM_DUTY         (1)
-#define MICROPY_PY_MACHINE_PWM_DUTY_U16_NS  (1)
 #define MICROPY_PY_MACHINE_PWM_INCLUDEFILE  "ports/esp32/machine_pwm.c"
 #define MICROPY_PY_MACHINE_I2C              (1)
 #define MICROPY_PY_MACHINE_I2C_TRANSFER_WRITE1 (1)
