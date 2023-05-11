17127bbee52f2c674c5c79b675fabacea7bc2751
Jim Mussared <jim.mussared@gmail.com>
2023-05-11 14:28:35 +1000

    tests/run-tests.py: Ensure correct cwd for mpy tests.
    Previously when using --via-mpy, the file was compiled to tests/<tmp>.mpy
    and then run using `micropython -m <tmp>` in the current cwd
    (usually tests/).  This meant that an import in the test would be resolved
    relative to tests/.
    
    This is different to regular (non-via-mpy) tests, where we run (for
    example) `micropython basics/test.py` which means that an import would be
    resolved relative to basics/.
    
    Now --via-mpy matches the .py behavior.  This is important because:
    a) It makes it so import tests do the right thing.
    b) There are directory names in tests/ that match built-in module names.
    
    Furthermore, it always ensures the cwd (for both micropython and cpython)
    is the test directory (e.g. basics/) rather than being left unset.  This
    also makes it clearer inside the test that e.g. file access is relative to
    the Python file.
    
    Updated tests with file paths to match.
    
    This work was funded through GitHub Sponsors.
    
    Signed-off-by: Jim Mussared <jim.mussared@gmail.com>

--- tests/io/file_readinto.py
+++ tests/io/file_readinto.py
@@ -1,13 +1,13 @@
 b = bytearray(30)
-f = open("io/data/file1", "rb")
+f = open("data/file1", "rb")
 print(f.readinto(b))
 print(b)
-f = open("io/data/file2", "rb")
+f = open("data/file2", "rb")
 print(f.readinto(b))
 print(b)
 
 # readinto() on writable file
-f = open("io/data/file1", "ab")
+f = open("data/file1", "ab")
 try:
     f.readinto(bytearray(4))
 except OSError:
