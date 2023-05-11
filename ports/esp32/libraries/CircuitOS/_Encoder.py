004ea2639db33667cc2572f045231abdbeb690ae
Filip Budiša <filip@circuitmess.com>
2023-05-11 13:11:20 +0200

    esp32/libraries/CircuitOS: Add Encoders::on_move_left and on_move_right

--- ports/esp32/libraries/CircuitOS/_Encoder.py
+++ ports/esp32/libraries/CircuitOS/_Encoder.py
@@ -8,6 +8,8 @@ class Encoder:
 
         self.prev_state = float('inf')
         self._on_move = None
+        self._on_move_l = None
+        self._on_move_r = None
 
     def scan(self):
         val = 0
@@ -26,6 +28,12 @@ class Encoder:
         if val != 0 and self._on_move is not None:
             self._on_move(val)
 
+        if val == -1 and self._on_move_l is not None:
+            self._on_move_l()
+
+        if val == 1 and self._on_move_r is not None:
+            self._on_move_r()
+
     async def loop(self):
         from uasyncio import sleep_ms
 
@@ -36,6 +44,12 @@ class Encoder:
     def on_move(self, listener: callable):
         self._on_move = listener
 
+    def on_move_left(self, listener: callable):
+        self._on_move_l = listener
+
+    def on_move_right(self, listener: callable):
+        self._on_move_r = listener
+
 
 class Encoders:
 
@@ -45,8 +59,17 @@ class Encoders:
     def on_move(self, i: int, listener: callable):
         if i < 0 or i >= len(self.enc):
             return
-
         self.enc[i].on_move(listener)
+
+    def on_move_left(self, i: int, listener: callable):
+        if i < 0 or i >= len(self.enc):
+            return
+        self.enc[i].on_move_left(listener)
+
+    def on_move_right(self, i: int, listener: callable):
+        if i < 0 or i >= len(self.enc):
+            return
+        self.enc[i].on_move_right(listener)
 
     def scan(self):
         for enc in self.enc:
