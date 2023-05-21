0a1b2f87714d2bf57c17b3f8d6fcdf6d066046d9
David Sullivan <treatkor@gmail.com>
2023-05-21 14:30:51 -0700

    include neopixel lib

--- ports/espressif/boards/lolin_c3_pico/mpconfigboard.mk
+++ ports/espressif/boards/lolin_c3_pico/mpconfigboard.mk
@@ -6,3 +6,6 @@ IDF_TARGET = esp32c3
 CIRCUITPY_ESP_FLASH_MODE=qio
 CIRCUITPY_ESP_FLASH_FREQ=80m
 CIRCUITPY_ESP_FLASH_SIZE=4MB
+
+# Include these Python libraries in firmware.
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_NeoPixel
