78e75f69773f071689ef5a614eb564193224d45d
Jeff Epler <jepler@gmail.com>
2023-05-21 13:16:27 -0500

    synthio: enable additional LFO test cases

--- tests/circuitpython/synthlfo.py
+++ tests/circuitpython/synthlfo.py
@@ -10,9 +10,9 @@ sine = array.array("h", [int(32767 * sin(i * 2 * pi / 600)) for i in range(600)]
 lfos = [
     LFO(bend_out, once=True, rate=4),
     LFO(bend_in, once=True, rate=2.5, scale=2),
-    LFO(triangle, rate=3)
-    #    LFO(bend_in, interpolate=False, rate=5),
-    #    LFO(bend_in, interpolate=True, rate=5),
+    LFO(triangle, rate=3),
+    LFO(bend_in, interpolate=False, rate=5),
+    LFO(bend_in, interpolate=True, rate=5),
 ]
 lfos[2].rate = lfos[1]
 lfos[2].offset = lfos[0]
