5c38dba60b080b401657f2f12a2498946ed8f27c
Philipp Schindler <philipp.schindler@uibk.ac.at>
2023-05-30 22:23:02 +0200

    Fixed formatting. Fixed pin configuration

--- ports/stm32/boards/NUCLEO_H7A3ZI/mpconfigboard.h
+++ ports/stm32/boards/NUCLEO_H7A3ZI/mpconfigboard.h
@@ -13,39 +13,21 @@
 #define MICROPY_HW_ENABLE_INTERNAL_FLASH_STORAGE (1)
 
 // The board has a 24MHz HSE, the following gives 280MHz CPU speed
-#define MICROPY_HW_CLK_PLLM (1)
-#define MICROPY_HW_CLK_PLLN (70)
-#define MICROPY_HW_CLK_PLLP (2)
-#define MICROPY_HW_CLK_PLLQ (14)
-#define MICROPY_HW_CLK_PLLR (14)
+#define MICROPY_HW_CLK_PLLM         (1)
+#define MICROPY_HW_CLK_PLLN         (70)
+#define MICROPY_HW_CLK_PLLP         (2)
+#define MICROPY_HW_CLK_PLLQ         (14)
+#define MICROPY_HW_CLK_PLLR         (14)
 #define MICROPY_HW_CLK_PLLVCI       (RCC_PLL1VCIRANGE_1)
 #define MICROPY_HW_CLK_PLLVCO       (RCC_PLL1VCOWIDE)
 #define MICROPY_HW_CLK_PLLFRAC      (0)
 
-//#define MICROPY_HW_CLK_PLLM         (12)
-//#define MICROPY_HW_CLK_PLLN         (280)
-//#define MICROPY_HW_CLK_PLLP         (2)
-//#define MICROPY_HW_CLK_PLLQ         (2)
-//#define MICROPY_HW_CLK_PLLR         (2)
-//#define MICROPY_HW_CLK_PLLVCI       (RCC_PLL1VCIRANGE_1)
-//#define MICROPY_HW_CLK_PLLVCO       (RCC_PLL1VCOWIDE)
-//#define MICROPY_HW_CLK_PLLFRAC      (0)
-
-
 // The USB clock is set using PLL3 (48Mhz usb clock)
-//#define MICROPY_HW_CLK_PLL3M        (12)
-//#define MICROPY_HW_CLK_PLL3N        (192)
-//#define MICROPY_HW_CLK_PLL3P        (17)
-//#define MICROPY_HW_CLK_PLL3Q        (8)
-//#define MICROPY_HW_CLK_PLL3R        (2)
-//#define MICROPY_HW_CLK_PLL3VCI      (RCC_PLL3VCIRANGE_1)
-//#define MICROPY_HW_CLK_PLL3VCO      (RCC_PLL3VCOWIDE)
-//#define MICROPY_HW_CLK_PLL3FRAC     (0)
-#define MICROPY_HW_CLK_PLL3M (1)
-#define MICROPY_HW_CLK_PLL3N (30)
-#define MICROPY_HW_CLK_PLL3P (2)
-#define MICROPY_HW_CLK_PLL3Q (5)
-#define MICROPY_HW_CLK_PLL3R (2)
+#define MICROPY_HW_CLK_PLL3M        (1)
+#define MICROPY_HW_CLK_PLL3N        (30)
+#define MICROPY_HW_CLK_PLL3P        (2)
+#define MICROPY_HW_CLK_PLL3Q        (5)
+#define MICROPY_HW_CLK_PLL3R        (2)
 #define MICROPY_HW_CLK_PLL3VCI      (RCC_PLL3VCIRANGE_1)
 #define MICROPY_HW_CLK_PLL3VCO      (RCC_PLL3VCOWIDE)
 #define MICROPY_HW_CLK_PLL3FRAC     (0)
@@ -56,32 +38,6 @@
 // SMPS configuration
 #define MICROPY_HW_PWR_SMPS_CONFIG  (PWR_DIRECT_SMPS_SUPPLY)
 
-#if 0
-// 512MBit external OSPI flash, used for either the filesystem or XIP memory mapped
-#define MICROPY_HW_OSPIFLASH_SIZE_BITS_LOG2 (29)
-#define MICROPY_HW_OSPIFLASH_CS     (pin_G6)
-#define MICROPY_HW_OSPIFLASH_CLK    (pin_B2)
-#define MICROPY_HW_OSPIFLASH_DQS    (pin_C5)
-#define MICROPY_HW_OSPIFLASH_IO0    (pin_P8)
-#define MICROPY_HW_OSPIFLASH_IO1    (pin_F9)
-#define MICROPY_HW_OSPIFLASH_IO2    (pin_F7)
-#define MICROPY_HW_OSPIFLASH_IO3    (pin_F6)
-#define MICROPY_HW_OSPIFLASH_IO4    (pin_C1)
-#define MICROPY_HW_OSPIFLASH_IO5    (pin_H3)
-#define MICROPY_HW_OSPIFLASH_IO6    (pin_D6)
-#define MICROPY_HW_OSPIFLASH_IO7    (pin_G14)
-#endif
-
-// UART buses
-//#define MICROPY_HW_UART1_TX         (pin_A9)
-//#define MICROPY_HW_UART1_RX         (pin_A10)
-//#define MICROPY_HW_UART4_TX         (pin_H13) // Arduino Connector CN11-Pin1
-//#define MICROPY_HW_UART4_RX         (pin_H14) // Arduino Connector CN11-Pin2
-//#define MICROPY_HW_UART_REPL        PYB_UART_1
-//#define MICROPY_HW_UART_REPL_BAUD   115200
-
-
-
 // UART config
 #define MICROPY_HW_UART2_TX                 (pin_D5)
 #define MICROPY_HW_UART2_RX                 (pin_D6)
@@ -89,8 +45,8 @@
 #define MICROPY_HW_UART2_CTS                (pin_D3)
 #define MICROPY_HW_UART3_TX                 (pin_D8)
 #define MICROPY_HW_UART3_RX                 (pin_D9)
-#define MICROPY_HW_UART5_TX                 (pin_B6)
-#define MICROPY_HW_UART5_RX                 (pin_B12)
+#define MICROPY_HW_UART5_TX                 (pin_B6) 
+#define MICROPY_HW_UART5_RX                 (pin_B12) 
 #define MICROPY_HW_UART6_TX                 (pin_C6)
 #define MICROPY_HW_UART6_RX                 (pin_C7)
 #define MICROPY_HW_UART7_TX                 (pin_F7)
@@ -109,8 +65,8 @@
 #define MICROPY_HW_USRSW_PRESSED    (1)
 
 // LEDs
-#define MICROPY_HW_LED1             (pin_G11)   // red
-#define MICROPY_HW_LED2             (pin_G2)    // blue
+#define MICROPY_HW_LED1             (pin_G11)   // red //not consisten with PB0
+#define MICROPY_HW_LED2             (pin_G2)    // blue //not consistent with PE1
 #define MICROPY_HW_LED_ON(pin)      (mp_hal_pin_low(pin))
 #define MICROPY_HW_LED_OFF(pin)     (mp_hal_pin_high(pin))
 
