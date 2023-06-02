edd020d6c8942ad58daee4b21badbd541b693045
IFX-Anusha <Anusha.TR@infineon.com>
2023-06-02 17:52:26 +0530

    ports/psoc6: Changes for fixing Warnings in PWM Module.
    Signed-off-by: IFX-Anusha <Anusha.TR@infineon.com>

--- ports/psoc6/modules/machine/machine_pwm.c
+++ ports/psoc6/modules/machine/machine_pwm.c
@@ -31,7 +31,7 @@ STATIC void pwm_is_active(machine_pwm_obj_t *self) {
 STATIC void mp_machine_pwm_print(const mp_print_t *print, mp_obj_t self_in, mp_print_kind_t kind) {
     machine_pwm_obj_t *self = MP_OBJ_TO_PTR(self_in);
     pwm_is_active(self_in);
-    mp_printf(print, "frequency=%u duty_cycle=%f invert=%u", self->fz, self->duty, self->invert);
+    mp_printf(print, "frequency=%u duty_cycle=%f invert=%u", self->fz, (double)self->duty, self->invert);
 }
 
 STATIC void mp_machine_pwm_init_helper(machine_pwm_obj_t *self,
