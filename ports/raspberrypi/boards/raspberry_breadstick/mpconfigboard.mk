bfffc7106e5e35efddef20ab1837ac633ff1bcb0
mrangen <michael.rangen@gmail.com>
2023-06-14 23:14:05 -0600

    remove frozen mods

--- ports/raspberrypi/boards/raspberry_breadstick/mpconfigboard.mk
+++ ports/raspberrypi/boards/raspberry_breadstick/mpconfigboard.mk
@@ -12,5 +12,5 @@ CIRCUITPY__EVE = 1
 CIRCUITPY_PICODVI = 1
 
 # Include these Python libraries in firmware.
-FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_LSM6DS/frozen_cpx
-FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_DotStar/frozen_cpx
+#FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_LSM6DS/frozen_cpx
+#FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_DotStar/frozen_cpx
