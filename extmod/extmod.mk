c8299de8960fbe920cc12d6a7930b1334d612570
Amir Gonnen <amirgonnen@gmail.com>
2023-06-16 23:48:55 +0300

    Move unix specific build rules

--- extmod/extmod.mk
+++ extmod/extmod.mk
@@ -368,43 +368,6 @@ endif
 endif
 
 ################################################################################
-# LVGL
-
-# Additional optional libraries
-
-ifneq ($(UNAME_S),Darwin)
-CFLAGS_EXTMOD += -DMICROPY_FB=1
-endif
-
-SDL_CFLAGS_EXTMOD :=  $(shell pkg-config --silence-errors --cflags sdl2)
-SDL_LDFLAGS_EXTMOD := $(shell pkg-config --silence-errors --libs   sdl2)
-ifneq ($(SDL_LDFLAGS_EXTMOD),)
-CFLAGS_EXTMOD += $(SDL_CFLAGS_EXTMOD) -DMICROPY_SDL=1
-LDFLAGS_EXTMOD += $(SDL_LDFLAGS_EXTMOD)
-endif
-
-RLOTTIE_CFLAGS_EXTMOD :=  $(shell pkg-config --silence-errors --cflags rlottie)
-RLOTTIE_LDFLAGS_EXTMOD := $(shell pkg-config --silence-errors --libs   rlottie)
-ifneq ($(RLOTTIE_LDFLAGS_EXTMOD),)
-CFLAGS_EXTMOD += $(RLOTTIE_CFLAGS_EXTMOD) -DMICROPY_RLOTTIE=1
-LDFLAGS_EXTMOD += $(RLOTTIE_LDFLAGS_EXTMOD)
-endif
-
-FREETYPE_CFLAGS_EXTMOD :=  $(shell pkg-config --silence-errors --cflags freetype2)
-FREETYPE_LDFLAGS_EXTMOD := $(shell pkg-config --silence-errors --libs   freetype2)
-ifneq ($(FREETYPE_LDFLAGS_EXTMOD),)
-CFLAGS_EXTMOD += $(FREETYPE_CFLAGS_EXTMOD) -DMICROPY_FREETYPE=1
-LDFLAGS_EXTMOD += $(FREETYPE_LDFLAGS_EXTMOD)
-endif
-
-FFMPEG_LIBS := libavformat libavcodec libswscale libavutil
-FFMPEG_CFLAGS_EXTMOD :=  $(shell pkg-config --silence-errors --cflags $(FFMPEG_LIBS))
-FFMPEG_LDFLAGS_EXTMOD := $(shell pkg-config --silence-errors --libs   $(FFMPEG_LIBS))
-ifneq ($(FFMPEG_LDFLAGS_EXTMOD),)
-CFLAGS_EXTMOD += $(FFMPEG_CFLAGS_EXTMOD) -DMICROPY_FFMPEG=1
-LDFLAGS_EXTMOD += $(FFMPEG_LDFLAGS_EXTMOD)
-endif
-
 # LVGL build rules
 
 LVGL_BINDING_DIR = $(TOP)/lib/lv_bindings
