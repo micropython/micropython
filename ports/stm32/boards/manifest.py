52615da65431db71fc5184d7d7a094f6ed1a53f9
Amir Gonnen <amirgonnen@gmail.com>
2023-06-17 10:11:15 +0300

    stm32: fix manifest

--- ports/stm32/boards/manifest.py
+++ ports/stm32/boards/manifest.py
@@ -1,5 +1,5 @@
 include("$(MPY_DIR)/extmod/uasyncio")
 require("dht")
 require("onewire")
-freeze("$(MPY_DIR)/drivers/display", ("lcd160cr.py", "lcd160cr_test.py"))
+freeze("$(MPY_DIR)/lib/micropython-lib/micropython/drivers/display/lcd160cr", ("lcd160cr.py", "lcd160cr_test.py"))
 freeze("$(MPY_DIR)/lib/lv_bindings/lib", "lv_utils.py")
