7d915db3fa11da179f76f83cb9a025b281d2fd86
mrangen <michael.rangen@gmail.com>
2023-06-19 17:17:20 -0600

    edited PID and VID info

--- ports/raspberrypi/boards/raspberry_breadstick/mpconfigboard.mk
+++ ports/raspberrypi/boards/raspberry_breadstick/mpconfigboard.mk
@@ -1,7 +1,7 @@
-USB_VID = 0x239A
-USB_PID = 0x80F4
-USB_PRODUCT = "Pico"
-USB_MANUFACTURER = "Raspberry Pi"
+USB_VID = 0x2E8A
+USB_PID = 0x105E
+USB_PRODUCT = "Raspberry Breadstick"
+USB_MANUFACTURER = "J&R Creative Technologies Inc."
 
 CHIP_VARIANT = RP2040
 CHIP_FAMILY = rp2
@@ -12,5 +12,5 @@ CIRCUITPY__EVE = 1
 CIRCUITPY_PICODVI = 1
 
 # Include these Python libraries in firmware.
-#FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_LSM6DS/frozen_cpx
-#FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_DotStar/frozen_cpx
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_LSM6DS/
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_DotStar/
