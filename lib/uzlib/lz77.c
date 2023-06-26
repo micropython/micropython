88f0d24dd39db6b3378c3304ced2e5536e52f3da
Jim Mussared <jim.mussared@gmail.com>
2023-06-27 02:56:58 +1000

    lib/uzlib/defl_static: Optimize zlib_start/finish_block.
    Collapsing the two adjacent calls to outbits saves 32 bytes.
    
    Bringing defl_static.c into lz77.c allows better inlining, saves 24 bytes.
    
    Merge the Outbuf/uzlib_lz77_state_t structs, a minor simplification that
    doesn't change code size.
    
    This work was funded through GitHub Sponsors.
    
    Signed-off-by: Jim Mussared <jim.mussared@gmail.com>

--- lib/uzlib/lz77.c
+++ lib/uzlib/lz77.c
@@ -11,6 +11,8 @@
 
 #include "uzlib.h"
 
+#include "defl_static.c"
+
 #define MATCH_LEN_MIN (3)
 #define MATCH_LEN_MAX (258)
 
@@ -18,8 +20,8 @@
 // hist_max should be greater than 0 a power of 2 (ie 1, 2, 4, 8, ...).
 // It's possible to pass in hist=NULL, and then the history window will be taken from the
 // src passed in to uzlib_lz77_compress (this is useful when not doing streaming compression).
-void uzlib_lz77_init(struct uzlib_lz77_state *state, uint8_t *hist, size_t hist_max) {
-    memset(&state->outbuf, 0, sizeof(state->outbuf));
+void uzlib_lz77_init(uzlib_lz77_state_t *state, uint8_t *hist, size_t hist_max) {
+    memset(state, 0, sizeof(uzlib_lz77_state_t));
     state->hist_buf = hist;
     state->hist_max = hist_max;
     state->hist_start = 0;
@@ -30,7 +32,7 @@ void uzlib_lz77_init(struct uzlib_lz77_state *state, uint8_t *hist, size_t hist_
 // Search back in the history for the maximum match of the given src data,
 // with support for searching beyond the end of the history and into the src buffer
 // (effectively the history and src buffer are concatenated).
-static size_t uzlib_lz77_search_max_match(struct uzlib_lz77_state *state, const uint8_t *src, size_t len, size_t *longest_offset) {
+static size_t uzlib_lz77_search_max_match(uzlib_lz77_state_t *state, const uint8_t *src, size_t len, size_t *longest_offset) {
     size_t longest_len = 0;
     for (size_t hist_search = 0; hist_search < state->hist_len; ++hist_search) {
         size_t match_len;
@@ -55,7 +57,7 @@ static size_t uzlib_lz77_search_max_match(struct uzlib_lz77_state *state, const 
 }
 
 // Compress the given chunk of data.
-void uzlib_lz77_compress(struct uzlib_lz77_state *state, const uint8_t *src, unsigned len) {
+void uzlib_lz77_compress(uzlib_lz77_state_t *state, const uint8_t *src, unsigned len) {
     const uint8_t *top = src + len;
     while (src < top) {
         // Look for a match in the history window.
@@ -64,10 +66,10 @@ void uzlib_lz77_compress(struct uzlib_lz77_state *state, const uint8_t *src, uns
 
         // Encode the literal byte or the match.
         if (match_len == 0) {
-            zlib_literal(&state->outbuf, *src);
+            uzlib_literal(state, *src);
             match_len = 1;
         } else {
-            zlib_match(&state->outbuf, match_offset, match_len);
+            uzlib_match(state, match_offset, match_len);
         }
 
         // Push the bytes into the history buffer.
