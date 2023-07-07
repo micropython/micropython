8991f0cd7a7d2074f3e8127cec7baaa4427020e8
Miroslav Zuzelka <mzuzelka@gmail.com>
2023-07-07 11:48:03 +0200

    Update ES3ink board definition
    - fix wrong VID
    - add NEOPIXEL definition
    - add DOUBLE_TAP_PIN definition
    - add definition for default SPI bus

--- ports/espressif/boards/es3ink/mpconfigboard.h
+++ ports/espressif/boards/es3ink/mpconfigboard.h
@@ -29,6 +29,9 @@
 #define MICROPY_HW_BOARD_NAME       "ES3ink"
 #define MICROPY_HW_MCU_NAME         "ESP32S3"
 
+#define MICROPY_HW_NEOPIXEL         (&pin_GPIO8)
+#define CIRCUITPY_STATUS_LED_POWER  (&pin_GPIO18)
+
 #define CIRCUITPY_BOOT_BUTTON       (&pin_GPIO0)
 
 #define DEFAULT_UART_BUS_RX         (&pin_GPIO44)
@@ -36,3 +39,9 @@
 
 #define DEFAULT_I2C_BUS_SCL         (&pin_GPIO3)
 #define DEFAULT_I2C_BUS_SDA         (&pin_GPIO4)
+
+#define DEFAULT_SPI_BUS_SCK         (&pin_GPIO12)
+#define DEFAULT_SPI_BUS_MOSI        (&pin_GPIO11)
+#define DEFAULT_SPI_BUS_MISO        (&pin_GPIO13)
+
+#define DOUBLE_TAP_PIN              (&pin_GPIO38)
