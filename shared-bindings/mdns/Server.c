3d212f70ebcddba7d4ac849a5b3e87cd3798369e
anecdata <16617689+anecdata@users.noreply.github.com>
2023-07-12 23:32:44 -0500

    mdns: can't use web workflow port

--- shared-bindings/mdns/Server.c
+++ shared-bindings/mdns/Server.c
@@ -171,6 +171,8 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_KW(mdns_server_find_obj, 1, _mdns_server_find);
 //|         ``service_type`` and ``protocol`` can only occur on one port. Any call after the first
 //|         will update the entry's port.
 //|
+//|         If web workflow is active, the port it uses can't also be used to advertise a service.
+//|
 //|         :param str service_type: The service type such as "_http"
 //|         :param str protocol: The service protocol such as "_tcp"
 //|         :param int port: The port used by the service"""
