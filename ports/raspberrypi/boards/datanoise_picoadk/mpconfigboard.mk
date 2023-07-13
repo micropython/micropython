3ac84281bf2fa7a3fcb1323e2ef4d3cf98b279db
Sylwester <syso@Mac-Pro.lan>
2023-07-13 12:04:27 +0200

    Add NeoPixel Library for Datanoise PicoADK.

--- ports/raspberrypi/boards/datanoise_picoadk/mpconfigboard.mk
+++ ports/raspberrypi/boards/datanoise_picoadk/mpconfigboard.mk
@@ -7,3 +7,6 @@ CHIP_VARIANT = RP2040
 CHIP_FAMILY = rp2
 
 EXTERNAL_FLASH_DEVICES = "GD25Q32C,W25Q32JVxQ"
+
+# Include these Python libraries in firmware.
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_NeoPixel
