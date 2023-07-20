afa14b53a2e44095245778ae02496053e49076d3
enriquezgarc <enriquezgarcia.external@infineon.com>
2023-07-20 16:52:07 +0200

    tests/psoc6/multi/network_config: Added ifconfig tests.
    Signed-off-by: enriquezgarc <enriquezgarcia.external@infineon.com>

--- tests/psoc6/multi/network_config.py
+++ tests/psoc6/multi/network_config.py
@@ -5,13 +5,17 @@ ssid_new = "mpy-test-conf-wlan"
 pass_new = "alicessecret"
 sec_new = network.WLAN.WPA2
 
+ap_ip = "10.20.0.1"
+gateway_ip = ap_ip
+netmask_ip = "255.255.255.128"
+dns_ip = "0.0.0.0"
+ap_net_conf = (ap_ip, netmask_ip, gateway_ip, dns_ip)
+
 
 # Access Point
 def instance0():
     ap_if = network.WLAN(network.AP_IF)
     print("ap instance created")
-
-    # ifconfig()
 
     # get config()
     ap_if.config(channel=channel_new)
@@ -30,6 +34,12 @@ def instance0():
     while ap_if.active() == False:
         pass
     print("ap is activated")
+
+    # set ifconfig()
+    ap_if.ifconfig(ap_net_conf)
+
+    # get ifconfig()
+    print("ap ip settings: ", ap_if.ifconfig() == ap_net_conf)
 
     print(" > yield station")
     multitest.next()
@@ -68,4 +78,11 @@ def instance1():
     except ValueError as err:
         print(err)
 
-    # # ifconfig()
+    # ifconfig()
+    sta_net_conf = ("10.20.0.2", netmask_ip, ap_ip, ap_ip)
+    print("sta ip settings: ", sta_if.ifconfig() == sta_net_conf)
+
+    try:
+        sta_if.ifconfig(sta_net_conf)  # not for STA
+    except ValueError as err:
+        print(err)
