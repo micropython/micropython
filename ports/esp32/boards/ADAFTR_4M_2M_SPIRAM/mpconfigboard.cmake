cde2920aa61e3afb8b985f02557e920717bc4c49
lukasz-lobocki <lukasz.lobocki@googlemail.com>
2023-07-22 19:36:45 +0200

    build(add): ADAFTR_4M_2M_SPRAM
    Feather 4MB Flash 2MB PSRAM/SPIRAM
    
    Signed-off-by: lukasz-lobocki <lukasz.lobocki@googlemail.com>

---
+++ ports/esp32/boards/ADAFTR_4M_2M_SPIRAM/mpconfigboard.cmake
@@ -0,0 +1,9 @@
+set(IDF_TARGET esp32s3)
+
+set(SDKCONFIG_DEFAULTS
+    boards/sdkconfig.base
+    boards/sdkconfig.usb
+    boards/sdkconfig.ble
+    boards/sdkconfig.spiram_sx
+    boards/GENERIC_S3_SPIRAM/sdkconfig.board
+)
