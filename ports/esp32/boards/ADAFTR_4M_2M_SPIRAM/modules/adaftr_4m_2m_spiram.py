45b7b23672b038e952f6b5c27e3bd4ce63630ae5
lukasz-lobocki <lukasz.lobocki@googlemail.com>
2023-07-23 07:37:25 +0200

    esp32/boards: Adding ADAFTR_4M_2M_SPIRAM.
    Adafruit ESP32-S3 Feather with 4MB Flash 2MB PSRAM

---
+++ ports/esp32/boards/ADAFTR_4M_2M_SPIRAM/modules/adaftr_4m_2m_spiram.py
@@ -0,0 +1,19 @@
+from micropython import const
+
+# SPI
+SPI_SCK = const(36)
+SPI_MOSI = const(35)
+SPI_MISO = const(37)
+
+# UART
+UART_RX = const(38)
+UART_TX = const(39)
+
+# I2C
+I2C_SCL = const(4)
+I2C_SDA = const(3)
+
+# Board specific
+POWER = const(21)
+NEOPIXEL = const(33)
+
