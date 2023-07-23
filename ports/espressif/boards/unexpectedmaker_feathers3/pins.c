4bc12d9fb0887a7db264afea4e14f1978bfc76e4
Unexpected Maker <seon@unexpectedmaker.com>
2023-07-23 13:39:53 +1000

    Update pins.c
    Changed naming of the second I2C board reference to match how others name things.

--- ports/espressif/boards/unexpectedmaker_feathers3/pins.c
+++ ports/espressif/boards/unexpectedmaker_feathers3/pins.c
@@ -126,7 +126,7 @@ STATIC const mp_rom_map_elem_t board_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_I2C), MP_ROM_PTR(&board_i2c_obj) },
     { MP_ROM_QSTR(MP_QSTR_STEMMA_I2C), MP_ROM_PTR(&board_i2c_obj) },
     { MP_ROM_QSTR(MP_QSTR_STEMMA_VERTICAL_I2C), MP_ROM_PTR(&board_stemma_i2c_obj) },
-    { MP_ROM_QSTR(MP_QSTR_STEMMA2_I2C), MP_ROM_PTR(&board_stemma_i2c_obj) },
+    { MP_ROM_QSTR(MP_QSTR_STEMMA_I2C2), MP_ROM_PTR(&board_stemma_i2c_obj) },
     { MP_ROM_QSTR(MP_QSTR_I2C2), MP_ROM_PTR(&board_stemma_i2c_obj) },
 
     // SPI
