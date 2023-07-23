463687fa42bffb8624f460cd42df214c5fb7d88d
lukasz-lobocki <lukasz.lobocki@googlemail.com>
2023-07-23 09:12:19 +0200

    esp32/boards: Adding ADAFRUIT_4M_2M_SPIRAM.
    Adafruit ESP32-S3 Feather with 4MB Flash 2MB PSRAM
    
    https://www.adafruit.com/product/5477
    
    Signed-off-by: lukasz-lobocki <lukasz.lobocki@googlemail.com>

---
+++ ports/esp32/boards/ADAFRUIT_4M_2M_SPIRAM/mpconfigboard.h
@@ -0,0 +1,12 @@
+#define MICROPY_HW_BOARD_NAME               "Adafruit Feather 4MB Flash 2MB SPIRAM"
+#define MICROPY_HW_MCU_NAME                 "ESP32S3"
+#define MICROPY_PY_NETWORK_HOSTNAME_DEFAULT "FeatherSPIRAM"
+
+#define MICROPY_PY_MACHINE_DAC              (0)
+
+#define MICROPY_HW_I2C0_SCL                 (4)
+#define MICROPY_HW_I2C0_SDA                 (3)
+
+#define MICROPY_HW_SPI1_SCK                 (36)
+#define MICROPY_HW_SPI1_MOSI                (35)
+#define MICROPY_HW_SPI1_MISO                (37)
