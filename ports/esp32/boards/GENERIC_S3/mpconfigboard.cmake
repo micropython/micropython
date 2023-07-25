afd407af8f1049aa764cc46d501286be36345a82
Jim Mussared <jim.mussared@gmail.com>
2023-07-25 15:25:07 +1000

    esp32/boards/GENERIC_S3: Merge with GENERIC_S3_{SPIRAM,SPIRAM_OCT}.
    These are now variants of the GENERIC_S3 board.
    
    This work was funded through GitHub Sponsors.
    
    Signed-off-by: Jim Mussared <jim.mussared@gmail.com>

--- ports/esp32/boards/GENERIC_S3/mpconfigboard.cmake
+++ ports/esp32/boards/GENERIC_S3/mpconfigboard.cmake
@@ -4,5 +4,18 @@ set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.base
     boards/sdkconfig.usb
     boards/sdkconfig.ble
+    boards/sdkconfig.spiram_sx
     boards/GENERIC_S3/sdkconfig.board
 )
+
+if(MICROPY_BOARD_VARIANT STREQUAL "spiram-oct")
+    set(SDKCONFIG_DEFAULTS
+        ${SDKCONFIG_DEFAULTS}
+        boards/sdkconfig.240mhz
+        boards/sdkconfig.spiram_oct
+    )
+
+    list(APPEND MICROPY_DEF_BOARD
+        MICROPY_HW_BOARD_NAME="Generic ESP32S3 module with Octal-SPIRAM"
+    )
+endif()
