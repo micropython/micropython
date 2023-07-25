565b9260a0d985b37b7847aa72ce3c4ee57840f8
Jim Mussared <jim.mussared@gmail.com>
2023-07-25 15:03:30 +1000

    esp32/boards/GENERIC: Merge with GENERIC_{SPIRAM,OTA,D2WD,UNICORE}.
    These are now variants of the GENERIC board.
    
    This work was funded through GitHub Sponsors.
    
    Signed-off-by: Jim Mussared <jim.mussared@gmail.com>

--- ports/esp32/boards/GENERIC/mpconfigboard.cmake
+++ ports/esp32/boards/GENERIC/mpconfigboard.cmake
@@ -2,3 +2,47 @@ set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.base
     boards/sdkconfig.ble
 )
+
+if(MICROPY_BOARD_VARIANT STREQUAL "d2wd")
+    set(SDKCONFIG_DEFAULTS
+        ${SDKCONFIG_DEFAULTS}
+        boards/GENERIC/sdkconfig.d2wd
+    )
+
+    list(APPEND MICROPY_DEF_BOARD
+        MICROPY_HW_MCU_NAME="ESP32-D2WD"
+    )
+endif()
+
+if(MICROPY_BOARD_VARIANT STREQUAL "ota")
+    set(SDKCONFIG_DEFAULTS
+        ${SDKCONFIG_DEFAULTS}
+        boards/GENERIC/sdkconfig.ota
+    )
+
+    list(APPEND MICROPY_DEF_BOARD
+        MICROPY_HW_BOARD_NAME="Generic ESP32 module with OTA"
+    )
+endif()
+
+if(MICROPY_BOARD_VARIANT STREQUAL "spiram")
+    set(SDKCONFIG_DEFAULTS
+        ${SDKCONFIG_DEFAULTS}
+        boards/sdkconfig.spiram
+    )
+
+    list(APPEND MICROPY_DEF_BOARD
+        MICROPY_HW_BOARD_NAME="Generic ESP32 module with SPIRAM"
+    )
+endif()
+
+if(MICROPY_BOARD_VARIANT STREQUAL "unicore")
+    set(SDKCONFIG_DEFAULTS
+        ${SDKCONFIG_DEFAULTS}
+        boards/GENERIC/sdkconfig.unicore
+    )
+
+    list(APPEND MICROPY_DEF_BOARD
+        MICROPY_HW_MCU_NAME="ESP32-UNICORE"
+    )
+endif()
