28aa82f6ffd02a3b7fb6667abb66d6fe154f07db
Jim Mussared <jim.mussared@gmail.com>
2023-07-25 15:25:07 +1000

    esp32/boards/GENERIC_S3: Merge with GENERIC_S3_{SPIRAM,SPIRAM_OCT}.
    These are now variants of the GENERIC_S3 board.
    
    This work was funded through GitHub Sponsors.
    
    Signed-off-by: Jim Mussared <jim.mussared@gmail.com>

--- ports/esp32/boards/GENERIC_S3/mpconfigboard.h
+++ ports/esp32/boards/GENERIC_S3/mpconfigboard.h
@@ -1,4 +1,7 @@
-#define MICROPY_HW_BOARD_NAME               "ESP32S3 module"
+#ifndef MICROPY_HW_BOARD_NAME
+// Can be set by mpconfigboard.cmake.
+#define MICROPY_HW_BOARD_NAME               "Generic ESP32S3 module"
+#endif
 #define MICROPY_HW_MCU_NAME                 "ESP32S3"
 
 #define MICROPY_PY_MACHINE_DAC              (0)
