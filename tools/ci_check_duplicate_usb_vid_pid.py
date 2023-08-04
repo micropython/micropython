1d3b2b019a25a42b41327933feb8623619a7a21a
Melissa LeBlanc-Williams <melissa@adafruit.com>
2023-08-04 08:54:22 -0700

    Update settings to match micropython

--- tools/ci_check_duplicate_usb_vid_pid.py
+++ tools/ci_check_duplicate_usb_vid_pid.py
@@ -63,7 +63,7 @@ DEFAULT_CLUSTERLIST = {
     ],
     "0x239A:0x102E": ["weact_studio_pico", "weact_studio_pico_16mb"],
     "0x303A:0x8166": ["yd_esp32_s3_n8r8", "yd_esp32_s3_n16r8"],
-    "0x2341:0x0070": ["arduino_nano_esp32s3", "arduino_nano_esp32s3_inverted_statusled"],
+    "0x2341:0x056B": ["arduino_nano_esp32s3", "arduino_nano_esp32s3_inverted_statusled"],
 }
 
 cli_parser = argparse.ArgumentParser(
