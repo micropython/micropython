8a5fe42fb49259d484b1286563d5bd3d45a06b42
Kwabena W. Agyeman <kwagyeman@live.com>
2023-08-03 17:55:27 -0700

    mimxrt/hal: Make flash clock frequency configurable.
    Signed-off-by: "Kwabena W. Agyeman" <kwagyeman@live.com>

--- ports/mimxrt/hal/qspi_nor_flash_config.c
+++ ports/mimxrt/hal/qspi_nor_flash_config.c
@@ -28,6 +28,10 @@ __attribute__((section(".boot_hdr.conf")))
 #define MICROPY_HW_FLASH_DQS kFlexSPIReadSampleClk_LoopbackFromDqsPad
 #endif
 
+#ifndef MICROPY_HW_FLASH_CLK
+#define MICROPY_HW_FLASH_CLK kFlexSpiSerialClk_100MHz
+#endif
+
 const flexspi_nor_config_t qspiflash_config = {
     .memConfig =
     {
@@ -48,7 +52,7 @@ const flexspi_nor_config_t qspiflash_config = {
         // Enable DDR mode, Wordaddassable, Safe configuration, Differential clock
         .deviceType = kFlexSpiDeviceType_SerialNOR,
         .sflashPadType = kSerialFlash_4Pads,
-        .serialClkFreq = kFlexSpiSerialClk_100MHz,
+        .serialClkFreq = MICROPY_HW_FLASH_CLK,
         .sflashA1Size = MICROPY_HW_FLASH_SIZE,
         .lookupTable =
         {
