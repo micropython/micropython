0522598dc0b126cdc67e5756bd9465a2abf66472
enriquezgarc <enriquezgarcia.external@infineon.com>
2023-08-11 12:12:24 +0200

    tools/psoc6: Adding dev environment setup support script.
    Signed-off-by: enriquezgarc <enriquezgarcia.external@infineon.com>

---
+++ tools/psoc6/dev-setup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Use this script to setup your development environment.
+# With the ModusToolbox installed:
+#
+# $ source dev-setup.sh && toolchain_setup [path_to_modustoolbox]
+# 
+# The path to the ModusToolbox is required if installed somewhere
+# else than in the default home path (~/ModusToolbox)
+
+function set_mtb_tools_path {
+    mtb_path=$1
+    if [ "$mtb_path" = "" ]; then
+        mtb_path=~/ModusToolbox
+    fi
+    
+    echo ${mtb_path}/tools_*
+}
+
+function export_path {
+    mtb_tools_path=$(set_mtb_tools_path)
+    export PATH=${mtb_tools_path}/openocd/bin:${mtb_tools_path}/library-manager:${mtb_tools_path}/gcc/bin:$PATH
+}
+
+function install_udev_rules {
+    mtb_tools_path=$(set_mtb_tools_path)
+    ${mtb_tools_path}/openocd/udev_rules/install_rules.sh
+}
+
+function toolchain_setup {
+    mtb_path=$1
+    export_path ${mtb_path}
+    install_udev_rules
+}
+
+function git_add_ssh {
+    ssh_key=$1
+    eval $(ssh-agent -s)
+    ssh-add ${ssh_key}
+}
