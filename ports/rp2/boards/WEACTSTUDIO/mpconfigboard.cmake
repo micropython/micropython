45845acf6d1cba5705f11abd08d8491bcc35c0a5
Jim Mussared <jim.mussared@gmail.com>
2023-08-15 23:12:57 +1000

    rp2: Use uppercase variant names.
    This is to support a future change to add the variant name to the build
    directory and therefore should be the same style as the board name.
    
    This only affects the WEACTSTUDIO board. Also standardises on a convention
    for naming flash-size variants. Normally we would write e.g. 2MiB, but in
    uppercase, it's awkward to write 2MIB, so instead use 2M, 512K, etc for
    variant names, but use 2MiB when not constrained by case (e.g. a regular
    filename).
    
    This work was funded through GitHub Sponsors.
    
    Signed-off-by: Jim Mussared <jim.mussared@gmail.com>

--- ports/rp2/boards/WEACTSTUDIO/mpconfigboard.cmake
+++ ports/rp2/boards/WEACTSTUDIO/mpconfigboard.cmake
@@ -8,17 +8,17 @@ list(APPEND PICO_BOARD_HEADER_DIRS ${MICROPY_BOARD_DIR})
 set(MICROPY_FROZEN_MANIFEST ${MICROPY_BOARD_DIR}/manifest.py)
 
 # Select the 16MB variant as the default
-set(PICO_BOARD "weactstudio_16mb")
+set(PICO_BOARD "weactstudio_16MiB")
 
 # Provide different variants for the downloads page
-if(MICROPY_BOARD_VARIANT STREQUAL "flash_2mb")
-    set(PICO_BOARD "weactstudio_2mb")
+if(MICROPY_BOARD_VARIANT STREQUAL "FLASH_2M")
+    set(PICO_BOARD "weactstudio_2MiB")
 endif()
 
-if(MICROPY_BOARD_VARIANT STREQUAL "flash_4mb")
-    set(PICO_BOARD "weactstudio_4mb")
+if(MICROPY_BOARD_VARIANT STREQUAL "FLASH_4M")
+    set(PICO_BOARD "weactstudio_4MiB")
 endif()
 
-if(MICROPY_BOARD_VARIANT STREQUAL "flash_8mb")
-    set(PICO_BOARD "weactstudio_8mb")
+if(MICROPY_BOARD_VARIANT STREQUAL "FLASH_8M")
+    set(PICO_BOARD "weactstudio_8MiB")
 endif()
