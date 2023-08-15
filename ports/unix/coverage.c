843fdbb2508cd9cbafc5043046e26cd29a8461ee
Jeff Epler <jepler@gmail.com>
2023-08-15 13:25:25 -0500

    Add a missing MP_PROTO_IMPLEMENT

--- ports/unix/coverage.c
+++ ports/unix/coverage.c
@@ -101,6 +101,7 @@ STATIC const mp_rom_map_elem_t rawfile_locals_dict_table[] = {
 STATIC MP_DEFINE_CONST_DICT(rawfile_locals_dict, rawfile_locals_dict_table);
 
 STATIC const mp_stream_p_t fileio_stream_p = {
+    MP_PROTO_IMPLEMENT(MP_QSTR_protocol_stream)
     .read = stest_read,
     .write = stest_write,
     .ioctl = stest_ioctl,
