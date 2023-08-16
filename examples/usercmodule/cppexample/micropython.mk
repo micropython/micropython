9c91448bc2936062ad5136210ad6038c93318898
Jeff Epler <jepler@gmail.com>
2023-08-16 09:05:21 -0500

    Re-add usercmodule sources & endorse new extra_coverage output

---
+++ examples/usercmodule/cppexample/micropython.mk
@@ -0,0 +1,12 @@
+CPPEXAMPLE_MOD_DIR := $(USERMOD_DIR)
+
+# Add our source files to the respective variables.
+SRC_USERMOD += $(CPPEXAMPLE_MOD_DIR)/examplemodule.c
+SRC_USERMOD_CXX += $(CPPEXAMPLE_MOD_DIR)/example.cpp
+
+# Add our module directory to the include path.
+CFLAGS_USERMOD += -I$(CPPEXAMPLE_MOD_DIR)
+CXXFLAGS_USERMOD += -I$(CPPEXAMPLE_MOD_DIR) -std=c++11
+
+# We use C++ features so have to link against the standard library.
+LDFLAGS_USERMOD += -lstdc++
