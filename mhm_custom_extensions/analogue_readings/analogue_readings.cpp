a758a492550ce669cef9687d33f0c52ea9ae2fa0
Ali Abbas <ali@manholemetrics.com>
2023-08-25 18:49:34 +0100

    Placeholder changes until reading is working

--- mhm_custom_extensions/analogue_readings/analogue_readings.cpp
+++ mhm_custom_extensions/analogue_readings/analogue_readings.cpp
@@ -1,15 +1,23 @@
 extern "C" {
 #include <analogue_readings.hpp>
 
-#define MAX_READINGS 16384
-STATIC int cnt = 0;
+mp_obj_t trigger_readings() {
+    // code to do readings
+    // and write to EPROM
+    return mp_const_none;
+}
 
-mp_obj_t make_analogue_readings() {
-    mp_obj_t list = mp_obj_new_list(0, NULL);
+mp_obj_t read_batch(mp_obj_t batch_size, mp_obj_t offset) {
+    int byte_size = mp_obj_get_int(batch_size);
+	int byte_offset = mp_obj_get_int(offset);
+    mp_obj_t list = mp_obj_new_list(size, NULL);
 
-    for (int i = 0; i < MAX_READINGS; i++) {
-        mp_obj_list_append(list, mp_obj_new_int(i));
-        cnt += 1;
+	// some function to read from EPROM
+    // maybe a lambda called read_eprom
+
+    for (int i = 0; i < batch_size; i++) {
+        auto eprom_reading = read_eprom(offset + i);
+        mp_obj_list_append(list, mp_obj_new_int(eprom_reading));
     }
 
     return list;
