b9e39562e903809ce00345d58f19b10554e4de81
enriquezgarc <enriquezgarcia.external@infineon.com>
2023-08-28 10:14:29 +0200

    ports/psoc6/boards/CY8CPROTO-063-BLE: Adding new board files.
    Signed-off-by: enriquezgarc <enriquezgarcia.external@infineon.com>

---
+++ ports/psoc6/boards/CY8CPROTO-063-BLE/manifest.py
@@ -0,0 +1,3 @@
+freeze("$(PORT_DIR)/freeze")
+include("$(MPY_DIR)/extmod/asyncio")
+require("mip")
---
+++ ports/psoc6/boards/CY8CPROTO-063-BLE/mpconfigboard.h
@@ -0,0 +1,5 @@
+// Board and hardware specific configuration
+#define MICROPY_HW_BOARD_NAME                   "CY8CPROTO-063-BLE"
+
+#define MICROPY_PY_NETWORK              (1)
+#define MICROPY_PY_NETWORK_HOSTNAME_DEFAULT     "CY8C-063-BLE"
