2b049355cc8b1914d43070208bd4597a60040379
Ali Abbas <ali@manholemetrics.com>
2023-08-29 00:10:39 +0100

    Added partition, updated custom module property

--- mhm_custom_extensions/analogue_readings/module_config.c
+++ mhm_custom_extensions/analogue_readings/module_config.c
@@ -9,7 +9,7 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_0(make_analogue_readings_obj, make_analogue_readi
 // All identifiers and strings are written as MP_QSTR_xxx and will be
 // optimized to word-sized integers by the build system (interned strings).
 STATIC const mp_rom_map_elem_t analogue_readings_module_globals_table[] = {
-    { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_ultrasound_readings) },
+    { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_analogue_readings) },
     { MP_ROM_QSTR(MP_QSTR_make_analogue_readings), MP_ROM_PTR(&make_analogue_readings_obj) } // need to expose it in the module
 };
 STATIC MP_DEFINE_CONST_DICT(analogue_readings_module_globals, analogue_readings_module_globals_table);
