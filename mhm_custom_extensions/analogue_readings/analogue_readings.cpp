bab9ee9db3fffd07b5be8329d9e1e51ae3c126f2
Ali Abbas <ali@manholemetrics.com>
2023-08-30 13:15:06 +0100

    Fixed analogue readings example custom module (note that this is still unused)

--- mhm_custom_extensions/analogue_readings/analogue_readings.cpp
+++ mhm_custom_extensions/analogue_readings/analogue_readings.cpp
@@ -1,16 +1,9 @@
 extern "C" {
 #include <analogue_readings.hpp>
 
-mp_obj_t trigger_readings() {
-    // code to do readings
-    // and write to EPROM
+mp_obj_t make_analogue_readings() {
+    printf("doing test function\n");
     return mp_const_none;
-}
-
-mp_obj_t read_batch(mp_obj_t batch_size, mp_obj_t offset) {
-    mp_obj_t list = mp_obj_new_list(10, NULL);
-
-    return list;
 }
 
 }
