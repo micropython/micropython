5bf2574900ba493247a6c140a171d86bff86c23b
WallyWare <pi-plates@users.noreply.github.com>
2023-08-31 13:12:39 -0400

    Update mpconfigboard.h
    Signed-off-by: WallyWare <pi-plates@users.noreply.github.com>

--- ports/rp2/boards/PI-PLATES_MICROPI/mpconfigboard.h
+++ ports/rp2/boards/PI-PLATES_MICROPI/mpconfigboard.h
@@ -1,6 +1,6 @@
 // Board and hardware specific configuration
 #define MICROPY_HW_BOARD_NAME                   "MICROpi"
-#define MICROPY_HW_FLASH_STORAGE_BYTES          (15* 1024 * 1024)
+#define MICROPY_HW_FLASH_STORAGE_BYTES          (15 * 1024 * 1024)
 
 #define MICROPY_HW_USB_VID (0x2E8A)
 #define MICROPY_HW_USB_PID (0x1065)
