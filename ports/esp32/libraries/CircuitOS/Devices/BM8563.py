5e443027566e6e3a7a7b807a81229da99f529edb
Emil Gajšak <emil.gajsak@gmail.com>
2023-09-01 15:21:09 +0200

    Added Clockstar BM8563 RTC support

---
+++ ports/esp32/libraries/CircuitOS/Devices/BM8563.py
@@ -0,0 +1,96 @@
+from machine import I2C
+
+
+def bcd2dec(bcd: int) -> int:
+    return ((bcd >> 4) * 10) + (bcd & 0x0f)
+
+
+def dec2bcd(dec: int) -> int:
+    return ((dec // 10) << 4) | (dec % 10)
+
+
+class BM8563:
+    ADDR = const(0x51)
+
+    def __init__(self, i2c: I2C, address: int = ADDR):
+        self._i2c = i2c
+        self._addr = address
+
+    def begin(self) -> bool:
+        try:
+            self._i2c.writeto(self._addr, bytearray([0x0, 0x0, 0x0]))  # clear status registers
+            return True
+        except OSError:
+            return False
+
+    def get_time(self) -> tuple[int, int, int, int, int, int, int]:
+        self._i2c.writeto(self._addr, bytearray([0x02]))
+
+        data = self._i2c.readfrom(self._addr, 7)
+
+        # 0..59
+        sec = bcd2dec(data[0] & 0b01111111)
+
+        # 0..59
+        mins = bcd2dec(data[1] & 0b01111111)
+
+        # 0..23
+        hour = bcd2dec(data[2] & 0b00111111)
+
+        # 1..31
+        day = bcd2dec(data[3] & 0b00111111)
+
+        # 0..6
+        weekday = bcd2dec(data[4] & 0b00000111)
+
+        # 0..11
+        month = bcd2dec(data[5] & 0b00011111) - 1
+
+        '''If century bit set assume it is 2000. Note that it seems 
+        that unlike PCF8563, the BM8563 does NOT automatically 
+        toggle the century bit when year overflows from 99 to 00. '''
+
+        century = 100 if (data[5] & 0b10000000) else 0
+
+        # Number of years since 1900
+        year = bcd2dec(data[6] & 0b11111111) + century + 1900
+
+        time = (year, month, day, weekday, hour, mins, sec)
+
+        return time
+
+    def set_time(self, time: tuple[int, int, int, int, int, int, int]):
+        data = bytearray(7)
+        # (year, month, day, weekday, hour, mins, sec)
+
+        # 0..59
+        data[0] = dec2bcd(time[6]) & 0b01111111
+
+        # 0..59
+        data[1] = dec2bcd(time[5]) & 0b01111111
+
+        # 0..23
+        data[2] = dec2bcd(time[4]) & 0b00111111
+
+        # 1..31
+        data[3] = dec2bcd(time[2]) & 0b00111111
+
+        # 0..6
+        data[4] = dec2bcd(time[3]) & 0b00000111
+
+        # 1..12
+        data[5] = dec2bcd(time[1] + 1) & 0b00011111
+
+        # If 2000 set the century bit.
+        if time[0] >= 100:
+            data[5] |= 0b10000000
+
+        # 0..99
+        data[6] = dec2bcd(time[0] % 100) & 0b11111111
+
+        wdata = bytearray(8)
+        wdata[0] = 0x02
+        for i in range(7):
+            wdata[i + 1] = data[i]
+
+        self._i2c.writeto(self._addr, wdata)
