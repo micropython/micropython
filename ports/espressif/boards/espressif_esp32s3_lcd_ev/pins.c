cff65bbbe29f94552e7c2acc455612d938e024da
Jeff Epler <jepler@gmail.com>
2023-09-15 14:10:32 -0500

    esp32 lcd ev: fix pin assignments

--- ports/espressif/boards/espressif_esp32s3_lcd_ev/pins.c
+++ ports/espressif/boards/espressif_esp32s3_lcd_ev/pins.c
@@ -5,11 +5,11 @@ STATIC const mp_rom_obj_tuple_t tft_r_pins = {
     {&mp_type_tuple},
     5,
     {
-        MP_ROM_PTR(&pin_GPIO10),
-        MP_ROM_PTR(&pin_GPIO11),
-        MP_ROM_PTR(&pin_GPIO12),
-        MP_ROM_PTR(&pin_GPIO13),
-        MP_ROM_PTR(&pin_GPIO14),
+        MP_ROM_PTR(&pin_GPIO1),
+        MP_ROM_PTR(&pin_GPIO2),
+        MP_ROM_PTR(&pin_GPIO42),
+        MP_ROM_PTR(&pin_GPIO41),
+        MP_ROM_PTR(&pin_GPIO40),
     }
 };
 
@@ -30,23 +30,22 @@ STATIC const mp_rom_obj_tuple_t tft_b_pins = {
     {&mp_type_tuple},
     5,
     {
-        MP_ROM_PTR(&pin_GPIO40),
-        MP_ROM_PTR(&pin_GPIO41),
-        MP_ROM_PTR(&pin_GPIO42),
-        MP_ROM_PTR(&pin_GPIO2),
-        MP_ROM_PTR(&pin_GPIO1),
+        MP_ROM_PTR(&pin_GPIO10),
+        MP_ROM_PTR(&pin_GPIO11),
+        MP_ROM_PTR(&pin_GPIO12),
+        MP_ROM_PTR(&pin_GPIO13),
+        MP_ROM_PTR(&pin_GPIO14),
     }
 };
 
 STATIC const mp_rom_map_elem_t tft_table[] = {
     { MP_ROM_QSTR(MP_QSTR_de), MP_ROM_PTR(&pin_GPIO17) },
-    { MP_ROM_QSTR(MP_QSTR_vsync), MP_ROM_PTR(&pin_GPIO46) },
-    { MP_ROM_QSTR(MP_QSTR_hsync), MP_ROM_PTR(&pin_GPIO3) },
+    { MP_ROM_QSTR(MP_QSTR_vsync), MP_ROM_PTR(&pin_GPIO3) },
+    { MP_ROM_QSTR(MP_QSTR_hsync), MP_ROM_PTR(&pin_GPIO46) },
     { MP_ROM_QSTR(MP_QSTR_dclk), MP_ROM_PTR(&pin_GPIO9) },
     { MP_ROM_QSTR(MP_QSTR_red), MP_ROM_PTR(&tft_r_pins) },
     { MP_ROM_QSTR(MP_QSTR_green), MP_ROM_PTR(&tft_g_pins) },
     { MP_ROM_QSTR(MP_QSTR_blue), MP_ROM_PTR(&tft_b_pins) },
-    { MP_ROM_QSTR(MP_QSTR_frequency), MP_ROM_INT(9000000) },
 };
 MP_DEFINE_CONST_DICT(tft_dict, tft_table);
 
@@ -54,11 +53,11 @@ STATIC const mp_rom_map_elem_t board_module_globals_table[] = {
     CIRCUITPYTHON_BOARD_DICT_STANDARD_ITEMS
 
     { MP_ROM_QSTR(MP_QSTR_TFT), MP_ROM_PTR(&tft_dict) },
-    { MP_ROM_QSTR(MP_QSTR_BACKLIGHT), MP_ROM_PTR(&pin_GPIO10) },
 
-    { MP_ROM_QSTR(MP_QSTR_I2S_SCK), MP_ROM_PTR(&pin_GPIO9) },
+    { MP_ROM_QSTR(MP_QSTR_I2S_SCK), MP_ROM_PTR(&pin_GPIO16) },
+    { MP_ROM_QSTR(MP_QSTR_I2S_MCLK), MP_ROM_PTR(&pin_GPIO5) },
     { MP_ROM_QSTR(MP_QSTR_I2S_WS), MP_ROM_PTR(&pin_GPIO7) },
-    { MP_ROM_QSTR(MP_QSTR_I2S_SDO), MP_ROM_PTR(&pin_GPIO15) },
+    { MP_ROM_QSTR(MP_QSTR_I2S_SDO), MP_ROM_PTR(&pin_GPIO6) },
 
     { MP_ROM_QSTR(MP_QSTR_TX), MP_ROM_PTR(&pin_GPIO43) },
     { MP_ROM_QSTR(MP_QSTR_RX), MP_ROM_PTR(&pin_GPIO44) },
@@ -66,14 +65,9 @@ STATIC const mp_rom_map_elem_t board_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_SCL), MP_ROM_PTR(DEFAULT_I2C_BUS_SCL) },
     { MP_ROM_QSTR(MP_QSTR_SDA), MP_ROM_PTR(DEFAULT_I2C_BUS_SDA) },
 
-    { MP_ROM_QSTR(MP_QSTR_MOSI), MP_ROM_PTR(&pin_GPIO11) },
-    { MP_ROM_QSTR(MP_QSTR_MISO), MP_ROM_PTR(&pin_GPIO13) },
-    { MP_ROM_QSTR(MP_QSTR_SCK), MP_ROM_PTR(&pin_GPIO12) },
-
     // boot mode button can be used in SW as well
     { MP_ROM_QSTR(MP_QSTR_BUTTON), MP_ROM_PTR(&pin_GPIO0) },
 
     { MP_ROM_QSTR(MP_QSTR_I2C), MP_ROM_PTR(&board_i2c_obj) },
-    { MP_ROM_QSTR(MP_QSTR_SPI), MP_ROM_PTR(&board_spi_obj) },
 };
 MP_DEFINE_CONST_DICT(board_module_globals, board_module_globals_table);
