75ba82cdb02e9f9eb2e5b2f57b78b33beb882be5
Jeff Epler <jepler@gmail.com>
2023-09-18 10:35:36 -0500

    lcd_ev: add timings; rename TFT -> TFT_PINS

--- ports/espressif/boards/espressif_esp32s3_lcd_ev/pins.c
+++ ports/espressif/boards/espressif_esp32s3_lcd_ev/pins.c
@@ -39,7 +39,7 @@ STATIC const mp_rom_obj_tuple_t tft_b_pins = {
     }
 };
 
-STATIC const mp_rom_map_elem_t tft_table[] = {
+STATIC const mp_rom_map_elem_t tft_pins_table[] = {
     { MP_ROM_QSTR(MP_QSTR_de), MP_ROM_PTR(&pin_GPIO17) },
     { MP_ROM_QSTR(MP_QSTR_vsync), MP_ROM_PTR(&pin_GPIO3) },
     { MP_ROM_QSTR(MP_QSTR_hsync), MP_ROM_PTR(&pin_GPIO46) },
@@ -48,12 +48,31 @@ STATIC const mp_rom_map_elem_t tft_table[] = {
     { MP_ROM_QSTR(MP_QSTR_green), MP_ROM_PTR(&tft_g_pins) },
     { MP_ROM_QSTR(MP_QSTR_blue), MP_ROM_PTR(&tft_b_pins) },
 };
-MP_DEFINE_CONST_DICT(tft_dict, tft_table);
+MP_DEFINE_CONST_DICT(tft_pins_dict, tft_pins_table);
+
+STATIC const mp_rom_map_elem_t tft_timings_table[] = {
+    { MP_ROM_QSTR(MP_QSTR_frequency), MP_ROM_INT(6500000) }, // nominal 16MHz, but display is unstable/tears at that frequency
+    { MP_ROM_QSTR(MP_QSTR_width), MP_ROM_INT(480) },
+    { MP_ROM_QSTR(MP_QSTR_height), MP_ROM_INT(480) },
+    { MP_ROM_QSTR(MP_QSTR_hsync_pulse_width), MP_ROM_INT(13) },
+    { MP_ROM_QSTR(MP_QSTR_hsync_front_porch), MP_ROM_INT(20) },
+    { MP_ROM_QSTR(MP_QSTR_hsync_back_porch), MP_ROM_INT(40) },
+    { MP_ROM_QSTR(MP_QSTR_hsync_idle_low), MP_ROM_FALSE },
+    { MP_ROM_QSTR(MP_QSTR_vsync_pulse_width), MP_ROM_INT(15) },
+    { MP_ROM_QSTR(MP_QSTR_vsync_front_porch), MP_ROM_INT(20) },
+    { MP_ROM_QSTR(MP_QSTR_vsync_back_porch), MP_ROM_INT(40) },
+    { MP_ROM_QSTR(MP_QSTR_vsync_idle_low), MP_ROM_FALSE },
+    { MP_ROM_QSTR(MP_QSTR_de_idle_high), MP_ROM_FALSE },
+    { MP_ROM_QSTR(MP_QSTR_pclk_active_high), MP_ROM_FALSE },
+    { MP_ROM_QSTR(MP_QSTR_pclk_idle_high), MP_ROM_FALSE },
+};
+MP_DEFINE_CONST_DICT(tft_timings_dict, tft_timings_table);
 
 STATIC const mp_rom_map_elem_t board_module_globals_table[] = {
     CIRCUITPYTHON_BOARD_DICT_STANDARD_ITEMS
 
-    { MP_ROM_QSTR(MP_QSTR_TFT), MP_ROM_PTR(&tft_dict) },
+    { MP_ROM_QSTR(MP_QSTR_TFT_PINS), MP_ROM_PTR(&tft_pins_dict) },
+    { MP_ROM_QSTR(MP_QSTR_TFT_TIMINGS), MP_ROM_PTR(&tft_timings_dict) },
 
     { MP_ROM_QSTR(MP_QSTR_I2S_SCK), MP_ROM_PTR(&pin_GPIO16) },
     { MP_ROM_QSTR(MP_QSTR_I2S_MCLK), MP_ROM_PTR(&pin_GPIO5) },
