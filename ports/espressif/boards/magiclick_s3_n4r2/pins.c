9284d25ad49029051b7c72c7253ab5afb01a6364
MakerM0 <117961102+MakerM0@users.noreply.github.com>
2023-09-24 22:28:48 +0800

    Update pins.c

--- ports/espressif/boards/magiclick_s3_n4r2/pins.c
+++ ports/espressif/boards/magiclick_s3_n4r2/pins.c
@@ -1,7 +1,5 @@
 #include "shared-bindings/board/__init__.h"
 
-CIRCUITPY_BOARD_BUS_SINGLETON(stemma_i2c, i2c, 1)
-
 STATIC const mp_rom_map_elem_t board_module_globals_table[] = {
     CIRCUITPYTHON_BOARD_DICT_STANDARD_ITEMS
 
@@ -62,7 +60,6 @@ STATIC const mp_rom_map_elem_t board_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_BACKLIGHT), MP_ROM_PTR(&pin_GPIO37) },
     { MP_ROM_QSTR(MP_QSTR_LCD_RESET), MP_ROM_PTR(&pin_GPIO10) },
     { MP_ROM_QSTR(MP_QSTR_I2C), MP_ROM_PTR(&board_i2c_obj) },
-    { MP_ROM_QSTR(MP_QSTR_STEMMA_I2C), MP_ROM_PTR(&board_stemma_i2c_obj) },
     { MP_ROM_QSTR(MP_QSTR_SPI), MP_ROM_PTR(&board_spi_obj) },
     { MP_ROM_QSTR(MP_QSTR_UART), MP_ROM_PTR(&board_uart_obj) },
 };
