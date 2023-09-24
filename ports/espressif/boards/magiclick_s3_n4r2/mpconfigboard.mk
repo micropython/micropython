9d906805217d7515466e28e93775daaef5cfaac7
MakerM0 <117961102+MakerM0@users.noreply.github.com>
2023-09-24 20:48:30 +0800

    Add board def for MagiClick S3
    The board is related here: https://github.com/MakerM0/MagiClick-esp32s3
    
    VID&PID can be found here: https://github.com/espressif/usb-pids

---
+++ ports/espressif/boards/magiclick_s3_n4r2/mpconfigboard.mk
@@ -0,0 +1,31 @@
+USB_VID = 0x303A
+USB_PID = 0x81AA
+
+USB_PRODUCT = "MagiClick S3 n4r2"
+USB_MANUFACTURER = "MakerM0"
+
+IDF_TARGET = esp32s3
+
+CIRCUITPY_ESP_FLASH_MODE = dio
+CIRCUITPY_ESP_FLASH_FREQ = 80m
+CIRCUITPY_ESP_FLASH_SIZE = 8MB
+
+CIRCUITPY_ESPCAMERA = 0
+CIRCUITPY_DISPLAYIO = 1
+CIRCUITPY_GIFIO = 1
+ 
+
+
+# # Include these Python libraries in firmware.
+# FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_NeoPixel
+# FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Display_Text
+# FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Display_Shapes
+# FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_LSM6DS
+# FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_ST7789
+# FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Ticks
+# FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_asyncio
+# FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_HID
+# FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_MIDI
+# FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Register
+# FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_FakeRequests
+# FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Requests
