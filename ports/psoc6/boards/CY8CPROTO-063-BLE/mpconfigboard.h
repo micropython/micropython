2b4abcc6aec7ea15444a8248b4d01afaa2cc830a
enriquezgarc <enriquezgarcia.external@infineon.com>
2023-09-26 09:34:26 +0200

    ports/psoc6: Removed 063-BLE default disabled config param.
    Signed-off-by: enriquezgarc <enriquezgarcia.external@infineon.com>

--- ports/psoc6/boards/CY8CPROTO-063-BLE/mpconfigboard.h
+++ ports/psoc6/boards/CY8CPROTO-063-BLE/mpconfigboard.h
@@ -6,8 +6,4 @@
 
 #define MICROPY_GC_HEAP_SIZE                    (64 * 1024) // 64 KB
 
-#define MICROPY_PY_HASHLIB                      (0)
-#define MICROPY_PY_HASHLIB_MD5                  (0)
-#define MICROPY_PY_HASHLIB_SHA1                 (0)
-#define MICROPY_PY_HASHLIB_SHA256               (0)
 
