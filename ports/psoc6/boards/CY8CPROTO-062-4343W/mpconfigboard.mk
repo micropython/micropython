6614ecc19f2544e3d6f0d79b35235d8aad1dbfbd
NikhitaR-IFX <Nikhita.Rajasekhar@infineon.com>
2023-09-26 10:43:03 +0530

    ports/psoc6: Moving pin package var  to board specifice makefile.
    Signed-off-by: NikhitaR-IFX <Nikhita.Rajasekhar@infineon.com>

--- ports/psoc6/boards/CY8CPROTO-062-4343W/mpconfigboard.mk
+++ ports/psoc6/boards/CY8CPROTO-062-4343W/mpconfigboard.mk
@@ -6,3 +6,6 @@ MICROPY_PY_SSL = 1
 MICROPY_PSOC6_SSL_MBEDTLS = 1
 MICROPY_PY_EXT_FLASH = 1
 BOARD_VERSION=release-v4.0.0
+
+# Variables to support make-pins
+PIN_PACKAGE_FILE = cyhal_psoc6_02_124_bga.h
--- ports/psoc6/boards/CY8CPROTO-063-BLE/mpconfigboard.mk
+++ ports/psoc6/boards/CY8CPROTO-063-BLE/mpconfigboard.mk
@@ -2,3 +2,6 @@ FROZEN_MANIFEST ?= boards/manifest.py
 MICROPY_PY_SSL = 0
 MICROPY_PSOC6_SSL_MBEDTLS = 0
 BOARD_VERSION=release-v4.2.0
+
+# Variables to support make-pins
+PIN_PACKAGE_FILE = cyhal_psoc6_01_116_bga_ble.h
