e40abda1bc42e8154b0818c8a747664e775ac407
Jeff Epler <jepler@gmail.com>
2023-09-27 21:34:24 -0500

    ci_set_matrix: Don't use .get(), it hides errors
    Variables that are explicitly fetched always have a value, even if it
    is the empty value. However, using .get() hid the error that
    shared_bindings_matrix wasn't fetching SRC_SUPERVISOR.

--- tools/ci_set_matrix.py
+++ tools/ci_set_matrix.py
@@ -189,14 +189,14 @@ def set_boards(build_all: bool):
 
                     # Check frozen files to see if they are in each board
                     if file.startswith("frozen"):
-                        if file in settings.get("FROZEN_MPY_DIRS", ""):
+                        if file in settings["FROZEN_MPY_DIRS"]:
                             boards_to_build.add(board)
                             continue
 
                     # Check supervisor files
                     # This is useful for limiting workflow changes to the relevant boards
                     if file.startswith("supervisor"):
-                        if file in settings.get("SRC_SUPERVISOR", ""):
+                        if file in settings["SRC_SUPERVISOR"]:
                             boards_to_build.add(board)
                             continue
 
