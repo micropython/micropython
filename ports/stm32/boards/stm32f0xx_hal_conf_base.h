d910157fa21ccc3358d1515837155e02988b8ad7
Damien George <damien@micropython.org>
2023-09-28 16:24:45 +1000

    stm32/boards: Move includes to after defines in all hal_conf.h files.
    The include of HAL headers should come after the HAL configuration defines,
    so that the headers can see whether the defines were made or not, to
    provide defaults and configure various things.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/stm32/boards/stm32f0xx_hal_conf_base.h
+++ ports/stm32/boards/stm32f0xx_hal_conf_base.h
@@ -26,31 +26,6 @@
 #ifndef MICROPY_INCLUDED_STM32F0XX_HAL_CONF_BASE_H
 #define MICROPY_INCLUDED_STM32F0XX_HAL_CONF_BASE_H
 
-// Include various HAL modules for convenience
-#include "stm32f0xx_hal_dma.h"
-#include "stm32f0xx_hal_adc.h"
-#include "stm32f0xx_hal_can.h"
-#include "stm32f0xx_hal_cortex.h"
-#include "stm32f0xx_hal_crc.h"
-#include "stm32f0xx_hal_dac.h"
-#include "stm32f0xx_hal_flash.h"
-#include "stm32f0xx_hal_gpio.h"
-#include "stm32f0xx_hal_i2c.h"
-#include "stm32f0xx_hal_i2s.h"
-#include "stm32f0xx_hal_iwdg.h"
-#include "stm32f0xx_hal_pcd.h"
-#include "stm32f0xx_hal_pwr.h"
-#include "stm32f0xx_hal_rcc.h"
-#include "stm32f0xx_hal_rtc.h"
-#include "stm32f0xx_hal_spi.h"
-#include "stm32f0xx_hal_tim.h"
-#include "stm32f0xx_hal_uart.h"
-#include "stm32f0xx_hal_usart.h"
-#include "stm32f0xx_hal_wwdg.h"
-#include "stm32f0xx_ll_adc.h"
-#include "stm32f0xx_ll_rtc.h"
-#include "stm32f0xx_ll_usart.h"
-
 // Enable various HAL modules
 #define HAL_MODULE_ENABLED
 #define HAL_ADC_MODULE_ENABLED
@@ -86,6 +61,31 @@
 #define PREFETCH_ENABLE 1
 #define USE_RTOS        0
 #define USE_SPI_CRC     1
+
+// Include various HAL modules for convenience
+#include "stm32f0xx_hal_dma.h"
+#include "stm32f0xx_hal_adc.h"
+#include "stm32f0xx_hal_can.h"
+#include "stm32f0xx_hal_cortex.h"
+#include "stm32f0xx_hal_crc.h"
+#include "stm32f0xx_hal_dac.h"
+#include "stm32f0xx_hal_flash.h"
+#include "stm32f0xx_hal_gpio.h"
+#include "stm32f0xx_hal_i2c.h"
+#include "stm32f0xx_hal_i2s.h"
+#include "stm32f0xx_hal_iwdg.h"
+#include "stm32f0xx_hal_pcd.h"
+#include "stm32f0xx_hal_pwr.h"
+#include "stm32f0xx_hal_rcc.h"
+#include "stm32f0xx_hal_rtc.h"
+#include "stm32f0xx_hal_spi.h"
+#include "stm32f0xx_hal_tim.h"
+#include "stm32f0xx_hal_uart.h"
+#include "stm32f0xx_hal_usart.h"
+#include "stm32f0xx_hal_wwdg.h"
+#include "stm32f0xx_ll_adc.h"
+#include "stm32f0xx_ll_rtc.h"
+#include "stm32f0xx_ll_usart.h"
 
 // HAL parameter assertions are disabled
 #define assert_param(expr) ((void)0)
