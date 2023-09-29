d7a27d8d2bbdffe324110cc0fa02d0fed8f42e21
James Ward <james@notjam.es>
2023-09-29 02:19:34 -0400

    heltec esp32s3 wifi lora v3: new espressif board

---
+++ ports/espressif/boards/heltec_esp32s3_wifi_lora_v3/mpconfigboard.mk
@@ -0,0 +1,27 @@
+USB_VID = 0x303A
+USB_PID = 0x1001
+
+CIRCUITPY_CREATOR_ID = 0x19960000
+CIRCUITPY_CREATION_ID = 0x00242705
+
+USB_PRODUCT = "ESP32-S3-WIFI-LoRa-V3"
+USB_MANUFACTURER = "Heltec"
+
+IDF_TARGET = esp32s3
+
+# This board doesn't have USB by default, it
+# instead uses a CP2102 USB-to-Serial chip
+CIRCUITPY_USB = 0
+CIRCUITPY_ESP_USB_SERIAL_JTAG = 0
+
+CIRCUITPY_ESP_FLASH_MODE = qio
+CIRCUITPY_ESP_FLASH_FREQ = 80m
+CIRCUITPY_ESP_FLASH_SIZE = 8MB
+
+CIRCUITPY_ESPCAMERA = 0
+
+CIRCUITPY_DISPLAYIO = 1
+
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Display_Shapes
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Display_Text
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_RFM9x
