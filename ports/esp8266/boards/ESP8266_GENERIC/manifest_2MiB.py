c2e9a6f2a566ce4e1045f3ab93969a1b43543c6c
Damien George <damien@micropython.org>
2023-10-05 13:25:55 +1100

    esp8266/boards/ESP8266_GENERIC: Remove urllib from the 2MiB manifest.
    No other network-enabled board has urllib.urequest frozen in to the
    firmware, and esp8266 is relatively low on flash, so remove this module.
    
    And (u)requests is already included by bundle-networking.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/esp8266/boards/ESP8266_GENERIC/manifest_2MiB.py
+++ ports/esp8266/boards/ESP8266_GENERIC/manifest_2MiB.py
@@ -10,10 +10,6 @@ require("ssd1306")
 # micropython-lib: file utilities
 require("upysh")
 
-# micropython-lib: requests
-require("urequests")
-require("urllib.urequest")
-
 # micropython-lib: umqtt
 require("umqtt.simple")
 require("umqtt.robust")
