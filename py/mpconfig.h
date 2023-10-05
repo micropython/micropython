e00a144008f368df878c12606fdbf651af2a1dc0
Damien George <damien@micropython.org>
2023-10-06 10:32:07 +1100

    all: Bump version to 1.21.0.
    Signed-off-by: Damien George <damien@micropython.org>

--- py/mpconfig.h
+++ py/mpconfig.h
@@ -28,7 +28,7 @@
 
 // Current version of MicroPython
 #define MICROPY_VERSION_MAJOR 1
-#define MICROPY_VERSION_MINOR 20
+#define MICROPY_VERSION_MINOR 21
 #define MICROPY_VERSION_MICRO 0
 
 // Combined version as a 32-bit number for convenience
