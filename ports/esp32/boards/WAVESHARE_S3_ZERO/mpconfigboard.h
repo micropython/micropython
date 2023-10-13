526acf396840ecddc763d9d9e347de9c28065642
siuwah <zhongsiuwah@gmail.com>
2023-10-13 20:37:06 +0800

    ports/esp32/boards: add Waveshare ESP32-S3-Zero board.

--- ports/esp32/boards/WAVESHARE_S3_ZERO/mpconfigboard.h
+++ ports/esp32/boards/WAVESHARE_S3_ZERO/mpconfigboard.h
@@ -3,8 +3,3 @@
 #define MICROPY_HW_BOARD_NAME               "ESP32-S3-Zero module"
 #endif
 #define MICROPY_HW_MCU_NAME                 "ESP32S3"
-
-#define MICROPY_PY_MACHINE_DAC              (0)
-
-// Enable USB REPL for modules that use native USB.
-#define MICROPY_HW_ENABLE_USBDEV	    (1)
