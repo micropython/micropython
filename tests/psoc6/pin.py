4981400036261fca4f365f5d88a4e2c5fcf79826
enriquezgarc <enriquezgarcia.external@infineon.com>
2023-10-18 16:18:19 +0200

    ports/psoc6/pin.py: Modified for new board support.
    Signed-off-by: enriquezgarc <enriquezgarcia.external@infineon.com>

--- tests/psoc6/pin.py
+++ tests/psoc6/pin.py
@@ -9,15 +9,14 @@ if "CY8CPROTO-062-4343W" in machine:
     pin1_name = "P13_7"
     pin2_name = "P0_4"
 elif "CY8CPROTO-063-BLE" in machine:
-    print("SKIP")
-    raise SystemExit
+    pin1_name = "P6_3"
+    pin2_name = "P0_4"
 
 # Tests for Output pin
 # Instantiate output pin for led operation
 # Constr
 p1 = Pin(pin1_name)
 p1.init(Pin.OUT)
-print(p1)  # Pin:111 or P13_7, Mode=OUT, Pull=None, Value=1
 
 p1.value(0)  # Led glows
 print(p1.value())  # None
@@ -31,6 +30,5 @@ print(p1.value())  # None
 # Tests for Input pin
 # Instantiate input pin for button operation
 p2 = Pin(pin2_name, Pin.IN)
-print(p2)  # Pin:4 or P0_4, Mode=IN, Pull=None, Value=1
 p2.value()
 print(p2.value())  # 1 - button is released
