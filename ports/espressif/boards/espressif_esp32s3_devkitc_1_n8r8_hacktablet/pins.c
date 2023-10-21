9d1b9b3c0d1fc594cb92b2d0765c3ab687c627ff
RetiredWizard <github@retiredwizard.com>
2023-10-21 15:46:05 -0400

    Add/Remove correct board level devices

--- ports/espressif/boards/espressif_esp32s3_devkitc_1_n8r8_hacktablet/pins.c
+++ ports/espressif/boards/espressif_esp32s3_devkitc_1_n8r8_hacktablet/pins.c
@@ -117,6 +117,8 @@ STATIC const mp_rom_map_elem_t board_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_TX), MP_ROM_PTR(&pin_GPIO43) },
     { MP_ROM_QSTR(MP_QSTR_RX), MP_ROM_PTR(&pin_GPIO44) },
 
+    { MP_ROM_QSTR(MP_QSTR_I2C), MP_ROM_PTR(&board_i2c_obj) },
+    { MP_ROM_QSTR(MP_QSTR_STEMMA_I2C), MP_ROM_PTR(&board_i2c_obj) },
     { MP_ROM_QSTR(MP_QSTR_UART), MP_ROM_PTR(&board_uart_obj) },
 };
 MP_DEFINE_CONST_DICT(board_module_globals, board_module_globals_table);
