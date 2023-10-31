c7222d1667bd6aafc83e00d272211b76a4a96935
Alessandro Gatti <a.gatti@frob.it>
2023-10-31 13:28:25 +0100

    esp32: Use better build settings for ESP32-C3.
    ESP32-C3 is not Xtensa-based, so build settings are now tailored a bit
    better following that fact.  ESP-IDF 5.x already adds architecture-specific
    modules by itself so there is no need to specify either the `xtensa` or the
    `riscv` module in the build settings.
    
    Signed-off-by: Alessandro Gatti <a.gatti@frob.it>

--- ports/esp32/esp32_common.cmake
+++ ports/esp32/esp32_common.cmake
@@ -134,7 +134,6 @@ list(APPEND IDF_COMPONENTS
     spi_flash
     ulp
     vfs
-    xtensa
 )
 
 # Register the main IDF component.
@@ -161,7 +160,9 @@ idf_component_register(
 set(MICROPY_TARGET ${COMPONENT_TARGET})
 
 # Define mpy-cross flags, for use with frozen code.
+if(NOT IDF_TARGET STREQUAL "esp32c3")
 set(MICROPY_CROSS_FLAGS -march=xtensawin)
+endif()
 
 # Set compile options for this port.
 target_compile_definitions(${MICROPY_TARGET} PUBLIC
