861a84742178291845a3b56cde2db46f1c549efa
Alex Tremblay <alex.tremblay@utoronto.ca>
2023-11-02 09:10:16 -0400

    Update manufacturer from Keeb.io to Maple Computing

--- ports/raspberrypi/boards/maple_elite_pi/mpconfigboard.h
+++ ports/raspberrypi/boards/maple_elite_pi/mpconfigboard.h
@@ -1,4 +1,4 @@
-#define MICROPY_HW_BOARD_NAME "Keeb.io Elite-Pi"
+#define MICROPY_HW_BOARD_NAME "Maple Computing Elite-Pi"
 #define MICROPY_HW_MCU_NAME "rp2040"
 
 #define DEFAULT_I2C_BUS_SCL (&pin_GPIO13)
