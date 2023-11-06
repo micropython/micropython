00a0adf1c5b161d5fe8f82d32ad71d2a01fa2496
NikhitaR-IFX <Nikhita.Rajasekhar@infineon.com>
2023-11-06 17:50:23 +0530

    tests/psoc6/dut: Fixing typo.
    Signed-off-by: NikhitaR-IFX <Nikhita.Rajasekhar@infineon.com>

--- tests/psoc6/dut/adc.py
+++ tests/psoc6/dut/adc.py
@@ -20,7 +20,7 @@ elif "CY8CPROTO-063-BLE" in machine:
 adc0 = ADC(adc_pin0, sample_ns=1000)
 adc1 = ADC(adc_pin1, sample_ns=1000)
 
-print("Volatge (in microvolts) on pin", adc_pin0, "is max: ", adc0.read_uv() > 1000000)
-print("Volatge (raw count) on pin", adc_pin0, "is max: ", adc0.read_u16() > 500)
-print("Volatge (in microvolts) on pin", adc_pin1, "is max: ", adc1.read_uv() < 1000000)
-print("Volatge (raw count) on pin", adc_pin1, "is max: ", adc1.read_u16() < 500)
+print("Voltage (in microvolts) on pin", adc_pin0, "is max: ", adc0.read_uv() > 1000000)
+print("Voltage (raw count) on pin", adc_pin0, "is max: ", adc0.read_u16() > 500)
+print("Voltage (in microvolts) on pin", adc_pin1, "is max: ", adc1.read_uv() < 1000000)
+print("Voltage (raw count) on pin", adc_pin1, "is max: ", adc1.read_u16() < 500)
