69a95d3e948e580779f3e588d754620ccb6a4a54
enriquezgarc <enriquezgarcia.external@infineon.com>
2023-11-06 16:19:28 +0100

    docs/conf.py: Minor formatting.
    Signed-off-by: enriquezgarc <enriquezgarcia.external@infineon.com>

--- docs/conf.py
+++ docs/conf.py
@@ -46,13 +46,13 @@ html_context = {
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinxcontrib.jquery',
-    'sphinx_tabs.tabs',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx.ext.coverage",
+    "sphinxcontrib.jquery",
+    "sphinx_tabs.tabs",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
