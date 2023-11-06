d4808f91bbae3d07a4f720a60bb32bbcfbf73d10
NikhitaR-IFX <Nikhita.Rajasekhar@infineon.com>
2023-11-06 15:02:22 +0530

    tests/psoc6/dut: Codeformatting i2c tests.
    Signed-off-by: NikhitaR-IFX <Nikhita.Rajasekhar@infineon.com>

--- tests/psoc6/dut/i2c_hard.py
+++ tests/psoc6/dut/i2c_hard.py
@@ -13,16 +13,16 @@ elif "CY8CPROTO-063-BLE" in machine:
     scl_pin = "P6_4"
     sda_pin = "P6_5"
 
-i2c = I2C(0,scl=scl_pin,sda=sda_pin,freq=400000)
+i2c = I2C(0, scl=scl_pin, sda=sda_pin, freq=400000)
 addr = i2c.scan()
-master_data = b'\x01\x00\x17'
+master_data = b"\x01\x00\x17"
 counter = 0
 
-while(counter!=5):
+while counter != 5:
     i2c.writeto(addr[0], master_data)
     time.sleep(1)
     slave_data = i2c.readfrom(addr[0], 3)
     print("Slave sent data: ", slave_data)
     master_data = slave_data
     time.sleep(1)
-    counter = counter+1
+    counter = counter + 1
--- tests/psoc6/dut/i2c_soft.py
+++ tests/psoc6/dut/i2c_soft.py
@@ -14,15 +14,14 @@ elif "CY8CPROTO-063-BLE" in machine:
 
 i2c = SoftI2C(scl=scl_pin, sda=sda_pin, freq=400000)
 addr = i2c.scan()
-master_data = b'\x01\x00\x17'
+master_data = b"\x01\x00\x17"
 counter = 0
 
-while(counter!=5):
+while counter != 5:
     i2c.writeto(addr[0], master_data)
     time.sleep(1)
     slave_data = i2c.readfrom(addr[0], 3)
     print("Slave sent data: ", slave_data)
     master_data = slave_data
     time.sleep(1)
-    counter = counter+1
-
+    counter = counter + 1
