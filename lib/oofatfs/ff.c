2e72ea99e43a01f5bc40b74bbc911c12d229e9ab
eightycc <eightycc@gmail.com>
2023-11-07 13:51:18 -0800

    Update lib/oofatfs/ff.c
    Co-authored-by: Dan Halbert <halbert@halwitz.org>

--- lib/oofatfs/ff.c
+++ lib/oofatfs/ff.c
@@ -5422,7 +5422,8 @@ FRESULT f_mkfs (
 )
 {
     const UINT n_fats = 1;      /* Number of FATs for FAT/FAT32 volume (1 or 2) */
-    UINT n_rootdir = 512;       /* Number of root directory entries for FAT volume */
+    // CIRCUITPY-CHANGE: Make number of root directory entries changeable. See below.
+    UINT n_rootdir = 512;       /* Default number of root directory entries for FAT volume */
     static const WORD cst[] = {1, 4, 16, 64, 256, 512, 0};  /* Cluster size boundary for FAT volume (4Ks unit) */
 #if FF_MKFS_FAT32
     static const WORD cst32[] = {1, 2, 4, 8, 16, 32, 0};    /* Cluster size boundary for FAT32 volume (128Ks unit) */
