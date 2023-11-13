388807e7253bc4cd223e73e23378fecae08c7095
Jacques Supcik <jacques@supcik.net>
2023-11-13 22:46:39 +0100

    fix typo

--- ports/espressif/boards/oxocard_connect/mpconfigboard.h
+++ ports/espressif/boards/oxocard_connect/mpconfigboard.h
@@ -26,7 +26,7 @@
 
 // Micropython setup
 
-#define MICROPY_HW_BOARD_NAME "Oxocard Connnect"
+#define MICROPY_HW_BOARD_NAME "Oxocard Connect"
 #define MICROPY_HW_MCU_NAME "ESP32"
 
 #define MICROPY_HW_LED_STATUS (&pin_GPIO2)
