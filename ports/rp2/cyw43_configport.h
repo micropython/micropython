f7a563db31603cf52acb1e22ab6adb03b0c6d063
Damien George <damien@micropython.org>
2023-11-14 13:26:27 +1100

    rp2/mpnetworkport: Rework lwIP polling to use soft_timer.
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/rp2/cyw43_configport.h
+++ ports/rp2/cyw43_configport.h
@@ -104,11 +104,9 @@ void cyw43_post_poll_hook(void);
 extern volatile int cyw43_has_pending;
 
 static inline void cyw43_yield(void) {
-    uint32_t my_interrupts = save_and_disable_interrupts();
     if (!cyw43_has_pending) {
-        __WFI();
+        best_effort_wfe_or_timeout(make_timeout_time_ms(1));
     }
-    restore_interrupts(my_interrupts);
 }
 
 static inline void cyw43_delay_us(uint32_t us) {
