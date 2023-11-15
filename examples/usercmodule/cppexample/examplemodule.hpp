95741c8f23224986bfcc578ce68ccaacd7ea9cf7
mo-FEAR <150607140+mo-FEAR@users.noreply.github.com>
2023-11-15 19:24:31 +0100

    Update and rename examplemodule.h to examplemodule.hpp
    migrated header file to CXX
    
    Signed-off-by: mo-FEAR <150607140+mo-FEAR@users.noreply.github.com>

---
+++ examples/usercmodule/cppexample/examplemodule.hpp
@@ -0,0 +1,7 @@
+// Include MicroPython API.
+extern "C" { // so imported symbols don't get mangled, otherwise linker undefined reference errors
+  #include "py/runtime.h"
+}
+
+// Declare the function we'll make available in Python as cppexample.cppfunc().
+extern mp_obj_t cppusermod_func(const mp_obj_t o_in);
