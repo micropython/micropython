6117aa686f5faac6692f0def91be8e40a696aca3
Sebastian Romero <s.romero@arduino.cc>
2023-11-15 11:38:01 +0100

    renesas-ra/boards/ARDUINO_PORTENTA_C33: Fix incorrect I2C pins.
    Also change the number of the interface to 0 as it's connected to IIC0.
    
    Signed-off-by: Sebastian Romero <s.romero@arduino.cc>

--- ports/renesas-ra/boards/ARDUINO_PORTENTA_C33/mpconfigboard.h
+++ ports/renesas-ra/boards/ARDUINO_PORTENTA_C33/mpconfigboard.h
@@ -63,8 +63,8 @@ void PORTENTA_C33_board_enter_bootloader(void);
 #endif
 
 // I2C
-#define MICROPY_HW_I2C2_SCL         (pin_P407)
-#define MICROPY_HW_I2C2_SDA         (pin_P408)
+#define MICROPY_HW_I2C0_SCL         (pin_P408)
+#define MICROPY_HW_I2C0_SDA         (pin_P407)
 
 // SPI
 #define MICROPY_HW_SPI1_SSL         (pin_P104)
