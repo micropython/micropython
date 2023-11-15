22c54c02546b6f55dcccd04fd932a7c9b01f081a
mo-FEAR <150607140+mo-FEAR@users.noreply.github.com>
2023-11-15 21:59:29 +0100

    Update examplemodule.hpp
    added include guards
    
    Signed-off-by: mo-FEAR <150607140+mo-FEAR@users.noreply.github.com>

--- examples/usercmodule/cppexample/examplemodule.hpp
+++ examples/usercmodule/cppexample/examplemodule.hpp
@@ -1,3 +1,6 @@
+#ifndef EXAMPLEMODULE_HPP
+#define EXAMPLEMODULE_HPP
+
 // Include MicroPython API.
 extern "C" { // so imported symbols don't get mangled, otherwise linker undefined reference errors
   #include "py/runtime.h"
@@ -5,3 +8,5 @@ extern "C" { // so imported symbols don't get mangled, otherwise linker undefine
 
 // Declare the function we'll make available in Python as cppexample.cppfunc().
 extern mp_obj_t cppusermod_func(const mp_obj_t o_in);
+
+#endif
