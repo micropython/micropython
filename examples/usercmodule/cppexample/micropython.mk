472ea9949b1d51fb649fb8b32de0472723b449e2
mo-FEAR <150607140+mo-FEAR@users.noreply.github.com>
2023-11-15 19:32:07 +0100

    Update micropython.mk
    Signed-off-by: mo-FEAR <150607140+mo-FEAR@users.noreply.github.com>

--- examples/usercmodule/cppexample/micropython.mk
+++ examples/usercmodule/cppexample/micropython.mk
@@ -1,11 +1,12 @@
 CPPEXAMPLE_MOD_DIR := $(USERMOD_DIR)
 
 # Add our source files to the respective variables.
-SRC_USERMOD += $(CPPEXAMPLE_MOD_DIR)/examplemodule.c
-SRC_USERMOD_CXX += $(CPPEXAMPLE_MOD_DIR)/example.cpp
+# If you want to mix C & C++ sources in the same folder, uncomment the following line, and add the sources here and in micropython.cmake
+#SRC_USERMOD += $(CPPEXAMPLE_MOD_DIR)/examplemodule.c
+SRC_USERMOD_CXX += $(CPPEXAMPLE_MOD_DIR)/examplemodule.cpp
 
 # Add our module directory to the include path.
-CFLAGS_USERMOD += -I$(CPPEXAMPLE_MOD_DIR)
+#CFLAGS_USERMOD += -I$(CPPEXAMPLE_MOD_DIR)
 CXXFLAGS_USERMOD += -I$(CPPEXAMPLE_MOD_DIR) -std=c++11
 
 # We use C++ features so have to link against the standard library.
