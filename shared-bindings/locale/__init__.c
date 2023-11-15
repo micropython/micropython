43e7fcb2279a6d28886365bcd8277a63266346dc
Jeff Epler <jepler@gmail.com>
2023-11-15 07:22:11 -0600

    Fix the locale module docstring

--- shared-bindings/locale/__init__.c
+++ shared-bindings/locale/__init__.c
@@ -27,9 +27,16 @@
 #include "py/obj.h"
 #include "py/objtuple.h"
 
-//| def getlocale(path: str, times: Tuple[int, int]) -> None:
-//|     """Change the timestamp of a file."""
-//|     ...
+//| """Locale support module"""
+//|
+//| def getlocale() -> None:
+//|     """Returns the current locale setting as a tuple ``(language code, "utf-8")``
+//|
+//|     The language code comes from the installed translation of CircuitPython, specifically the "Language:" code specified in the translation metadata.
+//|     This can be useful to allow modules coded in Python to show messages in the user's preferred language.
+//|
+//|     Differences from CPython: No ``LC_*`` argument is permitted.
+//|     """
 //|
 STATIC mp_obj_t getlocale(void) {
 
