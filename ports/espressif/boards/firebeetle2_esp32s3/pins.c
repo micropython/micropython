4934ae2954b0a0f485776c178b4bb2a22d2d15a8
Bill Sideris <bill88t@feline.gr>
2023-11-18 13:44:09 +0200

    Introduce some comments and formatting changes to escape ci bug

--- ports/espressif/boards/firebeetle2_esp32s3/pins.c
+++ ports/espressif/boards/firebeetle2_esp32s3/pins.c
@@ -16,8 +16,8 @@ STATIC const mp_rom_obj_tuple_t camera_data_tuple = {
         MP_ROM_PTR(&pin_GPIO40), // Y3
         MP_ROM_PTR(&pin_GPIO41), // Y4
         MP_ROM_PTR(&pin_GPIO4),  // Y5
-        MP_ROM_PTR(&pin_GPIO7), // Y6
-        MP_ROM_PTR(&pin_GPIO8), // Y7
+        MP_ROM_PTR(&pin_GPIO7),  // Y6
+        MP_ROM_PTR(&pin_GPIO8),  // Y7
         MP_ROM_PTR(&pin_GPIO46), // Y8
         MP_ROM_PTR(&pin_GPIO48)  // Y9
     }
@@ -45,6 +45,7 @@ STATIC const mp_rom_map_elem_t board_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_IO1),      MP_ROM_PTR(&pin_GPIO1) },
     { MP_ROM_QSTR(MP_QSTR_SDA),      MP_ROM_PTR(&pin_GPIO3) },
 
+    // I2C cannot be used when CAM_I2C is in use.
     { MP_ROM_QSTR(MP_QSTR_I2C),  MP_ROM_PTR(&board_i2c_obj) },
 
     { MP_ROM_QSTR(MP_QSTR_IO0),      MP_ROM_PTR(&pin_GPIO0) },
@@ -85,14 +86,14 @@ STATIC const mp_rom_map_elem_t board_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR_IO10),     MP_ROM_PTR(&pin_GPIO10)},
     { MP_ROM_QSTR(MP_QSTR_A4),       MP_ROM_PTR(&pin_GPIO10)},
 
-    { MP_ROM_QSTR(MP_QSTR_IO8),      MP_ROM_PTR(&pin_GPIO8)},
-    { MP_ROM_QSTR(MP_QSTR_A3),       MP_ROM_PTR(&pin_GPIO8)},
+    { MP_ROM_QSTR(MP_QSTR_IO8),      MP_ROM_PTR(&pin_GPIO8) },
+    { MP_ROM_QSTR(MP_QSTR_A3),       MP_ROM_PTR(&pin_GPIO8) },
 
-    { MP_ROM_QSTR(MP_QSTR_IO6),      MP_ROM_PTR(&pin_GPIO6)},
-    { MP_ROM_QSTR(MP_QSTR_A2),       MP_ROM_PTR(&pin_GPIO6)},
+    { MP_ROM_QSTR(MP_QSTR_IO6),      MP_ROM_PTR(&pin_GPIO6) },
+    { MP_ROM_QSTR(MP_QSTR_A2),       MP_ROM_PTR(&pin_GPIO6) },
 
-    { MP_ROM_QSTR(MP_QSTR_IO5),      MP_ROM_PTR(&pin_GPIO5)},
-    { MP_ROM_QSTR(MP_QSTR_A1),       MP_ROM_PTR(&pin_GPIO5)},
+    { MP_ROM_QSTR(MP_QSTR_IO5),      MP_ROM_PTR(&pin_GPIO5) },
+    { MP_ROM_QSTR(MP_QSTR_A1),       MP_ROM_PTR(&pin_GPIO5) },
 
     { MP_ROM_QSTR(MP_QSTR_IO21),     MP_ROM_PTR(&pin_GPIO21)},
     { MP_ROM_QSTR(MP_QSTR_D13),      MP_ROM_PTR(&pin_GPIO21)},
@@ -110,9 +111,9 @@ STATIC const mp_rom_map_elem_t board_module_globals_table[] = {
     // Camera data
     { MP_ROM_QSTR(MP_QSTR_CAM_DATA),  MP_ROM_PTR(&camera_data_tuple)},
 
-    { MP_ROM_QSTR(MP_QSTR_CAM_VSYNC), MP_ROM_PTR(&pin_GPIO6)},
+    { MP_ROM_QSTR(MP_QSTR_CAM_VSYNC), MP_ROM_PTR(&pin_GPIO6) },
     { MP_ROM_QSTR(MP_QSTR_CAM_HREF),  MP_ROM_PTR(&pin_GPIO42)},
-    { MP_ROM_QSTR(MP_QSTR_CAM_PCLK),  MP_ROM_PTR(&pin_GPIO5)},
+    { MP_ROM_QSTR(MP_QSTR_CAM_PCLK),  MP_ROM_PTR(&pin_GPIO5) },
     { MP_ROM_QSTR(MP_QSTR_CAM_XCLK),  MP_ROM_PTR(&pin_GPIO45)},
 
     { MP_ROM_QSTR(MP_QSTR_CAM_I2C),   MP_ROM_PTR(&board_cam_i2c_obj)},
