c87abe758035e589f40285c7c4e3c47bab54af75
Jeff Epler <jepler@gmail.com>
2023-11-21 08:45:41 -0600

    run-tests: re-add circuitpython tests

--- tests/run-tests.py
+++ tests/run-tests.py
@@ -977,6 +977,7 @@ the last matching regex is used:
         if args.test_dirs is None:
             test_dirs = (
                 "basics",
+                "circuitpython",  # CIRCUITPY-CHANGE
                 "micropython",
                 "misc",
                 "extmod",
