0d57fc4d8c78856711e8d963a0f019bc10add959
Michael Rangen <73085145+mrangen@users.noreply.github.com>
2023-11-26 18:20:07 -0700

    Update mpconfigboard.mk

--- ports/raspberrypi/boards/breadstick_raspberry/mpconfigboard.mk
+++ ports/raspberrypi/boards/breadstick_raspberry/mpconfigboard.mk
@@ -13,3 +13,5 @@ CIRCUITPY_BITBANG_APA102 = 1
 # Include these Python libraries in firmware.
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_LSM6DS/
 FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_DotStar/
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Register/
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_FancyLED/
