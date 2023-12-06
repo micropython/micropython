0c007937d7cd61ef1cfa25cfe3b2630fe01602da
IFX-Anusha <Anusha.TR@infineon.com>
2023-12-06 15:31:50 +0530

    tests/psoc6: Pin.irq test and Periodic Timer test.
    Signed-off-by: IFX-Anusha <Anusha.TR@infineon.com>

--- tests/psoc6/timer.py
+++ tests/psoc6/timer.py
@@ -2,5 +2,13 @@ import time
 from machine import Timer
 
 t = Timer(0)
-t.init(period=2000, mode=Timer.ONE_SHOT, callback=lambda t: print("hello"))
-time.sleep_ms(3000)
+t.init(period=2000, mode=Timer.ONE_SHOT, callback=lambda t: print("Oneshot Timer"))
+time.sleep(30)
+t.deinit()
+t1 = Timer(0)
+t1.init(period=2000, mode=Timer.PERIODIC, callback=lambda t: print("Periodic Timer"))
+for i in range(200000):
+    pass
+
+t.deinit()
+
