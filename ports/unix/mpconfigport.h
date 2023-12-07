2c828a88153dfdbd767fc0d8f2124cfbefdcbc81
Angus Gratton <angus@redyak.com.au>
2023-12-07 13:32:41 +1100

    unix: Update port to use the new event functions.
    Necessary to get coverage of the new event functions.
    
    Deletes the case that called usleep(delay) for mp_hal_delay_ms(), it seems
    like this wouldn't have ever happened anyhow (MICROPY_EVENT_POOL_HOOK is
    always defined for the unix port).
    
    This work was funded through GitHub Sponsors.
    
    Signed-off-by: Angus Gratton <angus@redyak.com.au>

--- ports/unix/mpconfigport.h
+++ ports/unix/mpconfigport.h
@@ -222,14 +222,10 @@ static inline unsigned long mp_random_seed_init(void) {
 #endif
 
 // In lieu of a WFI(), slow down polling from being a tight loop.
-#ifndef MICROPY_EVENT_POLL_HOOK
-#define MICROPY_EVENT_POLL_HOOK \
-    do { \
-        extern void mp_handle_pending(bool); \
-        mp_handle_pending(true); \
-        usleep(500); /* equivalent to mp_hal_delay_us(500) */ \
-    } while (0);
-#endif
+//
+// Note that we don't delay for the full TIMEOUT_MS, as execution
+// can't be woken from the delay.
+#define MICROPY_INTERNAL_WFE(TIMEOUT_MS) mp_hal_delay_us(500)
 
 // Configure the implementation of machine.idle().
 #include <sched.h>
