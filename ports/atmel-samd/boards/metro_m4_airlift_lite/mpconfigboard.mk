21de0a734922295e03dd2a0fb6773ad38d6cbc31
Jeff Epler <jepler@gmail.com>
2023-12-08 10:41:36 -0600

    Enable jpegio almost anywhere we can

--- ports/atmel-samd/boards/metro_m4_airlift_lite/mpconfigboard.mk
+++ ports/atmel-samd/boards/metro_m4_airlift_lite/mpconfigboard.mk
@@ -12,6 +12,7 @@ LONGINT_IMPL = MPZ
 
 CIRCUITPY__EVE = 1
 CIRCUITPY_FLOPPYIO = 0
+CIRCUITPY_JPEGIO = 0
 CIRCUITPY_SYNTHIO = 0
 
 # We don't have room for the fonts for terminalio for certain languages,
