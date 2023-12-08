241dbac9284433cbf8c9b665ab12e1c932e6ef04
Damien George <damien@micropython.org>
2023-12-08 15:39:28 +1100

    extmod: Add lists of libm/libm_dbl source files for ports to use.
    So that ports don't need to specify each of these files, they can simply
    refer to the appropriate make/cmake variable.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- extmod/extmod.cmake
+++ extmod/extmod.cmake
@@ -62,6 +62,41 @@ set(MICROPY_SOURCE_EXTMOD
     ${MICROPY_EXTMOD_DIR}/nimble/modbluetooth_nimble.c
 )
 
+# Single-precision libm math library.
+
+set(MICROPY_SOURCE_LIB_LIBM
+    ${MICROPY_DIR}/lib/libm/acoshf.c
+    ${MICROPY_DIR}/lib/libm/asinfacosf.c
+    ${MICROPY_DIR}/lib/libm/asinhf.c
+    ${MICROPY_DIR}/lib/libm/atan2f.c
+    ${MICROPY_DIR}/lib/libm/atanf.c
+    ${MICROPY_DIR}/lib/libm/atanhf.c
+    ${MICROPY_DIR}/lib/libm/ef_rem_pio2.c
+    ${MICROPY_DIR}/lib/libm/erf_lgamma.c
+    ${MICROPY_DIR}/lib/libm/fmodf.c
+    ${MICROPY_DIR}/lib/libm/kf_cos.c
+    ${MICROPY_DIR}/lib/libm/kf_rem_pio2.c
+    ${MICROPY_DIR}/lib/libm/kf_sin.c
+    ${MICROPY_DIR}/lib/libm/kf_tan.c
+    ${MICROPY_DIR}/lib/libm/log1pf.c
+    ${MICROPY_DIR}/lib/libm/math.c
+    ${MICROPY_DIR}/lib/libm/nearbyintf.c
+    ${MICROPY_DIR}/lib/libm/roundf.c
+    ${MICROPY_DIR}/lib/libm/sf_cos.c
+    ${MICROPY_DIR}/lib/libm/sf_erf.c
+    ${MICROPY_DIR}/lib/libm/sf_frexp.c
+    ${MICROPY_DIR}/lib/libm/sf_ldexp.c
+    ${MICROPY_DIR}/lib/libm/sf_modf.c
+    ${MICROPY_DIR}/lib/libm/sf_sin.c
+    ${MICROPY_DIR}/lib/libm/sf_tan.c
+    ${MICROPY_DIR}/lib/libm/wf_lgamma.c
+    ${MICROPY_DIR}/lib/libm/wf_tgamma.c
+)
+
+# Choose only one of these sqrt implementations, software or hardware.
+set(MICROPY_SOURCE_LIB_LIBM_SQRT_SW ${MICROPY_DIR}/lib/libm/ef_sqrt.c)
+set(MICROPY_SOURCE_LIB_LIBM_SQRT_HW ${MICROPY_DIR}/lib/libm/thumb_vfp_sqrtf.c)
+
 # Library for btree module and associated code
 
 if(MICROPY_PY_BTREE)
