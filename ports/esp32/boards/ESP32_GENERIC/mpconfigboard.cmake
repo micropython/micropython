c03f74634ea103d1bb40245ce16805b6eedbc731
Damien George <damien@micropython.org>
2023-12-11 12:01:40 +1100

    esp32/boards/ESP32_GENERIC: Disable network.LAN and VM-opt on D2WD.
    To reduce firmware size, because IDF v5.0.4 has increased in size.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/esp32/boards/ESP32_GENERIC/mpconfigboard.cmake
+++ ports/esp32/boards/ESP32_GENERIC/mpconfigboard.cmake
@@ -11,6 +11,9 @@ if(MICROPY_BOARD_VARIANT STREQUAL "D2WD")
 
     list(APPEND MICROPY_DEF_BOARD
         MICROPY_HW_MCU_NAME="ESP32-D2WD"
+        # Disable some options to reduce firmware size.
+        MICROPY_OPT_COMPUTED_GOTO=0
+        MICROPY_PY_NETWORK_LAN=0
     )
 endif()
 
