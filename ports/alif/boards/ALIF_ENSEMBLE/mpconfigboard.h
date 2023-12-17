59f26af5d169051f6885b95e5db4aa36787efa45
iabdalkader <i.abdalkader@gmail.com>
2023-12-17 12:12:06 +0100

    alif: Add basic Ensemble board config.
    Signed-off-by: iabdalkader <i.abdalkader@gmail.com>

---
+++ ports/alif/boards/ALIF_ENSEMBLE/mpconfigboard.h
@@ -0,0 +1,4 @@
+#define MICROPY_HW_BOARD_NAME       "Alif Ensemble DevKit"
+#define MICROPY_HW_MCU_NAME         "AE722F80F55D5XX"
+
+#define MICROPY_HW_UART_REPL        (4)
--- ports/alif/mpconfigport.h
+++ ports/alif/mpconfigport.h
@@ -29,12 +29,13 @@
 #include <stdint.h>
 #include <alloca.h> // for alloca()
 
+// board specific definitions
+#include "mpconfigboard.h"
+
 #ifndef MICROPY_CONFIG_ROM_LEVEL
 #define MICROPY_CONFIG_ROM_LEVEL                (MICROPY_CONFIG_ROM_LEVEL_EXTRA_FEATURES)
 #endif
 
-#define MICROPY_HW_BOARD_NAME                   "Alif Ensemble DevKit"
-#define MICROPY_HW_MCU_NAME                     "AE722F80F55D5XX"
 #define MICROPY_HW_ENABLE_UART_REPL             (1) // useful if there is no USB
 #define MICROPY_HW_ENABLE_USBDEV                (1)
 #define MICROPY_HW_USB_PRODUCT_FS_STRING        "Board in HS mode"
