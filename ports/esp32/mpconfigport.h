77993007609a5ac366d01222e989fe8e5edd493a
IhorNehrutsa <IhorNehrutsa@gmail.com>
2023-12-19 16:28:06 +0200

    esp32/mpconfigport: Enable MICROPY_PY_HASHLIB_MD5.
    Signed-off-by: IhorNehrutsa <Ihor.Nehrutsa@gmail.com>

--- ports/esp32/mpconfigport.h
+++ ports/esp32/mpconfigport.h
@@ -101,6 +101,7 @@
 #define MICROPY_BLUETOOTH_NIMBLE            (1)
 #define MICROPY_BLUETOOTH_NIMBLE_BINDINGS_ONLY (1)
 #endif
+#define MICROPY_PY_HASHLIB_MD5              (1)
 #define MICROPY_PY_HASHLIB_SHA1             (1)
 #define MICROPY_PY_HASHLIB_SHA256           (1)
 #define MICROPY_PY_CRYPTOLIB                (1)
