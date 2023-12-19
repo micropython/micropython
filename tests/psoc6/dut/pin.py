8d80afec31575cb9192eb2facde2615631085f87
enriquezgarc <enriquezgarcia.external@infineon.com>
2023-12-19 15:51:43 +0100

    tests/psoc6/dut/pin.py: Extended pin modes and features tests.
    Signed-off-by: enriquezgarc <enriquezgarcia.external@infineon.com>

--- tests/psoc6/dut/pin.py
+++ tests/psoc6/dut/pin.py
@@ -1,6 +1,7 @@
 import os
 from machine import Pin
 import machine
+import time
 
 # Allocate pin based on board
 machine = os.uname().machine
@@ -14,10 +15,17 @@ elif "CY8CPROTO-063-BLE" in machine:
 # Pin out and pin in must be connected
 # together in the board
 
-pin_out = Pin(pin1_name)
-pin_out.init(Pin.OUT)
+pin_out = Pin(pin1_name, mode=Pin.OUT, value=True)
 pin_in = Pin(pin2_name, Pin.IN)
 
+# Validating initialization values
+print("pin out initial value 1: ", pin_in.value() == 1)
+
+pin_out.deinit()
+pin_out = Pin(pin1_name, mode=Pin.OUT, value=False)
+print("pin out initial value 0: ", pin_in.value() == 0)
+
+# Validation different output setting
 pin_out.value(1)
 print("pin out value 1: ", pin_in.value() == 1)
 
@@ -42,7 +50,40 @@ print("pin out value on: ", pin_in.value() == 1)
 pin_out.off()
 print("pin out value off: ", pin_in.value() == 0)
 
-pin_in.irq(
-    handler=lambda t: print("Interrupt triggered"), trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING
-)
+pin_out(1)
+print("pin out callable 1: ", pin_in.value() == 1)
+
+pin_out(0)
+print("pin out callable 0: ", pin_in.value() == 0)
+
+
+# Validating pull resistors configurations and open drain mode
+pin_out.deinit()
+pin_out = Pin(pin1_name, pull=None, mode=Pin.OPEN_DRAIN)
+print("pin out with pull none initially 0 or 1: ", pin_in.value() == 0 or pin_in.value() == 1)
+
+pin_out.deinit()
+pin_out = Pin(pin1_name, pull=Pin.PULL_DOWN, mode=Pin.OUT)
+print("pin out with pull down initially down: ", pin_in.value() == 1)
+
+pin_out.deinit()
+pin_out = Pin(pin1_name, pull=Pin.PULL_UP, mode=Pin.OUT)
+print("pin out with pull up initially high: ", pin_in.value() == 0)
+
+
+# Validating interrupts
+def blocking_delay_ms(delay_ms):
+    start = time.ticks_ms()
+    while time.ticks_diff(time.ticks_ms(), start) < delay_ms:
+        pass
+
+
+pin_in.irq(handler=lambda t: print("interrupt triggered rising"), trigger=Pin.IRQ_RISING)
 pin_out.high()
+
+blocking_delay_ms(1000)
+
+pin_in.irq(handler=lambda t: print("interrupt triggered falling"), trigger=Pin.IRQ_FALLING)
+pin_out.low()
+
+blocking_delay_ms(1000)
