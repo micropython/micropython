035569d7fe96d36e639fcf3ae4804dcac4870caa
Dan Halbert <halbert@halwitz.org>
2023-12-29 10:29:00 -0500

    recent MicroPython broke out binascii.crc32()

--- py/circuitpy_mpconfig.h
+++ py/circuitpy_mpconfig.h
@@ -132,6 +132,7 @@ extern void common_hal_mcu_enable_interrupts(void);
 #define MICROPY_PY_BUILTINS_STR_UNICODE  (1)
 
 #define MICROPY_PY_BINASCII             (CIRCUITPY_BINASCII)
+#define MICROPY_PY_BINASCII_CRC32       (CIRCUITPY_BINASCII && CIRCUITPY_ZLIB)
 #define MICROPY_PY_CMATH                 (0)
 #define MICROPY_PY_COLLECTIONS           (CIRCUITPY_COLLECTIONS)
 #define MICROPY_PY_DESCRIPTORS           (1)
