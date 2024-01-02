74fb42aa82d537b6fefb463907274bd660c6ce8d
Angus Gratton <angus@redyak.com.au>
2024-01-02 12:19:33 +1100

    rp2: Refactor soft timer to use hardware timer alarm.
    Progress towards removing pico-sdk alarm pool, due to a known issue.
    
    This work was funded through GitHub Sponsors.
    
    Signed-off-by: Angus Gratton <angus@redyak.com.au>

--- ports/rp2/mphalport.c
+++ ports/rp2/mphalport.c
@@ -35,6 +35,7 @@
 #include "pendsv.h"
 #include "tusb.h"
 #include "uart.h"
+#include "hardware/irq.h"
 #include "hardware/rtc.h"
 #include "pico/unique_id.h"
 
@@ -45,8 +46,6 @@
 // This needs to be added to the result of time_us_64() to get the number of
 // microseconds since the Epoch.
 static uint64_t time_us_64_offset_from_epoch;
-
-static alarm_id_t soft_timer_alarm_id = 0;
 
 #if MICROPY_HW_ENABLE_UART_REPL || MICROPY_HW_USB_CDC
 
@@ -273,21 +272,26 @@ uint32_t storage_read_blocks(uint8_t *dest, uint32_t block_num, uint32_t num_blo
     panic_unsupported();
 }
 
-static int64_t soft_timer_callback(alarm_id_t id, void *user_data) {
-    soft_timer_alarm_id = 0;
-    pendsv_schedule_dispatch(PENDSV_DISPATCH_SOFT_TIMER, soft_timer_handler);
-    return 0; // don't reschedule this alarm
-}
-
 uint32_t soft_timer_get_ms(void) {
     return mp_hal_ticks_ms();
 }
 
 void soft_timer_schedule_at_ms(uint32_t ticks_ms) {
-    if (soft_timer_alarm_id != 0) {
-        cancel_alarm(soft_timer_alarm_id);
-    }
     int32_t ms = soft_timer_ticks_diff(ticks_ms, mp_hal_ticks_ms());
     ms = MAX(0, ms);
-    soft_timer_alarm_id = add_alarm_in_ms(ms, soft_timer_callback, NULL, true);
+    if (hardware_alarm_set_target(MICROPY_HW_SOFT_TIMER_ALARM_NUM, delayed_by_ms(get_absolute_time(), ms))) {
+        // "missed" hardware alarm target
+        hardware_alarm_force_irq(MICROPY_HW_SOFT_TIMER_ALARM_NUM);
+    }
+}
+
+static void soft_timer_hardware_callback(unsigned int alarm_num) {
+    // The timer alarm ISR needs to call here and trigger PendSV dispatch via
+    // a second ISR, as PendSV may be currently suspended by the other CPU.
+    pendsv_schedule_dispatch(PENDSV_DISPATCH_SOFT_TIMER, soft_timer_handler);
+}
+
+void soft_timer_init(void) {
+    hardware_alarm_claim(MICROPY_HW_SOFT_TIMER_ALARM_NUM);
+    hardware_alarm_set_callback(MICROPY_HW_SOFT_TIMER_ALARM_NUM, soft_timer_hardware_callback);
 }
