6bc96e7991d69c10b7ac5da9a036e1f8acdcb981
Damien George <damien@micropython.org>
2024-01-05 09:53:05 +1100

    tests/thread: Adjust thread tests so most are able to run on rp2 port.
    The aim of this commit is to make it so that the existing thread tests can
    be used to test the _thread module on the rp2 port.  The rp2 port only
    allows up to one thread to be created at a time, and does not have the GIL
    enabled.
    
    The following changes have been made:
    - run-tests.py skips mutation tests on rp2, because there's no GIL.
    - run-tests.py skips other tests on rp2 that require more than one thread.
    - The tests stop trying to start a new thread after there is an OSError,
      which indicates that the system cannot create more threads.
    - Some of these tests also now run the test function on the main thread,
      not just the spawned threads.
    - In some tests the output printing is adjusted so it's the same regardless
      of how many threads were spawned.
    - Some time.sleep(1) are replaced with time.sleep(0) to make the tests run
      a little faster (finish sooner when the work is done).
    
    For the most part the tests are unchanged for existing platforms like esp32
    and unix.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- tests/thread/stress_schedule.py
+++ tests/thread/stress_schedule.py
@@ -19,6 +19,7 @@ _TIMEOUT_MS = 10000
 
 n = 0  # How many times the task successfully ran.
 t = None  # Start time of test, assigned here to preallocate entry in globals dict.
+thread_run = True  # If the thread should continue running.
 
 
 def task(x):
@@ -27,7 +28,7 @@ def task(x):
 
 
 def thread():
-    while True:
+    while thread_run:
         try:
             micropython.schedule(task, None)
         except RuntimeError:
@@ -36,12 +37,20 @@ def thread():
 
 
 for i in range(8):
-    _thread.start_new_thread(thread, ())
+    try:
+        _thread.start_new_thread(thread, ())
+    except OSError:
+        # System cannot create a new thead, so stop trying to create them.
+        break
 
 # Wait up to 10 seconds for 10000 tasks to be scheduled.
 t = time.ticks_ms()
 while n < _NUM_TASKS and time.ticks_diff(time.ticks_ms(), t) < _TIMEOUT_MS:
     pass
+
+# Stop all threads.
+thread_run = False
+time.sleep_ms(20)
 
 if n < _NUM_TASKS:
     # Not all the tasks were scheduled, likely the scheduler stopped working.
