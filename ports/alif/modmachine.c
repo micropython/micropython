53b7c14836b00a432855439513721dcf07392041
Damien George <damien@micropython.org>
2024-01-10 23:41:10 +1100

    alif/modmachine: Enable machine.Timer.
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/alif/modmachine.c
+++ ports/alif/modmachine.c
@@ -29,6 +29,7 @@
 
 #define MICROPY_PY_MACHINE_EXTRA_GLOBALS \
     { MP_ROM_QSTR(MP_QSTR_Pin),                 MP_ROM_PTR(&machine_pin_type) }, \
+    { MP_ROM_QSTR(MP_QSTR_Timer),               MP_ROM_PTR(&machine_timer_type) }, \
 
 static void mp_machine_idle(void) {
     mp_event_wait_indefinite();
