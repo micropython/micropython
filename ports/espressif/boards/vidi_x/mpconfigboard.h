c0c7e220f4cedecf7fb3570ae6f7aa4e161a6172
Lovro Jakic <lovro.jakicc@gmail.com>
2024-01-13 18:55:53 +0100

    Removed I2C

--- ports/espressif/boards/vidi_x/mpconfigboard.h
+++ ports/espressif/boards/vidi_x/mpconfigboard.h
@@ -31,19 +31,13 @@
 
 #define MICROPY_HW_LED_STATUS (&pin_GPIO2)
 
-#define CIRCUITPY_BOARD_I2C         (1)
-#define CIRCUITPY_BOARD_I2C_PIN     {{.scl = &pin_GPIO15, .sda = &pin_GPIO4}}
-
 #define CIRCUITPY_BOARD_SPI         (1)
 #define CIRCUITPY_BOARD_SPI_PIN     {{.clock = &pin_GPIO18, .mosi = &pin_GPIO23, .miso = &pin_GPIO19}}
 
-#define CIRCUITPY_BOARD_UART        (0)
-
-// For entering safe mode, use VOL button
 #define CIRCUITPY_BOOT_BUTTON       (&pin_GPIO0)
 
 // Explanation of how a user got into safe mode
-#define BOARD_USER_SAFE_MODE_ACTION MP_ERROR_TEXT("You pressed the VOL button at start up.")
+#define BOARD_USER_SAFE_MODE_ACTION MP_ERROR_TEXT("You pressed the VOLUME button at start up.")
 
 // UART pins attached to the USB-serial converter chip
 #define CIRCUITPY_CONSOLE_UART_TX (&pin_GPIO1)
