7db904dad9dadb69c1d40acb560e088049099e11
Josh Corn <josh@doubletake.design>
2024-01-17 14:21:31 -0500

    fixed whitespace

--- ports/espressif/boards/columbia-dsl-sensor/mpconfigboard.h
+++ ports/espressif/boards/columbia-dsl-sensor/mpconfigboard.h
@@ -29,8 +29,8 @@
 #define MICROPY_HW_BOARD_NAME       "ColumbiaDSL-Sensor-Board-V1"
 #define MICROPY_HW_MCU_NAME         "ESP32S3"
 
-#define DEFAULT_I2C_BUS_SCL 		(&pin_GPIO39)
-#define DEFAULT_I2C_BUS_SDA 		(&pin_GPIO40)
+#define DEFAULT_I2C_BUS_SCL (&pin_GPIO39)
+#define DEFAULT_I2C_BUS_SDA (&pin_GPIO40)
 
-#define DEFAULT_UART_BUS_RX         (&pin_GPIO44)
-#define DEFAULT_UART_BUS_TX         (&pin_GPIO43)
+#define DEFAULT_UART_BUS_RX (&pin_GPIO44)
+#define DEFAULT_UART_BUS_TX (&pin_GPIO43)
