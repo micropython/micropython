5b04d56b861c97e270a6667d4ec80b812f3c15b5
James Nadeau <james.nadeau@gmail.com>
2024-01-20 07:36:59 -0500

    clean up comments

--- ports/espressif/boards/m5stack_cardputer/mpconfigboard.h
+++ ports/espressif/boards/m5stack_cardputer/mpconfigboard.h
@@ -30,26 +30,7 @@
 #define MICROPY_HW_MCU_NAME         "ESP32S3"
 
 #define MICROPY_HW_NEOPIXEL (&pin_GPIO21)
-// #define CIRCUITPY_STATUS_LED_POWER (&pin_GPIO34)
 
-// #define MICROPY_HW_LED_STATUS (&pin_GPIO13)
-
-#define DEFAULT_I2C_BUS_SCL (&pin_GPIO41)
-#define DEFAULT_I2C_BUS_SDA (&pin_GPIO42)
-
-// sd - do not use here, causes tft to stop working
-// #define DEFAULT_SPI_BUS_SCK (&pin_GPIO40)
-// #define DEFAULT_SPI_BUS_MOSI (&pin_GPIO14)
-// #define DEFAULT_SPI_BUS_MISO (&pin_GPIO39)
-// orig
 #define DEFAULT_SPI_BUS_SCK (&pin_GPIO36)
 #define DEFAULT_SPI_BUS_MOSI (&pin_GPIO35)
 #define DEFAULT_SPI_BUS_MISO (&pin_GPIO34)
-
-#define DEFAULT_UART_BUS_RX (&pin_GPIO44)
-#define DEFAULT_UART_BUS_TX (&pin_GPIO43)
-// orig
-// #define DEFAULT_UART_BUS_RX (&pin_GPIO2)
-// #define DEFAULT_UART_BUS_TX (&pin_GPIO1)
-
-// #define DOUBLE_TAP_PIN (&pin_GPIO38)
