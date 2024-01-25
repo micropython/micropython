081686e1b34124879becf37ad1e9fe2c433e5e20
Scott Shawcroft <scott@tannewt.org>
2024-01-25 10:33:37 -0800

    Make `supervisor.reload()` from REPL work
    It will then run the next script.
    
    Fixes #8148

--- main.c
+++ main.c
@@ -1099,7 +1099,7 @@ int __attribute__((used)) main(void) {
             exit_code = run_repl(get_safe_mode());
             supervisor_set_run_reason(RUN_REASON_REPL_RELOAD);
         }
-        if (exit_code == PYEXEC_FORCED_EXIT) {
+        if (exit_code & (PYEXEC_FORCED_EXIT | PYEXEC_RELOAD)) {
             if (!simulate_reset) {
                 serial_write_compressed(MP_ERROR_TEXT("soft reboot\n"));
             }
