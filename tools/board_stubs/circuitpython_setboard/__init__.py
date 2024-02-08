71e6ecac6b8bdfb8aef12f87f77997a65a6503c5
Justin Myers <justmobilize@users.noreply.github.com>
2024-02-08 12:42:33 -0800

    Use importlib.resources

--- tools/board_stubs/circuitpython_setboard/__init__.py
+++ tools/board_stubs/circuitpython_setboard/__init__.py
@@ -1,22 +1,26 @@
 # SPDX-FileCopyrightText: 2024 Tim Cocks
 #
 # SPDX-License-Identifier: MIT
-import sys
+from importlib import resources
 import os
+import sys
 import shutil
 
 
 def set_board():
+    if len(sys.argv) != 2:
+        print(f"Incorrect args. Please call with: 'circuitpython_setboard chosen_board'")
+        return
+
     chosen_board = sys.argv[1]
     print(f"setting board: {chosen_board}")
-    site_packages_path = os.path.sep.join(__file__.split(os.path.sep)[:-2])
-    board_defs_path = f"{site_packages_path}{os.path.sep}board_definitions{os.path.sep}"
-    board_stubs_path = f"{site_packages_path}{os.path.sep}board-stubs{os.path.sep}__init__.pyi"
 
-    if chosen_board not in os.listdir(board_defs_path):
+    board_stubs_file = resources.files('board-stubs').joinpath("__init__.pyi")
+    board_definitions_path = resources.files('board_definitions').joinpath(chosen_board)
+    board_definitions_file = board_definitions_path.joinpath("__init__.pyi")
+
+    if not board_definitions_file.is_file():
         print(f"Board: '{chosen_board}' was not found")
         return
 
-    shutil.copyfile(
-        board_defs_path + f"{os.path.sep}{chosen_board}{os.path.sep}__init__.pyi", board_stubs_path
-    )
+    shutil.copyfile(board_definitions_file, board_stubs_file)
