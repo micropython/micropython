6bdb4aecd2570537639fab64bba3d80e2ebec409
Justin Myers <justmobilize@users.noreply.github.com>
2024-02-08 11:09:53 -0800

    Fix stub circuitpython_setboard OS path separator issue

--- tools/board_stubs/circuitpython_setboard/__init__.py
+++ tools/board_stubs/circuitpython_setboard/__init__.py
@@ -9,13 +9,9 @@ import shutil
 def set_board():
     chosen_board = sys.argv[1]
     print(f"setting board: {chosen_board}")
-    board_defs_path = (
-        os.path.sep.join(__file__.split("/")[:-2]) + f"{os.path.sep}board_definitions{os.path.sep}"
-    )
-    board_stubs_path = (
-        os.path.sep.join(__file__.split("/")[:-2])
-        + f"{os.path.sep}board-stubs{os.path.sep}__init__.pyi"
-    )
+    site_packages_path = os.path.sep.join(__file__.split(os.path.sep)[:-2])
+    board_defs_path = f"{site_packages_path}{os.path.sep}board_definitions{os.path.sep}"
+    board_stubs_path = f"{site_packages_path}{os.path.sep}board-stubs{os.path.sep}__init__.pyi"
 
     if chosen_board not in os.listdir(board_defs_path):
         print(f"Board: '{chosen_board}' was not found")
