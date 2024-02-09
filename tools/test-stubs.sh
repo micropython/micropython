c8964dfdab541fe21e158c5b824fec2e10c408f1
Justin Myers <justmobilize@users.noreply.github.com>
2024-02-09 09:13:40 -0800

    Update testing messages

--- tools/test-stubs.sh
+++ tools/test-stubs.sh
@@ -7,7 +7,9 @@ rm -rf circuitpython-stubs .mypy_cache
 make stubs
 pip install --force-reinstall circuitpython-stubs/dist/circuitpython-stubs-*.tar.gz
 export MYPYPATH=circuitpython-stubs/
+echo "The following test should pass:"
 mypy -c 'import busio; b: busio.I2C; b.writeto(0x30, b"")'
+echo "The next two tests are expected to show type errors:"
 ! mypy -c 'import busio; b: busio.I2C; b.readfrom_into(0x30, b"")'
 ! mypy -c 'import busio; b: busio.I2C; b.write(0x30, b"")'
-echo "(The above two tests are expected to show type errors)"
+echo "Typing tests complete"
