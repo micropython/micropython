1cd89f4f394ea2ca033cd9f1e2c70bf69af0cc64
Kattni <kattni@kattni.com>
2024-02-09 14:16:47 -0500

    Add Pimoroni Inky Frame 7.3in

---
+++ ports/raspberrypi/boards/pimoroni_inky_frame_7_3/inky-shared.h
@@ -0,0 +1,8 @@
+#ifndef PIMORONI_INKY_SHARED
+#define PIMORONI_INKY_SHARED
+
+#include "shared-bindings/digitalio/DigitalInOut.h"
+
+extern digitalio_digitalinout_obj_t enable_pin_obj;
+
+#endif // PIMORONI_INKY_SHARED
