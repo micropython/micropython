da95ed8176283c9a33625b428b9dc55c3bd27d68
Jeff Epler <jepler@gmail.com>
2024-02-08 16:56:52 -0800

    tests/basics/nanbox_smallint.py: Fix incorrect use of int() in test.
    The literal is in base 16 but int()'s default radix in CPython is 10, not
    0.
    
    Signed-off-by: Jeff Epler <jepler@gmail.com>

--- tests/basics/nanbox_smallint.py
+++ tests/basics/nanbox_smallint.py
@@ -23,17 +23,17 @@ if float("1e100") == float("inf"):
     raise SystemExit
 
 micropython.heap_lock()
-print(int("0x80000000"))
+print(int("0x80000000", 16))
 micropython.heap_unlock()
 
 # This is the most positive small integer.
 micropython.heap_lock()
-print(int("0x3fffffffffff"))
+print(int("0x3fffffffffff", 16))
 micropython.heap_unlock()
 
 # This is the most negative small integer.
 micropython.heap_lock()
-print(int("-0x3fffffffffff") - 1)
+print(int("-0x3fffffffffff", 16) - 1)
 micropython.heap_unlock()
 
 x = 1
