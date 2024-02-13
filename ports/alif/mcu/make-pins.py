5dda806a13d8d81792df1c40009457232b79cada
Damien George <damien@micropython.org>
2024-02-13 18:05:05 +1100

    alif/mcu: Add ADC12 information to pin struct.
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/alif/mcu/make-pins.py
+++ ports/alif/mcu/make-pins.py
@@ -10,11 +10,34 @@ import boardgen
 NUM_PORTS = 16
 NUM_PINS_PER_PORT = 8
 
+# Maps pad name to (adc12_periph, adc12_channel).
+ADC12_ANA_MAP = {
+    "GPIO0_0": (0, 0),
+    "GPIO0_1": (0, 1),
+    "GPIO0_2": (0, 2),
+    "GPIO0_3": (0, 3),
+    "GPIO0_4": (0, 4),
+    "GPIO0_5": (0, 5),
+    "GPIO0_6": (1, 0),
+    "GPIO0_7": (1, 1),
+    "GPIO1_0": (1, 2),
+    "GPIO1_1": (1, 3),
+    "GPIO1_2": (1, 4),
+    "GPIO1_3": (1, 5),
+    "GPIO1_4": (2, 0),
+    "GPIO1_5": (2, 1),
+    "GPIO1_6": (2, 2),
+    "GPIO1_7": (2, 3),
+    "GPIO2_0": (2, 4),
+    "GPIO2_1": (2, 5),
+}
+
 
 class AlifPin(boardgen.Pin):
     # Emit the struct which contains the pin instance.
     def definition(self):
         port, pin = self.name()[4:].split("_")
+        adc12_periph, adc12_channel = ADC12_ANA_MAP.get(self.name(), (3, 7))
         base = "LPGPIO_BASE" if port == "15" else "GPIO{}_BASE".format(port)
         return (
             "{{ "
@@ -22,8 +45,16 @@ class AlifPin(boardgen.Pin):
             ".gpio = (GPIO_Type *){base}, "
             ".port = PORT_{port}, "
             ".pin = PIN_{pin}, "
+            ".adc12_periph = {adc12_periph}, "
+            ".adc12_channel = {adc12_channel}, "
             ".name = MP_QSTR_GPIO{port}_{pin} "
-            "}}".format(port=port, pin=pin, base=base)
+            "}}".format(
+                port=port,
+                pin=pin,
+                base=base,
+                adc12_periph=adc12_periph,
+                adc12_channel=adc12_channel,
+            )
         )
 
     # Alif cpu names must be "GPIOn_m".
