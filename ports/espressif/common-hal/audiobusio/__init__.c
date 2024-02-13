d202ab45ab5277fc8e389a78c689a5cc7878bc70
Scott Shawcroft <scott@tannewt.org>
2024-02-12 17:03:57 -0800

    Fix I2S deinit/pause
    Fixes #8867

--- ports/espressif/common-hal/audiobusio/__init__.c
+++ ports/espressif/common-hal/audiobusio/__init__.c
@@ -140,6 +140,9 @@ void port_i2s_allocate_init(i2s_t *self, bool left_justified) {
     if (err == ESP_ERR_NOT_FOUND) {
         mp_raise_RuntimeError(MP_ERROR_TEXT("Peripheral in use"));
     }
+    self->playing = false;
+    self->paused = false;
+    self->stopping = false;
 
     i2s_event_callbacks_t callbacks = {
         .on_recv = NULL,
@@ -157,6 +160,8 @@ void port_i2s_deinit(i2s_t *self) {
 }
 
 void port_i2s_play(i2s_t *self, mp_obj_t sample, bool loop) {
+    // Pause to disable the I2S channel so we can adjust the clock.
+    port_i2s_pause(self);
     self->sample = sample;
     self->loop = loop;
     self->bytes_per_sample = audiosample_bits_per_sample(sample) / 8;
@@ -204,6 +209,9 @@ void port_i2s_play(i2s_t *self, mp_obj_t sample, bool loop) {
 }
 
 bool port_i2s_playing(i2s_t *self) {
+    // TODO: Reason about stopping. This check leads to cases where the DMA is
+    // "playing" but the common-hal thinks it isn't and skips pausing. Probably
+    // best to move this functionality into I2SOut directly.
     return self->playing && !self->stopping;
 }
 
@@ -219,14 +227,14 @@ void port_i2s_stop(i2s_t *self) {
 }
 
 void port_i2s_pause(i2s_t *self) {
-    if (!self->paused) {
+    if (self->playing && !self->paused) {
         self->paused = true;
         CHECK_ESP_RESULT(i2s_channel_disable(self->handle));
     }
 }
 
 void port_i2s_resume(i2s_t *self) {
-    if (self->paused) {
+    if (self->playing && self->paused) {
         self->paused = false;
         CHECK_ESP_RESULT(i2s_channel_enable(self->handle));
     }
