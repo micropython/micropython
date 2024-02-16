38f78788af5d08ec1a1d49ba71c059ee337db22b
YAMAMOTO Takashi <yamamoto@midokura.com>
2024-02-16 18:15:10 +0900

    ports/unix/variants/wasi/mpconfigvariant.mk: Disable several things.
    Signed-off-by: YAMAMOTO Takashi <yamamoto@midokura.com>

--- ports/unix/variants/wasi/mpconfigvariant.mk
+++ ports/unix/variants/wasi/mpconfigvariant.mk
@@ -1,3 +1,28 @@
 # This is the default variant when you `make` the Unix port.
 
 FROZEN_MANIFEST ?= $(VARIANT_DIR)/manifest.py
+
+# WASI doesn't have FFI
+MICROPY_PY_FFI=0
+
+# When threading is enabled, micropython GC uses signals, which is
+# not available on WASI.
+MICROPY_PY_THREAD=0
+
+# Disable for now because network support is limited in WASI.
+MICROPY_PY_SOCKET=0
+MICROPY_PY_SSL=0
+
+# ../../lib/berkeley-db-1.xx/PORT/include/db.h:40:10:
+# fatal error: 'sys/cdefs.h' file not found
+MICROPY_PY_BTREE=0
+
+# WASI doesn't have termios
+MICROPY_PY_TERMIOS=0
+MICROPY_USE_READLINE=0
+
+# The following things might just work as they are.
+# Disabled for now because I'm not interested in them.
+MICROPY_VFS_FAT=0
+MICROPY_VFS_LFS1=0
+MICROPY_VFS_LFS2=0
