fbbfa3a75bf9e346f7aeb5035c4501aff897500d
Dan Halbert <halbert@halwitz.org>
2024-02-19 18:14:34 -0500

    ports/espressif/tools/check-sdkconfig.py: use click

--- ports/espressif/tools/check-sdkconfig.py
+++ ports/espressif/tools/check-sdkconfig.py
@@ -1,38 +1,60 @@
 #!/usr/bin/env python3
 import sys
 
-sdk_config = {}
+import click
 
-config_h = sys.argv[-1]
-with open(config_h) as f:
-    for row in f:
-        if row.startswith("#define "):
-            _, k, v = row.strip().split(None, 2)
-            # ad-hoc handle lines like '#define CONFIG_TCP_MSL CONFIG_LWIP_TCP_MSL'
+
+def int_or_string(s):
+    try:
+        return int(s)
+    except ValueError:
+        return s.strip('"')
+
+
+def collect_definitions(file):
+    """Collect all definitions in supplied sdkconfig.h."""
+    sdk_config = {}
+    for line in file:
+        if line.startswith("#define "):
+            _, k, v = line.strip().split(None, 2)
+            # Handle transitive definitions like '#define CONFIG_TCP_MSL CONFIG_LWIP_TCP_MSL'
             v = sdk_config.get(k, v)
-            if v[0] == '"':
-                v = eval(v)  # Assume it is a simple C string
+            sdk_config[k] = int_or_string(v)
+    return sdk_config
 
-            # ad-hoc convert to integer
-            try:
-                v = int(v)
-            except ValueError:
-                pass
-            sdk_config[k] = v
 
-del sys.argv[-1]
+def validate(sdk_config, circuitpy_config):
+    partition_table = sdk_config.get("CONFIG_PARTITION_TABLE_FILENAME")
+    for var in ("CIRCUITPY_STORAGE_EXTEND", "CIRCUITPY_DUALBANK"):
+        if circuitpy_config.get(var):
+            with open(partition_table) as f:
+                content = f.read()
+                if not "ota_1" in content:
+                    raise SystemExit(
+                        f"{var} is incompatible with {partition_table=} (no ota_1 partition)"
+                    )
 
-circuitpy_config = {}
-for arg in sys.argv[1:]:
-    k, v = arg.split("=", 1)
-    circuitpy_config[k] = int(v)
+    # Add more checks here for other things we want to verify.
+    return
 
-partition_table = sdk_config.get("CONFIG_PARTITION_TABLE_FILENAME")
-for var in ("CIRCUITPY_STORAGE_EXTEND", "CIRCUITPY_DUALBANK"):
-    if circuitpy_config.get(var):
-        with open(partition_table) as f:
-            content = f.read()
-        if not "ota_1" in content:
-            raise SystemExit(f"{var} is incompatible with {partition_table=} (no ota_1 partition)")
 
-    # Add more checks here
+@click.command()
+@click.argument("definitions", nargs=-1, metavar="CIRCUITPY_X=1 CIRCUITPY_Y=0 ...")
+@click.argument(
+    "sdkconfig_h", required=True, nargs=1, type=click.File("r"), metavar="<path to sdkconfig.h>"
+)
+def run(definitions, sdkconfig_h):
+    sdk_config = collect_definitions(sdkconfig_h)
+
+    # Parse definitions arguments
+    circuitpy_config = {}
+    for definition in definitions:
+        k, v = definition.split("=", 1)
+        circuitpy_config[k] = int_or_string(v)
+
+    # Validate.
+    validate(sdk_config, circuitpy_config)
+
+
+if __name__ == "__main__":
+    run()
