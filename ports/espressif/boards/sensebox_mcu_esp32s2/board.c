8d09b6a4f09697fe67fb7f35403bea1012f45541
Mario Pesch <mario.pesch@uni-muenster.de>
2024-02-21 14:06:49 +0000

    update board.c

--- ports/espressif/boards/sensebox_mcu_esp32s2/board.c
+++ ports/espressif/boards/sensebox_mcu_esp32s2/board.c
@@ -27,7 +27,7 @@
 #include "supervisor/board.h"
 #include "mpconfigboard.h"
 #include "shared-bindings/microcontroller/Pin.h"
-#include "components/driver/include/driver/gpio.h"
+#include "components/driver/gpio/include/driver/gpio.h"
 #include "components/hal/include/hal/gpio_hal.h"
 #include "common-hal/microcontroller/Pin.h"
 
