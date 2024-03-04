7ec184d7c59bdd2e22a631d2dddb1e3a9b54fdd6
iabdalkader <i.abdalkader@gmail.com>
2024-03-04 17:22:36 +0100

    alif: Update USB driver.
    An updated USB driver from Alif with the following changes:
    - Transfer aborting, cleanup and restarting has been added
    (done automatically inside the driver).
    - Critical sections added during transfer setup
    (preventing rare race conditions).
    - Full logging toggle (default: off).
    - A dedicated function added to uninitialize the driver (dcd_uninit).
    - Different STATUS IN handling, based more on the USB controller demands
    as opposed to TinyUSB requests. This fixes behavior on macOS and further
    improves stability and speed on all platforms.
    
    Signed-off-by: iabdalkader <i.abdalkader@gmail.com>

--- ports/alif/tinyusb_port/tusb_alif_dcd.c
+++ ports/alif/tinyusb_port/tusb_alif_dcd.c
@@ -1,4 +1,13 @@
 // *FORMAT-OFF*
+/* Copyright (C) 2024 Alif Semiconductor - All Rights Reserved.
+ * Use, distribution and modification of this code is permitted under the
+ * terms stated in the Alif Semiconductor Software License Agreement
+ *
+ * You should have received a copy of the Alif Semiconductor Software
+ * License Agreement with this file. If not, please write to:
+ * contact@alifsemi.com, or visit: https://alifsemi.com/license
+ *
+ */
 #include "tusb_option.h"
 
 #if CFG_TUD_ENABLED
@@ -11,14 +20,20 @@
 #include "clk.h"
 #include "power.h"
 
-#if 1
-// This log and memset is needed to get the USB working, something to do with the extra delay.
-void snprintf_dummy(char *str, size_t size, const char *fmt, ...) {
-}
-static char logbuf[48];
+// #define TUSB_ALIF_DEBUG
+// #define TUSB_ALIF_DEBUG_DEPTH (2048)
 
-#define LOG(...)      memset(logbuf, ' ', 48); \
-                      snprintf_dummy(logbuf, 48, __VA_ARGS__)
+#if defined(TUSB_ALIF_DEBUG)
+#if (1 < TUSB_ALIF_DEBUG_DEPTH)
+#define LOG(...)      memset(logbuf[bi % TUSB_ALIF_DEBUG_DEPTH], ' ', 48);\
+                          snprintf(logbuf[(bi++) % TUSB_ALIF_DEBUG_DEPTH], 48, __VA_ARGS__);
+char logbuf[TUSB_ALIF_DEBUG_DEPTH][48];
+int bi = 0;
+#else
+#define LOG(...)      memset(logbuf, ' ', 48);\
+                          snprintf(logbuf, 48, __VA_ARGS__)
+char logbuf[48];
+#endif
 #else
 #define LOG(...)
 #endif
@@ -41,6 +56,10 @@ static uint8_t _dcd_start_xfer(uint8_t ep, void* buf, uint32_t size, uint8_t typ
 
 static void _dcd_handle_depevt(uint8_t ep, uint8_t evt, uint8_t sts, uint16_t par);
 static void _dcd_handle_devt(uint8_t evt, uint16_t info);
+
+/// API Extension --------------------------------------------------------------
+
+void dcd_uninit(void);
 
 /// Device Setup ---------------------------------------------------------------
 
@@ -81,8 +100,7 @@ void dcd_init(uint8_t rhport)
     // set device speed (USBHS only)
     udev->dcfg_b.devspd = 0x0; // HS, this will need #if condition [TODO]
 
-    // allocate buffers for events
-    // write gevntadr with addr to the event buffer (remember addr map)
+    // allocate ring buffer for events
     memset(_evnt_buf, 0, sizeof(_evnt_buf));
     RTSS_CleanDCache_by_Addr(_evnt_buf, sizeof(_evnt_buf));
     _evnt_tail = _evnt_buf;
@@ -150,6 +168,9 @@ void dcd_int_handler(uint8_t rhport)
     // process failures first
     if (ugbl->gsts_b.device_ip) {
         if (ugbl->gsts_b.csrtimeout || ugbl->gsts_b.buserraddrvld) {
+            // buserraddrvld is usually set when USB tries to write to protected
+            // memory region. Check linker script to ensure USB event buffer and
+            // TRBs reside in bulk memory or other NS-allowed region
             __BKPT(0);
         }
     }
@@ -342,20 +363,21 @@ bool dcd_edpt_xfer(uint8_t rhport, uint8_t ep_addr, uint8_t * buffer, uint16_t t
             }
         } break;
         case 1: { // CONTROL IN
-            // DATA OUT in control reads is not supported
-            // thus the only STATUS IN we produce is for data-less control txn
-            uint8_t type = TRBCTL_CTL_STAT2;
             _xfer_bytes[1] = total_bytes;
 
             if (0 < total_bytes) {
                 RTSS_CleanDCache_by_Addr(buffer, total_bytes);
-                type = _ctrl_long_data ? TRBCTL_NORMAL : TRBCTL_CTL_DATA;
+                uint8_t type = _ctrl_long_data ? TRBCTL_NORMAL : TRBCTL_CTL_DATA;
                 if (64 == total_bytes) {
                     _ctrl_long_data = true;
                 }
+                _dcd_start_xfer(1, buffer, total_bytes, type);
+            } else {
+                // status events are handled directly from the ISR when USB
+                // controller triggers XferNotReady event for status stage
+                dcd_event_xfer_complete(rhport, tu_edpt_addr(0, TUSB_DIR_IN),
+                                        0, XFER_RESULT_SUCCESS, false);
             }
-
-            _dcd_start_xfer(1, buffer, total_bytes, type);
         } break;
         default: { // DATA EPs (BULK & INTERRUPT only)
             _xfer_bytes[ep] = total_bytes;
@@ -367,7 +389,7 @@ bool dcd_edpt_xfer(uint8_t rhport, uint8_t ep_addr, uint8_t * buffer, uint16_t t
             }
             uint8_t ret = _dcd_start_xfer(ep, buffer, total_bytes,
                             total_bytes ? TRBCTL_NORMAL : TRBCTL_NORMAL_ZLP);
-            (void)ret;
+            (void) ret;
             LOG("start xfer sts %u", ret);
         }
     }
@@ -403,6 +425,14 @@ void dcd_edpt_clear_stall(uint8_t rhport, uint8_t ep_addr)
     _dcd_cmd_wait(ep, CMDTYP_DEPCSTALL, 0);
 }
 
+void dcd_uninit(void)
+{
+    CLKCTL_PER_MST->USB_CTRL2 |= 1 << 8; // set usb phy power-on-reset signal
+    enable_usb_phy_isolation(); // enable usb phy isolation
+    disable_usb_phy_power(); // power down usb phy
+    disable_usb_periph_clk(); // disable usb peripheral clock
+}
+
 MP_WEAK void USB_IRQHandler(void)
 {
     dcd_int_handler(TUD_OPT_RHPORT);
@@ -411,17 +441,21 @@ MP_WEAK void USB_IRQHandler(void)
 
 static uint8_t _dcd_cmd_wait(uint8_t ep, uint8_t typ, uint16_t param)
 {
+    // capture phy state and disable lpm and suspend
     uint32_t phycfg = ugbl->gusb2phycfg0;
     ugbl->gusb2phycfg0_b.enblslpm = 0;
     ugbl->gusb2phycfg0_b.suspendusb20 = 0;
 
+    // set up command in depcmd register
     udev->depcmd[ep].depcmd_b.cmdtyp = typ;
     udev->depcmd[ep].depcmd_b.cmdioc = 0;
     udev->depcmd[ep].depcmd_b.commandparam = param;
 
+    // dispatch command and wait for completion
     udev->depcmd[ep].depcmd_b.cmdact = 1;
     while(0 != udev->depcmd[ep].depcmd_b.cmdact);
 
+    // restore phy state
     ugbl->gusb2phycfg0 = phycfg;
 
     return udev->depcmd[ep].depcmd_b.cmdstatus;
@@ -436,7 +470,7 @@ static void _dcd_handle_depevt(uint8_t ep, uint8_t evt, uint8_t sts, uint16_t pa
         case DEPEVT_XFERCOMPLETE: {
             LOG("Transfer complete");
             RTSS_InvalidateDCache_by_Addr(_xfer_trb[ep], sizeof(_xfer_trb[0]));
-            if (0 == ep) { // [TODO] validate EP stage (CONTROL_STAGE_SETUP)
+            if (0 == ep) {
                 uint8_t trbctl = (_xfer_trb[0][3] >> 4) & 0x3F;
                 LOG("ep0 xfer trb3 = %08x", _xfer_trb[0][3]);
                 if (TRBCTL_CTL_SETUP == trbctl) {
@@ -460,10 +494,13 @@ static void _dcd_handle_depevt(uint8_t ep, uint8_t evt, uint8_t sts, uint16_t pa
                     __BKPT(0);
                 }
             } else if (1 == ep) {
+                uint8_t trbctl = (_xfer_trb[1][3] >> 4) & 0x3F;
                 LOG("ep1 xfer trb3 = %08x trb2 = %08x", _xfer_trb[1][3], _xfer_trb[1][2]);
-                dcd_event_xfer_complete(TUD_OPT_RHPORT, tu_edpt_addr(0, TUSB_DIR_IN),
-                                        _xfer_bytes[1] - (_xfer_trb[1][2] & 0xFFFFFF),
-                                        XFER_RESULT_SUCCESS, true);
+                if (TRBCTL_CTL_STAT2 != trbctl) { // STATUS IN notification is done at xfer request
+                    dcd_event_xfer_complete(TUD_OPT_RHPORT, tu_edpt_addr(0, TUSB_DIR_IN),
+                                            _xfer_bytes[1] - (_xfer_trb[1][2] & 0xFFFFFF),
+                                            XFER_RESULT_SUCCESS, true);
+                }
             } else {
                 // [TODO] check if ep is open
                 LOG("ep%u xfer trb3 = %08x trb2 = %08x", ep, _xfer_trb[ep][3], _xfer_trb[ep][2]);
@@ -485,11 +522,37 @@ static void _dcd_handle_depevt(uint8_t ep, uint8_t evt, uint8_t sts, uint16_t pa
         } break;
         case DEPEVT_XFERNOTREADY: {
             LOG("Transfer not ready: %s", sts & 8 ? "no TRB" : "no XFER");
-            // this event is received when host tries to send/receive data but
-            // there's no trb set up for it
+
+            // XferNotReady NotActive for status stage
+            if ((1 == ep) && (0b0010 == (sts & 0b1011))) {
+                _dcd_start_xfer(1, NULL, 0, TRBCTL_CTL_STAT2);
+                break;
+            }
+
             if (0 == ep) {
                 // with TinyUSB implementing this isn't really necessary
-                // the stack will control the flow of the transactions by itself
+                // the stack will control the flow EP0 by itself
+            }
+
+            if ((1 > ep) && (sts & (1 << 3))) {
+                if (_xfer_trb[ep][3] & (1 << 0)) { // transfer was configured
+                    // dependxfer can only block when actbitlater is set
+                    ugbl->guctl2_b.rst_actbitlater = 1;
+                    _dcd_cmd_wait(ep, CMDTYP_DEPENDXFER, 0);
+                    ugbl->guctl2_b.rst_actbitlater = 0;
+
+                    // reset the trb byte count and clean the cache
+                    RTSS_InvalidateDCache_by_Addr(_xfer_trb[ep], sizeof(_xfer_trb[0]));
+                    _xfer_trb[ep][2] = _xfer_bytes[ep];
+                    RTSS_CleanDCache_by_Addr(_xfer_trb[ep], sizeof(_xfer_trb[ep]));
+
+                    // prepare ep command
+                    udev->depcmd[ep].par1 = (uint32_t) LocalToGlobal(_xfer_trb[ep]);
+                    udev->depcmd[ep].par0 = 0;
+
+                    // issue the block command and pass the status
+                    _dcd_cmd_wait(ep, CMDTYP_DEPSTRTXFER, 0);
+                }
             }
         } break;
         case DEPEVT_EPCMDCMPLT: {
@@ -558,8 +621,10 @@ static void _dcd_handle_devt(uint8_t evt, uint16_t info)
 
 static uint8_t _dcd_start_xfer(uint8_t ep, void* buf, uint32_t size, uint8_t type)
 {
+    dcd_int_disable(TUD_OPT_RHPORT); // prevent race conditions
+
     // program the trb and clean the cache
-    _xfer_trb[ep][0] = (uint32_t) LocalToGlobal(buf);
+    _xfer_trb[ep][0] = buf ? (uint32_t) LocalToGlobal(buf) : 0;
     _xfer_trb[ep][1] = 0;
     _xfer_trb[ep][2] = size;
     _xfer_trb[ep][3] = (1 << 11) | (1 << 10) | (type << 4) | (1 << 1) | (1 << 0);
@@ -568,6 +633,8 @@ static uint8_t _dcd_start_xfer(uint8_t ep, void* buf, uint32_t size, uint8_t typ
     // prepare ep command
     udev->depcmd[ep].par1 = (uint32_t) LocalToGlobal(_xfer_trb[ep]);
     udev->depcmd[ep].par0 = 0;
+
+    dcd_int_enable(TUD_OPT_RHPORT);
 
     // issue the block command and pass the status
     return _dcd_cmd_wait(ep, CMDTYP_DEPSTRTXFER, 0);
