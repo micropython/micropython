fed41efa5d8d991c933ba270cd29c7e23f824016
Damien George <damien@micropython.org>
2024-03-07 00:51:35 +1100

    py: Add support for function docstrings.
    This stores the docstring in the function object at compile time.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- tests/basics/docstring.py
+++ tests/basics/docstring.py
@@ -4,15 +4,31 @@
 """Module docstring"""
 
 
+def func_no_docstring():
+    pass
+
+
+def func_with_docstring():
+    """Function docstring"""
+
+
 class A:
     """Class docstring"""
+
+    def method(self):
+        """Method docstring"""
 
 
 if not hasattr(A, "__doc__"):
     print("SKIP")
     raise SystemExit
 
+print(func_no_docstring.__doc__)
+print(func_with_docstring.__doc__)
+
 print(A.__doc__)
 print(A().__doc__)
+print(A.method.__doc__)
+print(A().method.__doc__)
 
 print(globals()["__doc__"])
