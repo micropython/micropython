fa4dc588f00253255889b9a587dcac3b3ff10236
iabdalkader <i.abdalkader@gmail.com>
2024-03-13 09:09:42 +0100

    stm32/boards/ARDUINO_PORTENTA_H7: Add Arduino's external library.
    Signed-off-by: iabdalkader <i.abdalkader@gmail.com>

--- ports/stm32/boards/ARDUINO_PORTENTA_H7/manifest.py
+++ ports/stm32/boards/ARDUINO_PORTENTA_H7/manifest.py
@@ -10,3 +10,10 @@ require("logging")
 
 # Bluetooth
 require("aioble")
+
+# Register external library
+add_library("arduino-lib", "$(ARDUINO_LIB_DIR)")
+
+# RPC
+require("cmwx1", library="arduino-lib")
+require("msgpackrpc", library="arduino-lib")
