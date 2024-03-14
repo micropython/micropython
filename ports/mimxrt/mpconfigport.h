fff66c3069dd2f2b1152cf3412dfbcb073ea804b
Kwabena W. Agyeman <kwagyeman@live.com>
2024-03-14 11:25:56 -0700

    mimxrt/mpconfigport: Enable cryptolib and hashlib md5.
    Signed-off-by: Kwabena W. Agyeman <kwagyeman@live.com>

--- ports/mimxrt/mpconfigport.h
+++ ports/mimxrt/mpconfigport.h
@@ -131,9 +131,9 @@ uint32_t trng_random_u32(void);
 #define MICROPY_PY_WEBSOCKET                (MICROPY_PY_LWIP)
 #define MICROPY_PY_WEBREPL                  (MICROPY_PY_LWIP)
 #define MICROPY_PY_LWIP_SOCK_RAW            (MICROPY_PY_LWIP)
-// #define MICROPY_PY_HASHLIB_MD5              (MICROPY_PY_SSL)
+#define MICROPY_PY_HASHLIB_MD5              (MICROPY_PY_SSL)
 #define MICROPY_PY_HASHLIB_SHA1             (MICROPY_PY_SSL)
-// #define MICROPY_PY_CRYPTOLIB                (MICROPY_PY_SSL)
+#define MICROPY_PY_CRYPTOLIB                (MICROPY_PY_SSL)
 
 #ifndef MICROPY_PY_BLUETOOTH_ENABLE_CENTRAL_MODE
 #define MICROPY_PY_BLUETOOTH_ENABLE_CENTRAL_MODE (1)
