b6399e24122736c57c6128e5d4196d10716616d9
Bill Sideris <bill88t@feline.gr>
2024-03-14 15:50:56 +0200

    Treat 4way as authfail, in a the desired way

--- ports/espressif/common-hal/wifi/Radio.c
+++ ports/espressif/common-hal/wifi/Radio.c
@@ -376,7 +376,10 @@ wifi_radio_error_t common_hal_wifi_radio_connect(wifi_radio_obj_t *self, uint8_t
     } while ((bits & (WIFI_CONNECTED_BIT | WIFI_DISCONNECTED_BIT)) == 0 && !mp_hal_is_interrupted());
 
     if ((bits & WIFI_DISCONNECTED_BIT) != 0) {
-        if (self->last_disconnect_reason == WIFI_REASON_AUTH_FAIL) {
+        if (
+            (self->last_disconnect_reason == WIFI_REASON_AUTH_FAIL) ||
+            (self->last_disconnect_reason == WIFI_REASON_4WAY_HANDSHAKE_TIMEOUT)
+            ) {
             return WIFI_RADIO_ERROR_AUTH_FAIL;
         } else if (self->last_disconnect_reason == WIFI_REASON_NO_AP_FOUND) {
             return WIFI_RADIO_ERROR_NO_AP_FOUND;
