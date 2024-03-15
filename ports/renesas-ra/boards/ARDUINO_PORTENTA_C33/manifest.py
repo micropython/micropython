b62b91c2b706ac9aae731a839826ae598c27e766
iabdalkader <i.abdalkader@gmail.com>
2024-03-15 12:05:30 +0100

    renesas-ra/boards/ARDUINO_PORTENTA_C33: Add Arduino's external library.
    Signed-off-by: iabdalkader <i.abdalkader@gmail.com>

--- ports/renesas-ra/boards/ARDUINO_PORTENTA_C33/manifest.py
+++ ports/renesas-ra/boards/ARDUINO_PORTENTA_C33/manifest.py
@@ -10,3 +10,9 @@ require("logging")
 
 # Bluetooth
 require("aioble")
+
+# Register external library
+add_library("arduino-lib", "$(ARDUINO_LIB_DIR)")
+
+# CMWX1 Lora module.
+require("cmwx1", library="arduino-lib")
