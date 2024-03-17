7d2e46aa19ab7a6d29b268d5ab915298464f0eb4
Jeff Epler <jepler@gmail.com>
2024-03-17 14:01:37 -0500

    Update adafruit_floppy

--- shared-module/floppyio/__init__.c
+++ shared-module/floppyio/__init__.c
@@ -25,6 +25,7 @@
  */
 
 #include "py/runtime.h"
+#include "py/mphal.h"
 
 #include "shared-bindings/time/__init__.h"
 #include "shared-bindings/floppyio/__init__.h"
@@ -38,17 +39,18 @@
 #define T3_5 (FLOPPYIO_SAMPLERATE * 7 / 2 / 1000000)
 #endif
 
-#define MFM_IO_MMIO (1)
 #include "lib/adafruit_floppy/src/mfm_impl.h"
 
+MP_WEAK
 __attribute__((optimize("O3")))
-int common_hal_floppyio_flux_readinto(void *buf, size_t len, digitalio_digitalinout_obj_t *data, digitalio_digitalinout_obj_t *index) {
+int common_hal_floppyio_flux_readinto(void *buf, size_t len, digitalio_digitalinout_obj_t *data, digitalio_digitalinout_obj_t *index, mp_int_t index_wait_ms) {
     uint32_t index_mask;
     volatile uint32_t *index_port = common_hal_digitalio_digitalinout_get_reg(index, DIGITALINOUT_REG_READ, &index_mask);
 
     uint32_t data_mask;
     volatile uint32_t *data_port = common_hal_digitalio_digitalinout_get_reg(data, DIGITALINOUT_REG_READ, &data_mask);
 
+    uint32_t index_deadline_ms = mp_hal_ticks_ms() + index_wait_ms;
 #undef READ_INDEX
 #undef READ_DATA
 #define READ_INDEX() (!!(*index_port & index_mask))
@@ -62,6 +64,9 @@ int common_hal_floppyio_flux_readinto(void *buf, size_t len, digitalio_digitalin
 
     // wait for index pulse low
     while (READ_INDEX()) { /* NOTHING */
+        if (supervisor_ticks_ms32() > index_deadline_ms) {
+            return 0;
+        }
     }
 
 
@@ -96,15 +101,17 @@ int common_hal_floppyio_flux_readinto(void *buf, size_t len, digitalio_digitalin
     return pulses_ptr - pulses;
 }
 
-int common_hal_floppyio_mfm_readinto(void *buf, size_t n_sectors, digitalio_digitalinout_obj_t *data, digitalio_digitalinout_obj_t *index) {
-    mfm_io_t io;
-    io.index_port = common_hal_digitalio_digitalinout_get_reg(index, DIGITALINOUT_REG_READ, &io.index_mask);
-    io.data_port = common_hal_digitalio_digitalinout_get_reg(data, DIGITALINOUT_REG_READ, &io.data_mask);
-
-    common_hal_mcu_disable_interrupts();
-    uint8_t validity[n_sectors];
-    int result = read_track(io, n_sectors, buf, validity);
-    common_hal_mcu_enable_interrupts();
+int common_hal_floppyio_mfm_readinto(const mp_buffer_info_t *buf, const mp_buffer_info_t *flux_buf, uint8_t *validity, size_t t2_max, size_t t3_max) {
+    mfm_io_t io = {
+        .T2_max = t2_max,
+        .T3_max = t3_max,
+        .pulses = flux_buf->buf,
+        .n_pulses = flux_buf->len,
+        .sectors = buf->buf,
+        .sector_validity = validity,
+        .n_sectors = buf->len / 512,
+    };
+    int result = decode_track_mfm(&io);
 
     return result;
 }
