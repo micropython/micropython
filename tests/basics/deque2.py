5e5c40bdb5695cd8cd46ca933a235d021eeb1b6e
Damien George <damien@micropython.org>
2024-03-18 22:41:28 +1100

    tests/basics: Split MicroPython-specific deque tests to separate file.
    So that the MicroPython-specific behaviour can be isolated, and the CPython
    compatible test don't need a .exp file.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- tests/basics/deque2.py
+++ tests/basics/deque2.py
@@ -1,5 +1,3 @@
-# Tests for deques with "check overflow" flag and other extensions
-# wrt to CPython.
 try:
     from collections import deque
 except ImportError:
@@ -43,69 +41,3 @@ try:
     d[4] = 0
 except IndexError:
     print("IndexError")
-
-# Removing elements with del is not supported, fall back on mp_obj_subscr() error message
-try:
-    del d[0]
-except TypeError:
-    print("TypeError")
-
-
-# Only fixed-size deques are supported, so length arg is mandatory
-try:
-    deque(())
-except TypeError:
-    print("TypeError")
-
-d = deque((), 2, True)
-
-try:
-    d.popleft()
-except IndexError:
-    print("IndexError")
-
-try:
-    d.pop()
-except IndexError:
-    print("IndexError")
-
-print(d.append(1))
-print(d.popleft())
-
-d.append(2)
-print(d.popleft())
-
-d.append(3)
-d.append(4)
-print(d.popleft(), d.popleft())
-try:
-    d.popleft()
-except IndexError as e:
-    print(repr(e))
-
-try:
-    d.pop()
-except IndexError as e:
-    print(repr(e))
-
-d.append(5)
-d.append(6)
-print(len(d))
-try:
-    d.append(7)
-except IndexError as e:
-    print(repr(e))
-
-try:
-    d.appendleft(8)
-except IndexError as e:
-    print(repr(e))
-
-print(len(d))
-
-print(d.popleft(), d.popleft())
-print(len(d))
-try:
-    d.popleft()
-except IndexError as e:
-    print(repr(e))
