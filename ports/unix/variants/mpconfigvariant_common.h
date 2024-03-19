b80607ecafeb47664b35e4f255ed43171719ecf5
Damien George <damien@micropython.org>
2024-03-19 23:09:11 +1100

    unix/variants: Don't use native _Float16 type.
    Using it increases code size by about 2k.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/unix/variants/mpconfigvariant_common.h
+++ ports/unix/variants/mpconfigvariant_common.h
@@ -41,6 +41,11 @@
 #define MICROPY_FLOAT_IMPL             (MICROPY_FLOAT_IMPL_DOUBLE)
 #endif
 
+// Don't use native _Float16 because it increases code size by a lot.
+#ifndef MICROPY_FLOAT_USE_NATIVE_FLT16
+#define MICROPY_FLOAT_USE_NATIVE_FLT16 (0)
+#endif
+
 // Enable arbitrary precision long-int by default.
 #ifndef MICROPY_LONGINT_IMPL
 #define MICROPY_LONGINT_IMPL           (MICROPY_LONGINT_IMPL_MPZ)
