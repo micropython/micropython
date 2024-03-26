221eca33d28e0871f6f01cc00908e33c29eac01a
IFX-Anusha <Anusha.TR@infineon.com>
2024-03-26 11:37:16 +0530

    tests/psoc6: Fix Ruff.
    Signed-off-by: IFX-Anusha <Anusha.TR@infineon.com>

--- tests/psoc6/test_inputs/main.py
+++ tests/psoc6/test_inputs/main.py
@@ -8,20 +8,18 @@ from machine import bitstream
 # Allocate pin based on board
 machine = os.uname().machine
 if "CY8CPROTO-062-4343W" in machine:
-    gpio_pin = 'P12_1'
-    input_pin = 'P13_6'
+    gpio_pin = "P12_1"
+    input_pin = "P13_6"
 elif "CY8CPROTO-063-BLE" in machine:
-    gpio_pin = 'P5_2'
-    input_pin = 'P12_7'
+    gpio_pin = "P5_2"
+    input_pin = "P12_7"
 
 print("bitstream execution")
 timing = [3000000, 1000000, 8000000, 5000000]
 buf = bytearray([0xF0])
 p0 = Pin(gpio_pin, Pin.OUT, value=0)
 input_pin = Pin(input_pin, Pin.IN)
-while(input_pin.value()==1):
+while input_pin.value() == 1:
     pass
 for i in range(2):
     bitstream(p0, 0, timing, buf)
-
-
