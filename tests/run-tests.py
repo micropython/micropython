e33acf89c038d7e16d2c58712759247c11d44b60
stijn <stijn@ignitron.net>
2024-03-26 16:11:11 +0100

    tests/run-tests.py: Make Windows test skipping more granular.
    Signed-off-by: stijn <stijn@ignitron.net>

--- tests/run-tests.py
+++ tests/run-tests.py
@@ -580,7 +580,7 @@ def run_tests(pyb, tests, args, result_dir, num_threads=1):
     if os.getenv("GITHUB_ACTIONS") == "true":
         skip_tests.add("thread/stress_schedule.py")  # has reliability issues
 
-        if os.getenv("RUNNER_OS") == "Windows":
+        if os.getenv("RUNNER_OS") == "Windows" and os.getenv("CI_BUILD_CONFIGURATION") == "Debug":
             # fails with stack overflow on Debug builds
             skip_tests.add("misc/sys_settrace_features.py")
 
