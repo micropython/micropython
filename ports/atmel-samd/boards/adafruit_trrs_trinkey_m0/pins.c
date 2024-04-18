1c3112b5405420069a992119ac98164677a0d171
Dan Halbert <halbert@halwitz.org>
2024-04-18 13:11:28 -0400

    Adafruit TRRS Trinkey M0

---
+++ ports/atmel-samd/boards/adafruit_trrs_trinkey_m0/pins.c
@@ -0,0 +1,16 @@
+#include "shared-bindings/board/__init__.h"
+
+STATIC const mp_rom_map_elem_t board_module_globals_table[] = {
+    CIRCUITPYTHON_BOARD_DICT_STANDARD_ITEMS
+
+    { MP_ROM_QSTR(MP_QSTR_NEOPIXEL), MP_ROM_PTR(&pin_PA01) },
+
+    { MP_ROM_QSTR(MP_QSTR_TIP), MP_ROM_PTR(&pin_PA02) },
+    { MP_ROM_QSTR(MP_QSTR_TIP_SWITCH), MP_ROM_PTR(&pin_PA03) },
+    { MP_ROM_QSTR(MP_QSTR_RING_2), MP_ROM_PTR(&pin_PA04) },
+    { MP_ROM_QSTR(MP_QSTR_SLEEVE), MP_ROM_PTR(&pin_PA05) },
+    { MP_ROM_QSTR(MP_QSTR_RING_1), MP_ROM_PTR(&pin_PA06) },
+    { MP_ROM_QSTR(MP_QSTR_RING_1_SWITCH), MP_ROM_PTR(&pin_PA07) },
+
+};
+MP_DEFINE_CONST_DICT(board_module_globals, board_module_globals_table);
