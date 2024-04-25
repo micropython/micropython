fcd5938f7eeebe157e6d29cf47cc3c8976019972
Bill Sideris <bill88t@feline.gr>
2024-04-25 18:10:54 +0300

    Use IO0 for safemode

--- ports/espressif/boards/wemos_lolin32_lite/mpconfigboard.h
+++ ports/espressif/boards/wemos_lolin32_lite/mpconfigboard.h
@@ -32,6 +32,9 @@
 #define MICROPY_HW_LED_STATUS       (&pin_GPIO22)
 #define MICROPY_HW_LED_STATUS_INVERTED (1)
 
+// For entering safe mode, use SW38 button
+#define CIRCUITPY_BOOT_BUTTON       (&pin_GPIO0)
+
 // UART pins attached to the USB-serial converter chip
 #define CIRCUITPY_CONSOLE_UART_TX (&pin_GPIO1)
 #define CIRCUITPY_CONSOLE_UART_RX (&pin_GPIO3)
