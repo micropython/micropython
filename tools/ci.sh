285c7cd9b3b7d4deaa4cb5d0a084bfd8c52ee5b9
Maureen Helm <maureen.helm@analog.com>
2024-04-27 14:08:44 -0500

    zephyr: Upgrade to Zephyr v3.6.0.
    Updates the Zephyr port build instructions and CI to use the latest
    Zephyr release tag.
    
    Tested on frdm_k64f.
    
    Signed-off-by: Maureen Helm <maureen.helm@analog.com>

--- tools/ci.sh
+++ tools/ci.sh
@@ -737,7 +737,7 @@ function ci_windows_build {
 
 ZEPHYR_DOCKER_VERSION=v0.26.7
 ZEPHYR_SDK_VERSION=0.16.5
-ZEPHYR_VERSION=v3.5.0
+ZEPHYR_VERSION=v3.6.0
 
 function ci_zephyr_setup {
     docker pull zephyrprojectrtos/ci:${ZEPHYR_DOCKER_VERSION}
