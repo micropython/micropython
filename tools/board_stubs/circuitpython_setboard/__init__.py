933efe082e80e16f44d6125cb2e8d4553a9a4f0c
elpekenin <elpekenin@elpekenin.dev>
2024-05-03 16:53:13 +0200

    add `--list` to stubs setboard

--- tools/board_stubs/circuitpython_setboard/__init__.py
+++ tools/board_stubs/circuitpython_setboard/__init__.py
@@ -1,26 +1,42 @@
 # SPDX-FileCopyrightText: 2024 Tim Cocks
 #
 # SPDX-License-Identifier: MIT
-from importlib import resources
-import os
+import argparse
 import sys
 import shutil
+from importlib import resources
 
 
 def set_board():
-    if len(sys.argv) != 2:
-        print(f"Incorrect args. Please call with: 'circuitpython_setboard chosen_board'")
-        return
+    parser = argparse.ArgumentParser(
+        prog=__name__,
+        usage="Install CircuitPython board-specific stubs",
+    )
+    parser.add_argument("chosen_board", help="selected board", nargs="?")
+    parser.add_argument("-l", "--list", help="show available boards", action="store_true")
 
-    chosen_board = sys.argv[1]
-    print(f"setting board: {chosen_board}")
+    args = parser.parse_args()
+
+    if args.list:
+        sys.stdout.write("Available boards are: \n")
+
+        for board in resources.files("board_definitions").iterdir():
+            sys.stdout.write(f"{board.name}\n")
+
+        sys.exit(0)
+
+    if args.chosen_board is None:
+        sys.stderr.write("Must select a board")
+        sys.exit(1)
+
+    print(f"setting board: {args.chosen_board}")
 
     board_stubs_file = resources.files("board-stubs").joinpath("__init__.pyi")
-    board_definitions_path = resources.files("board_definitions").joinpath(chosen_board)
+    board_definitions_path = resources.files("board_definitions").joinpath(args.chosen_board)
     board_definitions_file = board_definitions_path.joinpath("__init__.pyi")
 
     if not board_definitions_file.is_file():
-        print(f"Board: '{chosen_board}' was not found")
-        return
+        print(f"Board: '{args.chosen_board}' was not found")
+        sys.exit(1)
 
     shutil.copyfile(board_definitions_file, board_stubs_file)
