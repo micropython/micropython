49e0f5ab476db56a45fd14f2781afecc0932aa9e
elpekenin <elpekenin@elpekenin.dev>
2024-05-06 21:21:36 +0200

    search by port, dont print dev info

--- tools/board_stubs/circuitpython_setboard/__init__.py
+++ tools/board_stubs/circuitpython_setboard/__init__.py
@@ -9,7 +9,7 @@ from importlib import resources
 from importlib.abc import Traversable
 
 
-def get_definitions_or_raise(board: str) -> Traversable:
+def get_definitions_or_exit(board: str) -> Traversable:
     """Get the definitions file for a board given its name."""
 
     path = resources.files("board_definitions").joinpath(board)
@@ -22,10 +22,10 @@ def get_definitions_or_raise(board: str) -> Traversable:
     return file
 
 
-def get_doc_or_raise(board: str) -> str:
+def get_doc_or_exit(board: str) -> str:
     """Get the docstring for a board given its name."""
 
-    with get_definitions_or_raise(board).open("r") as f:
+    with get_definitions_or_exit(board).open("r") as f:
         return f.read().split('"""')[1]
 
 
@@ -52,14 +52,10 @@ def set_board():
     if args.list:
         port_boards: defaultdict[str, list[str]] = defaultdict(list)
 
-        for board in resources.files("board_definitions").iterdir():
-            # if we receive both the list flag and a board name, use it as filter
-            if (
-                args.chosen_board is not None
-                and args.chosen_board.lower() not in board.name.lower()
-            ):
-                continue
+        # NOTE: "" in some_str == True
+        looking_for = "" if args.chosen_board is None else args.chosen_board.lower()
 
+        for board in resources.files("board_definitions").iterdir():
             # NOTE: For the hand-crafted finding of port in the docstring, its
             #       format is assumed to be:
             #
@@ -72,8 +68,11 @@ def set_board():
             #  - Frozen libraries: ...
             #
 
-            lines = get_doc_or_raise(board).split("\n")
+            lines = get_doc_or_exit(board).split("\n")
             port = lines[2].split("-")[1].split(":")[1].strip()
+
+            if looking_for not in board.name.lower() and looking_for not in port.lower():
+                continue
 
             port_boards[port].append(board.name)
 
@@ -98,11 +97,7 @@ def set_board():
         sys.stderr.write("Must select a board\n")
         sys.exit(1)
 
-    board_definitions_file = get_definitions_or_raise(args.chosen_board)
+    board_definitions_file = get_definitions_or_exit(args.chosen_board)
 
     board_stubs_file = resources.files("board-stubs").joinpath("__init__.pyi")
     shutil.copyfile(board_definitions_file, board_stubs_file)
-
-    sys.stdout.write(
-        header("Information about the board") + get_doc_or_raise(args.chosen_board) + "\n"
-    )
