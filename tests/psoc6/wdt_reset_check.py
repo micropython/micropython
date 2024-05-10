e53353d63f0ffbd09aba0ae3cf45178b93fa3b0a
enriquezgarc <enriquezgarcia.external@infineon.com>
2024-05-10 10:58:41 +0200

    tests/psoc6/wdt_reset_cause.py: Excluded 063-BLE from test.
    Signed-off-by: enriquezgarc <enriquezgarcia.external@infineon.com>

--- tests/psoc6/wdt_reset_check.py
+++ tests/psoc6/wdt_reset_check.py
@@ -1,6 +1,11 @@
 import machine
 import time
 
+if "CY8CPROTO-063-BLE" in machine:
+    # TODO: Remove after reviewing reset_cause() for 063-BLE.
+    print("SKIP")
+    raise SystemExit
+
 """ Note: Run this test only after wdt.py tests to check if a Watchdog reset has occurred """
 
 print("\n***** Test 1: Check if WDT triggered reset *****\n")
@@ -8,4 +13,5 @@ print("\n***** Test 1: Check if WDT triggered reset *****\n")
 if machine.reset_cause() == machine.WDT_RESET:
     print("PASS")
 else:
+    print(machine.reset_cause())
     print("FAIL")
