2834b7cc55b73a8f099207f48697fc8e9a1afa2c
enriquezgarc <enriquezgarcia.external@infineon.com>
2024-05-10 13:29:08 +0200

    tests/psoc6/wdt_reset_cause.py: Skipped until reset_cause() fix.
    Signed-off-by: enriquezgarc <enriquezgarcia.external@infineon.com>

--- tests/psoc6/wdt_reset_check.py
+++ tests/psoc6/wdt_reset_check.py
@@ -2,11 +2,11 @@ import machine
 import time
 import os
 
-board = os.uname().machine
-if "CY8CPROTO-063-BLE" in board:
-    # TODO: Remove after reviewing reset_cause() for 063-BLE.
-    print("SKIP")
-    raise SystemExit
+# board = os.uname().machine
+# if "CY8CPROTO-063-BLE" in board:
+#     # TODO: Remove after reviewing reset_cause() for 063-BLE.
+print("SKIP")
+raise SystemExit
 
 """ Note: Run this test only after wdt.py tests to check if a Watchdog reset has occurred """
 
