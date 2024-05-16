56ef699cd086964d408d085258adf62108c5d4cc
enriquezgarc <enriquezgarcia.external@infineon.com>
2024-05-16 10:16:40 +0200

    ports/psoc6/mpconfigport.h: Removed commented not needed macro.
    Signed-off-by: enriquezgarc <enriquezgarcia.external@infineon.com>

--- ports/psoc6/mpconfigport.h
+++ ports/psoc6/mpconfigport.h
@@ -138,7 +138,6 @@
 #define MICROPY_PY_MACHINE_I2S_RING_BUF         (1)
 #define MICROPY_PY_MACHINE_I2S_FINALISER        (1)
 #define MICROPY_PY_MACHINE_I2S_INCLUDEFILE      "ports/psoc6/machine_i2s.c"
-// #define MICROPY_PY_MACHINE_PSOC6_I2S            (1)
 
 // VFS
 #define MICROPY_VFS                             (1)
