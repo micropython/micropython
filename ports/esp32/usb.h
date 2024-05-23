5ae622ef7b31881cb38b56baa66d8aec3d132cf9
Andrew Leech <andrew.leech@planetinnovation.com.au>
2024-05-23 22:08:12 +1000

    esp32: Add automatic bootloader handling for S2 and S3.
    Enables support for the ESP standard DTR/RTS based reboot to bootloader.
    
    Switches from OTG to Serial/Jtag mode to workaround issue discussed
    in: https://github.com/espressif/arduino-esp32/issues/6762
    
    Signed-off-by: Andrew Leech <andrew@alelec.net>

--- ports/esp32/usb.h
+++ ports/esp32/usb.h
@@ -29,5 +29,6 @@
 #define MICROPY_HW_USB_CDC_TX_TIMEOUT_MS (500)
 
 void usb_init(void);
+void usb_usj_mode(void);
 
 #endif // MICROPY_INCLUDED_ESP32_USB_H
--- shared/tinyusb/mp_usbd_cdc.c
+++ shared/tinyusb/mp_usbd_cdc.c
@@ -138,15 +138,25 @@ void tud_sof_cb(uint32_t frame_count) {
 
 #endif
 
-#if MICROPY_HW_ENABLE_USBDEV && (MICROPY_HW_USB_CDC_1200BPS_TOUCH || MICROPY_HW_USB_CDC)
+#if MICROPY_HW_ENABLE_USBDEV && ( \
+    MICROPY_HW_USB_CDC_1200BPS_TOUCH || \
+    MICROPY_HW_USB_CDC || \
+    MICROPY_HW_USB_CDC_DTR_RTS_BOOTLOADER)
 
-#if MICROPY_HW_USB_CDC_1200BPS_TOUCH
+#if MICROPY_HW_USB_CDC_1200BPS_TOUCH || MICROPY_HW_USB_CDC_DTR_RTS_BOOTLOADER
 static mp_sched_node_t mp_bootloader_sched_node;
 
 static void usbd_cdc_run_bootloader_task(mp_sched_node_t *node) {
     mp_hal_delay_ms(250);
     machine_bootloader(0, NULL);
 }
+#endif
+
+#if MICROPY_HW_USB_CDC_DTR_RTS_BOOTLOADER
+static struct {
+    bool dtr : 1;
+    bool rts : 1;
+} prev_line_state = {0};
 #endif
 
 void tud_cdc_line_state_cb(uint8_t itf, bool dtr, bool rts) {
@@ -158,6 +168,15 @@ void tud_cdc_line_state_cb(uint8_t itf, bool dtr, bool rts) {
         cdc_connected_flush_delay = (tud_speed_get() == TUSB_SPEED_HIGH) ? 128 : 16;
         tud_sof_cb_enable(true);
     }
+    #endif
+    #if MICROPY_HW_USB_CDC_DTR_RTS_BOOTLOADER
+    if (dtr && !rts) {
+        if (prev_line_state.rts && !prev_line_state.dtr) {
+            mp_sched_schedule_node(&mp_bootloader_sched_node, usbd_cdc_run_bootloader_task);
+        }
+    }
+    prev_line_state.rts = rts;
+    prev_line_state.dtr = dtr;
     #endif
     #if MICROPY_HW_USB_CDC_1200BPS_TOUCH
     if (dtr == false && rts == false) {
