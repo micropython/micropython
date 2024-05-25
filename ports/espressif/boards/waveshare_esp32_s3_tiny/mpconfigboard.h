eb4cd43c707ab10d7c3653c25865df5576604b75
Kyle Mohr <6644803+kylefmohr@users.noreply.github.com>
2024-05-25 01:10:35 +0000

    init

---
+++ ports/espressif/boards/waveshare_esp32_s3_tiny/mpconfigboard.h
@@ -0,0 +1,15 @@
+// This file is part of the CircuitPython project: https://circuitpython.org
+//
+// SPDX-FileCopyrightText: Copyright (c) 2019 Scott Shawcroft for Adafruit Industries
+//
+// SPDX-License-Identifier: MIT
+
+// Micropython setup
+
+#define MICROPY_HW_BOARD_NAME       "Waveshare ESP32-S3-Tiny"
+#define MICROPY_HW_MCU_NAME         "ESP32S3"
+
+#define MICROPY_HW_NEOPIXEL         (&pin_GPIO38)
+
+#define DEFAULT_UART_BUS_RX         (&pin_GPIO19)
+#define DEFAULT_UART_BUS_TX         (&pin_GPIO20)
