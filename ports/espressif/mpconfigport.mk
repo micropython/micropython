8d578670be362d3f462a5dda2bd5ed6fb290c50c
Tuan Nguyen <tuan.nguyen@brtchip.com>
2024-05-26 22:59:13 +0700

    espressif: Enable the _eve module by default for future ESP boards
    -  Refactored build configurations into mpconfigport
    
    Signed-off-by: Tuan Nguyen <tuan.nguyen@brtchip.com>

--- ports/espressif/mpconfigport.mk
+++ ports/espressif/mpconfigport.mk
@@ -45,6 +45,9 @@ CIRCUITPY_TOUCHIO_USE_NATIVE ?= 1
 CIRCUITPY_WATCHDOG ?= 1
 CIRCUITPY_WIFI ?= 1
 
+# Enable _eve module
+CIRCUITPY__EVE ?= 1
+
 # Conditionally turn off modules/features
 ifeq ($(IDF_TARGET),esp32)
 # Modules
@@ -153,6 +156,12 @@ ifeq ($(CIRCUITPY_ESP_FLASH_SIZE),2MB)
 CIRCUITPY_BITMAPFILTER ?= 0
 CIRCUITPY_DUALBANK = 0
 CIRCUITPY_AUDIOMP3 = 0
+endif
+
+
+# No room for _eve on boards with 2MB flash
+ifeq ($(CIRCUITPY_ESP_FLASH_SIZE),2MB)
+CIRCUITPY__EVE = 0
 endif
 
 # Modules dependent on other modules
