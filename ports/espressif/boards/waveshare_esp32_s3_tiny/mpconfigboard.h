2c5e79b004f94fa371000d47a625e757ffd48111
Kyle Mohr <6644803+kylefmohr@users.noreply.github.com>
2024-05-29 17:41:54 +0000

    Fix color ordering of onboard NeoPixel, add `#pragma once` line

--- ports/espressif/boards/waveshare_esp32_s3_tiny/mpconfigboard.h
+++ ports/espressif/boards/waveshare_esp32_s3_tiny/mpconfigboard.h
@@ -4,11 +4,14 @@
 //
 // SPDX-License-Identifier: MIT
 
+#pragma once
+
 // Micropython setup
 
 #define MICROPY_HW_BOARD_NAME       "Waveshare ESP32-S3-Tiny"
 #define MICROPY_HW_MCU_NAME         "ESP32S3"
 
+#define MICROPY_HW_NEOPIXEL_ORDER_GRB (1)
 #define MICROPY_HW_NEOPIXEL         (&pin_GPIO38)
 
 #define DEFAULT_UART_BUS_RX         (&pin_GPIO19)
