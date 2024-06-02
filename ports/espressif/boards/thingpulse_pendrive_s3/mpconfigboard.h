089b3862e4b72df339625276dd429941373aaef1
Daniel Eichhorn <dani.eichhorn@squix.ch>
2024-06-02 17:59:40 +0200

    Add support for ThingPulse Pendrive S3

---
+++ ports/espressif/boards/thingpulse_pendrive_s3/mpconfigboard.h
@@ -0,0 +1,29 @@
+// This file is part of the CircuitPython project: https://circuitpython.org
+//
+// SPDX-FileCopyrightText: Copyright (c) 2019 Scott Shawcroft for Adafruit Industries
+//
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+// Micropython setup
+
+#define MICROPY_HW_BOARD_NAME       "ThingPulse Pendrive S3"
+#define MICROPY_HW_MCU_NAME         "ESP32S3"
+
+#define MICROPY_HW_NEOPIXEL (&pin_GPIO5)
+#define CIRCUITPY_STATUS_LED_POWER (&pin_GPIO21)
+
+#define MICROPY_HW_LED_STATUS (&pin_GPIO13)
+
+#define DEFAULT_I2C_BUS_SCL (&pin_GPIO4)
+#define DEFAULT_I2C_BUS_SDA (&pin_GPIO3)
+
+#define DEFAULT_SPI_BUS_SCK (&pin_GPIO36)
+#define DEFAULT_SPI_BUS_MOSI (&pin_GPIO35)
+#define DEFAULT_SPI_BUS_MISO (&pin_GPIO37)
+
+#define DEFAULT_UART_BUS_RX (&pin_GPIO38)
+#define DEFAULT_UART_BUS_TX (&pin_GPIO39)
+
+#define DOUBLE_TAP_PIN (&pin_GPIO34)
