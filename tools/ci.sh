f116083a393fc521fe293f644763df887f2d3b30
Hans Polders <hans.polders@gmail.com>
2024-06-05 13:15:28 +0200

    esp32: Change minimum supported IDF version to v5.1.2.
    Signed-off-by: Hans Polders <hans.polders@gmail.com>

--- tools/ci.sh
+++ tools/ci.sh
@@ -111,7 +111,7 @@ function ci_cc3200_build {
 # ports/esp32
 
 # GitHub tag of ESP-IDF to use for CI (note: must be a tag or a branch)
-IDF_VER=v5.0.4
+IDF_VER=v5.1.2
 
 export IDF_CCACHE_ENABLE=1
 
