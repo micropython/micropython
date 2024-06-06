ed2f32fadb959fb9e5dbd88f95afa7a08eb64fa6
Scott Shawcroft <scott@tannewt.org>
2024-06-06 16:55:13 -0700

    Enable deep sleep on all ESP chips
    Features of each chip is documented in this spreadsheet:
    https://docs.google.com/spreadsheets/d/1NyKzHOWeWMb8UttDDugeeOANZ4m-idMHM6kiTlUUVIg/edit?usp=sharing
    
    Fixes #9056

--- ports/espressif/boards/espressif_esp32_lyrat/mpconfigboard.mk
+++ ports/espressif/boards/espressif_esp32_lyrat/mpconfigboard.mk
@@ -11,9 +11,6 @@ CIRCUITPY_ESP_PSRAM_SIZE = 4MB
 CIRCUITPY_ESP_PSRAM_MODE = qio
 CIRCUITPY_ESP_PSRAM_FREQ = 40m
 
-# Override partition layout to preserve compatibility because the default has changed.
-FLASH_SIZE_SDKCONFIG = esp-idf-config/sdkconfig-flash-4MB-no-uf2.defaults
-CIRCUITPY_DUALBANK = 1
-CIRCUITPY_BLEIO = 0
+CIRCUITPY_LEGACY_4MB_FLASH_LAYOUT = 1
 
 CIRCUITPY_ESPCAMERA = 0
