867daaa2ace93d424a0a6f8e0bc96017857ad2bb
Alessandro Gatti <a.gatti@frob.it>
2024-06-14 21:15:18 +0200

    tests/extmod/machine_timer: Fix tests.
    `machine.Timer` requires a timer id in its constructor, which was not
    passed when building objects.
    
    Signed-off-by: Alessandro Gatti <a.gatti@frob.it>

--- tests/extmod/machine_timer.py
+++ tests/extmod/machine_timer.py
@@ -9,30 +9,30 @@ except:
     raise SystemExit
 
 # create and deinit
-t = machine.Timer(freq=1)
+t = machine.Timer(0, freq=1)
 t.deinit()
 
 # deinit again
 t.deinit()
 
 # create 2 and deinit
-t = machine.Timer(freq=1)
-t2 = machine.Timer(freq=1)
+t = machine.Timer(0, freq=1)
+t2 = machine.Timer(1, freq=1)
 t.deinit()
 t2.deinit()
 
 # create 2 and deinit in different order
-t = machine.Timer(freq=1)
-t2 = machine.Timer(freq=1)
+t = machine.Timer(0, freq=1)
+t2 = machine.Timer(1, freq=1)
 t2.deinit()
 t.deinit()
 
 # create one-shot timer with callback and wait for it to print (should be just once)
-t = machine.Timer(period=1, mode=machine.Timer.ONE_SHOT, callback=lambda t: print("one-shot"))
+t = machine.Timer(0, period=1, mode=machine.Timer.ONE_SHOT, callback=lambda t: print("one-shot"))
 time.sleep_ms(5)
 t.deinit()
 
 # create periodic timer with callback and wait for it to print
-t = machine.Timer(period=4, mode=machine.Timer.PERIODIC, callback=lambda t: print("periodic"))
+t = machine.Timer(0, period=4, mode=machine.Timer.PERIODIC, callback=lambda t: print("periodic"))
 time.sleep_ms(14)
 t.deinit()
