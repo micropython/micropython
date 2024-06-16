5e56febe4a1ae46e8150a5c31bc2c881526b3865
Alessandro Gatti <a.gatti@frob.it>
2024-06-16 20:40:28 +0200

    mpy-cross: Add RISC-V RV32IMC support in MPY files.
    MPY files can now hold generated RV32IMC native code.  This can be
    accomplished by passing the `-march=rv32imc` flag to mpy-cross.
    
    Signed-off-by: Alessandro Gatti <a.gatti@frob.it>

--- mpy-cross/main.c
+++ mpy-cross/main.c
@@ -129,7 +129,8 @@ static int usage(char **argv) {
         "\n"
         "Target specific options:\n"
         "-msmall-int-bits=number : set the maximum bits used to encode a small-int\n"
-        "-march=<arch> : set architecture for native emitter; x86, x64, armv6, armv6m, armv7m, armv7em, armv7emsp, armv7emdp, xtensa, xtensawin\n"
+        "-march=<arch> : set architecture for native emitter;\n"
+        "                x86, x64, armv6, armv6m, armv7m, armv7em, armv7emsp, armv7emdp, xtensa, xtensawin, rv32imc\n"
         "\n"
         "Implementation specific options:\n", argv[0]
         );
@@ -312,6 +313,9 @@ MP_NOINLINE int main_(int argc, char **argv) {
                 } else if (strcmp(arch, "xtensawin") == 0) {
                     mp_dynamic_compiler.native_arch = MP_NATIVE_ARCH_XTENSAWIN;
                     mp_dynamic_compiler.nlr_buf_num_regs = MICROPY_NLR_NUM_REGS_XTENSAWIN;
+                } else if (strcmp(arch, "rv32imc") == 0) {
+                    mp_dynamic_compiler.native_arch = MP_NATIVE_ARCH_RV32IMC;
+                    mp_dynamic_compiler.nlr_buf_num_regs = MICROPY_NLR_NUM_REGS_RV32I;
                 } else if (strcmp(arch, "host") == 0) {
                     #if defined(__i386__) || defined(_M_IX86)
                     mp_dynamic_compiler.native_arch = MP_NATIVE_ARCH_X86;
