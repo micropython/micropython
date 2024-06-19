b053de23c050a41ed22fc4b5fb2d78bb11b0f2c4
vboxuser <vboxuser@Ubuntu22.04ok.myguest.virtualbox.org>
2024-06-19 15:39:12 -0300

    add ARCHI NEWSAN board

---
+++ ports/raspberrypi/boards/archi/mpconfigboard.mk
@@ -0,0 +1,16 @@
+USB_VID = 0x2E8A
+USB_PID = 0x1043
+USB_PRODUCT = "ARCHI"
+USB_MANUFACTURER = "NEWSAN"
+
+CHIP_VARIANT = RP2040
+CHIP_FAMILY = rp2
+
+EXTERNAL_FLASH_DEVICES = "W25Q32JVxQ"
+
+CIRCUITPY__EVE = 1
+CIRCUITPY_PICODVI = 1
+
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_MPU6050
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Pixel_Framebuf
+FROZEN_MPY_DIRS += $(TOP)/frozen/Adafruit_CircuitPython_Led_Animation
---
+++ ports/raspberrypi/boards/archi/pico-sdk-configboard.h
@@ -0,0 +1,7 @@
+// This file is part of the CircuitPython project: https://circuitpython.org
+//
+// SPDX-FileCopyrightText: Copyright (c) 2021 Scott Shawcroft for Adafruit Industries
+//
+// SPDX-License-Identifier: MIT
+
+// Put board-specific pico-sdk definitions here. This file must exist.
