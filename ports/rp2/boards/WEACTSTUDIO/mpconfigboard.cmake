2a005fc120a45e81483bfa47cd7fe9bd2388dcce
Damien George <damien@micropython.org>
2024-06-24 12:37:13 +1000

    rp2: Rework board variant support to require mpconfigvariant file.
    Following how the board variants now work in the esp32 port.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/rp2/boards/WEACTSTUDIO/mpconfigboard.cmake
+++ ports/rp2/boards/WEACTSTUDIO/mpconfigboard.cmake
@@ -6,19 +6,3 @@ list(APPEND PICO_BOARD_HEADER_DIRS ${MICROPY_BOARD_DIR})
 
 # Freeze board.py
 set(MICROPY_FROZEN_MANIFEST ${MICROPY_BOARD_DIR}/manifest.py)
-
-# Select the 16MB variant as the default
-set(PICO_BOARD "weactstudio_16MiB")
-
-# Provide different variants for the downloads page
-if(MICROPY_BOARD_VARIANT STREQUAL "FLASH_2M")
-    set(PICO_BOARD "weactstudio_2MiB")
-endif()
-
-if(MICROPY_BOARD_VARIANT STREQUAL "FLASH_4M")
-    set(PICO_BOARD "weactstudio_4MiB")
-endif()
-
-if(MICROPY_BOARD_VARIANT STREQUAL "FLASH_8M")
-    set(PICO_BOARD "weactstudio_8MiB")
-endif()
