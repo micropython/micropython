5d84dd3e1686fd05c80c3da565f55ca0fd07bc7c
TianShuang Ke <QinYUN575@Gmail.com>
2024-06-30 20:04:19 +0800

    esp32/esp32c2: fix esp32 timer clock source.
    Signed-off-by: TianShuangKe <qinyun575@gmail.com>

--- ports/esp32/machine_timer.c
+++ ports/esp32/machine_timer.c
@@ -159,7 +159,7 @@ static void machine_timer_enable(machine_timer_obj_t *self) {
     timer_ll_enable_counter(self->hal_context.dev, self->index, false);
 #if CONFIG_IDF_TARGET_ESP32C2
     // ESP32C2 Only supports clock source GPTIMER_CLK_SRC_PLL_F40M and GPTIMER_CLK_SRC_XTAL
-    timer_ll_set_clock_source(self->hal_context.dev, self->index, GPTIMER_CLK_SRC_PLL_F40M);
+    timer_ll_set_clock_source(self->hal_context.dev, self->index, GPTIMER_CLK_SRC_XTAL);
 #else
     timer_ll_set_clock_source(self->hal_context.dev, self->index, GPTIMER_CLK_SRC_APB);
 #endif
