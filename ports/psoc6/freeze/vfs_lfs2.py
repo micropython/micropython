dd2f2f36d811d6ed73cec03d1bc2f6a6c8d150d7
enriquezgarc <enriquezgarcia.external@infineon.com>
2024-07-04 18:57:47 +0200

    ports/psoc6/vfs_lfs2: Removed SD card at hardware on reset.
    Signed-off-by: enriquezgarc <enriquezgarcia.external@infineon.com>

--- ports/psoc6/freeze/vfs_lfs2.py
+++ ports/psoc6/freeze/vfs_lfs2.py
@@ -22,40 +22,7 @@ except:
     vfs = os.VfsLfs2(bdev, progsize=write_size, readsize=read_size)
     os.mount(vfs, "/")
 
-if "QSPI_Flash" in dir(psoc6):
-    print("\nExternal LFS2 filesystem mounted at /\n")
-else:
-    print("\nInternal LFS2 filesystem mounted at /\n")
 
-if "SD_CARD" in dir(psoc6):
-    bdev2 = machine.SDCard(
-        slot=1,
-        width=4,
-        cd="P13_5",
-        cmd="P12_4",
-        clk="P12_5",
-        dat0="P13_0",
-        dat1="P13_1",
-        dat2="P13_2",
-        dat3="P13_3",
-    )
-
-    # sector size 512 B
-    read_size = 512
-    # page size 512 B
-    write_size = 512
-
-    # create a LFS2 fs and mount it, else format and mount it
-    try:
-        vfs2 = os.VfsLfs2(bdev2, progsize=write_size, readsize=read_size)
-        os.mount(vfs2, "/sd")
-    except:
-        os.VfsLfs2.mkfs(bdev2, progsize=write_size, readsize=read_size)
-        vfs2 = os.VfsLfs2(bdev2, progsize=write_size, readsize=read_size)
-        os.mount(vfs2, "/sd")
-
-    print("SD card mounted at /sd\n")
-
-    del bdev2, vfs2
+print("\nInternal LFS2 filesystem mounted at /\n")
 
 del machine, os, psoc6, bdev, vfs, read_size, write_size
