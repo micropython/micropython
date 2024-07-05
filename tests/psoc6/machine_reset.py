b57c7a042f4549715fa0eb926b8190ca310b129d
enriquezgarc <enriquezgarcia.external@infineon.com>
2024-07-05 15:39:45 +0200

    tests/psoc6: Removed psoc6 after moving to ports dir.
    Signed-off-by: enriquezgarc <enriquezgarcia.external@infineon.com>

--- tests/psoc6/machine_reset.py
+++
@@ -1,8 +0,0 @@
-import machine
-
-print("*****Test :Reset Validation after a soft Reset*****")
-if machine.reset_cause() == machine.SOFT_RESET:
-    print("PASS")
-else:
-    print(machine.reset_cause())
-    print("FAIL")
