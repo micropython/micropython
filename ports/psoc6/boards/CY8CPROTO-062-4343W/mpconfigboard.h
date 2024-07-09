28d025051540ed41f22035b0988fa76cb538eabf
NikhitaR-IFX <Nikhita.Rajasekhar@infineon.com>
2024-07-09 17:55:09 +0530

    ports/psoc6/boards: Removing comments.
    Signed-off-by: NikhitaR-IFX <Nikhita.Rajasekhar@infineon.com>

--- ports/psoc6/boards/CY8CPROTO-062-4343W/mpconfigboard.h
+++ ports/psoc6/boards/CY8CPROTO-062-4343W/mpconfigboard.h
@@ -23,5 +23,5 @@
 
 #define MAX_UART             10 // TODO: Change as per bsp?
 
-// Board specific configurations // TODO: Move other machine module configs
+// Board specific configurations
 #define MAX_TIMER (32)
