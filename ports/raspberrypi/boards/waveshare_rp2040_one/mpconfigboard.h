d67a1ef86743c0738dba955be8abaa94d6851b1a
johnnohj <166672127+johnnohj@users.noreply.github.com>
2024-07-30 08:45:29 -0400

    Add Waveshare RP2040-One

---
+++ ports/raspberrypi/boards/waveshare_rp2040_one/mpconfigboard.h
@@ -0,0 +1,22 @@
+// This file is part of the CircuitPython project: https://circuitpython.org
+//
+// SPDX-FileCopyrightText: Copyright (c) 2021 Scott Shawcroft for Adafruit Industries
+//
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#define MICROPY_HW_BOARD_NAME "Waveshare RP2040-One"
+#define MICROPY_HW_MCU_NAME "rp2040"
+
+#define DEFAULT_UART_BUS_TX (&pin_GPIO0)
+#define DEFAULT_UART_BUS_RX (&pin_GPIO1)
+
+#define DEFAULT_I2C_BUS_SDA (&pin_GPIO4)
+#define DEFAULT_I2C_BUS_SCL (&pin_GPIO5)
+
+#define DEFAULT_SPI_BUS_CK  (&pin_GPIO6)
+#define DEFAULT_SPI_BUS_MOSI (&pin_GPIO7)
+#define DEFAULT_SPI_BUS_MISO (&pin_GPIO8)
+
+#define MICROPY_HW_NEOPIXEL (&pin_GPIO16)
