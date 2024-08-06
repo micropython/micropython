fbc19596f052016dee10a6af56441f4fb532243c
Angus Gratton <angus@redyak.com.au>
2024-08-06 15:52:54 +1000

    rp2: Switch to use new cstack API for stack limit checks.
    This work was funded through GitHub Sponsors.
    
    Signed-off-by: Angus Gratton <angus@redyak.com.au>

--- ports/rp2/mpconfigport.h
+++ ports/rp2/mpconfigport.h
@@ -81,6 +81,7 @@
 #define MICROPY_TRACKED_ALLOC                   (MICROPY_SSL_MBEDTLS || MICROPY_BLUETOOTH_BTSTACK)
 #define MICROPY_READER_VFS                      (1)
 #define MICROPY_ENABLE_GC                       (1)
+#define MICROPY_STACK_CHECK_MARGIN              (256)
 #define MICROPY_ENABLE_EMERGENCY_EXCEPTION_BUF  (1)
 #define MICROPY_LONGINT_IMPL                    (MICROPY_LONGINT_IMPL_MPZ)
 #define MICROPY_FLOAT_IMPL                      (MICROPY_FLOAT_IMPL_FLOAT)
