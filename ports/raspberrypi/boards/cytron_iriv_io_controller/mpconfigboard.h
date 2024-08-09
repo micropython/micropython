91ddc9e9f4b163370c860d6a78fe34e4425b1240
Kong Wai Weng <waiweng@cytron.io>
2024-08-10 00:40:52 +0800

    Added IRIV IO Controller.

---
+++ ports/raspberrypi/boards/cytron_iriv_io_controller/mpconfigboard.h
@@ -0,0 +1,12 @@
+#define MICROPY_HW_BOARD_NAME "Cytron IRIV IO Controller"
+#define MICROPY_HW_MCU_NAME "rp2350a"
+
+#define DEFAULT_I2C_BUS_SCL (&pin_GPIO17)
+#define DEFAULT_I2C_BUS_SDA (&pin_GPIO16)
+
+#define DEFAULT_SPI_BUS_SCK (&pin_GPIO22)
+#define DEFAULT_SPI_BUS_MOSI (&pin_GPIO19)
+#define DEFAULT_SPI_BUS_MISO (&pin_GPIO20)
+
+#define DEFAULT_UART_BUS_RX (&pin_GPIO25)
+#define DEFAULT_UART_BUS_TX (&pin_GPIO24)
