aee002dd80ad8829ae247950f784b1f145fb822e
Damien George <damien@micropython.org>
2024-08-20 15:55:07 +1000

    stm32/lwip_inc: Implement LWIP_PLATFORM_DIAG macro in terms of printf.
    This allows enabling lwIP debugging output.  For example, to enable PPP
    debugging add the following to `mpconfigboard.h`:
    
        #define LWIP_DEBUG 1
        #define PPP_DEBUG LWIP_DBG_ON
    
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/stm32/lwip_inc/arch/cc.h
+++ ports/stm32/lwip_inc/arch/cc.h
@@ -2,7 +2,9 @@
 #define MICROPY_INCLUDED_STM32_LWIP_ARCH_CC_H
 
 #include <assert.h>
-#define LWIP_PLATFORM_DIAG(x)
+#include <stdio.h>
+
+#define LWIP_PLATFORM_DIAG(x)    do { printf x; } while (0)
 #define LWIP_PLATFORM_ASSERT(x)  { assert(1); }
 
 #define LWIP_NO_CTYPE_H 1
