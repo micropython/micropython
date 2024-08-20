718eb1b44ac722c56a5758ac15eca077f42429e1
Alessandro Gatti <a.gatti@frob.it>
2024-08-20 07:21:55 +0200

    tests/inlineasm: Make room for RV32IMC inline asm tests.
    Thumb/Thumb2 tests are now into their own subdirectory, as
    RV32IMC-specific tests will be added as part of the RV32 inline
    assembler support.
    
    Signed-off-by: Alessandro Gatti <a.gatti@frob.it>

--- tests/ports/qemu/asm_test.py
+++ tests/ports/qemu/asm_test.py
@@ -1,4 +1,11 @@
-import frozen_asm
+import sys
+
+arch = getattr(sys.implementation, "_mpy", 0) >> 10
+if arch >= 3 and arch <= 8:
+    import frozen_asm_thumb as frozen_asm
+else:
+    raise ImportError
+
 
 print(frozen_asm.asm_add(1, 2))
 print(frozen_asm.asm_add1(3))
