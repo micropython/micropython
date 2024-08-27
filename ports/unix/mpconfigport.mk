d04974d8d0531f2f868c2d698ec8c44016a0c115
Angus Gratton <angus@redyak.com.au>
2024-08-27 11:21:27 +1000

    unix: Expand the build steps in the README.
    - Present the default build dependencies in one place at the top, and make
      a separate section about building standalone.
    
    - Add steps for the "minimal" variant as well.
    
    - Document that building standalone requires autoconf and libtool.
    
    - Allow MICROPY_STANDALONE to be set as an environment variable.
    
    Fixes issue #11313.
    
    This work was funded through GitHub Sponsors.
    
    Signed-off-by: Angus Gratton <angus@redyak.com.au>

--- ports/unix/mpconfigport.mk
+++ ports/unix/mpconfigport.mk
@@ -37,7 +37,7 @@ MICROPY_PY_JNI = 0
 
 # Avoid using system libraries, use copies bundled with MicroPython
 # as submodules (currently affects only libffi).
-MICROPY_STANDALONE = 0
+MICROPY_STANDALONE ?= 0
 
 MICROPY_ROM_TEXT_COMPRESSION = 1
 
