f3f209542c167c6698c9ef58927f2cd0a7dbc56c
timdechant <timdechant.git@gmail.com>
2024-09-03 09:06:36 -0400

    shared/runtime/sys_stdio_mphal: Fix docstring for stdio.
    Signed-off-by: timdechant <timdechant.git@gmail.com>

--- tests/basics/sys_stdio_buffer.py
+++ tests/basics/sys_stdio_buffer.py
@@ -19,3 +19,15 @@ print('TextIOWrapper' in str(sys.stdin))
 print('TextIOWrapper' in str(type(sys.stdout)))
 print('TextIOWrapper' in str(type(sys.stderr)))
 print('TextIOWrapper' in str(type(sys.stdin)))
+
+# # .buffer member is optional
+# try:
+#     print('FileIO' in str(sys.stdout.buffer))
+#     print('FileIO' in str(sys.stderr.buffer))
+#     print('FileIO' in str(sys.stdin.buffer))
+#
+#     print('FileIO' in str(type(sys.stdout.buffer)))
+#     print('FileIO' in str(type(sys.stderr.buffer)))
+#     print('FileIO' in str(type(sys.stdin.buffer)))
+# except AttributeError:
+#     print("SKIP")
