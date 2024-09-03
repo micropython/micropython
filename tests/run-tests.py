8177db3d544543c6016e1d06de31763f5972a1ff
Damien George <damien@micropython.org>
2024-09-03 12:55:20 +1000

    tests/run-tests.py: Add a zephyr test target.
    So that certain tests can be skipped when running on this target.  These
    thread tests do not pass because the zephyr port cannot create more than 4
    threads at once.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- tests/run-tests.py
+++ tests/run-tests.py
@@ -637,6 +637,9 @@ def run_tests(pyb, tests, args, result_dir, num_threads=1):
         skip_tests.add("thread/thread_lock2.py")
         skip_tests.add("thread/thread_lock3.py")
         skip_tests.add("thread/thread_shared2.py")
+    elif args.target == "zephyr":
+        skip_tests.add("thread/stress_heap.py")
+        skip_tests.add("thread/thread_lock3.py")
 
     # Some tests shouldn't be run on pyboard
     if args.target != "unix":
@@ -1057,6 +1060,7 @@ the last matching regex is used:
         "qemu",
         "renesas-ra",
         "rp2",
+        "zephyr",
     )
     if args.list_tests:
         pyb = None
