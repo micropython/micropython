8e58bf4ad56f7e5e14b60d3f6da3afca2162e112
iabdalkader <i.abdalkader@gmail.com>
2024-09-14 15:15:42 +0200

    alif/boards/OPENMV_AE3: Set board USB PID:VID.
    Signed-off-by: iabdalkader <i.abdalkader@gmail.com>

--- ports/alif/boards/OPENMV_AE3/mpconfigboard.h
+++ ports/alif/boards/OPENMV_AE3/mpconfigboard.h
@@ -1,13 +1,18 @@
-#define MICROPY_HW_BOARD_NAME                   "OpenMV AE3"
-#define MICROPY_HW_MCU_NAME                     "AE722F80F55D5XX"
+#define MICROPY_HW_BOARD_NAME   "OpenMV-AE3"
+#define MICROPY_HW_MCU_NAME     "AE722F80F55D5XX"
 
-#define MICROPY_HW_ENABLE_UART_REPL             (0)
-#define MICROPY_HW_ENABLE_OSPI                  (0)
+#define MICROPY_HW_UART_REPL    (4)
+#define MICROPY_HW_USB_MSC      (0)
+
+#define MICROPY_HW_ENABLE_OSPI  (0)
+#define MICROPY_HW_ENABLE_UART_REPL (0)
+
+#define MICROPY_HW_USB_VID      0x1209
+#define MICROPY_HW_USB_PID      0xabd1
+
+extern void board_startup(void);
+#define MICROPY_BOARD_STARTUP   board_startup
 
 // This is used for alif.Flash() and USB MSC.
 #define MICROPY_HW_FLASH_STORAGE_BASE_ADDR      (0)
 #define MICROPY_HW_FLASH_STORAGE_BYTES          (32 * 1024 * 1024)
-
-#define MICROPY_BOARD_STARTUP()                 board_startup()
-
-void board_startup(void);
