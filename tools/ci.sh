c622b5ea9c0c9eedbc86ac62d22b57adbad3526f
Ayush Singh <ayush@beagleboard.org>
2024-09-21 23:40:09 +0530

    tools/ci.sh: Use mainline zephyr.
    - Not using latest container images or sdk since they generally work
      fine across zephyr versions. And is better for caching.
    
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

--- tools/ci.sh
+++ tools/ci.sh
@@ -765,7 +765,7 @@ function ci_windows_build {
 
 ZEPHYR_DOCKER_VERSION=v0.26.13
 ZEPHYR_SDK_VERSION=0.16.8
-ZEPHYR_VERSION=v3.7.0
+ZEPHYR_VERSION=main
 
 function ci_zephyr_setup {
     IMAGE=ghcr.io/zephyrproject-rtos/ci:${ZEPHYR_DOCKER_VERSION}
