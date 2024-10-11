44ed1c20ce070538068d9f708c8181e0be68fe55
Damien George <damien@micropython.org>
2024-10-11 12:53:10 +1100

    esp32: Disable hardware stack protection on ESP32-C6.
    The same as fee9d66e3a7308bd9edffb2624b52f4e04ecc4f3 but for C6.
    
    Fixes issue #15667.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/esp32/boards/UM_TINYC6/mpconfigboard.cmake
+++ ports/esp32/boards/UM_TINYC6/mpconfigboard.cmake
@@ -3,6 +3,7 @@ set(IDF_TARGET esp32c6)
 set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.base
     ${SDKCONFIG_IDF_VERSION_SPECIFIC}
+    boards/sdkconfig.c6
     boards/sdkconfig.ble
     boards/UM_TINYC6/sdkconfig.board
 )
