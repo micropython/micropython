3e400ef8361a564e1b9b3d01b065c04a177ed12f
iabdalkader <i.abdalkader@gmail.com>
2024-10-25 16:35:10 +0200

    alif/OPENMV_AE3: Update board config.
    Changes:
    - Update VID:PID.
    - Update USB settings.
    - Disable MSC/OSPI on HE.
    - Set filesystem size.
    
    Note this board config doesn't have to be added upstream, when
    the port is published eventually, it just makes it easier to test.
    
    Signed-off-by: iabdalkader <i.abdalkader@gmail.com>

--- ports/alif/boards/OPENMV_AE3/mpconfigboard.h
+++ ports/alif/boards/OPENMV_AE3/mpconfigboard.h
@@ -1,17 +1,20 @@
 #define MICROPY_HW_BOARD_NAME   "OpenMV-AE3"
 #define MICROPY_HW_MCU_NAME     "AE302F80F55D5AE"
 
-#define MICROPY_HW_USB_MSC      (0)
-
-#define MICROPY_HW_ENABLE_OSPI  (0)
+#define MICROPY_HW_USB_MSC      (CORE_M55_HP)
+#define MICROPY_HW_ENABLE_OSPI  (CORE_M55_HP)
 #define MICROPY_HW_ENABLE_UART_REPL (0)
 
-#define MICROPY_HW_USB_VID      0x1209
-#define MICROPY_HW_USB_PID      0xabd1
+#define MICROPY_HW_USB_VID      0x37C5
+#define MICROPY_HW_USB_PID      0x16E3
+
+#define MICROPY_HW_USB_MANUFACTURER_STRING  "OpenMV"
+#define MICROPY_HW_USB_PRODUCT_FS_STRING    "OpenMV Camera"
+#define MICROPY_HW_USB_MSC_INQUIRY_VENDOR_STRING "OpenMV"
 
 extern void board_startup(void);
 #define MICROPY_BOARD_STARTUP   board_startup
 
 // This is used for alif.Flash() and USB MSC.
 #define MICROPY_HW_FLASH_STORAGE_BASE_ADDR      (0)
-#define MICROPY_HW_FLASH_STORAGE_BYTES          (32 * 1024 * 1024)
+#define MICROPY_HW_FLASH_STORAGE_BYTES          (24 * 1024 * 1024)
