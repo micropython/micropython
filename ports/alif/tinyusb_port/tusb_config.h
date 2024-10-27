00ed16d2d7f4094a081dc538c3d037b6629a40b5
iabdalkader <i.abdalkader@gmail.com>
2024-10-27 13:47:40 +0100

    alif/tusb_config.h: Increase TinyUSB CDC buffers.
    Signed-off-by: iabdalkader <i.abdalkader@gmail.com>

--- ports/alif/tinyusb_port/tusb_config.h
+++ ports/alif/tinyusb_port/tusb_config.h
@@ -64,7 +64,9 @@
 #define CFG_TUD_ENDPOINT0_SIZE    64
 
 // CDC Endpoint transfer buffer size, more is faster
-#define CFG_TUD_CDC_EP_BUFSIZE   (TUD_OPT_HIGH_SPEED ? 512 : 64)
+#define CFG_TUD_CDC_EP_BUFSIZE  (4096)
+#define CFG_TUD_CDC_RX_BUFSIZE  (4096)
+#define CFG_TUD_CDC_TX_BUFSIZE  (4096)
 
 #include "shared/tinyusb/tusb_config.h"
 
