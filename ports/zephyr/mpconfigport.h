4685db0842dcbfd1b182bfc369fb32b4cf458b65
Anton Blanchard <antonb@tenstorrent.com>
2024-10-31 15:41:28 +1100

    zephyr: Fix mp_int_t and mp_uint_t on 64bit
    These both need to fit a pointer, so make them intptr_t and
    mp_uint_t, similar to other ports.
    
    Signed-off-by: Anton Blanchard <antonb@tenstorrent.com>

--- ports/zephyr/mpconfigport.h
+++ ports/zephyr/mpconfigport.h
@@ -133,8 +133,8 @@ void mp_hal_signal_event(void);
 #define MICROPY_HW_MCU_NAME "unknown-cpu"
 #endif
 
-typedef int mp_int_t; // must be pointer size
-typedef unsigned mp_uint_t; // must be pointer size
+typedef intptr_t mp_int_t; // must be pointer size
+typedef uintptr_t mp_uint_t; // must be pointer size
 typedef long mp_off_t;
 
 #define MP_STATE_PORT MP_STATE_VM
