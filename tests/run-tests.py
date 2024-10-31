3264303cd4e3a2d2f9fd6a26c209dc5d6f420b59
Damien George <damien@micropython.org>
2024-10-31 12:38:27 +1100

    tests/run-tests.py: Capture KeyboardInterrupt for nicer exit.
    Signed-off-by: Damien George <damien@micropython.org>

--- tests/run-tests.py
+++ tests/run-tests.py
@@ -1474,4 +1474,9 @@ the last matching regex is used:
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print()
+        print("INTERRUPT")
+        sys.exit(1)
