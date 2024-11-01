6e5d8d009349a7d7932ab0c93a739a576a3594d3
Angus Gratton <angus@redyak.com.au>
2024-11-01 16:32:48 +1100

    esp32: Drop support for ESP-IDF below V5.2.0.
    Specifically, remove all conditional compilation for these earlier versions
    and change the idf_component.yml specifiers to require >=5.2.0.
    
    Signed-off-by: Angus Gratton <angus@redyak.com.au>

--- ports/esp32/modmachine.c
+++ ports/esp32/modmachine.c
@@ -110,24 +110,11 @@ static void mp_machine_set_freq(size_t n_args, const mp_obj_t *args) {
         mp_raise_ValueError(MP_ERROR_TEXT("frequency must be 20MHz, 40MHz, 80Mhz, 160MHz or 240MHz"));
         #endif
     }
-    #if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 1, 0)
-    esp_pm_config_t pm;
-    #else
-    #if CONFIG_IDF_TARGET_ESP32
-    esp_pm_config_esp32_t pm;
-    #elif CONFIG_IDF_TARGET_ESP32C3
-    esp_pm_config_esp32c3_t pm;
-    #elif CONFIG_IDF_TARGET_ESP32C6
-    esp_pm_config_esp32c6_t pm;
-    #elif CONFIG_IDF_TARGET_ESP32S2
-    esp_pm_config_esp32s2_t pm;
-    #elif CONFIG_IDF_TARGET_ESP32S3
-    esp_pm_config_esp32s3_t pm;
-    #endif
-    #endif
-    pm.max_freq_mhz = freq;
-    pm.min_freq_mhz = freq;
-    pm.light_sleep_enable = false;
+    esp_pm_config_t pm = {
+        .max_freq_mhz = freq,
+        .min_freq_mhz = freq,
+        .light_sleep_enable = false,
+    };
     esp_err_t ret = esp_pm_configure(&pm);
     if (ret != ESP_OK) {
         mp_raise_ValueError(NULL);
