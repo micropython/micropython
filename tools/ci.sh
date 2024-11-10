8a1535bfa738bc487d8f3c865ca54a5178da8c63
Maureen Helm <maureen.helm@analog.com>
2024-11-10 08:24:02 -0600

    tools/ci.sh: Upgrade Zephyr docker image 0.27.4 and SDK 0.17.0.
    Upgrades CI to use the latest versions of the Zephyr docker image and
    Zephyr SDK.
    
    Signed-off-by: Maureen Helm <maureen.helm@analog.com>

--- tools/ci.sh
+++ tools/ci.sh
@@ -747,8 +747,8 @@ function ci_windows_build {
 ########################################################################################
 # ports/zephyr
 
-ZEPHYR_DOCKER_VERSION=v0.26.13
-ZEPHYR_SDK_VERSION=0.16.8
+ZEPHYR_DOCKER_VERSION=v0.27.4
+ZEPHYR_SDK_VERSION=0.17.0
 ZEPHYR_VERSION=v3.7.0
 
 function ci_zephyr_setup {
