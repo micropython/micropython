3b101b0e4bc77f7715ed22b5d7dd36356afbf8af
Damien George <damien@micropython.org>
2024-11-18 11:10:36 +1100

    esp32: Implement vfs.rom_ioctl.
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/esp32/modules/_boot.py
+++ ports/esp32/modules/_boot.py
@@ -3,11 +3,15 @@ import vfs
 from esp32 import Partition
 from flashbdev import bdev
 
+"""
 if romfs := Partition.find(Partition.TYPE_DATA, label="romfs"):
     import sys
 
-    sys.path.insert(0, "/romfs")
+    # TODO support the buffer protocol on Partition types to make this work
+    vfs.mount(vfs.VfsRom(romfs), "/rom")
+    sys.path.insert(0, "/rom")
 del romfs
+"""
 
 try:
     if bdev:
