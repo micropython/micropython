9252dfd4a4ce6fe44562eedd690f6df26b5568d5
honboyxy <honboylc@gmail.com>
2024-11-26 20:35:49 +0100

    Update Mymodule.c
    Signed-off-by: honboyxy <honboylc@gmail.com>

--- ports/stm32/Mymodule.c
+++ ports/stm32/Mymodule.c
@@ -2,34 +2,42 @@
 #include "py/runtime.h"
 #include "stm32f4xx.h"
 
-// 自定义的 gpio_toggle 函数：翻转 GPIOA 的某个引脚
-static mp_obj_t gpio_toggle(mp_obj_t pin_obj) {
-    uint32_t pin = mp_obj_get_int(pin_obj);
-    GPIOA->ODR ^= (1 << pin);  // 直接操作寄存器翻转 GPIOA 引脚
-    return mp_const_none;
-}
-
 // 自定义的 gpio_write 函数：设置 GPIOA 的某个引脚为 HIGH 或 LOW
-static mp_obj_t gpio_write(mp_obj_t pin_obj, mp_obj_t value_obj) {
-    uint32_t pin = mp_obj_get_int(pin_obj);  // 获取引脚号
+static mp_obj_t gpioa_write(mp_obj_t pin_obj, mp_obj_t value_obj) {
+    uint32_t pin = mp_obj_get_int(pin_obj);      // 获取引脚号
     uint32_t value = mp_obj_get_int(value_obj);  // 获取值（0 或 1）
-
-    if (value) {
-        GPIOA->ODR |= (1 << pin);  // 设置引脚为高电平
-    } else {
-        GPIOA->ODR &= ~(1 << pin);  // 设置引脚为低电平
-    }
+    if (value) GPIOA->BRR = (1 << pin);  // 设置引脚为高电平
+    else GPIOA->BSRR = (1 << pin);  // 设置引脚为低电平
     return mp_const_none;  // 返回 None，表示函数执行完毕
 }
 
+static mp_obj_t gpiob_write(mp_obj_t pin_obj, mp_obj_t value_obj) {
+    uint32_t pin = mp_obj_get_int(pin_obj);      // 获取引脚号
+    uint32_t value = mp_obj_get_int(value_obj);  // 获取值（0 或 1）
+    if (value) GPIOB->BRR = (1 << pin);  // 设置引脚为高电平
+    else GPIOB->BSRR = (1 << pin);  // 设置引脚为低电平
+    return mp_const_none;  // 返回 None，表示函数执行完毕
+}
+
+static mp_obj_t gpioc_write(mp_obj_t pin_obj, mp_obj_t value_obj) {
+    uint32_t pin = mp_obj_get_int(pin_obj);      // 获取引脚号
+    uint32_t value = mp_obj_get_int(value_obj);  // 获取值（0 或 1）
+    if (value) GPIOC->BRR = (1 << pin);  // 设置引脚为高电平
+    else GPIOC->BSRR = (1 << pin);  // 设置引脚为低电平
+    return mp_const_none;  // 返回 None，表示函数执行完毕
+}
+
+
 // 使用宏定义函数对象
-static MP_DEFINE_CONST_FUN_OBJ_1(gpio_toggle_obj, gpio_toggle);
-static MP_DEFINE_CONST_FUN_OBJ_2(gpio_write_obj, gpio_write);
+static MP_DEFINE_CONST_FUN_OBJ_2(gpioa_write_obj, gpioa_write);
+static MP_DEFINE_CONST_FUN_OBJ_2(gpiob_write_obj, gpiob_write);
+static MP_DEFINE_CONST_FUN_OBJ_2(gpioc_write_obj, gpioc_write);
 
 // 定义模块全局方法表，包含我们新增的 gpio_write 函数
 static const mp_rom_map_elem_t mymodule_globals_table[] = {
-    { MP_ROM_QSTR(MP_QSTR_gpio_toggle), MP_ROM_PTR(&gpio_toggle_obj) },
-    { MP_ROM_QSTR(MP_QSTR_gpio_write), MP_ROM_PTR(&gpio_write_obj) },  // 新增的 gpio_write 函数
+    { MP_ROM_QSTR(MP_QSTR_gpioa_write), MP_ROM_PTR(&gpioa_write_obj) },  
+    { MP_ROM_QSTR(MP_QSTR_gpiob_write), MP_ROM_PTR(&gpiob_write_obj) },
+    { MP_ROM_QSTR(MP_QSTR_gpioc_write), MP_ROM_PTR(&gpioc_write_obj) },
 };
 
 // 定义模块全局字典
