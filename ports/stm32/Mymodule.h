dd6036a162e393d6abca87aa022304b9bf611d3e
honboyxy <honboylc@gmail.com>
2024-11-27 08:44:35 +0100

    Update Mymodule.h
    Signed-off-by: honboyxy <honboylc@gmail.com>

--- ports/stm32/Mymodule.h
+++ ports/stm32/Mymodule.h
@@ -4,13 +4,13 @@
 #include "py/obj.h"
 
 // 模块的函数声明
-mp_obj_t gpio_toggle(mp_obj_t pin_obj);
-mp_obj_t gpio_write(mp_obj_t pin_obj, mp_obj_t value_obj);
-
+mp_obj_t gpioa_write(mp_obj_t pin_obj, mp_obj_t value_obj)
+mp_obj_t gpiob_write(mp_obj_t pin_obj, mp_obj_t value_obj)
+mp_obj_t gpioc_write(mp_obj_t pin_obj, mp_obj_t value_obj)
 // 模块的函数对象声明
-extern const mp_obj_fun_builtin_t gpio_toggle_obj;
-extern const mp_obj_fun_builtin_t gpio_write_obj;
-
+extern const mp_obj_fun_builtin_t gpioa_write_obj;
+extern const mp_obj_fun_builtin_t gpiob_write_obj;
+extern const mp_obj_fun_builtin_t gpioc_write_obj;
 // 模块的全局字典声明
 extern const mp_obj_module_t mymodule;
 
