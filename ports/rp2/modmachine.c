micropython/micropython

commit 74fb42aa82d537b6fefb463907274bd660c6ce8d

rp2: Refactor soft timer to use hardware timer alarm.
Progress towards removing pico-sdk alarm pool, due to a known issue.

This work was funded through GitHub Sponsors.

Signed-off-by: Angus Gratton <angus@redyak.com.au>

ports/rp2/mphalport.c
@@ -35,6 +35,7 @@
 #include "pendsv.h"
 #include "tusb.h"
 #include "uart.h"
+#include "hardware/irq.h"
 #include "hardware/rtc.h"
 #include "pico/unique_id.h"
 
@@ -45,8 +46,6 @@
 // This needs to be added to the result of time_us_64() to get the number of
 // microseconds since the Epoch.
 static uint64_t time_us_64_offset_from_epoch;
-
-static alarm_id_t soft_timer_alarm_id = 0;
 
 #if MICROPY_HW_ENABLE_UART_REPL || MICROPY_HW_USB_CDC
 
@@ -273,21 +272,26 @@ uint32_t storage_read_blocks(uint8_t *dest, uint32_t block_num, uint32_t num_blo
     panic_unsupported();
 }
 
-static int64_t soft_timer_callback(alarm_id_t id, void *user_data) {
-    soft_timer_alarm_id = 0;
-    pendsv_schedule_dispatch(PENDSV_DISPATCH_SOFT_TIMER, soft_timer_handler);
-    return 0; // don't reschedule this alarm
-}
-
 uint32_t soft_timer_get_ms(void) {
     return mp_hal_ticks_ms();
 }
 
 void soft_timer_schedule_at_ms(uint32_t ticks_ms) {
-    if (soft_timer_alarm_id != 0) {
-        cancel_alarm(soft_timer_alarm_id);
-    }
     int32_t ms = soft_timer_ticks_diff(ticks_ms, mp_hal_ticks_ms());
     ms = MAX(0, ms);
-    soft_timer_alarm_id = add_alarm_in_ms(ms, soft_timer_callback, NULL, true);
+    if (hardware_alarm_set_target(MICROPY_HW_SOFT_TIMER_ALARM_NUM, delayed_by_ms(get_absolute_time(), ms))) {
+        // "missed" hardware alarm target
+        hardware_alarm_force_irq(MICROPY_HW_SOFT_TIMER_ALARM_NUM);
+    }
+}
+
+static void soft_timer_hardware_callback(unsigned int alarm_num) {
+    // The timer alarm ISR needs to call here and trigger PendSV dispatch via
+    // a second ISR, as PendSV may be currently suspended by the other CPU.
+    pendsv_schedule_dispatch(PENDSV_DISPATCH_SOFT_TIMER, soft_timer_handler);
+}
+
+void soft_timer_init(void) {
+    hardware_alarm_claim(MICROPY_HW_SOFT_TIMER_ALARM_NUM);
+    hardware_alarm_set_callback(MICROPY_HW_SOFT_TIMER_ALARM_NUM, soft_timer_hardware_callback);
 }

ports/rp2/main.c
@@ -76,6 +76,8 @@ int main(int argc, char **argv) {
     // This is a tickless port, interrupts should always trigger SEV.
     SCB->SCR |= SCB_SCR_SEVONPEND_Msk;
 
+    soft_timer_init();
+
     #if MICROPY_HW_ENABLE_UART_REPL
     bi_decl(bi_program_feature("UART REPL"))
     setup_default_uart();

ports/rp2/mpconfigport.h
@@ -154,6 +154,10 @@
 #define MICROPY_SSL_MBEDTLS                     (1)
 #define MICROPY_PY_LWIP_SOCK_RAW                (MICROPY_PY_LWIP)
 
+// Hardware timer alarm index. Available range 0-3.
+// Number 3 is currently used by pico-sdk (PICO_TIME_DEFAULT_ALARM_POOL_HARDWARE_ALARM_NUM)
+#define MICROPY_HW_SOFT_TIMER_ALARM_NUM         (2)
+
 // fatfs configuration
 #define MICROPY_FATFS_ENABLE_LFN                (1)
 #define MICROPY_FATFS_LFN_CODE_PAGE             437 /* 1=SFN/ANSI 437=LFN/U.S.(OEM) */

ports/rp2/mpnetworkport.c
@@ -66,7 +66,7 @@ static void gpio_irq_handler(void) {
 void cyw43_irq_init(void) {
     gpio_add_raw_irq_handler_with_order_priority(CYW43_PIN_WL_HOST_WAKE, gpio_irq_handler, CYW43_SHARED_IRQ_HANDLER_PRIORITY);
     irq_set_enabled(IO_IRQ_BANK0, true);
-    NVIC_SetPriority(PendSV_IRQn, PICO_LOWEST_IRQ_PRIORITY);
+    NVIC_SetPriority(PendSV_IRQn, IRQ_PRI_PENDSV);
 }
 
 void cyw43_post_poll_hook(void) {

ports/rp2/pendsv.h
@@ -42,6 +42,9 @@ enum {
 
 #define PENDSV_DISPATCH_NUM_SLOTS PENDSV_DISPATCH_MAX
 
+// PendSV IRQ priority, to run system-level tasks that preempt the main thread.
+#define IRQ_PRI_PENDSV PICO_LOWEST_IRQ_PRIORITY
+
 typedef void (*pendsv_dispatch_t)(void);
 
 void pendsv_suspend(void);

shared/runtime/softtimer.h
@@ -81,6 +81,9 @@ static inline void soft_timer_reinsert(soft_timer_entry_t *entry, uint32_t initi
 //   pend-SV IRQ level, or equivalent.
 uint32_t soft_timer_get_ms(void);
 void soft_timer_schedule_at_ms(uint32_t ticks_ms);
+
+// Optional port-specific initialisation function (provided and called by the port if needed).
+void soft_timer_init(void);
 #endif
 
 #endif // MICROPY_INCLUDED_SHARED_RUNTIME_SOFTTIMER_H

commit 46b7ec723a9eb61eda925b919499de738db5c163

rp2: Use the soft timer hardware alarm to wake from lightsleep.
Fixes issue where PICO-W build won't lightsleep() for more than 64ms. This
is a regression introduced in 74fb42a when we switched away from the
pico-sdk alarm pool for soft timers and accidentally made soft timer expiry
a wakeup source.

Before 74fb42a, both the pico-sdk alarm pool and the lightsleep wakeup
timer uses alarm 3. The lightsleep wakeup would quietly clobber alarm 3's
timeout, meaning softtimer wouldn't wake the chip from lightsleep.

After 74fb42a, soft timer wakeup happens on timer alarm 2 so this interrupt
wakes the chip from light sleep. On PICO-W builds this happens every lwIP
tick (64ms).

The change is to go back to using the same timer alarm for both lightsleep
wakeup and soft timer, but now being explicit about lightsleep wakeup
clobbering any soft timer wakeup. Also adds a "catch up" call to soft timer
handler as it's currently possible to miss soft timer events after waking
up.

This also reworks the changes added in 19844b to enable the timer IRQ on
CPU1. This is still needed, but have changed the implementation to be more
explicit about when it's needed.

This work was funded through GitHub Sponsors.

Signed-off-by: Angus Gratton <angus@redyak.com.au>

ports/rp2/modmachine.c
@@ -196,17 +196,8 @@ static void mp_machine_lightsleep(size_t n_args, const mp_obj_t *args) {
         #endif
         xosc_dormant();
     } else {
-        bool timer3_enabled = irq_is_enabled(3);
-
-        const uint32_t alarm_num = 3;
-        const uint32_t irq_num = TIMER_ALARM_IRQ_NUM(timer_hw, alarm_num);
+        uint32_t timer_irq_num;
         if (use_timer_alarm) {
-            // Make sure ALARM3/IRQ3 is enabled on _this_ core
-            if (!timer3_enabled) {
-                irq_set_enabled(irq_num, true);
-            }
-            hw_set_bits(&timer_hw->inte, 1u << alarm_num);
-            // Use timer alarm to wake.
             clocks_hw->sleep_en0 = 0x0;
             #if PICO_RP2040
             clocks_hw->sleep_en1 = CLOCKS_SLEEP_EN1_CLK_SYS_TIMER_BITS;
@@ -215,8 +206,18 @@ static void mp_machine_lightsleep(size_t n_args, const mp_obj_t *args) {
             #else
             #error Unknown processor
             #endif
-            timer_hw->intr = 1u << alarm_num; // clear any IRQ
-            timer_hw->alarm[alarm_num] = timer_hw->timerawl + delay_ms * 1000;
+
+            // Clobber the soft timer hardware alarm (as soft timer doesn't wake lightsleep)
+            // and repurpose it for the lightsleep wakeup
+            //
+            // The timer peripheral will already be configured via softtimer_init().
+            if (get_core_num() == 1) {
+                // On CPU0 the hardware alarm interrupt is already enabled, but IRQs won't be serviced
+                // due to the critical section so we also need to temporarily enable this IRQ on our CPU
+                timer_irq_num = hardware_alarm_get_irq_num(PICO_DEFAULT_TIMER_INSTANCE(), MICROPY_HW_SOFT_TIMER_ALARM_NUM);
+                irq_set_enabled(timer_irq_num, true);
+            }
+            hardware_alarm_set_target(MICROPY_HW_SOFT_TIMER_ALARM_NUM, delayed_by_ms(get_absolute_time(), delay_ms));
         } else {
             // TODO: Use RTC alarm to wake.
             clocks_hw->sleep_en0 = 0x0;
@@ -247,16 +248,24 @@ static void mp_machine_lightsleep(size_t n_args, const mp_obj_t *args) {
 
         // Go into low-power mode.
         __wfi();
-
-        if (!timer3_enabled) {
-            irq_set_enabled(irq_num, false);
-        }
         clocks_hw->sleep_en0 |= ~(0u);
         clocks_hw->sleep_en1 |= ~(0u);
+
+        if (use_timer_alarm && get_core_num() == 1) {
+            // We don't have an easy way to clear any pending IRQ on this CPU,
+            // so we might get a spurious soft timer interrupt handler run on
+            // CPU1 unfortunately - but this should be harmless
+            irq_set_enabled(timer_irq_num, false);
+        }
     }
 
     // Enable ROSC.
     rosc_hw->ctrl = ROSC_CTRL_ENABLE_VALUE_ENABLE << ROSC_CTRL_ENABLE_LSB;
+
+    // we might have missed at least one soft timer expiry that hasn't been processed
+    // depending on when we woke up, so force soft timer processing for when we exit
+    // the atomic section
+    hardware_alarm_force_irq(MICROPY_HW_SOFT_TIMER_ALARM_NUM);
 
     // Bring back all clocks.
     runtime_init_clocks_optional_usb(disable_usb);

ports/rp2/mphalport.c
@@ -250,9 +250,13 @@ static void soft_timer_hardware_callback(unsigned int alarm_num) {
     // a second ISR, as PendSV may be currently suspended by the other CPU.
     pendsv_schedule_dispatch(PENDSV_DISPATCH_SOFT_TIMER, soft_timer_handler);
 
-    // This ISR only runs on core0, but if core1 is running Python code then it
-    // may be blocked in WFE so wake it up as well. Unfortunately this also sets
-    // the event flag on core0, so a subsequent WFE on this core will not suspend
+    // This ISR mostly only runs on core0 but may run on core1 if core1 calls
+    // machine.lightsleep().
+    //
+    // Either way, if the other core is running Python code then it may be
+    // blocked in WFE so wake it up as well. Unfortunately this also sets the
+    // event flag on this core, so a subsequent WFE on this core will not
+    // suspend
     #if MICROPY_PY_THREAD
     if (core1_entry != NULL) {
         __sev();