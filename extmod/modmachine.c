ce9eafb8c1ba7d8d5afbd680053960cd3fde0df8
Grace-Koo <Grace@wiznet.io>
2024-12-23 15:14:12 +0900

    extmod/modmachine: Add functions and define for wiznet_pio_spi support.
    Signed-off-by: Grace-Koo <Grace@wiznet.io>

--- extmod/modmachine.c
+++ extmod/modmachine.c
@@ -228,6 +228,9 @@ static const mp_rom_map_elem_t machine_module_globals_table[] = {
     #if MICROPY_PY_MACHINE_SPI
     { MP_ROM_QSTR(MP_QSTR_SPI), MP_ROM_PTR(&machine_spi_type) },
     #endif
+    #if MICROPY_PY_MACHINE_WIZNET_PIO_SPI
+    { MP_ROM_QSTR(MP_QSTR_WIZNET_PIO_SPI), MP_ROM_PTR(&machine_wiznet_pio_spi_type) },
+    #endif
     #if MICROPY_PY_MACHINE_UART
     { MP_ROM_QSTR(MP_QSTR_UART), MP_ROM_PTR(&machine_uart_type) },
     #endif
