0deddc23ccb042a570450cf13ae8141eacd09a5a
Damien George <damien@micropython.org>
2024-12-24 00:19:39 +1100

    alif/boards/ALIF_ENSEMBLE: Define fs and romfs region.
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/alif/boards/ALIF_ENSEMBLE/mpconfigboard.h
+++ ports/alif/boards/ALIF_ENSEMBLE/mpconfigboard.h
@@ -4,7 +4,12 @@
 #define MICROPY_HW_UART_REPL                    (4)
 #define MICROPY_HW_USB_MSC                      (1)
 
+// ROMFS partitions
+#define MICROPY_HW_ROMFS_ENABLE_PART0           (1)
+#define MICROPY_HW_ROMFS_ENABLE_PART1           (CORE_M55_HP)
+
 // This is used for alif.Flash() and USB MSC.
 #define MICROPY_HW_FLASH_STORAGE_BASE_ADDR      (0)
 #define MICROPY_HW_FLASH_STORAGE_BYTES          (32 * 1024 * 1024)
-#define MICROPY_HW_FLASH_STORAGE_FS_BYTES       (32 * 1024 * 1024)
+#define MICROPY_HW_FLASH_STORAGE_FS_BYTES       (16 * 1024 * 1024)
+#define MICROPY_HW_FLASH_STORAGE_ROMFS_BYTES    (16 * 1024 * 1024)
