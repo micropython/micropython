9d0a5ac7e9ac8b90d1bbea402fd16a410aa4008f
StrayCat <marcin.eu@gmail.com>
2025-01-02 23:46:44 +1300

    esp32/boards: Enable I2S on ESP32C3 boards.
    Signed-off-by: StrayCat <marcin.eu@gmail.com>

--- ports/esp32/boards/ESP32_GENERIC_C3/mpconfigboard.h
+++ ports/esp32/boards/ESP32_GENERIC_C3/mpconfigboard.h
@@ -4,7 +4,6 @@
 #define MICROPY_HW_MCU_NAME                 "ESP32C3"
 
 #define MICROPY_HW_ENABLE_SDCARD            (0)
-#define MICROPY_PY_MACHINE_I2S              (0)
 
 // Enable UART REPL for modules that have an external USB-UART and don't use native USB.
 #define MICROPY_HW_ENABLE_UART_REPL         (1)
