f37bbbadc7899e904d1da69dc669bc1525800359
Dryw Wade <dryw.wade@sparkfun.com>
2025-01-08 11:33:33 -0700

    ports/rp2/boards: Change Thing Plus RP2350 flash to 14MiB
    Signed-off-by: Dryw Wade <dryw.wade@sparkfun.com>

--- ports/rp2/boards/SPARKFUN_THINGPLUS_RP2350/mpconfigboard.h
+++ ports/rp2/boards/SPARKFUN_THINGPLUS_RP2350/mpconfigboard.h
@@ -1,6 +1,6 @@
 // Board and hardware specific configuration
 #define MICROPY_HW_BOARD_NAME          "SparkFun Thing Plus RP2350"
-#define MICROPY_HW_FLASH_STORAGE_BYTES (15 * 1024 * 1024)
+#define MICROPY_HW_FLASH_STORAGE_BYTES (14 * 1024 * 1024)
 
 #define MICROPY_PY_NETWORK_HOSTNAME_DEFAULT "ThingPlusRP2350"
 
