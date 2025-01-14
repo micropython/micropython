6f032bec00f73ff998293be6a02a3c0175e3f2a1
Dryw Wade <dryw.wade@sparkfun.com>
2025-01-14 16:35:06 -0700

    ports/rp2: Update Thing Plus RP2350 radio definitions No longer depends on #16057

--- ports/rp2/boards/SPARKFUN_THINGPLUS_RP2350/mpconfigboard.h
+++ ports/rp2/boards/SPARKFUN_THINGPLUS_RP2350/mpconfigboard.h
@@ -2,7 +2,20 @@
 #define MICROPY_HW_BOARD_NAME          "SparkFun Thing Plus RP2350"
 #define MICROPY_HW_FLASH_STORAGE_BYTES (14 * 1024 * 1024)
 
+// Enable networking.
+#define MICROPY_PY_NETWORK 1
 #define MICROPY_PY_NETWORK_HOSTNAME_DEFAULT "ThingPlusRP2350"
+
+// CYW43 driver configuration.
+#define CYW43_USE_SPI (1)
+#define CYW43_LWIP (1)
+#define CYW43_GPIO (1)
+#define CYW43_SPI_PIO (1)
+
+#define MICROPY_HW_PIN_EXT_COUNT    CYW43_WL_GPIO_COUNT
+
+int mp_hal_is_pin_reserved(int n);
+#define MICROPY_HW_PIN_RESERVED(i) mp_hal_is_pin_reserved(i)
 
 #define MICROPY_HW_USB_VID (0x1B4F)
 #define MICROPY_HW_USB_PID (0x0038)
@@ -33,5 +46,3 @@
 
 #define MICROPY_HW_PSRAM_CS_PIN (8)
 #define MICROPY_HW_ENABLE_PSRAM (1)
-
-#include "enable_cyw43.h"
