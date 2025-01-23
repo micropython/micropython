aa4cdfd0278b0169f502e9dfe22d013e49886469
robert-hh <robert@hammelrath.com>
2025-01-23 13:54:28 +0100

    samd/boards: Add generic SAMD51x20 board definitions.
    The definition uses the internal oscillator for clock and only
    internal flash for the file system.
    
    It works at SAMD51J20 device as well, only that fewer pins are
    accessible.
    
    Tested with a SAMD51J20 board.
    
    Signed-off-by: robert-hh <robert@hammelrath.com>

---
+++ ports/samd/boards/SAMD_GENERIC_D51X20/mpconfigboard.mk
@@ -0,0 +1,13 @@
+MCU_SERIES = SAMD51
+CMSIS_MCU = SAMD51P20A
+LD_FILES = boards/samd51x19a.ld sections.ld
+TEXT0 = 0x4000
+
+
+# The ?='s allow overriding in mpconfigboard.mk.
+# MicroPython settings
+# The size of a MCU flash filesystem will be
+# 1008k - MICROPY_HW_CODESIZE - MICROPY_HW_VFSROMSIZE
+# The default for MICROPY_HW_VFSROMSIZE is 64K
+MICROPY_HW_CODESIZE ?= 752K
+MICROPY_HW_VFSROMSIZE ?= 128K
