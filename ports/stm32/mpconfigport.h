931243ea8c170880b84fe339a329d450156fea72
Angus Gratton <angus@redyak.com.au>
2025-01-28 11:54:57 +1100

    docs: Note which ports have default or optional network.PPP support.
    Also add the default values of these macros to the respective
    `mpconfigport.h` files, to improve discoverability.
    
    Signed-off-by: Angus Gratton <angus@redyak.com.au>

--- ports/stm32/mpconfigport.h
+++ ports/stm32/mpconfigport.h
@@ -146,14 +146,22 @@
 #define MICROPY_HW_SOFTSPI_MAX_BAUDRATE (HAL_RCC_GetSysClockFreq() / 48)
 #define MICROPY_PY_WEBSOCKET        (MICROPY_PY_LWIP)
 #define MICROPY_PY_WEBREPL          (MICROPY_PY_LWIP)
-#ifndef MICROPY_PY_SOCKET
-#define MICROPY_PY_SOCKET           (1)
-#endif
 #ifndef MICROPY_PY_NETWORK
 #define MICROPY_PY_NETWORK          (1)
 #endif
 #ifndef MICROPY_PY_ONEWIRE
 #define MICROPY_PY_ONEWIRE          (1)
+#endif
+
+// optional network features
+#if MICROPY_PY_NETWORK
+#ifndef MICROPY_PY_SOCKET
+#define MICROPY_PY_SOCKET           (1)
+#endif
+
+#ifndef MICROPY_PY_NETWORK_PPP_LWIP
+#define MICROPY_PY_NETWORK_PPP_LWIP     (0)
+#endif
 #endif
 
 // fatfs configuration used in ffconf.h
