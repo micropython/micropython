52c8742ae2b250b49cc5da9eaf1befe9769c35b7
Angus Gratton <angus@redyak.com.au>
2025-02-05 18:17:02 +1100

    esp32/boards: Enable machine.SDCard on all boards.
    This increases binary size by about 4KB on C3, probably a bit less on S2.
    
    This work was funded through GitHub Sponsors.
    
    Signed-off-by: Angus Gratton <angus@redyak.com.au>

--- ports/esp32/boards/ESP32_GENERIC_C3/mpconfigboard.h
+++ ports/esp32/boards/ESP32_GENERIC_C3/mpconfigboard.h
@@ -3,7 +3,5 @@
 #define MICROPY_HW_BOARD_NAME               "ESP32C3 module"
 #define MICROPY_HW_MCU_NAME                 "ESP32C3"
 
-#define MICROPY_HW_ENABLE_SDCARD            (0)
-
 // Enable UART REPL for modules that have an external USB-UART and don't use native USB.
 #define MICROPY_HW_ENABLE_UART_REPL         (1)
--- ports/esp32/boards/ESP32_GENERIC_S2/mpconfigboard.h
+++ ports/esp32/boards/ESP32_GENERIC_S2/mpconfigboard.h
@@ -2,7 +2,6 @@
 #define MICROPY_HW_MCU_NAME "ESP32S2"
 
 #define MICROPY_PY_BLUETOOTH (0)
-#define MICROPY_HW_ENABLE_SDCARD (0)
 
 // Enable UART REPL for modules that have an external USB-UART and don't use native USB.
 #define MICROPY_HW_ENABLE_UART_REPL         (1)
