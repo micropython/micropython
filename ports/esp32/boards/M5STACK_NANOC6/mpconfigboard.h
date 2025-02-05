464121f30158e769b4bcd4e0187f9fc58b6e09d9
Angus Gratton <angus@redyak.com.au>
2025-02-05 18:17:02 +1100

    esp32/boards: Enable machine.SDCard on all boards.
    This increases binary size by about 4KB on C3, probably a bit less on S2.
    
    This work was funded through GitHub Sponsors.
    
    Signed-off-by: Angus Gratton <angus@redyak.com.au>

--- ports/esp32/boards/M5STACK_NANOC6/mpconfigboard.h
+++ ports/esp32/boards/M5STACK_NANOC6/mpconfigboard.h
@@ -1,7 +1,6 @@
 #define MICROPY_HW_BOARD_NAME    "M5Stack NanoC6"
 #define MICROPY_HW_MCU_NAME      "ESP32C6"
 
-#define MICROPY_HW_ENABLE_SDCARD (0)
 #define MICROPY_PY_MACHINE_I2S   (0)
 
 #define MICROPY_HW_I2C0_SCL      (1)
