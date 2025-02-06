6c91d5440f1fba794c1b8b5db0a48df81aba5ceb
Detlev Zundel <dzu@member.fsf.org>
2025-02-06 16:45:25 +0100

    zephyr: Fix call to thread_analyzer_print for v4.0.
    The function "thread_analyzer_print" requires an unsigned int paramter
    since commit 1b6e0f64796dfd6f86a8679ea6d24e1fca1e63a8 even though it
    is ignored when THREAD_ANALYZER_AUTO_SEPARATE_CORES=n which is the
    default on single core machines.
    
    Signed-off-by: Detlev Zundel <dzu@member.fsf.org>

--- ports/zephyr/modzephyr.c
+++ ports/zephyr/modzephyr.c
@@ -33,6 +33,7 @@
 #include <zephyr/debug/thread_analyzer.h>
 #include <zephyr/shell/shell.h>
 #include <zephyr/shell/shell_uart.h>
+#include <zephyr/version.h>
 
 #include "modzephyr.h"
 #include "py/runtime.h"
@@ -49,7 +50,11 @@ static MP_DEFINE_CONST_FUN_OBJ_0(mod_current_tid_obj, mod_current_tid);
 
 #ifdef CONFIG_THREAD_ANALYZER
 static mp_obj_t mod_thread_analyze(void) {
+    #if KERNEL_VERSION_NUMBER >= ZEPHYR_VERSION(4, 0, 0)
+    thread_analyzer_print(0);
+    #else
     thread_analyzer_print();
+    #endif
     return mp_const_none;
 }
 static MP_DEFINE_CONST_FUN_OBJ_0(mod_thread_analyze_obj, mod_thread_analyze);
