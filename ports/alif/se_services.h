65a302014b8e0868403c13db06d1c5629b8fd8b3
iabdalkader <i.abdalkader@gmail.com>
2025-02-15 13:53:44 +0100

    alif/se_services: Add a secondary MHU channel.
    This channel can be used to communicate (pass messages) between
    the M55 cores in the RTSS. Currently it's only used to notify
    the cores.
    
    Signed-off-by: iabdalkader <i.abdalkader@gmail.com>

--- ports/alif/se_services.h
+++ ports/alif/se_services.h
@@ -29,12 +29,16 @@
 #include "services_lib_api.h"
 
 void se_services_init(void);
+void se_services_deinit(void);
 void se_services_dump_device_data(void);
 void se_services_get_unique_id(uint8_t id[8]);
 __attribute__((noreturn)) void se_services_reset_soc(void);
 uint64_t se_services_rand64(void);
+uint32_t se_services_notify(void);
+
 uint32_t se_services_enable_clock(clock_enable_t clock, bool enable);
 uint32_t se_services_select_pll_source(pll_source_t source, pll_target_t target);
+
 uint32_t se_services_get_run_profile(run_profile_t *profile);
 uint32_t se_services_set_run_profile(run_profile_t *profile);
 uint32_t se_services_get_off_profile(off_profile_t *profile);
