d41ca9de97b75733096b7bd39361faa384b7f6d0
Damien George <damien@micropython.org>
2025-03-08 23:19:47 +1100

    alif/modules: Make HE core set /rom as current dir.
    This allows HE to execute code from the ROMFS in MRAM.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/alif/modules/he/_boot.py
+++ ports/alif/modules/he/_boot.py
@@ -1,21 +1,7 @@
-import openamp
-import time
-from machine import Pin
+# Change working directory to ROMFS, so boot.py and main.py can run from there.
+try:
+    import os
 
-
-def ept_recv_callback(src_addr, data):
-    print("Received message on endpoint", data)
-
-
-# Create a new RPMsg endpoint to communicate with main core.
-ept = openamp.Endpoint("vuart-channel", callback=ept_recv_callback)
-
-pin = Pin("LED_BLUE", Pin.OUT)
-
-count = 0
-while True:
-    if ept.is_ready():
-        ept.send("Hello from HE %d" % count, timeout=1000)
-        count += 1
-    time.sleep_ms(100)
-    pin(not pin())
+    os.chdir("/rom")
+except:
+    pass
