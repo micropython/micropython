fa0ca4936c053a222f77ba819c043cab4d3edc8f
Damien George <damien@micropython.org>
2025-03-11 12:04:21 +1100

    alif: Use alif-security-toolkit submodule.
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/alif/boards/OPENMV_AE3/mpconfigboard.mk
+++ ports/alif/boards/OPENMV_AE3/mpconfigboard.mk
@@ -1,8 +1,10 @@
 # TODO: alif_ensemble-cmsis-dfp only supports AE722F80F55D5XX at the moment.
 MCU_SERIES = E7
 MCU_VARIANT = AE722F80F55D5XX
+ALIF_TOOLKIT_CFG_PART = AE302F80F55D5AE
 JLINK_DEV = AE302F80F55D5_HP
 LD_FILE = boards/OPENMV_AE3/board.ld.S
+PORT = /dev/ttyUSB0
 
 CORE_M55_HP := $(if $(filter M55_HP,$(MCU_CORE)),1,0)
 
