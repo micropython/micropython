ed45a8c5eef0c332dc50303921ea1b4625d38418
Angus Gratton <angus@redyak.com.au>
2025-03-11 12:22:38 +1100

    extmod/nimble,btstack: Don't add source files to LIST_SUBMODULES pass.
    This work was funded through GitHub Sponsors.
    
    Signed-off-by: Angus Gratton <angus@redyak.com.au>

--- extmod/nimble/nimble.cmake
+++ extmod/nimble/nimble.cmake
@@ -1,7 +1,15 @@
+string(CONCAT GIT_SUBMODULES "${GIT_SUBMODULES} " lib/mynewt-nimble)
+
 set(NIMBLE_LIB_DIR "${MICROPY_DIR}/lib/mynewt-nimble")
 set(NIMBLE_EXTMOD_DIR "${MICROPY_DIR}/extmod/nimble")
 
 add_library(micropy_extmod_nimble INTERFACE)
+
+if(LIST_SUBMODULES)
+    return()
+elseif(NOT EXISTS ${MICROPY_DIR}/lib/mynewt-nimble/nimble/host/include/host/ble_hs.h)
+    message(FATAL_ERROR " mynewt-nimble not initialized.\n Run 'make BOARD=${MICROPY_BOARD} submodules'")
+endif()
 
 target_include_directories(micropy_extmod_nimble INTERFACE
     ${MICROPY_DIR}/
@@ -19,62 +27,65 @@ target_include_directories(micropy_extmod_nimble INTERFACE
     ${NIMBLE_LIB_DIR}/porting/nimble/include
 )
 
-target_sources(micropy_extmod_nimble INTERFACE
-    ${NIMBLE_EXTMOD_DIR}/hal/hal_uart.c
-    ${NIMBLE_EXTMOD_DIR}/nimble/nimble_npl_os.c
-    ${NIMBLE_LIB_DIR}/ext/tinycrypt/src/aes_encrypt.c
-    ${NIMBLE_LIB_DIR}/ext/tinycrypt/src/cmac_mode.c
-    ${NIMBLE_LIB_DIR}/ext/tinycrypt/src/ecc.c
-    ${NIMBLE_LIB_DIR}/ext/tinycrypt/src/ecc_dh.c
-    ${NIMBLE_LIB_DIR}/ext/tinycrypt/src/utils.c
-    ${NIMBLE_LIB_DIR}/nimble/host/services/gap/src/ble_svc_gap.c
-    ${NIMBLE_LIB_DIR}/nimble/host/services/gatt/src/ble_svc_gatt.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_att.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_att_clt.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_att_cmd.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_att_svr.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_eddystone.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_gap.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_gattc.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_gatts.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs_adv.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs_atomic.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs_cfg.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs_conn.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs_flow.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs_hci.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs_hci_cmd.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs_hci_evt.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs_hci_util.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs_id.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs_log.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs_mbuf.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs_misc.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs_mqueue.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs_pvcy.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs_startup.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs_stop.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_ibeacon.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_l2cap.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_l2cap_coc.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_l2cap_sig.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_l2cap_sig_cmd.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_monitor.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_sm.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_sm_alg.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_sm_cmd.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_sm_lgcy.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_sm_sc.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_store.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_store_util.c
-    ${NIMBLE_LIB_DIR}/nimble/host/src/ble_uuid.c
-    ${NIMBLE_LIB_DIR}/nimble/host/util/src/addr.c
-    ${NIMBLE_LIB_DIR}/nimble/transport/uart/src/ble_hci_uart.c
-    ${NIMBLE_LIB_DIR}/porting/nimble/src/endian.c
-    ${NIMBLE_LIB_DIR}/porting/nimble/src/mem.c
-    ${NIMBLE_LIB_DIR}/porting/nimble/src/nimble_port.c
-    ${NIMBLE_LIB_DIR}/porting/nimble/src/os_mbuf.c
-    ${NIMBLE_LIB_DIR}/porting/nimble/src/os_mempool.c
-    ${NIMBLE_LIB_DIR}/porting/nimble/src/os_msys_init.c
-)
+
+if(NOT LIST_SUBMODULES)
+    target_sources(micropy_extmod_nimble INTERFACE
+        ${NIMBLE_EXTMOD_DIR}/hal/hal_uart.c
+        ${NIMBLE_EXTMOD_DIR}/nimble/nimble_npl_os.c
+        ${NIMBLE_LIB_DIR}/ext/tinycrypt/src/aes_encrypt.c
+        ${NIMBLE_LIB_DIR}/ext/tinycrypt/src/cmac_mode.c
+        ${NIMBLE_LIB_DIR}/ext/tinycrypt/src/ecc.c
+        ${NIMBLE_LIB_DIR}/ext/tinycrypt/src/ecc_dh.c
+        ${NIMBLE_LIB_DIR}/ext/tinycrypt/src/utils.c
+        ${NIMBLE_LIB_DIR}/nimble/host/services/gap/src/ble_svc_gap.c
+        ${NIMBLE_LIB_DIR}/nimble/host/services/gatt/src/ble_svc_gatt.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_att.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_att_clt.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_att_cmd.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_att_svr.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_eddystone.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_gap.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_gattc.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_gatts.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs_adv.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs_atomic.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs_cfg.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs_conn.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs_flow.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs_hci.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs_hci_cmd.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs_hci_evt.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs_hci_util.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs_id.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs_log.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs_mbuf.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs_misc.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs_mqueue.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs_pvcy.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs_startup.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_hs_stop.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_ibeacon.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_l2cap.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_l2cap_coc.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_l2cap_sig.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_l2cap_sig_cmd.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_monitor.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_sm.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_sm_alg.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_sm_cmd.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_sm_lgcy.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_sm_sc.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_store.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_store_util.c
+        ${NIMBLE_LIB_DIR}/nimble/host/src/ble_uuid.c
+        ${NIMBLE_LIB_DIR}/nimble/host/util/src/addr.c
+        ${NIMBLE_LIB_DIR}/nimble/transport/uart/src/ble_hci_uart.c
+        ${NIMBLE_LIB_DIR}/porting/nimble/src/endian.c
+        ${NIMBLE_LIB_DIR}/porting/nimble/src/mem.c
+        ${NIMBLE_LIB_DIR}/porting/nimble/src/nimble_port.c
+        ${NIMBLE_LIB_DIR}/porting/nimble/src/os_mbuf.c
+        ${NIMBLE_LIB_DIR}/porting/nimble/src/os_mempool.c
+        ${NIMBLE_LIB_DIR}/porting/nimble/src/os_msys_init.c
+    )
+endif()
