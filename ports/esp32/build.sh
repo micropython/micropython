f0031ae985684adc552101126dfc0e46cb48d221
Wang Xuancong <xuancong84@gmail.com>
2025-03-16 13:47:19 +0800

    esp32 working after merge

--- ports/esp32/build.sh
+++ ports/esp32/build.sh
@@ -2,7 +2,7 @@
 
 cd `dirname $(readlink -f "$0")`
 
-export PATH=~/.espressif/python_env/idf5*_env/bin:$PATH
+export PATH=~/.espressif/python_env/idf5.2*_env/bin:$PATH
 
 cd ~/projects/esp-idf
 source export.sh
