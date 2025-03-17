bfa9c451a396f4f7a943a6488289a600a48b4606
Malcolm McKellips <malcolm.mckellips@sparkfun.com>
2025-03-17 14:33:59 -0600

    rp2/boards/SPARKFUN_IOTNODE_LORAWAN_RP2350: Add SD card support.
    The IOTNODE_LORAWAN_RP2350 has an SD card and we want users to be able to
    `import sdcard` without copying `sdcard.py` over to their board.
    
    Signed-off-by: Malcolm McKellips <malcolm.mckellips@sparkfun.com>

--- ports/rp2/boards/SPARKFUN_IOTNODE_LORAWAN_RP2350/mpconfigboard.cmake
+++ ports/rp2/boards/SPARKFUN_IOTNODE_LORAWAN_RP2350/mpconfigboard.cmake
@@ -5,3 +5,6 @@ set(PICO_BOARD_HEADER_DIRS ${MICROPY_PORT_DIR}/boards/${MICROPY_BOARD})
 
 set(PICO_BOARD "sparkfun_iotnode_lorawan_rp2350")
 set(PICO_PLATFORM "rp2350")
+
+# Board specific version of the frozen manifest
+set(MICROPY_FROZEN_MANIFEST ${MICROPY_BOARD_DIR}/manifest.py)
