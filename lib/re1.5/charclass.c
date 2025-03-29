d88f2640de696fee9c528d5975862399fd6eadd1
Andrew Leech <andrew.leech@planetinnovation.com.au>
2025-03-29 23:20:44 +1100

    lib/re1.5/charclass: Use faster character range checks.
    Signed-off-by: Andrew Leech <andrew@alelec.net>

--- lib/re1.5/charclass.c
+++ lib/re1.5/charclass.c
@@ -1,19 +1,28 @@
 #include "re1.5.h"
 
+// More efficient character range check macro
+#define CHAR_IN_RANGE(c, min, max) ((unsigned char)(c) - (unsigned char)(min) <= (unsigned char)(max) - (unsigned char)(min))
+
+// Specialized character class checks for better performance
+#define IS_DIGIT(c) CHAR_IN_RANGE(c, '0', '9')
+#define IS_SPACE(c) ((c) == ' ' || CHAR_IN_RANGE(c, '\t', '\r'))
+#define IS_WORD(c) (IS_DIGIT(c) || CHAR_IN_RANGE(c, 'A', 'Z') || CHAR_IN_RANGE(c, 'a', 'z') || (c) == '_')
+
 int _re1_5_classmatch(const char *pc, const char *sp)
 {
     // pc points to "cnt" byte after opcode
     int is_positive = (pc[-1] == Class);
-    int cnt = *pc++;
+    int cnt = *(unsigned char*)pc++;  // Use unsigned to avoid sign extension
+    char sp_c = *sp;                  // Cache value for performance
+    
+    // Fast path for common classes
     while (cnt--) {
         if (*pc == RE15_CLASS_NAMED_CLASS_INDICATOR) {
             if (_re1_5_namedclassmatch(pc + 1, sp)) {
                 return is_positive;
             }
-        } else {
-            if (*sp >= *pc && *sp <= pc[1]) {
-                return is_positive;
-            }
+        } else if (CHAR_IN_RANGE(sp_c, *pc, pc[1])) {
+            return is_positive;
         }
         pc += 2;
     }
@@ -23,19 +32,20 @@ int _re1_5_classmatch(const char *pc, const char *sp)
 int _re1_5_namedclassmatch(const char *pc, const char *sp)
 {
     // pc points to name of class
-    int off = (*pc >> 5) & 1;
-    if ((*pc | 0x20) == 'd') {
-        if (!(*sp >= '0' && *sp <= '9')) {
-            off ^= 1;
-        }
-    } else if ((*pc | 0x20) == 's') {
-        if (!(*sp == ' ' || (*sp >= '\t' && *sp <= '\r'))) {
-            off ^= 1;
-        }
-    } else { // w
-        if (!((*sp >= 'A' && *sp <= 'Z') || (*sp >= 'a' && *sp <= 'z') || (*sp >= '0' && *sp <= '9') || *sp == '_')) {
-            off ^= 1;
-        }
+    char sp_c = *sp;                  // Cache value for performance
+    char class_type = *pc | 0x20;     // Case-insensitive comparison
+    int inverted = (*pc >> 5) & 1;    // Upper bits = inversion flag
+    int result;
+
+    // Specialized class matching logic with dedicated macros
+    if (class_type == 'd') {
+        result = IS_DIGIT(sp_c);
+    } else if (class_type == 's') {
+        result = IS_SPACE(sp_c);
+    } else { // 'w'
+        result = IS_WORD(sp_c);
     }
-    return off;
+    
+    // XOR with inverted flag to handle negated classes (\D, \S, \W)
+    return result ^ inverted;
 }
