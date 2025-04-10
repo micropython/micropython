2ffcaf89294797895102a2816b305d6a0b3c8bc7
Rick Sorensen <rick.sorensen@gmail.com>
2025-04-10 15:29:52 -0500

    ESP32_GENERIC_C3/mpconfigboard.h: Add defaults for I2C pins.
    Signed-off-by: Rick Sorensen <rick.sorensen@gmail.com>

--- ports/esp32/boards/ESP32_GENERIC_C3/mpconfigboard.h
+++ ports/esp32/boards/ESP32_GENERIC_C3/mpconfigboard.h
@@ -5,3 +5,21 @@
 
 // Enable UART REPL for modules that have an external USB-UART and don't use native USB.
 #define MICROPY_HW_ENABLE_UART_REPL         (1)
+
+/**
+   22 Pins total
+                          SDA      SCL
+   For adafruit qtpy        5        6
+   For xiao c3              6        7
+
+   GPIO 8&9 are strapping (okay to use after start)
+   GPIO 12-17 are typically used for SPI flash/PSRAM
+   GPIO 18&19 are typically used for USB/JTAG
+
+   GPIO 0-7,10-11,20-21 generally possible
+   Without defining, SDL/SDA default is 19/18 which
+   conflict with USB
+**/
+# define I2C Defaults:
+#define MICROPY_HW_I2C0_SCL                 (7)
+#define MICROPY_HW_I2C0_SDA                 (6)
