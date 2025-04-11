92a6473c28049d1783c90567f7849ca186d0943e
Rick Sorensen <rick.sorensen@gmail.com>
2025-04-10 20:29:44 -0500

    ESP32_GENERIC_C3/mpconfigport.h: Correct format error.
    Signed-off-by: Rick Sorensen <rick.sorensen@gmail.com>

--- ports/esp32/boards/ESP32_GENERIC_C3/mpconfigboard.h
+++ ports/esp32/boards/ESP32_GENERIC_C3/mpconfigboard.h
@@ -20,6 +20,6 @@
    Without defining, SDL/SDA default is 19/18 which
    conflict with USB
 **/
-# define I2C Defaults:
+#define I2C Defaults:
 #define MICROPY_HW_I2C0_SCL                 (7)
 #define MICROPY_HW_I2C0_SDA                 (6)
