cc855c98e525429e3951a905ce25eb31ee7a0022
Keenan Johnson <keenan.johnson@gmail.com>
2025-04-29 19:26:29 -0700

    Update test

--- tests/multi_net/sslcontext_server_client_psk_cipher.py
+++ tests/multi_net/sslcontext_server_client_psk_cipher.py
@@ -10,21 +10,28 @@ except ImportError:
 
 PORT = 8000
 
+PSK_ID = "PSK-Identity-1"
+PSK_KEY = "c0ffee"
+PSK_CIPHER = "TLS-PSK-WITH-AES-128-CBC-SHA256"
+
 # Server
 def instance0():
     multitest.globals(IP=multitest.get_network_ip())
+
     s = socket.socket()
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind(socket.getaddrinfo("0.0.0.0", PORT)[0][-1])
     s.listen(1)
+    
     multitest.next()
+    
     s2, _ = s.accept()
     server_ctx = tls.SSLContext(tls.PROTOCOL_TLS_SERVER)
     
     # Configure PSK with specific ciphersuite
-    server_ctx.set_psk_identity("PSK-Identity-1")
-    server_ctx.set_psk_key(bytes.fromhex("c0ffee"))
-    server_ctx.set_ciphers("TLS-PSK-WITH-AES-128-CBC-SHA256")
+    server_ctx.set_psk_identity(PSK_ID)
+    server_ctx.set_psk_key(bytes.fromhex(PSK_KEY))
+    server_ctx.set_ciphers(PSK_CIPHER)
     
     s2 = server_ctx.wrap_socket(s2, server_side=True)
     print(s2.read(16))
@@ -36,14 +43,15 @@ def instance0():
 # Client
 def instance1():
     multitest.next()
+    
     s = socket.socket()
     s.connect(socket.getaddrinfo(IP, PORT)[0][-1])
     client_ctx = tls.SSLContext(tls.PROTOCOL_TLS_CLIENT)
     
     # Configure PSK with specific ciphersuite
-    client_ctx.set_psk_identity("PSK-Identity-1")
-    client_ctx.set_psk_key(bytes.fromhex("c0ffee"))
-    client_ctx.set_ciphers("TLS-PSK-WITH-AES-128-CBC-SHA256")
+    client_ctx.set_psk_identity(PSK_ID)
+    client_ctx.set_psk_key(bytes.fromhex(PSK_KEY))
+    client_ctx.set_ciphers(PSK_CIPHER)
     
     s = client_ctx.wrap_socket(s, server_hostname="micropython.local")
     s.write(b"client to server")
