f53c433d670e867196359376faddbb8bf963270b
Keenan Johnson <keenan.johnson@gmail.com>
2025-04-29 19:58:44 -0700

    Fix formatting

--- tests/multi_net/sslcontext_server_client_psk_cipher.py
+++ tests/multi_net/sslcontext_server_client_psk_cipher.py
@@ -17,22 +17,17 @@ PSK_CIPHER = "TLS-PSK-WITH-AES-128-CBC-SHA256"
 # Server
 def instance0():
     multitest.globals(IP=multitest.get_network_ip())
-
     s = socket.socket()
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind(socket.getaddrinfo("0.0.0.0", PORT)[0][-1])
     s.listen(1)
-    
     multitest.next()
-    
     s2, _ = s.accept()
     server_ctx = tls.SSLContext(tls.PROTOCOL_TLS_SERVER)
-    
     # Configure PSK with specific ciphersuite
     server_ctx.set_psk_identity(PSK_ID)
     server_ctx.set_psk_key(bytes.fromhex(PSK_KEY))
     server_ctx.set_ciphers(PSK_CIPHER)
-    
     s2 = server_ctx.wrap_socket(s2, server_side=True)
     print(s2.read(16))
     s2.write(b"server to client")
@@ -43,16 +38,13 @@ def instance0():
 # Client
 def instance1():
     multitest.next()
-    
     s = socket.socket()
     s.connect(socket.getaddrinfo(IP, PORT)[0][-1])
     client_ctx = tls.SSLContext(tls.PROTOCOL_TLS_CLIENT)
-    
     # Configure PSK with specific ciphersuite
     client_ctx.set_psk_identity(PSK_ID)
     client_ctx.set_psk_key(bytes.fromhex(PSK_KEY))
     client_ctx.set_ciphers(PSK_CIPHER)
-    
     s = client_ctx.wrap_socket(s, server_hostname="micropython.local")
     s.write(b"client to server")
     print(s.read(16))
