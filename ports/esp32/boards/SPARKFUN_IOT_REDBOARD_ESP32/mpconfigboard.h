9e34599e60d19fbc2ad9be5c3c496b446d188976
Malcolm McKellips <malcolm.mckellips@sparkfun.com>
2025-05-21 10:05:08 -0600

    ports/esp32/boards/SPARKFUN_IOT_REDBOARD_ESP32: Make fixes from PR.

--- ports/esp32/boards/SPARKFUN_IOT_REDBOARD_ESP32/mpconfigboard.h
+++ ports/esp32/boards/SPARKFUN_IOT_REDBOARD_ESP32/mpconfigboard.h
@@ -9,9 +9,6 @@
 #define MICROPY_HW_I2C0_SCL            (22)
 #define MICROPY_HW_I2C0_SDA            (21)
 
-#define MICROPY_HW_SPI0_SCK  (18)
-#define MICROPY_HW_SPI0_MOSI (23)
-#define MICROPY_HW_SPI0_MISO (19)
-
-#define MICROPY_HW_UART0_TX (1)
-#define MICROPY_HW_UART0_RX (3)
+#define MICROPY_HW_SPI1_SCK  (18)
+#define MICROPY_HW_SPI1_MOSI (23)
+#define MICROPY_HW_SPI1_MISO (19)
