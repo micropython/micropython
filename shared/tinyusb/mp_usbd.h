be9287715125c50476529ca35d17998dbc26e5b5
Andrew Leech <andrew.leech@planetinnovation.com.au>
2025-05-23 11:03:45 +1000

    shared/tinyusb: Add support for USB Network (NCM) interface.
    This provides a network interface over the USB connection which
    is connected to the existing LWIP network stack.
    
    Signed-off-by: Andrew Leech <andrew.leech@planetinnovation.com.au>

--- shared/tinyusb/mp_usbd.h
+++ shared/tinyusb/mp_usbd.h
@@ -35,6 +35,10 @@
 #include "py/objarray.h"
 #include "py/runtime.h"
 
+#if MICROPY_HW_NETWORK_USBNET
+#include "extmod/network_usbd_ncm.h"
+#endif
+
 #ifndef NO_QSTR
 #include "tusb.h"
 #include "device/dcd.h"
@@ -66,6 +70,7 @@ void mp_usbd_hex_str(char *out_str, const uint8_t *bytes, size_t bytes_len);
 #define MP_USBD_BUILTIN_DESC_CFG_LEN ( \
     (CFG_TUD_CDC ? (TUD_CDC_DESC_LEN) : 0) + \
     (CFG_TUD_MSC ? (TUD_MSC_DESC_LEN) : 0) + \
+    (CFG_TUD_NCM ? (TUD_CDC_NCM_DESC_LEN) : 0) + \
     TUD_CONFIG_DESC_LEN)
 
 // Built-in USB device and configuration descriptor values
