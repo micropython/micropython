14fa053f61258d42130a9e10011fe49500e738ad
Garatronic <fpierson@garatronic.fr>
2025-05-28 12:09:16 +0200

    New board in ESP32 port
    ESP32C3 board from GARATRONIC

---
+++ ports/esp32/GARATRONIC_PYBSTICK26_ESP32C3/mpconfigboard.h
@@ -0,0 +1,10 @@
+#define MICROPY_HW_BOARD_NAME               "PYBSTICK26_ESP32C3"
+#define MICROPY_HW_MCU_NAME                 "ESP32C3"
+#define MICROPY_PY_NETWORK_HOSTNAME_DEFAULT "pybstick26_esp32c3"
+
+#define MICROPY_HW_I2C0_SCL                 (1)
+#define MICROPY_HW_I2C0_SDA                 (0)
+
+#define MICROPY_HW_SPI1_MOSI                (7)
+#define MICROPY_HW_SPI1_MISO                (2)
+#define MICROPY_HW_SPI1_SCK                 (6)
