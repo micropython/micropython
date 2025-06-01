f549cdd6b7d08c3033aece413a529669e4417472
Jeff Epler <jepler@gmail.com>
2025-06-01 20:11:55 +0200

    unix: Check for UB during coverage build.
    Signed-off-by: Jeff Epler <jepler@gmail.com>

--- ports/unix/variants/coverage/mpconfigvariant.mk
+++ ports/unix/variants/coverage/mpconfigvariant.mk
@@ -1,13 +1,15 @@
 # Disable optimisations and enable assert() on coverage builds.
 DEBUG ?= 1
 
+SANITIZER ?= -fsanitize=undefined
+
 CFLAGS += \
 	-fprofile-arcs -ftest-coverage \
 	-Wformat -Wmissing-declarations -Wmissing-prototypes \
 	-Wold-style-definition -Wpointer-arith -Wshadow -Wuninitialized -Wunused-parameter \
-	-DMICROPY_UNIX_COVERAGE
+	-DMICROPY_UNIX_COVERAGE $(SANITIZER)
 
-LDFLAGS += -fprofile-arcs -ftest-coverage
+LDFLAGS += -fprofile-arcs -ftest-coverage $(SANITIZER)
 
 FROZEN_MANIFEST ?= $(VARIANT_DIR)/manifest.py
 USER_C_MODULES = $(TOP)/examples/usercmodule
