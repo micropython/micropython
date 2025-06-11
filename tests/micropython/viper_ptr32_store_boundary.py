9ebd4ba22de7e96479099d1020cfed4fa55df115
Alessandro Gatti <a.gatti@frob.it>
2025-06-11 09:31:34 +0200

    tests/micropython/viper_ptr: Improve boundary tests.
    This commit reworks the Viper pointer boundary tests in order to make
    them more accurate and easier to extend.
    
    The tests are now easier to reason about in their output, using easier
    to read values, and bit thresholds are now more configurable.  If a new
    conditional code sequence is introduced, adding a new bit threshold is
    just a matter of adding a value into a tuple at the beginning of the
    relevant test file.
    
    Load tests have also been made more accurate, with better function
    templates to test register-indexed operations.
    
    Signed-off-by: Alessandro Gatti <a.gatti@frob.it>

--- tests/micropython/viper_ptr32_store_boundary.py
+++ tests/micropython/viper_ptr32_store_boundary.py
@@ -1,35 +1,58 @@
 # Test boundary conditions for various architectures
 
-TEST_DATA = (
-    (3, (0x04030201, 0x08070605, 0x0C0B0A09)),
-    (63, (0x100F0E0D, 0x14131211, 0x18171615)),
-    (1023, (0x1C1B1A19, 0x201F1E1D, 0x24232221)),
-)
-
 SET_TEMPLATE = """
 @micropython.viper
 def set{off}(dest: ptr32):
-    dest[{off}] = {val} & 0x3FFFFFFF
-set{off}(b)
-print(b[{off} * 4:({off} + 1) * 4])
+    dest[{off}] = {val}
+set{off}(buffer)
+print(hex(get_index(buffer, {off})))
 """
+
+BIT_THRESHOLDS = (5, 8, 11, 12)
+SIZE = 4
+MASK = (1 << (8 * SIZE)) - 1
 
 
 @micropython.viper
-def set_index(dest: ptr32, i: int, val: int):
+def set_index(dest: ptr32, i: int, val: uint):
     dest[i] = val
 
 
-b = bytearray(5000)
-for start, vals in TEST_DATA:
-    for i, v in enumerate(vals):
-        set_index(b, start + i, v)
-        print(b[(start + i) * 4 : (start + i + 1) * 4])
+def get_index(src, i):
+    return (
+        src[i * SIZE]
+        + (src[(i * SIZE) + 1] << 8)
+        + (src[(i * SIZE) + 2] << 16)
+        + (src[(i * SIZE) + 3] << 24)
+    )
 
-for i in range(len(b)):
-    b[i] = 0
 
-
-for start, vals in TEST_DATA:
-    for i, v in enumerate(vals):
-        exec(SET_TEMPLATE.format(off=start + i, val=v + 0x01010101))
+buffer = bytearray(((1 << max(BIT_THRESHOLDS) + 1) // 1024) * 1024)
+next = 1
+val = 0
+for bit in BIT_THRESHOLDS:
+    print("---", bit)
+    pre, idx, post = (
+        (((1 << bit) - (2 * SIZE)) // SIZE),
+        (((1 << bit) - (1 * SIZE)) // SIZE),
+        ((1 << bit) // SIZE),
+    )
+    val = (val << 8) + next
+    next += 1
+    set_index(buffer, pre, val & MASK)
+    val = (val << 8) + next
+    next += 1
+    set_index(buffer, idx, val & MASK)
+    val = (val << 8) + next
+    next += 1
+    set_index(buffer, post, val & MASK)
+    val = (val << 8) + next
+    next += 1
+    print(hex(get_index(buffer, pre)), hex(get_index(buffer, idx)), hex(get_index(buffer, post)))
+    exec(SET_TEMPLATE.format(off=pre, val=val & MASK))
+    val = (val << 8) + next
+    next += 1
+    exec(SET_TEMPLATE.format(off=idx, val=val & MASK))
+    val = (val << 8) + next
+    next += 1
+    exec(SET_TEMPLATE.format(off=post, val=val & MASK))
