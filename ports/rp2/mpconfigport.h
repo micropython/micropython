002dfd274d09e7bbac6a1b002b651a229492cf34
Hugo Frisk <hugopaf@outlook.com>
2025-06-13 14:29:06 +0200

    rp2/machine_i2c: Allow disabling soft i2c.
    Allow rp2 board definitions to disable soft i2c.
    
    Signed-off-by: Hugo Frisk <hugopaf@outlook.com>

--- ports/rp2/mpconfigport.h
+++ ports/rp2/mpconfigport.h
@@ -180,7 +180,9 @@
 #ifndef MICROPY_PY_MACHINE_I2C
 #define MICROPY_PY_MACHINE_I2C                  (1)
 #endif
+#ifndef MICROPY_PY_MACHINE_SOFTI2C
 #define MICROPY_PY_MACHINE_SOFTI2C              (1)
+#endif
 #define MICROPY_PY_MACHINE_I2S                  (1)
 #define MICROPY_PY_MACHINE_I2S_INCLUDEFILE      "ports/rp2/machine_i2s.c"
 #define MICROPY_PY_MACHINE_I2S_CONSTANT_RX      (RX)
