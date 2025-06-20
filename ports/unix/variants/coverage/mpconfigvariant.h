989b0be60f01d16e8eb634cdd9da0a3aa7413681
Jeff Epler <jepler@gmail.com>
2025-06-20 19:44:07 +0200

    coverage: Enable sys.settrace.
    Signed-off-by: Jeff Epler <jepler@gmail.com>

--- ports/unix/variants/coverage/mpconfigvariant.h
+++ ports/unix/variants/coverage/mpconfigvariant.h
@@ -39,6 +39,7 @@
 
 // Enable additional features.
 #define MICROPY_DEBUG_PARSE_RULE_NAME  (1)
+#define MICROPY_PY_SYS_SETTRACE        (1)
 #define MICROPY_TRACKED_ALLOC          (1)
 #define MICROPY_WARNINGS_CATEGORY      (1)
 #undef MICROPY_VFS_ROM_IOCTL
