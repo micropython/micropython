061bc5cddf7704fff1d890b80e05a5a9f04355d9
David Schneider <schneidav81@gmail.com>
2025-06-23 14:52:57 +0200

    zephyr: Update generated header path.
    Support disabled LEGACY_GENERATED_INCLUDE_PATH compatibility option.
    Since Zephyr 3.7 generated include files are namespaced.
    
    See also: zephyrproject-rtos/zephyr@bbe5e1e6ebf4a1a66c0
    
    Signed-off-by: David Schneider <schneidav81@gmail.com>

--- ports/zephyr/mpconfigport_minimal.h
+++ ports/zephyr/mpconfigport_minimal.h
@@ -26,7 +26,7 @@
 #include <alloca.h>
 
 // Include Zephyr's autoconf.h, which should be made first by Zephyr makefiles
-#include "autoconf.h"
+#include <zephyr/autoconf.h>
 // Included here to get basic Zephyr environment (macros, etc.)
 #include <zephyr/kernel.h>
 
