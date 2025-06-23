6b82eb75bef70d44ef583385301b7c483ac9ae94
Damien George <damien@micropython.org>
2025-06-23 12:53:49 +1000

    zephyr/main: Add /flash/lib or /sd/lib to sys.path on start up.
    If there is a filesystem available, this change makes sure there is a "lib"
    in `sys.path`, eg so that "mip install" works correctly.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/zephyr/main.c
+++ ports/zephyr/main.c
@@ -99,6 +99,7 @@ static void vfs_init(void) {
     mp_obj_t bdev = NULL;
     mp_obj_t mount_point;
     const char *mount_point_str = NULL;
+    qstr path_lib_qstr = MP_QSTRnull;
     int ret = 0;
 
     #ifdef CONFIG_DISK_DRIVER_SDMMC
@@ -109,15 +110,18 @@ static void vfs_init(void) {
     #endif
     bdev = MP_OBJ_TYPE_GET_SLOT(&zephyr_disk_access_type, make_new)(&zephyr_disk_access_type, ARRAY_SIZE(args), 0, args);
     mount_point_str = "/sd";
+    path_lib_qstr = MP_QSTR__slash_sd_slash_lib;
     #elif defined(CONFIG_FLASH_MAP) && FIXED_PARTITION_EXISTS(storage_partition)
     mp_obj_t args[] = { MP_OBJ_NEW_SMALL_INT(FIXED_PARTITION_ID(storage_partition)), MP_OBJ_NEW_SMALL_INT(4096) };
     bdev = MP_OBJ_TYPE_GET_SLOT(&zephyr_flash_area_type, make_new)(&zephyr_flash_area_type, ARRAY_SIZE(args), 0, args);
     mount_point_str = "/flash";
+    path_lib_qstr = MP_QSTR__slash_flash_slash_lib;
     #endif
 
     if ((bdev != NULL)) {
         mount_point = mp_obj_new_str_from_cstr(mount_point_str);
         ret = mp_vfs_mount_and_chdir_protected(bdev, mount_point);
+        mp_obj_list_append(mp_sys_path, MP_OBJ_NEW_QSTR(path_lib_qstr));
         // TODO: if this failed, make a new file system and try to mount again
     }
 }
