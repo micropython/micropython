78fdb14270500ace11de1010177f7b27674d3f91
Damien George <damien@micropython.org>
2025-06-26 12:25:01 +1000

    tests/micropython: Add missing SystemExit after printing SKIP.
    The test runner expects `print("SKIP")` to be followed by
    `raise SystemExit`.  Otherwise it waits for 10 seconds for the target to
    do a soft reset before timing out and continuing.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- tests/micropython/meminfo.py
+++ tests/micropython/meminfo.py
@@ -5,8 +5,9 @@ import micropython
 # these functions are not always available
 if not hasattr(micropython, "mem_info"):
     print("SKIP")
-else:
-    micropython.mem_info()
-    micropython.mem_info(1)
-    micropython.qstr_info()
-    micropython.qstr_info(1)
+    raise SystemExit
+
+micropython.mem_info()
+micropython.mem_info(1)
+micropython.qstr_info()
+micropython.qstr_info(1)
