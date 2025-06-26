57bcdcb7fa5fc6ef9ae0bc5016cadfa314fa2845
Koudai Aono <koxudaxi@gmail.com>
2025-06-26 13:04:15 +0900

    test: enhance heap allocation failure tests for PEP 750 template strings
    Signed-off-by: Koudai Aono <koxudaxi@gmail.com>

--- tests/micropython/heapalloc_fail_tstring.py
+++ tests/micropython/heapalloc_fail_tstring.py
@@ -3,102 +3,172 @@
 import micropython
 from string.templatelib import Template, Interpolation
 
-# Template string allocation failures during various operations
+# Pre-create objects that will be used in tests
+strings1 = ("x" * 100,) * 10
+interps1 = tuple(Interpolation(i, "x" + str(i)) for i in range(9))
 
-def test(msg, func):
-    micropython.heap_lock()
-    try:
-        func()
-        print(f"FAIL: {msg} - expected MemoryError")
-    except MemoryError:
-        print(f"OK: {msg}")
-    except Exception as e:
-        print(f"FAIL: {msg} - got {type(e).__name__}: {e}")
-    finally:
-        micropython.heap_unlock()
+# Test 1: Template creation with heap locked
+micropython.heap_lock()
+try:
+    Template(strings1, interps1)
+    print("FAIL: Template creation")
+except MemoryError:
+    print("OK: Template creation")
+micropython.heap_unlock()
 
-# Test 1: Template creation with large strings
-def test_template_creation():
-    strings = ("x" * 1000,) * 10
-    interps = tuple(Interpolation(i, f"x{i}") for i in range(9))
-    Template(strings, interps)
-
-test("Template creation", test_template_creation)
-
-# Test 2: Template.__str__() with memory exhaustion
+# Test 2: Template.__str__() with heap locked
 t = t"Hello {42} world {99}"
-test("Template.__str__()", lambda: t.__str__())
+micropython.heap_lock()
+try:
+    t.__str__()
+    print("FAIL: Template.__str__()")
+except MemoryError:
+    print("OK: Template.__str__()")
+micropython.heap_unlock()
 
-# Test 3: Values property with large number of interpolations
-vals = list(range(100))
-t_many = t"{vals[0]}{vals[1]}{vals[2]}{vals[3]}{vals[4]}{vals[5]}{vals[6]}{vals[7]}{vals[8]}{vals[9]}"
-test("Template.values heap allocation", lambda: t_many.values)
+# Test 3: Template.values property
+vals = list(range(10))
+t_many = t"{vals[0]}{vals[1]}{vals[2]}{vals[3]}{vals[4]}"
+micropython.heap_lock()
+try:
+    t_many.values
+    print("FAIL: Template.values heap allocation")
+except MemoryError:
+    print("OK: Template.values heap allocation")
+micropython.heap_unlock()
 
 # Test 4: Template concatenation
 t1 = t"Hello"
 t2 = t"World"
-test("Template concatenation", lambda: t1 + t2)
+micropython.heap_lock()
+try:
+    t1 + t2
+    print("FAIL: Template concatenation")
+except MemoryError:
+    print("OK: Template concatenation")
+micropython.heap_unlock()
 
-# Test 5: Iterator creation
+# Test 5: Template iterator
 t_iter = t"a{1}b{2}c"
-test("Template iterator", lambda: iter(t_iter))
+micropython.heap_lock()
+try:
+    iter(t_iter)
+    print("FAIL: Template iterator")
+except MemoryError:
+    print("OK: Template iterator")
+micropython.heap_unlock()
 
-# Test 6: __template__ builtin with allocation failure
-def test_builtin():
-    strings = ("test",) * 10
-    interps = ((42, "x", None, ""),) * 9
-    __template__(strings, interps)
+# Test 6: __template__ builtin
+strings2 = ("test",) * 5
+interps2 = ((42, "x", None, ""),) * 4
+micropython.heap_lock()
+try:
+    __template__(strings2, interps2)
+    print("FAIL: __template__ builtin")
+except MemoryError:
+    print("OK: __template__ builtin")
+micropython.heap_unlock()
 
-test("__template__ builtin", test_builtin)
-
-# Test 7: Format spec evaluation with interpolations
-# This tests the vstr allocations in format spec handling
+# Test 7: Format spec interpolation
 width = 10
 t_fmt = t"{42:{width}d}"
-test("Format spec interpolation", lambda: t_fmt.__str__())
+micropython.heap_lock()
+try:
+    t_fmt.__str__()
+    print("FAIL: Format spec interpolation")
+except MemoryError:
+    print("OK: Format spec interpolation")
+micropython.heap_unlock()
 
-# Test 8: Debug format (=) handling
+# Test 8: Debug format
 x = 42
 t_debug = t"{x=}"
-test("Debug format", lambda: t_debug.__str__())
+micropython.heap_lock()
+try:
+    t_debug.__str__()
+    print("FAIL: Debug format")
+except MemoryError:
+    print("OK: Debug format")
+micropython.heap_unlock()
 
-# Test 9: Complex conversion handling  
+# Test 9: Conversion with format
 obj = "test"
 t_conv = t"{obj!r:>10}"
-test("Conversion + format", lambda: t_conv.__str__())
+micropython.heap_lock()
+try:
+    t_conv.__str__()
+    print("FAIL: Conversion + format")
+except MemoryError:
+    print("OK: Conversion + format")
+micropython.heap_unlock()
 
-# Test 10: Multiple format spec interpolations
+# Test 10: Complex format spec
 fill = "*"
 align = ">"
 width = 10
 precision = 2
 t_complex = t"{3.14159:{fill}{align}{width}.{precision}f}"
-test("Complex format spec", lambda: t_complex.__str__())
+micropython.heap_lock()
+try:
+    t_complex.__str__()
+    print("FAIL: Complex format spec")
+except MemoryError:
+    print("OK: Complex format spec")
+micropython.heap_unlock()
 
-# Test 11: Interpolation object creation
-test("Interpolation creation", lambda: Interpolation("value", "expr", "r", ".2f"))
+# Test 11: Interpolation creation
+micropython.heap_lock()
+try:
+    Interpolation("value", "expr", "r", ".2f")
+    print("FAIL: Interpolation creation")
+except MemoryError:
+    print("OK: Interpolation creation")
+micropython.heap_unlock()
 
-# Test 12: Large values array allocation (heap path)
-# Create a template with many interpolations to trigger heap allocation
+# Test 12: Large values array
 n = 20
-interps = tuple(Interpolation(i, f"x{i}") for i in range(n))
-strings = ("",) * (n + 1)
-t_large = Template(strings, interps)
-test("Large values array", lambda: t_large.values)
+interps3 = tuple(Interpolation(i, "x" + str(i)) for i in range(n))
+strings3 = ("",) * (n + 1)
+t_large = Template(strings3, interps3)
+micropython.heap_lock()
+try:
+    t_large.values
+    print("FAIL: Large values array")
+except MemoryError:
+    print("OK: Large values array")
+micropython.heap_unlock()
 
-# Test 13: Template iterator with allocation failure during iteration
-# Create iterator before locking heap, then test iteration
+# Test 13: Iterator next
 t_iter2 = t"a{1}b{2}c{3}d"
 it = iter(t_iter2)
-test("Iterator next", lambda: list(it))
+micropython.heap_lock()
+try:
+    list(it)
+    print("FAIL: Iterator next")
+except MemoryError:
+    print("OK: Iterator next")
+micropython.heap_unlock()
 
-# Test 14: Format spec with escaped braces
+# Test 14: Escaped braces format
 t_escaped = t"{42:{{}}}"
-test("Escaped braces format", lambda: t_escaped.__str__())
+micropython.heap_lock()
+try:
+    t_escaped.__str__()
+    print("FAIL: Escaped braces format")
+except MemoryError:
+    print("OK: Escaped braces format")
+micropython.heap_unlock()
 
-# Test 15: Empty expression parsing edge case
-# This might trigger allocation in the parser
-t_empty_expr = t"{}"
-test("Empty expression", lambda: t_empty_expr.__str__())
+# Test 15: Simple expression
+x = 42
+t_simple = t"{x}"
+micropython.heap_lock()
+try:
+    t_simple.__str__()
+    print("FAIL: Simple expression")
+except MemoryError:
+    print("OK: Simple expression")
+micropython.heap_unlock()
 
-print("\nHeap allocation failure tests completed!")
+print()
+print("Heap allocation failure tests completed!", end="")
