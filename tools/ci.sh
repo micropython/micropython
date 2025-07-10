081213ec9dbdf78becc1eb0c2f2837397fd47d86
Damien George <damien@micropython.org>
2025-07-11 01:11:09 +1000

    tools/ci.sh: Increase timeout for unix qemu test runs.
    The qemu emulation introduces enough overhead that the
    `tests/thread/stress_aes.py` test overruns the default timeout.  So
    increase it to allow this test to pass.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- tools/ci.sh
+++ tools/ci.sh
@@ -797,8 +797,10 @@ function ci_unix_qemu_mips_build {
 }
 
 function ci_unix_qemu_mips_run_tests {
+    # Issues with MIPS tests:
+    # - thread/stress_aes.py takes around 50 seconds
     file ./ports/unix/build-coverage/micropython
-    (cd tests && MICROPY_MICROPYTHON=../ports/unix/build-coverage/micropython ./run-tests.py)
+    (cd tests && MICROPY_MICROPYTHON=../ports/unix/build-coverage/micropython MICROPY_TEST_TIMEOUT=90 ./run-tests.py)
 }
 
 function ci_unix_qemu_arm_setup {
@@ -818,8 +820,9 @@ function ci_unix_qemu_arm_build {
 function ci_unix_qemu_arm_run_tests {
     # Issues with ARM tests:
     # - (i)listdir does not work, it always returns the empty list (it's an issue with the underlying C call)
+    # - thread/stress_aes.py takes around 70 seconds
     file ./ports/unix/build-coverage/micropython
-    (cd tests && MICROPY_MICROPYTHON=../ports/unix/build-coverage/micropython ./run-tests.py --exclude 'vfs_posix.*\.py')
+    (cd tests && MICROPY_MICROPYTHON=../ports/unix/build-coverage/micropython MICROPY_TEST_TIMEOUT=90 ./run-tests.py --exclude 'vfs_posix.*\.py')
 }
 
 function ci_unix_qemu_riscv64_setup {
@@ -837,8 +840,10 @@ function ci_unix_qemu_riscv64_build {
 }
 
 function ci_unix_qemu_riscv64_run_tests {
+    # Issues with RISCV-64 tests:
+    # - thread/stress_aes.py takes around 140 seconds
     file ./ports/unix/build-coverage/micropython
-    (cd tests && MICROPY_MICROPYTHON=../ports/unix/build-coverage/micropython ./run-tests.py)
+    (cd tests && MICROPY_MICROPYTHON=../ports/unix/build-coverage/micropython MICROPY_TEST_TIMEOUT=180 ./run-tests.py)
 }
 
 ########################################################################################
