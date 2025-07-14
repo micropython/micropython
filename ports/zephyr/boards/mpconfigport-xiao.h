37270cdf0f579384a9dba2d028fc901abd40cf1c
Ned Konz <ned@metamagix.tech>
2025-07-14 09:58:12 -0700

    zephyr: Add comments and copyright notices to XIAO NRF52840 SENSE board files.
    Signed-off-by: Ned Konz <ned@metamagix.tech>

--- ports/zephyr/boards/mpconfigport-xiao.h
+++ ports/zephyr/boards/mpconfigport-xiao.h
@@ -3,7 +3,7 @@
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2016 Linaro Limited
+ * Copyright (c) 2025 Ned Konz <ned@metamagix.tech>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
