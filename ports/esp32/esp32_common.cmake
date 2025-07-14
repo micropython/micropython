233ef84925774ced5778510e97f529c8936b4f5a
Shlomo Zippel <shlomozippel@users.noreply.github.com>
2025-07-14 02:54:30 -0700

    ESP32: allow external USER_C_MODULES from command line (#100)

--- ports/esp32/esp32_common.cmake
+++ ports/esp32/esp32_common.cmake
@@ -11,7 +11,7 @@ endif()
 # Include core source components.
 include(${MICROPY_DIR}/py/py.cmake)
 
-set(USER_C_MODULES "${MICROPY_DIR}/user_modules/lv_binding_micropython/micropython.cmake")
+list(APPEND USER_C_MODULES "${MICROPY_DIR}/user_modules/lv_binding_micropython/micropython.cmake")
 
 # CMAKE_BUILD_EARLY_EXPANSION is set during the component-discovery phase of
 # `idf.py build`, so none of the extmod/usermod (and in reality, most of the
