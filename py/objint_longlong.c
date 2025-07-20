3a72f95919323d7a36cb3d153d92de90d64853a1
Yoctopuce dev <dev@yoctopuce.com>
2025-07-20 16:38:33 +0200

    py/objint_longlong: Fix longlong interoperability with floats.
    Current longlong implementation does not allow a float as RHS of mathematic
    operators, as it lacks the delegation code present in mpz.
    
    Signed-off-by: Yoctopuce dev <dev@yoctopuce.com>

--- py/objint_longlong.c
+++ py/objint_longlong.c
@@ -165,10 +165,27 @@ mp_obj_t mp_obj_int_binary_op(mp_binary_op_t op, mp_obj_t lhs_in, mp_obj_t rhs_i
         rhs_val = MP_OBJ_SMALL_INT_VALUE(rhs_in);
     } else if (mp_obj_is_exact_type(rhs_in, &mp_type_int)) {
         rhs_val = ((mp_obj_int_t *)rhs_in)->val;
+    #if MICROPY_PY_BUILTINS_FLOAT
+    } else if (mp_obj_is_float(rhs_in)) {
+        return mp_obj_float_binary_op(op, (mp_float_t)lhs_val, rhs_in);
+    #endif
+    #if MICROPY_PY_BUILTINS_COMPLEX
+    } else if (mp_obj_is_type(rhs_in, &mp_type_complex)) {
+        return mp_obj_complex_binary_op(op, (mp_float_t)lhs_val, 0, rhs_in);
+    #endif
     } else {
         // delegate to generic function to check for extra cases
         return mp_obj_int_binary_op_extra_cases(op, lhs_in, rhs_in);
     }
+
+    #if MICROPY_PY_BUILTINS_FLOAT
+    if (op == MP_BINARY_OP_TRUE_DIVIDE || op == MP_BINARY_OP_INPLACE_TRUE_DIVIDE) {
+        if (rhs_val == 0) {
+            goto zero_division;
+        }
+        return mp_obj_new_float((mp_float_t)lhs_val / (mp_float_t)rhs_val);
+    }
+    #endif
 
     switch (op) {
         case MP_BINARY_OP_ADD:
