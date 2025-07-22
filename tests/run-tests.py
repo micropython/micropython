26d9bf2ff743823ccd19f1ac644d23172d598410
Anson Mansfield <amansfield@mantaro.com>
2025-07-22 12:27:45 -0400

    tests/extmod/select_poll_eintr: Skip unreliable test in Github CI.
    extmod/select_poll_eintr.py is a constant source of spurious failures in
    Github CI. This PR adds it to the list of tests skipped in that
    environment in order to improve the test suite's false positive rate and
    positive predictive value in detecting defects.
    
    Signed-off-by: Anson Mansfield <amansfield@mantaro.com>

--- tests/run-tests.py
+++ tests/run-tests.py
@@ -766,6 +766,7 @@ def run_tests(pyb, tests, args, result_dir, num_threads=1):
     # Some tests shouldn't be run on GitHub Actions
     if os.getenv("GITHUB_ACTIONS") == "true":
         skip_tests.add("thread/stress_schedule.py")  # has reliability issues
+        skip_tests.add("extmod/select_poll_eintr.py")  # has reliability issues
 
         if os.getenv("RUNNER_OS") == "Windows" and os.getenv("CI_BUILD_CONFIGURATION") == "Debug":
             # fails with stack overflow on Debug builds
