2a329257c17517141d12852b44737487f368b7ca
Angus Gratton <angus@redyak.com.au>
2025-07-23 11:18:33 +1000

    py/binary: Remove mp_binary_set_val_array_from_int().
    This is a long-standing C function in binary.h, but it's no longer called
    from inside MicroPython.
    
    This work was funded through GitHub Sponsors.
    
    Signed-off-by: Angus Gratton <angus@redyak.com.au>

--- py/binary.h
+++ py/binary.h
@@ -37,7 +37,6 @@
 size_t mp_binary_get_size(char struct_type, char val_type, size_t *palign);
 mp_obj_t mp_binary_get_val_array(char typecode, void *p, size_t index);
 void mp_binary_set_val_array(char typecode, void *p, size_t index, mp_obj_t val_in);
-void mp_binary_set_val_array_from_int(char typecode, void *p, size_t index, mp_int_t val);
 mp_obj_t mp_binary_get_val(char struct_type, char val_type, byte *p_base, byte **ptr);
 void mp_binary_set_val(char struct_type, char val_type, mp_obj_t val_in, byte *p_base, byte **ptr);
 long long mp_binary_get_int(size_t size, bool is_signed, bool big_endian, const byte *src);
