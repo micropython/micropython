77c9eb7795da13dfe499f45f19e867362799b38a
Angus Gratton <angus@redyak.com.au>
2025-07-25 13:22:00 +1000

    esp32: Add "Free RAM" optimisation config flags.
    This is necessary for ESP32-C2 Wi-Fi & BT to work reliably (and for TLS to
    work at all). On IDF 5.4.2 the free static RAM goes from 60KB to 100KB, and
    there will also be a reduction in lwIP & Wi-Fi memory use at runtime.
    
    The performance trade-off seems low for most use cases, although it will
    probably be significant for certain combinations of load (i.e. heavy
    TCP/IP, heavy BT throughput, and some peripheral driver functions).
    
    Added as a set of config flags because this is potentially useful on other
    SoCs where the goal is to maximise RAM available for MicroPython.
    
    This work was funded through GitHub Sponsors.
    
    Signed-off-by: Angus Gratton <angus@redyak.com.au>

--- ports/esp32/boards/ESP32_GENERIC_C2/mpconfigboard.cmake
+++ ports/esp32/boards/ESP32_GENERIC_C2/mpconfigboard.cmake
@@ -4,4 +4,6 @@ set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.base
     boards/sdkconfig.ble
     boards/sdkconfig.c2
+    # C2 has unusably low free RAM without these optimisations
+    boards/sdkconfig.free_ram
 )
