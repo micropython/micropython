7bc83afee2115d1a470bc2e901d998b63cef7b25
Damien George <damien@micropython.org>
2025-07-28 15:20:19 +1000

    esp32/machine_i2c_target: Implement I2CTarget class.
    Only soft IRQs are supported.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/esp32/main.c
+++ ports/esp32/main.c
@@ -51,6 +51,7 @@
 #include "py/repl.h"
 #include "py/gc.h"
 #include "py/mphal.h"
+#include "extmod/modmachine.h"
 #include "shared/readline/readline.h"
 #include "shared/runtime/pyexec.h"
 #include "shared/timeutils/timeutils.h"
@@ -199,7 +200,11 @@ soft_reset_exit:
     machine_pwm_deinit_all();
     // TODO: machine_rmt_deinit_all();
     machine_pins_deinit();
+    #if MICROPY_PY_MACHINE_I2C_TARGET
+    mp_machine_i2c_target_deinit_all();
+    #endif
     machine_deinit();
+
     #if MICROPY_PY_SOCKET_EVENTS
     socket_events_deinit();
     #endif
