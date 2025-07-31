519d90d928ac174e935fc9cc746919f5d196869f
Jeff Epler <jepler@gmail.com>
2025-07-31 09:56:02 -0500

    Switch to our own docker image
    .. it has the packages needed to `make mkapi` preinstalled.

--- ports/m68kmac/docker-run.sh
+++ ports/m68kmac/docker-run.sh
@@ -7,4 +7,5 @@ if [ $# -eq 0 ]; then it=-it; else it=; fi
 docker run \
     -w /work/ports/m68kmac --rm \
     -v "${TOP}":/work \
-    ${it} ghcr.io/autc04/retro68 "$@"
+    ${it} ghcr.io/m68k-micropython/builder:latest "$@"
+
