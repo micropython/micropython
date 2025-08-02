ae646d40df025d63ecc4aac2d72be7d5c459530d
Yuuki NAGAO <wf.yn386@gmail.com>
2025-08-02 14:35:21 +0900

    stm32/boards: Update board definition for WeAct_H743VI.
    Add ROMFS feature.
    Combined efforts from #12540:
     * Change peripheral ports, pin definitions.
     * Add QSPI definition.
     * Change board definition name: WeAct_H743VI-> WEACT_STM32H743.
     * Add deploy.md.
    
    Signed-off-by: Yuuki NAGAO <wf.yn386@gmail.com>

--- ports/stm32/boards/WEACT_STM32H743/board_init.c
+++ ports/stm32/boards/WEACT_STM32H743/board_init.c
@@ -1,7 +1,11 @@
 #include "py/mphal.h"
+#include "storage.h"
 
 void WeAct_Core_early_init(void) {
     // Turn off the USB switch.
     mp_hal_pin_output(pyb_pin_OTG_FS_POWER);
     mp_hal_pin_low(pyb_pin_OTG_FS_POWER);
+
+    // Explicitly init SPI2 because it's not enabled as a block device
+    spi_bdev_ioctl(&spi_bdev2, BDEV_IOCTL_INIT, (uint32_t)&spiflash2_config);
 }
