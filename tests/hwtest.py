0fc2361fb9fb3caa8a237457ca98b36a9b4a4123
Damien George <damien@micropython.org>
2025-08-06 16:40:05 +1000

    tests/hwtest.py: Add argparse and support a reference board.
    Signed-off-by: Damien George <damien@micropython.org>

--- tests/hwtest.py
+++ tests/hwtest.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # MIT license; Copyright (c) 2024 Damien P. George
 
+import argparse
 import sys
 import glob
 import os
@@ -21,6 +22,9 @@ WLAN_PASS = os.getenv("WLAN_PASS")
 if not WLAN_SSID or not WLAN_PASS:
     print("WLAN_SSID and/or WLAN_PASS not provided")
 
+with open("feature_check/target_info.py", "rb") as f:
+    TARGET_INFO_CHECK = f.read()
+
 
 class Target:
     def __init__(self, device, serial_number):
@@ -30,7 +34,13 @@ class Target:
         self.port = None
         self.build = None
         self.machine = None
+        self.can_import_mpy = None
+        self.has_vfs = None
+        self.has_filesystem = None
+        self.has_unittest = None
+        self.has_wlan = None
         self.has_wlan_connected = False
+        self.has_ble = None
 
     def info(self):
         features = ""
@@ -42,6 +52,35 @@ class Target:
             features += " BLE"
         return f"{self.device} {self.port} {self.build} {self.arch}{features} {self.version}"
 
+    def detect_features(self):
+        t = SerialTransport(self.device)
+        t.enter_raw_repl()
+        t.exec("import sys")
+        sys_info = t.eval(
+            "(sys.platform, getattr(sys.implementation, '_build', 'UNKNOWN'), sys.implementation._machine, sys.version)"
+        )
+        self.port, self.build, self.machine, self.version = sys_info
+        self.can_import_mpy = t.eval("hasattr(sys.implementation, '_mpy')")
+        _, self.arch, _ = str(t.exec(TARGET_INFO_CHECK), "ascii").strip().split()
+        if self.arch == "None":
+            self.arch = None
+        self.has_vfs = try_import(t, "vfs")
+        self.has_filesystem = self.has_vfs and t.eval("len(vfs.mount())")
+        self.has_unittest = try_import(t, "unittest")
+        self.has_wlan = try_import(t, "network") and t.eval("hasattr(network, 'WLAN')")
+        self.has_ble = try_import(t, "bluetooth") and t.eval("hasattr(bluetooth, 'BLE')")
+        t.close()
+
+    def setup_wlan(self):
+        t = SerialTransport(self.device)
+        t.enter_raw_repl()
+        rtc_set(self, t)
+        if self.has_filesystem:
+            for file in glob.glob("net_inet/*.der") + glob.glob("multi_net/*.der"):
+                put_file(t, *file.rsplit("/", 1))
+        self.has_wlan_connected = wlan_connect(self, t)
+        t.close()
+
 
 def map_device_name(d):
     if d[0] == "a" and d[1:].isdigit():
@@ -51,29 +90,55 @@ def map_device_name(d):
     return d
 
 
-def list_ports(args):
-    ports = []
-    for p in sorted(serial.tools.list_ports.comports()):
-        if p.device.startswith("/dev/ttyS"):
-            # Skip invalid ttySx ports.
-            continue
-        if not args or p.device in args:
-            t = SerialTransport(p.device)
-            t.enter_raw_repl()
-            t.exec("import os")
-            is_octoprobe_infra = "OCTOPROBE" in t.eval("os.listdir()")
-            t.close()
-            if is_octoprobe_infra:
-                if not args:
-                    # If no devices specified, skip Octoprobe infrastructure.
-                    print(f"{p.device} Octoprobe infrastructure device, skipping")
-                    continue
-                print(f"{p.device} Octoprobe infrastructure device")
-            ports.append(Target(p.device, p.serial_number))
-    return ports
+def detect_octoprobe(device):
+    t = SerialTransport(device)
+    t.enter_raw_repl()
+    t.exec("import os")
+    is_octoprobe_infra = "OCTOPROBE" in t.eval("os.listdir()")
+    t.close()
+    return is_octoprobe_infra
 
 
-NATMOD_ARCHS = ("x86", "x64", "armv6m", "armv7m", "armv7emsp", "armv7emdp", "xtensa", "xtensawin", "rv32imc")
+def list_targets(args, exclude_devices):
+    targets = []
+    valid_serial_ports = sorted(serial.tools.list_ports.comports())
+    if args:
+        for device in args:
+            for p in valid_serial_ports:
+                if device == p.device:
+                    if detect_octoprobe(device):
+                        print(f"{device} Octoprobe infrastructure device")
+                    targets.append(Target(device, p.serial_number))
+                    break
+            else:
+                print(f"{device} not found")
+                sys.exit(1)
+    else:
+        for p in valid_serial_ports:
+            if p.device.startswith("/dev/ttyS"):
+                # Skip invalid ttySx ports.
+                continue
+            if p.device in exclude_devices:
+                continue
+            if detect_octoprobe(p.device):
+                # If no devices specified, skip Octoprobe infrastructure.
+                print(f"{p.device} Octoprobe infrastructure device, skipping")
+                continue
+            targets.append(Target(p.device, p.serial_number))
+    return targets
+
+
+NATMOD_ARCHS = (
+    "x86",
+    "x64",
+    "armv6m",
+    "armv7m",
+    "armv7emsp",
+    "armv7emdp",
+    "xtensa",
+    "xtensawin",
+    "rv32imc",
+)
 NATMOD_LIBS = ("btree", "deflate", "framebuf", "heapq", "random", "re")
 
 
@@ -82,7 +147,9 @@ def build_natmods(archs):
         archs = NATMOD_ARCHS
     for arch in archs:
         for lib in NATMOD_LIBS:
-            subprocess.run(["make", "-C", f"../examples/natmod/{lib}", "-j8", "-B", f"ARCH={arch}"])
+            subprocess.run(
+                ["make", "-C", f"../examples/natmod/{lib}", "-j8", "-B", f"ARCH={arch}"]
+            )
 
 
 def try_import(target, module):
@@ -170,9 +237,11 @@ def do_test(cmd):
         time.sleep(2)
 
 
-def run_multitests_on_one_target(targets, tests):
-    for target in targets:
-        do_test(["./run-multitests.py", "-i", f"pyb:{target.device}", "-p2"] + tests)
+def run_multitests_p2(instances, tests):
+    cmd = ["./run-multitests.py", "-p2"]
+    for instance in instances:
+        cmd.extend(["-i", f"pyb:{instance}"])
+    do_test(cmd + tests)
 
 
 def run_multitests_on_two_targets(targets, tests):
@@ -187,26 +256,41 @@ def run_multitests_on_two_targets(targets, tests):
         )
 
 
-def main():
-    args = sys.argv[1:]
+ALL_TEST_CODES = "vphmnwb"
 
-    if len(args) > 0 and args[0] == "build-natmod":
-        build_natmods(args[1:])
+
+def main():
+    cmd_parser = argparse.ArgumentParser(description="Run all tests on hardware targets")
+    cmd_parser.add_argument(
+        "--build-natmods", action="store_true", help="build all/selected natmods"
+    )
+    cmd_parser.add_argument("-s", "--tests", default=ALL_TEST_CODES, help="tests to run")
+    cmd_parser.add_argument(
+        "-r", "--reference", default=None, help="reference device for WLAN and BLE tests"
+    )
+    cmd_parser.add_argument("test_instances", nargs="*", help="target devices to test")
+    args = cmd_parser.parse_args()
+
+    if args.build_natmods:
+        build_natmods(args.test_instances)
         return
 
-    selected_tests = "vphmnwb"
-    if len(args) > 0:
-        # Select certain tests.
-        if (a := args.pop(0)) != "all":
-            selected_tests = a
+    selected_tests = args.tests
+    for test_code in selected_tests:
+        if test_code not in ALL_TEST_CODES:
+            print(f"{test_code} is not a valid test code")
+            sys.exit(1)
 
-    with open("feature_check/target_info.py", "rb") as f:
-        target_info_check = f.read()
+    ref_target = None
+    if args.reference:
+        ref_device = map_device_name(args.reference)
+        ref_target = Target(ref_device, "")
+        ref_target.detect_features()
 
     print("Selected tests:", selected_tests)
 
-    selected_devices = [map_device_name(d) for d in args]
-    targets = list_ports(selected_devices)
+    selected_devices = [map_device_name(d) for d in args.test_instances]
+    targets = list_targets(selected_devices, [ref_target.device] if ref_target else [])
 
     select_vcprate = "v" in selected_tests
     select_python = "p" in selected_tests
@@ -216,28 +300,18 @@ def main():
     select_wlan = "w" in selected_tests
     select_ble = "b" in selected_tests
 
-    targets_ble = []
     for target in targets:
-        t = SerialTransport(target.device)
-        t.enter_raw_repl()
-        t.exec("import sys")
-        sys_info = t.eval(
-            "(sys.platform, getattr(sys.implementation, '_build', 'UNKNOWN'), sys.implementation._machine, sys.version)"
-        )
-        target.port, target.build, target.machine, target.version = sys_info
-        target.can_import_mpy = t.eval("hasattr(sys.implementation, '_mpy')")
-        _, target.arch, _ = str(t.exec(target_info_check), "ascii").strip().split()
-        if target.arch == "None":
-            target.arch = None
-        target.has_vfs = try_import(t, "vfs")
-        target.has_filesystem = target.has_vfs and t.eval("len(vfs.mount())")
-        target.has_unittest = try_import(t, "unittest")
-        target.has_wlan = try_import(t, "network") and t.eval("hasattr(network, 'WLAN')")
-        target.has_ble = try_import(t, "bluetooth") and t.eval("hasattr(bluetooth, 'BLE')")
-        t.close()
+        target.detect_features()
 
     for target in targets:
         print(target.info())
+
+    if ref_target:
+        print("=" * 64)
+        print("REFERENCE")
+        print(ref_target.info())
+        if select_wlan and ref_target.has_wlan:
+            ref_target.setup_wlan()
 
     print("=" * 64)
     print("=" * 64)
@@ -287,7 +361,8 @@ def main():
             if select_via_mpy and target.can_import_mpy:
                 port_specific = []
                 if target.port == "esp8266":
-                    port_specific.extend(("-d", "basics", "extmod", "float"))
+                    # extreme_exc has a stack overflow and crashes the device
+                    port_specific.extend(("--exclude", "extreme_exc"))
                 do_test(run_tests_cmd + ["--via-mpy"] + port_specific)
 
             if select_native and target.arch is not None:
@@ -304,21 +379,21 @@ def main():
                         do_test(run_cmd + ["-d", "ports/stm32_hardware"])
 
             if select_wlan and target.has_wlan:
-                t = SerialTransport(target.device)
-                t.enter_raw_repl()
-                rtc_set(target, t)
-                if target.has_filesystem:
-                    for file in glob.glob("net_inet/*.der") + glob.glob("multi_net/*.der"):
-                        put_file(t, *file.rsplit("/", 1))
-                target.has_wlan_connected = wlan_connect(target, t)
-                t.close()
+                target.setup_wlan()
                 if target.has_wlan_connected:
                     do_test(run_tests_cmd + ["-d", "net_hosted", "net_inet"])
+                    run_multitests_p2([target.device], tests_multi_net)
+                    if ref_target and ref_target.has_wlan_connected:
+                        run_multitests_p2([target.device, ref_target.device], tests_multi_net)
+                        run_multitests_p2([target.device, ref_target.device], tests_multi_wlan)
+
+            if select_ble and target.has_ble:
+                if ref_target and ref_target.has_ble:
+                    run_multitests_p2([target.device, ref_target.device], tests_multi_bluetooth)
 
         targets_wlan = [t for t in targets if t.has_wlan_connected]
         if select_wlan and targets_wlan:
             print("=" * 64)
-            run_multitests_on_one_target(targets_wlan, tests_multi_net)
             run_multitests_on_two_targets(targets_wlan, tests_multi_net)
             run_multitests_on_two_targets(targets_wlan, tests_multi_wlan)
 
