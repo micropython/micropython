90012e7d6afb4148df33e4d95d2a6b57818bf766
Jeff Epler <jepler@gmail.com>
2025-08-07 09:10:57 -0500

    tests: Add test of invalid unicode strings.
    .. from non UTF-8 inputs. In this case, MicroPython raises
    UnicodeError while CPython uses SyntaxError. By catching either
    exception, the test does not require an .exp file.
    
    Signed-off-by: Jeff Epler <jepler@gmail.com>

---
+++ tests/unicode/unicode_parser.py
@@ -0,0 +1,23 @@
+# test invalid UTF-8 string via eval
+# Passing byte strings to exec/eval is a micropython extension
+try:
+    eval(b"'ab\xa1'")
+except SyntaxError:
+    print("Exception")
+try:
+    eval(b"'ab\xf8'")
+except SyntaxError:
+    print("Exception")
+try:
+    eval(bytearray(b"'ab\xc0a'"))
+except SyntaxError:
+    print("Exception")
+try:
+    eval(b"'\xf0\xe0\xed\xe8'")
+except SyntaxError:
+    print("Exception")
+
+try:
+    exec(b"b\xff = 1")
+except SyntaxError:
+    print("Exception")
