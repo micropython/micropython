939e7a0af35db0bd9ca47543cc9dac582deb3f64
Daniël van de Giessen <daniel@dvdgiessen.nl>
2025-08-12 13:09:24 +0200

    esp32/network_ppp: Stop polling if PPP was disconnected.
    When disconnecting from PPP the modem sends a confirmation. This message
    is received, like all messages, through the poll() method. lwIP may then
    immediately call our status callback with code PPPERR_USER to indicate
    the connection was closed. Our callback then immediately proceeds to
    free the PCB. Thus, during each new iteration of the loop in poll() we
    must check if we haven't disconnected in the meantime to prevent calling
    the pppos_input_tcpip with a PCB that is now NULL.
    
    Signed-off-by: Daniël van de Giessen <daniel@dvdgiessen.nl>

--- ports/esp32/network_ppp.c
+++ ports/esp32/network_ppp.c
@@ -153,7 +153,7 @@ static mp_obj_t network_ppp_poll(size_t n_args, const mp_obj_t *args) {
     }
 
     mp_int_t total_len = 0;
-    while (self->stream != mp_const_none) {
+    while (self->state >= STATE_ACTIVE && self->stream != mp_const_none) {
         uint8_t buf[256];
         int err;
         mp_uint_t len = mp_stream_rw(self->stream, buf, sizeof(buf), &err, 0);
