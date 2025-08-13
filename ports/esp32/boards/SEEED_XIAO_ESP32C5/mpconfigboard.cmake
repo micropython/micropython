ede46f0f6d060b0c9738e6cf189da6667f607511
yuan_mo <2286087148@qq.com>
2025-08-13 13:58:03 +0800

    fix app partition is too small for binary micropython.bin size 0x1f9260

--- ports/esp32/boards/SEEED_XIAO_ESP32C5/mpconfigboard.cmake
+++ ports/esp32/boards/SEEED_XIAO_ESP32C5/mpconfigboard.cmake
@@ -4,5 +4,6 @@ set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.base
     boards/sdkconfig.ble
     boards/sdkconfig.240mhz
+    boards/sdkconfig.free_ram
     boards/SEEED_XIAO_ESP32C5/sdkconfig.board
 )
