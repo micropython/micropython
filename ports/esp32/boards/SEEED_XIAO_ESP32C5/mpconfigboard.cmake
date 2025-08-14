334073cf4fa9bcaf0059f96980611511fb824b57
yuan_mo <2286087148@qq.com>
2025-08-14 09:18:23 +0800

    esp32: Add new board seeed_xiao_esp32c5.
    Signed-off-by: yuan_mo <2286087148@qq.com>

---
+++ ports/esp32/boards/SEEED_XIAO_ESP32C5/mpconfigboard.cmake
@@ -0,0 +1,9 @@
+set(IDF_TARGET esp32c5)
+
+set(SDKCONFIG_DEFAULTS
+    boards/sdkconfig.base
+    boards/sdkconfig.ble
+    boards/sdkconfig.240mhz
+    boards/sdkconfig.free_ram
+    boards/SEEED_XIAO_ESP32C5/sdkconfig.board
+)
