be6b7b103b9ae00cc77148890b30304bfb2fca72
yuan_mo <2286087148@qq.com>
2025-08-14 09:15:59 +0800

    esp32: Support esp32c5 with esp-idf v5.5.
    Signed-off-by: yuan_mo <2286087148@qq.com>

---
+++ ports/esp32/boards/ESP32_GENERIC_C5/board.md
@@ -0,0 +1,2 @@
+The following files are firmware images that should work on most ESP32-C5-based
+boards with at least 4MiB of flash and 40MHz/48MHz crystal frequency. 
---
+++ ports/esp32/boards/ESP32_GENERIC_C5/mpconfigboard.cmake
@@ -0,0 +1,9 @@
+set(IDF_TARGET esp32c5)
+
+set(SDKCONFIG_DEFAULTS
+    boards/sdkconfig.base
+    boards/sdkconfig.ble
+    boards/sdkconfig.240mhz
+    boards/sdkconfig.free_ram
+    boards/ESP32_GENERIC_C5/sdkconfig.board
+)
