e8e39d78c354a915d72730ead9074a18af3afa0a
Damien George <damien@micropython.org>
2025-08-17 00:42:17 +1000

    tests/net_inet/resolve_on_connect.py: Convert to use unittest.
    Signed-off-by: Damien George <damien@micropython.org>

--- tests/net_inet/resolve_on_connect.py
+++ tests/net_inet/resolve_on_connect.py
@@ -7,52 +7,32 @@ if sys.implementation.name == "micropython" and sys.platform != "esp32":
     print("SKIP")
     raise SystemExit
 
-import socket, sys
+import socket
+import unittest
 
 
-def test_bind_resolves_0_0_0_0():
-    s = socket.socket()
-    try:
+class Test(unittest.TestCase):
+    def test_bind_resolves_0_0_0_0(self):
+        s = socket.socket()
         s.bind(("0.0.0.0", 31245))
-        print("bind actually bound")
         s.close()
-    except Exception as e:
-        print("bind raised", e)
 
-
-def test_bind_resolves_localhost():
-    s = socket.socket()
-    try:
+    def test_bind_resolves_localhost(self):
+        s = socket.socket()
         s.bind(("localhost", 31245))
-        print("bind actually bound")
         s.close()
-    except Exception as e:
-        print("bind raised", e)
 
-
-def test_connect_resolves():
-    s = socket.socket()
-    try:
+    def test_connect_resolves(self):
+        s = socket.socket()
         s.connect(("micropython.org", 80))
-        print("connect actually connected")
         s.close()
-    except Exception as e:
-        print("connect raised", e)
 
-
-def test_connect_non_existent():
-    s = socket.socket()
-    try:
-        s.connect(("nonexistent.example.com", 80))
-        print("connect actually connected")
+    def test_connect_non_existent(self):
+        s = socket.socket()
+        with self.assertRaises(OSError):
+            s.connect(("nonexistent.example.com", 80))
         s.close()
-    except OSError as e:
-        print("connect raised OSError")
-    except Exception as e:
-        print("connect raised", e)
 
 
-test_funs = [n for n in dir() if n.startswith("test_")]
-for f in sorted(test_funs):
-    print("--", f, end=": ")
-    eval(f + "()")
+if __name__ == "__main__":
+    unittest.main()
