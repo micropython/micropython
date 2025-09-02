abaf67ce3444d2eec8a512fea4cc85e3084dbbde
2286087148 <2286087148@qq.com>
2025-09-02 10:23:55 +0800

    esp32: Resolve conflicts according to the suggestions.
    Signed-off-by: 2286087148 <2286087148@qq.com>

--- ports/esp32/boards/SEEED_XIAO_ESP32C5/mpconfigboard.h
+++ ports/esp32/boards/SEEED_XIAO_ESP32C5/mpconfigboard.h
@@ -1,6 +1,6 @@
-// This configuration is for a generic ESP32C2 board with 4MiB (or more) of flash.
+// This configuration is for a generic ESP32C5 board with 4MiB (or more) of flash.
 
-#define MICROPY_HW_BOARD_NAME               "ESP32C5 module"
+#define MICROPY_HW_BOARD_NAME               "SEEED XIAO ESP32C5"
 #define MICROPY_HW_MCU_NAME                 "ESP32C5"
 
 #define MICROPY_PY_MACHINE_I2S              (0)
