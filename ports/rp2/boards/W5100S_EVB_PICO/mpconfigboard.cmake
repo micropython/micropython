a4af5efd9555b3877ce9370140a71aced81a2c73
wiznet-grace <Grace@wiznet.io>
2025-09-03 23:13:55 +0900

    rp2/boards: Add WIZnet EVB boards and wiznet6k submodule.
    Signed-off-by: wiznet-grace <Grace@wiznet.io>

--- ports/rp2/boards/W5100S_EVB_PICO/mpconfigboard.cmake
+++ ports/rp2/boards/W5100S_EVB_PICO/mpconfigboard.cmake
@@ -1,5 +1,7 @@
 # cmake file for Wiznet W5100S-EVB-Pico.
 set(PICO_BOARD wiznet_w5100s_evb_pico)
-set(MICROPY_PY_NETWORK_WIZNET5K W5100S)
+set(MICROPY_PY_NETWORK_WIZNET6K W5100S)
 set(MICROPY_PY_LWIP 1)
 set(MICROPY_FROZEN_MANIFEST ${MICROPY_BOARD_DIR}/manifest.py)
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-unused-variable -Wno-misleading-indentation -Wno-incompatible-pointer-types -Wno-error=unused-function -Wno-comment -Wno-unused-but-set-variable")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mthumb -mcpu=cortex-m0plus")
