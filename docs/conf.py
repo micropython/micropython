b33c0cb235ae916a708ea6f79e0f9980a17fefc6
jaenrig-ifx <enriquezgarcia.external@infineon.com>
2025-09-11 15:13:48 +0200

    docs/conf.py: Added rtd there usage.
    Signed-off-by: jaenrig-ifx <enriquezgarcia.external@infineon.com>

--- docs/conf.py
+++ docs/conf.py
@@ -129,9 +129,14 @@ rst_epilog = """
 
 # -- Options for HTML output ----------------------------------------------
 
-import sphinx_rtd_theme
+# on_rtd is whether we are on readthedocs.org
+try:
+    import sphinx_rtd_theme
 
-html_theme = "sphinx_rtd_theme"
+    html_theme = "sphinx_rtd_theme"
+    html_theme_path = ["."]
+except:
+    html_theme = "default"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
