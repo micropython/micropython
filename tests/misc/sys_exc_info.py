f414dc224292a16cc030c9f589e5debbb4d32743
Damien George <damien@micropython.org>
2025-09-14 08:30:28 +1000

    tests/misc/sys_exc_info.py: Don't rely on slicing.
    Signed-off-by: Damien George <damien@micropython.org>

--- tests/misc/sys_exc_info.py
+++ tests/misc/sys_exc_info.py
@@ -8,13 +8,14 @@ except:
 
 
 def f():
-    print(sys.exc_info()[0:2])
+    e = sys.exc_info()
+    print(e[0], e[1])
 
 
 try:
     raise ValueError("value", 123)
 except:
-    print(sys.exc_info()[0:2])
+    print(sys.exc_info()[0], sys.exc_info()[1])
     f()
 
 # Outside except block, sys.exc_info() should be back to None's
