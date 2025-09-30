11c16ae104b20c42db2a562b07331b02ec53254b
Koudai Aono <koxudaxi@gmail.com>
2025-09-30 15:59:07 +0900

    py: add support for raw t-strings, enhance expression parsing.
    Signed-off-by: Koudai Aono <koxudaxi@gmail.com>

--- py/modtstring.c
+++ py/modtstring.c
@@ -333,6 +333,13 @@ static mp_obj_t template_binary_op(mp_binary_op_t op, mp_obj_t lhs_in, mp_obj_t 
             return MP_OBJ_FROM_PTR(result);
         }
 
+        case MP_BINARY_OP_REVERSE_ADD: {
+            if (mp_obj_is_str(rhs_in)) {
+                mp_raise_TypeError(MP_ERROR_TEXT("cannot mix t-strings with strings or f-strings"));
+            }
+            return MP_OBJ_NULL;
+        }
+
         default:
             return MP_OBJ_NULL; // op not supported
     }
