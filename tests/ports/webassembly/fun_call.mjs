01a11ea45efe8e5fe7d2049902f7d6e6818d3ec1
Damien George <damien@micropython.org>
2025-10-02 19:49:40 +1000

    webassembly/objjsproxy: Support arbitrary number of args with kwargs.
    When calling from Python into JavaScript and passing along keyword
    arguments, the FFI bindings currently only support at most 1 positional
    argument. For example:
    
        import js
        js.func(1, b=2, c=3)
    
    This commit fixes that by supporting arbitrary number of positional
    arguments, in combination with keyword arguments.  So now the following
    works:
    
        import js
        js.func(1, 2, c=3, d=4)
    
    Tests are added for these new, supported cases.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- tests/ports/webassembly/fun_call.mjs
+++ tests/ports/webassembly/fun_call.mjs
@@ -15,3 +15,30 @@ js.f(1, 2, 3, 4)
 js.f(1, 2, 3, 4, 5)
 js.f(1, 2, 3, 4, 5, 6)
 `);
+
+globalThis.g = (...args) => {
+    console.log(args);
+};
+mp.runPython(`
+import js
+js.g()
+js.g(a=1)
+js.g(a=1, b=2)
+js.g(a=1, b=2, c=3)
+js.g(a=1, b=2, c=3, d=4)
+js.g(a=1, b=2, c=3, d=4, e=5)
+js.g(1)
+js.g(1, b=2)
+js.g(1, b=2, c=3)
+js.g(1, b=2, c=3, d=4)
+js.g(1, b=2, c=3, d=4, e=5)
+js.g(1, 2)
+js.g(1, 2, c=3)
+js.g(1, 2, c=3, d=4)
+js.g(1, 2, c=3, d=4, e=5)
+js.g(1, 2, 3)
+js.g(1, 2, 3, d=4)
+js.g(1, 2, 3, d=4, e=5)
+js.g(1, 2, 3, 4)
+js.g(1, 2, 3, 4, e=5)
+`);
