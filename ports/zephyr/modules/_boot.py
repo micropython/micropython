0d4378fcb96d0a9876566475f6479ecb9a02b057
Ned Konz <ned@metamagix.tech>
2025-10-03 17:04:23 -0700

    zephyr: Add mounting of SD card to initialization.
    This will now mount an SD card on /sd (and add /sd/lib
    to sys.path) if possible, then mount or create an LFS2
    filesystem on the flash partition named "storage",
    appending /flash/lib to the sys.path if successful.
    
    If an SD card was mounted, the working directory will
    be set to /sd, else it will be /flash.
    
    Then boot.py and main.py will be executed from the
    current working directory if they exist.
    
    Signed-off-by: Ned Konz <ned@metamagix.tech>

--- ports/zephyr/modules/_boot.py
+++ ports/zephyr/modules/_boot.py
@@ -5,37 +5,54 @@ import os
 from zephyr import FlashArea
 from micropython import const
 
+_SD = const("/sd")
 _FLASH = const("/flash")
 _FLASH_LIB = const("/flash/lib")
 _STORAGE_KEY = const("id_storage")
 
 
-def flash_partition_mounted():
-    """Check if /flash is already mounted"""
+def partition_mounted(name):
+    """Check if a given partition is already mounted.
+    Return True if mounted, False otherwise.
+    """
     mounts = vfs.mount()
     for mount in mounts:
-        if mount[1] == _FLASH:
+        if mount[1] == name:
             return True
     return False
 
 
 def create_flash_partition():
     """Create an LFS2 filesystem on the partition labeled Storage
-    and mount it on /flash"""
+    and mount it on /flash.
+    Return True if successful, False otherwise.
+    """
     for key, value in FlashArea.__dict__.items():
         if key.lower() == _STORAGE_KEY:
             bdev = FlashArea(value, 4096)
+            retval = True
             try:
                 vfs.mount(bdev, _FLASH)
             except OSError:
-                vfs.VfsLfs2.mkfs(bdev)
-                vfs.mount(bdev, _FLASH)
-            sys.path.append(_FLASH_LIB)
-            os.chdir(_FLASH)
+                try:
+                    vfs.VfsLfs2.mkfs(bdev)
+                    vfs.mount(bdev, _FLASH)
+                except OSError:
+                    retval = False
+            return retval
+    return False
 
 
-if not flash_partition_mounted():
-    create_flash_partition()
+if partition_mounted(_FLASH) or create_flash_partition():
+    _FLASH_EXISTS = True
+    sys.path.append(_FLASH_LIB)
+
+# Prefer /sd to /flash for initial working directory.
+if partition_mounted(_SD):
+    os.chdir(_SD)
+elif _FLASH_EXISTS:
+    os.chdir(_FLASH)
+
 
 del sys, vfs, os, const, FlashArea
-del flash_partition_mounted, create_flash_partition
+del partition_mounted, create_flash_partition, _FLASH_EXISTS
