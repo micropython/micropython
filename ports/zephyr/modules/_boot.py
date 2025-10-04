b248d4cfbcd89ae7c4c71e65926b010e449945a2
Ned Konz <ned@metamagix.tech>
2025-10-04 16:26:52 -0700

    zephyr: Auto-mount every disk area if possible at startup.
    This commit (thanks to a contribution from @VynDragon) mounts every available disk
    on separate mount points, as well as the 'storage' partition on /flash.
    
    The earlier C logic in `main.c` has been moved into a pure-Python solution
    in `_boot.py`, which is frozen and executed at startup.
    
    Signed-off-by: Ned Konz <ned@metamagix.tech>

--- ports/zephyr/modules/_boot.py
+++ ports/zephyr/modules/_boot.py
@@ -2,13 +2,13 @@
 import vfs
 import sys
 import os
-from zephyr import FlashArea
+from zephyr import FlashArea, DiskAccess
 from micropython import const
 
-_SD = const("/sd")
 _FLASH = const("/flash")
 _FLASH_LIB = const("/flash/lib")
-_STORAGE_KEY = const("id_storage")
+_STORAGE_KEY = const("storage")
+_FLASH_EXISTS = False
 
 
 def partition_mounted(name):
@@ -23,36 +23,51 @@ def partition_mounted(name):
 
 
 def create_flash_partition():
-    """Create an LFS2 filesystem on the partition labeled Storage
+    """Create an LFS2 filesystem on the partition labeled storage
     and mount it on /flash.
     Return True if successful, False otherwise.
     """
-    for key, value in FlashArea.__dict__.items():
-        if key.lower() == _STORAGE_KEY:
-            bdev = FlashArea(value, 4096)
-            retval = True
+    if _STORAGE_KEY in FlashArea.Areas:
+        bdev = FlashArea(FlashArea.Areas[_STORAGE_KEY], 4096)
+        retval = True
+        try:
+            vfs.mount(bdev, _FLASH)
+        except OSError:
             try:
+                vfs.VfsLfs2.mkfs(bdev)
                 vfs.mount(bdev, _FLASH)
             except OSError:
-                try:
-                    vfs.VfsLfs2.mkfs(bdev)
-                    vfs.mount(bdev, _FLASH)
-                except OSError:
-                    retval = False
-            return retval
+                retval = False
+        return retval
     return False
+
+
+def mount_all_disks():
+    # Now mount all the DiskAreas (if any).
+    retval = False
+    for da_name in DiskAccess.Disks:
+        mount_name = f"/{da_name.lower()}"
+        da = DiskAccess(da_name)
+        try:
+            vfs.mount(da, mount_name)
+            sys.path.append(f"{mount_name}/lib")
+            os.chdir(mount_name)
+            retval = True
+        except OSError:
+            pass
+    return retval
 
 
 if partition_mounted(_FLASH) or create_flash_partition():
     _FLASH_EXISTS = True
+
+# Prefer disks to /flash
+if not mount_all_disks():
+    if _FLASH_EXISTS:
+        os.chdir(_FLASH)
+
+if _FLASH_EXISTS:
     sys.path.append(_FLASH_LIB)
 
-# Prefer /sd to /flash for initial working directory.
-if partition_mounted(_SD):
-    os.chdir(_SD)
-elif _FLASH_EXISTS:
-    os.chdir(_FLASH)
-
-
-del sys, vfs, os, const, FlashArea
-del partition_mounted, create_flash_partition, _FLASH_EXISTS
+del sys, vfs, os, const, FlashArea, DiskAccess
+del partition_mounted, create_flash_partition, mount_all_disks, _FLASH_EXISTS
