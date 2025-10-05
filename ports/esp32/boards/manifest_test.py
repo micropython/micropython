3635d96e2405116df2e70e5a1e7ef1d897180dfa
Andrew Leech <andrew.leech@planetinnovation.com.au>
2025-10-05 21:15:24 +1100

    esp32: Update manifest_test to use c_module().
    Replace USER_C_MODULES usage with c_module() calls in the ESP32
    manifest_test.py. This tests the c_module() functionality on ESP32
    during CI builds.
    
    Signed-off-by: Andrew Leech <andrew.leech@planetinnovation.com.au>

--- ports/esp32/boards/manifest_test.py
+++ ports/esp32/boards/manifest_test.py
@@ -8,3 +8,7 @@ freeze("$(MPY_DIR)/tests/micropython", "native_misc.py")
 
 # Test freezing @micropython.viper code.
 freeze("$(MPY_DIR)/tests/micropython", "viper_misc.py")
+
+# Test user C modules.
+c_module("$(MPY_DIR)/examples/usercmodule/cexample")
+c_module("$(MPY_DIR)/examples/usercmodule/cppexample")
