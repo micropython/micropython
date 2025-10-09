3975e2ce407c94e75c8df42c89195a5561a72ebe
jaenrig-ifx <enriquezgarcia.external@infineon.com>
2025-10-09 20:59:47 +0200

    tool/ci: Added ci support for psoc-edge.
    Signed-off-by: jaenrig-ifx <enriquezgarcia.external@infineon.com>

--- tools/ci.sh
+++ tools/ci.sh
@@ -354,6 +354,65 @@ function ci_powerpc_build {
 }
 
 ########################################################################################
+# ports/psoc-edge
+
+MPY_MTB36_CI_DOCKER_VERSION=0.1.0
+
+function ci_psoc_edge_setup {
+    # Access to serial device 
+    if [ "$1" = "--dev-access" ]; then
+        device0_flag=--device=/dev/ttyACM0 
+        device1_flag=--device=/dev/ttyACM1
+    else
+        device0_flag=
+        device1_flag=
+    fi
+
+    docker pull ifxmakers/mpy-mtb36-ci:${MPY_MTB36_CI_DOCKER_VERSION}
+    docker run --name mtb36-ci --rm --privileged -d -it \
+      ${device0_flag} \
+      ${device1_flag} \
+      -v "$(pwd)":/micropython-psoc-edge \
+      -w /micropython-psoc-edge/ports/psoc-edge \
+      ifxmakers/mpy-mtb36-ci:${MPY_MTB36_CI_DOCKER_VERSION}
+    docker ps -a
+
+    # This command prevents the issue "fatal: detected dubious ownership in repository at '/micropython'""
+    docker exec mtb36-ci /bin/bash -c "git config --global --add safe.directory /micropython-psoc-edge"
+    docker exec mtb36-ci /bin/bash -c "git config --global --add safe.directory /micropython-psoc-edge/lib/mtb-psoc-edge-libs"
+
+    # Initialize the submodules
+    docker exec mtb36-ci make submodules
+}
+
+function ci_psoc_edge_build {
+    board=$1
+    docker exec mtb36-ci make BOARD=${board}
+}
+
+function ci_psoc_edge_deploy {
+    docker exec mtb36-ci make deploy
+}
+
+# TODO: Enable for HIL testing
+# function ci_psoc_edge_flash_multiple_devices {
+#     board=$1
+#     # hex file including path with respect to micropython root
+#     hex_file=$2
+#     devs_file=$3
+
+#     # TBD
+# }
+
+# function ci_psoc_edge_run_tests {
+#     docker exec mtb36-ci /bin/bash -c "cd ../../tests && ./run-tests.py --target psoc-edge --device /dev/ttyACM0 -d psoc-edge"
+# }
+
+function ci_psoc_edge_teardown {
+    docker stop mtb36-ci
+}
+
+########################################################################################
 # ports/qemu
 
 function ci_qemu_setup_arm {
