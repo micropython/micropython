5065b465288915afe5955838dddd47957768cc86
Vdragon <mail@massdriver.space>
2025-10-13 00:50:53 +0200

    zephyr: Enable Dynamic Pinctrl on RP2 boards.
    Enables Dynamic Pinctrl on RP2 boards.
    
    Signed-off-by: Vdragon <mail@massdriver.space>

---
+++ ports/zephyr/boards/rpi_pico/rp2350.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <zephyr/version.h>
+
+/* Patch rpi_pico zephyr release pinctrl bug */
+#if KERNEL_VERSION_NUMBER <= ZEPHYR_VERSION(4, 4, 2)
+#define RP2_PINCTRL_GPIO_FUNC_UART_ALT 11
+#endif
+
+#include "mpconfigport.h"
