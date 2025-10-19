6160b1f7c4dbdde512e4ffec3cf513a1f537945b
Andrew Leech <andrew.leech@planetinnovation.com.au>
2025-10-19 14:34:37 +1100

    stm32/tinyusb: Add High Speed USB controller support.
    Implements mapping from MICROPY_HW_USB_MAIN_DEV to TinyUSB RHPORT
    configuration, enabling board-specific USB PHY selection for TinyUSB
    stack. Adds support for HS-in-FS mode (High Speed controller running
    at Full Speed) which is the default for STM32 boards without external
    ULPI PHY. Includes additional TinyUSB source files for DWC2 host
    controller support.
    
    Signed-off-by: Andrew Leech <andrew.leech@planetinnovation.com.au>

--- ports/stm32/stm32_it.c
+++ ports/stm32/stm32_it.c
@@ -374,7 +374,7 @@ void OTG_FS_IRQHandler(void) {
 void USB1_OTG_HS_IRQHandler(void) {
     IRQ_ENTER(USB1_OTG_HS_IRQn);
     #if MICROPY_HW_TINYUSB_STACK
-    tud_int_handler(0);
+    tud_int_handler(TUD_OPT_RHPORT);
     #else
     HAL_PCD_IRQHandler(&pcd_hs_handle);
     #endif
@@ -384,7 +384,7 @@ void USB1_OTG_HS_IRQHandler(void) {
 void OTG_HS_IRQHandler(void) {
     IRQ_ENTER(OTG_HS_IRQn);
     #if MICROPY_HW_TINYUSB_STACK
-    tud_int_handler(0);
+    tud_int_handler(TUD_OPT_RHPORT);
     #else
     HAL_PCD_IRQHandler(&pcd_hs_handle);
     #endif
