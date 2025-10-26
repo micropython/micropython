743f46181097d3b71f07723b6673b1f5389a9565
Elvis Pfutzenreuter <epxx@epxx.co>
2025-10-26 19:21:36 -0300

    esp32: Add RMT RX module.
    Signed-off-by: Elvis Pfutzenreuter <elvis.pfutzenreuter@gmail.com>

--- ports/esp32/modesp32.h
+++ ports/esp32/modesp32.h
@@ -81,6 +81,7 @@ extern bool esp32_rmt_bitstream_enabled;
 extern const mp_obj_type_t esp32_nvs_type;
 extern const mp_obj_type_t esp32_partition_type;
 extern const mp_obj_type_t esp32_rmt_type;
+extern const mp_obj_type_t esp32_rmtrx_type;
 extern const mp_obj_type_t esp32_ulp_type;
 
 #if MICROPY_PY_ESP32_PCNT
