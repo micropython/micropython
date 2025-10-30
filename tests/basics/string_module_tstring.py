8ef2caad2f93ab906760850883b367378278d2ad
Koudai Aono <koxudaxi@gmail.com>
2025-10-30 16:23:51 +0000

    py: Avoid module error.
    Signed-off-by: Koudai Aono <koxudaxi@gmail.com>

--- tests/basics/string_module_tstring.py
+++ tests/basics/string_module_tstring.py
@@ -1,6 +1,12 @@
+# Test requires t-strings support
+# SPDX-License-Identifier: MIT
+
 try:
     from string.templatelib import Template
 except ImportError:
+    print("SKIP")
+    raise SystemExit
+except SyntaxError:
     print("SKIP")
     raise SystemExit
 
