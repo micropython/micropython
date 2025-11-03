7176acbd294ffe99117047371780d8196b2ab607
Dryw Wade <dryw.wade@sparkfun.com>
2025-11-03 10:01:34 -0700

    py: Add LIBS_USERMOD to LIBS.
    This enables C++ modules to correctly postion -l
    linker flags at the end of the flags instead of at
    the start. Updated the example C++ micropython.mk
    accordingly.
    
    Signed-off-by: Dryw Wade <dryw.wade@sparkfun.com>

--- examples/usercmodule/cppexample/micropython.mk
+++ examples/usercmodule/cppexample/micropython.mk
@@ -8,5 +8,8 @@ SRC_USERMOD_CXX += $(CPPEXAMPLE_MOD_DIR)/example.cpp
 CFLAGS_USERMOD += -I$(CPPEXAMPLE_MOD_DIR)
 CXXFLAGS_USERMOD += -I$(CPPEXAMPLE_MOD_DIR) -std=c++11
 
+# Add any necessary paths to library files.
+# LDFLAGS_USERMOD += -Lpath/to/libs
+
 # We use C++ features so have to link against the standard library.
-LDFLAGS_USERMOD += -lstdc++
+LIBS_USERMOD += -lstdc++
