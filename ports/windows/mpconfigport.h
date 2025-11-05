3d67b2a5ad7450fec350d6735155db56b5dd8fe5
Andrew Leech <andrew.leech@planetinnovation.com.au>
2025-11-05 16:35:41 +1100

    shared/runtime/pyexec: Set PYEXEC_FORCED_EXIT flag for SystemExit.
    When MICROPY_PYEXEC_ENABLE_EXIT_CODE_HANDLING is enabled, SystemExit
    now sets the PYEXEC_FORCED_EXIT flag in addition to the exit code. This
    allows the REPL to properly detect and exit when SystemExit is raised,
    while still preserving the exit code in the lower bits.
    
    Enable MICROPY_PYEXEC_ENABLE_EXIT_CODE_HANDLING in Windows port.
    
    Fixes repl_lock.py test which expects REPL to exit on SystemExit.
    
    Signed-off-by: Andrew Leech <andrew.leech@planetinnovation.com.au>

--- ports/windows/mpconfigport.h
+++ ports/windows/mpconfigport.h
@@ -164,6 +164,9 @@
 // Enable support for compile-only mode.
 #define MICROPY_PYEXEC_COMPILE_ONLY (1)
 
+// Enable handling of sys.exit() exit codes.
+#define MICROPY_PYEXEC_ENABLE_EXIT_CODE_HANDLING (1)
+
 #define MICROPY_ERROR_REPORTING     (MICROPY_ERROR_REPORTING_DETAILED)
 #define MICROPY_ERROR_PRINTER       (&mp_stderr_print)
 #define MICROPY_WARNINGS            (1)
