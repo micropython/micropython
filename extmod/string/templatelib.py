7602143da42276ae25754d0bf24f72e79faf87e3
Koudai Aono <koxudaxi@gmail.com>
2025-11-10 17:10:32 +0000

    py: Add runtime and parser support for t-strings (PEP 750).
    Signed-off-by: Koudai Aono <koxudaxi@gmail.com>

---
+++ extmod/string/templatelib.py
@@ -0,0 +1,6 @@
+# MicroPython string.templatelib module (PEP 750)
+# MIT license; Copyright (c) 2025 Koudai Aono
+
+from _tstring import Template, Interpolation
+
+__all__ = ["Template", "Interpolation"]
