29cf2a47b241e4644308a8fb5ea78c0fa5786691
Alessandro Gatti <a.gatti@frob.it>
2025-11-14 04:27:17 +0100

    samd/pin_af: Reduce footprint of pin structures.
    This commit performs a few minor changes to the structures used to store
    board pin information, in order to reduce the impact on the .rodata
    section of the firmware of instances of those structures.
    
    The pin objects structure ("machine_pin_obj_t") instead has its QSTR
    variable holder changed from "qstr" to "qstr_short_t", and some elements
    of that structure have been rearranged to remove enough padding bytes
    inside structure elements to let the linker allocate less data for
    instances of that structure.
    
    Signed-off-by: Alessandro Gatti <a.gatti@frob.it>

--- ports/samd/boards/pins_prefix.c
+++ ports/samd/boards/pins_prefix.c
@@ -11,11 +11,11 @@
 #if defined(MCU_SAMD21)
 
 #define PIN(p_name, p_eic, p_adc0, p_sercom1, p_sercom2, p_tcc1, p_tcc2) \
-    {{&machine_pin_type}, PIN_##p_name, MP_QSTR_##p_name, p_eic, p_adc0, p_sercom1, p_sercom2, p_tcc1, p_tcc2 }
+    {{&machine_pin_type}, MP_QSTR_##p_name, PIN_##p_name, p_eic, p_adc0, p_sercom1, p_sercom2, p_tcc1, p_tcc2 }
 
 #elif defined(MCU_SAMD51)
 
 #define PIN(p_name, p_eic, p_adc0, p_adc1, p_sercom1, p_sercom2, p_tc, p_tcc1, p_tcc2) \
-    {{&machine_pin_type}, PIN_##p_name, MP_QSTR_##p_name, p_eic, p_adc0, p_adc1, p_sercom1, p_sercom2, p_tc, p_tcc1, p_tcc2 }
+    {{&machine_pin_type}, MP_QSTR_##p_name, PIN_##p_name, p_eic, p_adc0, p_adc1, p_sercom1, p_sercom2, p_tc, p_tcc1, p_tcc2 }
 
 #endif
