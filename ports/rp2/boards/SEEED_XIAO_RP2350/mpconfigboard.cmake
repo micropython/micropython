45ff311001440068ea3d89ea31cb313075ae1133
Bohrium332 <2759897880@qq.com>
2025-11-21 10:31:42 +0800

    lib/pico-sdk: Update pico-sdk to 2.2.0 and define PIOASM_VERSION_STRING.
    Signed-off-by: zhida zhang<zhida.zhang@seeed.cc>

--- ports/rp2/boards/SEEED_XIAO_RP2350/mpconfigboard.cmake
+++ ports/rp2/boards/SEEED_XIAO_RP2350/mpconfigboard.cmake
@@ -3,3 +3,6 @@ set(PICO_BOARD "seeed_xiao_rp2350")
 
 # To change the gpio count for QFN-80
 # set(PICO_NUM_GPIOS 48)
+
+# Seeed XIAO RP2350 uses RP2350A with limited memory, reduce stack size 
+set(PICO_STACK_SIZE 0x1000)
