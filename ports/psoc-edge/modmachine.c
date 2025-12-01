62f9bd03b9e525cb66d11cf7426999eba648b508
Ramya Subramanyam <ramya.subramanyam@infineon.com>
2025-12-01 12:11:32 +0530

    psoc-edge/modmachine: Add machine clock frequency setter.
    Signed-off-by: Ramya Subramanyam <ramya.subramanyam@infineon.com>

---
+++ ports/psoc-edge/modmachine.c
@@ -0,0 +1,75 @@
+/*
+ * This file is part of the MicroPython project, http://micropython.org/
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2022-2024 Infineon Technologies AG
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+// std includes
+#include <stdio.h>
+#include <stdlib.h>
+
+// mpy includes
+#include "py/obj.h"
+#include "py/gc.h"
+#include "py/runtime.h"
+#include "py/mphal.h"
+#include "shared/runtime/pyexec.h"
+#include "extmod/modnetwork.h"
+
+// port-specific includes
+#include "cybsp.h"
+#include "modmachine.h"
+#include "mplogger.h"
+#include "modpsocedge.h"
+#if MICROPY_PY_MACHINE
+
+enum clock_freq_type PLL0_freq = AUDIO_SYS_CLOCK_73_728_000_HZ;
+enum clock_freq_type freq_peri;
+
+// machine.idle()
+// This executies a wfi machine instruction which reduces power consumption
+// of the MCU until an interrupt occurs, at which point execution continues.
+static void mp_machine_idle(void) {
+    __WFI(); // standard ARM instruction
+}
+
+static void mp_machine_set_freq(size_t n_args, const mp_obj_t *args) {
+    freq_peri = mp_obj_get_int(args[0]); // Assuming the enum values are used as integers
+    switch (freq_peri) {
+        case AUDIO_SYS_CLOCK_73_728_000_HZ:
+            PLL0_freq = AUDIO_SYS_CLOCK_73_728_000_HZ;
+            break;
+        case AUDIO_SYS_CLOCK_169_344_000_HZ:
+            PLL0_freq = AUDIO_SYS_CLOCK_169_344_000_HZ;
+            break;
+        default:
+            mp_raise_msg_varg(&mp_type_ValueError, MP_ERROR_TEXT("Invalid frequency type %lu"), freq_peri);
+            break;
+    }
+}
+
+#define MICROPY_PY_MACHINE_EXTRA_GLOBALS \
+    /* Modules */ \
+    { MP_ROM_QSTR(MP_QSTR_PDM_PCM),             MP_ROM_PTR(&machine_pdm_pcm_type) }, \
+
+#endif // MICROPY_PY_MACHINE
---
+++ ports/psoc-edge/modmachine.h
@@ -0,0 +1,45 @@
+/*
+ * This file is part of the MicroPython project, http://micropython.org/
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2022-2024 Infineon Technologies AG
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef MICROPY_INCLUDED_PSOCEDGE_MODMACHINE_H
+#define MICROPY_INCLUDED_PSOCEDGE_MODMACHINE_H
+
+
+// micropython includes
+#include "py/obj.h"
+
+extern const mp_obj_type_t machine_pdm_pcm_type;
+
+enum clock_freq_type {
+    AUDIO_SYS_CLOCK_73_728_000_HZ = 73728000UL /* (Ideally 73.728 MHz) For sample rates: 8 KHz / 16 KHz / 48 KHz */,
+    AUDIO_SYS_CLOCK_169_344_000_HZ = 169344000UL /* (Ideally 169.344 MHz) For sample rates: 22.05 KHz / 44.1 KHz */,
+    CM4,
+    CM4_FLL
+};
+
+extern enum clock_freq_type PLL0_freq;
+
+#endif // MICROPY_INCLUDED_PSOCEDGE_MODMACHINE_H
