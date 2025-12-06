504adc689bf1e56783b394b91ecc9d7a38ccf72e
Steve Sanbeg <steve.sanbeg@gmail.com>
2025-12-06 17:28:26 -0500

    stm32/boards/NUCLEO_G474RE: Restore disabled modules on g474re.
    Among other things, the framebuf module is missing on NUCEO_G474RE. This
    board seems to disable a lot of modules, while other *E boards with the
    same flash configuration (eg NUCLEO_F411RE) don't seem to disable any
    modules in this way.
    
    So, remove all of the lines disabling modules to make it consistent with
    other boards.
    
    Signed-off-by: Steve Sanbeg <steve.sanbeg@gmail.com>

--- ports/stm32/boards/NUCLEO_G474RE/mpconfigboard.h
+++ ports/stm32/boards/NUCLEO_G474RE/mpconfigboard.h
@@ -9,16 +9,6 @@
 #define MICROPY_HW_HAS_SWITCH       (1)
 #define MICROPY_HW_HAS_FLASH        (0) // QSPI extflash not mounted
 
-#define MICROPY_PY_ASYNCIO          (0)
-#define MICROPY_PY_DEFLATE          (0)
-#define MICROPY_PY_BINASCII         (0)
-#define MICROPY_PY_HASHLIB          (0)
-#define MICROPY_PY_JSON             (0)
-#define MICROPY_PY_RE               (0)
-#define MICROPY_PY_FRAMEBUF         (0)
-#define MICROPY_PY_SOCKET           (0)
-#define MICROPY_PY_NETWORK          (0)
-
 // The board has an 24MHz HSE, the following gives 170MHz CPU speed
 #define MICROPY_HW_CLK_PLLM         (6)
 #define MICROPY_HW_CLK_PLLN         (85)
