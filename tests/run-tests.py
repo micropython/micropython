d6879e4bfd3fb13bdf7ac5278d22b12bcd1d0490
Konstantin Kim <kimstik@gmail.com>
2025-12-10 17:16:06 +0100

    tests/run-tests.py: Update comment for UART hardware requirement.
    no loopback requirement
    
    Signed-off-by: Konstantin Kim <kimstik@gmail.com>

--- tests/run-tests.py
+++ tests/run-tests.py
@@ -211,7 +211,7 @@ platform_tests_to_skip = {
         "thread/thread_shared2.py",
     ),
     "win32": (
-        # These tests require physical UART hardware with loopback wiring.
+        # These tests require physical UART hardware.
         "extmod/machine_uart_irq_txidle.py",
         "extmod/machine_uart_tx.py",
     ),
