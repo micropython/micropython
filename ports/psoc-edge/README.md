019f85d4a5ea80352cea4857b7fac677d59bbce9
jaenrig-ifx <enriquezgarcia.external@infineon.com>
2025-12-12 09:53:29 +0100

    psoc-edge: Added basic REPL enablement for Infineon PSOC Edge MCUs.
    Signed-off-by: jaenrig-ifx <enriquezgarcia.external@infineon.com>

---
+++ ports/psoc-edge/README.md
@@ -0,0 +1,93 @@
+# PSOC™ Edge port
+
+This port is intended to run on PSOC™ Edge microcontrollers.
+
+## Pre-requisites
+
+The instruction below assumes you are running on a Linux host.
+
+### Installation of MTB PSOC Edge Tools
+
+1. Download the [ModusToolbox Tools Package 3.6](https://softwaretools-hosting.infineon.com/packages/com.ifx.tb.tool.modustoolbox/versions/3.6.0.17979/artifacts/modustoolbox_3.6.0.17979_Linux_x64.deb/download). and install the toolbox package:
+
+```
+sudo apt install ./modustoolbox_3.6.0.17979_Linux_x64.deb
+```
+
+And go to the `/opt/Tools/ModusToolbox/tools_3.6/modus-shell` directory and run the postinstall script:
+
+```
+cd /opt/Tools/ModusToolbox/tools_3.6/modus-shell
+./postinstall
+```
+
+2. Install the compiler  [GCC ARM Embedded 14.2.1](https://softwaretools-hosting.infineon.com/packages/com.ifx.tb.tool.mtbgccpackage/versions/14.2.1.265/artifacts/mtbgccpackage_14.2.1.265_Linux_x64.deb/download):
+
+```
+sudo apt install ./mtbgccpackage_14.2.1.265_Linux_x64.deb
+```
+3. Install the [Edge Protect Security Suite 1.6.0](https://softwaretools-hosting.infineon.com/packages/com.ifx.tb.tool.modustoolboxedgeprotectsecuritysuite/versions/1.6.0.512/artifacts/modustoolboxedgeprotectsecuritysuite_1.6.0.512_Linux_x64.deb/download):
+
+```
+sudo apt install ./modustoolboxedgeprotectsecuritysuite_1.6.0.512_Linux_x64.deb
+```
+
+4. Install the [ModusToolbox Programming Tools 1.5.0](https://softwaretools-hosting.infineon.com/packages/com.ifx.tb.tool.modustoolboxprogtools/versions/1.5.0.1534/artifacts/ModusToolboxProgtools_1.5.0.1534.deb/download):
+
+```
+sudo apt install ./ModusToolboxProgtools_1.5.0.1534.deb
+```
+
+### Setting up the environment
+
+Run the following script from MicroPython repository root to add the required tools to the system PATH:
+
+    source tools/psoc-edge/dev-setup.sh && toolchain_setup
+
+> [!NOTE] 
+> This command needs to be run every time you open a new shell session, as it only sets environment variables for the current session. 
+
+## Building and running on Linux
+
+As we are working on the `psoc-edge-main` branch, first checkout that branch after cloning this repo:
+
+    git checkout --track origin/psoc-edge-main
+
+The following build commands should be run from the `ports/psoc-edge` directory:
+
+Retrieve submodules:
+
+    make submodules
+
+Build the firmware:
+
+    make BOARD=KIT_PSE84_AI
+
+> [!NOTE] 
+> The first time we call `make` the board needs to be specified with 
+> `BOARD=<board>`. This is required as the ModusToolbox libraries need to be
+> initialized for the selected board. 
+> This board will then be set as the default board for subsequent builds, you
+> can just call `make` without the `BOARD=` argument.
+
+And flash it to the board:
+
+    make deploy
+
+> [!NOTE]
+> This will also build the firmware if it has not been built yet.
+> Use `deploy` target to avoid rebuilding the firmware.
+
+If you have multiple boards connected, you can specify the serial number of the board to be programmed:
+
+    make deploy DEVICE_SN=123456
+
+Find more information about the all available makefile targets:
+
+    make help
+-----------------------------------------------------
+
+# Run micropython
+
+Use any serial terminal (putty, minicom..) and establish a session with your device with 115200 bauds and 8-N-1 configuration. 
+
---
+++ ports/psoc-edge/mpconfigport.h
@@ -0,0 +1,111 @@
+/*
+ * This file is part of the MicroPython project, http://micropython.org/
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2022-2024 Infineon Technologies AG
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+// Options controlling how MicroPython is built, overriding defaults in py/mpconfig.h
+#include <alloca.h>
+#include <stdint.h>
+
+// #include "shared/runtime/interrupt_char.h"
+#include "mpconfigboard.h"
+
+// options to control how MicroPython is built
+
+#define MICROPY_PY_BUILTINS_HELP                (1)
+#define MICROPY_PY_BUILTINS_HELP_MODULES        (1)
+#define MICROPY_PY_BUILTINS_HELP_TEXT           psoc_edge_help_text
+#define MICROPY_USE_INTERNAL_PRINTF             (0)
+#define MICROPY_REPL_INFO                       (1)
+#define MICROPY_HELPER_REPL                     (1)
+
+// Use the minimal starting configuration (disables all optional features).
+#define MICROPY_CONFIG_ROM_LEVEL (MICROPY_CONFIG_ROM_LEVEL_MINIMUM)
+
+// You can disable the built-in MicroPython compiler by setting the following
+// config option to 0.  If you do this then you won't get a REPL prompt, but you
+// will still be able to execute pre-compiled scripts, compiled with mpy-cross.
+#define MICROPY_ENABLE_COMPILER           (1)
+
+#define MICROPY_ENABLE_GC                 (0)
+#define MICROPY_HELPER_REPL               (1)
+#define MICROPY_ENABLE_EXTERNAL_IMPORT    (1)
+
+#define MICROPY_ALLOC_PATH_MAX            (256)
+
+// Use the minimum headroom in the chunk allocator for parse nodes.
+#define MICROPY_ALLOC_PARSE_CHUNK_INIT    (16)
+
+#define MICROPY_LONGINT_IMPL                    (MICROPY_LONGINT_IMPL_MPZ)
+
+// Fine control over Python builtins, classes, modules, etc
+#define MICROPY_PY_SYS_PLATFORM                 "psoc-edge"
+#define MICROPY_PY_SYS_EXC_INFO                 (1)
+
+// Extended modules
+#define MICROPY_PY_TIME                         (1)
+#define MICROPY_PY_TIME_GMTIME_LOCALTIME_MKTIME (1)
+#define MICROPY_PY_TIME_TIME_TIME_NS            (1)
+#define MICROPY_PY_TIME_INCLUDEFILE             "ports/psoc-edge/modtime.c"
+#define MICROPY_PY_MACHINE_FREQ_NUM_ARGS_MAX    (2)
+
+#define MICROPY_TIME_SUPPORT_Y1969_AND_BEFORE   (1)
+
+#define MICROPY_PY_MACHINE                      (1)
+#define MICROPY_PY_MACHINE_INCLUDEFILE          "ports/psoc-edge/modmachine.c"
+#define MICROPY_PY_MACHINE_PDM_PCM              (1)
+#define MICROPY_PY_MACHINE_PDM_PCM_RING_BUF     (1)
+
+#define MICROPY_LOGGER_DEBUG                    (1)
+
+// type definitions for the specific machine
+
+typedef intptr_t mp_int_t; // must be pointer size
+typedef uintptr_t mp_uint_t; // must be pointer size
+typedef long mp_off_t;
+
+
+// #if defined(__linux__) || defined(__APPLE__)
+// #define MICROPY_MIN_USE_STDOUT (1)
+// #define MICROPY_HEAP_SIZE      (25600) // heap size 25 kilobytes
+// #endif
+
+// #ifdef __thumb__
+// #define MICROPY_MIN_USE_CORTEX_CPU (1)
+// #define MICROPY_MIN_USE_STM32_MCU (1)
+// #define MICROPY_HEAP_SIZE      (2048) // heap size 2 kilobytes
+// #endif
+
+#define MP_STATE_PORT MP_STATE_VM
+
+#define MICROPY_EVENT_POLL_HOOK_FAST \
+    do { \
+        extern void mp_handle_pending(bool); \
+        mp_handle_pending(true); \
+    } while (0);
+
+#define MICROPY_EVENT_POLL_HOOK \
+    do { \
+        MICROPY_EVENT_POLL_HOOK_FAST; \
+    } while (0);
