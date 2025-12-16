7333bd0db2cd39abd5d53ee3db69eeddcce71bdf
jaenrig-ifx <enriquezgarcia.external@infineon.com>
2025-12-16 17:01:16 +0100

    tests/ports/psoc-edge/../pin: Added print pin check to test.
    Signed-off-by: jaenrig-ifx <enriquezgarcia.external@infineon.com>

--- tests/ports/psoc-edge/board_ext_hw/single/pin.py
+++ tests/ports/psoc-edge/board_ext_hw/single/pin.py
@@ -1,5 +1,9 @@
 from machine import Pin
 
-pin1_name = "P16_0"
+pin_out_name = "P16_0"  # Use string label
+pin_out = Pin(pin_out_name, mode=Pin.OUT, value=True)
+print(pin_out)
 
-pin_out = Pin(pin1_name, mode=Pin.OUT, value=True)
+pin_in = Pin.cpu.P16_1
+pin_in.init(mode=Pin.IN, pull=Pin.PULL_UP)
+print(pin_in)
