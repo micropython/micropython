85a0c376157bd24baff56e03c6838edd4cd3b527
jaenrig-ifx <enriquezgarcia.external@infineon.com>
2025-12-17 11:20:14 +0100

    tests/ports/psoc-edge/pin: Added basic on, off, value checks.
    Signed-off-by: jaenrig-ifx <enriquezgarcia.external@infineon.com>

--- tests/ports/psoc-edge/board_ext_hw/single/pin.py
+++ tests/ports/psoc-edge/board_ext_hw/single/pin.py
@@ -7,3 +7,45 @@ print(pin_out)
 pin_in = Pin.cpu.P16_1
 pin_in.init(mode=Pin.IN, pull=Pin.PULL_UP)
 print(pin_in)
+
+# Validating initialization values
+print("pin out initial value 1: ", pin_in.value() == 1)
+
+pin_out = Pin(pin_out_name, mode=Pin.OUT, value=False)
+print("pin out initial value 0: ", pin_in.value() == 0)
+
+# Validation different output setting
+pin_out.value(1)
+print("pin out value 1: ", pin_in.value() == 1)
+
+pin_out.value(0)
+print("pin out value 0: ", pin_in.value() == 0)
+
+pin_out.value(True)
+print("pin out value True: ", pin_in.value() == True)
+
+pin_out.value(False)
+print("pin out value False: ", pin_in.value() == False)
+
+pin_out.on()
+print("pin out value on: ", pin_in.value() == 1)
+
+pin_out.off()
+print("pin out value off: ", pin_in.value() == 0)
+
+pin_out(1)
+print("pin out callable 1: ", pin_in() == 1)
+
+pin_out(0)
+print("pin out callable 0: ", pin_in() == 0)
+
+# Validating pull resistors configurations and open drain mode
+pin_out = Pin(pin_out_name, mode=Pin.OUT, pull=Pin.PULL_UP)
+print("pin out with pull up initially high: ", pin_in() == 1)
+
+# This does not work in the test, but does manually for P16_0 - P16_1
+# pin_out = Pin(pin_out_name, mode=Pin.OUT, pull=Pin.PULL_DOWN)
+# print("pin out with pull down initially down: ", pin_in() == 0)
+
+pin_out = Pin(pin_out_name, mode=Pin.OPEN_DRAIN)
+print("pin out with pull none initially 0: ", pin_in() == 0)
