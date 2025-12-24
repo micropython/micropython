bc0d892c4b43936bab30955e7e1e6dab83f9d447
Ihor Nehrutsa <Ihor.Nehrutsa@gmail.com>
2025-12-24 17:16:42 +0300

    esp32/MCPWM: Add motor control MCPWM driver.
    Signed-off-by: Ihor Nehrutsa <Ihor.Nehrutsa@gmail.com>

--- ports/esp32/modesp32.h
+++ ports/esp32/modesp32.h
@@ -67,6 +67,9 @@ extern const mp_obj_type_t esp32_nvs_type;
 extern const mp_obj_type_t esp32_partition_type;
 extern const mp_obj_type_t esp32_rmt_type;
 extern const mp_obj_type_t esp32_ulp_type;
+#if MICROPY_PY_MACHINE_MCPWM
+extern const mp_obj_type_t esp32_mcpwm_type;
+#endif
 
 #if MICROPY_PY_ESP32_PCNT
 extern const mp_obj_type_t esp32_pcnt_type;
