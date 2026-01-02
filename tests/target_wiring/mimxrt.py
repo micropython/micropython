374af7a7c8721ccf8f010b57bcc4aa8fc3920eb4
robert-hh <robert@hammelrath.com>
2026-01-02 15:33:05 +0100

    mimxrt/machine_encoder: Re-struture the documentation.
    - Drop the separate documentation file for Encoder/Counter and move
      the related documentation into the common Encoder/Counter documents.
    - Move the Encoder/Counter pinout to the mimxrt pinout document.
    - Use Teensy D0/D1 Pins for the tests.
    
    Signed-off-by: robert-hh <robert@hammelrath.com>

--- tests/target_wiring/mimxrt.py
+++ tests/target_wiring/mimxrt.py
@@ -9,5 +9,5 @@ uart_loopback_kwargs = {}
 spi_standalone_args_list = [()]
 
 encoder_loopback_id = 0
-encoder_loopback_out_pins = ("D2", "D5")
-encoder_loopback_in_pins = ("D3", "D4")
+encoder_loopback_out_pins = ("D0", "D2")
+encoder_loopback_in_pins = ("D1", "D3")
