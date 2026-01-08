b02068e57188abb09a95b83e11a4051f7a0fc14f
BSoD123456 <bsod123456@gmail.com>
2026-01-08 22:36:12 +0800

    firmware overweight, re-partition

--- ports/esp32/boards/ESP32_LCD_C6/mpconfigboard.cmake
+++ ports/esp32/boards/ESP32_LCD_C6/mpconfigboard.cmake
@@ -5,6 +5,7 @@ set(SDKCONFIG_DEFAULTS
     ${SDKCONFIG_IDF_VERSION_SPECIFIC}
     boards/sdkconfig.c6
     boards/sdkconfig.ble
+    boards/ESP32_LCD_C6/sdkconfig.board
 )
 
 set(MICROPY_FROZEN_MANIFEST ${MICROPY_BOARD_DIR}/manifest.py)
