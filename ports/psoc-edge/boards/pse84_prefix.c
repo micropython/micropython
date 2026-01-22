10e3082dcee99878f3d68aa15afb4d299da3d51a
jaenrig-ifx <enriquezgarcia.external@infineon.com>
2026-01-22 11:43:37 +0100

    psoc-edge/boards/pse84_prefix: Added macros for alt functions defs.
    Signed-off-by: jaenrig-ifx <enriquezgarcia.external@infineon.com>

--- ports/psoc-edge/boards/pse84_prefix.c
+++ ports/psoc-edge/boards/pse84_prefix.c
@@ -1,11 +1,24 @@
 #include "py/obj.h"
 #include "py/mphal.h"
 #include "machine_pin.h"
+#include "machine_pin_af.h"
+#include "cy_device_headers_ns.h"
 
-#define PIN(p_port, p_pin) \
+#define AF(af_name_idx, af_fn, af_unit, af_signal, af_ptr) \
+    { \
+        .idx = HSIOM_SEL_##af_name_idx, \
+        .fn = MACHINE_PIN_AF_FN_##af_fn, \
+        .unit = (af_unit), \
+        .signal = MACHINE_PIN_AF_SIGNAL_##af_fn##_##af_signal, \
+        .periph = (af_ptr) \
+    }
+
+#define PIN(p_port, p_pin, p_af) \
     { \
         { &machine_pin_type }, \
         .name = MP_QSTR_P##p_port##_##p_pin, \
         .port = (p_port), \
         .pin = (p_pin), \
+        .af_num = (sizeof(p_af) / sizeof(machine_pin_af_obj_t)), \
+        .af = (p_af) \
     }
