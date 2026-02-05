8ab91d078d86804f8b13f884f0df009108b8f282
zhanglinjing <Linjing.Zhang@infineon.com>
2026-02-05 14:38:53 +0100

    psoc-edge: Fix compile bug, adapt mp_handle_pending.
    Signed-off-by: zhanglinjing <Linjing.Zhang@infineon.com>

--- ports/psoc-edge/mpconfigport.h
+++ ports/psoc-edge/mpconfigport.h
@@ -138,8 +138,7 @@ typedef long mp_off_t;
 
 #define MICROPY_EVENT_POLL_HOOK_FAST \
     do { \
-        extern void mp_handle_pending(bool); \
-        mp_handle_pending(true); \
+        mp_handle_pending(MP_HANDLE_PENDING_CALLBACKS_AND_EXCEPTIONS); \
     } while (0);
 
 #define MICROPY_EVENT_POLL_HOOK \
