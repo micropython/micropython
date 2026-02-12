06bfefeaeee0922da4a507c297d45ffe58b7b88b
Damien George <damien@micropython.org>
2026-02-12 12:39:29 +1100

    py/objstr: Fix b"".hex() so it returns an empty str object.
    As per CPython behaviour, `b"".hex()` should return an empty str object
    (not an empty bytes object).
    
    Fixes issue #18807.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- tests/basics/builtin_str_hex.py
+++ tests/basics/builtin_str_hex.py
@@ -3,6 +3,7 @@ if not hasattr(bytes, "fromhex"):
     raise SystemExit
 
 for x in (
+    b"",
     b"\x00\x01\x02\x03\x04\x05\x06\x07",
     b"\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f",
     b"\x7f\x80\xff",
