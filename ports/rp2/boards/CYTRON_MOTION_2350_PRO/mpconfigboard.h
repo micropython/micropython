b276375d2d27f6b05ed9f8becc8f026fc6b2578e
sync-on-luma <spencerc@ayershale.net>
2026-02-22 23:43:34 -0800

    Fixed flash size and removed useless defintions
    Signed-off-by: sync-on-luma <spencerc@ayershale.net>

--- ports/rp2/boards/CYTRON_MOTION_2350_PRO/mpconfigboard.h
+++ ports/rp2/boards/CYTRON_MOTION_2350_PRO/mpconfigboard.h
@@ -1,10 +1,12 @@
 // Board and hardware specific configuration
 #define MICROPY_HW_BOARD_NAME                   "Cytron MOTION 2350 Pro"
-#define MICROPY_HW_FLASH_STORAGE_BYTES          (PICO_FLASH_SIZE_BYTES - 1024 * 1024)
-
-#define DEFAULT_I2C_BUS_SCL (&pin_GPIO17)
-#define DEFAULT_I2C_BUS_SDA (&pin_GPIO16)
+#define MICROPY_HW_FLASH_STORAGE_BYTES          (1024 * 1024)
 
 #define DEFAULT_SPI_BUS_SCK (&pin_GPIO18)
 #define DEFAULT_SPI_BUS_MOSI (&pin_GPIO19)
 #define DEFAULT_SPI_BUS_MISO (&pin_GPIO16)
+
+// Enable USB Mass Storage with FatFS filesystem.
+#define MICROPY_HW_USB_MSC              (1)
+#define MICROPY_HW_USB_VID              (0x2e8a)
+#define MICROPY_HW_USB_PID              (0x0005)
