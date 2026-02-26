3a286cacd49854e4fa75e0bf86bf82de067b4a69
sync-on-luma <spencerc@ayershale.net>
2026-02-26 03:06:16 -0800

    Remove default SPI bus pin configurations
    Removed default SPI bus pin definitions.
    
    Signed-off-by: sync-on-luma <spencerc@ayershale.net>

--- ports/rp2/boards/CYTRON_MOTION_2350_PRO/mpconfigboard.h
+++ ports/rp2/boards/CYTRON_MOTION_2350_PRO/mpconfigboard.h
@@ -2,10 +2,6 @@
 #define MICROPY_HW_BOARD_NAME                   "Cytron MOTION 2350 Pro"
 #define MICROPY_HW_FLASH_STORAGE_BYTES          (1024 * 1024)
 
-#define DEFAULT_SPI_BUS_SCK (&pin_GPIO18)
-#define DEFAULT_SPI_BUS_MOSI (&pin_GPIO19)
-#define DEFAULT_SPI_BUS_MISO (&pin_GPIO16)
-
 // Enable USB Mass Storage with FatFS filesystem.
 #define MICROPY_HW_USB_MSC              (1)
 #define MICROPY_HW_USB_VID              (0x2e8a)
