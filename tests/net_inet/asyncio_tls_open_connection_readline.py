f01373587d5717880be923c8876bf29d60f77c41
Angus Gratton <angus@redyak.com.au>
2026-02-26 18:53:21 +1100

    tests/net_inet: Use the letsencrypt TLS root cert for all tests.
    Previously, mpycert.der was the Intermediate certificate which is regularly
    re-issued by Letsencrypt.
    
    Also changes ssl_cert.py to load the cert data from the same file as
    test_sslcontext_client.py, so the DER string doesn't have to be pasted
    into the source.
    
    This work was funded through GitHub Sponsors.
    
    Signed-off-by: Angus Gratton <angus@redyak.com.au>

--- tests/net_inet/asyncio_tls_open_connection_readline.py
+++ tests/net_inet/asyncio_tls_open_connection_readline.py
@@ -6,25 +6,10 @@ if not hasattr(ssl, "CERT_REQUIRED"):
     print("SKIP")
     raise SystemExit
 
-# This certificate was obtained from micropython.org using openssl:
-# $ openssl s_client -showcerts -connect micropython.org:443 </dev/null 2>/dev/null
-# The certificate is from Let's Encrypt:
-# 1 s:/C=US/O=Let's Encrypt/CN=R3
-#   i:/C=US/O=Internet Security Research Group/CN=ISRG Root X1
-# Validity
-#            Not Before: Sep  4 00:00:00 2020 GMT
-#            Not After : Sep 15 16:00:00 2025 GMT
-# Copy PEM content to a file (certmpy.pem) and convert to DER e.g.
-# $ openssl x509 -in certmpy.pem -out certmpy.der -outform DER
-# Then convert to hex format, eg using binascii.hexlify(data).
+# For details about this cert, see comment in test_sslcontext_client.py
+root_cert_path = "isrgrootx1.der"
 
-# Note that the instructions above is to obtain an intermediate
-# root CA cert that works for MicroPython. However CPython needs the ultimate root CA
-# cert from ISRG
-
-ca_cert_chain = "isrg.der"
-
-with open(ca_cert_chain, "rb") as ca:
+with open(root_cert_path, "rb") as ca:
     cadata = ca.read()
 
 client_ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
