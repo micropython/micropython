e9131e501c9732bcb85c849e6de5784770eb31ee
sync-on-luma <spencerc@ayershale.net>
2026-02-26 03:07:06 -0800

    Remove USB MSC configuration from mpconfigboard.h
    Removed USB Mass Storage configuration options.
    
    Signed-off-by: sync-on-luma <spencerc@ayershale.net>

--- ports/rp2/boards/CYTRON_MOTION_2350_PRO/mpconfigboard.h
+++ ports/rp2/boards/CYTRON_MOTION_2350_PRO/mpconfigboard.h
@@ -1,8 +1,3 @@
 // Board and hardware specific configuration
 #define MICROPY_HW_BOARD_NAME                   "Cytron MOTION 2350 Pro"
 #define MICROPY_HW_FLASH_STORAGE_BYTES          (1024 * 1024)
-
-// Enable USB Mass Storage with FatFS filesystem.
-#define MICROPY_HW_USB_MSC              (1)
-#define MICROPY_HW_USB_VID              (0x2e8a)
-#define MICROPY_HW_USB_PID              (0x0005)
