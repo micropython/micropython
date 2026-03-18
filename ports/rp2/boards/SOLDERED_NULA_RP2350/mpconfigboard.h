93119a6b6ca12d5d9898c1630234c0e89c26ae9a
Josip Šimun Kuči <josipsimunkuci@gmail.com>
2026-03-18 09:56:51 +0100

    rp2/boards/SOLDERED_NULA_RP2350: Better pin mapping.
    Signed-off-by: Josip Šimun Kuči <josipsimunkuci@gmail.com>

--- ports/rp2/boards/SOLDERED_NULA_RP2350/mpconfigboard.h
+++ ports/rp2/boards/SOLDERED_NULA_RP2350/mpconfigboard.h
@@ -1,6 +1,6 @@
 // Board and hardware specific configuration
 #define MICROPY_HW_BOARD_NAME                   "Soldered NULA RP2350"
-#define MICROPY_HW_FLASH_STORAGE_BYTES          (PICO_FLASH_SIZE_BYTES - 1536 * 1024)
+#define MICROPY_HW_FLASH_STORAGE_BYTES          (14 * 1024 *1024)
 
 // Enable networking.
 #define MICROPY_PY_NETWORK 1
@@ -16,24 +16,14 @@
 #define MICROPY_HW_USB_PID (0x10EC)
 
 // UART0
-#define MICROPY_HW_UART0_TX  (6)
-#define MICROPY_HW_UART0_RX  (7)
-#define MICROPY_HW_UART0_CTS (10)
-#define MICROPY_HW_UART0_RTS (11)
-
-// UART1
-#define MICROPY_HW_UART1_TX  (41)
-#define MICROPY_HW_UART1_RX  (42)
-#define MICROPY_HW_UART1_CTS (43)
-#define MICROPY_HW_UART1_RTS (44)
+#define MICROPY_HW_UART0_TX  (32)
+#define MICROPY_HW_UART0_RX  (33)
+#define MICROPY_HW_UART0_CTS (30)
+#define MICROPY_HW_UART0_RTS (31)
 
 // I2C0
 #define MICROPY_HW_I2C0_SCL  (9)
 #define MICROPY_HW_I2C0_SDA  (8)
-
-// I2C1
-#define MICROPY_HW_I2C1_SCL  (31)
-#define MICROPY_HW_I2C1_SDA  (30)
 
 // SD Card/SPI0
 #define MICROPY_HW_SPI0_SCK  (2)
@@ -41,11 +31,10 @@
 #define MICROPY_HW_SPI0_MISO (0)
 
 // SPI1
-#define MICROPY_HW_SPI1_SCK  (32)
-#define MICROPY_HW_SPI1_MOSI (33)
-#define MICROPY_HW_SPI1_MISO (34)
+#define MICROPY_HW_SPI1_SCK  (42)
+#define MICROPY_HW_SPI1_MOSI (43)
+#define MICROPY_HW_SPI1_MISO (44)
 
-// #include "enable_cyw43.h"
 
 // For debugging mbedtls - also set
 // Debug level (0-4) 1=warning, 2=info, 3=debug, 4=verbose
