2dc2e30d98ee225070e990586526f8e43b3c95a2
Matt Trentini <matthew.trentini@planetinnovation.com.au>
2026-03-21 10:17:08 +1100

    esp32/boards/SEEED_XIAO_ESP32C6: Add new XIAO board definition.
    Signed-off-by: Matt Trentini <matthew.trentini@planetinnovation.com.au>
    Signed-off-by: Matt Trentini <matt.trentini@gmail.com>

---
+++ ports/esp32/boards/SEEED_XIAO_ESP32C6/mpconfigboard.h
@@ -0,0 +1,9 @@
+#define MICROPY_HW_BOARD_NAME               "Seeed XIAO ESP32C6"
+#define MICROPY_HW_MCU_NAME                 "ESP32C6"
+
+#define MICROPY_HW_I2C0_SCL  (23)
+#define MICROPY_HW_I2C0_SDA  (22)
+
+#define MICROPY_HW_SPI1_MOSI (18)
+#define MICROPY_HW_SPI1_MISO (20)
+#define MICROPY_HW_SPI1_SCK  (19)
