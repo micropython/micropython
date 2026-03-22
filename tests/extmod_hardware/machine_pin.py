d77264b1a1bde6e58f90740ba51a826649efd989
Damien George <damien@micropython.org>
2026-03-22 21:56:56 +1100

    tests/extmod_hardware: Add Pin test.
    Signed-off-by: Damien George <damien@micropython.org>

---
+++ tests/extmod_hardware/machine_pin.py
@@ -0,0 +1,43 @@
+# Test machine.Pin.
+#
+# IMPORTANT: This test requires hardware connections.
+
+try:
+    from machine import Pin
+except ImportError:
+    print("SKIP")
+    raise SystemExit
+
+import unittest
+from target_wiring import pwm_loopback_pins
+
+
+class TestBase:
+    @classmethod
+    def setUpClass(cls):
+        print("set up pins:", cls.pin_out, cls.pin_in)
+        cls.p0 = Pin(cls.pin_out)
+        cls.p1 = Pin(cls.pin_in)
+
+    def setUp(self):
+        self.p0.init(Pin.OUT)
+        self.p1.init(Pin.IN)
+
+    def test_connections(self):
+        print(self.pin_out, self.pin_in)
+        print(self.p0, self.p1)
+        self.p0(1)
+        self.assertEqual(1, self.p1())
+        self.p0(0)
+        self.assertEqual(0, self.p1())
+
+
+# Generate test classes, one for each set of pins to test.
+for pin_out, pin_in in pwm_loopback_pins:
+    cls_name = "Test_{}_{}".format(pin_out, pin_in)
+    globals()[cls_name] = type(
+        cls_name, (TestBase, unittest.TestCase), {"pin_out": pin_out, "pin_in": pin_in}
+    )
+
+if __name__ == "__main__":
+    unittest.main()
--- tests/run-tests.py
+++ tests/run-tests.py
@@ -296,6 +296,7 @@ tests_requiring_target_wiring = (
     "extmod/machine_uart_tx.py",
     "extmod_hardware/machine_can_timings.py",
     "extmod_hardware/machine_encoder.py",
+    "extmod_hardware/machine_pin.py",
     "extmod_hardware/machine_pwm.py",
     "extmod_hardware/machine_uart_irq_break.py",
     "extmod_hardware/machine_uart_irq_rx.py",
