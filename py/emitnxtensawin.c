6864bc2d1a61860c1e37974ed415806c50725b7d
Jeff Epler <jepler@unpythonic.net>
2026-03-30 15:54:47 -0500

    emitn: Error if register offset not known for setjmp.
    Except for emitxtensa (which uses setjmp now), the current offsets
    are specific to micropython's nlr regs[] array layout. The offset
    used by a target platform's setjmp implementation is unlikely to
    match except by coincidence. So, simply error in this case unless a
    definition of NLR_BUF_IDX_LOCAL_1 has been provided e.g., in the
    mpconfigport.h file.
    
    Signed-off-by: Jeff Epler <jepler@unpythonic.net>

--- py/emitnxtensawin.c
+++ py/emitnxtensawin.c
@@ -9,8 +9,14 @@
 #define GENERIC_ASM_API_WIN (1)
 #include "py/asmxtensa.h"
 
+#if MICROPY_NLR_SETJMP
+#if !defined(NLR_BUF_IDX_LOCAL_1)
+#error Must define NLR_BUF_IDX_LOCAL_1 (depends on layout of jmp_buf)
+#endif
+#else
 // Word indices of REG_LOCAL_x in nlr_buf_t
 #define NLR_BUF_IDX_LOCAL_1 (2 + 4) // a4
+#endif
 
 #define N_NLR_SETJMP (1)
 #define N_XTENSAWIN (1)
