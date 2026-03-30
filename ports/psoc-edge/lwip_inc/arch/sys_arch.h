deb2cd67662164bc8a6c9efc6820a21452299736
IFX-Anusha <Anusha.TR@infineon.com>
2026-03-30 15:08:25 +0530

    psoc-edge: Added lwip_inc headers for WiFi/lwIP stack.
    Signed-off-by: IFX-Anusha <Anusha.TR@infineon.com>

---
+++ ports/psoc-edge/lwip_inc/arch/sys_arch.h
@@ -0,0 +1,6 @@
+#ifndef MICROPY_INCLUDED_PSOC_EDGE_LWIP_ARCH_SYS_ARCH_H
+#define MICROPY_INCLUDED_PSOC_EDGE_LWIP_ARCH_SYS_ARCH_H
+
+#include "../../../../lib/psoc-edge/lwip-freertos-integration/arch/sys_arch.h"
+
+#endif // MICROPY_INCLUDED_PSOC_EDGE_LWIP_ARCH_SYS_ARCH_H
