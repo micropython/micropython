f3e79c8b565aa4e74dfeedd1b562784e01cac81f
Alessandro Gatti <a.gatti@frob.it>
2026-04-08 19:56:42 +0200

    qemu/boards/VIRT_RV64: Add ROMFS support.
    This commit reserves a memory area to mount ROMFS partitions into, and
    lets the port Makefile know the memory area details.
    
    A 4 MiB segment is allocated at 0x8062_0000, which is inside the
    emulated machine's DRAM segment.  The virt board requires the image
    loaded in its DRAM segment to be contiguous, so the ROMFS segment is
    placed right after the stack area.
    
    Signed-off-by: Alessandro Gatti <a.gatti@frob.it>

--- ports/qemu/boards/VIRT_RV64/mpconfigboard.mk
+++ ports/qemu/boards/VIRT_RV64/mpconfigboard.mk
@@ -6,6 +6,12 @@ CFLAGS += -DMICROPY_HW_MCU_NAME='"$(RV64_ARCH)"'
 
 LDSCRIPT = mcu/rv64/virt.ld
 
+# If the ROMFS partition size is not enough, remember to change
+# ports/qemu/mcu/rv64/virt.ld to resize the ROMFS section as well.
+
+MICROPY_HW_ROMFS_PART0_START = 0x80620000
+MICROPY_HW_ROMFS_PART0_SIZE = 0x00400000
+
 SRC_BOARD_O += shared/runtime/gchelper_native.o shared/runtime/gchelper_rv64i.o
 
 MPY_CROSS_FLAGS += -march=rv64imc
