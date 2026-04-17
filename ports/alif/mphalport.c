65399bc36e94ab4a0c8e6bef1fb395b5157e21e8
Damien George <damien@micropython.org>
2026-04-17 14:20:04 +1000

    alif/mphalport: Use DWT->CYCCNT for CPU tick if SysTick/LPTIMER is used.
    Using CYCCNT gives much higher resolution, at 400MHz.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/alif/mphalport.c
+++ ports/alif/mphalport.c
@@ -116,7 +116,19 @@ mp_uint_t mp_hal_stdout_tx_strn(const char *str, size_t len) {
 }
 
 mp_uint_t mp_hal_ticks_cpu(void) {
+    #if MICROPY_HW_SYSTEM_TICK_USE_SYSTICK || MICROPY_HW_SYSTEM_TICK_USE_LPTIMER
+    // SysTick and LPTIMER run relatively slowly, so use cycle counter for CPU ticks.
+    if (!(DWT->CTRL & DWT_CTRL_CYCCNTENA_Msk)) {
+        // Enable CYCCNT.
+        CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
+        DWT->CYCCNT = 0;
+        DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
+    }
+    return DWT->CYCCNT;
+    #else
+    // UTIMER runs at CPU frequency so use it directly as CPU ticks.
     return system_tick_get_u32();
+    #endif
 }
 
 mp_uint_t mp_hal_ticks_us(void) {
