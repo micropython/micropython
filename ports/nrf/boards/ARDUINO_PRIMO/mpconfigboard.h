11f339cd84baf56d900517c0184410d350f6bc61
Damien George <damien@micropython.org>
2026-04-22 15:03:24 +1000

    nrf: Change all boards to use "nrf" for sys.platform value.
    The `sys.platform` variable on bare-metal ports is uniformly the port name
    (except for stm32 which uses "pyboard" for `sys.platform`), regardless of
    the board.
    
    Change the nrf port to follow this convention.
    
    Among other things, this gets the test auto-detection working properly on
    nrf boards, which relies on the value of `sys.platform` (eg for target
    wiring selection).
    
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/nrf/boards/ARDUINO_PRIMO/mpconfigboard.h
+++ ports/nrf/boards/ARDUINO_PRIMO/mpconfigboard.h
@@ -26,7 +26,6 @@
 
 #define MICROPY_HW_BOARD_NAME       "Arduino Primo"
 #define MICROPY_HW_MCU_NAME         "NRF52832"
-#define MICROPY_PY_SYS_PLATFORM     "nrf52"
 
 #define MICROPY_PY_MACHINE_SOFT_PWM (1)
 #define MICROPY_PY_MUSIC            (1)
--- ports/nrf/boards/PCA10031/mpconfigboard.h
+++ ports/nrf/boards/PCA10031/mpconfigboard.h
@@ -26,7 +26,6 @@
 
 #define MICROPY_HW_BOARD_NAME       "PCA10031"
 #define MICROPY_HW_MCU_NAME         "NRF51822"
-#define MICROPY_PY_SYS_PLATFORM     "nrf51-dongle"
 
 #define MICROPY_PY_MACHINE_UART     (1)
 #define MICROPY_PY_MACHINE_SOFT_PWM (1)
