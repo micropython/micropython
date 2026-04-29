17e720dde237d748911afea55079d71def282500
jaenrig-ifx <enriquezgarcia.external@infineon.com>
2026-04-29 09:46:12 +0200

    psoc-edge/mpconfigport: Added additional configuration.
    Signed-off-by: jaenrig-ifx <enriquezgarcia.external@infineon.com>

--- ports/psoc-edge/mpconfigport.h
+++ ports/psoc-edge/mpconfigport.h
@@ -32,6 +32,12 @@
 // Use core features for Thonny compatibility (larger firmware)
 #define MICROPY_CONFIG_ROM_LEVEL                (MICROPY_CONFIG_ROM_LEVEL_EXTRA_FEATURES)
 
+// MicroPython emitters
+#define MICROPY_PERSISTENT_CODE_LOAD            (1)
+#define MICROPY_EMIT_THUMB                      (1)
+#define MICROPY_EMIT_INLINE_THUMB               (1)
+#define MICROPY_MAKE_POINTER_CALLABLE(p)        ((void *)((mp_uint_t)(p) | 1))
+
 // options to control how MicroPython is built
 #define MICROPY_PY_BUILTINS_HELP_TEXT           psoc_edge_help_text
 #define MICROPY_USE_INTERNAL_PRINTF             (0)
@@ -52,20 +58,27 @@
 #define MICROPY_PY_SYS_EXC_INFO                 (1)
 
 // Extended modules
+#define MICROPY_VFS                             (1)
+#define MICROPY_READER_VFS                      (1)
+#define MICROPY_SCHEDULER_DEPTH                 (8)
+#define MICROPY_SCHEDULER_STATIC_NODES          (1)
+#define MICROPY_USE_INTERNAL_ERRNO              (1)
 #define MICROPY_PY_TIME_GMTIME_LOCALTIME_MKTIME (1)
 #define MICROPY_PY_TIME_TIME_TIME_NS            (1)
 #define MICROPY_PY_TIME_INCLUDEFILE             "ports/psoc-edge/modtime.c"
-#define MICROPY_PY_MACHINE_FREQ_NUM_ARGS_MAX    (2)
+#define MICROPY_TIME_SUPPORT_Y1969_AND_BEFORE   (1)
+#define MICROPY_EPOCH_IS_1970                   (1)
+#define MICROPY_ENABLE_EMERGENCY_EXCEPTION_BUF  (1)
 
 // Logger
 #define MICROPY_LOGGER_DEBUG                    (0)
 
 // Machine module
 #define MICROPY_PY_MACHINE                      (1)
+#define MICROPY_PY_MACHINE_INCLUDEFILE          "ports/psoc-edge/modmachine.c"
 
 #define MICROPY_PY_MACHINE_PIN_MAKE_NEW         mp_pin_make_new
 
-#define MICROPY_PY_MACHINE_INCLUDEFILE          "ports/psoc-edge/modmachine.c"
 #define MICROPY_PY_MACHINE_I2C                  (1)
 #define MICROPY_PY_MACHINE_SOFTI2C              (0)
 
@@ -76,14 +89,6 @@
 #define MICROPY_PY_MACHINE_PDM_PCM              (1)
 #define MICROPY_PY_MACHINE_PDM_PCM_RING_BUF     (1)
 #define MICROPY_PY_MACHINE_PDM_PCM_INCLUDEFILE  "machine_pdm_pcm.c"
-
-#define MICROPY_SCHEDULER_DEPTH                 (8)
-
-#define MICROPY_TIME_SUPPORT_Y1969_AND_BEFORE   (1)
-
-// VFS
-#define MICROPY_VFS                             (1)
-#define MICROPY_READER_VFS                      (1)
 
 // type definitions for the specific machine
 #define MP_SSIZE_MAX (0x7fffffff)
