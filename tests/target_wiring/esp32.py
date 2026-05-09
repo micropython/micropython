466e6ac82de15dfe4199392ca9a0ad11945a69d3
Damien George <damien@micropython.org>
2026-05-09 23:22:11 +1000

    tests/target_wiring/esp32.py: Update esp32 target wiring.
    Signed-off-by: Damien George <damien@micropython.org>

--- tests/target_wiring/esp32.py
+++ tests/target_wiring/esp32.py
@@ -19,3 +19,21 @@ pwm_loopback_pins = [(4, 5)]
 encoder_loopback_id = 0
 encoder_loopback_out_pins = (4, 12)
 encoder_loopback_in_pins = (5, 13)
+
+import sys
+if "C6" in sys.implementation._machine:
+    # 01Space C6 board
+    # also ESP32-C6-DevKitC-1 doesn't seem to like 12/13 connected together
+    encoder_loopback_out_pins = (4, 6)
+    encoder_loopback_in_pins = (5, 7)
+
+if "P4" in sys.implementation._machine:
+    # FireBeetle 2 board
+    encoder_loopback_out_pins = (4, 7)
+    encoder_loopback_in_pins = (5, 8)
+
+if "S2" in sys.implementation._machine:
+    # FeatherS2 board
+    uart_loopback_kwargs = {"tx": 5, "rx": 6}
+    encoder_loopback_out_pins = (5, 10)
+    encoder_loopback_in_pins = (6, 11)
