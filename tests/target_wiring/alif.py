ac60c523303fe839771a3736aa0d3e0ee71d2b6c
Damien George <damien@micropython.org>
2026-05-09 23:21:25 +1000

    tests/target_wiring: Update target wiring.
    Signed-off-by: Damien George <damien@micropython.org>

--- tests/target_wiring/alif.py
+++ tests/target_wiring/alif.py
@@ -9,3 +9,6 @@ uart_loopback_kwargs = {}
 spi_standalone_args_list = [(0,)]
 
 pwm_loopback_pins = [("P0_4", "P0_5")]
+
+i2c_args_controller = {"scl": "P1_1", "sda": "P1_0"}
+i2c_args_target = (0,)  # on pins P0_3/P0_2
--- tests/target_wiring/rp2.py
+++ tests/target_wiring/rp2.py
@@ -2,6 +2,7 @@
 #
 # Connect:
 # - GPIO0 to GPIO1
+# - GPIO4 to GPIO6 and GPIO5 to GPIO7
 
 uart_loopback_args = (0,)
 uart_loopback_kwargs = {"tx": "GPIO0", "rx": "GPIO1"}
@@ -9,3 +10,6 @@ uart_loopback_kwargs = {"tx": "GPIO0", "rx": "GPIO1"}
 spi_standalone_args_list = [(0,), (1,)]
 
 pwm_loopback_pins = [("GPIO0", "GPIO1")]
+
+i2c_args_controller = {"scl": 5, "sda": 4}
+i2c_args_target = (1,)  # on pins 7/6
