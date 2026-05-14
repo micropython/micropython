cdd5fd4663895c26cc784afb4b9cf840ad96f67a
Jacob Peck <gatesphere@gmail.com>
2026-05-14 18:11:45 -0400

    rp2/boards/ADAFRUIT_FEATHER_RP2350: Add board definition.
    Signed-off-by: Jacob Peck <gatesphere@gmail.com>

---
+++ ports/rp2/boards/ADAFRUIT_FEATHER_RP2350/mpconfigboard.cmake
@@ -0,0 +1,5 @@
+# cmake file for Adafruit Feather RP2350
+set(PICO_BOARD "pico2")
+
+# To change the gpio count for QFN-80
+# set(PICO_NUM_GPIOS 48)
---
+++ ports/rp2/boards/ADAFRUIT_FEATHER_RP2350/mpconfigboard.h
@@ -0,0 +1,14 @@
+// https://www.adafruit.com/product/6000
+// https://learn.adafruit.com/adafruit-feather-rp2350/pinouts
+
+#define MICROPY_HW_BOARD_NAME          "Adafruit Feather RP2350"
+#define MICROPY_HW_FLASH_STORAGE_BYTES (PICO_FLASH_SIZE_BYTES - 1 * 1024 * 1024)
+
+#define MICROPY_HW_USB_VID (0x239A)
+#define MICROPY_HW_USB_PID (0x814F)
+
+// NeoPixel GPIO21, power not toggleable
+
+// Red user LED GPIO7
+
+// Optional PSRAM on GPIO8
