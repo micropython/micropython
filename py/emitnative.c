59180805d0370859d4d94357234895f30e39ef79
Damien George <damien@micropython.org>
2026-05-19 19:16:34 +1000

    py/emitnative: Change assert to exception for raise with 0 or 2 args.
    The native emitter cannot generate code for "raise" (re-raising) or
    "raise x from y" (exception chaining).  Trying to generate such code can
    happen in normal programs, so raise an NotImplementedError instead of using
    an assert (that won't even be enabled in most production builds).
    
    And add a coverage test for this new exception.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- py/emitnative.c
+++ py/emitnative.c
@@ -2934,8 +2934,9 @@ static void emit_native_return_value(emit_t *emit) {
 
 static void emit_native_raise_varargs(emit_t *emit, mp_uint_t n_args) {
     DEBUG_printf("raise_varargs(%d)\n", n_args);
-    (void)n_args;
-    assert(n_args == 1);
+    if (n_args != 1) {
+        mp_raise_NotImplementedError(MP_ERROR_TEXT("native raise"));
+    }
     vtype_kind_t vtype_exc;
     emit_pre_pop_reg(emit, &vtype_exc, REG_ARG_1); // arg1 = object to raise
     if (vtype_exc != VTYPE_PYOBJ) {
