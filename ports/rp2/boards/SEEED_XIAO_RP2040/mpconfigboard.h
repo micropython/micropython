8cb7558d0098306953a978807548c22add1fa5d6
cumin <13809292481@163.com>
2026-05-21 19:43:14 +0800

    rp2/boards: Extend SEEED_XIAO_RP2040 to support new Plus boards.
    This commit adds board support for the Seeed Studio XIAO RP2040 Plus to the
    rp2 port.  It reuses the existing SEEED_XIAO_RP2040 board definition so
    that a single firmware works on both the Standard and the Plus variants.
    
    Also fix the Standard I2C instance: it must be I2C1 on pins 7/6.
    
    Signed-off-by: cumin <13809292481@163.com>

--- ports/rp2/boards/SEEED_XIAO_RP2040/mpconfigboard.h
+++ ports/rp2/boards/SEEED_XIAO_RP2040/mpconfigboard.h
@@ -6,9 +6,14 @@
 // #define MICROPY_HW_USB_VID (0x)
 // #define MICROPY_HW_USB_PID (0x)
 
-// I2C0
-#define MICROPY_HW_I2C0_SCL  (7)
-#define MICROPY_HW_I2C0_SDA  (6)
+// I2C0 is available on XIAO RP2040 Plus on GPIO20/GPIO21.
+// These pins are not fitted on the standard XIAO RP2040.
+#define MICROPY_HW_I2C0_SCL  (21)
+#define MICROPY_HW_I2C0_SDA  (20)
+
+// I2C1 is the default bus on GPIO6/GPIO7 (the standard XIAO RP2040 pins).
+#define MICROPY_HW_I2C1_SCL  (7)
+#define MICROPY_HW_I2C1_SDA  (6)
 
 // SPI0
 #define MICROPY_HW_SPI0_SCK  (2)
