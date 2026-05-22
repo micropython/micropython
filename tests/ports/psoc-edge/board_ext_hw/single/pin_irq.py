a64ccbc77eb43b98f510477151667093af12ce6b
jaenrig-ifx <enriquezgarcia.external@infineon.com>
2026-05-22 15:22:43 +0200

    tests/psoc-edge/../pin_irq: Added pin irq obj and hard tests.
    Signed-off-by: jaenrig-ifx <enriquezgarcia.external@infineon.com>

--- tests/ports/psoc-edge/board_ext_hw/single/pin_irq.py
+++ tests/ports/psoc-edge/board_ext_hw/single/pin_irq.py
@@ -1,4 +1,5 @@
 from machine import Pin
+import time
 
 # Two pins are connected together.
 # An output pin triggers the interrupt.
@@ -13,23 +14,38 @@ def irq_handler(pin):
 
 # Configure the IRQ handler to trigger on falling edge
 # The handler should be called twice
-pin_irq.irq(irq_handler, trigger=Pin.IRQ_FALLING)
+irq = pin_irq.irq(irq_handler, trigger=Pin.IRQ_FALLING)
 pin_trigger(0)
 pin_trigger(1)
 pin_trigger(0)
+# Wait for the interrupts to be processed
+time.sleep_ms(50)
+print("irq flag falling:", irq.flags() == Pin.IRQ_FALLING)
+
 
 # Now it is configured to trigger on rising edge.
 # The handler should be called twice
-pin_irq.irq(irq_handler, trigger=Pin.IRQ_RISING)
+irq.trigger(Pin.IRQ_RISING)
 pin_trigger(0)
 pin_trigger(1)
+time.sleep_ms(50)
 pin_trigger(0)
 pin_trigger(1)
+time.sleep_ms(50)
+print("irq flag rising:", irq.flags() == Pin.IRQ_RISING)
 
-# TODO: The priority and ishard parameters are not tested.
-# These might be added in the future if we find a suitable HIL functional test.
 
-# Wait for the interrupts to be processed
-import time
+# Test hard IRQ
+def irq_handler_hard(pin):
+    print("irq hard")
 
-time.sleep_ms(500)
+
+irq = pin_irq.irq(irq_handler_hard, trigger=Pin.IRQ_FALLING, hard=True)
+pin_trigger(0)
+
+
+# Invalid trigger
+try:
+    irq.trigger(0x35)
+except ValueError as e:
+    print("error:", e)
