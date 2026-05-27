2696d563ef1ea71cf2c70d41f1c7fe75a52a9ba5
Damien George <damien@micropython.org>
2026-05-27 12:12:40 +1000

    rp2/boards/WEACTSTUDIO: Use board headers from pico-sdk.
    Fixes:
    - Flash size is now correct for the given variant.
    - Filesystem size is adjusted per variant to leave 1MB for application.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/rp2/boards/WEACTSTUDIO/mpconfigboard.cmake
+++ ports/rp2/boards/WEACTSTUDIO/mpconfigboard.cmake
@@ -1,12 +1,4 @@
 # CMake file for WeAct Studio RP2040 boards
 
-# The WeAct Studio boards don't have official pico-sdk support so we define it
-# See also: https://github.com/raspberrypi/pico-sdk/tree/master/src/boards/include/boards
-list(APPEND PICO_BOARD_HEADER_DIRS ${MICROPY_BOARD_DIR})
-
 # Freeze board.py
 set(MICROPY_FROZEN_MANIFEST ${MICROPY_BOARD_DIR}/manifest.py)
-
-if(NOT DEFINED MICROPY_HW_FLASH_STORAGE_BYTES)
-    set(MICROPY_HW_FLASH_STORAGE_BYTES 15728640)  # PICO_FLASH_SIZE_BYTES - (1 * 1024 * 1024) = 16MB - 1MB
-endif()
--- ports/rp2/boards/WEACTSTUDIO/mpconfigboard.h
+++ ports/rp2/boards/WEACTSTUDIO/mpconfigboard.h
@@ -1,3 +1,1 @@
 #define MICROPY_HW_BOARD_NAME "WeAct Studio RP2040"
-
-// Allow 1MB for the firmware image itself, allocate the remainder to the filesystem
