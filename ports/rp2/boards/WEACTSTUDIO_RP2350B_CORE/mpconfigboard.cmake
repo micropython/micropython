e5a7c7bcde048dbe9501a6913f39a90f018dd6d0
Damien George <damien@micropython.org>
2026-05-27 12:05:23 +1000

    rp2/boards/WEACTSTUDIO_RP2350B_CORE: Use board header from pico-sdk.
    Fixes:
    - Flash size is 16MB instead of 4MB.
    - Now supports RP2350 A2.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/rp2/boards/WEACTSTUDIO_RP2350B_CORE/mpconfigboard.cmake
+++ ports/rp2/boards/WEACTSTUDIO_RP2350B_CORE/mpconfigboard.cmake
@@ -3,11 +3,7 @@
 # The Core is powered by an RP2350B with 48 GPIOs
 set(PICO_NUM_GPIOS 48)
 
-# The WeAct Studio boards don't have official pico-sdk support.
-# So, add this board directory to the header search path and define PICO_BOARD
-# which will instruct pico-sdk to look for weactstudio_rp2350b_core.h
-list(APPEND PICO_BOARD_HEADER_DIRS ${MICROPY_BOARD_DIR})
-set(PICO_BOARD "weactstudio_rp2350b_core")
+set(PICO_BOARD "weact_studio_rp2350b_core")
 
 # Freeze manifest and modules
 set(MICROPY_FROZEN_MANIFEST ${MICROPY_BOARD_DIR}/manifest.py)
