251337628af3fd191123e37e82eb2000ed60dbfb
Damien George <damien@micropython.org>
2026-06-01 12:51:49 +1000

    tests/target_wiring/samd.py: Add pin_loopback_pins.
    pin.irq doesn't yet work, seems it files rising and falling regardless of
    config.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- tests/target_wiring/samd.py
+++ tests/target_wiring/samd.py
@@ -3,9 +3,10 @@
 # Connect:
 # - D0 to D1
 
+pin_loopback_pins = [("D0", "D1")]
+pwm_loopback_pins = [("D0", "D1")]
+
 uart_loopback_args = ()
 uart_loopback_kwargs = {"tx": "D1", "rx": "D0"}
 
 spi_standalone_args_list = [()]
-
-pwm_loopback_pins = [("D0", "D1")]
