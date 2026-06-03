d3a69ee822b218d5449171eea15b02659fb9eeab
jaenrig-ifx <enriquezgarcia.external@infineon.com>
2026-06-03 14:04:01 +0200

    psoc-edge/systick: Refactored time ticks based functionality.
    Signed-off-by: jaenrig-ifx <enriquezgarcia.external@infineon.com>

--- ports/psoc-edge/mphalport.c
+++ ports/psoc-edge/mphalport.c
@@ -25,53 +25,16 @@
  */
 
 // std includes
-#include "stdbool.h"   // because of missing include in shared/timeutils/timeutils.h
 #include "stdio.h"
 
 // MTB includes
 #include "mtb_hal_system.h"
-#include "mtb_hal_timer.h"
 
 // micropython includes
 #include "mpconfigport.h"
 #include "mphalport.h"
 
 #include "py/runtime.h"
-#include "shared/timeutils/timeutils.h"
-
-extern mtb_hal_timer_t psoc_edge_timer;
-
-void mp_hal_delay_ms(mp_uint_t ms) {
-    mtb_hal_system_delay_ms(ms);
-}
-
-void mp_hal_delay_us(mp_uint_t us) {
-    mtb_hal_system_delay_us(us);
-}
-
-uint64_t mp_hal_time_ns(void) {
-    // TODO: This is not fully functional until rtc machine module is implemented.
-    cy_stc_rtc_config_t current_date_time = {0};
-    Cy_RTC_GetDateAndTime(&current_date_time);
-
-    uint64_t s = timeutils_seconds_since_epoch(current_date_time.year, current_date_time.month, current_date_time.date,
-        current_date_time.hour, current_date_time.min, current_date_time.sec);
-
-    // add ticks to make sure time is strictly monotonic
-    return s * 1000000000ULL + mtb_hal_timer_read(&psoc_edge_timer) * 1000ULL;
-}
-
-mp_uint_t mp_hal_ticks_ms(void) {
-    return mtb_hal_timer_read(&psoc_edge_timer) / 1000;
-}
-
-mp_uint_t mp_hal_ticks_us(void) {
-    return mtb_hal_timer_read(&psoc_edge_timer);
-}
-
-mp_uint_t mp_hal_ticks_cpu(void) {
-    return mtb_hal_timer_read(&psoc_edge_timer);
-}
 
 extern void machine_uart_repl_init(void);
 extern int machine_uart_repl_readchar(void);
