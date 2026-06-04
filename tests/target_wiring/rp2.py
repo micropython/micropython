2ddf4db17d37c808356111e339f8d44f79490564
Damien George <damien@micropython.org>
2026-06-04 23:21:09 +1000

    tests/target_wiring/rp2.py: Add wiring for pins tests.
    Signed-off-by: Damien George <damien@micropython.org>

--- tests/target_wiring/rp2.py
+++ tests/target_wiring/rp2.py
@@ -4,12 +4,13 @@
 # - GPIO0 to GPIO1
 # - GPIO4 to GPIO6 and GPIO5 to GPIO7
 
+pin_loopback_pins = [("GPIO0", "GPIO1"), ("GPIO4", "GPIO6"), ("GPIO5", "GPIO7")]
+pwm_loopback_pins = [("GPIO0", "GPIO1")]
+
 uart_loopback_args = (0,)
 uart_loopback_kwargs = {"tx": "GPIO0", "rx": "GPIO1"}
 
 spi_standalone_args_list = [(0,), (1,)]
-
-pwm_loopback_pins = [("GPIO0", "GPIO1")]
 
 i2c_args_controller = {"scl": 5, "sda": 4}
 i2c_args_target = (1,)  # on pins 7/6
