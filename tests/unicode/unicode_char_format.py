0c3dd02aa4cde43f075c82c0f5ba80d811e75d96
Jos Verlinde <jos_verlinde@hotmail.com>
2026-06-06 21:48:58 +0200

    tests/unicode/unicode_char_format: Test unicode character formatting.
    Signed-off-by: Jos Verlinde <jos_verlinde@hotmail.com>

--- tests/unicode/unicode_char_format.py
+++ tests/unicode/unicode_char_format.py
@@ -1,6 +1,8 @@
 # test %c formatting with unicode characters (issue #3364)
 # tests that character codes >= 128 are properly encoded as UTF-8
 
+print("%c%c" % (0x3BC, 0x1F40D))  # Greek letter mu and snake emoji
+
 # ASCII character
 print("%c" % 65)
 
@@ -18,9 +20,39 @@ print("%c" % 0x1F600)  # emoji 😀
 # test with .format() method
 print("{:c}".format(169))
 print("{:c}".format(0x4E00))
+print("{:c}{:c}".format(0x3BC, 0x1F40D))
 
 # test with f-strings
 c = 169
 print(f"{c:c}")
 c = 0x1F600
 print(f"{c:c}")
+
+# Test boundary values - valid maximum unicode codepoint
+print("%c" % 0x10FFFF)  # Last valid unicode codepoint
+
+# Test invalid codepoint - >= 0x110000 should raise OverflowError
+try:
+    print("%c" % 0x110000)
+    print("UNEXPECTED: should have raised OverflowError")
+except OverflowError:
+    print("OverflowError")
+
+try:
+    print("%c" % 0x110001)
+    print("UNEXPECTED: should have raised OverflowError")
+except OverflowError:
+    print("OverflowError")
+
+# Test format() method with invalid codepoint
+try:
+    print("{:c}".format(0x110000))
+    print("UNEXPECTED: should have raised OverflowError")
+except OverflowError:
+    print("OverflowError")
+
+try:
+    print("{:c}".format(0x200000))
+    print("UNEXPECTED: should have raised OverflowError")
+except OverflowError:
+    print("OverflowError")
