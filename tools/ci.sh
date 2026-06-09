d8eb65bb829d42da44f128f1a31466d2e84a2ced
Alessandro Gatti <a.gatti@frob.it>
2026-06-09 15:53:52 +0200

    tools/ci.sh: Add new Unix target with no error messages.
    This commit adds a new CI target for the Unix port, which is the
    `standard` variant being built with no error messages (as in, exceptions
    won't report anything but the exception type).
    
    Even though it won't cover all possible tests failing with no error
    messages (some occur only in the `coverage` build variant), this should
    be a good start for preventing new tests to not assume a particular
    error message level.
    
    To make things automated, the new target is also added to the regular CI
    jobs set in the Unix port's GitHub workflow file.
    
    Signed-off-by: Alessandro Gatti <a.gatti@frob.it>

--- tools/ci.sh
+++ tools/ci.sh
@@ -721,11 +721,19 @@ function ci_unix_standard_v2_run_tests {
     ci_unix_run_tests_full_helper standard
 }
 
-function ci_unix_standard_terse_build {
+function ci_unix_standard_error_terse_build {
     ci_unix_build_helper VARIANT=standard CFLAGS_EXTRA="-DMICROPY_ERROR_REPORTING=MICROPY_ERROR_REPORTING_TERSE"
 }
 
-function ci_unix_standard_terse_run_tests {
+function ci_unix_standard_error_terse_run_tests {
+    make -C ports/unix VARIANT=standard test
+}
+
+function ci_unix_standard_error_none_build {
+    ci_unix_build_helper VARIANT=standard CFLAGS_EXTRA="-DMICROPY_ERROR_REPORTING=MICROPY_ERROR_REPORTING_NONE" MICROPY_ROM_TEXT_COMPRESSION=0
+}
+
+function ci_unix_standard_error_none_run_tests {
     make -C ports/unix VARIANT=standard test
 }
 
