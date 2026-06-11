5537e10474fa23c746e6249517b2f7e2bdb0d690
Damien George <damien@micropython.org>
2026-06-11 22:33:35 +1000

    tests/extmod: Rewrite with unittest select/socket tests that use UDP.
    Three tests are rewritten using unittest, so that they don't need to be run
    under CPython to get the expected output.  Because running under CPython
    requires port 8000 to be available, which it may not (at least for tests in
    the extmod directory having a port available should not be a requirement).
    
    Signed-off-by: Damien George <damien@micropython.org>

--- tests/extmod/select_poll_udp.py
+++ tests/extmod/select_poll_udp.py
@@ -2,12 +2,12 @@
 
 try:
     import socket, select
-
-    select.poll  # Raises AttributeError for CPython implementations without poll()
-except (ImportError, AttributeError):
+except ImportError:
     print("SKIP")
     raise SystemExit
 
+# Some targets (eg PYBV10) have the socket module but are unable to create
+# UDP sockets without a registered NIC.
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind(socket.getaddrinfo("127.0.0.1", 8000)[0][-1])
@@ -15,19 +15,33 @@ except OSError:
     print("SKIP")
     raise SystemExit
 
-poll = select.poll()
+import unittest
 
-# UDP socket should not be readable
-poll.register(s, select.POLLIN)
-print(len(poll.poll(0)))
 
-# UDP socket should be writable
-poll.modify(s, select.POLLOUT)
-print(poll.poll(0)[0][1] == select.POLLOUT)
+class Test(unittest.TestCase):
+    def test_poll(self):
+        poll = select.poll()
 
-# same test for select.select, but just skip it if the function isn't available
-if hasattr(select, "select"):
-    r, w, e = select.select([s], [], [], 0)
-    assert not r and not w and not e
+        # UDP socket should not be readable.
+        poll.register(s, select.POLLIN)
+        res = poll.poll(0)
+        self.assertEqual(len(res), 0)
+
+        # UDP socket should be writable.
+        poll.modify(s, select.POLLOUT)
+        res = poll.poll(0)
+        self.assertEqual(res[0][1], select.POLLOUT)
+
+    @unittest.skipUnless(hasattr(select, "select"), "no select")
+    def test_select(self):
+        # UDP socket should only be writable.
+        r, w, e = select.select([s], [s], [s], 0)
+        self.assertEqual(r, [])
+        self.assertEqual(w, [s])
+        self.assertEqual(e, [])
+
+
+if __name__ == "__main__":
+    unittest.main()
 
 s.close()
