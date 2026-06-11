14647cf26638d6fb575742c28fbbd6267d288351
Damien George <damien@micropython.org>
2026-06-11 14:42:57 +1000

    stm32/boards/OPENMV_N6: Update CAN pin names.
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/stm32/boards/OPENMV_N6/mpconfigboard.h
+++ ports/stm32/boards/OPENMV_N6/mpconfigboard.h
@@ -79,13 +79,12 @@
 
 // FDCAN bus
 #define MICROPY_HW_CAN1_NAME        "FDCAN1"
-#define MICROPY_HW_CAN1_TX          (pyb_pin_CAN1_TXFD)
-#define MICROPY_HW_CAN1_RX          (pyb_pin_CAN1_RXFD)
+#define MICROPY_HW_CAN1_TX          (pyb_pin_CAN1_TX)
+#define MICROPY_HW_CAN1_RX          (pyb_pin_CAN1_RX)
 // Support is not yet added for FDCAN CAN3 (see fdcan.c for details)
 // #define MICROPY_HW_CAN3_NAME        "FDCAN3"
-// #define MICROPY_HW_CAN3_TX          (pyb_pin_CAN3_TXFD)
-// #define MICROPY_HW_CAN3_RX          (pyb_pin_CAN3_RXFD)
-#define MICROPY_HW_CAN_IS_RESERVED(id) ((id != PYB_CAN_1) && (id != PYB_CAN_3))
+// #define MICROPY_HW_CAN3_TX          (pyb_pin_CAN3_TX)
+// #define MICROPY_HW_CAN3_RX          (pyb_pin_CAN3_RX)
 
 // USER is pulled high, and pressing the button makes the input go low.
 #define MICROPY_HW_USRSW_PIN        (pyb_pin_BUTTON)
