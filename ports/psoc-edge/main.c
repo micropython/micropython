0bae84ae4ff5dba9120cfbc944e490feec8fb140
jaenrig-ifx <enriquezgarcia.external@infineon.com>
2026-06-15 22:59:24 +0200

    psoc-edge/main: Deinit all UART instances on soft reset.
    Signed-off-by: jaenrig-ifx <enriquezgarcia.external@infineon.com>

--- ports/psoc-edge/main.c
+++ ports/psoc-edge/main.c
@@ -60,6 +60,7 @@ extern uint8_t __HeapBase, __HeapLimit;
 
 extern void machine_rtc_init_all(void);
 extern void machine_pin_irq_deinit_all(void);
+extern void machine_uart_deinit_all(void);
 extern void mp_hal_ticks_init(void);
 
 boot_mode_t check_boot_mode(void) {
@@ -163,6 +164,7 @@ soft_reset:
     mp_printf(&mp_plat_print, "MPY: soft reboot\n");
 
     machine_pin_irq_deinit_all();
+    machine_uart_deinit_all();
 
     #if MICROPY_ENABLE_GC
     gc_sweep_all();
