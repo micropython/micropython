768bc8fe0a5fe843f22bd726e1a7b5518cb24bc0
Damien George <damien@micropython.org>
2026-06-16 16:44:01 +1000

    stm32/flash: Implement flash_get_max_sector_size helper function.
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/stm32/flash.h
+++ ports/stm32/flash.h
@@ -27,6 +27,7 @@
 #define MICROPY_INCLUDED_STM32_FLASH_H
 
 bool flash_is_valid_addr(uint32_t addr);
+uint32_t flash_get_max_sector_size(void);
 int32_t flash_get_sector_info(uint32_t addr, uint32_t *start_addr, uint32_t *size);
 int flash_erase(uint32_t flash_dest);
 int flash_write(uint32_t flash_dest, const uint32_t *src, uint32_t num_word32);
