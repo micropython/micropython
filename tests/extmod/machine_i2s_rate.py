fae34652f747bcdabcb43864f7b27b4d14e51366
Rafal Wadowski <rafal@wadowski.com.au>
2026-06-24 19:24:17 +1000

    tests: Removed I2S try-except block and replaced with if block.
    Signed-off-by: Rafal Wadowski <rafal@wadowski.com.au>

--- tests/extmod/machine_i2s_rate.py
+++ tests/extmod/machine_i2s_rate.py
@@ -26,16 +26,12 @@ elif "mimxrt" in sys.platform:
         (2, Pin("D4"), Pin("D3"), Pin("D2"), None),
     )
 elif "esp32" in sys.platform:
-    try:
+    if "ESP32-C3" in sys.implementation._machine:
+        i2s_instances = ((0, Pin(6), Pin(7), Pin(10), Pin(11)),)
+    elif "ESP32-H2" in sys.implementation._machine:
+        i2s_instances = ((0, Pin(3), Pin(4), Pin(10), Pin(12)),)
+    else:
         i2s_instances = ((0, Pin(18), Pin(19), Pin(21), Pin(14)),)
-    except ValueError:
-        if sys.implementation._machine:
-            if "ESP32-H2" in sys.implementation._machine:
-                # fallback to lower pin number for ESP32-H2
-                i2s_instances = ((0, Pin(3), Pin(4), Pin(10), Pin(12)),)
-            else:
-                # fallback to lower pin number for ESP32-C3
-                i2s_instances = ((0, Pin(6), Pin(7), Pin(10), Pin(11)),)
     # Allow for small additional RTOS overhead
     MAX_DELTA_MS = 8
 
