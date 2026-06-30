0d326759d23999dc01b51261858f0e3e092ed87e
Phil Howard <github@gadgetoid.com>
2026-06-30 15:30:43 +0100

    rp2: Fix RP2350 watchdog time to ~16s.
    The RP2350 watchdog supports a 16777ms period vs the RP2040s 8388ms.
    
    Signed-off-by: Phil Howard <github@gadgetoid.com>

--- ports/rp2/machine_wdt.c
+++ ports/rp2/machine_wdt.c
@@ -29,8 +29,14 @@
 
 #include "hardware/watchdog.h"
 
-// The maximum timeout in milliseconds is: 0xffffff / 2 / 1000
+// The maximum timeout is set by the 24-bit watchdog counter and the number of
+// ticks it decrements per microsecond (WATCHDOG_XFACTOR): 2 on RP2040, 1 on
+// RP2350.  So the max in milliseconds is 0xffffff / WATCHDOG_XFACTOR / 1000.
+#if PICO_RP2350
+#define WDT_TIMEOUT_MAX 16777
+#else
 #define WDT_TIMEOUT_MAX 8388
+#endif
 
 typedef struct _machine_wdt_obj_t {
     mp_obj_base_t base;
