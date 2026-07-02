5a715ad941de6f1e3add3e777c83f905bf646348
Frédéric Pierson <fpierson@garatronic.fr>
2026-07-02 09:25:22 +0200

    Rename GARATRONIC_PYMATE_CORE folder for a more accurate GARATRONIC_PYMATE_CORE8ADI8DOSC reference

---
+++ ports/stm32/boards/GARATRONIC_PYMATE_CORE8ADI8DOSC/mpconfigboard.mk
@@ -0,0 +1,19 @@
+# MCU settings
+MCU_SERIES = f4
+CMSIS_MCU = STM32F412Rx
+MICROPY_FLOAT_IMPL = single
+AF_FILE = boards/stm32f412_af.csv
+LD_FILES = boards/GARATRONIC_PYMATE_CORE/f412_qspi.ld boards/common_bl.ld
+TEXT0_ADDR = 0x08000000
+TEXT0_SECTIONS = .isr_vector .text .data .ARM
+
+# MicroPython settings
+MICROPY_VFS_LFS2 = 1
+
+#I2C4 needs fmpi2c
+SRC_HAL += $(STM32LIB_HAL_BASE)/Src/stm32f4xx_hal_fmpi2c.c
+
+# wiznet5k module for ethernet support; valid values are:
+#   0    : no Wiznet support
+#   5500 : support for W5500 module
+MICROPY_PY_NETWORK_WIZNET5K = 5500
