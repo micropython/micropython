1a4df82620265ac7273d91952a7ffb91464b24ac
Owen <owen@easytarget.org>
2026-07-21 18:24:28 +0200

    esp32/machine_sdcard: Expose SDMMC host bus slot width in board config.
    This commit changes the default ESP32-P4 bus width from 1 to 4.
    
    MICROPY_HW_SDMMC_DEFAULT_WIDTH can be used for esp32 chips that support
    SDMMC host buses; it defaults to 1 except for the esp32-p4, where it
    defaults to 4.
    
    Signed-off-by: Owen <owen@easytarget.org>

--- ports/esp32/mpconfigport.h
+++ ports/esp32/mpconfigport.h
@@ -231,6 +231,13 @@
 #define MICROPY_HW_SDMMC_DEFAULT_SLOT       (1)
 #endif
 #endif
+#ifndef MICROPY_HW_SDMMC_DEFAULT_WIDTH
+#if CONFIG_IDF_TARGET_ESP32P4
+#define MICROPY_HW_SDMMC_DEFAULT_WIDTH      (4)
+#else
+#define MICROPY_HW_SDMMC_DEFAULT_WIDTH      (1)
+#endif
+#endif
 #define MICROPY_HW_SOFTSPI_MIN_DELAY        (0)
 #define MICROPY_HW_SOFTSPI_MAX_BAUDRATE     (esp_rom_get_cpu_ticks_per_us() * 1000000 / 200) // roughly
 #define MICROPY_PY_SSL                      (MICROPY_PY_NETWORK)
