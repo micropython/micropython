7750bf8488a4cbb535132898488fe754268d683e
Damien George <damien@micropython.org>
2026-08-05 14:27:53 +1000

    stm32/mphalport: Only build mp_hal_get_random if hardware RNG available.
    So that there's a build error if `mp_hal_get_random()` is used without
    being backed by a hardware RNG.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/stm32/mphalport.c
+++ ports/stm32/mphalport.c
@@ -222,6 +222,7 @@ void mp_hal_get_mac_ascii(int idx, size_t chr_off, size_t chr_len, char *dest) {
     }
 }
 
+#if MICROPY_HW_ENABLE_RNG
 void mp_hal_get_random(size_t n, uint8_t *buf) {
     uint32_t val = 0;
     for (int i = 0; i < n; i++) {
@@ -232,5 +233,6 @@ void mp_hal_get_random(size_t n, uint8_t *buf) {
         val >>= 8;
     }
 }
+#endif
 
 MP_REGISTER_ROOT_POINTER(struct _machine_uart_obj_t *pyb_stdio_uart);
