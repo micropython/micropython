370d4ed91bf60eec1b5b60d2625587efc299e9c0
Damien George <damien@micropython.org>
2026-08-05 14:35:42 +1000

    stm32/rng: Remove PRNG implementation of rng_get.
    Due to parent commits it's no longer needed.  Boards must enable
    `MICROPY_HW_ENABLE_RNG` to get features needing a random number.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/stm32/rng.c
+++ ports/stm32/rng.c
@@ -3,7 +3,7 @@
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2013-2018 Damien P. George
+ * Copyright (c) 2013-2026 Damien P. George
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -25,7 +25,6 @@
  */
 
 #include "py/mphal.h"
-#include "rtc.h"
 #include "rng.h"
 
 #if MICROPY_HW_ENABLE_RNG
@@ -76,41 +75,5 @@ static mp_obj_t pyb_rng_get(void) {
     return mp_obj_new_int(rng_get() >> 2);
 }
 MP_DEFINE_CONST_FUN_OBJ_0(pyb_rng_get_obj, pyb_rng_get);
-
-#else // MICROPY_HW_ENABLE_RNG
-
-// For MCUs that don't have an RNG we still need to provide a rng_get() function,
-// eg for lwIP and random.seed().  A pseudo-RNG is not really ideal but we go with
-// it for now, seeding with numbers which will be somewhat different each time.  We
-// don't want to use random's pRNG because then the user won't see a reproducible
-// random stream.
-
-// Yasmarang random number generator by Ilya Levin
-// http://www.literatecode.com/yasmarang
-static uint32_t pyb_rng_yasmarang(void) {
-    static bool seeded = false;
-    static uint32_t pad = 0, n = 0, d = 0;
-    static uint8_t dat = 0;
-
-    if (!seeded) {
-        seeded = true;
-        rtc_init_finalise();
-        pad = *(uint32_t *)MP_HAL_UNIQUE_ID_ADDRESS ^ SysTick->VAL;
-        n = RTC->TR;
-        d = RTC->SSR;
-    }
-
-    pad += dat + d * n;
-    pad = (pad << 3) + (pad >> 29);
-    n = pad | 2;
-    d ^= (pad << 31) + (pad >> 1);
-    dat ^= (char)pad ^ (d >> 8) ^ 1;
-
-    return pad ^ (d << 5) ^ (pad >> 18) ^ (dat << 1);
-}
-
-uint32_t rng_get(void) {
-    return pyb_rng_yasmarang();
-}
 
 #endif // MICROPY_HW_ENABLE_RNG
