3df833773a639c47384182f95e887c36e69bfdc1
NaveenChengappa-IFX <Naveen.Chandura@infineon.com>
2026-08-12 04:03:12 +0000

    psoc-edge/mpconfigport: Enable machine IRQ API.
    Signed-off-by: NaveenChengappa-IFX <Naveen.Chandura@infineon.com>

--- ports/psoc-edge/mpconfigport.h
+++ ports/psoc-edge/mpconfigport.h
@@ -68,6 +68,7 @@
 #define MICROPY_PY_MACHINE                      (1)
 #define MICROPY_PY_MACHINE_INCLUDEFILE          "ports/psoc-edge/modmachine.c"
 #define MICROPY_PY_MACHINE_BARE_METAL_FUNCS     (1)
+#define MICROPY_PY_MACHINE_DISABLE_IRQ_ENABLE_IRQ (1)
 #define MICROPY_PY_MACHINE_PULSE                (1)
 
 #define MICROPY_PY_MACHINE_PIN_MAKE_NEW         mp_pin_make_new
