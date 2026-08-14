b9ebb3e58a383c2bf8df608a0fef3caf590f36e6
Pavel Revak <pavelrevak@gmail.com>
2026-08-14 05:51:15 +0200

    esp32/machine_sdcard: Move SDMMC default slot/width defines to header.
    Move MICROPY_HW_SDMMC_DEFAULT_SLOT and MICROPY_HW_SDMMC_DEFAULT_WIDTH out
    of mpconfigport.h and into machine_sdcard.h, next to the SD card SPI pin
    defaults.  These defines are only used by machine_sdcard.c, so they
    belong with the rest of the SD card configuration.  The defaults are now
    the conservative slot 1, 1-bit values, leaving per-board wiring to each
    board to override.
    
    Signed-off-by: Pavel Revak <pavelrevak@gmail.com>

--- ports/esp32/mpconfigport.h
+++ ports/esp32/mpconfigport.h
@@ -224,20 +224,6 @@
 #ifndef MICROPY_HW_ENABLE_SDCARD
 #define MICROPY_HW_ENABLE_SDCARD            (1)
 #endif
-#ifndef MICROPY_HW_SDMMC_DEFAULT_SLOT
-#if CONFIG_IDF_TARGET_ESP32P4
-#define MICROPY_HW_SDMMC_DEFAULT_SLOT       (0)
-#else
-#define MICROPY_HW_SDMMC_DEFAULT_SLOT       (1)
-#endif
-#endif
-#ifndef MICROPY_HW_SDMMC_DEFAULT_WIDTH
-#if CONFIG_IDF_TARGET_ESP32P4
-#define MICROPY_HW_SDMMC_DEFAULT_WIDTH      (4)
-#else
-#define MICROPY_HW_SDMMC_DEFAULT_WIDTH      (1)
-#endif
-#endif
 #define MICROPY_HW_SOFTSPI_MIN_DELAY        (0)
 #define MICROPY_HW_SOFTSPI_MAX_BAUDRATE     (esp_rom_get_cpu_ticks_per_us() * 1000000 / 200) // roughly
 #define MICROPY_PY_SSL                      (MICROPY_PY_NETWORK)
