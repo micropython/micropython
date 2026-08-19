665ec04d1e627ae1811b4573f15a6f55423d716d
Angus Gratton <angus@redyak.com.au>
2026-08-19 16:29:50 +1000

    esp32: Make esp32.LDO objects static, release on soft reset.
    - Fixes the issue that an LDO remains enabled after soft reset, but
      the channel is still claimed in ESP-IDF so can't reconfigure or
      create a duplicate adjustable channel.
    - Allows creating multiple LDO objects for the same channel, provided
      the values are the same. Includes adjustable channels (previously
      not possible).
    - Release LDO channels deterministically from the soft reset sequence,
      close to the end so that other peripherals remain powered.
    
    This work was funded through GitHub Sponsors.
    
    Signed-off-by: Angus Gratton <angus@redyak.com.au>

--- ports/esp32/esp32_ldo.c
+++ ports/esp32/esp32_ldo.c
@@ -34,17 +34,35 @@
 
 #include "esp_ldo_regulator.h"
 
+// ESP-IDF doesn't yet contain a macro for the number of LDO channels available
+#if CONFIG_IDF_TARGET_ESP32P4
+#define LDO_CHANNELS 4
+#else
+#error "Please add this chip's LDO driver support"
+#endif
+
 // LDO (Low-Dropout Regulator) Driver
 //
 // This module provides access to the internal LDO voltage regulators,
 // which can supply adjustable voltages to internal peripherals or external components.
 
-typedef struct _esp32_ldo_obj_t {
+static mp_obj_t esp32_ldo_release(mp_obj_t self_in);
+
+typedef struct {
     mp_obj_base_t base;
-    esp_ldo_channel_handle_t ldo_channel_handle;
-    int chan_id; // Cached for display
-    int voltage_mv; // Cached for display
+    esp_ldo_channel_handle_t ldo_channel_handle;  // Non-NULL if channel is acquired
+    uint16_t voltage_mv;
+    bool adjustable;
 } esp32_ldo_obj_t;
+
+static esp32_ldo_obj_t ldos[LDO_CHANNELS];
+
+// As the only esp32_ldo_obj_t instances are part of ldo array, don't need to store channel id
+static mp_uint_t get_chan_id(const esp32_ldo_obj_t *ldo) {
+    mp_uint_t result = ldo - ldos;
+    assert(result < LDO_CHANNELS);
+    return result + 1;  // 1-based channel ID numbering scheme
+}
 
 static mp_obj_t esp32_ldo_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *all_args) {
     static const mp_arg_t allowed_args[] = {
@@ -55,28 +73,60 @@ static mp_obj_t esp32_ldo_make_new(const mp_obj_type_t *type, size_t n_args, siz
     mp_arg_val_t args[MP_ARRAY_SIZE(allowed_args)];
     mp_arg_parse_all_kw_array(n_args, n_kw, all_args, MP_ARRAY_SIZE(allowed_args), allowed_args, args);
 
-    esp_ldo_channel_config_t channel_config = {
-        .chan_id = args[0].u_int,
-        .voltage_mv = args[1].u_int,
-        .flags.adjustable = args[2].u_bool,
-    };
+    mp_uint_t chan_id = args[0].u_int;
+    mp_uint_t voltage_mv = args[1].u_int;
+    mp_uint_t adjustable = args[2].u_bool;
 
-    esp32_ldo_obj_t *self = mp_obj_malloc(esp32_ldo_obj_t, &esp32_ldo_type);
-    self->ldo_channel_handle = NULL;
-    self->chan_id = args[0].u_int;
-    self->voltage_mv = args[1].u_int;
+    if (chan_id < 1 || chan_id > LDO_CHANNELS) {
+        mp_raise_ValueError(MP_ERROR_TEXT("channel_id"));
+    }
 
-    check_esp_err(esp_ldo_acquire_channel(&channel_config, &self->ldo_channel_handle));
-    return MP_OBJ_FROM_PTR(self);
+    mp_uint_t chan_idx = chan_id - 1;  // Hardware & IDF API uses 1-based indexing for channel IDs, array is 0-indexed
+
+    esp32_ldo_obj_t *result = &ldos[chan_idx];
+    result->base.type = &esp32_ldo_type;
+
+    // If the channel is acquired already then settings have to match
+    if (result->ldo_channel_handle != NULL &&
+        (result->voltage_mv != voltage_mv ||
+         result->adjustable != adjustable)) {
+        mp_raise_msg(&mp_type_OSError, MP_ERROR_TEXT("LDO channel conflict"));
+    }
+
+    if (result->ldo_channel_handle == NULL) {
+        // Acquire the channel, unless we're returning an active LDO channel without changing it
+        esp_ldo_channel_config_t config = {
+            .chan_id = chan_id,
+            .voltage_mv = voltage_mv,
+            .flags.adjustable = adjustable,
+        };
+        check_esp_err(esp_ldo_acquire_channel(&config, &result->ldo_channel_handle));
+    }
+
+    result->voltage_mv = voltage_mv;
+    result->adjustable = adjustable;
+
+    return MP_OBJ_FROM_PTR(result);
+}
+
+void esp32_ldo_deinit_all(void) {
+    for (int i = 0; i < LDO_CHANNELS; i++) {
+        if (ldos[i].ldo_channel_handle != NULL) {
+            esp32_ldo_release(MP_OBJ_FROM_PTR(&ldos[i]));
+            ldos[i].ldo_channel_handle = NULL;
+        }
+    }
 }
 
 static void esp32_ldo_print(const mp_print_t *print, mp_obj_t self_in, mp_print_kind_t kind) {
     (void)kind;
     const esp32_ldo_obj_t *self = self_in;
+    mp_uint_t chan_id = get_chan_id(self);
     if (self->ldo_channel_handle != NULL) {
-        mp_printf(print, "LDO(channel=%d, voltage=%dmV)", self->chan_id, self->voltage_mv);
+        mp_printf(print, "LDO(channel=%d, voltage=%dmV, adjustable=%d)", chan_id, self->voltage_mv,
+            self->adjustable);
     } else {
-        mp_printf(print, "LDO(released)");
+        mp_printf(print, "LDO(channel=%d, released)", chan_id);
     }
 }
 
