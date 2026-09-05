cbf4ccdcdf2f8cc900549b390a5296894f558e63
Pablo Ventura <pablogventura@gmail.com>
2026-09-04 23:22:08 -0300

    shared/netutils: Factor mp_hal_get_mac_ascii out of ports.
    Move the identical helper from stm32/rp2/mimxrt/renesas-ra/alif into
    shared/netutils and assert the hex character slice stays within the
    12-character MAC representation.
    
    Fixes #19668.
    
    Signed-off-by: Pablo Ventura <pablogventura@gmail.com>

--- ports/mimxrt/mphalport.c
+++ ports/mimxrt/mphalport.c
@@ -136,12 +136,3 @@ void mp_hal_generate_laa_mac(int idx, uint8_t buf[6]) {
 MP_WEAK void mp_hal_get_mac(int idx, uint8_t buf[6]) {
     mp_hal_generate_laa_mac(idx, buf);
 }
-
-void mp_hal_get_mac_ascii(int idx, size_t chr_off, size_t chr_len, char *dest) {
-    static const char hexchr[16] = "0123456789ABCDEF";
-    uint8_t mac[6];
-    mp_hal_get_mac(idx, mac);
-    for (; chr_len; ++chr_off, --chr_len) {
-        *dest++ = hexchr[mac[chr_off >> 1] >> (4 * (1 - (chr_off & 1))) & 0xf];
-    }
-}
