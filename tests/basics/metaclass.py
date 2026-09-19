45c09faf4fdc6a2fa81abd42cfca709079e110a9
Jared Hancock <gravydish@gmail.com>
2026-09-19 18:28:03 -0500

    Drop attribute assignment in metaclass test.

--- tests/basics/metaclass.py
+++ tests/basics/metaclass.py
@@ -5,7 +5,6 @@ class MyMeta(type):
     def __init__(cls, name, bases, dct):
         print('MyMeta', '__init__')
         super().__init__(name, bases, dct)
-        cls.extra2 = 'my_meta_was_here'
 
     def __new__(cls, name, bases, dct):
         print('MyMeta', '__new__')
@@ -21,7 +20,6 @@ class B:
 class A(metaclass=MyMeta):
     def __init__(self):
         print('A', '__init__')
-        print(type(self).extra2 == 'my_meta_was_here')
 
 class C(metaclass=MyMeta):
     def __init__(self):
